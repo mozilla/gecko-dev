5fe9ea1e43e8aa01f455390cd6ae6ce513e7769a
Jeff Walden <jwalden@mit.edu>
2011-03-18 17:57:17 -0700

    Bug 639343 - Slight adjustment to how typed-array properties are handled in the tracer.  r=dvander
    --HG--
    extra : transplant_source : %5C%BE%7F%D8%86%14%11%F9%18%C3z%99%0D%3C%3C%81%16c%AAb

--- js/src/methodjit/PolyIC.cpp
+++ js/src/methodjit/PolyIC.cpp
@@ -2269,12 +2269,7 @@ GetElementIC::attachTypedArray(JSContext *cx, JSObject *obj, const Value &v, jsi
         LoadFromTypedArray(masm, tarray, addr, typeReg, objReg);
     }
 
-    Jump done1 = masm.jump();
-
-    outOfBounds.linkTo(masm.label(), &masm);
-    masm.loadValueAsComponents(UndefinedValue(), typeReg, objReg);
-
-    Jump done2 = masm.jump();
+    Jump done = masm.jump();
 
     PICLinker buffer(masm, *this);
     if (!buffer.init(cx))
@@ -2284,8 +2279,8 @@ GetElementIC::attachTypedArray(JSContext *cx, JSObject *obj, const Value &v, jsi
         return disable(cx, "code memory is out of range");
 
     buffer.link(claspGuard, slowPathStart);
-    buffer.link(done1, fastPathRejoin);
-    buffer.link(done2, fastPathRejoin);
+    buffer.link(outOfBounds, slowPathStart);
+    buffer.link(done, fastPathRejoin);
 
     CodeLocationLabel cs = buffer.finalizeCodeAddendum();
     JaegerSpew(JSpew_PICs, "generated getelem typed array stub at %p\n", cs.executableAddress());
