cce5fb62801b2dc2d43fed147961f47cb033ca97
Matt Brubeck <mbrubeck@mozilla.com>
2011-06-09 11:11:04 -0700

    Backed out changeset 08a50378d37c (landed on relbranch by accident). a=backout
    --HG--
    branch : GECKO50_2011060814_RELBRANCH

--- ipc/testshell/XPCShellEnvironment.cpp
+++ ipc/testshell/XPCShellEnvironment.cpp
@@ -297,11 +297,7 @@ Dump(JSContext *cx,
     str = JS_ValueToString(cx, JS_ARGV(cx, vp)[0]);
     if (!str)
         return JS_FALSE;
-    JSAutoByteString bytes(cx, str);
-    if (!bytes)
-      return JS_FALSE;
-
-    fputs(bytes.ptr(), stdout);
+    JS_FileEscapedString(stdout, str, 0);
     fflush(stdout);
     return JS_TRUE;
 }
