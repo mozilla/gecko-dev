e75fcabb374c1740aeee51e943c180769cccd19c
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2011-06-30 14:41:39 -0700

    Bug 661583: Hold onto the cache token if our response blob is backed by a cache file. r=sicking

--- content/base/public/nsDOMFile.h
+++ content/base/public/nsDOMFile.h
@@ -73,8 +73,10 @@ public:
   NS_DECL_NSIDOMFILE
   NS_DECL_NSIXHRSENDABLE
 
-  nsDOMFile(nsIFile *aFile, const nsAString& aContentType)
+  nsDOMFile(nsIFile *aFile, const nsAString& aContentType,
+            nsISupports *aCacheToken = nsnull)
     : mFile(aFile),
+      mCacheToken(aCacheToken),
       mContentType(aContentType),
       mIsFullFile(true)
   {}
@@ -87,6 +89,7 @@ public:
   nsDOMFile(const nsDOMFile* aOther, PRUint64 aStart, PRUint64 aLength,
             const nsAString& aContentType)
     : mFile(aOther->mFile),
+      mCacheToken(aOther->mCacheToken),
       mStart(aOther->mIsFullFile ? aStart :
                                    (aOther->mStart + aStart)),
       mLength(aLength),
@@ -116,6 +119,7 @@ public:
 
 protected:
   nsCOMPtr<nsIFile> mFile;
+  nsCOMPtr<nsISupports> mCacheToken;
 
   // start and length in 
   PRUint64 mStart;
