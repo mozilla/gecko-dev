6e29bc0c8aadb18d3d539779c9122942b4885b93
Ehsan Akhgari <ehsan@mozilla.com>
2011-07-26 09:05:15 -0400

    Bug 667512 - Do not return a bogus visual frame if we don't have any visual frames in reality; r=smontagu a=LegNeato
    --HG--
    extra : transplant_source : %8F%7E%18-B%A1%05%9A%93e%ED%CB%5De%19%8F%9FQ%1FC

--- layout/base/nsBidiPresUtils.cpp
+++ layout/base/nsBidiPresUtils.cpp
@@ -1172,7 +1172,7 @@ nsBidiPresUtils::GetFrameToRightOf(const nsIFrame*  aFrame,
   Reorder(isReordered, hasRTLFrames);
   PRInt32 count = mVisualFrames.Length();
 
-  if (aFrame == nsnull)
+  if (aFrame == nsnull && count)
     return mVisualFrames[0];
   
   for (PRInt32 i = 0; i < count - 1; i++) {
@@ -1196,7 +1196,7 @@ nsBidiPresUtils::GetFrameToLeftOf(const nsIFrame*  aFrame,
   Reorder(isReordered, hasRTLFrames);
   PRInt32 count = mVisualFrames.Length();
   
-  if (aFrame == nsnull)
+  if (aFrame == nsnull && count)
     return mVisualFrames[count-1];
   
   for (PRInt32 i = 1; i < count; i++) {
