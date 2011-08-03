722ed6289a90bd24b075821fa31a850e3bbc1590
Benoit Jacob <bjacob@mozilla.com>
2011-08-03 19:35:56 -0400

    Bug 673378 - Crash at nsHTMLCanvasElement::GetContext - r=roc, a=clegnitto
    When UpdateContext fails, we only reset mCurrentContext, and forgot to reset mCurrentContextId.

--- content/html/content/src/nsHTMLCanvasElement.cpp
+++ content/html/content/src/nsHTMLCanvasElement.cpp
@@ -571,18 +571,21 @@ nsHTMLCanvasElement::UpdateContext(nsIPropertyBag *aNewContextOptions)
   rv = mCurrentContext->SetIsOpaque(GetIsOpaque());
   if (NS_FAILED(rv)) {
     mCurrentContext = nsnull;
+    mCurrentContextId.AssignLiteral("");
     return rv;
   }
 
   rv = mCurrentContext->SetContextOptions(aNewContextOptions);
   if (NS_FAILED(rv)) {
     mCurrentContext = nsnull;
+    mCurrentContextId.AssignLiteral("");
     return rv;
   }
 
   rv = mCurrentContext->SetDimensions(sz.width, sz.height);
   if (NS_FAILED(rv)) {
     mCurrentContext = nsnull;
+    mCurrentContextId.AssignLiteral("");
     return rv;
   }
 
