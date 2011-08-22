2b7f5a67eae701e3765bdb1bbd0aeeb75e76a2da
Blake Kaplan <mrbkap@gmail.com>
2011-08-22 15:05:54 -0700

    Fix bug 653926. r=jst a=LegNeato

--- js/src/xpconnect/loader/mozJSSubScriptLoader.cpp
+++ js/src/xpconnect/loader/mozJSSubScriptLoader.cpp
@@ -213,13 +213,24 @@ mozJSSubScriptLoader::LoadSubScript (const PRUnichar * aURL
 
     // Remember an object out of the calling compartment so that we
     // can properly wrap the result later.
+    nsCOMPtr<nsIPrincipal> principal = mSystemPrincipal;
     JSObject *result_obj = target_obj;
     target_obj = JS_FindCompilationScope(cx, target_obj);
     if (!target_obj) return NS_ERROR_FAILURE;
-#ifdef DEBUG_rginda
     if (target_obj != result_obj)
+    {
+        nsCOMPtr<nsIScriptSecurityManager> secman =
+            do_GetService(NS_SCRIPTSECURITYMANAGER_CONTRACTID);
+        if (!secman)
+            return NS_ERROR_FAILURE;
+
+        rv = secman->GetObjectPrincipal(cx, target_obj, getter_AddRefs(principal));
+        NS_ENSURE_SUCCESS(rv, rv);
+
+#ifdef DEBUG_rginda
         fprintf (stderr, "Final global: %p\n", target_obj);
 #endif
+    }
 
     JSAutoEnterCompartment ac;
     if (!ac.enter(cx, target_obj))
@@ -356,7 +367,7 @@ mozJSSubScriptLoader::LoadSubScript (const PRUnichar * aURL
     /* we can't hold onto jsPrincipals as a module var because the
      * JSPRINCIPALS_DROP macro takes a JSContext, which we won't have in the
      * destructor */
-    rv = mSystemPrincipal->GetJSPrincipals(cx, &jsPrincipals);
+    rv = principal->GetJSPrincipals(cx, &jsPrincipals);
     if (NS_FAILED(rv) || !jsPrincipals)
     {
         errmsg = JS_NewStringCopyZ (cx, LOAD_ERROR_NOPRINCIPALS);
