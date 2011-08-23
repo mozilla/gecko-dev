abd3da11d54c35712ca64c2e59ac4ec9ebd5f20b
Jonathan Kew <jfkthame@gmail.com>
2011-08-23 09:39:34 +0100

    bug 665360 - hold reference to font family from loader. r=jdaggett a=jst

--- layout/style/nsFontFaceLoader.h
+++ layout/style/nsFontFaceLoader.h
@@ -140,6 +140,7 @@ public:
 
 private:
   nsRefPtr<gfxProxyFontEntry>  mFontEntry;
+  nsRefPtr<gfxFontFamily>      mFontFamily;
   nsCOMPtr<nsIURI>        mFontURI;
   nsRefPtr<nsUserFontSet> mFontSet;
   nsCOMPtr<nsIChannel>    mChannel;
