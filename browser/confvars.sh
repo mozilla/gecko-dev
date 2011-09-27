6c7b664d0dd6f4c87768a93e0559439a551eb59d
Boris Zbarsky <bzbarsky@mit.edu>
2011-09-27 13:20:42 -0400

    Change to aurora branding

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -49,5 +49,5 @@ MOZ_EXTENSIONS_DEFAULT=" gnomevfs"
 # MOZ_APP_DISPLAYNAME will be set by branding/configure.sh
 # Changing either of these values requires a clobber to ensure correct results,
 # because branding dependencies are broken.
-MOZ_BRANDING_DIRECTORY=browser/branding/nightly
+MOZ_BRANDING_DIRECTORY=browser/branding/aurora
 MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
