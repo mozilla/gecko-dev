ef5903136b5b06b318cbe94a3406ec92c8de98d6
Georg Kahest <georg.kahest@eestiinternet.ee>
2011-09-29 00:29:00 -0400

    Bug 664109 - "Requests for changes to the TLD whitelist for IDN, to add .ee" [a=akeybl ; implied r=gerv]

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -896,6 +896,7 @@ pref("network.IDN.whitelist.cn", true);
 pref("network.IDN.whitelist.de", true);
 pref("network.IDN.whitelist.dk", true);
 pref("network.IDN.whitelist.es", true);
+pref("network.IDN.whitelist.ee", true);
 pref("network.IDN.whitelist.fi", true);
 pref("network.IDN.whitelist.gr", true);
 pref("network.IDN.whitelist.hu", true);
