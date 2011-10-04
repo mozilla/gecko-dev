ec1559168222b41532d8b001baefe2b3c898ffbe
Matt Brubeck <mbrubeck@mozilla.com>
2011-10-04 10:36:46 -0700

    Bug 691054 - Back out bug 667980 (getNetworkLinkType) on Android because of scary permissions [r=dougt, a=LegNeato]

--- embedding/android/GeckoApp.java
+++ embedding/android/GeckoApp.java
@@ -61,7 +61,6 @@ import android.util.*;
 import android.net.*;
 import android.database.*;
 import android.provider.*;
-import android.telephony.*;
 
 abstract public class GeckoApp
     extends Activity
@@ -81,7 +80,6 @@ abstract public class GeckoApp
     public Handler mMainHandler;
     private IntentFilter mConnectivityFilter;
     private BroadcastReceiver mConnectivityReceiver;
-    private PhoneStateListener mPhoneStateListener;
 
     enum LaunchState {PreLaunch, Launching, WaitButton,
                       Launched, GeckoRunning, GeckoExiting};
@@ -242,8 +240,6 @@ abstract public class GeckoApp
         mConnectivityFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
         mConnectivityReceiver = new GeckoConnectivityReceiver();
 
-        mPhoneStateListener = new GeckoPhoneStateListener();
-
         if (!checkAndSetLaunchState(LaunchState.PreLaunch,
                                     LaunchState.Launching))
             return;
@@ -354,10 +350,6 @@ abstract public class GeckoApp
         super.onPause();
 
         unregisterReceiver(mConnectivityReceiver);
-
-        TelephonyManager tm = (TelephonyManager)
-            GeckoApp.mAppContext.getSystemService(Context.TELEPHONY_SERVICE);
-        tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
     }
 
     @Override
@@ -376,13 +368,6 @@ abstract public class GeckoApp
             onNewIntent(getIntent());
 
         registerReceiver(mConnectivityReceiver, mConnectivityFilter);
-
-        TelephonyManager tm = (TelephonyManager)
-            GeckoApp.mAppContext.getSystemService(Context.TELEPHONY_SERVICE);
-        tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_DATA_CONNECTION_STATE);
-
-        // Notify if network state changed since we paused
-        GeckoAppShell.onNetworkStateChange(true);
     }
 
     @Override
