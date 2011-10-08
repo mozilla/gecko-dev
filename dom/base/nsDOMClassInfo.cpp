ceaa4737b41a4653e6242dbccbf0539e77e64a53
Olli Pettay <Olli.Pettay@helsinki.fi>
2011-10-08 12:20:47 +0300

    Bug 681387: backout bug 654352 (caretPositionFromPoint), r=ehsan, a=blassey

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -96,7 +96,6 @@
 #include "nsIDOMGlobalPropertyInitializer.h"
 #include "mozilla/Preferences.h"
 #include "nsLocation.h"
-#include "nsIDOMCaretPosition.h"
 
 // Window scriptable helper includes
 #include "nsIDocShell.h"
@@ -658,9 +657,6 @@ static nsDOMClassInfoData sClassInfoData[] = {
   NS_DEFINE_CLASSINFO_DATA(Location, nsLocationSH,
                            (DOM_DEFAULT_SCRIPTABLE_FLAGS &
                             ~nsIXPCScriptable::ALLOW_PROP_MODS_TO_PROTOTYPE))
-
-  NS_DEFINE_CLASSINFO_DATA(CaretPosition, nsDOMGenericSH,
-                           DOM_DEFAULT_SCRIPTABLE_FLAGS)
 
   NS_DEFINE_CLASSINFO_DATA(Navigator, nsNavigatorSH,
                            DOM_DEFAULT_SCRIPTABLE_FLAGS |
@@ -2274,10 +2270,6 @@ nsDOMClassInfo::Init()
 
   DOM_CLASSINFO_MAP_BEGIN(Location, nsIDOMLocation)
     DOM_CLASSINFO_MAP_ENTRY(nsIDOMLocation)
-  DOM_CLASSINFO_MAP_END
-
-  DOM_CLASSINFO_MAP_BEGIN(CaretPosition, nsIDOMCaretPosition)
-    DOM_CLASSINFO_MAP_ENTRY(nsIDOMCaretPosition)
   DOM_CLASSINFO_MAP_END
 
   DOM_CLASSINFO_MAP_BEGIN(Navigator, nsIDOMNavigator)
