3cbe6564dc8400b7d2bbd0bd6d280856f8d50170
Brian Nicholson <bnicholson@mozilla.com>
2012-01-12 12:26:26 -0800

    Bug 709888 - Remove UA switcher. r=mfinkle a=akeybl

--- mobile/android/base/Tab.java
+++ mobile/android/base/Tab.java
@@ -57,7 +57,6 @@ import java.util.List;
 import org.mozilla.gecko.db.BrowserDB;
 
 public class Tab {
-    public static enum AgentMode { MOBILE, DESKTOP };
     private static final String LOGTAG = "GeckoTab";
     private static final int kThumbnailWidth = 120;
     private static final int kThumbnailHeight = 80;
@@ -80,7 +79,6 @@ public class Tab {
     private HashMap<String, DoorHanger> mDoorHangers;
     private long mFaviconLoadId;
     private CheckBookmarkTask mCheckBookmarkTask;
-    private AgentMode mAgentMode = AgentMode.MOBILE;
     private String mDocumentURI;
     private String mContentType;
 
@@ -482,13 +480,5 @@ public class Tab {
         protected void onPostExecute(Void unused) {
             setBookmark(false);
         }
-    }
-
-    public void setAgentMode(AgentMode agentMode) {
-        mAgentMode = agentMode;
-    }
-
-    public AgentMode getAgentMode() {
-        return mAgentMode;
     }
 }
