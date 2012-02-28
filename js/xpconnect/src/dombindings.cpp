d5930b89d36570608f34ce8ce30526759320ac87
Matt Brubeck <mbrubeck@mozilla.com>
2012-02-28 14:44:10 -0800

    Back out 47550f045496 (bug 691707) because of test failures. a=bustage

--- js/xpconnect/src/dombindings.cpp
+++ js/xpconnect/src/dombindings.cpp
@@ -770,9 +770,8 @@ template<class LC>
 bool
 ListBase<LC>::enumerate(JSContext *cx, JSObject *proxy, AutoIdVector &props)
 {
-    JSObject *proto = JS_GetPrototype(cx, proxy);
-    return getOwnPropertyNames(cx, proxy, props) &&
-           (!proto || js::GetPropertyNames(cx, proto, 0, &props));
+    // FIXME: enumerate proto as well
+    return getOwnPropertyNames(cx, proxy, props);
 }
 
 template<class LC>
