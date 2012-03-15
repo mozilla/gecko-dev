6bb3a0441851e2bb8c388c9132957fa7d2b1a08f
Wes Johnston <wjohnston@mozilla.com>
2012-03-15 15:33:39 -0700

    Bug 716515 - Send DOMAutocomplete event when autocomplete is selected. r=mleibovic

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -2893,6 +2893,11 @@ var FormAssistant = {
         // Remove focus from the textbox to avoid some bad IME interactions
         this._currentInputElement.blur();
         this._currentInputElement.value = aData;
+
+        let event = this._currentInputElement.ownerDocument.createEvent("Events");
+        event.initEvent("DOMAutoComplete", true, true);
+        this._currentInputElement.dispatchEvent(event);
+
         break;
 
       case "FormAssist:Hidden":
