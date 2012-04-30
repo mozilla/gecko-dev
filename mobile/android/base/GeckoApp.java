2a680f4ba9f0c2b1d605bb0eb44fd20aa6877342
Mark Finkle <mfinkle@mozilla.com>
2012-04-30 16:17:29 -0400

    Bug 750195 - java.lang.NullPointerException: at org.mozilla.gecko.GeckoApp.onDestroy(GeckoApp.java) r=bnicholson a=drivers

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -2110,7 +2110,8 @@ abstract public class GeckoApp
         GeckoAppShell.unregisterGeckoEventListener("Session:StatePurged", GeckoApp.mAppContext);
         GeckoAppShell.unregisterGeckoEventListener("Bookmark:Insert", GeckoApp.mAppContext);
 
-        mFavicons.close();
+        if (mFavicons != null)
+            mFavicons.close();
 
         if (SmsManager.getInstance() != null) {
             SmsManager.getInstance().stop();
@@ -2120,11 +2121,11 @@ abstract public class GeckoApp
 
         super.onDestroy();
 
-        mBatteryReceiver.unregisterFor(mAppContext);
+        if (mBatteryReceiver != null)
+            mBatteryReceiver.unregisterFor(mAppContext);
 
-        if (mAboutHomeContent != null) {
+        if (mAboutHomeContent != null)
             mAboutHomeContent.onDestroy();
-        }
 
         ((GeckoApplication) getApplication()).removeApplicationLifecycleCallbacks(this);
     }
