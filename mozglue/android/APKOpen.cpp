9876c9223d87cbf3a82a06b91ca082e51a44fcd8
Brad Lassey <blassey@mozilla.com>
2012-05-31 20:07:07 -0400

    bug 759146 - Looper throws uncaught exception on (some?) HTC Sense 4.0 phones (HTC Evo 4G LTE, HTC J, etc) r=snorp a=johnath

--- mozglue/android/APKOpen.cpp
+++ mozglue/android/APKOpen.cpp
@@ -351,6 +351,7 @@ SHELL_WRAPPER7(notifyGotNextMessage, jint, jstring, jstring, jstring, jlong, jin
 SHELL_WRAPPER3(notifyReadingMessageListFailed, jint, jint, jlong)
 SHELL_WRAPPER2(notifyFilePickerResult, jstring, jlong)
 SHELL_WRAPPER1_WITH_RETURN(getSurfaceBits, jobject, jobject)
+SHELL_WRAPPER1_WITH_RETURN(getNextMessageFromQueue, jobject, jobject)
 
 static void * xul_handle = NULL;
 static void * sqlite_handle = NULL;
@@ -768,6 +769,7 @@ loadGeckoLibs(const char *apkName)
   GETFUNC(notifyReadingMessageListFailed);
   GETFUNC(notifyFilePickerResult);
   GETFUNC(getSurfaceBits);
+  GETFUNC(getNextMessageFromQueue);
 #undef GETFUNC
   sStartupTimeline = (uint64_t *)__wrap_dlsym(xul_handle, "_ZN7mozilla15StartupTimeline16sStartupTimelineE");
   gettimeofday(&t1, 0);
