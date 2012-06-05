aa640c8d1eccdd47149a8f983cf809c444420225
Justin Lebar <justin.lebar@gmail.com>
2012-06-05 12:55:59 -0400

    Bug 757376. r=bz, a=akeybl

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -693,6 +693,8 @@ protected:
         nsRefPtr<nsDocShell> mDocShell;
     };
 
+    bool JustStartedNetworkLoad();
+
     // hash of session storages, keyed by domain
     nsInterfaceHashtable<nsCStringHashKey, nsIDOMStorage> mStorages;
 
