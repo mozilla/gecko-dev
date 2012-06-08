de24831216e98ab943fec580f446767c18211621
Panos Astithas <past@mozilla.com>
2012-06-08 10:01:19 +0300

    Bug 762403 - Disable the chrome debugger menu item until it's ready; r=dcamp,dtownsend a=akeybl

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1049,6 +1049,7 @@ pref("devtools.responsiveUI.enabled", true);
 
 // Enable the Debugger
 pref("devtools.debugger.enabled", true);
+pref("devtools.debugger.chrome-enabled", false);
 pref("devtools.debugger.remote-host", "localhost");
 pref("devtools.debugger.remote-autoconnect", false);
 pref("devtools.debugger.remote-connection-retries", 3);
