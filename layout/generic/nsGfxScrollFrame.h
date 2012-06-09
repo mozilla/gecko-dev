e70cc1f7779de314f0515c3ed34cc7de71da271c
Kartikaya Gupta <kgupta@mozilla.com>
2012-06-09 12:14:39 -0400

    Bug 705106 - Use the correct scroll port size when trying to scroll to an area. r=roc a=akeybl

--- layout/generic/nsGfxScrollFrame.h
+++ layout/generic/nsGfxScrollFrame.h
@@ -147,6 +147,7 @@ public:
   nsRect GetScrollRange() const;
   // Get the scroll range assuming the scrollport has size (aWidth, aHeight).
   nsRect GetScrollRange(nscoord aWidth, nscoord aHeight) const;
+  nsSize GetScrollPositionClampingScrollPortSize() const;
 protected:
   nsRect GetScrollRangeForClamping() const;
 
@@ -459,6 +460,9 @@ public:
   virtual nsRect GetScrollRange() const {
     return mInner.GetScrollRange();
   }
+  virtual nsSize GetScrollPositionClampingScrollPortSize() const {
+    return mInner.GetScrollPositionClampingScrollPortSize();
+  }
   virtual nsSize GetLineScrollAmount() const {
     return mInner.GetLineScrollAmount();
   }
@@ -700,6 +704,9 @@ public:
   }
   virtual nsRect GetScrollRange() const {
     return mInner.GetScrollRange();
+  }
+  virtual nsSize GetScrollPositionClampingScrollPortSize() const {
+    return mInner.GetScrollPositionClampingScrollPortSize();
   }
   virtual nsSize GetLineScrollAmount() const {
     return mInner.GetLineScrollAmount();
