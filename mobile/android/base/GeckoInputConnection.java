01adbd3bc1b9b325e8a9ae850f1df8c36a5e4b0e
Chris Peterson <cpeterson@mozilla.com>
2012-06-28 11:03:53 -0700

    Bug 758427 - Ignore KeyEvents with keyCodes greater than getMaxKeyCode(), such as Gingerbread Galaxy Note's bogus stylus events. r=blassey a=lsblakk

--- mobile/android/base/GeckoInputConnection.java
+++ mobile/android/base/GeckoInputConnection.java
@@ -897,6 +897,9 @@ public class GeckoInputConnection
                           + isPreIme + ")");
         }
 
+        if (keyCode > KeyEvent.getMaxKeyCode())
+            return false;
+
         clampSelection();
 
         switch (keyCode) {
@@ -957,6 +960,9 @@ public class GeckoInputConnection
             Log.d(LOGTAG, "IME: processKeyUp(keyCode=" + keyCode + ", event=" + event + ", "
                           + isPreIme + ")");
         }
+
+        if (keyCode > KeyEvent.getMaxKeyCode())
+            return false;
 
         switch (keyCode) {
             case KeyEvent.KEYCODE_BACK:
