139de38338abdaf8c46b77f50df9407fc41db7fb
Mounir Lamouri <mounir.lamouri@gmail.com>
2012-07-04 19:35:57 +0200

    Bug 764753 - Unlock screen orientation when the document is left without explicitly exiting fullscreen. r=smaug a=akeybl

--- dom/base/nsScreen.h
+++ dom/base/nsScreen.h
@@ -25,7 +25,7 @@ class nsScreen : public nsDOMEventTargetHelper
 public:
   static already_AddRefed<nsScreen> Create(nsPIDOMWindow* aWindow);
 
-  void Invalidate();
+  void Reset();
 
   NS_DECL_ISUPPORTS
   NS_DECL_NSIDOMSCREEN
