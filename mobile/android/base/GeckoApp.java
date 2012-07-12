e3c8c6cd4ce3adc0a8c7b542361e4f0d6373071c
Margaret Leibovic <margaret.leibovic@gmail.com>
2012-07-12 11:09:40 -0700

    Bug 770160 - Page title changes for a brief period when a new page is loaded. r=mfinkle a=akeybl

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -839,6 +839,7 @@ abstract public class GeckoApp
 
         tab.setContentType(contentType);
         tab.clearFavicon();
+        tab.updateTitle(null);
         tab.updateIdentityData(null);
         tab.setReaderEnabled(false);
         tab.setAllowZoom(true);
