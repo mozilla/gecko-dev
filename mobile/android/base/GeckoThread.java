64f69afa218c61c66f2b37ebdc8226de3e02a85f
Mike Hommey <mh+mozilla@glandium.org>
2012-08-01 09:51:12 +0200

    Backout changeset d945ae198516 (bug 760152). r=kats,a=lsblakk

--- mobile/android/base/GeckoThread.java
+++ mobile/android/base/GeckoThread.java
@@ -18,7 +18,6 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Date;
 import java.util.Locale;
-import java.util.concurrent.CountDownLatch;
 
 public class GeckoThread extends Thread {
     private static final String LOGTAG = "GeckoThread";
@@ -26,23 +25,13 @@ public class GeckoThread extends Thread {
     Intent mIntent;
     String mUri;
     int mRestoreMode;
-    CountDownLatch mStartSignal;
 
-    GeckoThread() {
-        mStartSignal = new CountDownLatch(1);
-        setName("Gecko");
-    }
-
-    public void init(Intent intent, String uri, int restoreMode) {
+    GeckoThread(Intent intent, String uri, int restoreMode) {
         mIntent = intent;
         mUri = uri;
         mRestoreMode = restoreMode;
-    }
 
-    public void reallyStart() {
-        mStartSignal.countDown();
-        if (getState() == Thread.State.NEW)
-            start();
+        setName("Gecko");
     }
 
     public void run() {
@@ -59,11 +48,6 @@ public class GeckoThread extends Thread {
         GeckoAppShell.loadGeckoLibs(resourcePath);
 
         Locale.setDefault(locale);
-
-        try {
-            mStartSignal.await();
-        } catch (Exception e) { }
-
         Resources res = app.getBaseContext().getResources();
         Configuration config = res.getConfiguration();
         config.locale = locale;
