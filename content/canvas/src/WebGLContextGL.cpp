057a80dcee7c78ee4cc90162c62028a1b4068489
Benoit Jacob <bjacob@mozilla.com>
2012-08-02 17:28:02 -0400

    Bug 749678 - re-add missing MakeCurrent call in certain WebGL uniform setters - r=jgilbert, a=lsblakk

--- content/canvas/src/WebGLContextGL.cpp
+++ content/canvas/src/WebGLContextGL.cpp
@@ -4487,6 +4487,7 @@ WebGLContext::name##_base(WebGLUniformLocation *location_object,                
     }                                                                           \
                                                                                 \
     uint32_t numElementsToUpload = NS_MIN(info.arraySize, arrayLength/expectedElemSize);     \
+    MakeContextCurrent();                                                       \
     gl->f##name(location, numElementsToUpload, data);    \
 }
 
