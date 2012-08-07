1acee7208093bf8fa1de5ed0173e45654818859d
Margaret Leibovic <margaret.leibovic@gmail.com>
2012-08-07 11:20:11 -0700

    Bug 778263 - Clean up SelectionHandler data when we bail in startSelection. r=mfinkle a=lsblakk

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -1610,13 +1610,14 @@ var SelectionHandler = {
       selectionController.wordMove(!this._isRTL, true);
     } catch(e) {
       // If we couldn't select the word at the given point, bail
-      Cu.reportError("Error selecting word: " + e);
+      this._cleanUp();
       return;
     }
 
     // If there isn't an appropriate selection, bail
     if (!selection.rangeCount || !selection.getRangeAt(0) || !selection.toString().trim().length) {
       selection.collapseToStart();
+      this._cleanUp();
       return;
     }
 
@@ -1752,12 +1753,16 @@ var SelectionHandler = {
       }
     }
 
+    this._cleanUp();
+
+    return selectedText;
+  },
+
+  _cleanUp: function sh_cleanUp() {
     this._view.removeEventListener("pagehide", this, false);
     this._view = null;
     this._isRTL = false;
     this.cache = null;
-
-    return selectedText;
   },
 
   _getViewOffset: function sh_getViewOffset() {
