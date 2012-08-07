72002ddb9bc46320670ffcf4b476da455ee976b9
Michael Comella <michael.l.comella@gmail.com>
2012-08-06 20:27:04 -0400

    Bug 780367 - AboutHomeContent should no longer have two views. r=sriram, a=lsblakk

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -298,6 +298,9 @@ abstract public class BrowserApp extends GeckoApp
 
         invalidateOptionsMenu();
         mTabsPanel.refresh();
+
+        if (mAboutHomeContent != null)
+            mAboutHomeContent.refresh();
     }
 
     @Override
