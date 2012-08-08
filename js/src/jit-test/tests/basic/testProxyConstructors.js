a7f97d31e366e06bd79fa2473765f7f7807d992d
Boris Zbarsky <bzbarsky@mit.edu>
2012-08-08 11:41:46 -0400

    Bug 780542.  Make construction via a function proxy with an object in the constructor slot actually construct instead of just calling.  r=ejpbruel a=lsblakk

--- js/src/jit-test/tests/basic/testProxyConstructors.js
+++ js/src/jit-test/tests/basic/testProxyConstructors.js
@@ -1,17 +1,25 @@
 // |jit-test| error: ExitCleanly
 
-// proxies can return primitives
-assertEq(new (Proxy.createFunction({}, function(){}, function(){})), undefined);
-x = Proxy.createFunction((function () {}), Uint16Array, wrap)
-new(wrap(x))
+assertEq((new (Proxy.createFunction({},
+                                    function(){ this.x = 1 },
+                                    function(){ this.x = 2 }))).x, 2);
+try {
+    x = Proxy.createFunction((function () {}), Uint16Array, wrap)
+    new(wrap(x))
+    throw "Should not be reached"
+}
+catch (e) {
+    assertEq(String(e.message).indexOf('is not a constructor') === -1, false);
+}
 // proxies can return the callee
 var x = Proxy.createFunction({}, function (q) { return q; });
 assertEq(new x(x), x);
 try {
     var x = (Proxy.createFunction({}, "".indexOf));
     new x;
+    throw "Should not be reached"
 }
 catch (e) {
-    assertEq(e.message, 'new x is not a constructor');
+    assertEq(String(e.message).indexOf('is not a constructor') === -1, false);
 }
 throw "ExitCleanly"
