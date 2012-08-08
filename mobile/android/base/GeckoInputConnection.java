52fa3a34790922bb7d9e078a72eff3bc25214b4d
Chris Peterson <cpeterson@mozilla.com>
2012-08-08 16:16:59 -0700

    Bug 781020 - Remove IME's composing spans when Gecko resets the composition state. r=blassey a=lsblakk

--- mobile/android/base/GeckoInputConnection.java
+++ mobile/android/base/GeckoInputConnection.java
@@ -310,13 +310,6 @@ public class GeckoInputConnection
         // setComposingText() places the given text into the editable, replacing any existing
         // composing text. This method will likely be called multiple times while we are composing
         // text.
-
-        // If the replacement composition string is empty and we have no active composition string
-        // to replace, then just ignore the empty string. Some VKBs, such as TouchPal Keyboard,
-        // send us empty strings at inopportune times, deleting committed text. See bug 768106.
-        if (text.length() == 0 && !hasCompositionString())
-            return true;
-
         clampSelection();
         return super.setComposingText(text, newCursorPosition);
     }
@@ -568,6 +561,7 @@ public class GeckoInputConnection
     }
 
     protected void resetCompositionState() {
+        removeComposingSpans(mEditable);
         mCompositionStart = NO_COMPOSITION_STRING;
         mBatchMode = false;
         mUpdateRequest = null;
