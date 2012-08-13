07edc46f09372186b7e4287224a3c0486653868c
Patrick McManus <mcmanus@ducksong.com>
2012-08-13 08:44:27 -0400

    bug 780522 - test case r=honzab a=lsblakk

---
+++ netwerk/test/unit/test_invalidport.js
@@ -0,0 +1,39 @@
+// This is essentially a crashtest for accessing an out of range port
+// Perform the async open several times in order to induce exponential
+// scheduling behavior bugs.
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const CC = Components.Constructor;
+
+var counter = 0;
+
+var listener = {
+  onStartRequest: function test_onStartR(request, ctx) {
+  },
+
+  onDataAvailable: function test_ODA() {
+    do_throw("Should not get any data!");
+  },
+
+  onStopRequest: function test_onStopR(request, ctx, status) {
+    if (counter++ == 10)
+      do_test_finished();
+    else
+      execute_test();
+  },
+};
+
+function run_test() {
+  do_test_pending();
+  execute_test();
+}
+
+function execute_test() {
+  var ios = Cc["@mozilla.org/network/io-service;1"].
+                       getService(Ci.nsIIOService);
+  var chan = ios.newChannel("http://localhost:75000", "", null);
+  chan.QueryInterface(Ci.nsIHttpChannel);
+  chan.asyncOpen(listener, null);
+}
+
