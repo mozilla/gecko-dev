9f7e1c764c141e2f2cbaee34d94ad374fc1d0880
John Schoenick <jschoenick@mozilla.com>
2012-08-20 16:16:02 -0700

    Bug 548133 - Remove special handling for object 'pluginurl' param. r=dolske, r=josh, a=akeybl

--- dom/plugins/base/nsPluginInstanceOwner.cpp
+++ dom/plugins/base/nsPluginInstanceOwner.cpp
@@ -3843,8 +3843,7 @@ nsObjectFrame* nsPluginInstanceOwner::GetFrame()
 // |value| for certain inputs of |name|
 void nsPluginInstanceOwner::FixUpURLS(const nsString &name, nsAString &value)
 {
-  if (name.LowerCaseEqualsLiteral("pluginurl") ||
-      name.LowerCaseEqualsLiteral("pluginspage")) {        
+  if (name.LowerCaseEqualsLiteral("pluginspage")) {
     nsCOMPtr<nsIURI> baseURI = mContent->GetBaseURI();
     nsAutoString newURL;
     NS_MakeAbsoluteURI(newURL, value, baseURI);
