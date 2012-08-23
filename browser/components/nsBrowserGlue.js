0469e9f49e04dde199c87f0a991b92bfee503724
Tim Taubert <ttaubert@mozilla.com>
2012-08-23 14:56:42 -0700

    Backed out changeset 3d01c3e9573c (re-landing bug 744388); a=akeybl

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -28,6 +28,9 @@ XPCOMUtils.defineLazyModuleGetter(this, "BookmarkHTMLUtils",
 XPCOMUtils.defineLazyModuleGetter(this, "webappsUI", 
                                   "resource:///modules/webappsUI.jsm");
 
+XPCOMUtils.defineLazyModuleGetter(this, "PageThumbs",
+                                  "resource:///modules/PageThumbs.jsm");
+
 XPCOMUtils.defineLazyModuleGetter(this, "PdfJs",
                                   "resource://pdf.js/PdfJs.jsm");
 
@@ -332,6 +335,8 @@ BrowserGlue.prototype = {
     // Initialize webapps UI
     webappsUI.init();
 
+    PageThumbs.init();
+
     PdfJs.init();
 
     Services.obs.notifyObservers(null, "browser-ui-startup-complete", "");
@@ -355,6 +360,7 @@ BrowserGlue.prototype = {
   _onProfileShutdown: function BG__onProfileShutdown() {
     this._shutdownPlaces();
     this._sanitizer.onShutdown();
+    PageThumbs.uninit();
   },
 
   // All initial windows have opened.
