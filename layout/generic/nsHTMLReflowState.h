13db0004e3f369ce8d6011284b7f79bd1ea6696a
Ehsan Akhgari <ehsan@mozilla.com>
2012-08-27 20:40:26 -0400

    Backout bug 157681 in order to fix bug 775350 on Beta. a=akeybl

--- layout/generic/nsHTMLReflowState.h
+++ layout/generic/nsHTMLReflowState.h
@@ -458,13 +458,6 @@ public:
     return mDiscoveredClearance && *mDiscoveredClearance;
   }
 
-  // Compute the offsets for a relative position element
-  static void ComputeRelativeOffsets(PRUint8 aCBDirection,
-                                     nsIFrame* aFrame,
-                                     nscoord aContainingBlockWidth,
-                                     nscoord aContainingBlockHeight,
-                                     nsMargin& aComputedOffsets);
-
 #ifdef DEBUG
   // Reflow trace methods.  Defined in nsFrame.cpp so they have access
   // to the display-reflow infrastructure.
@@ -518,6 +511,11 @@ protected:
                                nscoord aContainingBlockWidth,
                                nscoord aContainingBlockHeight,
                                nsIAtom* aFrameType);
+
+  void ComputeRelativeOffsets(const nsHTMLReflowState* cbrs,
+                              nscoord aContainingBlockWidth,
+                              nscoord aContainingBlockHeight,
+                              nsPresContext* aPresContext);
 
   // Calculates the computed values for the 'min-Width', 'max-Width',
   // 'min-Height', and 'max-Height' properties, and stores them in the assorted
