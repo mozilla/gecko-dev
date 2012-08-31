b5fdf6f4ed5bfa72bd9b1941566361b692a446d5
Ehsan Akhgari <ehsan@mozilla.com>
2012-08-30 21:32:02 -0400

    Bug 785720 - Move the MathML token frame leading/trailing whitespace trimming logic to nsTextFrame; r=roc,a=lsblakk

--- layout/mathml/nsMathMLmoFrame.cpp
+++ layout/mathml/nsMathMLmoFrame.cpp
@@ -122,6 +122,7 @@ nsMathMLmoFrame::ProcessTextData()
 
   nsAutoString data;
   nsContentUtils::GetNodeTextContent(mContent, false, data);
+  data.CompressWhitespace();
   PRInt32 length = data.Length();
   PRUnichar ch = (length == 0) ? kNullCh : data[0];
 
