2da6942a9137043d460a54c58fa865b87fd85c65
Ehsan Akhgari <ehsan@mozilla.com>
2012-09-11 14:11:54 -0400

    Bug 789422 - Part 1: Remove UpdateStatusText because it does the exact same thing as UpdateStatusFile(const char*); r=rstrong a=akeybl
    --HG--
    extra : transplant_source : %87%00%C0%C5%98%02%8E%EC%3B%15%85%13/%3F%9D%E0%15%B5%04%1C

--- toolkit/mozapps/update/updater/updater.cpp
+++ toolkit/mozapps/update/updater/updater.cpp
@@ -1649,20 +1649,21 @@ LaunchCallbackApp(const NS_tchar *workingDir,
 #endif
 }
 
-static void
-WriteStatusText(const char* text)
+static bool
+WriteStatusFile(const char* aStatus)
 {
-  // This is how we communicate our completion status to the main application.
-
   NS_tchar filename[MAXPATHLEN];
   NS_tsnprintf(filename, sizeof(filename)/sizeof(filename[0]),
                NS_T("%s/update.status"), gSourcePath);
 
   AutoFile file = NS_tfopen(filename, NS_T("wb+"));
   if (file == NULL)
-    return;
+    return false;
 
-  fwrite(text, strlen(text), 1, file);
+  if (fwrite(aStatus, strlen(aStatus), 1, file) != 1)
+    return false;
+
+  return true;
 }
 
 static void
@@ -1682,24 +1683,7 @@ WriteStatusFile(int status)
     text = buf;
   }
 
-  WriteStatusText(text);
-}
-
-static bool
-WriteStatusFile(const char* aStatus)
-{
-  NS_tchar filename[MAXPATHLEN];
-  NS_tsnprintf(filename, sizeof(filename)/sizeof(filename[0]),
-               NS_T("%s/update.status"), gSourcePath);
-
-  AutoFile file = NS_tfopen(filename, NS_T("wb+"));
-  if (file == NULL)
-    return false;
-
-  if (fwrite(aStatus, strlen(aStatus), 1, file) != 1)
-    return false;
-
-  return true;
+  WriteStatusFile(text);
 }
 
 #ifdef MOZ_MAINTENANCE_SERVICE
@@ -2135,7 +2119,7 @@ UpdateThreadFunc(void *param)
                    installDir);
 
       ensure_remove_recursive(stageDir);
-      WriteStatusText(sUsingService ? "pending-service" : "pending");
+      WriteStatusFile(sUsingService ? "pending-service" : "pending");
       putenv("MOZ_PROCESS_UPDATES="); // We need to use -process-updates again in the tests
       reportRealResults = false; // pretend success
     }
