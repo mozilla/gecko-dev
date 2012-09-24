7391af7e880117a585ea08fafa4f01ec28c3463a
Robert O'Callahan <robert@ocallahan.org>
2012-09-24 16:30:33 +1200

    Bug 791616. Part 1: Add nsIScrollableFrame::GetPerceivedScrollingDirections to consolidate logic for whether UI actions should be allowed to scroll in a given direction. r=mats,a=lsblakk

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -4141,3 +4141,18 @@ nsGfxScrollFrameInner::FireScrolledAreaEvent()
     nsEventDispatcher::Dispatch(doc, prescontext, &event, nullptr);
   }
 }
+
+uint32_t
+nsIScrollableFrame::GetPerceivedScrollingDirections() const
+{
+  nscoord oneDevPixel = GetScrolledFrame()->PresContext()->AppUnitsPerDevPixel();
+  uint32_t directions = GetScrollbarVisibility();
+  nsRect scrollRange = GetScrollRange();
+  if (scrollRange.width >= oneDevPixel) {
+    directions |= HORIZONTAL;
+  }
+  if (scrollRange.height >= oneDevPixel) {
+    directions |= VERTICAL;
+  }
+  return directions;
+}
