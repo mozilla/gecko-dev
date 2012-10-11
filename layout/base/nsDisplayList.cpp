ecc03d054b583faec01938afaab1588600578158
Chris Jones <jones.chris.g@gmail.com>
2012-10-11 16:55:42 -0700

    Bug 799768: Only bother computing an invalid rect if it affects composition and enable omtc for X11 b2g builds. r=mattwoodrow a=blocking-basecamp

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -1053,7 +1053,7 @@ void nsDisplayList::PaintForFrame(nsDisplayListBuilder* aBuilder,
   NotifySubDocInvalidationFunc computeInvalidFunc =
     presContext->MayHavePaintEventListenerInSubDocument() ? nsPresContext::NotifySubDocInvalidation : 0;
   bool computeInvalidRect = (computeInvalidFunc ||
-                             (layerManager->GetBackendType() == LAYERS_BASIC)) &&
+                             !layerManager->IsCompositingCheap()) &&
                             widgetTransaction;
 
   nsAutoPtr<LayerProperties> props(computeInvalidRect ? 
