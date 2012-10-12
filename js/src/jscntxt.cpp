138a75ce7e77d3873b206b12bb8b2feca7d880e0
Justin Lebar <justin.lebar@gmail.com>
2012-10-12 10:26:06 -0400

    Bug 798510 - Part 1: Use mincore to correctly calculate js::StackSpace::sizeOfCommitted() on *nix. r=luke,njn a=akeybl
    Previously, we assumed all memory was committed, thus vastly overstating the memory used by our stack.

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -138,7 +138,7 @@ JSRuntime::sizeOfIncludingThis(JSMallocSizeOfFun mallocSizeOf, RuntimeSizes *rtS
         rtSizes->unusedCode = 0;
     }
 
-    rtSizes->stackCommitted = stackSpace.sizeOfCommitted();
+    rtSizes->stack = stackSpace.sizeOf();
 
     rtSizes->gcMarker = gcMarker.sizeOfExcludingThis(mallocSizeOf);
 
@@ -162,7 +162,7 @@ JSRuntime::sizeOfExplicitNonHeap()
 
     size_t jaegerCode, ionCode, regexpCode, unusedCode;
     execAlloc_->sizeOfCode(&jaegerCode, &ionCode, &regexpCode, &unusedCode);
-    return jaegerCode + ionCode + regexpCode + unusedCode + stackSpace.sizeOfCommitted();
+    return jaegerCode + ionCode + regexpCode + unusedCode + stackSpace.sizeOf();
 }
 
 void
