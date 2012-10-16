56b2fa669f4e966597ba66abca96790e26b8d04b
Matt Woodrow <mwoodrow@mozilla.com>
2012-10-16 14:10:48 +1300

    Bug 800198 - Make mContainerReferenceFrame the reference frame for the items inside the container, not the container itself. r=roc a=bajaj

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -249,7 +249,7 @@ public:
   {
     nsPresContext* presContext = aContainerFrame->PresContext();
     mAppUnitsPerDevPixel = presContext->AppUnitsPerDevPixel();
-    mContainerReferenceFrame = aContainerItem ? aContainerItem->ReferenceFrame() :
+    mContainerReferenceFrame = aContainerItem ? aContainerItem->ReferenceFrameForChildren() :
       mBuilder->FindReferenceFrameFor(mContainerFrame);
     // When AllowResidualTranslation is false, display items will be drawn
     // scaled with a translation by integer pixels, so we know how the snapping
