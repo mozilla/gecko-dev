f0248216c334d2aed5622667345d19656dc219dd
Jonathan Griffin <jgriffin@mozilla.com>
2012-10-17 16:46:04 -0700

    Bug 802877 - Delay a bit before setting default emulator geolocation, DONTBUILD, a=NPOTB

--- testing/marionette/client/marionette/emulator.py
+++ testing/marionette/client/marionette/emulator.py
@@ -346,6 +346,9 @@ waitFor(
             online, offline = self._get_adb_devices()
         self.port = int(list(online - original_online)[0])
         self._emulator_launched = True
+
+        # bug 802877
+        time.sleep(10)
         self.geo.set_default_location()
 
         if self.logcat_dir:
