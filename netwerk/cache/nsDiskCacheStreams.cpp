92edbc88ec1f4bc67342e87136d80d8b5030bf81
Michal Novotny <michal.novotny@gmail.com>
2012-10-17 13:59:51 +0200

    Bug 771832 - File not found error for HTTP URL, can be fixed by a forced reload, r=hurley, a=lsblakk

--- netwerk/cache/nsDiskCacheStreams.cpp
+++ netwerk/cache/nsDiskCacheStreams.cpp
@@ -531,6 +531,10 @@ nsDiskCacheStreamIO::Flush()
                 NS_WARNING("cacheMap->DeleteStorage() failed.");
                 return rv;
             }
+            if (mFD) {
+                PR_Close(mFD);
+                mFD = nullptr;
+            }
         }
 
         // flush buffer to block files
