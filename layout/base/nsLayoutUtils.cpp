08bdb30d7544db27315339376d3cc0a8485186f9
Bas Schouten <bschouten@mozilla.com>
2012-10-23 00:37:08 +0200

    Bug 803949 - Do not draw anything for zero size images. r=roc, a=lsblakk

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -3628,7 +3628,7 @@ ComputeSnappedImageDrawingParameters(gfxContext*     aCtx,
                                      const nsIntSize aImageSize)
 
 {
-  if (aDest.IsEmpty() || aFill.IsEmpty())
+  if (aDest.IsEmpty() || aFill.IsEmpty() || !aImageSize.width || !aImageSize.height)
     return SnappedImageDrawingParameters();
 
   gfxRect devPixelDest =
