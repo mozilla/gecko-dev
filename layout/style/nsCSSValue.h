5468b81f7d27976c2eb178c01f48c50e9312f733
Mats Palmgren <matspal@gmail.com>
2012-10-24 02:43:56 +0200

    Bug 798853, style system.  r=roc,dbaron a=lsblakk

--- layout/style/nsCSSValue.h
+++ layout/style/nsCSSValue.h
@@ -21,6 +21,7 @@
 #include "nsStringBuffer.h"
 #include "nsTArray.h"
 #include "nsStyleConsts.h"
+#include "mozilla/FloatingPoint.h"
 
 class imgIRequest;
 class nsIDocument;
@@ -348,6 +349,7 @@ public:
   float GetFloatValue() const
   {
     NS_ABORT_IF_FALSE(eCSSUnit_Number <= mUnit, "not a float value");
+    MOZ_ASSERT(!MOZ_DOUBLE_IS_NaN(mValue.mFloat));
     return mValue.mFloat;
   }
 
