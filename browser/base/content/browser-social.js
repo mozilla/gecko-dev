036dcc2f09aee5be13623766699ff4236b748cc8
Alfred Kayser <alfredkayser@gmail.com>
2012-10-24 13:46:05 +1100

    Bug 804068 - Set social sharebutton image via .src, plus css tweaks to ensure it is sized correctly. r=jaws, a=gavin

--- browser/base/content/browser-social.js
+++ browser/base/content/browser-social.js
@@ -617,7 +617,7 @@ let SocialShareButton = {
       shareButton.setAttribute("tooltiptext", this.promptMessages['shareTooltip']);
       imageURL = this.promptImages["share"]
     }
-    shareButton.style.backgroundImage = 'url("' + encodeURI(imageURL) + '")';
+    shareButton.src = imageURL;
   }
 };
 
