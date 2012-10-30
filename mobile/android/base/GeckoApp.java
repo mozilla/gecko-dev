3f99fbd2706707a0c6aacc9aaa769076f8d5a09e
Richard Newman <rnewman@mozilla.com>
2012-10-30 13:27:33 -0700

    Bug 806634 - Part 2: record browser launch after GeckoApp startup. r=mfinkle, a=bajaj

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -5,6 +5,7 @@
 
 package org.mozilla.gecko;
 
+import org.mozilla.gecko.background.announcements.AnnouncementsBroadcastService;
 import org.mozilla.gecko.db.BrowserDB;
 import org.mozilla.gecko.gfx.Layer;
 import org.mozilla.gecko.gfx.LayerView;
@@ -1729,11 +1730,16 @@ abstract public class GeckoApp
                 // no hurry in starting this.
                 checkMigrateSync();
 
+                // Record our launch time for the announcements service
+                // to use in assessing inactivity.
+                final Context context = GeckoApp.mAppContext;
+                AnnouncementsBroadcastService.recordLastLaunch(context);
+
                 // Kick off our background service to fetch product announcements.
                 // We do this by invoking the broadcast receiver, which uses the
                 // system alarm infrastructure to perform tasks at intervals.
                 Log.d(LOGTAG, "Broadcasting announcements pref.");
-                GeckoPreferences.broadcastAnnouncementsPref(GeckoApp.mAppContext);
+                GeckoPreferences.broadcastAnnouncementsPref(context);
 
                 /*
                   XXXX see bug 635342
