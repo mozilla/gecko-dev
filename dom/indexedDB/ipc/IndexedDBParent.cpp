d69117a579143fff13e1bc66011673552463c777
Jonas Sicking <jonas@sicking.cc>
2012-10-30 07:07:09 +0100

    Bug 786688 - Part 1: Add chrome only openForPrincipal() and deleteForPrincipal(). r=bent a=bajaj

--- dom/indexedDB/ipc/IndexedDBParent.cpp
+++ dom/indexedDB/ipc/IndexedDBParent.cpp
@@ -162,8 +162,8 @@ IndexedDBParent::RecvPIndexedDBDatabaseConstructor(
 
   nsRefPtr<IDBOpenDBRequest> request;
   nsresult rv =
-    mFactory->OpenCommon(aName, aVersion, false, nullptr,
-                         getter_AddRefs(request));
+    mFactory->OpenCommon(aName, aVersion, mFactory->GetASCIIOrigin(), false,
+                         nullptr, getter_AddRefs(request));
   NS_ENSURE_SUCCESS(rv, false);
 
   IndexedDBDatabaseParent* actor =
@@ -199,7 +199,8 @@ IndexedDBParent::RecvPIndexedDBDeleteDatabaseRequestConstructor(
   nsRefPtr<IDBOpenDBRequest> request;
 
   nsresult rv =
-    mFactory->OpenCommon(aName, 0, true, nullptr, getter_AddRefs(request));
+    mFactory->OpenCommon(aName, 0, mFactory->GetASCIIOrigin(), true, nullptr,
+                         getter_AddRefs(request));
   NS_ENSURE_SUCCESS(rv, false);
 
   rv = actor->SetOpenRequest(request);
