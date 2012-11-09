4d0da3caec24e2452de5c768827b83ce5d759fd3
Dave Hylands <dhylands@mozilla.com>
2012-11-09 10:23:18 -0800

    Bug 783630 - Fix bustage on inbound. a=sicking

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -348,7 +348,7 @@ private:
     friend class CrashReporterParent;
 
     nsRefPtr<nsConsoleService>  mConsoleService;
-    already_AddRefed<nsConsoleService> GetConsoleService();
+    nsConsoleService* GetConsoleService();
 };
 
 } // namespace dom
