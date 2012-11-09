e7edc49a6c40662b533ae0d0e55f6443a6e31620
Bobby Holley <bobbyholley@gmail.com>
2012-11-09 10:59:22 -0800

    Bug 806751 - Pile more garbage into GetWrappedNativeOfJSObject. r=mrbkap a=bbajaj
    --HG--
    extra : rebase_source : 9d290c7058dd4a1984aff94ef84376913947189e
    
    --HG--
    extra : rebase_source : bba9ff71fbcbb8a54ecb377e0744413522968ccf

--- js/xpconnect/src/XPCWrappedNative.cpp
+++ js/xpconnect/src/XPCWrappedNative.cpp
@@ -26,6 +26,7 @@
 #include "mozilla/Util.h"
 
 using namespace xpc;
+using namespace mozilla;
 
 bool
 xpc_OkToHandOutWrapper(nsWrapperCache *cache)
@@ -1804,6 +1805,15 @@ XPCWrappedNative::GetWrappedNativeOfJSObject(JSContext* cx,
     XPCWrappedNativeProto* proto = nullptr;
     nsIClassInfo* protoClassInfo = nullptr;
 
+    // When an object is unwrapped (near the bottom of this function), we need
+    // to enter its compartment. Unfortunately, given the crazy usage of gotos,
+    // scoped JSAutoCompartments are pretty much impossible. So instead, we use
+    // a single Maybe<JSAutoCompartment>, and let anyone who wants to enter a
+    // compartment destroy and then reconstruct it.
+    Maybe<JSAutoCompartment> mac;
+    if (cx)
+        mac.construct(cx, obj);
+
     // If we were passed a function object then we need to find the correct
     // wrapper out of those that might be in the callee obj's proto chain.
 
@@ -1833,6 +1843,10 @@ XPCWrappedNative::GetWrappedNativeOfJSObject(JSContext* cx,
     }
 
   restart:
+    if (cx) {
+        mac.destroy();
+        mac.construct(cx, obj);
+    }
     for (cur = obj; cur; ) {
         // this is on two lines to make the compiler happy given the goto.
         js::Class* clazz;
