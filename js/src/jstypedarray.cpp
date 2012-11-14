f0347dec58223d51f69ae1ca3a98bb7c90b35a6a
Steve Fink <sfink@mozilla.com>
2012-11-13 16:20:43 -0800

    Bug 811050 - Intialize stolen ArrayBuffer data. r=billm a=lsblakk

--- js/src/jstypedarray.cpp
+++ js/src/jstypedarray.cpp
@@ -467,6 +467,7 @@ ArrayBufferObject::stealContents(JSContext *cx, JSObject *obj, void **contents,
     JSObject *views = *GetViewList(&buffer);
     js::ObjectElements *header = js::ObjectElements::fromElements((js::HeapSlot*)buffer.dataPointer());
     if (buffer.hasDynamicElements()) {
+        *GetViewList(&buffer) = NULL;
         *contents = header;
         *data = buffer.dataPointer();
 
@@ -3618,6 +3619,7 @@ JS_NewArrayBufferWithContents(JSContext *cx, void *contents)
         return NULL;
     JSObject *obj = ArrayBufferObject::create(cx, 0);
     obj->setDynamicElements(reinterpret_cast<js::ObjectElements *>(contents));
+    JS_ASSERT(*GetViewList(&obj->asArrayBuffer()) == NULL);
     return obj;
 }
 
