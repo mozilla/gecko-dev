ea19916e84aed6e7690d981c3f3fbdb046302e3f
Robert O'Callahan <robert@ocallahan.org>
2012-11-15 20:51:46 +0100

    Bug 796452 - Add mozpasspointerevents attribute for IFRAMEs in chrome windows. r=mats. a=blocking-basecamp

--- layout/generic/nsSubDocumentFrame.cpp
+++ layout/generic/nsSubDocumentFrame.cpp
@@ -243,6 +243,19 @@ nsSubDocumentFrame::GetSubdocumentRootFrame()
   return subdocView ? subdocView->GetFrame() : nullptr;
 }
 
+bool
+nsSubDocumentFrame::PassPointerEventsToChildren()
+{
+  if (GetStyleVisibility()->mPointerEvents != NS_STYLE_POINTER_EVENTS_NONE) {
+    return true;
+  }
+  if (mContent->HasAttr(kNameSpaceID_None, nsGkAtoms::mozpasspointerevents) &&
+      PresContext()->IsChrome()) {
+    return true;
+  }
+  return false;
+}
+
 NS_IMETHODIMP
 nsSubDocumentFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
                                      const nsRect&           aDirtyRect,
@@ -251,8 +264,9 @@ nsSubDocumentFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
   if (!IsVisibleForPainting(aBuilder))
     return NS_OK;
 
-  if (aBuilder->IsForEventDelivery() &&
-      GetStyleVisibility()->mPointerEvents == NS_STYLE_POINTER_EVENTS_NONE)
+  // If mozpasspointerevents is set, then we should allow subdocument content
+  // to handle events even if we're pointer-events:none.
+  if (aBuilder->IsForEventDelivery() && !PassPointerEventsToChildren())
     return NS_OK;
 
   nsresult rv = DisplayBorderBackgroundOutline(aBuilder, aLists);
