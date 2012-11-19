19145daf799ac3efda4d5e540211a284a58c8dd6
Chris Jones <jones.chris.g@gmail.com>
2012-11-18 18:57:41 -0800

    Bug 798694, part 3: Remove UA overrides from b2g product proper. r=fabrice a=blocking-basecamp

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -588,10 +588,6 @@ pref("browser.prompt.allowNative", false);
 // a restart is required to enable a new value.
 pref("network.activity.blipIntervalMilliseconds", 250);
 
-// Send some sites a custom user-agent.
-pref("general.useragent.override.facebook.com", "\(Mobile#(Android; Mobile");
-pref("general.useragent.override.youtube.com", "\(Mobile#(Android; Mobile");
-
 pref("jsloader.reuseGlobal", false);
 
 // Enable font inflation for browser tab content.
