d214a55d24c0ddc452f566bbb02223bab92f2ac4
Jeff Gilbert <jgilbert@mozilla.com>
2012-11-20 01:39:26 -0800

    Bug 813278 - Overload JSAutoCompartment for TimeZoneSettingObserver. r=luke, a=blocking-basecamp

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -3253,6 +3253,7 @@ class JS_PUBLIC_API(JSAutoCompartment)
     JSAutoCompartment(JSContext *cx, JSRawObject target);
     JSAutoCompartment(JSContext *cx, JSScript *target);
     JSAutoCompartment(JSContext *cx, JSStackFrame *target);
+    JSAutoCompartment(JSContext *cx, JSString *target);
     ~JSAutoCompartment();
 };
 
