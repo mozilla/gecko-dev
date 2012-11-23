0a8e7fc261d6ee0176b743fd78a8bf50655bdeac
Alex Keybl <akeybl@mozilla.com>
2012-11-23 07:36:22 -0800

    Bug 814232 - Aurora nightly build is actually packaged central Nightly (blue icon) r=akeybl, a=akeybl

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -34,7 +34,7 @@ MOZ_EXTENSIONS_DEFAULT=" gio"
 # For mozilla-beta, mozilla-release, or mozilla-central repositories, use
 # "nightly" branding (until bug 659568 is fixed).
 # For the mozilla-aurora repository, use "aurora".
-MOZ_BRANDING_DIRECTORY=browser/branding/nightly
+MOZ_BRANDING_DIRECTORY=browser/branding/aurora
 MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
 MOZ_APP_ID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}
 # This should usually be the same as the value MAR_CHANNEL_ID.
