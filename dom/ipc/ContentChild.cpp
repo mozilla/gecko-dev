571542fe0723e9926d50acc865ea29b8f5dd6856
Chris Jones <jones.chris.g@gmail.com>
2012-11-27 12:43:52 -0800

    Bug 811315 - Use process-per-mozbrowser by default and make scrolling behavior selectable by content. r=jlebar, sr=roc, a=blocking-basecamp
    This is a rollup of the following patches
    
    Bug 811315, part 0.9: Fix pre-existing race condition that allows dying ContentParents to accidentally try to load new TabParents. r=jlebar
    
    Bug 811315, part 0.91: Fix another pre-existing race condition where FirstIdle might run after ContentChild::ActorDestroy(). r=jlebar
    
    Bug 811315, part 1: Make "browser" process limit infinite and unload the processes when all tabs are closed.  Firefox with process-per-tab!  r=jlebar
    
    Bug 811315, part 2: Pass scrolling-behavior hint down into TabParent instead of inferring it from app/browser. r=jlebar
    
    Bug 811315, part 3: Add a mozasyncpanzoom attribute for iframes and honor it when constructing a remote frame. r=jlebar sr=roc

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -481,8 +481,12 @@ ContentChild::AllocPImageBridge(mozilla::ipc::Transport* aTransport,
     return ImageBridgeChild::StartUpInChildProcess(aTransport, aOtherProcess);
 }
 
+static CancelableTask* sFirstIdleTask;
+
 static void FirstIdle(void)
 {
+    MOZ_ASSERT(sFirstIdleTask);
+    sFirstIdleTask = nullptr;
     ContentChild::GetSingleton()->SendFirstIdle();
 }
 
@@ -492,7 +496,9 @@ ContentChild::AllocPBrowser(const IPCTabContext& aContext,
 {
     static bool firstIdleTaskPosted = false;
     if (!firstIdleTaskPosted) {
-        MessageLoop::current()->PostIdleTask(FROM_HERE, NewRunnableFunction(FirstIdle));
+        MOZ_ASSERT(!sFirstIdleTask);
+        sFirstIdleTask = NewRunnableFunction(FirstIdle);
+        MessageLoop::current()->PostIdleTask(FROM_HERE, sFirstIdleTask);
         firstIdleTaskPosted = true;
     }
 
@@ -822,6 +828,10 @@ ContentChild::ActorDestroy(ActorDestroyReason why)
     // keep persistent state.
     QuickExit();
 #endif
+
+    if (sFirstIdleTask) {
+        sFirstIdleTask->Cancel();
+    }
 
     mAlertObservers.Clear();
 
