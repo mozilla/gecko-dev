a5d000c938e36be178eae566463e78b0ac51bce9
Robert O'Callahan <robert@ocallahan.org>
2012-11-27 22:05:10 +1300

    Bug 815030 - Invalidate cached background images in when a frame gets a visual style change. r=mattwoodrow, a=lsblakk

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -257,15 +257,6 @@ nsIFrame::MarkAsAbsoluteContainingBlock() {
   Properties().Set(AbsoluteContainingBlockProperty(), new nsAbsoluteContainingBlock(GetAbsoluteListID()));
 }
 
-void
-nsIFrame::ClearDisplayItemCache()
-{
-  if (GetStateBits() & NS_FRAME_HAS_CACHED_BACKGROUND) {
-    Properties().Delete(CachedBackgroundImage());
-    RemoveStateBits(NS_FRAME_HAS_CACHED_BACKGROUND);
-  }
-}
-
 bool
 nsIFrame::CheckAndClearPaintedState()
 {
@@ -4806,6 +4797,10 @@ static void InvalidateFrameInternal(nsIFrame *aFrame, bool aHasDisplayItem = tru
   if (aFrame->HasAnyStateBits(NS_FRAME_HAS_INVALID_RECT)) {
     aFrame->Properties().Delete(nsIFrame::InvalidationRect());
     aFrame->RemoveStateBits(NS_FRAME_HAS_INVALID_RECT);
+  }
+  if (aFrame->HasAnyStateBits(NS_FRAME_HAS_CACHED_BACKGROUND)) {
+    aFrame->Properties().Delete(nsIFrame::CachedBackgroundImage());
+    aFrame->RemoveStateBits(NS_FRAME_HAS_CACHED_BACKGROUND);
   }
 }
 
