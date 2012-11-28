2259439c089ae01b6f8c61ddbb192ddee0b6ce81
Ryan VanderMeulen <ryanvm@gmail.com>
2012-11-27 21:20:15 -0500

    Bug 812782 - Update pdf.js to version 0.6.143 to pick up string changes. r=bdahl, a=akeybl

--- browser/extensions/pdfjs/content/build/pdf.js
+++ browser/extensions/pdfjs/content/build/pdf.js
@@ -22,7 +22,7 @@ var PDFJS = {};
   'use strict';
 
   PDFJS.build =
-'033545c';
+'4e90760';
 
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
