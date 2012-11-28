4b38b2c891e6a10309ef2f587062983567fef53e
Bas Schouten <bas@basschouten.com>
2012-11-28 20:00:53 +0000

    bug 814101 - disables Azure content when cleartype is turned off in Windows. r=jfkthame a=lsblakk

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -1225,6 +1225,24 @@ gfxPlatform::UseAzureContentDrawing()
   static bool sAzureContentDrawingPrefCached = false;
 
   if (!sAzureContentDrawingPrefCached) {
+    BOOL font_smoothing;
+    UINT smoothing_type;
+
+    if (!SystemParametersInfo(SPI_GETFONTSMOOTHING, 0, &font_smoothing, 0)) {
+      return false;
+    }
+
+    if (font_smoothing) {
+      if (!SystemParametersInfo(SPI_GETFONTSMOOTHINGTYPE,
+                                0, &smoothing_type, 0)) {
+        return false;
+      }
+
+      if (smoothing_type != FE_FONTSMOOTHINGCLEARTYPE) {
+        return false;
+      }
+    }
+
     sAzureContentDrawingPrefCached = true;
     mozilla::Preferences::AddBoolVarCache(&sAzureContentDrawingEnabled,
                                           "gfx.content.azure.enabled");
