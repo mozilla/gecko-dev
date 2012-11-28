00946e5434e7b90ad5b253e55dfa56b6859d9054
John Schoenick <jschoenick@mozilla.com>
2012-11-28 12:34:34 -0800

    Bug 813947 - Don't try to call OnStartRequest on failures in nsObjectLoadingContent::LoadObject. r=bsmedberg a=lsblakk

--- content/base/src/nsObjectLoadingContent.cpp
+++ content/base/src/nsObjectLoadingContent.cpp
@@ -1803,6 +1803,9 @@ nsObjectLoadingContent::LoadObject(bool aNotify,
       CloseChannel();
     }
 
+    // Don't try to initialize final listener below
+    finalListener = nullptr;
+
     // Don't notify, as LoadFallback doesn't know of our previous state
     // (so really this is just setting mFallbackType)
     LoadFallback(fallbackType, false);
@@ -1815,8 +1818,6 @@ nsObjectLoadingContent::LoadObject(bool aNotify,
   // Pass load on to finalListener if loading with a channel
   //
 
-  // If we re-entered and loaded something else, that load will have cleaned up
-  // our our listener.
   if (!mIsLoading) {
     LOG(("OBJLC [%p]: Re-entered before dispatching to final listener", this));
   } else if (finalListener) {
@@ -1841,6 +1842,7 @@ nsObjectLoadingContent::LoadObject(bool aNotify,
       mIsLoading = true;
       UnloadObject(false);
       NS_ENSURE_TRUE(mIsLoading, NS_OK);
+      CloseChannel();
       LoadFallback(fallbackType, true);
     }
   }
