af6c9daaeec680403b846169ae7b8561214b8807
Ed Morley <emorley@mozilla.com>
2012-12-03 14:30:35 +0000

    Bug 817349 - Ensure topFrame is always defined; r=ted a=test-only
    --HG--
    extra : transplant_source : %F1%9C%16%AA%80%F2%91%01%27%3DVB%EC%94%22%9B%B3%9Ad%B3

--- build/automationutils.py
+++ build/automationutils.py
@@ -167,6 +167,7 @@ def checkForCrashes(dumpDir, symbolsPath, testName=None):
     for d in dumps:
       stackwalkOutput = []
       stackwalkOutput.append("Crash dump filename: " + d)
+      topFrame = None
       if symbolsPath and stackwalkPath and os.path.exists(stackwalkPath):
         # run minidump stackwalk
         p = subprocess.Popen([stackwalkPath, d, symbolsPath],
