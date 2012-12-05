709a3bf2813b77001106b7fe68fdb5edf4bbfc56
Andrea Marchesini <amarchesini@mozilla.com>
2012-12-05 00:56:17 -0800

    Bug 805333 part 3 - Disable audiochannel for desktop build. r=sicking a=blocking-basecamp

--- content/html/content/src/nsHTMLMediaElement.cpp
+++ content/html/content/src/nsHTMLMediaElement.cpp
@@ -3770,6 +3770,9 @@ void nsHTMLMediaElement::GetMimeType(nsCString& aMimeType)
 
 nsresult nsHTMLMediaElement::UpdateChannelMuteState()
 {
+  // Only on B2G we mute the nsHTMLMediaElement following the rules of
+  // AudioChannelService.
+#ifdef MOZ_B2G
   bool hidden = false;
   nsCOMPtr<nsIDOMDocument> domDoc = do_QueryInterface(OwnerDoc());
   if (!domDoc) {
@@ -3796,12 +3799,15 @@ nsresult nsHTMLMediaElement::UpdateChannelMuteState()
     SetMutedInternal(mMuted);
     DispatchAsyncEvent(NS_LITERAL_STRING("mozinterruptend"));
   }
+#endif
 
   return NS_OK;
 }
 
 void nsHTMLMediaElement::UpdateAudioChannelPlayingState()
 {
+  // The nsHTMLMediaElement is registered to the AudioChannelService only on B2G.
+#ifdef MOZ_B2G
   bool playingThroughTheAudioChannel =
      (mReadyState >= nsIDOMHTMLMediaElement::HAVE_FUTURE_DATA &&
       IsPotentiallyPlaying());
@@ -3819,6 +3825,7 @@ void nsHTMLMediaElement::UpdateAudioChannelPlayingState()
       audioChannelService->UnregisterMediaElement(this);
     }
   }
+#endif
 }
 
 nsresult nsHTMLMediaElement::NotifyAudioChannelStateChanged()
