2bbdd1e87fa1c43d5b2cd74373a05eb33e4d339c
Ryan VanderMeulen <ryanvm@gmail.com>
2012-12-10 11:03:21 -0500

    Backed out changesets ed2e7e4c6699 and 9f4823f294d5 (bug 813901) for build failures. a=backout
    --HG--
    branch : GECKO1701_2013010313_RELBRANCH

--- js/xpconnect/wrappers/AccessCheck.h
+++ js/xpconnect/wrappers/AccessCheck.h
@@ -19,7 +19,6 @@ namespace xpc {
 class AccessCheck {
   public:
     static bool subsumes(JSCompartment *a, JSCompartment *b);
-    static bool subsumes(JSObject *a, JSObject *b);
     static bool subsumesIgnoringDomain(JSCompartment *a, JSCompartment *b);
     static bool isChrome(JSCompartment *compartment);
     static bool callerIsChrome();
