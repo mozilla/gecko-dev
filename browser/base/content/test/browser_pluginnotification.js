7a050c92d01d00091ad5303eb1c3f16da4d878fd
David Keeler <dkeeler@mozilla.com>
2012-12-11 14:28:30 -0800

    bug 820109 - click-to-play: open "Check for updates..." link in new tab (beta) r=dao a=akeybl

--- browser/base/content/test/browser_pluginnotification.js
+++ browser/base/content/test/browser_pluginnotification.js
@@ -918,5 +918,51 @@ function test21e() {
     ok(objLoadingContent.activated, "Test 21e, Plugin with id=" + plugin.id + " should be activated");
   }
 
-  finishTest();
+  registerFakeBlocklistService(Ci.nsIBlocklistService.STATE_VULNERABLE_UPDATE_AVAILABLE);
+  prepareTest(test22a, gHttpTestRoot + "plugin_test.html");
+}
+
+// Test that the "Check for updates..." link in the popup notification
+// opens the plugin check page in a new tab.
+function test22a() {
+  var notification = PopupNotifications.getNotification("click-to-play-plugins", gTestBrowser);
+  ok(notification, "Test 22a, Should have a click-to-play notification");
+  ok(notification.options.centerActions.length == 1, "Test 22a, Should have one type of plugin in the notification");
+
+  // we have to actually show the panel to get the bindings to instantiate
+  notification.options.dismissed = false;
+  notification.options.eventCallback = test22b;
+  PopupNotifications._showPanel([notification], notification.anchorElement);
+}
+
+function test22b() {
+  var notification = PopupNotifications.getNotification("click-to-play-plugins", gTestBrowser);
+  notification.options.eventCallback = null;
+  var centerAction = notification.options.centerActions[0];
+  is(centerAction.message, "Test", "Test 22b, found center action for the Test plugin");
+
+  var centerItem = null;
+  for (var item of centerAction.popupnotification.childNodes) {
+    if (item.action == centerAction) {
+      centerItem = item;
+      break;
+    }
+  }
+  ok(centerItem, "Test 22b, found center item for the Test plugin");
+
+  var updateLink = centerItem.ownerDocument.getAnonymousElementByAttribute(centerItem, "class", "text-link");
+  ok(updateLink, "Test 22b, Should have update link");
+
+  var tabOpenListener = new TabOpenListener(Services.urlFormatter.formatURLPref("plugins.update.url"), false, false);
+  tabOpenListener.handleEvent = function(event) {
+    if (event.type == "TabOpen") {
+      gBrowser.tabContainer.removeEventListener("TabOpen", this, false);
+      this.tab = event.originalTarget;
+      ok(event.target.label == this.url, "Test 22b, Update link should open up the plugin check page");
+      gBrowser.removeTab(this.tab);
+      unregisterFakeBlocklistService();
+      finishTest();
+    }
+  };
+  EventUtils.synthesizeMouseAtCenter(updateLink, {}, centerItem.ownerDocument.defaultView);
 }
