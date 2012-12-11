aa2da42001802bd8c8c03ad50fe7d1b2303ba4dd
Benoit Jacob <bjacob@mozilla.com>
2012-12-11 16:57:30 -0500

    Bug 814407 - reattach textures when a texture image is replaced - r=jgilbert, a=akeybl

--- content/canvas/src/WebGLContext.h
+++ content/canvas/src/WebGLContext.h
@@ -1432,6 +1432,8 @@ protected:
     template <typename WebGLObjectType>
     JSObject* WebGLObjectAsJSObject(JSContext *cx, const WebGLObjectType *, ErrorResult& rv) const;
 
+    void ReattachTextureToAnyFramebufferToWorkAroundBugs(WebGLTexture *tex, WebGLint level);
+
 #ifdef XP_MACOSX
     // see bug 713305. This RAII helper guarantees that we're on the discrete GPU, during its lifetime
     // Debouncing note: we don't want to switch GPUs too frequently, so try to not create and destroy
