fed866d338d2442d6916bed79d4268be4aa27c94
Doug Turner <dougt@dougt.org>
2012-12-21 09:18:19 -0800

    Bug 823191 - DeviceStorage enumerate doesn't see files with 0060 perms. r=bent a=blocking-basecamp

--- dom/devicestorage/nsDeviceStorage.cpp
+++ dom/devicestorage/nsDeviceStorage.cpp
@@ -1013,7 +1013,7 @@ public:
     mFile->CollectFiles(cursor->mFiles, cursor->mSince);
 
     nsCOMPtr<ContinueCursorEvent> event = new ContinueCursorEvent(mRequest);
-    NS_DispatchToMainThread(event);
+    event->Continue();
 
     return NS_OK;
   }
@@ -1151,7 +1151,7 @@ nsDOMDeviceStorageCursor::Continue()
   }
 
   nsCOMPtr<ContinueCursorEvent> event = new ContinueCursorEvent(this);
-  NS_DispatchToMainThread(event);
+  event->Continue();
 
   mOkToCallContinue = false;
   return NS_OK;
