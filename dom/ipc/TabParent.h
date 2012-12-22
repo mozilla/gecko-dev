7e2cf684f033e54e677a7c91f7cc315bee069b07
Chris Jones <jones.chris.g@gmail.com>
2012-12-21 18:25:08 -0800

    Back out bug 782542 for regressions. a=backout

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -190,6 +190,8 @@ public:
     virtual POfflineCacheUpdateParent* AllocPOfflineCacheUpdate(
             const URIParams& aManifestURI,
             const URIParams& aDocumentURI,
+            const bool& isInBrowserElement,
+            const uint32_t& appId,
             const bool& stickDocument);
     virtual bool DeallocPOfflineCacheUpdate(POfflineCacheUpdateParent* actor);
 
