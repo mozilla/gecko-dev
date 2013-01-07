27aca42569d198f01e6e1deb2f0016452a9539c2
Justin Lebar <justin.lebar@gmail.com>
2013-01-07 09:44:22 +0100

    Bug 827217 - Fix null-pointer crash with webgl.can-lose-context-in-foreground=false. r=cjones, a=akeybl

--- dom/ipc/ProcessPriorityManager.cpp
+++ dom/ipc/ProcessPriorityManager.cpp
@@ -423,6 +423,12 @@ InitProcessPriorityManager()
 bool
 CurrentProcessIsForeground()
 {
+  // The process priority manager is the only thing which changes our priority,
+  // so if the manager does not exist, then we must be in the foreground.
+  if (!sManager) {
+    return true;
+  }
+
   return sManager->GetPriority() >= PROCESS_PRIORITY_FOREGROUND;
 }
 
