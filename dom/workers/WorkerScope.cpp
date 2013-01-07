0d9448f9007f6e95ae841b90e96a1ef8d281d170
Gavin Sharp <gavin@gavinsharp.com>
2013-01-07 11:18:19 -0800

    Bug 827193: fix typo in WorkerScope, r=khuey, a=akeybl
    --HG--
    extra : transplant_source : %E3p%FCo%AE%8E%CC2%CF%CFW%21%88%DE%FA%9C%D3%F1%24%24

--- dom/workers/WorkerScope.cpp
+++ dom/workers/WorkerScope.cpp
@@ -966,14 +966,12 @@ CreateDedicatedWorkerGlobalScope(JSContext* aCx)
     return NULL;
   }
 
-  if (worker->IsChromeWorker() &&
-      (!chromeworker::InitClass(aCx, global, workerProto, false) ||
-       !DefineChromeWorkerFunctions(aCx, global))) {
-    return NULL;
-  }
-
-  if (!DefineOSFileConstants(aCx, global)) {
-    return NULL;
+  if (worker->IsChromeWorker()) {
+    if (!chromeworker::InitClass(aCx, global, workerProto, false) ||
+        !DefineChromeWorkerFunctions(aCx, global) ||
+        !DefineOSFileConstants(aCx, global)) {
+      return NULL;
+    }
   }
 
   // Init other classes we care about.
