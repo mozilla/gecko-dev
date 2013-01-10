628c1803a49d8d6f8143c9cb6c4e46c225205bf3
Gregor Wagner <anygregor@gmail.com>
2013-01-10 23:50:40 +0100

    Bug 828887 - "unused-arenas" is not freed unless worker is idle for 5 seconds. r=bent, a=bb+

--- dom/workers/WorkerPrivate.h
+++ dom/workers/WorkerPrivate.h
@@ -192,6 +192,7 @@ private:
   Status mParentStatus;
   uint32_t mJSContextOptions;
   uint32_t mJSRuntimeHeapSize;
+  uint32_t mJSWorkerAllocationThreshold;
   uint8_t mGCZeal;
   bool mJSObjectRooted;
   bool mParentSuspended;
@@ -303,7 +304,7 @@ public:
   UpdateJSContextOptions(JSContext* aCx, uint32_t aOptions);
 
   void
-  UpdateJSRuntimeHeapSize(JSContext* aCx, uint32_t aJSRuntimeHeapSize);
+  UpdateJSWorkerMemoryParameter(JSContext* aCx, JSGCParamKey key, uint32_t value);
 
 #ifdef JS_GC_ZEAL
   void
@@ -482,6 +483,12 @@ public:
   GetJSRuntimeHeapSize() const
   {
     return mJSRuntimeHeapSize;
+  }
+
+  uint32_t
+  GetJSWorkerAllocationThreshold() const
+  {
+    return mJSWorkerAllocationThreshold;
   }
 
 #ifdef JS_GC_ZEAL
@@ -704,7 +711,7 @@ public:
   UpdateJSContextOptionsInternal(JSContext* aCx, uint32_t aOptions);
 
   void
-  UpdateJSRuntimeHeapSizeInternal(JSContext* aCx, uint32_t aJSRuntimeHeapSize);
+  UpdateJSWorkerMemoryParameterInternal(JSContext* aCx, JSGCParamKey key, uint32_t aValue);
 
   void
   ScheduleDeletion(bool aWasPending);
