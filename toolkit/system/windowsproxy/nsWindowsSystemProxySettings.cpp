952f568df849dea15fa7a864c6ed228944c013b6
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2013-01-13 05:30:15 +0900

    Bug 817568, 829518 - Enclose some API calls in __try...__except to suppress crash. r=mcmanus a=akeybl

--- toolkit/system/windowsproxy/nsWindowsSystemProxySettings.cpp
+++ toolkit/system/windowsproxy/nsWindowsSystemProxySettings.cpp
@@ -67,8 +67,12 @@ static nsresult ReadInternetOption(uint32_t aOption, uint32_t& aFlags,
 {
     DWORD connFlags = 0;
     WCHAR connName[RAS_MaxEntryName + 1];
-    InternetGetConnectedStateExW(&connFlags, connName,
-                                 mozilla::ArrayLength(connName), 0);
+    MOZ_SEH_TRY {
+        InternetGetConnectedStateExW(&connFlags, connName,
+                                     mozilla::ArrayLength(connName), 0);
+    } MOZ_SEH_EXCEPT(EXCEPTION_EXECUTE_HANDLER) {
+        return NS_ERROR_FAILURE;
+    }
 
     INTERNET_PER_CONN_OPTIONW options[2];
     options[0].dwOption = INTERNET_PER_CONN_FLAGS_UI;
@@ -90,8 +94,12 @@ static nsresult ReadInternetOption(uint32_t aOption, uint32_t& aFlags,
         }
         options[0].dwOption = INTERNET_PER_CONN_FLAGS;
         size = sizeof(INTERNET_PER_CONN_OPTION_LISTW);
-        if (!InternetQueryOptionW(NULL, INTERNET_OPTION_PER_CONNECTION_OPTION,
-                                  &list, &size)) {
+        MOZ_SEH_TRY {
+            if (!InternetQueryOptionW(NULL, INTERNET_OPTION_PER_CONNECTION_OPTION,
+                                      &list, &size)) {
+                return NS_ERROR_FAILURE;
+            }
+        } MOZ_SEH_EXCEPT(EXCEPTION_EXECUTE_HANDLER) {
             return NS_ERROR_FAILURE;
         }
     }
