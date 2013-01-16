f6807302cf48412a4ec9e3fbebb197f02df5e3ac
Justin Lebar <justin.lebar@gmail.com>
2013-01-16 17:49:27 -0500

    Bug 809949 - Dump memory reports when 'memory report' is written to a special fifo on disk. r=glandium, r=njn, a=sicking

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -3878,3 +3878,7 @@ pref("dom.browserElement.maxScreenshotDelayMS", 2000);
 // tentatively deciding the gesture is actually a tap and activating
 // the target element?
 pref("ui.touch_activation.delay_ms", 50);
+
+// MemoryInfoDumper can watch a fifo in the temp directory and take various
+// actions when the fifo is written to.  Disable this in general.
+pref("memory_info_dumper.watch_fifo", false);
