1f535b3244cf1db7036443e09c54d91903417754
Sriram Ramasubramanian <sriram@mozilla.com>
2013-01-16 14:04:53 -0800

    Bug 831402: Dividers in tabs UI should be with action-bar. [r=mfinkle] [a=bbajaj] [needs-clobber]

--- mobile/android/base/TabsPanel.java
+++ mobile/android/base/TabsPanel.java
@@ -57,7 +57,7 @@ public class TabsPanel extends TabHost
     private Context mContext;
     private GeckoApp mActivity;
     private PanelView mPanel;
-    private TabsPanelToolbar mFooter;
+    private LinearLayout mFooter;
     private TabsLayoutChangeListener mLayoutChangeListener;
 
     private static ImageButton mMenuButton;
@@ -149,7 +149,7 @@ public class TabsPanel extends TabHost
     }
 
     void initToolbar() {
-        mFooter = (TabsPanelToolbar) findViewById(R.id.tabs_panel_footer);
+        mFooter = (LinearLayout) findViewById(R.id.tabs_panel_footer);
 
         mTabWidget = (TabWidget) findViewById(android.R.id.tabs);
 
