5d62114bc9a5a7b7d1350bd24e94bf0cca8cd91d
Jim Chen <nchen@mozilla.com>
2013-01-22 13:18:06 -0500

    Bug 829318 - Don't mess with mIMEState during focus/blur; r=cpeterson a=akeybl

--- mobile/android/base/GeckoInputConnection.java
+++ mobile/android/base/GeckoInputConnection.java
@@ -43,6 +43,7 @@ class GeckoInputConnection
 
     private static final Timer mIMETimer = new Timer("GeckoInputConnection Timer");
 
+    // Managed only by notifyIMEEnabled; see comments in notifyIMEEnabled
     private static int mIMEState;
     private String mIMETypeHint = "";
     private String mIMEModeHint = "";
@@ -261,9 +262,9 @@ class GeckoInputConnection
         mBatchTextChanged = false;
         mUpdateRequest = null;
 
-        mIMEState = IME_STATE_DISABLED;
-        mIMETypeHint = mIMEModeHint = mIMEActionHint = "";
         mCurrentInputMethod = "";
+
+        // Do not reset mIMEState here; see comments in notifyIMEEnabled
     }
 
     public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
@@ -518,6 +519,13 @@ class GeckoInputConnection
         if (v == null)
             return;
 
+        // mIMEState and the mIME*Hint fields should only be changed by notifyIMEEnabled,
+        // and not reset anywhere else. Usually, notifyIMEEnabled is called right after a
+        // focus or blur, so resetting mIMEState during the focus or blur seems harmless.
+        // However, this behavior is not guaranteed. Gecko may call notifyIMEEnabled
+        // independent of focus change; that is, a focus change may not be accompanied by
+        // a notifyIMEEnabled call. So if we reset mIMEState inside focus, there may not
+        // be another notifyIMEEnabled call to set mIMEState to a proper value (bug 829318)
         /* When IME is 'disabled', IME processing is disabled.
            In addition, the IME UI is hidden */
         mIMEState = state;
