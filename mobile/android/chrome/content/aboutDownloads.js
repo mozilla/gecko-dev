bf559a16734396d9784d82d7ca91f61fa77a2cfd
Brian Nicholson <bnicholson@mozilla.com>
2013-01-24 13:52:12 -0800

    Bug 834048 - Add required param for f.remove(). r=mfinkle a=lsblakk

--- mobile/android/chrome/content/aboutDownloads.js
+++ mobile/android/chrome/content/aboutDownloads.js
@@ -437,7 +437,7 @@ let Downloads = {
       }
       aDownload.remove();
       try {
-        if (f) f.remove();
+        if (f) f.remove(false);
       } catch (ex) {
         this.logError("removeDownload() " + ex, aDownload);
       }
