135d4811aa1599aa6eb38eed1c772d895163cf78
Margaret Leibovic <margaret.leibovic@gmail.com>
2013-02-01 15:45:33 -0800

    Bug 836838 - Avoid race condition in distribution initialization. r=bnicholson,mfinkle a=lsblakk

--- mobile/android/base/ReferrerReceiver.java
+++ mobile/android/base/ReferrerReceiver.java
@@ -52,7 +52,7 @@ public class ReferrerReceiver
                     data.put("version", campaign);
 
                     // Try to make sure the prefs are written as a group
-                    GeckoEvent event = GeckoEvent.createBroadcastEvent("Distribution:Set", data.toString());
+                    GeckoEvent event = GeckoEvent.createBroadcastEvent("Campaign:Set", data.toString());
                     GeckoAppShell.sendEventToGecko(event);
                 } catch (JSONException e) {
                     Log.e(LOGTAG, "Error setting distribution", e);
