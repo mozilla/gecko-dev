f38832057a25d4c49a49e5903682c5039d7c6cc5
Josh Matthews <josh@joshmatthews.net>
2013-02-19 23:53:51 -0500

    Bug 832232 - Protect against missing geolocation services during shutdown. r=dougt a=tef+

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -2473,7 +2473,9 @@ ContentParent::RecvSetGeolocationHigherAccuracy(const bool& aEnable)
 {
     nsRefPtr<nsGeolocationService> geoSvc =
         nsGeolocationService::GetGeolocationService();
-    geoSvc->SetHigherAccuracy(aEnable);
+    if (geoSvc) {
+        geoSvc->SetHigherAccuracy(aEnable);
+    }
     return true;
 }
 
