fffe0559373d119e24f517535fb852ca39190c62
Lucas Rocha <lucasr@mozilla.com>
2013-03-01 21:30:07 +0000

    Bug 817828 - Don't use hardware layers on toolbar animations. r=sriram, a=lsblakk

--- mobile/android/base/BrowserToolbar.java
+++ mobile/android/base/BrowserToolbar.java
@@ -549,6 +549,7 @@ public class BrowserToolbar implements ViewSwitcher.ViewFactory,
         proxy.setAlpha(1);
 
         final PropertyAnimator contentAnimator = new PropertyAnimator(250);
+        contentAnimator.setUseHardwareLayer(false);
 
         // Shrink the awesome entry back to its original size
         contentAnimator.attach(mAwesomeBarRightEdge,
@@ -629,6 +630,7 @@ public class BrowserToolbar implements ViewSwitcher.ViewFactory,
         }
 
         final PropertyAnimator contentAnimator = new PropertyAnimator(250);
+        contentAnimator.setUseHardwareLayer(false);
 
         int translation = prepareAwesomeBarAnimation();
 
