ec9b4d6b7057748e810a8b53fba8320f888d6545
Brian Hackett <bhackett1024@gmail.com>
2013-03-01 17:08:18 -0700

    Bug 834240 - Add missing setPendingNukeTypes. r=dvander, a=bajaj

--- js/src/jsinfer.cpp
+++ js/src/jsinfer.cpp
@@ -4991,6 +4991,7 @@ CheckNewScriptProperties(JSContext *cx, HandleTypeObject type, JSFunction *fun)
     if (!baseobj) {
         if (type->newScript)
             type->clearNewScript(cx);
+        cx->compartment->types.setPendingNukeTypes(cx);
         return;
     }
 
