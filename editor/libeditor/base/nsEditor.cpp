c6d92d612d1ec955f8f9bba881296c2de9b894e3
Ehsan Akhgari <ehsan@mozilla.com>
2013-03-07 01:12:41 -0500

    Bug 848644 - Do not flush when determining if a node is preformatted or not; r=bzbarsky a=dveditz
    --HG--
    extra : transplant_source : %98%9F%15%F6%EEa%28%8E%97i%A3%06%91%1Al%A6QDl%04

--- editor/libeditor/base/nsEditor.cpp
+++ editor/libeditor/base/nsEditor.cpp
@@ -3985,9 +3985,9 @@ nsEditor::IsPreformatted(nsIDOMNode *aNode, bool *aResult)
     content = content->GetParent();
   }
   if (content && content->IsElement()) {
-    elementStyle = nsComputedDOMStyle::GetStyleContextForElement(content->AsElement(),
-                                                                 nullptr,
-                                                                 ps);
+    elementStyle = nsComputedDOMStyle::GetStyleContextForElementNoFlush(content->AsElement(),
+                                                                        nullptr,
+                                                                        ps);
   }
 
   if (!elementStyle)
