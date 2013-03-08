9826f98252a54ab36c0a5d48ecc6a84095737876
Wes Johnston <wjohnston@mozilla.com>
2013-03-08 13:01:51 -0800

    Bug 838440 - Refresh about:home twhen the activity is resumed. r=lucasr a=lsblakk

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -1215,4 +1215,14 @@ abstract public class BrowserApp extends GeckoApp
             }
         }).execute();
     }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (mAboutHomeContent != null) {
+            mAboutHomeContent.refresh();
+        }
+
+    }
 }
