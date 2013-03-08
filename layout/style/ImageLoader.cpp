da0ec2a507e922bd6123351273fc0e4f5c5ce939
Matt Woodrow <mwoodrow@mozilla.com>
2013-03-08 15:18:45 +1300

    Bug 844750 - Don't use per-display-item invalidation for table frames. r=roc, a=lsblakk

--- layout/style/ImageLoader.cpp
+++ layout/style/ImageLoader.cpp
@@ -341,7 +341,14 @@ ImageLoader::DoRedraw(FrameSet* aFrameSet)
     nsIFrame* frame = aFrameSet->ElementAt(i);
 
     if (frame->GetStyleVisibility()->IsVisible()) {
-      FrameLayerBuilder::IterateRetainedDataFor(frame, InvalidateImagesCallback);
+      if (frame->IsFrameOfType(nsIFrame::eTablePart)) {
+        // Tables don't necessarily build border/background display items
+        // for the individual table part frames, so IterateRetainedDataFor
+        // might not find the right display item.
+        frame->InvalidateFrame();
+      } else {
+        FrameLayerBuilder::IterateRetainedDataFor(frame, InvalidateImagesCallback);
+      }
     }
   }
 }
