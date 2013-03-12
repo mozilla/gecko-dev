9e3493591939229719662b1cd04a55eab24d9055
Yoshi Huang <allstars.chh@mozilla.com>
2013-03-12 14:43:01 +0800

    Bug 849757 - Part 3: spck and cck in RIL. r=vicamo, a=tef+

--- dom/system/gonk/ril_worker.js
+++ dom/system/gonk/ril_worker.js
@@ -884,7 +884,7 @@ let RIL = {
     Buf.simpleRequest(REQUEST_GET_SIM_STATUS);
   },
 
-   /**
+  /**
    * Helper function for unlocking ICC locks.
    */
   iccUnlockCardLock: function iccUnlockCardLock(options) {
@@ -903,6 +903,14 @@ let RIL = {
         break;
       case "nck":
         options.type = CARD_PERSOSUBSTATE_SIM_NETWORK;
+        this.enterDepersonalization(options);
+        break;
+      case "cck":
+        options.type = CARD_PERSOSUBSTATE_SIM_CORPORATE;
+        this.enterDepersonalization(options);
+        break;
+      case "spck":
+        options.type = CARD_PERSOSUBSTATE_SIM_SERVICE_PROVIDER;
         this.enterDepersonalization(options);
         break;
       default:
