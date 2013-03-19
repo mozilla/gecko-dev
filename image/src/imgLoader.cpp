ce5c0b8f9aebdc653f91b19278370daecd0a63c9
Ryan VanderMeulen <ryanvm@gmail.com>
2013-03-19 13:16:38 -0400

    Bug 847310 - Yet another bustage fix. a=bustage

--- image/src/imgLoader.cpp
+++ image/src/imgLoader.cpp
@@ -1997,7 +1997,7 @@ nsresult imgLoader::GetMimeTypeFromContent(const char* aContents, uint32_t aLeng
   else if (aLength >= 2 && (static_cast<unsigned char>(aContents[0]) == 0x00 &&
                             (static_cast<unsigned char>(aContents[1]) & 0x9F) == 0x00))
   {
-    aContentType.AssignLiteral(IMAGE_WBMP);
+    aContentType.AssignLiteral("image/vnd.wap.wbmp");
   }
 
   else {
