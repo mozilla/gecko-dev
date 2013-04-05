c72465dcc7ee8e2b4caabc0497a67db2f50e3fdd
Henri Sivonen <hsivonen@iki.fi>
2013-04-05 15:36:16 +0300

    Bug 849597 - Add more kung fu to nsParser. r=smaug, a=bajaj

--- parser/htmlparser/src/nsParser.cpp
+++ parser/htmlparser/src/nsParser.cpp
@@ -1107,6 +1107,7 @@ nsParser::ContinueInterruptedParsing()
   // the reenabling process, hold a reference to ourselves.
   nsresult result=NS_OK;
   nsCOMPtr<nsIParser> kungFuDeathGrip(this);
+  nsCOMPtr<nsIContentSink> sinkDeathGrip(mSink);
 
 #ifdef DEBUG
   if (!(mFlags & NS_PARSER_FLAG_PARSER_ENABLED)) {
@@ -2118,6 +2119,8 @@ nsParser::OnDataAvailable(nsIRequest *request, nsISupports* aContext,
     // non-whitespace data
     if (IsOkToProcessNetworkData() &&
         theContext->mScanner->FirstNonWhitespacePosition() >= 0) {
+      nsCOMPtr<nsIParser> kungFuDeathGrip(this);
+      nsCOMPtr<nsIContentSink> sinkDeathGrip(mSink);
       mProcessingNetworkData = true;
       if (mSink) {
         mSink->WillParse();
