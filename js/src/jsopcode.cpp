371ed20232aecbd24ca2ba5fb5fbd6bdcccf2a79
Jan de Mooij <jdemooij@mozilla.com>
2013-04-05 09:25:15 -0400

    Bug 787283 - Ensure spindex is in range. r=bhackett, a=lsblakk

--- js/src/jsopcode.cpp
+++ js/src/jsopcode.cpp
@@ -6152,6 +6152,9 @@ FindStartPC(JSContext *cx, ScriptFrameIter &iter, int spindex, int skipStackHits
     if (!pcstack.init(cx, iter.script(), current))
         return false;
 
+    if (spindex < 0 && spindex + pcstack.depth() < 0)
+        spindex = JSDVG_SEARCH_STACK;
+
     if (spindex == JSDVG_SEARCH_STACK) {
         size_t index = iter.numFrameSlots();
         Value s;
