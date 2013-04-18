9a05e9180addab304e7840c1d2bbc25f9dfd8cbc
Vicamo Yang <vyang@mozilla.com>
2013-04-18 09:09:38 +0800

    Bug 838467 5/5: test cases. r=gwagner, a=leo+

--- dom/mobilemessage/tests/marionette/test_message_classes.js
+++ dom/mobilemessage/tests/marionette/test_message_classes.js
@@ -72,12 +72,11 @@ function test_message_class_0() {
          "Message's timestamp should be lesser than the timestamp of now");
 
       // Make sure the message is not stored.
-      let request = sms.getMessages(null, false);
-      request.onsuccess = function onsuccess() {
-        let cursor = request.result;
-        if (cursor.message) {
+      let cursor = sms.getMessages(null, false);
+      cursor.onsuccess = function onsuccess() {
+        if (cursor.result) {
           // Here we check whether there is any message of the same sender.
-          isnot(cursor.message.sender, message.sender, "cursor.message.sender");
+          isnot(cursor.result.sender, message.sender, "cursor.result.sender");
 
           cursor.continue();
           return;
@@ -91,7 +90,7 @@ function test_message_class_0() {
           window.setTimeout(do_test.bind(null, dcsIndex), 0);
         }
       };
-      request.onerror = function onerror() {
+      cursor.onerror = function onerror() {
         ok(false, "Can't fetch messages from SMS database");
       };
     });
