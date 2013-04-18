abb3f4134e4a5bd5d5115857d8d683e589df6022
Chris Peterson <cpeterson@mozilla.com>
2013-04-18 16:35:41 -0700

    Bug 863490 - Part 1: Get the correct JNIEnv for the worker thread that is destroying the Flash plugin. r=blassey a=lsblakk

--- widget/android/AndroidBridge.cpp
+++ widget/android/AndroidBridge.cpp
@@ -2301,7 +2301,8 @@ AndroidBridge::RegisterSurfaceTextureFrameListener(jobject surfaceTexture, int i
 void
 AndroidBridge::UnregisterSurfaceTextureFrameListener(jobject surfaceTexture)
 {
-    JNIEnv* env = GetJNIEnv();
+    // This function is called on a worker thread when the Flash plugin is unloaded.
+    JNIEnv* env = GetJNIForThread();
     if (!env)
         return;
 
