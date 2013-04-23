d00534b0ad1d32cf7ed204a458eed53a5dcf5cc7
Till Schneidereit <tschneidereit@gmail.com>
2013-04-23 12:58:24 +0100

    Bug 851788 - prevent jsd_SetExecutionHook from operating on self-hosted functions. a=philor

--- js/src/jsdbgapi.cpp
+++ js/src/jsdbgapi.cpp
@@ -612,6 +612,12 @@ JS_SetScriptUserBit(JSScript *script, bool b)
     script->userBit = b;
 }
 
+JS_PUBLIC_API(bool)
+JS_GetScriptIsSelfHosted(JSScript *script)
+{
+    return script->selfHosted;
+}
+
 /***************************************************************************/
 
 JS_PUBLIC_API(void)
