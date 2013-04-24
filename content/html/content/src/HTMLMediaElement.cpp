9c84d4e1e6c4abe0d3903bf95d09526df33cb1c2
Robert O'Callahan <robert@ocallahan.org>
2013-04-24 21:19:02 +1200

    Bug 786539. Part 2: AreAllStreamsForResourceSuspended should ignore streams that can't read the resource data we need. r=cpearce a=akeybl

--- content/html/content/src/HTMLMediaElement.cpp
+++ content/html/content/src/HTMLMediaElement.cpp
@@ -2925,9 +2925,9 @@ void HTMLMediaElement::UpdateReadyStateForData(MediaDecoderOwner::NextFrameStatu
   // move to HAVE_ENOUGH_DATA if we can play through the entire media
   // without stopping to buffer.
   MediaDecoder::Statistics stats = mDecoder->GetStatistics();
-  if (stats.mTotalBytes < 0 ? stats.mDownloadRateReliable :
-                              stats.mTotalBytes == stats.mDownloadPosition ||
-      mDecoder->CanPlayThrough())
+  if (stats.mTotalBytes < 0 ? stats.mDownloadRateReliable
+                            : stats.mTotalBytes == stats.mDownloadPosition ||
+                              mDecoder->CanPlayThrough())
   {
     ChangeReadyState(nsIDOMHTMLMediaElement::HAVE_ENOUGH_DATA);
     return;
