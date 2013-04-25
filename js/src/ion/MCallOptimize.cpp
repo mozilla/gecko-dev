02ec98e17840c2ed29702414e7b0ae7513447495
Shu-yu Guo <shu@rfrn.org>
2013-04-24 18:41:03 -0700

    Bug 861904. r=sstangl, a=bajaj

--- js/src/ion/MCallOptimize.cpp
+++ js/src/ion/MCallOptimize.cpp
@@ -1154,6 +1154,8 @@ IonBuilder::inlineParallelArrayTail(CallInfo &callInfo,
     if (returnTypes->getObjectCount() != 1)
         return InliningStatus_NotInlined;
     types::TypeObject *typeObject = returnTypes->getTypeObject(0);
+    if (typeObject->clasp != &ParallelArrayObject::class_)
+        return InliningStatus_NotInlined;
 
     // Create the call and add in the non-this arguments.
     uint32_t targetArgs = argc;
