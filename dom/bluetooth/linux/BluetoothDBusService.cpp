a5a6fcbb3d4bf58bfffac0478c4766ebb93faa24
Eric Chou <echou@mozilla.com>
2013-04-26 18:09:00 +0800

    Bug 817972 - Make Gecko Bluetooth capable of queueing file-sending requests. r=gyeh, a=leo+

--- dom/bluetooth/linux/BluetoothDBusService.cpp
+++ dom/bluetooth/linux/BluetoothDBusService.cpp
@@ -2783,7 +2783,7 @@ BluetoothDBusService::SendFile(const nsAString& aDeviceAddress,
   BluetoothValue v = true;
   nsAutoString errorStr;
 
-  if (!opp->SendFile(aBlobParent)) {
+  if (!opp->SendFile(aDeviceAddress, aBlobParent)) {
     errorStr.AssignLiteral("Calling SendFile() failed");
   }
 
