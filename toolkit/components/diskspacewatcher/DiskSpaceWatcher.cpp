a9369a5b8f533fc471cd27229bc98284a1891dd1
Fabrice Desré <fabrice@mozilla.com>
2013-05-10 16:55:25 -0400

    Bug 853350 - Follow-up: Add missing include. a=bustage

--- toolkit/components/diskspacewatcher/DiskSpaceWatcher.cpp
+++ toolkit/components/diskspacewatcher/DiskSpaceWatcher.cpp
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "DiskSpaceWatcher.h"
+#include "mozilla/StaticPtr.h"
 #include "mozilla/Hal.h"
 #include "mozilla/ModuleUtils.h"
 #include "mozilla/Preferences.h"
