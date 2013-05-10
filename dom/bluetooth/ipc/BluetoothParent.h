5d266ed258e486fd4b8de95f1ced796b21d45eca
Gina Yeh <gyeh@mozilla.com>
2013-05-10 16:24:52 -0400

    Bug 830213 - Patch 1: Add ConnectSco, DisconnectSco, IsScoConnected in nsIDOMBluetoothAdapter. r=echou, sr=mrbkap, a=tef+

--- dom/bluetooth/ipc/BluetoothParent.h
+++ dom/bluetooth/ipc/BluetoothParent.h
@@ -183,6 +183,15 @@ protected:
 
   bool
   DoRequest(const DenyReceivingFileRequest& aRequest);
+
+  bool
+  DoRequest(const ConnectScoRequest& aRequest);
+
+  bool
+  DoRequest(const DisconnectScoRequest& aRequest);
+
+  bool
+  DoRequest(const IsScoConnectedRequest& aRequest);
 };
 
 END_BLUETOOTH_NAMESPACE
