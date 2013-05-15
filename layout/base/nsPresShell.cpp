081ffcf8811f7620ed3c459f92cabc877b6cf581
Olli Pettay <Olli.Pettay@helsinki.fi>
2013-05-15 14:38:05 +0300

    Bug 862309 - Remove useless warning about missing presshell. r=mats, a=akeybl

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -6365,6 +6365,7 @@ PresShell::HandleEventWithTarget(nsEvent* aEvent, nsIFrame* aFrame,
     NS_ASSERTION(!doc || doc->GetShell() == this, "wrong shell");
   }
 #endif
+  NS_ENSURE_STATE(!aContent || aContent->GetCurrentDoc() == mDocument);
 
   PushCurrentEventInfo(aFrame, aContent);
   nsresult rv = HandleEventInternal(aEvent, aStatus);
