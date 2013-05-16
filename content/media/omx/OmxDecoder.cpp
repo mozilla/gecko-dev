b6fcca2b7fae4eb43e44dc399237d67c2e80f61a
Sotaro Ikeda <sikeda@mozilla.com>
2013-05-15 21:35:34 -0400

    Bug 868806 - Handle OMXCodec's error. r=doublec, a=tef+

--- content/media/omx/OmxDecoder.cpp
+++ content/media/omx/OmxDecoder.cpp
@@ -578,6 +578,9 @@ bool OmxDecoder::ReadVideo(VideoFrame *aFrame, int64_t aTimeUs,
     // don't keep trying to decode if the decoder doesn't want to.
     return false;
   }
+  else if (err != OK && err != -ETIMEDOUT) {
+    return false;
+  }
 
   return true;
 }
@@ -629,6 +632,9 @@ bool OmxDecoder::ReadAudio(AudioFrame *aFrame, int64_t aSeekTimeUs)
     }
   }
   else if (err == UNKNOWN_ERROR) {
+    return false;
+  }
+  else if (err != OK && err != -ETIMEDOUT) {
     return false;
   }
 
