82bc36e3e4d7df8fa16886a3f85f1ba99137106a
Boris Zbarsky <bzbarsky@mit.edu>
2013-05-20 23:54:38 -0400

    Bug 868715 part 10.  Create specializations of Optional for 'any' and 'object' types so that we can have those look like Optional<Handle<Value> > and Optional<Handle<JSObject*> > respectively.  r=peterv,sfink, a=lsblakk

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -2619,7 +2619,12 @@ def getJSToNativeConversionInfo(type, descriptorProvider, failureCode=None,
     # A helper function for converting things that look like a JSObject*.
     def handleJSObjectType(type, isMember, failureCode):
         if not isMember:
-            declType = CGGeneric("JS::Rooted<JSObject*>")
+            if isOptional:
+                # We have a specialization of Optional that will use a
+                # Rooted for the storage here.
+                declType = CGGeneric("JS::Handle<JSObject*>")
+            else:
+                declType = CGGeneric("JS::Rooted<JSObject*>")
         else:
             assert (isMember == "Sequence" or isMember == "Variadic" or
                     isMember == "Dictionary")
@@ -3322,7 +3327,12 @@ for (uint32_t i = 0; i < length; ++i) {
             declType = "JS::Value"
         else:
             assert not isMember
-            declType = "JS::Rooted<JS::Value>"
+            if isOptional:
+                # We have a specialization of Optional that will use a
+                # Rooted for the storage here.
+                declType = "JS::Handle<JS::Value>"
+            else:
+                declType = "JS::Rooted<JS::Value>"
             declArgs = "cx"
 
         templateBody = "${declName} = ${val};"
@@ -8474,8 +8484,6 @@ class CGNativeMember(ClassMethod):
             # Don't do the rooting stuff for variadics for now
             if isMember:
                 declType = "JS::Value"
-            elif optional:
-                declType = "JS::Rooted<JS::Value>"
             else:
                 declType = "JS::Handle<JS::Value>"
             return declType, False, False
@@ -8483,8 +8491,6 @@ class CGNativeMember(ClassMethod):
         if type.isObject():
             if isMember:
                 declType = "JSObject*"
-            elif optional:
-                declType = "JS::Rooted<JSObject*>"
             else:
                 declType = "JS::Handle<JSObject*>"
             return declType, False, False
