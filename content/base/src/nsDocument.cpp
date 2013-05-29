eff218f9c3f4c435bd9399937e2d9933d643af5a
Mats Palmgren <matspal@gmail.com>
2013-05-29 22:17:17 +0000

    Bug 871099.  r=bzbarsky a=akeybl

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -2175,9 +2175,9 @@ nsDocument::ResetToURI(nsIURI *aURI, nsILoadGroup *aLoadGroup,
       nsNodeUtils::ContentRemoved(this, content, i, previousSibling);
       content->UnbindFromTree();
     }
+    mCachedRootElement = nullptr;
   }
   mInUnlinkOrDeletion = oldVal;
-  mCachedRootElement = nullptr;
 
   // Reset our stylesheets
   ResetStylesheetsToURI(aURI);
