de4c84d41f327448bcb210c9031654d2f087d919
Bobby Holley <bobbyholley@gmail.com>
2013-06-05 08:12:36 -0400

    Bug 858101 - Run the DefaultValue algorithm directly on the wrapper for Xrays. r=mrbkap, a=akeybl

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -4836,7 +4836,7 @@ MaybeCallMethod(JSContext *cx, HandleObject obj, Handle<jsid> id, MutableHandleV
     return Invoke(cx, ObjectValue(*obj), vp, 0, NULL, vp.address());
 }
 
-JSBool
+JS_FRIEND_API(JSBool)
 DefaultValue(JSContext *cx, HandleObject obj, JSType hint, MutableHandleValue vp)
 {
     JS_ASSERT(hint == JSTYPE_NUMBER || hint == JSTYPE_STRING || hint == JSTYPE_VOID);
