01ad69a0421ea1f362819311520107b8f29738dc
Robert O'Callahan <robert@ocallahan.org>
2013-06-07 16:26:28 +1200

    Bug 865537 - Part 1: Add nsRange::SetEnableGravitationOnElementRemoval to suppress 'gravitation' behavior on node removal. r=smaug, a=lsblakk

--- content/base/src/nsRange.cpp
+++ content/base/src/nsRange.cpp
@@ -649,6 +649,11 @@ nsRange::ContentRemoved(nsIDocument* aDocument,
     gravitateEnd = true;
   }
 
+  if (!mEnableGravitationOnElementRemoval) {
+    // Do not gravitate.
+    return;
+  }
+
   if (gravitateStart || gravitateEnd) {
     DoSetRange(gravitateStart ? container : mStartParent.get(),
                gravitateStart ? aIndexInContainer : mStartOffset,
