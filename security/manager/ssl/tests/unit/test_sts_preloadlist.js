d1f8eb4e39d2ae89223b14f9627aeb423b900036
David Keeler <dkeeler@mozilla.com>
2013-06-20 13:12:23 -0700

    Bug 883582 - Use only domains likely to stay on the HSTS preload list in the test. r=bsmith, a=test-only

--- security/manager/ssl/tests/unit/test_sts_preloadlist.js
+++ security/manager/ssl/tests/unit/test_sts_preloadlist.js
@@ -1,3 +1,9 @@
+// This test attempts to use only domains that are likely to remain on the
+// preload list for a long time. Currently this includes bugzilla.mozilla.org
+// and login.persona.org because they are Mozilla properties and we are
+// invested in HSTS. Additionally, www.torproject.org was deemed likely to
+// continue to use HSTS.
+
 var Cc = Components.classes;
 var Ci = Components.interfaces;
 
@@ -32,22 +38,30 @@ Observer.prototype = {
 
 var gObserver = new Observer();
 
-// This is a list of every host we call processStsHeader with
-// (we have to remove any state added to the sts service so as to not muck
-// with other tests).
-var hosts = ["http://keyerror.com", "http://subdomain.ottospora.nl",
-             "http://subdomain.pixi.me", "http://crypto.cat",
-             "http://logentries.com"];
+// nsIStrictTransportSecurityService.removeStsState removes a given domain's
+// HSTS status. This means that a domain on the preload list will be
+// considered not HSTS if this is called. So, to reset everything to its
+// original state, we have to reach into the permission manager and clear
+// any HSTS-related state manually.
+function clearStsState() {
+  var permissionManager = Cc["@mozilla.org/permissionmanager;1"]
+                            .getService(Ci.nsIPermissionManager);
+  // This is a list of every host we call processStsHeader with
+  // (so we can remove any state added to the sts service)
+  var hosts = ["bugzilla.mozilla.org", "login.persona.org",
+               "subdomain.www.torproject.org",
+               "subdomain.bugzilla.mozilla.org" ];
+  for (var host of hosts) {
+    permissionManager.remove(host, "sts/use");
+    permissionManager.remove(host, "sts/subd");
+  }
+}
 
 function cleanup() {
   Services.obs.removeObserver(gObserver, "private-browsing-transition-complete");
   if (getPBSvc())
     getPBSvc().privateBrowsingEnabled = false;
-
-  for (var host of hosts) {
-    var uri = Services.io.newURI(host, null, null);
-    gSTSService.removeStsState(uri);
-  }
+  clearStsState();
 }
 
 function run_test() {
@@ -71,77 +85,65 @@ function test_part1() {
   // check that an ancestor domain is not identified as an sts host
   do_check_false(gSTSService.isStsHost("com"));
 
-  // Note: the following were taken from the STS preload list
-  // as of Sept. 2012. If the list changes, this test will need to be modified.
-
-  // check that an entry at the beginning of the list is an sts host
-  do_check_true(gSTSService.isStsHost("arivo.com.br"));
-
   // check that a subdomain is an sts host (includeSubdomains is set)
-  do_check_true(gSTSService.isStsHost("subdomain.arivo.com.br"));
+  do_check_true(gSTSService.isStsHost("subdomain.bugzilla.mozilla.org"));
 
   // check that another subdomain is an sts host (includeSubdomains is set)
-  do_check_true(gSTSService.isStsHost("a.b.c.subdomain.arivo.com.br"));
-
-  // check that an entry in the middle of the list is an sts host
-  do_check_true(gSTSService.isStsHost("neg9.org"));
+  do_check_true(gSTSService.isStsHost("a.b.c.def.bugzilla.mozilla.org"));
 
   // check that a subdomain is not an sts host (includeSubdomains is not set)
-  do_check_false(gSTSService.isStsHost("subdomain.neg9.org"));
-
-  // check that an entry at the end of the list is an sts host
-  do_check_true(gSTSService.isStsHost("www.noisebridge.net"));
-
-  // check that a subdomain is not an sts host (includeSubdomains is not set)
-  do_check_false(gSTSService.isStsHost("a.subdomain.www.noisebridge.net"));
+  do_check_false(gSTSService.isStsHost("subdomain.www.torproject.org"));
 
   // check that a host with a dot on the end won't break anything
   do_check_false(gSTSService.isStsHost("notsts.nonexistent.mozilla.com."));
 
   // check that processing a header with max-age: 0 will remove a preloaded
   // site from the list
-  var uri = Services.io.newURI("http://keyerror.com", null, null);
+  var uri = Services.io.newURI("http://bugzilla.mozilla.org", null, null);
   gSTSService.processStsHeader(uri, "max-age=0");
-  do_check_false(gSTSService.isStsHost("keyerror.com"));
-  do_check_false(gSTSService.isStsHost("subdomain.keyerror.com"));
+  do_check_false(gSTSService.isStsHost("bugzilla.mozilla.org"));
+  do_check_false(gSTSService.isStsHost("subdomain.bugzilla.mozilla.org"));
   // check that processing another header (with max-age non-zero) will
   // re-enable a site's sts status
   gSTSService.processStsHeader(uri, "max-age=1000");
-  do_check_true(gSTSService.isStsHost("keyerror.com"));
+  do_check_true(gSTSService.isStsHost("bugzilla.mozilla.org"));
   // but this time include subdomains was not set, so test for that
-  do_check_false(gSTSService.isStsHost("subdomain.keyerror.com"));
+  do_check_false(gSTSService.isStsHost("subdomain.bugzilla.mozilla.org"));
+  clearStsState();
 
   // check that processing a header with max-age: 0 from a subdomain of a site
   // will not remove that (ancestor) site from the list
-  var uri = Services.io.newURI("http://subdomain.ottospora.nl", null, null);
+  var uri = Services.io.newURI("http://subdomain.www.torproject.org", null, null);
   gSTSService.processStsHeader(uri, "max-age=0");
-  do_check_true(gSTSService.isStsHost("ottospora.nl"));
-  do_check_false(gSTSService.isStsHost("subdomain.ottospora.nl"));
+  do_check_true(gSTSService.isStsHost("www.torproject.org"));
+  do_check_false(gSTSService.isStsHost("subdomain.www.torproject.org"));
 
-  var uri = Services.io.newURI("http://subdomain.pixi.me", null, null);
+  var uri = Services.io.newURI("http://subdomain.bugzilla.mozilla.org", null, null);
   gSTSService.processStsHeader(uri, "max-age=0");
   // we received a header with "max-age=0", so we have "no information"
-  // regarding the sts state of subdomain.pixi.me specifically, but
-  // it is actually still an STS host, because of the preloaded pixi.me
-  // including subdomains.
+  // regarding the sts state of subdomain.bugzilla.mozilla.org specifically,
+  // but it is actually still an STS host, because of the preloaded
+  // bugzilla.mozilla.org including subdomains.
   // Here's a drawing:
-  // |-- pixi.me (in preload list, includes subdomains)      IS sts host
-  //     |-- subdomain.pixi.me                               IS sts host
-  //     |   `-- another.subdomain.pixi.me                   IS sts host
-  //     `-- sibling.pixi.me                                 IS sts host
-  do_check_true(gSTSService.isStsHost("subdomain.pixi.me"));
-  do_check_true(gSTSService.isStsHost("sibling.pixi.me"));
-  do_check_true(gSTSService.isStsHost("another.subdomain.pixi.me"));
+  // |-- bugzilla.mozilla.org (in preload list, includes subdomains) IS sts host
+  //     |-- subdomain.bugzilla.mozilla.org                          IS sts host
+  //     |   `-- another.subdomain.bugzilla.mozilla.org              IS sts host
+  //     `-- sibling.bugzilla.mozilla.org                            IS sts host
+  do_check_true(gSTSService.isStsHost("bugzilla.mozilla.org"));
+  do_check_true(gSTSService.isStsHost("subdomain.bugzilla.mozilla.org"));
+  do_check_true(gSTSService.isStsHost("sibling.bugzilla.mozilla.org"));
+  do_check_true(gSTSService.isStsHost("another.subdomain.bugzilla.mozilla.org"));
+
 
   gSTSService.processStsHeader(uri, "max-age=1000");
   // Here's what we have now:
-  // |-- pixi.me (in preload list, includes subdomains)      IS sts host
-  //     |-- subdomain.pixi.me (include subdomains is false) IS sts host
-  //     |   `-- another.subdomain.pixi.me                   IS NOT sts host
-  //     `-- sibling.pixi.me                                 IS sts host
-  do_check_true(gSTSService.isStsHost("subdomain.pixi.me"));
-  do_check_true(gSTSService.isStsHost("sibling.pixi.me"));
-  do_check_false(gSTSService.isStsHost("another.subdomain.pixi.me"));
+  // |-- bugzilla.mozilla.org (in preload list, includes subdomains) IS sts host
+  //     |-- subdomain.bugzilla.mozilla.org (include subdomains is false) IS sts host
+  //     |   `-- another.subdomain.bugzilla.mozilla.org              IS NOT sts host
+  //     `-- sibling.bugzilla.mozilla.org                            IS sts host
+  do_check_true(gSTSService.isStsHost("subdomain.bugzilla.mozilla.org"));
+  do_check_true(gSTSService.isStsHost("sibling.bugzilla.mozilla.org"));
+  do_check_false(gSTSService.isStsHost("another.subdomain.bugzilla.mozilla.org"));
 
   // Test private browsing correctly interacts with removing preloaded sites.
   // If we don't have the private browsing service, don't run those tests
@@ -155,26 +157,27 @@ function test_part1() {
 }
 
 function test_private_browsing1() {
-  // sanity - crypto.cat is preloaded, includeSubdomains set
-  do_check_true(gSTSService.isStsHost("crypto.cat"));
-  do_check_true(gSTSService.isStsHost("a.b.c.subdomain.crypto.cat"));
+  clearStsState();
+  // sanity - bugzilla.mozilla.org is preloaded, includeSubdomains set
+  do_check_true(gSTSService.isStsHost("bugzilla.mozilla.org");
+  do_check_true(gSTSService.isStsHost("a.b.c.subdomain.bugzilla.mozilla.org"));
 
-  var uri = Services.io.newURI("http://crypto.cat", null, null);
+  var uri = Services.io.newURI("http://bugzilla.mozilla.org", null, null);
   gSTSService.processStsHeader(uri, "max-age=0");
-  do_check_false(gSTSService.isStsHost("crypto.cat"));
-  do_check_false(gSTSService.isStsHost("a.b.subdomain.crypto.cat"));
+  do_check_false(gSTSService.isStsHost("bugzilla.mozilla.org"));
+  do_check_false(gSTSService.isStsHost("a.b.subdomain.bugzilla.mozilla.org"));
 
   // check adding it back in
   gSTSService.processStsHeader(uri, "max-age=1000");
-  do_check_true(gSTSService.isStsHost("crypto.cat"));
+  do_check_true(gSTSService.isStsHost("bugzilla.mozilla.org"));
   // but no includeSubdomains this time
-  do_check_false(gSTSService.isStsHost("b.subdomain.crypto.cat"));
+  do_check_false(gSTSService.isStsHost("b.subdomain.bugzilla.mozilla.org"));
 
   // do the hokey-pokey...
   gSTSService.processStsHeader(uri, "max-age=0");
-  do_check_false(gSTSService.isStsHost("crypto.cat"));
-  do_check_false(gSTSService.isStsHost("subdomain.crypto.cat"));
-
+  do_check_false(gSTSService.isStsHost("bugzilla.mozilla.org"));
+  do_check_false(gSTSService.isStsHost("subdomain.bugzilla.mozilla.org"));  
+  
   // TODO unfortunately we don't have a good way to know when an entry
   // has expired in the permission manager, so we can't yet extend this test
   // to that case.
@@ -184,12 +187,12 @@ function test_private_browsing1() {
   // a site on the preload list, and that header later expires. We need to
   // then treat that host as no longer an sts host.)
   // (sanity check first - this should be in the preload list)
-  do_check_true(gSTSService.isStsHost("logentries.com"));
-  var uri = Services.io.newURI("http://logentries.com", null, null);
+  do_check_true(gSTSService.isStsHost("login.persona.org"));
+  var uri = Services.io.newURI("http://login.persona.org", null, null);
   // according to the rfc, max-age can't be negative, but this is a great
   // way to test an expired entry
   gSTSService.processStsHeader(uri, "max-age=-1000");
-  do_check_false(gSTSService.isStsHost("logentries.com"));
+  do_check_false(gSTSService.isStsHost("login.persona.org"));
 
   // if this test gets this far, it means there's a private browsing service
   getPBSvc().privateBrowsingEnabled = false;
@@ -197,13 +200,13 @@ function test_private_browsing1() {
 
 function test_private_browsing2() {
   // if this test gets this far, it means there's a private browsing service
-  do_check_true(gSTSService.isStsHost("crypto.cat"));
-  // the crypto.cat entry has includeSubdomains set
-  do_check_true(gSTSService.isStsHost("subdomain.crypto.cat"));
+  do_check_true(gSTSService.isStsHost("bugzilla.mozilla.org"));
+  // the bugzilla.mozilla.org entry has includeSubdomains set
+  do_check_true(gSTSService.isStsHost("subdomain.bugzilla.mozilla.org"));
 
   // Now that we're out of private browsing mode, we need to make sure
   // we've "forgotten" that we "forgot" this site's sts status.
-  do_check_true(gSTSService.isStsHost("logentries.com"));
+  do_check_true(gSTSService.isStsHost("login.persona.org"));
 
   run_next_test();
 }
