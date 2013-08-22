01a48cf3e520715dbacb8ac24a0fa6567fcb3e8f
Jan Jongboom <janjongboom@gmail.com>
2013-08-22 09:53:47 -0400

    Bug 902847 - ContentEditable insertText doesnt handle CR right. r=yxl, a=leo+

--- b2g/chrome/content/forms.js
+++ b2g/chrome/content/forms.js
@@ -794,6 +794,9 @@ function replaceSurroundingText(element, text, selectionStart, beforeLength,
   }
 
   if (text) {
+    // We don't use CR but LF
+    // see https://bugzilla.mozilla.org/show_bug.cgi?id=902847
+    text = text.replace(/\r/g, '\n');
     // Insert the text to be replaced with.
     editor.insertText(text);
   }
