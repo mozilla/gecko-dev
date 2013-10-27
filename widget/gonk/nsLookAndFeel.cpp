7e1048e573c67d0a3570a5d107901b4b79eefc12
Jonathan Kew <jkew@mozilla.com>
2013-10-26 21:48:36 -0400

    Bug 919845 - Really make Fira Sans the default font, b2g18 version. r=mwu, a=hd+

--- widget/gonk/nsLookAndFeel.cpp
+++ widget/gonk/nsLookAndFeel.cpp
@@ -378,7 +378,7 @@ nsLookAndFeel::GetFontImpl(FontID aID, nsString& aFontName,
                            gfxFontStyle& aFontStyle,
                            float aDevPixPerCSSPixel)
 {
-    aFontName.AssignLiteral("\"Droid Sans\"");
+    aFontName.AssignLiteral("\"Fira Sans OT\"");
     aFontStyle.style = NS_FONT_STYLE_NORMAL;
     aFontStyle.weight = NS_FONT_WEIGHT_NORMAL;
     aFontStyle.stretch = NS_FONT_STRETCH_NORMAL;
