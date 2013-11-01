de50b78ea165f9de28abfbf25ad7fad9f80c482b
Olli Pettay <Olli.Pettay@helsinki.fi>
2013-11-01 14:55:05 +0200

    Bug 924681 - Return early from nsEventStateManager::FireContextClick if we don't have a prescontext. r=masayuki, a=koi+

--- content/events/src/nsEventStateManager.cpp
+++ content/events/src/nsEventStateManager.cpp
@@ -1659,8 +1659,9 @@ nsEventStateManager::sClickHoldCallback(nsITimer *aTimer, void* aESM)
 void
 nsEventStateManager::FireContextClick()
 {
-  if (!mGestureDownContent)
+  if (!mGestureDownContent || !mPresContext) {
     return;
+  }
 
 #ifdef XP_MACOSX
   // Hack to ensure that we don't show a context menu when the user
