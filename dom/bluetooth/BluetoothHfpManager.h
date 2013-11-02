26bf458273e593be2f881cfd679767121b15c3d0
Vicamo Yang <vyang@mozilla.com>
2013-11-02 21:53:57 +0800

    Bug 920551 - 2.i/2: fix Bluetooth. r=echou, a=koi+

--- dom/bluetooth/BluetoothHfpManager.h
+++ dom/bluetooth/BluetoothHfpManager.h
@@ -9,7 +9,9 @@
 
 #include "BluetoothCommon.h"
 #include "BluetoothProfileManagerBase.h"
+#ifdef MOZ_B2G_RIL
 #include "BluetoothRilListener.h"
+#endif
 #include "BluetoothSocketObserver.h"
 #include "mozilla/ipc/UnixSocket.h"
 #include "mozilla/Hal.h"
@@ -18,7 +20,10 @@ BEGIN_BLUETOOTH_NAMESPACE
 
 class BluetoothReplyRunnable;
 class BluetoothSocket;
+
+#ifdef MOZ_B2G_RIL
 class Call;
+#endif
 
 /**
  * These costants are defined in 4.33.2 "AT Capabilities Re-Used from GSM 07.07
@@ -91,6 +96,7 @@ public:
   bool DisconnectSco();
   bool ListenSco();
 
+#ifdef MOZ_B2G_RIL
   /**
    * @param aSend A boolean indicates whether we need to notify headset or not
    */
@@ -99,6 +105,7 @@ public:
                               const bool aIsOutgoing, bool aSend);
   void HandleIccInfoChanged();
   void HandleVoiceConnectionChanged();
+#endif
 
   bool IsConnected();
   bool IsScoConnected();
@@ -106,13 +113,17 @@ public:
 private:
   class CloseScoTask;
   class GetVolumeTask;
+#ifdef MOZ_B2G_RIL
   class RespondToBLDNTask;
   class SendRingIndicatorTask;
+#endif
 
   friend class CloseScoTask;
   friend class GetVolumeTask;
+#ifdef MOZ_B2G_RIL
   friend class RespondToBLDNTask;
   friend class SendRingIndicatorTask;
+#endif
   friend class BluetoothHfpManagerObserver;
 
   BluetoothHfpManager();
@@ -122,37 +133,49 @@ private:
   bool Init();
   void Notify(const hal::BatteryInformation& aBatteryInfo);
   void Reset();
+#ifdef MOZ_B2G_RIL
   void ResetCallArray();
   uint32_t FindFirstCall(uint16_t aState);
   uint32_t GetNumberOfCalls(uint16_t aState);
+#endif
 
   void NotifyConnectionStatusChanged(const nsAString& aType);
   void NotifyDialer(const nsAString& aCommand);
 
   bool SendCommand(const char* aCommand, uint32_t aValue = 0);
   bool SendLine(const char* aMessage);
+#ifdef MOZ_B2G_RIL
   void UpdateCIND(uint8_t aType, uint8_t aValue, bool aSend = true);
+#endif
   void OnScoConnectSuccess();
   void OnScoConnectError();
   void OnScoDisconnect();
 
   int mCurrentVgs;
   int mCurrentVgm;
+#ifdef MOZ_B2G_RIL
   bool mBSIR;
   bool mCCWA;
   bool mCLIP;
+#endif
   bool mCMEE;
   bool mCMER;
+#ifdef MOZ_B2G_RIL
   bool mFirstCKPD;
   int mNetworkSelectionMode;
+#endif
   bool mReceiveVgsFlag;
+#ifdef MOZ_B2G_RIL
   bool mDialingRequestProcessed;
+#endif
   nsString mDeviceAddress;
+#ifdef MOZ_B2G_RIL
   nsString mMsisdn;
   nsString mOperatorName;
 
   nsTArray<Call> mCurrentCallArray;
   nsAutoPtr<BluetoothRilListener> mListener;
+#endif
   nsRefPtr<BluetoothProfileController> mController;
   nsRefPtr<BluetoothReplyRunnable> mScoRunnable;
 
