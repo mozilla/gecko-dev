ebc33921c4e13b8cbe7071d4fa7bf32b74aede84
Eric Chou <echou@mozilla.com>
2013-11-12 17:37:30 +0800

    Bug 936711 - Fix crash which happened at disabling Bluetooth during reconnection. r=gyeh, a=koi+

--- dom/bluetooth/linux/BluetoothDBusService.cpp
+++ dom/bluetooth/linux/BluetoothDBusService.cpp
@@ -1976,6 +1976,8 @@ BluetoothDBusService::SendAsyncDBusMessage(const nsAString& aObjectPath,
   MOZ_ASSERT(!aObjectPath.IsEmpty());
   MOZ_ASSERT(aInterface);
 
+  NS_ENSURE_TRUE(mConnection, NS_ERROR_FAILURE);
+
   nsAutoPtr<BluetoothServiceClass> serviceClass(new BluetoothServiceClass());
   if (!strcmp(aInterface, DBUS_SINK_IFACE)) {
     *serviceClass = BluetoothServiceClass::A2DP;
@@ -2586,12 +2588,15 @@ BluetoothDBusService::SetPairingConfirmationInternal(
 static void
 NextBluetoothProfileController()
 {
-  sControllerArray[0] = nullptr;
+  MOZ_ASSERT(NS_IsMainThread());
+
+  // First, remove the task at the front which has been already done.
+  NS_ENSURE_FALSE_VOID(sControllerArray.IsEmpty());
   sControllerArray.RemoveElementAt(0);
 
-  if (!sControllerArray.IsEmpty()) {
-    sControllerArray[0]->Start();
-  }
+  // Re-check if the task array is empty, if it's not, the next task will begin.
+  NS_ENSURE_FALSE_VOID(sControllerArray.IsEmpty());
+  sControllerArray[0]->Start();
 }
 
 static void
