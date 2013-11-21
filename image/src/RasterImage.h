164ccdf7911a3d54b8d14643539e01a9da7a680c
Seth Fowler <seth@mozilla.com>
2013-11-20 17:21:51 -0800

    Bug 940714 - Add a RAII class to make synchronous decoding safer. r=tn, a=abillings

--- image/src/RasterImage.h
+++ image/src/RasterImage.h
@@ -750,7 +750,7 @@ private: // data
   // Decoding
   nsresult WantDecodedFrames();
   nsresult SyncDecode();
-  nsresult InitDecoder(bool aDoSizeDecode, bool aIsSynchronous = false);
+  nsresult InitDecoder(bool aDoSizeDecode);
   nsresult WriteToDecoder(const char *aBuffer, uint32_t aCount);
   nsresult DecodeSomeData(uint32_t aMaxBytes);
   bool     IsDecodeFinished();
