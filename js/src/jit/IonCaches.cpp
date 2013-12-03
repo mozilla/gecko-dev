7bf2255d6a0c119b879ee773aa9357697fa9f9e5
Eric Faust <efaustbmo@gmail.com>
2013-12-03 13:09:50 -0800

    Bug 929261 - Only generate TypedArray IC stubs on in-bounds accesses. (r=shu, a=abillings)

--- js/src/jit/IonCaches.cpp
+++ js/src/jit/IonCaches.cpp
@@ -2478,6 +2478,39 @@ GetElementIC::attachArgumentsElement(JSContext *cx, IonScript *ion, JSObject *ob
     return linkAndAttachStub(cx, masm, attacher, ion, "ArgsObj element (normal)");
 }
 
+static bool
+ShouldAttachGetElemTypedArray(JSObject *obj, const Value &idval,
+                              TypedOrValueRegister output)
+{
+    if (!obj->isTypedArray())
+        return false;
+
+    if (!idval.isInt32() && !idval.isString())
+        return false;
+
+    // Don't emit a stub if the access is out of bounds. We make to make
+    // certain that we monitor the type coming out of the typed array when
+    // we generate the stub. Out of bounds accesses will hit the fallback
+    // path.
+    uint32_t index;
+    if (idval.isInt32()) {
+        index = idval.toInt32();
+    } else {
+        index = GetIndexFromString(idval.toString());
+        if (index == UINT32_MAX)
+            return false;
+    }
+    if (index >= TypedArray::length(obj))
+        return false;
+
+    // The output register is not yet specialized as a float register, the only
+    // way to accept float typed arrays for now is to return a Value type.
+    int arrayType = TypedArray::type(obj);
+    bool floatOutput = arrayType == TypedArray::TYPE_FLOAT32 ||
+                       arrayType == TypedArray::TYPE_FLOAT64;
+    return !floatOutput || output.hasValue();
+}
+
 bool
 GetElementIC::update(JSContext *cx, size_t cacheIndex, HandleObject obj,
                      HandleValue idval, MutableHandleValue res)
@@ -2531,19 +2564,10 @@ GetElementIC::update(JSContext *cx, size_t cacheIndex, HandleObject obj,
                 return false;
             attachedStub = true;
         }
-        if (!attachedStub && obj->isTypedArray()) {
-            if ((idval.isInt32()) ||
-                (idval.isString() && GetIndexFromString(idval.toString()) != UINT32_MAX))
-            {
-                int arrayType = TypedArray::type(obj);
-                bool floatOutput = arrayType == TypedArray::TYPE_FLOAT32 ||
-                                   arrayType == TypedArray::TYPE_FLOAT64;
-                if (!floatOutput || cache.output().hasValue()) {
-                    if (!cache.attachTypedArrayElement(cx, ion, obj, idval))
-                        return false;
-                    attachedStub = true;
-                }
-            }
+        if (!attachedStub && ShouldAttachGetElemTypedArray(obj, idval, cache.output())) {
+            if (!cache.attachTypedArrayElement(cx, ion, obj, idval))
+                return false;
+            attachedStub = true;
         }
     }
 
