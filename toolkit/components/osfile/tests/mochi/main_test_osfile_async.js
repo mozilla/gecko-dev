cb4d98d39dc39c82ade4845c77b55904ff755f98
Ryan VanderMeulen <ryanvm@gmail.com>
2013-12-06 10:09:10 -0500

    Backed out changeset 5c7962855ea7 (bug 927560)

--- toolkit/components/osfile/tests/mochi/main_test_osfile_async.js
+++ toolkit/components/osfile/tests/mochi/main_test_osfile_async.js
@@ -160,6 +160,7 @@ let test = maketest("Main", function main(test) {
     yield test_iter();
     yield test_exists();
     yield test_debug_test();
+    yield test_system_shutdown();
     yield test_duration();
     info("Test is over");
     SimpleTest.finish();
