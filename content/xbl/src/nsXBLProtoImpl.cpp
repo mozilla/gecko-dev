05392b3d396f65a5845d3efc4060d8a18868020d
Bobby Holley <bobbyholley@gmail.com>
2013-12-13 13:22:06 -0800

    Bug 911864 - Only expose XBL members which have the exposeToUntrustedContent attribute set. r=smaug

--- content/xbl/src/nsXBLProtoImpl.cpp
+++ content/xbl/src/nsXBLProtoImpl.cpp
@@ -18,6 +18,7 @@
 #include "nsIDOMNode.h"
 #include "nsXBLPrototypeBinding.h"
 #include "nsXBLProtoImplProperty.h"
+#include "js/CharacterEncoding.h"
 
 using namespace mozilla;
 using js::GetGlobalForObjectCrossCompartment;
@@ -116,14 +117,22 @@ nsXBLProtoImpl::InstallImplementation(nsXBLPrototypeBinding* aPrototypeBinding,
   JSAutoCompartment ac2(cx, targetClassObject);
 
   // Now, if we're using a separate XBL scope, enter the compartment of the
-  // bound node and copy the properties to the prototype there. This rewraps
-  // them appropriately, which should result in cross-compartment function
-  // wrappers.
+  // bound node and copy exposable properties to the prototype there. This
+  // rewraps them appropriately, which should result in cross-compartment
+  // function wrappers.
   if (propertyHolder != targetClassObject) {
     AssertSameCompartment(propertyHolder, scopeObject);
     AssertSameCompartment(targetClassObject, globalObject);
-    bool ok = JS_CopyPropertiesFrom(cx, targetClassObject, propertyHolder);
-    NS_ENSURE_TRUE(ok, NS_ERROR_UNEXPECTED);
+    for (nsXBLProtoImplMember* curr = mMembers; curr; curr = curr->GetNext()) {
+      if (curr->ShouldExposeToUntrustedContent()) {
+        JS::Rooted<jsid> id(cx);
+        JS::TwoByteChars chars(curr->GetName(), NS_strlen(curr->GetName()));
+        bool ok = JS_CharsToId(cx, chars, id.address());
+        NS_ENSURE_TRUE(ok, NS_ERROR_UNEXPECTED);
+        JS_CopyPropertyFrom(cx, id, targetClassObject, propertyHolder);
+        NS_ENSURE_TRUE(ok, NS_ERROR_UNEXPECTED);
+      }
+    }
   }
 
   // Install all of our field accessors.
