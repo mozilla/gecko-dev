6d77eda158320cdf5a63d7b75e3ca66f3efb749d
Dan Gohman <sunfish@google.com>
2014-01-15 10:39:32 -0500

    Bug 950438 - IonMonkey: The intersection of two ranges that both contain NaN is not empty. r=nbp, a=bajaj

--- js/src/jit/RangeAnalysis.cpp
+++ js/src/jit/RangeAnalysis.cpp
@@ -339,7 +339,9 @@ Range::intersect(const Range *lhs, const Range *rhs, bool *emptyRange)
     // Instead, we should use it to eliminate the dead block.
     // (Bug 765127)
     if (r->upper_ < r->lower_) {
-        *emptyRange = true;
+        // If both ranges can be NaN, the result can still be NaN.
+        if (!lhs->isInfinite() || !rhs->isInfinite())
+            *emptyRange = true;
         r->makeRangeInfinite();
     }
 
