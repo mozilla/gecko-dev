25e503f11fd0c500904277071d9b1094c03f2b3a
Hsin-Yi Tsai <htsai@mozilla.com>
2014-01-23 13:58:30 +0800

    Bug 962447 - [fugu][b2g] Should call getFailCauseCode() even the connection hasn't been established. r=vicamo, a=fabrice

--- dom/system/gonk/ril_worker.js
+++ dom/system/gonk/ril_worker.js
@@ -3675,8 +3675,8 @@ let RIL = {
 
   _sendCallError: function _sendCallError(callIndex, errorMsg) {
     this.sendChromeMessage({rilMessageType: "callError",
-                           callIndex: callIndex,
-                           errorMsg: errorMsg});
+                            callIndex: callIndex,
+                            errorMsg: errorMsg});
   },
 
   _sendDataCallError: function _sendDataCallError(message, errorCode) {
@@ -5025,8 +5025,7 @@ RIL[REQUEST_DIAL] = function REQUEST_DIAL(length, options) {
   if (options.rilRequestError) {
     // The connection is not established yet.
     options.callIndex = -1;
-    this._sendCallError(options.callIndex,
-                        RIL_ERROR_TO_GECKO_ERROR[options.rilRequestError]);
+    this.getFailCauseCode(options);
   }
 };
 RIL[REQUEST_GET_IMSI] = function REQUEST_GET_IMSI(length, options) {
