e4a75ecb499e328c21fac8a040644a56c061457e
Ben Kelly <ben@wanderview.com>
2014-02-06 16:38:39 -0500

    Bug 960873 - Part 1: Refactor MediaDecoderReader.h into separate headers. r=cpearce, a=1.3+

---
+++ content/media/MediaInfo.h
@@ -0,0 +1,84 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#if !defined(MediaInfo_h)
+#define MediaInfo_h
+
+#include "nsSize.h"
+#include "nsRect.h"
+#include "ImageTypes.h"
+
+namespace mozilla {
+ 
+// Stores info relevant to presenting media frames.
+class VideoInfo {
+public:
+  VideoInfo()
+    : mDisplay(0,0),
+      mStereoMode(STEREO_MODE_MONO),
+      mHasVideo(false)
+  {}
+
+  // Returns true if it's safe to use aPicture as the picture to be
+  // extracted inside a frame of size aFrame, and scaled up to and displayed
+  // at a size of aDisplay. You should validate the frame, picture, and
+  // display regions before using them to display video frames.
+  static bool ValidateVideoRegion(const nsIntSize& aFrame,
+                                  const nsIntRect& aPicture,
+                                  const nsIntSize& aDisplay);
+
+  // Size in pixels at which the video is rendered. This is after it has
+  // been scaled by its aspect ratio.
+  nsIntSize mDisplay;
+
+  // Indicates the frame layout for single track stereo videos.
+  StereoMode mStereoMode;
+
+  // True if we have an active video bitstream.
+  bool mHasVideo;
+};
+
+class AudioInfo {
+public:
+  AudioInfo()
+    : mRate(44100),
+      mChannels(2),
+      mHasAudio(false)
+  {}
+
+  // Sample rate.
+  uint32_t mRate;
+
+  // Number of audio channels.
+  uint32_t mChannels;
+
+  // True if we have an active audio bitstream.
+  bool mHasAudio;
+};
+
+class MediaInfo {
+public:
+  bool HasVideo() const
+  {
+    return mVideo.mHasVideo;
+  }
+
+  bool HasAudio() const
+  {
+    return mAudio.mHasAudio;
+  }
+
+  bool HasValidMedia() const
+  {
+    return HasVideo() || HasAudio();
+  }
+
+  VideoInfo mVideo;
+  AudioInfo mAudio;
+};
+
+} // namespace mozilla
+
+#endif // MediaInfo_h
