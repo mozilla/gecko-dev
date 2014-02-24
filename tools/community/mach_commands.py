5dc031a11fda3b20e6db04666de015c8a849dd5e
Mike Hoye <mhoye@mozilla.com>
2014-02-24 16:54:59 -0500

    This looks like a minimum working product to me.
    TODO: Detect patches that span modules, warn about them.
    
    - mhoye

--- tools/community/mach_commands.py
+++ tools/community/mach_commands.py
@@ -24,45 +24,47 @@ class FindReviewerCommand(object):
         self._context = context
 
     @Command('find-reviewer', category='misc',
-        description='Find the right reviewer for your patch. Recommends the one most likely reviewer\n' + '')
+        description='Find the right reviewer for your patch.\n' + '')
 
-#    @CommandArgument(number, '--number','-n', default=1,
-#        description='Recommend some number of reviewers, in order of likelihood. Default is the 1 most likely reviewer.')
-#    @CommandArgument(history, '--history','-h', default=8, 
-#        description='Look back for some number of prior reviewers for each file. Default is 8.')
+    @CommandArgument('--number','-n', default='1', type=int,
+        help='Recommend a number of reviewers, in order of likelihood. Default is the 1 most likely reviewer.')
+    @CommandArgument('--history','-h', default='8', type=int,
+        help='Look back for some number of prior reviewers for each file. Default is 8.')
 
     # These may or may not be consistent over time, so:
-    def find_reviewers(self):
-        GetModifiedFiles_Git = "git ls-files --modified "
-        GetPreviousCheckins_Git = "git log -n 1" 
-        GetSummary_Git = "git log --pretty=oneline --abbrev-commit -n " + str(history) 
+    def find_reviewers(self, number, history):
+            
+        GetModifiedFiles_Git = "git ls-files -m "
+        GetPreviousCheckins_Git = "git log -n " + str(history) + " " 
+        GetSummary_Git = "git log --pretty=oneline --abbrev-commit -n " + str(history) + " " 
 
         GetModifiedFiles_Hg = "hg status -n --modified"
-        GetPreviousCheckins_Hg = "hg log -l 1 "
-        GetSummary_Hg          = "hg log --template '{desc|firstline}\\n' -l 10 "   
+        GetPreviousCheckins_Hg = "hg log -l " + str(history) + " " 
+        GetSummary_Hg          = "hg log --template '{desc|firstline}\\n' -l " + str(history) + " "  
 
        
         def GetReviewersFromGit(CanRevs):
-            ChangeSet = subprocess.check_output(GetModifiedFiles_Git.split(), shell=False, universal_newlines=True).split("\n")
-            for FileChanged in Changeset:
+            ChangeSets = subprocess.check_output(GetModifiedFiles_Git.split(),
+                              shell=False, universal_newlines=True).split("\n")
+            for FileChanged in ChangeSets:
                 GetChangeHistory = GetPreviousCheckins_Git + str(FileChanged)
-                PerFileChangeHistory = subprocess.check_output(GetChangeHistory.split(), shell=False, universal_newlines=True).split("\n")
+                PerFileChangeHistory = subprocess.check_output(GetChangeHistory.split(),
+                                        shell=False, universal_newlines=True).split("\n")
                 for Words in PerFileChangeHistory:
                     if "r=" in Words:
                         CanRevs.append(Words[Words.rfind("r=")+2:])
-                    
-
             return CanRevs
 
         def GetReviewersFromHg(CanRevs):
-            Changesets = subprocess.check_output(GetModifiedFiles_Hg.split(), shell=False, universal_newlines=True).split("\n")
+            Changesets = subprocess.check_output(GetModifiedFiles_Hg.split(), 
+                                                  shell=False, universal_newlines=True).split("\n")
             for FileChanged in Changesets:
                 GetChangeHistory = GetSummary_Hg + str(FileChanged)
-                PerFileChangeHistory = subprocess.check_output(GetChangeHistory.split(), shell=False, universal_newlines=True).split("\n")
+                PerFileChangeHistory = subprocess.check_output(GetChangeHistory.split(), 
+                                                  shell=False, universal_newlines=True).split("\n")
                 for Words in PerFileChangeHistory:
                     if "r=" in Words:
                         CanRevs.append(Words[Words.rfind("r=")+2:])
-                    
             return CanRevs
 
         def GetReviewers():
@@ -71,7 +73,16 @@ class FindReviewerCommand(object):
                 return GetReviewersFromHg(CandidateReviewers)
             else:
                 return GetReviewersFromGit(CandidateReviewers)
+            return Counter(CandidateReviewers)
 
-        print (Counter(GetReviewers())) 
+        if number == 1:
+            print ("The most likely reviewer for your patch is: " 
+                    + " ".join(GetReviewers()[:1])  )
+        else:
+            print ("It's likely that the reviewers for your patch are: " 
+                    + " ".join(GetReviewers()[:number])  )
 
+
+# This fails in a number of important ways - "r=gps." != "r=gps" for example, or "r=me".
+# But improvements are, as ever, on the way.
 
