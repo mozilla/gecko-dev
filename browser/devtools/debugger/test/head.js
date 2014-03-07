b4e109b04af60fd003a55a2381874f3d79b7b8e1
Jordan Santell <jsantell@gmail.com>
2014-03-07 00:09:14 -0800

    Bug 899052 Implement Addon Thread Actor

--- browser/devtools/debugger/test/head.js
+++ browser/devtools/debugger/test/head.js
@@ -153,6 +153,7 @@ function getTabActorForUrl(aClient, aUrl) {
 }
 
 function getAddonActorForUrl(aClient, aUrl) {
+  info("Get addon actor for URL: " + aUrl);
   let deferred = promise.defer();
 
   aClient.listAddons(aResponse => {
@@ -660,3 +661,14 @@ function filterTraces(aPanel, f) {
   return Array.filter(traces, f);
 }
 
+function attachAddonActorForUrl(aClient, aUrl) {
+  let deferred = promise.defer();
+
+  getAddonActorForUrl(aClient, aUrl).then(aGrip => {
+    aClient.attachAddon(aGrip.actor, aResponse => {
+      deferred.resolve([aGrip, aResponse]);
+    });
+  });
+
+  return deferred.promise;
+}
