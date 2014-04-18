416d8e9e7ce9050e1addce88694d0ff7a9cd7580
Star Cheng <scheng@mozilla.com>
2014-04-18 17:23:36 +0800

    Bug 984498 - Plumb audiochannel type from AudioContext to MediaStreamGraph. r=roc, a=fabrice

--- content/media/webaudio/AudioDestinationNode.h
+++ content/media/webaudio/AudioDestinationNode.h
@@ -29,6 +29,7 @@ public:
   // whether it's in offline mode.
   AudioDestinationNode(AudioContext* aContext,
                        bool aIsOffline,
+                       AudioChannel aChannel = AudioChannel::Normal,
                        uint32_t aNumberOfChannels = 0,
                        uint32_t aLength = 0,
                        float aSampleRate = 0.0f);
