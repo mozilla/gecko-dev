f307b537ee5044d5b7762b4270c7452ebe683d03
Ting-Yu Chou <tchou@mozilla.com>
2014-04-24 15:26:50 +0800

    Bug 999351 - Fix an IPC leak by clearing MessageChannel resource before destruction. r=bent a=1.3t+
    Once we have a channel error (like, child process dies) we don't immediately do
    anything with additional messages that were pending from that child. They stay
    queued and eventually get freed when the channel is destroyed. This patch clears
    the resource earlier whenever there is an error, or the channel is closed.

--- ipc/glue/MessageChannel.cpp
+++ ipc/glue/MessageChannel.cpp
@@ -274,6 +274,15 @@ MessageChannel::Clear()
         mChannelErrorTask->Cancel();
         mChannelErrorTask = nullptr;
     }
+
+    // Free up any memory used by pending messages.
+    mPending.clear();
+    mPendingUrgentRequest = nullptr;
+    mPendingRPCCall = nullptr;
+    mOutOfTurnReplies.clear();
+    while (!mDeferred.empty()) {
+        mDeferred.pop();
+    }
 }
 
 bool
