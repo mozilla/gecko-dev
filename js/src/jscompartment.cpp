13e1937d8e3afd6952b356dc985a9a17afa8cf8b
Shu-yu Guo <shu@rfrn.org>
2014-04-23 17:33:44 -0700

    Bug 1000598 - Clear TypeCompartment tables. r=billm, a=1.3+

--- js/src/jscompartment.cpp
+++ js/src/jscompartment.cpp
@@ -621,6 +621,7 @@ JSCompartment::clearTables()
     JS_ASSERT(!gcWeakMapList);
     JS_ASSERT(enumerators->next() == enumerators);
 
+    types.clearTables();
     if (baseShapes.initialized())
         baseShapes.clear();
     if (initialShapes.initialized())
