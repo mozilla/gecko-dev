206f93646f0686655d28e05719472f425dc4babe
Vincent Liu <vliu@mozilla.com>
2014-05-19 09:52:47 +0800

    Bug 988704 - [dolphin][camera] Camera preview is obviously slower than android. r=sotaro, a=1.4+

--- widget/gonk/nativewindow/GonkNativeWindowKK.cpp
+++ widget/gonk/nativewindow/GonkNativeWindowKK.cpp
@@ -33,10 +33,10 @@ using namespace mozilla::layers;
 
 namespace android {
 
-GonkNativeWindow::GonkNativeWindow() :
+GonkNativeWindow::GonkNativeWindow(int bufferCount) :
     GonkConsumerBase(new GonkBufferQueue(true), false)
 {
-    mConsumer->setMaxAcquiredBufferCount(GonkBufferQueue::MIN_UNDEQUEUED_BUFFERS);
+    mConsumer->setMaxAcquiredBufferCount(bufferCount);
 }
 
 GonkNativeWindow::GonkNativeWindow(const sp<GonkBufferQueue>& bq,
