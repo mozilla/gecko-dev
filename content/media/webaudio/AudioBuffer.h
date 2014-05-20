73f647e97e284c38f0661252ed39621f64b64c00
Karl Tomlinson <karlt+@karlt.net>
2014-05-20 10:02:31 -0400

    Bug 991533 - Limit AudioBuffer channel counts and sample rate range. r=padenot, r=ehsan, r=bz, a=1.2.x+

--- content/media/webaudio/AudioBuffer.h
+++ content/media/webaudio/AudioBuffer.h
@@ -34,14 +34,10 @@ class AudioBuffer MOZ_FINAL : public nsWrapperCache,
                               public EnableWebAudioCheck
 {
 public:
-  AudioBuffer(AudioContext* aContext, uint32_t aLength,
-              float aSampleRate);
-  ~AudioBuffer();
-
-  // This function needs to be called in order to allocate
-  // all of the channels.  It is fallible!
-  bool InitializeBuffers(uint32_t aNumberOfChannels,
-                         JSContext* aJSContext);
+  static already_AddRefed<AudioBuffer>
+  Create(AudioContext* aContext, uint32_t aNumberOfChannels,
+         uint32_t aLength, float aSampleRate,
+         JSContext* aJSContext, ErrorResult& aRv);
 
   NS_INLINE_DECL_CYCLE_COLLECTING_NATIVE_REFCOUNTING(AudioBuffer)
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_NATIVE_CLASS(AudioBuffer)
@@ -98,12 +94,16 @@ public:
   void MixToMono(JSContext* aJSContext);
 
 protected:
+  AudioBuffer(AudioContext* aContext, uint32_t aNumberOfChannels,
+              uint32_t aLength, float aSampleRate);
+  ~AudioBuffer();
+
   bool RestoreJSChannelData(JSContext* aJSContext);
   void ClearJSChannels();
 
   nsRefPtr<AudioContext> mContext;
   // Float32Arrays
-  AutoFallibleTArray<JS::Heap<JSObject*>, 2> mJSChannels;
+  nsAutoTArray<JS::Heap<JSObject*>, 2> mJSChannels;
 
   // mSharedChannels aggregates the data from mJSChannels. This is non-null
   // if and only if the mJSChannels are neutered.
