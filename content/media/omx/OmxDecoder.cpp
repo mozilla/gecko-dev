571b8d33355d032147500eb374cfe87498f904bb
Sotaro Ikeda <sikeda@mozilla.com>
2014-05-20 18:22:57 -0700

    Bug 1011130 - Fix video seek problem in OmxDecoder. r=cajbir, a=1.4+

--- content/media/omx/OmxDecoder.cpp
+++ content/media/omx/OmxDecoder.cpp
@@ -799,6 +799,7 @@ bool OmxDecoder::ReadVideo(VideoFrame *aFrame, int64_t aTimeUs,
   if (aDoSeek) {
     {
       Mutex::Autolock autoLock(mSeekLock);
+      ReleaseAllPendingVideoBuffersLocked();
       mIsVideoSeeking = true;
     }
     MediaSource::ReadOptions options;
