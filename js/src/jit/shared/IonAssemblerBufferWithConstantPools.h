8612874aed5c628ab7dbd8f2c381f829a11c7bef
Marty Rosenberg <mrosenberg@mozilla.com>
2014-05-27 09:40:35 -0400

    Bug 996715 - Remove the code that bails when determining if the second instruction in a chunk is a branch. r=dougc, a=lmandel

--- js/src/jit/shared/IonAssemblerBufferWithConstantPools.h
+++ js/src/jit/shared/IonAssemblerBufferWithConstantPools.h
@@ -216,8 +216,6 @@ struct BufferSliceTail : public BufferSlice<SliceSize> {
         isBranch[idx >> 3] |= 1 << (idx & 0x7);
     }
     bool isNextBranch() {
-        if (this->nodeSize == InstBaseSize)
-            return false;
         int idx = this->nodeSize / InstBaseSize;
         return (isBranch[idx >> 3] >> (idx & 0x7)) & 1;
     }
