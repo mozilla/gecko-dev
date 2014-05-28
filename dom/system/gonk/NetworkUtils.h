0566e7ac0cc027b81cffc551c45b802bdc39ca6e
Michael Vines <mvines@codeaurora.org>
2014-05-28 13:28:00 -0400

    Bug 1016022 - Restart softap when the link goes down unexpectedly. r=vchang, a=1.4+

--- dom/system/gonk/NetworkUtils.h
+++ dom/system/gonk/NetworkUtils.h
@@ -291,6 +291,7 @@ private:
   static CommandFunc sWifiEnableChain[];
   static CommandFunc sWifiDisableChain[];
   static CommandFunc sWifiFailChain[];
+  static CommandFunc sWifiRetryChain[];
   static CommandFunc sWifiOperationModeChain[];
   static CommandFunc sUSBEnableChain[];
   static CommandFunc sUSBDisableChain[];
@@ -316,6 +317,7 @@ private:
   static void createUpStream(PARAMS);
   static void startSoftAP(PARAMS);
   static void stopSoftAP(PARAMS);
+  static void clearWifiTetherParms(PARAMS);
   static void getRxBytes(PARAMS);
   static void getTxBytes(PARAMS);
   static void enableAlarm(PARAMS);
