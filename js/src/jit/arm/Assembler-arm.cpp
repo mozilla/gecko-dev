1f885a55119904c3d0670b52ad07e8903f8b0220
Brian Hackett <bhackett1024@gmail.com>
2014-06-12 09:11:00 -0400

    Bug 1019684 - Turn an assert into a release mode crash instead. r=mjrosenb, a=1.3+

--- js/src/jit/arm/Assembler-arm.cpp
+++ js/src/jit/arm/Assembler-arm.cpp
@@ -1866,7 +1866,7 @@ Assembler::as_b(Label *l, Condition c, bool isPatchable)
         // This will currently throw an assertion if we couldn't actually
         // encode the offset of the branch.
         if (!BOffImm::isInRange(old)) {
-            m_buffer.bail();
+            m_buffer.fail_bail();
             return ret;
         }
         ret = as_b(BOffImm(old), c, isPatchable);
@@ -1928,7 +1928,7 @@ Assembler::as_bl(Label *l, Condition c)
         // encode the offset of the branch.
         old = l->offset();
         if (!BOffImm::isInRange(old)) {
-            m_buffer.bail();
+            m_buffer.fail_bail();
             return ret;
         }
         ret = as_bl(BOffImm(old), c);
