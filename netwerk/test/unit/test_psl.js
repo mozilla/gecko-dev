994856d33e0338cb0378eeca454a6bca428acef7
Gervase Markham <gerv@gerv.net>
2014-07-03 15:22:34 +0100

    Bug 1033340 - port forward PSL test changes also, to try and get rid of xpcshell PSL bustage. a=bustage.

--- netwerk/test/unit/test_psl.js
+++ netwerk/test/unit/test_psl.js
@@ -1,8 +1,10 @@
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-
 var etld = Cc["@mozilla.org/network/effective-tld-service;1"]
              .getService(Ci.nsIEffectiveTLDService);
+
+var idna = Cc["@mozilla.org/network/idn-service;1"]
+           .getService(Ci.nsIIDNService);
+
+var Cr = Components.results;
 
 function run_test()
 {
@@ -13,7 +15,7 @@ function run_test()
   var scriptLoader = Cc["@mozilla.org/moz/jssubscript-loader;1"]
                        .getService(Ci.mozIJSSubScriptLoader);
   var srvScope = {};
-  scriptLoader.loadSubScript(uri.spec, srvScope);
+  scriptLoader.loadSubScript(uri.spec, srvScope, "utf-8");
 }
 
 function checkPublicSuffix(host, expectedSuffix)
@@ -21,8 +23,14 @@ function checkPublicSuffix(host, expectedSuffix)
   var actualSuffix = null;
   try {
     actualSuffix = etld.getBaseDomainFromHost(host);
-  } catch (e if e.name == "NS_ERROR_INSUFFICIENT_DOMAIN_LEVELS" ||
-                e.name == "NS_ERROR_ILLEGAL_VALUE") {
+  } catch (e if e.result == Cr.NS_ERROR_INSUFFICIENT_DOMAIN_LEVELS ||
+                e.result == Cr.NS_ERROR_ILLEGAL_VALUE) {
+  }
+  // The EffectiveTLDService always gives back punycoded labels.
+  // The test suite wants to get back what it put in.
+  if (actualSuffix !== null && expectedSuffix !== null &&
+      /(^|\.)xn--/.test(actualSuffix) && !/(^|\.)xn--/.test(expectedSuffix)) {
+    actualSuffix = idna.convertACEtoUTF8(actualSuffix);
   }
   do_check_eq(actualSuffix, expectedSuffix);
 }
