883db043b1e26edf30fc5a225124934ea0032dc3
Edgar Chen <echen@mozilla.com>
2014-08-06 15:35:34 +0800

    Bug 1048581 - Part 1: Voice() and Data() won't need to check mProvider is valid or not. r=smaug, a=2.0+

--- dom/mobileconnection/src/MobileConnection.cpp
+++ dom/mobileconnection/src/MobileConnection.cpp
@@ -203,20 +203,12 @@ MobileConnection::GetLastKnownHomeNetwork(nsString& aRetVal) const
 MobileConnectionInfo*
 MobileConnection::Voice() const
 {
-  if (!mProvider) {
-    return nullptr;
-  }
-
   return mVoice;
 }
 
 MobileConnectionInfo*
 MobileConnection::Data() const
 {
-  if (!mProvider) {
-    return nullptr;
-  }
-
   return mData;
 }
 
