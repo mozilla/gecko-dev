6b10a8db216cb9d0c5cc46d581ae94372965285b
Terrence Cole <terrence@mozilla.com>
2014-09-10 15:42:36 -0700

    Bug 1064346 - JSFunction's extended attributes expect POD-style initialization. r=billm, a=1.4+

--- js/public/Class.h
+++ js/public/Class.h
@@ -471,8 +471,12 @@ struct Class
         return flags & JSCLASS_EMULATES_UNDEFINED;
     }
 
+    bool isJSFunction() const {
+        return this == js::FunctionClassPtr;
+    }
+
     bool isCallable() const {
-        return this == js::FunctionClassPtr || call;
+        return isJSFunction() || call;
     }
 
     bool isProxy() const {
