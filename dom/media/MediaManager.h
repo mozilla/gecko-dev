24301a564b54ad65e5e13df7f1239c2afc39dac4
Randell Jesup <rjesup@jesup.org>
2014-09-19 17:24:28 -0400

    Bug 1062876 - Refactor window iteration code for MediaManager. r=jib, a=abillings

--- dom/media/MediaManager.h
+++ dom/media/MediaManager.h
@@ -470,6 +470,12 @@ public:
   MediaEngineAudioSource* GetSource();
 };
 
+// we could add MediaManager if needed
+typedef void (*WindowListenerCallback)(MediaManager *aThis,
+                                       uint64_t aWindowID,
+                                       StreamListeners *aListeners,
+                                       void *aData);
+
 class MediaManager MOZ_FINAL : public nsIMediaManagerService,
                                public nsIObserver
 {
@@ -542,8 +548,9 @@ private:
 
   ~MediaManager() {}
 
-  nsresult MediaCaptureWindowStateInternal(nsIDOMWindow* aWindow, bool* aVideo,
-                                           bool* aAudio);
+  void IterateWindowListeners(nsPIDOMWindow *aWindow,
+                              WindowListenerCallback aCallback,
+                              void *aData);
 
   void StopMediaStreams();
 
