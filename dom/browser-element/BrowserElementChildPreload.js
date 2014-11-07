71be912cc4fedd423869ad9dce72880de170851f
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2014-11-07 10:01:14 +0800

    Bug 1094083 - Make browser-element Handle nested alert gracefully. r=smaug, a=2.0m+
    Don't delete this._windowIDDict[outerID] until the last modal prompt is
    closed.

--- dom/browser-element/BrowserElementChildPreload.js
+++ dom/browser-element/BrowserElementChildPreload.js
@@ -387,6 +387,10 @@ BrowserElementChild.prototype = {
     }
     debug("Nested event loop - finish");
 
+    if (win.modalDepth == 0) {
+      delete this._windowIDDict[outerWindowID];
+    }
+
     // If we exited the loop because the inner window changed, then bail on the
     // modal prompt.
     if (innerWindowID !== this._tryGetInnerWindowID(win)) {
@@ -419,7 +423,6 @@ BrowserElementChild.prototype = {
     }
 
     let win = this._windowIDDict[outerID].get();
-    delete this._windowIDDict[outerID];
 
     if (!win) {
       debug("recvStopWaiting, but window is gone\n");
