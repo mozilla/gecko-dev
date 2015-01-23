e2cb0b385b8cde9dfc86372973ce88f88ab566b4
Szu-Yu Chen [:aknow] <szchen@mozilla.com>
2015-01-22 19:11:00 -0500

    Bug 1113027 - Part 2: Should pass clientId to notifyDialCallSuccess (dom). r=hsinyi, a=bajaj

--- dom/telephony/TelephonyDialCallback.cpp
+++ dom/telephony/TelephonyDialCallback.cpp
@@ -12,10 +12,8 @@ NS_IMPL_ISUPPORTS_INHERITED(TelephonyDialCallback, TelephonyCallback,
 
 TelephonyDialCallback::TelephonyDialCallback(nsPIDOMWindow* aWindow,
                                              Telephony* aTelephony,
-                                             Promise* aPromise,
-                                             uint32_t aServiceId)
-  : TelephonyCallback(aPromise), mWindow(aWindow), mTelephony(aTelephony),
-    mServiceId(aServiceId)
+                                             Promise* aPromise)
+  : TelephonyCallback(aPromise), mWindow(aWindow), mTelephony(aTelephony)
 {
   MOZ_ASSERT(mTelephony);
 }
@@ -23,12 +21,13 @@ TelephonyDialCallback::TelephonyDialCallback(nsPIDOMWindow* aWindow,
 // nsITelephonyDialCallback
 
 NS_IMETHODIMP
-TelephonyDialCallback::NotifyDialCallSuccess(uint32_t aCallIndex,
+TelephonyDialCallback::NotifyDialCallSuccess(uint32_t aClientId,
+                                             uint32_t aCallIndex,
                                              const nsAString& aNumber)
 {
   nsRefPtr<TelephonyCallId> id = mTelephony->CreateCallId(aNumber);
   nsRefPtr<TelephonyCall> call =
-    mTelephony->CreateCall(id, mServiceId, aCallIndex,
+    mTelephony->CreateCall(id, aClientId, aCallIndex,
                            nsITelephonyService::CALL_STATE_DIALING);
 
   mPromise->MaybeResolve(call);
