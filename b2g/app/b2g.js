ebc25685055cb53a4bcbcb086888f62623133ab0
Mason Chang <mchang@mozilla.com>
2015-01-28 10:22:00 -0500

    Bug 1118530 - Turn on by default hardware vsync and the Vsync Aligned Compositor on b2g. r=kats, a=bajaj

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1073,3 +1073,14 @@ pref("dom.mozSettings.SettingsService.verbose.enabled", false);
 // IndexedDB transactions to be opened as readonly or keep everything as
 // readwrite.
 pref("dom.mozSettings.allowForceReadOnly", false);
+
+// Only enable for kit kat and above devices
+// kit kat == 19, L = 21, 20 is kit-kat for wearables
+// 15 is for the ICS emulators which will fallback to software vsync
+#if ANDROID_VERSION == 19 || ANDROID_VERSION == 21 || ANDROID_VERSION == 15
+pref("gfx.vsync.hw-vsync.enabled", true);
+pref("gfx.vsync.compositor", true);
+#else
+pref("gfx.vsync.hw-vsync.enabled", false);
+pref("gfx.vsync.compositor", false);
+#endif
