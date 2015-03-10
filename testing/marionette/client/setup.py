cf8b7a7363cdc1970a9ccf0bb57bead958d77641
Dave Hunt <dhunt@mozilla.com>
2015-03-10 17:21:09 +0000

    Bug 1141590 - Create mozilla-b2g37_v2_2 branched versions of marionette-client and consumer packages. r=jgriffin a=testonly

--- testing/marionette/client/setup.py
+++ testing/marionette/client/setup.py
@@ -2,13 +2,13 @@ import os
 from setuptools import setup, find_packages
 import sys
 
-version = '0.8.6'
+version = '0.1'
 
 # dependencies
 with open('requirements.txt') as f:
     deps = f.read().splitlines()
 
-setup(name='marionette_client',
+setup(name='marionette_client-mozilla-b2g37_v2_2',
       version=version,
       description="Marionette test automation client",
       long_description='See http://marionette-client.readthedocs.org/',
