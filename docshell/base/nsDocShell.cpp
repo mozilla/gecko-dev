6ff989813d1f03d8e21c95bfa5336cfd69e774d2
Kyle Huey <khuey@kylehuey.com>
2015-03-20 19:15:13 -0700

    Bug 1144988. r=bz, a=2.0+

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -1310,7 +1310,7 @@ nsDocShell::LoadURI(nsIURI * aURI,
     
     // Note: we allow loads to get through here even if mFiredUnloadEvent is
     // true; that case will get handled in LoadInternal or LoadHistoryEntry.
-    if (IsPrintingOrPP()) {
+    if (IsPrintingOrPP() || mBlockNavigation) {
       return NS_OK; // JS may not handle returning of an error code
     }
 
