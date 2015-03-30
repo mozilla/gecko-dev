dbca96e58485ca69b2bfa39e100888e47b717ead
Steve Fink <sfink@mozilla.com>
2015-03-30 14:00:45 -0700

    Bug 1056410 - Mark more classes final. r=terrence, a=2.1+
    Bug 1047696 was blocked by the hazard here, so it ended up not marking problematic classes final. Mark them final.

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -249,10 +249,10 @@ AddToCCKind(JSGCTraceKind kind)
     return kind == JSTRACE_OBJECT || kind == JSTRACE_SCRIPT;
 }
 
-class nsXPConnect : public nsIXPConnect,
-                    public nsIThreadObserver,
-                    public nsSupportsWeakReference,
-                    public nsIJSRuntimeService
+class nsXPConnect final : public nsIXPConnect,
+                          public nsIThreadObserver,
+                          public nsSupportsWeakReference,
+                          public nsIJSRuntimeService
 {
 public:
     // all the interface method declarations...
@@ -2002,7 +2002,7 @@ void* xpc_GetJSPrivate(JSObject* obj);
 // XPCWrappedNative the wrapper around one instance of a native xpcom object
 // to be used from JavaScript.
 
-class XPCWrappedNative : public nsIXPConnectWrappedNative
+class XPCWrappedNative final : public nsIXPConnectWrappedNative
 {
 public:
     NS_DECL_CYCLE_COLLECTING_ISUPPORTS
@@ -2315,7 +2315,7 @@ NS_DEFINE_STATIC_IID_ACCESSOR(nsIXPCWrappedJSClass,
 // nsXPCWrappedJSClass represents the sharable factored out common code and
 // data for nsXPCWrappedJS instances for the same interface type.
 
-class nsXPCWrappedJSClass : public nsIXPCWrappedJSClass
+class nsXPCWrappedJSClass final : public nsIXPCWrappedJSClass
 {
     // all the interface method declarations...
     NS_DECL_ISUPPORTS
