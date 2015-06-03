c31502d819fdfe644c22b826d7c6c7591022a7a8
CHUANG CHENYU <CHENYU@CHUANGdeMacBook-Pro.local>
2015-06-03 18:14:41 +0800

    Get attribute from cloud storage

--- dom/system/gonk/cloudstorage/nsCloudStorageGeckoInterface.cpp
+++ dom/system/gonk/cloudstorage/nsCloudStorageGeckoInterface.cpp
@@ -6,6 +6,8 @@
 #include "CloudStorageLog.h"
 #include "CloudStorageManager.h"
 #include "CloudStorage.h"
+#include "fuse.h"
+#include <time.h>
 
 using namespace mozilla::system::cloudstorage;
 using namespace mozilla;
@@ -39,12 +41,32 @@ nsCloudStorageGeckoInterface::SetFileMeta(const nsACString_internal& aCloudName,
   RefPtr<CloudStorage> cloudStorage = CloudStorageManager::FindCloudStorageByName(cloudName);
   LOG("in CloudStorageGeckoInterface::SetFileMeta");
   LOG("file type: %s, size: %llu, modified time: %llu, created time: %llu", aIsDir?"Directory":"File", aSize, aMTime, aCTime);
-  CloudStorageResponseData resData;
-  resData.IsDir = aIsDir;
-  resData.FileSize = aSize;
-  resData.MTime = aMTime;
-  resData.CTime = aCTime;
-  cloudStorage->SetResponseData(resData);
+
+  FuseAttr attr;
+  if (aIsDir) {
+    attr.size = 4096;
+    attr.blocks = 8;
+    attr.blksize = 512;
+    attr.mode = 0x41ff;
+  } else {
+    attr.size = aSize;
+    attr.blocks = aSize/512;
+    attr.blksize = 512;
+    attr.mode = 0x81fd;
+  }
+  if (aMTime != 0 && aCTime != 0) {
+    attr.atime = aMTime/1000;
+    attr.mtime = aMTime/1000;
+    attr.ctime = aCTime/1000;
+  } else {
+    attr.atime = time(NULL);
+    attr.mtime = attr.atime;
+    attr.ctime = attr.ctime;
+  }
+  attr.uid = 0;
+  attr.gid = 1015;
+
+  cloudStorage->SetAttrByPath(path, attr);
   cloudStorage->SetWaitForRequest(false);
   return NS_OK;
 }
