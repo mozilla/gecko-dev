63068c3abc664181a38354739a94e625b40b3e6e
Randell Jesup <rjesup@jesup.org>
2015-06-05 20:27:38 -0400

    Bug 1132318 - Merge SelectSendFrameRate with SelectSendResolution. r=bwc, a=abillings
    --HG--
    extra : rebase_source : 56ec1bfe333a1b8ea133b59996ca4f8aaa019fdd

--- media/webrtc/signaling/src/media-conduit/VideoConduit.h
+++ media/webrtc/signaling/src/media-conduit/VideoConduit.h
@@ -140,9 +140,10 @@ public:
   /**
    * Function to select and change the encoding frame rate based on incoming frame rate
    * and max-mbps setting.
-   * @param framerate
+   * @param current framerate
+   * @result new framerate
    */
-  bool SelectSendFrameRate(unsigned int framerate);
+  unsigned int SelectSendFrameRate(unsigned int framerate) const;
 
   /**
    * Function to deliver a capture video frame for encoding and transport
@@ -295,7 +296,7 @@ private:
                            const VideoCodecConfig* codecInfo) const;
 
   //Checks the codec to be applied
-  MediaConduitErrorCode ValidateCodecConfig(const VideoCodecConfig* codecInfo, bool send) const;
+  MediaConduitErrorCode ValidateCodecConfig(const VideoCodecConfig* codecInfo, bool send);
 
   //Utility function to dump recv codec database
   void DumpCodecDB() const;
@@ -326,7 +327,10 @@ private:
   int mChannel; // Video Channel for this conduit
   int mCapId;   // Capturer for this conduit
   RecvCodecList    mRecvCodecList;
-  VideoCodecConfig* mCurSendCodecConfig;
+
+  Mutex mCodecMutex; // protects mCurrSendCodecConfig
+  nsAutoPtr<VideoCodecConfig> mCurSendCodecConfig;
+
   unsigned short mSendingWidth;
   unsigned short mSendingHeight;
   unsigned short mReceivingWidth;
