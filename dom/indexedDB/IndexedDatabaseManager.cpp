ecb3805b4425165d35b82874d4f9c55b75fb5390
Ryan VanderMeulen <ryanvm@gmail.com>
2015-06-09 12:12:13 -0400

    Bug 1142210 - Bustage follow-up on a CLOSED TREE.
    --HG--
    extra : transplant_source : %06B%8EGN%40%985%DC%D5%0E%DD%13%29%8AC%BF%1A%BA%B6

--- dom/indexedDB/IndexedDatabaseManager.cpp
+++ dom/indexedDB/IndexedDatabaseManager.cpp
@@ -108,6 +108,8 @@ END_INDEXEDDB_NAMESPACE
 
 namespace {
 
+NS_DEFINE_IID(kIDBRequestIID, PRIVATE_IDBREQUEST_IID);
+
 mozilla::StaticRefPtr<IndexedDatabaseManager> gDBManager;
 
 mozilla::Atomic<bool> gInitialized(false);
