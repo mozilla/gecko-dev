7eb2672e51dba4b5703150ba117c6f24e1997c27
Ryan VanderMeulen <ryanvm@gmail.com>
2015-08-31 23:09:18 -0400

    Backed out changeset b272be44ac17 (bug 1183153) for bustage.

--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -734,8 +734,7 @@ GlobalHelperThreadState::finishParseTask(JSContext* maybecx, JSRuntime* rt, void
         !GlobalObject::ensureConstructor(cx, global, JSProto_Array) ||
         !GlobalObject::ensureConstructor(cx, global, JSProto_Function) ||
         !GlobalObject::ensureConstructor(cx, global, JSProto_RegExp) ||
-        !GlobalObject::EnsureConstructor(cx, global, JSProto_Iterator) ||
-        !GlobalObject::EnsureConstructor(cx, global, JSProto_GeneratorFunction))
+        !GlobalObject::ensureConstructor(cx, global, JSProto_Iterator))
     {
         LeaveParseTaskZone(rt, parseTask);
         return nullptr;
@@ -757,18 +756,11 @@ GlobalHelperThreadState::finishParseTask(JSContext* maybecx, JSRuntime* rt, void
             continue;
 
         JSProtoKey key = JS::IdentifyStandardPrototype(proto.toObject());
-        if (key == JSProto_Null) {
-            // Generator functions don't have Function.prototype as prototype
-            // but a different function object, so IdentifyStandardPrototype
-            // doesn't work. Just special-case it here.
-            if (IsStandardPrototype(proto.toObject(), JSProto_GeneratorFunction))
-                key = JSProto_GeneratorFunction;
-            else
-                continue;
-        }
+        if (key == JSProto_Null)
+            continue;
         JS_ASSERT(key == JSProto_Object || key == JSProto_Array ||
                   key == JSProto_Function || key == JSProto_RegExp ||
-                  key == JSProto_Iterator || key == JSProto_GeneratorFunction);
+                  key == JSProto_Iterator);
 
         JSObject* newProto = GetBuiltinPrototypePure(global, key);
         JS_ASSERT(newProto);
