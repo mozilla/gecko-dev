efd89d03de5e6b5a41e71bbf06bd72365fbaca23
Ehsan Akhgari <ehsan@mozilla.com>
2015-09-02 19:53:35 -0400

    Bug 1200869 - Empty the header value for code hygiene. r=sicking, a=2.2+
    --HG--
    extra : transplant_source : %A7mJ%1C%7F%CC%E8%95Wf%06W%B06%FF/%28%BF%EE%A0

--- dom/security/nsCORSListenerProxy.cpp
+++ dom/security/nsCORSListenerProxy.cpp
@@ -948,6 +948,7 @@ nsCORSPreflightListener::AddResultToCache(nsIRequest *aRequest)
 
   // The "Access-Control-Allow-Methods" header contains a comma separated
   // list of method names.
+  headerVal.Truncate();
   http->GetResponseHeader(NS_LITERAL_CSTRING("Access-Control-Allow-Methods"),
                           headerVal);
 
@@ -978,6 +979,7 @@ nsCORSPreflightListener::AddResultToCache(nsIRequest *aRequest)
 
   // The "Access-Control-Allow-Headers" header contains a comma separated
   // list of method names.
+  headerVal.Truncate();
   http->GetResponseHeader(NS_LITERAL_CSTRING("Access-Control-Allow-Headers"),
                           headerVal);
 
