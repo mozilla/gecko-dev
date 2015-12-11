03541d81c1f7282b3c7643510add9ccfdc26b465
Andrea Marchesini <amarchesini@mozilla.com>
2015-12-11 11:17:33 -0500

    Bug 1214148 - patch 1 - propagation from the nested iframe back to the toplevel iframe, r=alwu, a=jocheng

--- dom/system/gonk/AudioChannelManager.cpp
+++ dom/system/gonk/AudioChannelManager.cpp
@@ -209,8 +209,8 @@ AudioChannelManager::GetAllowedAudioChannels(
   }
 
   nsBrowserElement::GenerateAllowedAudioChannels(window, nullptr, nullptr,
-                                                 manifestURL, aAudioChannels,
-                                                 aRv);
+                                                 manifestURL, nullptr,
+                                                 aAudioChannels, aRv);
   NS_WARN_IF(aRv.Failed());
 }
 
