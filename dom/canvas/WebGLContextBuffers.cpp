801fc2047c1793e2e45863abef35f5e39fb9470b
Jeff Gilbert <jgilbert@mozilla.com>
2016-01-11 19:51:00 -0800

    Bug 1220450 - Clear length on cache OOM. r=kamidphish, a=ritu

--- dom/canvas/WebGLContextBuffers.cpp
+++ dom/canvas/WebGLContextBuffers.cpp
@@ -190,6 +190,7 @@ WebGLContext::BufferData(GLenum target, WebGLsizeiptr size, GLenum usage)
 
     boundBuffer->SetByteLength(size);
     if (!boundBuffer->ElementArrayCacheBufferData(nullptr, size)) {
+        boundBuffer->SetByteLength(0);
         return ErrorOutOfMemory("bufferData: out of memory");
     }
 }
@@ -239,8 +240,10 @@ WebGLContext::BufferData(GLenum target,
     }
 
     boundBuffer->SetByteLength(data.Length());
-    if (!boundBuffer->ElementArrayCacheBufferData(data.Data(), data.Length()))
+    if (!boundBuffer->ElementArrayCacheBufferData(data.Data(), data.Length())) {
+        boundBuffer->SetByteLength(0);
         return ErrorOutOfMemory("bufferData: out of memory");
+    }
 }
 
 void
@@ -279,8 +282,10 @@ WebGLContext::BufferData(GLenum target, const dom::ArrayBufferView& data,
     }
 
     boundBuffer->SetByteLength(data.Length());
-    if (!boundBuffer->ElementArrayCacheBufferData(data.Data(), data.Length()))
+    if (!boundBuffer->ElementArrayCacheBufferData(data.Data(), data.Length())) {
+        boundBuffer->SetByteLength(0);
         return ErrorOutOfMemory("bufferData: out of memory");
+    }
 }
 
 void
