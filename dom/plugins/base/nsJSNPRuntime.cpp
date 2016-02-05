1c022bf9addc214eb21db748fd7e73ccf9a4e8df
Nicholas Nethercote <nnethercote@mozilla.com>
2016-02-05 16:36:05 +1100

    Bug 1246054 - Fix an erroneous nsNPObjWrapper assertion. r=froydnj. a=ritu
    --HG--
    extra : commitid : DTcUjGfRZjc

--- dom/plugins/base/nsJSNPRuntime.cpp
+++ dom/plugins/base/nsJSNPRuntime.cpp
@@ -1920,8 +1920,9 @@ nsNPObjWrapper::GetNewOrUsed(NPP npp, JSContext *cx, NPObject *npobj)
       // Reload entry if the JS_NewObject call caused a GC and reallocated
       // the table (see bug 445229). This is guaranteed to succeed.
 
-      NS_ASSERTION(PL_DHashTableSearch(&sNPObjWrappers, npobj),
-                   "Hashtable didn't find what we just added?");
+      entry = static_cast<NPObjWrapperHashEntry*>
+        (PL_DHashTableSearch(&sNPObjWrappers, npobj));
+      NS_ASSERTION(entry, "Hashtable didn't find what we just added?");
   }
 
   if (!obj) {
