13022b54a1a0dbb04570328a178d4ed497e13295
Cykesiopka <cykesiopka.bmo@gmail.com>
2016-03-18 19:43:51 -0700

    Bug 1253958 - Make getHSTSPreloadList.js and genHPKPStaticPins.js gracefully handle trailing whitespace in URL entries. r=keeler, a=npotb

--- security/manager/tools/getHSTSPreloadList.js
+++ security/manager/tools/getHSTSPreloadList.js
@@ -104,9 +104,12 @@ function getHosts(rawdata) {
     throw "ERROR: source data not formatted correctly: 'entries' not found";
   }
 
-  for (entry of rawdata.entries) {
+  for (let entry of rawdata.entries) {
     if (entry.mode && entry.mode == "force-https") {
       if (entry.name) {
+        // We trim the entry name here to avoid malformed URI exceptions when we
+        // later try to connect to the domain.
+        entry.name = entry.name.trim();
         entry.retries = MAX_RETRIES;
         entry.originalIncludeSubdomains = entry.include_subdomains;
         hosts.push(entry);
