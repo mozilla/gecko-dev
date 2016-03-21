d74a307d89c292db04524195196b5a892eb4d667
Nick Thomas <nthomas@mozilla.com>
2016-03-21 13:13:52 +1300

    Bug 1255245 - ESR45 generates mar files with the wrong channel ID, part II, r=rstrong, a=sylvestre
    --HG--
    branch : GECKO4501esr_2016031618_RELBRANCH
    extra : source : 0937306e704ac4a00a15849d379f74f75ee081b5

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -51,7 +51,7 @@ MOZ_APP_ID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}
 # of values.
 ACCEPTED_MAR_CHANNEL_IDS=firefox-mozilla-esr
 # The MAR_CHANNEL_ID must not contain the following 3 characters: ",\t "
-MAR_CHANNEL_ID=firefox-mozilla-release
+MAR_CHANNEL_ID=firefox-mozilla-esr
 MOZ_PROFILE_MIGRATOR=1
 MOZ_APP_STATIC_INI=1
 MOZ_WEBAPP_RUNTIME=1
