6391fd6c1fd6658940775fc4149bcbe47d6fa867
Jan de Mooij <jdemooij@mozilla.com>
2016-03-24 10:13:44 +0100

    Bug 1212328 followup - Add missing semicolon. r=bustage a=bustage

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -5799,7 +5799,7 @@ WorkerPrivate::ReportError(JSContext* aCx, const char* aMessage,
     filename = NS_ConvertUTF8toUTF16(aReport->filename);
     line.Assign(aReport->linebuf(), aReport->linebufLength());
     lineNumber = aReport->lineno;
-    columnNumber = aReport->tokenOffset()
+    columnNumber = aReport->tokenOffset();
     flags = aReport->flags;
     errorNumber = aReport->errorNumber;
     MOZ_ASSERT(aReport->exnType >= JSEXN_NONE && aReport->exnType < JSEXN_LIMIT);
