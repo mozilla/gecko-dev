3da25603fd1dbe480b8315f9b6227fd4497d43df
Randell Jesup <rjesup@jesup.org>
2016-04-15 23:11:01 -0400

    Bug 1263384: validate input frames against configured resolution in vp8 r=rillian, a=ritu,lizzard
    MozReview-Commit-ID: BxDCnJe0mzs

--- media/libvpx/update.py
+++ media/libvpx/update.py
@@ -598,6 +598,8 @@ def apply_patches():
     os.system("patch -p3 < clamp_abs_lvl_seg.patch")
     # Bug 1224361 - Clamp QIndex also in abs-value mode.
     os.system("patch -p3 < clamp-abs-QIndex.patch")
+    # Bug 1263384 - Check input frame resolution
+    os.system("patch -p3 < input_frame_validation.patch")
 
 def update_readme(commit):
     with open('README_MOZILLA') as f:
