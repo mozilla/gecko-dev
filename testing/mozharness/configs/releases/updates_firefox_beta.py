5a4ddae6a0b10df6f9881a72c6f87cf7e473d1ab
Kim Moir <kmoir@mozilla.com>
2016-05-12 15:20:55 -0400

    Bug 1252003 - Add publish to balrog builder r=rail a=release DONTBUILD
    MozReview-Commit-ID: CeNwtAd2o6e
    
    --HG--
    extra : source : e43945cb2c87ef34421f9ef5832cbb3645b804d3
    extra : intermediate-source : 4936acc6a6683fc222da08e297f774605b726dd1

--- testing/mozharness/configs/releases/updates_firefox_beta.py
+++ testing/mozharness/configs/releases/updates_firefox_beta.py
@@ -29,6 +29,7 @@ config = {
             ],
             "channel_names": ["beta", "beta-localtest", "beta-cdntest"],
             "rules_to_update": ["firefox-beta-cdntest", "firefox-beta-localtest"],
+            "publish_rules": ["firefox-beta"],
         },
     },
     "balrog_use_dummy_suffix": False,
