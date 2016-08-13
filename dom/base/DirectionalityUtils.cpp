8808cd144586df04376c4bb146455653719fa73d
Olli Pettay <Olli.Pettay@helsinki.fi>
2016-08-12 17:05:18 -0700

    Bug 1289970, ensure we don't remove valid Directionality map from textnode, r=ehsan a=abillings CLOSED TREE

--- dom/base/DirectionalityUtils.cpp
+++ dom/base/DirectionalityUtils.cpp
@@ -511,6 +511,12 @@ private:
                                                         oldTextNode);
     }
     if (newTextNode) {
+      nsINode* oldDirAutoSetBy = 
+        static_cast<nsTextNode*>(rootNode->GetProperty(nsGkAtoms::dirAutoSetBy));
+      if (oldDirAutoSetBy == newTextNode) {
+        // We're already registered.
+        return OpNext;
+      }
       nsTextNodeDirectionalityMap::AddEntryToMap(newTextNode, rootNode);
     } else {
       rootNode->ClearHasDirAutoSet();
@@ -683,7 +689,9 @@ WalkDescendantsResetAutoDirection(Element* aElement)
 
     if (child->HasTextNodeDirectionalityMap()) {
       nsTextNodeDirectionalityMap::ResetTextNodeDirection(child, nullptr);
-      nsTextNodeDirectionalityMap::EnsureMapIsClearFor(child);
+      // Don't call nsTextNodeDirectionalityMap::EnsureMapIsClearFor(child)
+      // since ResetTextNodeDirection may have kept elements in child's
+      // DirectionalityMap.
     }
     child = child->GetNextNode(aElement);
   }
