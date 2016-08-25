86300ae21bad0e8b3a8504d222602ca2fbef9e6f
Alexander Surkov <surkov.alexander@gmail.com>
2016-08-24 22:16:45 -0400

    Bug 1287721 - Keep strong pointers for nodes in the invalidation list of a document accessible. r=smaug a=sylvestre
    --HG--
    extra : source : bf6c0c4ad16c76cc56b3164576c720fc1811f5e7
    extra : intermediate-source : 7883a513cc6a78c3fc2bd814d0bff2504d5cbeef

--- accessible/generic/DocAccessible.h
+++ accessible/generic/DocAccessible.h
@@ -681,7 +681,7 @@ protected:
    *
    * @see ProcessInvalidationList
    */
-  nsTArray<nsIContent*> mInvalidationList;
+  nsTArray<RefPtr<nsIContent>> mInvalidationList;
 
   /**
    * Holds a list of aria-owns relocations.
