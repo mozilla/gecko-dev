e61faba8f02fea58c3cd0173ce3750c21f0da60d
Jason Orendorff <jorendorff@mozilla.com>
2016-11-30 15:31:56 -0600

    Bug 1312001 - Scramble hash codes securely, to avoid leaking bits of object and symbol addresses. r=jandem, a=lizzard
    MozReview-Commit-ID: yR1cIjrlPP

--- js/src/jscompartment.h
+++ js/src/jscompartment.h
@@ -611,6 +611,12 @@ struct JSCompartment
     void ensureRandomNumberGenerator();
 
   private:
+    mozilla::non_crypto::XorShift128PlusRNG randomKeyGenerator_;
+
+  public:
+    mozilla::HashCodeScrambler randomHashCodeScrambler();
+
+  private:
     JSCompartment* thisForCtor() { return this; }
 
   public:
