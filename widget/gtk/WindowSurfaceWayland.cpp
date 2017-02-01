4b1997086efc5fce74fdc5b1600a96db71f1c558
Martin Stransky <stransky@redhat.com>
2017-02-01 22:02:51 +0100

    Fixed flickering when wl_buffer is altered

--- widget/gtk/WindowSurfaceWayland.cpp
+++ widget/gtk/WindowSurfaceWayland.cpp
@@ -460,7 +460,10 @@ WindowSurfaceWayland::GetBufferToDraw(int aWidth, int aHeight)
       mFrontBuffer = mBackBuffer;
       mBackBuffer = tmp;
 
-      mFrontBuffer->Sync(mBackBuffer);      
+      mFrontBuffer->Sync(mBackBuffer);
+      
+      // TODO (https://bugzilla.redhat.com/show_bug.cgi?id=1418260)
+      wl_surface_damage(mSurface, 0, 0, aWidth, aHeight);
     }
 
     if (!mFrontBuffer->MatchSize(aWidth, aHeight)) {
