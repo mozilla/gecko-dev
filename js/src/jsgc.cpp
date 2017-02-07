783ff9b07029d25d77f2ad7b1794768a5df89a8a
Jon Coppeard <jcoppeard@mozilla.com>
2017-02-07 17:37:45 +0000

    Bug 1330687 - Fix computation of zone edges for weakmap key delegates. r=sfink, a=gchang

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -4609,6 +4609,11 @@ GCRuntime::findZoneEdgesForWeakMaps()
      * group.
      */
 
+#ifdef DEBUG
+    for (ZonesIter zone(rt, WithAtoms); !zone.done(); zone.next())
+        MOZ_ASSERT(zone->gcZoneGroupEdges.empty());
+#endif
+
     for (GCZonesIter zone(rt); !zone.done(); zone.next()) {
         if (!WeakMapBase::findInterZoneEdges(zone))
             return false;
@@ -4632,12 +4637,16 @@ GCRuntime::findZoneGroups()
     currentZoneGroup = zoneGroups;
     zoneGroupIndex = 0;
 
+#ifdef DEBUG
     for (Zone* head = currentZoneGroup; head; head = head->nextGroup()) {
         for (Zone* zone = head; zone; zone = zone->nextNodeInGroup())
             MOZ_ASSERT(zone->isGCMarking());
     }
 
     MOZ_ASSERT_IF(!isIncremental, !currentZoneGroup->nextGroup());
+    for (ZonesIter zone(rt, WithAtoms); !zone.done(); zone.next())
+        MOZ_ASSERT(zone->gcZoneGroupEdges.empty());
+#endif
 }
 
 static void
