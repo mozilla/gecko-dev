a3308076aae04b4b1885e3b2c24d2523a58d29d9
Michael Layzell <michael@thelayzells.com>
2017-02-24 16:06:35 -0500

    Bug 1342057 - Part 1: Use correct MIME type for files as DataTransferItem.type. r=baku, a=jcristau
    MozReview-Commit-ID: 4hrZ3YoGTJj
    
    --HG--
    extra : source : 742fc1cf3d2895bf109cfed11a4df276d5df3bed
    extra : intermediate-source : ccc12c0fbf9fc09ee395015a4b67cb57e47e41c3

--- dom/events/DataTransfer.cpp
+++ dom/events/DataTransfer.cpp
@@ -331,8 +331,11 @@ DataTransfer::GetTypes(nsTArray<nsString>& aTypes,
       continue;
     }
 
+    // NOTE: The reason why we get the internal type here is because we want
+    // kFileMime to appear in the types list for backwards compatibility
+    // reasons.
     nsAutoString type;
-    item->GetType(type);
+    item->GetInternalType(type);
     if (item->Kind() == DataTransferItem::KIND_STRING || type.EqualsASCII(kFileMime)) {
       // If the entry has kind KIND_STRING, we want to add it to the list.
       aTypes.AppendElement(type);
@@ -519,8 +522,11 @@ DataTransfer::MozTypesAt(uint32_t aIndex, ErrorResult& aRv) const
         continue;
       }
 
+      // NOTE: The reason why we get the internal type here is because we want
+      // kFileMime to appear in the types list for backwards compatibility
+      // reasons.
       nsAutoString type;
-      items[i]->GetType(type);
+      items[i]->GetInternalType(type);
       if (NS_WARN_IF(!types->Add(type))) {
         aRv.Throw(NS_ERROR_FAILURE);
         return nullptr;
@@ -987,7 +993,7 @@ DataTransfer::GetTransferable(uint32_t aIndex, nsILoadContext* aLoadContext)
       }
 
       nsAutoString type;
-      formatitem->GetType(type);
+      formatitem->GetInternalType(type);
 
       // If the data is of one of the well-known formats, use it directly.
       bool isCustomFormat = true;
