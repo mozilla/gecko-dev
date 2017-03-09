c87b2bde80adcf5cb520e5aac7f3236087c6573f
Mats Palmgren <mats@mozilla.com>
2017-03-09 13:18:26 +0100

    Bug 1343552 - Part 1: Invalidate the cached flow length when the next-in-flow/continuation changes. r=jfkthame, a=ritu
    MozReview-Commit-ID: Bz5YA60NY8I
    
    --HG--
    extra : source : 0f7465d1f1bba6ec2321793061f6a1959b1b40b4
    extra : intermediate-source : ddbca773eb5376aa6351d471e69364bdb8b9b254

--- layout/generic/nsTextFrame.h
+++ layout/generic/nsTextFrame.h
@@ -90,6 +90,9 @@ public:
     mNextContinuation = aNextContinuation;
     if (aNextContinuation)
       aNextContinuation->RemoveStateBits(NS_FRAME_IS_FLUID_CONTINUATION);
+    // Setting a non-fluid continuation might affect our flow length (they're
+    // quite rare so we assume it always does) so we delete our cached value:
+    GetContent()->DeleteProperty(nsGkAtoms::flowlength);
   }
   virtual nsIFrame* GetNextInFlowVirtual() const override { return GetNextInFlow(); }
   nsIFrame* GetNextInFlow() const {
@@ -102,8 +105,15 @@ public:
     NS_ASSERTION (!nsSplittableFrame::IsInNextContinuationChain(aNextInFlow, this),
                   "creating a loop in continuation chain!");
     mNextContinuation = aNextInFlow;
-    if (aNextInFlow)
+    if (mNextContinuation &&
+        !mNextContinuation->HasAnyStateBits(NS_FRAME_IS_FLUID_CONTINUATION)) {
+      // Changing from non-fluid to fluid continuation might affect our flow
+      // length, so we delete our cached value:
+      GetContent()->DeleteProperty(nsGkAtoms::flowlength);
+    }
+    if (aNextInFlow) {
       aNextInFlow->AddStateBits(NS_FRAME_IS_FLUID_CONTINUATION);
+    }
   }
   virtual nsIFrame* LastInFlow() const override;
   virtual nsIFrame* LastContinuation() const override;
