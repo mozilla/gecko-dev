4ce062e4b386a3e4e89ca9f81450e5363356ca96
Olli Pettay <Olli.Pettay@helsinki.fi>
2017-03-25 03:01:19 +0200

    Bug 1349946 - Ensure expected focus handling when redirecting focus. r=enndeakin, a=jcristau
    --HG--
    extra : source : c46c8e2804c2a64e8436290c032a3e8bf32b63b5
    extra : intermediate-source : ee0a7b55e470e6b90343a241e2117f8fc8d5ebab

--- layout/generic/nsPluginFrame.cpp
+++ layout/generic/nsPluginFrame.cpp
@@ -1633,8 +1633,10 @@ nsPluginFrame::HandleEvent(nsPresContext* aPresContext,
   }
   else if (anEvent->mMessage == ePluginFocus) {
     nsIFocusManager* fm = nsFocusManager::GetFocusManager();
-    if (fm)
-      return fm->FocusPlugin(GetContent());
+    if (fm) {
+      nsCOMPtr<nsIContent> content = GetContent();
+      return fm->FocusPlugin(content);
+    }
   }
 
   if (mInstanceOwner->SendNativeEvents() &&
