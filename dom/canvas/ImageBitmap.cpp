df9e06a7b942c60d5f5cee452a93872a46c82e21
Timothy Nikkel <tnikkel@gmail.com>
2017-04-13 03:40:45 -0500

    Bug 1355898 - Rename flag SFE_WANT_FIRST_FRAME to SFE_WANT_FIRST_FRAME_IF_IMAGE to represent what it does better. r=jgilbert, a=gchang
    We only pay attention to the flag for images.
    
    --HG--
    extra : source : bff28a19134fbf09808dffe05109464f0ddfa50b
    extra : intermediate-source : 3c1bf407a189e20f5013fdeccd82b5fa2abeafc4

--- dom/canvas/ImageBitmap.cpp
+++ dom/canvas/ImageBitmap.cpp
@@ -354,7 +354,7 @@ static already_AddRefed<SourceSurface>
 GetSurfaceFromElement(nsIGlobalObject* aGlobal, HTMLElementType& aElement, ErrorResult& aRv)
 {
   nsLayoutUtils::SurfaceFromElementResult res =
-    nsLayoutUtils::SurfaceFromElement(&aElement, nsLayoutUtils::SFE_WANT_FIRST_FRAME);
+    nsLayoutUtils::SurfaceFromElement(&aElement, nsLayoutUtils::SFE_WANT_FIRST_FRAME_IF_IMAGE);
 
   // check origin-clean
   if (!CheckSecurityForHTMLElements(res)) {
@@ -732,7 +732,7 @@ ImageBitmap::CreateFromOffscreenCanvas(nsIGlobalObject* aGlobal,
 
   nsLayoutUtils::SurfaceFromElementResult res =
     nsLayoutUtils::SurfaceFromOffscreenCanvas(&aOffscreenCanvas,
-                                              nsLayoutUtils::SFE_WANT_FIRST_FRAME);
+                                              nsLayoutUtils::SFE_WANT_FIRST_FRAME_IF_IMAGE);
 
   RefPtr<SourceSurface> surface = res.GetSourceSurface();
 
