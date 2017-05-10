a97bbff51439c8e85aa26d3047444c81e43680d1
Masayuki Nakano <masayuki@d-toybox.com>
2017-05-10 16:48:46 +0900

    Bug 1354443 - Part 1-1: Make nsPrintData refcountable. r=dholbert, a=abillings

--- layout/printing/nsPrintData.h
+++ layout/printing/nsPrintData.h
@@ -12,6 +12,7 @@
 #include "nsDeviceContext.h"
 #include "nsIPrintProgressParams.h"
 #include "nsIPrintSettings.h"
+#include "nsISupportsImpl.h"
 #include "nsTArray.h"
 #include "nsCOMArray.h"
 
@@ -36,11 +37,11 @@ class nsIWebProgressListener;
 //------------------------------------------------------------------------
 class nsPrintData {
 public:
-
   typedef enum {eIsPrinting, eIsPrintPreview } ePrintDataType;
 
   explicit nsPrintData(ePrintDataType aType);
-  ~nsPrintData(); // non-virtual
+
+  NS_INLINE_DECL_REFCOUNTING(nsPrintData)
 
   // Listener Helper Methods
   void OnEndPrinting();
@@ -88,6 +89,7 @@ private:
   nsPrintData() = delete;
   nsPrintData& operator=(const nsPrintData& aOther) = delete;
 
+  ~nsPrintData(); // non-virtual
 };
 
 #endif /* nsPrintData_h___ */
