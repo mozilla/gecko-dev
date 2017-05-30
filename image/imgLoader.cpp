455e88b3f140c5fad0f401ae45d007ebb44ca832
Timothy Nikkel <tnikkel@gmail.com>
2017-05-30 14:49:26 -0500

    Bug 1342567. r=aosmond, a=jcristau
    --HG--
    extra : source : 39f174dc2f917c2846e856f53015eba646cda76d
    extra : intermediate-source : 30ba0234b771e65510a852e5c8318f50195c9bdb

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -2136,7 +2136,7 @@ imgLoader::LoadImage(nsIURI* aURI,
           "Proxyless entry's request has cache entry!");
         request->SetCacheEntry(entry);
 
-        if (mCacheTracker) {
+        if (mCacheTracker && entry->GetExpirationState()->IsTracked()) {
           mCacheTracker->MarkUsed(entry);
         }
       }
@@ -2398,7 +2398,7 @@ imgLoader::LoadImageWithChannel(nsIChannel* channel,
             "Proxyless entry's request has cache entry!");
           request->SetCacheEntry(entry);
 
-          if (mCacheTracker) {
+          if (mCacheTracker && entry->GetExpirationState()->IsTracked()) {
             mCacheTracker->MarkUsed(entry);
           }
         }
