e1c7a8149636d1a8e70c6aa041dcfa9d2e1c3499
Jan Beich <jbeich@FreeBSD.org>
2017-06-21 18:45:14 +0000

    Bug 1375198 - FreeBSD bootstrap: drop explicit dependency on libGL. r=rillian, a=gchang
    libGL package was consolidated into mesa-libs but quarterly set still
    uses the old name. Since gtk3 (via libepoxy) and gtk2 (via cairo)
    already indirectly depend on mesa-libs take advantage of it to avoid
    churn on updates.
    
    MozReview-Commit-ID: F5LBOOthAMc
    
    --HG--
    extra : source : ce2cf9dcf07707692520e0f39ec993f40e5a8c68
    extra : intermediate-source : 8e494ed0894087d24f2f68104d4eadad8c4a42a4

--- python/mozboot/mozboot/freebsd.py
+++ python/mozboot/mozboot/freebsd.py
@@ -26,7 +26,6 @@ class FreeBSDBootstrapper(BaseBootstrapper):
             'gconf2',
             'gtk2',
             'gtk3',
-            'libGL',
             'pulseaudio',
             'v4l_compat',
             'yasm',
