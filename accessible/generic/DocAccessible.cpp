016dac90a49cd7ac12d2710e8d793f1f71d465ba
Eitan Isaacson <eitan@monotonous.org>
2017-07-20 12:57:00 -0400

    Bug 1376825 - Set eIsNotInDocument to accessible's state when unbinding from doc. r=surkov, a=jcristau
    --HG--
    extra : source : 63f14ebd247362d320d942f3676b09c1db33c7ce
    extra : intermediate-source : c2b9b5bec46f0e2e0e7d3ab115e9bd95c2252841

--- accessible/generic/DocAccessible.cpp
+++ accessible/generic/DocAccessible.cpp
@@ -1330,6 +1330,8 @@ DocAccessible::UnbindFromDocument(Accessible* aAccessible)
       mNodeToAccessibleMap.Get(aAccessible->GetNode()) == aAccessible)
     mNodeToAccessibleMap.Remove(aAccessible->GetNode());
 
+  aAccessible->mStateFlags |= eIsNotInDocument;
+
   // Update XPCOM part.
   xpcAccessibleDocument* xpcDoc = GetAccService()->GetCachedXPCDocument(this);
   if (xpcDoc)
