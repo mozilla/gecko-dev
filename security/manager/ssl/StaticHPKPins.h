6a707ff93a932cde17f0d7cde2ae2ce7b20009da
ffxbld <none@none>
2017-10-11 11:03:51 -0700

    No bug, Automated HPKP preload list update from host bld-linux64-spot-302 - a=hpkp-update

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -1161,4 +1161,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1516038036327000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1516212079946000);
