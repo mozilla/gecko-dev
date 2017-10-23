dbb53924d5da783e8837cda1042f6e64206ee8da
Andrew McCreight <continuation@gmail.com>
2017-10-23 10:18:42 -0400

    Bug 1408005 - Clear gApplicationReputationService in the dtor. r=francois, a=abillings
    --HG--
    extra : source : 1613834d8d1145200886a039f733b1e066c63cbd
    extra : intermediate-source : 9e21926958600c34dcdfec9a58986df3d7d41759

--- toolkit/components/downloads/ApplicationReputation.cpp
+++ toolkit/components/downloads/ApplicationReputation.cpp
@@ -1550,6 +1550,8 @@ ApplicationReputationService::ApplicationReputationService()
 
 ApplicationReputationService::~ApplicationReputationService() {
   LOG(("Application reputation service shutting down"));
+  MOZ_ASSERT(gApplicationReputationService == this);
+  gApplicationReputationService = nullptr;
 }
 
 NS_IMETHODIMP
