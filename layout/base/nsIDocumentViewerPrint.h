d837d9c9cf7e3e7f7c05978e90210873cab49eff
Bob Owen <bobowencode@gmail.com>
2018-01-12 10:17:36 +0000

    Bug 1426087 - Decrement nsDocumentViewer::mDestroyRefCount in a separate function. r=jwatt, a=RyanVM
    --HG--
    extra : source : 86d41b5efe074f6988085082df9ef537eee0813a
    extra : intermediate-source : ea406489f93a0a8fa05ff568acdff07ce6b118c4

--- layout/base/nsIDocumentViewerPrint.h
+++ layout/base/nsIDocumentViewerPrint.h
@@ -41,6 +41,7 @@ public:
   virtual mozilla::StyleSetHandle CreateStyleSet(nsIDocument* aDocument) = 0;
 
   virtual void IncrementDestroyRefCount() = 0;
+  virtual void DecrementDestroyRefCount() = 0;
 
   virtual void ReturnToGalleyPresentation() = 0;
 
@@ -75,6 +76,7 @@ NS_DEFINE_STATIC_IID_ACCESSOR(nsIDocumentViewerPrint,
   bool GetIsPrintPreview() override; \
   mozilla::StyleSetHandle CreateStyleSet(nsIDocument* aDocument) override; \
   void IncrementDestroyRefCount() override; \
+  void DecrementDestroyRefCount() override; \
   void ReturnToGalleyPresentation() override; \
   void OnDonePrinting() override; \
   bool IsInitializedForPrintPreview() override; \
