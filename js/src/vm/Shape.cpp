89907f841b430c4190d6e2af380c9ba3f3364836
Brian Hackett <bhackett1024@gmail.com>
2018-02-23 13:25:53 -0500

    Bug 1437507 - Fix JSObject::setFlags to call ensureShape before checking for dictionary mode. r=jandem, a=RyanVM
    --HG--
    extra : source : ca6b74831ec3db204e024b07f200b0d1ce93557e
    extra : intermediate-source : 9d7c295d9570e294851908465f56ec0779547d2a

--- js/src/vm/Shape.cpp
+++ js/src/vm/Shape.cpp
@@ -1214,6 +1214,10 @@ JSObject::setFlags(ExclusiveContext* cx, BaseShape::Flag flags, GenerateShape ge
 
     RootedObject self(cx, this);
 
+    Shape* existingShape = self->ensureShape(cx);
+    if (!existingShape)
+        return false;
+
     if (isNative() && as<NativeObject>().inDictionaryMode()) {
         if (generateShape == GENERATE_SHAPE && !as<NativeObject>().generateOwnShape(cx))
             return false;
@@ -1226,10 +1230,6 @@ JSObject::setFlags(ExclusiveContext* cx, BaseShape::Flag flags, GenerateShape ge
         self->as<NativeObject>().lastProperty()->base()->adoptUnowned(nbase);
         return true;
     }
-
-    Shape* existingShape = self->ensureShape(cx);
-    if (!existingShape)
-        return false;
 
     Shape* newShape = Shape::setObjectFlags(cx, flags, self->taggedProto(), existingShape);
     if (!newShape)
