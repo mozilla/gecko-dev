75ecf74aa73b30c95e1f9d0338d211d6a284cca0
Andrew McCreight <continuation@gmail.com>
2018-04-26 14:21:25 -0700

    Bug 1457281 - Remove dom.ipc.scheduler pref. r=froydnj, a=RyanVM
    The JS engine has changed the APIs that cooperative scheduling relies
    on into immediate crashes. Some users seem to set the prefs, and then
    we get a lot of crashes on Nightly. This removes the top-level pref to
    save them some grief.
    
    I continue to pass the pref as false to the child process to avoid
    weirdness if we somehow we get this far when the parent and child have
    different versions.
    
    MozReview-Commit-ID: 3o3yV2efx2r
    
    --HG--
    extra : source : dab18a77fd23c5b2794dd54727ded70f319d89a9
    extra : intermediate-source : 458ea52fc8610767a225addb9fbe2fa4447b356f

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3399,7 +3399,6 @@ pref("dom.ipc.useNativeEventProcessing.content", true);
 #endif
 
 // Quantum DOM scheduling:
-pref("dom.ipc.scheduler", false);
 pref("dom.ipc.scheduler.useMultipleQueues", true);
 pref("dom.ipc.scheduler.preemption", false);
 pref("dom.ipc.scheduler.threadCount", 2);
