9e2849e2b23a415f8635de2fdaffb8b3b62c6fc5
Michael Kaply <mozilla@kaply.com>
2018-06-06 15:58:24 -0500

    Bug 1466863 - Just use empty metadata if invalid. r=florian, a=RyanVM
    MozReview-Commit-ID: 30Q5Sdi5ZRt
    
    --HG--
    extra : source : 5404016c6d164a29c8dccdce58363d5b893af565
    extra : intermediate-source : 70c46487e5132963906f5caab8e0181e7b5e772e

--- toolkit/components/search/nsSearchService.js
+++ toolkit/components/search/nsSearchService.js
@@ -2080,7 +2080,7 @@ Engine.prototype = {
   },
 
   getAttr(name) {
-    return (this._metaData && this._metaData[name]) || undefined;
+    return this._metaData[name] || undefined;
   },
 
   // nsISearchEngine
@@ -3197,7 +3197,7 @@ SearchService.prototype = {
       let name = engine._name;
       if (name in this._engines) {
         LOG("_loadEnginesMetadataFromCache, transfering metadata for " + name);
-        this._engines[name]._metaData = engine._metaData;
+        this._engines[name]._metaData = engine._metaData || {};
       }
     }
   },
