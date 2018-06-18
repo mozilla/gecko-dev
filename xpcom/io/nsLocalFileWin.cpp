ea12c2668f7928b97f4794d82934afdd061eb5d3
Paolo Amadini <paolo.mozmail@amadzone.org>
2018-06-18 11:26:33 +0100

    Bug 1468217 - Add "SettingContent-ms" to the list of executable file extensions. r=Gijs, a=RyanVM
    --HG--
    extra : source : ca7b77362ad3f1740eb866e3435cd3acddfe3ce9
    extra : intermediate-source : 6f3df7ea04aa672f91f3f80cf52eb67114b0c2fe

--- xpcom/io/nsLocalFileWin.cpp
+++ xpcom/io/nsLocalFileWin.cpp
@@ -3074,6 +3074,7 @@ nsLocalFile::IsExecutable(bool* aResult)
       "scf",         // Windows explorer command
       "scr",
       "sct",
+      "settingcontent-ms",
       "shb",
       "shs",
       "url",
