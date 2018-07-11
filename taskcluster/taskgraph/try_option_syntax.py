f20474db2513977741ff5171e22b4900e73ef8f7
Ryan VanderMeulen <ryanvm@gmail.com>
2018-07-11 18:36:47 -0400

    Bug 1475076 - Remove reftest-stylo from ESR60. r=jlund, a=RyanVM
    --HG--
    extra : histedit_source : 4f752557182cbdd3a459dd4cd929b72e70c3703f

--- taskcluster/taskgraph/try_option_syntax.py
+++ taskcluster/taskgraph/try_option_syntax.py
@@ -98,7 +98,6 @@ UNITTEST_ALIASES = {
     'reftest-no-accel': alias_matches(r'^(plain-)?reftest-no-accel.*$'),
     'reftests': alias_matches(r'^(plain-)?reftest.*$'),
     'reftests-e10s': alias_matches(r'^(plain-)?reftest-e10s.*$'),
-    'reftest-stylo': alias_matches(r'^(plain-)?reftest-stylo.*$'),
     'reftest-gpu': alias_matches(r'^(plain-)?reftest-gpu.*$'),
     'robocop': alias_prefix('robocop'),
     'web-platform-test': alias_prefix('web-platform-tests'),
