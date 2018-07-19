356aadc5dbd0d9dae4194ee13966aa64ba4486fa
Rok Garbas <rok@garbas.si>
2018-07-19 13:30:32 +0200

    Bug 1471541 - switch to new tooltool url, r=rail, a=release
    --HG--
    extra : rebase_source : e8ca9e42e1869f6671b991ff871476672b74930e
    extra : amend_source : 2ff92dc40c3c904781df5bbc406faa4933a96b17

--- tools/lint/eslint/update.sh
+++ tools/lint/eslint/update.sh
@@ -6,7 +6,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-echo "To complete this script you will need the following tokens from https://api.pub.build.mozilla.org/tokenauth/"
+echo "To complete this script you will need the following tokens from https://mozilla-releng.net/tokens/"
 echo " - tooltool.upload.public"
 echo " - tooltool.download.public"
 echo ""
@@ -14,7 +14,7 @@ read -p "Are these tokens visible at the above URL (y/n)?" choice
 case "$choice" in
   y|Y )
     echo ""
-    echo "1. Go to https://api.pub.build.mozilla.org/"
+    echo "1. Go to https://mozilla-releng.net/"
     echo "2. Log in using your Mozilla LDAP account."
     echo "3. Click on \"Tokens.\""
     echo "4. Issue a user token with the permissions tooltool.upload.public and tooltool.download.public."
