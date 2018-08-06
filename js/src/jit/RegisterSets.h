b42f5a84378490e4f0a56cf034f35a19ecc004ac
Philip Chimento <philip.chimento@gmail.com>
2018-08-06 17:56:02 -0400

    Bug 1458382 - Repeat GCC bug workaround in another place. r=sstangl, a=RyanVM
    --HG--
    extra : histedit_source : 310705910112e5807c0796c2df2d626588e57837

--- js/src/jit/RegisterSets.h
+++ js/src/jit/RegisterSets.h
@@ -920,7 +920,10 @@ class SpecializedRegSet<Accessors, RegisterSet> : public Accessors
     }
 
     void take(Register reg) {
-        MOZ_ASSERT(has(reg));
+#ifdef DEBUG
+        bool hasReg = has(reg);
+        MOZ_ASSERT(hasReg);
+#endif
         takeUnchecked(reg);
     }
     void take(FloatRegister reg) {
