b3bc373e9ea573acc9a3034c431eb3a6c8cd0a11
Ryan VanderMeulen <ryanvm@gmail.com>
2018-10-04 18:07:12 -0400

    Backed out changeset 563a3c11bb88 (bug 1481844)

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -3650,7 +3650,7 @@ def CreateBindingJSObject(descriptor, properties):
         if descriptor.interface.getExtendedAttribute('OverrideBuiltins'):
             create = dedent(
                 """
-                aObject->mExpandoAndGeneration.expando.setUndefined();
+                MOZ_ASSERT(aObject->mExpandoAndGeneration.expando.isUndefined());
                 JS::Rooted<JS::Value> expandoValue(aCx, JS::PrivateValue(&aObject->mExpandoAndGeneration));
                 creator.CreateProxyObject(aCx, &sClass.mBase, DOMProxyHandler::getInstance(),
                                           proto, aObject, expandoValue, aReflector);
