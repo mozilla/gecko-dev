aa4eecf4882728b9c0ff3ae80aec733a00ad39ff
Tom Prince <mozilla@hocat.ca>
2018-10-30 18:21:40 +0000

    Bug 1492128: [mozrelease] Add command to generate buglist for a given release; r=mtabara
    Differential Revision: https://phabricator.services.mozilla.com/D10154
    
    --HG--
    extra : source : 5a4d509ac7a512558b8ceef69e45425938b0d11a
    extra : absorb_source : c710070ded0ee01616d18a772fe2a36006037de9

--- build/mach_bootstrap.py
+++ build/mach_bootstrap.py
@@ -46,6 +46,7 @@ MACH_MODULES = [
     'python/mozbuild/mozbuild/backend/mach_commands.py',
     'python/mozbuild/mozbuild/compilation/codecomplete.py',
     'python/mozbuild/mozbuild/frontend/mach_commands.py',
+    'python/mozrelease/mozrelease/mach_commands.py',
     'taskcluster/mach_commands.py',
     'testing/awsy/mach_commands.py',
     'testing/firefox-ui/mach_commands.py',
@@ -86,7 +87,7 @@ CATEGORIES = {
     'ci': {
         'short': 'CI',
         'long': 'Taskcluster commands',
-        'priority': 59
+        'priority': 59,
     },
     'devenv': {
         'short': 'Development Environment',
@@ -103,13 +104,18 @@ CATEGORIES = {
         'long': 'Potent potables and assorted snacks.',
         'priority': 10,
     },
+    'release': {
+        'short': 'Release automation',
+        'long': 'Commands for used in release automation.',
+        'priority': 5,
+    },
     'disabled': {
         'short': 'Disabled',
         'long': 'The disabled commands are hidden by default. Use -v to display them. '
         'These commands are unavailable for your current context, '
         'run "mach <command>" to see why.',
         'priority': 0,
-    }
+    },
 }
 
 
