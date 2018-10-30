74eecb05fbb16198cfff0c438717266c374c15c7
Tom Prince <mozilla@hocat.ca>
2018-10-30 21:03:48 +0000

    Bug 1492128: Set TASKCLUSTER_ROOT_URL in tasks that use the taskcluster proxy; r=dustin
    This sets `TASKCLUSTER_BASE_URL` instead of `TASKCLUSTER_ROOT_URL`, since the proxy doesn't support the redeployable URL schema yet.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10149
    
    --HG--
    extra : source : 31237fb789a25e489281bd692957f4a1fafbcdc8
    extra : absorb_source : 918a94745e4abc699466a278cb5f32b57cd0b56f

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -772,6 +772,7 @@ def build_docker_worker_payload(config, task, task_def):
 
     if worker.get('taskcluster-proxy'):
         features['taskclusterProxy'] = True
+        worker['env']['TASKCLUSTER_PROXY_URL'] = 'http://taskcluster/'
 
     if worker.get('allow-ptrace'):
         features['allowPtrace'] = True
@@ -990,6 +991,7 @@ def build_generic_worker_payload(config, task, task_def):
 
     if worker.get('taskcluster-proxy'):
         features['taskclusterProxy'] = True
+        worker['env']['TASKCLUSTER_PROXY_URL'] = 'http://taskcluster/'
 
     if worker.get('run-as-administrator', False):
         features['runAsAdministrator'] = True
