15737a791b00bcb41d389968856d979c4bf58e0b
Yaron Tausky <ytausky@mozilla.com>
2018-12-20 13:39:46 +0000

    Bug 1502871 - Get RefPtr to transaction before using it. r=janv, a=RyanVM
    Avoid use-after-free by getting a RefPtr to a transaction before calling
    content code that could cause its deallocation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D14427
    
    --HG--
    extra : source : fd8d109b37c1cc5642971fc254bfa60153f5d98d
    extra : intermediate-source : 5ae197bcb7f7d87dafcb6d90045dc8b01e4cd282

--- dom/indexedDB/ActorsChild.cpp
+++ dom/indexedDB/ActorsChild.cpp
@@ -3600,6 +3600,8 @@ mozilla::ipc::IPCResult BackgroundCursorChild::RecvResponse(
   RefPtr<IDBCursor> cursor;
   mStrongCursor.swap(cursor);
 
+  RefPtr<IDBTransaction> transaction = mTransaction;
+
   switch (aResponse.type()) {
     case CursorResponse::Tnsresult:
       HandleResponse(aResponse.get_nsresult());
@@ -3629,7 +3631,7 @@ mozilla::ipc::IPCResult BackgroundCursorChild::RecvResponse(
       MOZ_CRASH("Should never get here!");
   }
 
-  mTransaction->OnRequestFinished(/* aActorDestroyedNormally */ true);
+  transaction->OnRequestFinished(/* aActorDestroyedNormally */ true);
 
   return IPC_OK();
 }
