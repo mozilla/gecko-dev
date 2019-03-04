60738a20206b0fb9855fe232487f99a6449daf7c
Honza Bambas <honzab.moz@firemni.cz>
2019-03-04 11:46:33 +0100

    Bug 1518774, r=dragana a=RyanVM
    --HG--
    extra : amend_source : 7eaf1d995d5ce5e80baebda340b8dd70ee1ad863

--- xpcom/io/nsStringStream.cpp
+++ xpcom/io/nsStringStream.cpp
@@ -22,6 +22,7 @@
 #include "nsIClassInfoImpl.h"
 #include "mozilla/Attributes.h"
 #include "mozilla/ipc/InputStreamUtils.h"
+#include "mozilla/ReentrantMonitor.h"
 #include "nsIIPCSerializableInputStream.h"
 
 using namespace mozilla::ipc;
@@ -47,7 +48,7 @@ class nsStringInputStream final : public nsIStringInputStream,
   NS_DECL_NSIIPCSERIALIZABLEINPUTSTREAM
   NS_DECL_NSICLONEABLEINPUTSTREAM
 
-  nsStringInputStream() { Clear(); }
+  nsStringInputStream() : mMon("nsStringInputStream") { Clear(); }
 
  private:
   ~nsStringInputStream() {}
@@ -62,6 +63,8 @@ class nsStringInputStream final : public nsIStringInputStream,
 
   nsDependentCSubstring mData;
   uint32_t mOffset;
+
+  mozilla::ReentrantMonitor mMon;
 };
 
 // This class needs to support threadsafe refcounting since people often
@@ -91,6 +94,8 @@ nsStringInputStream::GetType(uint16_t* aType) {
 
 NS_IMETHODIMP
 nsStringInputStream::GetData(nsACString& data) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   // The stream doesn't have any data when it is closed.  We could fake it
   // and return an empty string here, but it seems better to keep this return
   // value consistent with the behavior of the other 'getter' methods.
@@ -104,6 +109,8 @@ nsStringInputStream::GetData(nsACString& data) {
 
 NS_IMETHODIMP
 nsStringInputStream::SetData(const nsACString& aData) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   if (NS_WARN_IF(!mData.Assign(aData, fallible))) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
@@ -124,6 +131,8 @@ nsStringInputStream::ToString(char** aResult) {
 
 NS_IMETHODIMP
 nsStringInputStream::SetData(const char* aData, int32_t aDataLen) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   if (NS_WARN_IF(!aData)) {
     return NS_ERROR_INVALID_ARG;
   }
@@ -138,6 +147,8 @@ nsStringInputStream::SetData(const char* aData, int32_t aDataLen) {
 
 NS_IMETHODIMP
 nsStringInputStream::AdoptData(char* aData, int32_t aDataLen) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   if (NS_WARN_IF(!aData)) {
     return NS_ERROR_INVALID_ARG;
   }
@@ -148,6 +159,8 @@ nsStringInputStream::AdoptData(char* aData, int32_t aDataLen) {
 
 NS_IMETHODIMP
 nsStringInputStream::ShareData(const char* aData, int32_t aDataLen) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   if (NS_WARN_IF(!aData)) {
     return NS_ERROR_INVALID_ARG;
   }
@@ -163,6 +176,8 @@ nsStringInputStream::ShareData(const char* aData, int32_t aDataLen) {
 
 NS_IMETHODIMP_(size_t)
 nsStringInputStream::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   size_t n = aMallocSizeOf(this);
   n += mData.SizeOfExcludingThisIfUnshared(aMallocSizeOf);
   return n;
@@ -174,12 +189,16 @@ nsStringInputStream::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) {
 
 NS_IMETHODIMP
 nsStringInputStream::Close() {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   Clear();
   return NS_OK;
 }
 
 NS_IMETHODIMP
 nsStringInputStream::Available(uint64_t* aLength) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   NS_ASSERTION(aLength, "null ptr");
 
   if (Closed()) {
@@ -199,6 +218,8 @@ nsStringInputStream::Read(char* aBuf, uint32_t aCount, uint32_t* aReadCount) {
 NS_IMETHODIMP
 nsStringInputStream::ReadSegments(nsWriteSegmentFun aWriter, void* aClosure,
                                   uint32_t aCount, uint32_t* aResult) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   NS_ASSERTION(aResult, "null ptr");
   NS_ASSERTION(Length() >= mOffset, "bad stream state");
 
@@ -240,6 +261,8 @@ nsStringInputStream::IsNonBlocking(bool* aNonBlocking) {
 
 NS_IMETHODIMP
 nsStringInputStream::Seek(int32_t aWhence, int64_t aOffset) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   if (Closed()) {
     return NS_BASE_STREAM_CLOSED;
   }
@@ -271,6 +294,8 @@ nsStringInputStream::Seek(int32_t aWhence, int64_t aOffset) {
 
 NS_IMETHODIMP
 nsStringInputStream::Tell(int64_t* aOutWhere) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   if (Closed()) {
     return NS_BASE_STREAM_CLOSED;
   }
@@ -281,6 +306,8 @@ nsStringInputStream::Tell(int64_t* aOutWhere) {
 
 NS_IMETHODIMP
 nsStringInputStream::SetEOF() {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   if (Closed()) {
     return NS_BASE_STREAM_CLOSED;
   }
@@ -295,6 +322,8 @@ nsStringInputStream::SetEOF() {
 
 void nsStringInputStream::Serialize(InputStreamParams& aParams,
                                     FileDescriptorArray& /* aFDs */) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   StringInputStreamParams params;
   params.data() = PromiseFlatCString(mData);
   aParams = params;
@@ -318,6 +347,8 @@ bool nsStringInputStream::Deserialize(const InputStreamParams& aParams,
 }
 
 Maybe<uint64_t> nsStringInputStream::ExpectedSerializedLength() {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   return Some(static_cast<uint64_t>(Length()));
 }
 
@@ -333,6 +364,8 @@ nsStringInputStream::GetCloneable(bool* aCloneableOut) {
 
 NS_IMETHODIMP
 nsStringInputStream::Clone(nsIInputStream** aCloneOut) {
+  ReentrantMonitorAutoEnter lock(mMon);
+
   RefPtr<nsStringInputStream> ref = new nsStringInputStream();
   nsresult rv = ref->SetData(mData);
   if (NS_WARN_IF(NS_FAILED(rv))) {
