76e1e2a134b25c37883bc3f1aa833b8be71a29c6
Sylvestre Ledru <sledru@mozilla.com>
2019-03-10 15:29:41 +0000

    Bug 1531176 - Split the Google key management between gls and safe browsing r=glandium a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D21459
    
    --HG--
    extra : source : 43376e0c35631124193d304daeea1c557d487a8a
    extra : intermediate-source : 01b634d28203ce7be051454f700be3d52af08494

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1342,10 +1342,12 @@ pref("dom.debug.propagate_gesture_events_through_content", false);
 
 // All the Geolocation preferences are here.
 //
-
-pref("geo.wifi.uri", "https://www.googleapis.com/geolocation/v1/geolocate?key=%GOOGLE_API_KEY%");
-// MLS URL:
-// pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+#ifndef EARLY_BETA_OR_EARLIER
+pref("geo.wifi.uri", "https://www.googleapis.com/geolocation/v1/geolocate?key=%GOOGLE_LOCATION_SERVICE_API_KEY%");
+#else
+// Use MLS on Nightly and early Beta.
+pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+#endif
 
 #ifdef XP_MACOSX
 pref("geo.provider.use_corelocation", true);
--- toolkit/components/urlformatter/nsURLFormatter.js
+++ toolkit/components/urlformatter/nsURLFormatter.js
@@ -107,7 +107,8 @@ nsURLFormatterService.prototype = {
     OS_VERSION:       function() { return this.OSVersion; },
     CHANNEL:          () => UpdateUtils.UpdateChannel,
     MOZILLA_API_KEY:  () => AppConstants.MOZ_MOZILLA_API_KEY,
-    GOOGLE_API_KEY:   () => AppConstants.MOZ_GOOGLE_API_KEY,
+    GOOGLE_LOCATION_SERVICE_API_KEY:   () => AppConstants.MOZ_GOOGLE_LOCATION_SERVICE_API_KEY,
+    GOOGLE_SAFEBROWSING_API_KEY:   () => AppConstants.MOZ_GOOGLE_SAFEBROWSING_API_KEY,
     BING_API_CLIENTID:() => AppConstants.MOZ_BING_API_CLIENTID,
     BING_API_KEY:     () => AppConstants.MOZ_BING_API_KEY,
     DISTRIBUTION:     function() { return this.distribution.id; },
@@ -151,7 +152,10 @@ nsURLFormatterService.prototype = {
 
   trimSensitiveURLs: function uf_trimSensitiveURLs(aMsg) {
     // Only the google API key is sensitive for now.
-    return AppConstants.MOZ_GOOGLE_API_KEY ? aMsg.replace(RegExp(AppConstants.MOZ_GOOGLE_API_KEY, 'g'),
+    aMsg = AppConstants.MOZ_GOOGLE_LOCATION_SERVICE_API_KEY ? aMsg.replace(RegExp(AppConstants.MOZ_GOOGLE_LOCATION_SERVICE_API_KEY, "g"),
+                                                 "[trimmed-google-api-key]")
+                                  : aMsg;
+    return AppConstants.MOZ_GOOGLE_SAFEBROWSING_API_KEY ? aMsg.replace(RegExp(AppConstants.MOZ_GOOGLE_SAFEBROWSING_API_KEY, "g"),
                                                  "[trimmed-google-api-key]")
                                   : aMsg;
   }
