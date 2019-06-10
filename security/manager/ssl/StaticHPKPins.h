2b904e22879ffd6d024a6df07ffd68cdb74ebd01
ffxbld <ffxbld@mozilla.com>
2019-06-10 12:41:26 +0000

    No Bug, mozilla-esr60 repo-update HSTS HPKP blocklist - a=repo-update r=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D34342

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -1154,4 +1154,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1568291737769000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1568637437183000);
