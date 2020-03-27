96a936a428b6c179373311f86cd8f7fac8db50cc
Brian Hackett <bhackett1024@gmail.com>
2020-03-26 15:42:24 -1000

    Backout 228f94d3ba00fe7273ab7073aa03fd41f660c8d0, an alternate approach will be used here.

--- devtools/server/actors/source.js
+++ devtools/server/actors/source.js
@@ -168,10 +168,7 @@ const SourceActor = ActorClassWithSpec(sourceSpec, {
     const source = this._source;
 
     let introductionUrl = null;
-    if (isReplaying) {
-      // Avoid waiting for the script to be loaded when replaying.
-      introductionUrl = source.replayIntroductionUrl;
-    } else if (source.introductionScript) {
+    if (source.introductionScript) {
       introductionUrl = source.introductionScript.source.url;
     }
 
