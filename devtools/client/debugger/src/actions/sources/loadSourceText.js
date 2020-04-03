d16e51834701320222f5daf6b850d116a96c6949
Brian Hackett <bhackett1024@gmail.com>
2020-04-03 09:40:34 -1000

    Fix bad reference.

--- devtools/client/debugger/src/actions/sources/loadSourceText.js
+++ devtools/client/debugger/src/actions/sources/loadSourceText.js
@@ -130,7 +130,7 @@ async function loadSourceTextPromise(
         : { type: "text", value: "", contentType: undefined }
     );
 
-    client.events.sourceLoaded(newSource.id);
+    client.eventMethods.sourceLoaded(newSource.id);
 
     // Update the text in any breakpoints for this source by re-adding them.
     const breakpoints = getBreakpointsForSource(getState(), source.id);
