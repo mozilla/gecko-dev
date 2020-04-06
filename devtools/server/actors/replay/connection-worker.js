f4e40de7fe1548c8b8af08cccc69beb465735aea
Brian Hackett <bhackett1024@gmail.com>
2020-04-06 09:48:24 -1000

    Tidy up connection worker, always use correct socket for sending data.

--- devtools/server/actors/replay/connection-worker.js
+++ devtools/server/actors/replay/connection-worker.js
@@ -14,13 +14,7 @@ let gVerbose;
 
 ChromeUtils.recordReplayRegisterConnectionWorker(doSend);
 
-self.addEventListener("message", msg => {
-  try {
-    onMainThreadMessage(msg);
-  } catch (e) {
-    PostError(e);
-  }
-});
+self.addEventListener("message", makeInfallible(onMainThreadMessage));
 
 function onMainThreadMessage({ data }) {
   switch (data.kind) {
@@ -29,6 +23,7 @@ function onMainThreadMessage({ data }) {
       gBuildId = data.buildId;
       gVerbose = data.verbose;
       openServerSocket();
+      break;
     case "connect":
       doConnect(data.id, data.channelId);
       break;
@@ -36,16 +31,16 @@ function onMainThreadMessage({ data }) {
       doLog(data.text);
       break;
     default:
-      PostError(`Unknown event kind ${data.kind}`);
+      postError(`Unknown event kind ${data.kind}`);
   }
 }
 
 function openServerSocket() {
   gServerSocket = new WebSocket(gServerAddress);
-  gServerSocket.onopen = onServerOpen;
-  gServerSocket.onclose = onServerClose;
-  gServerSocket.onmessage = onServerMessage;
-  gServerSocket.onerror = onServerError;
+  gServerSocket.onopen = makeInfallible(onServerOpen);
+  gServerSocket.onclose = makeInfallible(onServerClose);
+  gServerSocket.onmessage = makeInfallible(onServerMessage);
+  gServerSocket.onerror = makeInfallible(onServerError);
 }
 
 // Whether log messages can be sent to the cloud server.
@@ -80,28 +75,24 @@ function onServerError(evt) {
 let gSessionId;
 
 async function onServerMessage(evt) {
-  try {
-    const data = JSON.parse(evt.data);
-    switch (data.kind) {
-      case "modules": {
-        const { sessionId, controlJS, replayJS, updateNeeded, updateWanted } = data;
-        gConnected = true;
-        gSessionId = sessionId;
-        postMessage({ kind: "loaded", sessionId, controlJS, replayJS, updateNeeded, updateWanted });
-        break;
-      }
-      case "connectionAddress": {
-        const { id, replayerSessionId, address } = data;
-        gConnections[id].replayerSessionId = replayerSessionId;
-        gConnections[id].connectWaiter(address);
-        break;
-      }
-      case "connectionFailed":
-        postMessage({ kind: "connectionFailed" });
-        break;
+  const data = JSON.parse(evt.data);
+  switch (data.kind) {
+    case "modules": {
+      const { sessionId, controlJS, replayJS, updateNeeded, updateWanted } = data;
+      gConnected = true;
+      gSessionId = sessionId;
+      postMessage({ kind: "loaded", sessionId, controlJS, replayJS, updateNeeded, updateWanted });
+      break;
     }
-  } catch (e) {
-    PostError(e);
+    case "connectionAddress": {
+      const { id, replayerSessionId, address } = data;
+      gConnections[id].replayerSessionId = replayerSessionId;
+      gConnections[id].connectWaiter(address);
+      break;
+    }
+    case "connectionFailed":
+      postMessage({ kind: "connectionFailed" });
+      break;
   }
 }
 
@@ -123,38 +114,137 @@ function maybeLogMessage(prefix, id, message, count, delay) {
 // and reattempting to connect.
 const SocketTimeoutMs = 20000;
 
+// Information about a socket opened for a replayer connection.
+function ConnectionSocket(id, bulk) {
+  // Identifying information for the socket.
+  this.id = id;
+  this.bulk = bulk;
+
+  // Underlying websocket.
+  this.socket = null;
+
+  // Whether the socket is open, and messages can be sent.
+  this.open = false;
+
+  // Whether a message has been received from the other side.
+  this.connected = false;
+
+  // Messages queued up for sending before the socket opened.
+  this.pending = [];
+
+  // Any data received that does not contain a complete message.
+  this.partialMessage = null;
+
+  // How many messages have been sent/received over this connection.
+  this.numSends = 0;
+  this.numRecvs = 0;
+}
+
+ConnectionSocket.prototype = {
+  connect(address) {
+    if (this.socket) {
+      throw new Error("duplicate connect");
+    }
+
+    this.socket = new WebSocket(address);
+    this.socket.binaryType = "arraybuffer";
+    this.socket.onopen = makeInfallible(this.onOpen, this);
+    this.socket.onclose = makeInfallible(this.onClose, this);
+    this.socket.onmessage = makeInfallible(this.onMessage, this);
+    this.socket.onerror = makeInfallible(this.onError, this);
+  },
+
+  close() {
+    this.socket.close();
+  },
+
+  onOpen() {
+    this.open = true;
+    this.pending.forEach(buf => doSend(this.id, buf));
+    this.pending.length = 0;
+  },
+
+  onClose() {
+    doLog(`ReplayerDisconnected ${this.id} ${elapsedTime()}\n`);
+    postMessage({ kind: "disconnected", id: this.id });
+    gConnections[this.id] = null;
+  },
+
+  sendMessage(buf, delay) {
+    if (!this.open) {
+      this.pending.push(buf);
+      return;
+    }
+
+    maybeLogMessage("SocketSend", this.id, new Uint8Array(buf), ++this.numSends, delay);
+
+    this.socket.send(buf);
+  },
+
+  onMessage(evt) {
+    // When we have heard back from the replayer, we are fully connected to it.
+    if (!this.connected && !this.bulk) {
+      doLog(`ReplayerConnected ${this.id} ${elapsedTime()}\n`);
+      postMessage({ kind: "connected", id: this.id });
+    }
+    this.connected = true;
+
+    let data = new Uint8Array(evt.data);
+
+    if (this.partialMessage) {
+      const newData = new Uint8Array(this.partialMessage.length + data.length);
+      newData.set(this.partialMessage);
+      newData.set(data, this.partialMessage.length);
+      data = newData;
+      this.partialMessage = null;
+    }
+
+    // Process any complete messages found in the received data.
+    let offset = 0;
+    while (true) {
+      const info = readMessage(data, offset);
+      if (!info || offset + info.size > data.length) {
+        if (offset < data.length) {
+          this.partialMessage = new Uint8Array(data.buffer, offset);
+        }
+        break;
+      }
+
+      // Copy the message into its own ArrayBuffer.
+      const msg = new Uint8Array(info.size);
+      msg.set(new Uint8Array(data.buffer, offset, info.size));
+
+      maybeLogMessage("SocketRecv", this.id, msg, ++this.numRecvs);
+      ChromeUtils.recordReplayOnMessage(this.id, msg.buffer);
+
+      offset += info.size;
+    }
+  },
+
+  onError() {
+    postError("SocketError", this.id);
+  },
+};
+
 async function doConnect(id, channelId) {
   doLog(`ReplayerConnect ${id} ${elapsedTime()}\n`);
 
   if (gConnections[id]) {
-    PostError(`Duplicate connection ID ${id}`);
+    postError(`Duplicate connection ID ${id}`);
   }
   const connection = {
     // ID assigned to this session by the dispatcher.
     replayerSessionId: null,
 
-    // Websockets used by this connection.
-    socket: null,
-    bulkSocket: null,
-
-    // Messages queued up before the main socket opened.
-    outgoing: [],
-
-    // Whether the main socket is open.
-    open: false,
-
-    // Whether the bulk socket is open.
-    bulkOpen: false,
+    // Sockets used by this connection.
+    socket: new ConnectionSocket(id, false),
+    bulkSocket: new ConnectionSocket(id, true),
 
     // Whether the main socket is fully established.
     connected: false,
 
     // Resolve hook for any promise waiting on the socket to connect.
     connectWaiter: null,
-
-    // How many messages have been sent/received over this connection.
-    numSends: 0,
-    numRecvs: 0,
   };
   gConnections[id] = connection;
 
@@ -163,7 +253,7 @@ async function doConnect(id, channelId) {
   const address = await new Promise(resolve => (connection.connectWaiter = resolve));
 
   if (!/^wss?:\/\//.test(address)) {
-    PostError(`Invalid websocket address ${text}`);
+    postError(`Invalid websocket address ${address}`);
   }
 
   function urlParams(bulk) {
@@ -171,27 +261,14 @@ async function doConnect(id, channelId) {
     return `id=${id}&dispatchId=${gSessionId}&bulk=${bulk}&verbose=${gVerbose}`;
   }
 
-  const socket = new WebSocket(`${address}/connect?${urlParams(false)}`);
-  socket.binaryType = "arraybuffer";
-  socket.onopen = evt => onOpen(id);
-  socket.onclose = evt => onClose(id);
-  socket.onmessage = evt => onMessage(id, evt);
-  socket.onerror = evt => onError(id);
-  connection.socket = socket;
-
-  const bulkSocket = new WebSocket(`${address}/connect?${urlParams(true)}`);
-  bulkSocket.binaryType = "arraybuffer";
-  bulkSocket.onopen = evt => onOpen(id, true);
-  bulkSocket.onclose = evt => onClose(id);
-  bulkSocket.onmessage = evt => onMessage(id, evt, true);
-  bulkSocket.onerror = evt => onError(id);
-  connection.bulkSocket = bulkSocket;
+  connection.socket.connect(`${address}/connect?${urlParams(false)}`);
+  connection.bulkSocket.connect(`${address}/connect?${urlParams(true)}`);
 
   setTimeout(() => {
     if (!connection.connected) {
       doLog(`ReplayerConnectionTimedOut ${id} ${elapsedTime()}\n`);
-      socket.close();
-      bulkSocket.close();
+      connection.socket.close();
+      connection.bulkSocket.close();
     }
   }, SocketTimeoutMs);
 }
@@ -216,11 +293,11 @@ function messageDescription(msg) {
 
 function checkCompleteMessage(buf) {
   if (buf.byteLength < 4) {
-    PostError(`Message too short`);
+    postError(`Message too short`);
   }
   const { bulk, size } = readMessage(new Uint8Array(buf));
   if (size != buf.byteLength) {
-    PostError(`Message not complete`);
+    postError(`Message not complete`);
   }
   return bulk;
 }
@@ -232,40 +309,12 @@ function doSend(id, buf, delay) {
       return;
     }
 
-    if (!connection.open) {
-      connection.outgoing.push(buf);
-      return;
-    }
-
-    maybeLogMessage("SocketSend", id, new Uint8Array(buf), ++connection.numSends, delay);
-
     const bulk = checkCompleteMessage(buf);
-    if (bulk && connection.bulkOpen) {
-      connection.bulkSocket.send(buf);
-    } else {
-      connection.socket.send(buf);
-    }
+    const socket = bulk ? connection.bulkSocket : connection.socket;
+    socket.sendMessage(buf, delay);
   } catch (e) {
-    PostError(`SendError ${e}`);
+    postError(`SendError ${e}`);
   }
-}
-
-function onOpen(id, bulk) {
-  // Messages can now be sent to the socket.
-  const connection = gConnections[id];
-  if (bulk) {
-    connection.bulkOpen = true;
-  } else {
-    connection.open = true;
-    connection.outgoing.forEach(buf => doSend(id, buf));
-    connection.outgoing.length = 0;
-  }
-}
-
-function onClose(id, bulk) {
-  doLog(`ReplayerDisconnected ${id} ${elapsedTime()}\n`);
-  postMessage({ kind: "disconnected", id });
-  gConnections[id] = null;
 }
 
 function arrayToString(arr) {
@@ -276,63 +325,25 @@ function arrayToString(arr) {
   return escape(str);
 }
 
-// Buffers containing incomplete messages.
-const partialMessages = new Map();
-
-function extractCompleteMessages(id, bulk, data, time) {
-  const key = `${id}:${bulk}`;
-  const oldData = partialMessages.get(key);
-  if (oldData) {
-    partialMessages.delete(key);
-    const newData = new Uint8Array(oldData.length + data.length);
-    newData.set(oldData);
-    newData.set(data, oldData.length);
-    data = newData;
+function postError(why, id) {
+  let text;
+  try {
+    text = why.toString();
+  } catch (e) {
+    text = "<Unknown>";
   }
+  doLog(`ReplayerConnectionError ${id} ${text} ${elapsedTime()}\n`);
+  postMessage({ kind: "error", why: text, id });
+}
 
-  const messages = [];
-  let offset = 0;
-  while (true) {
-    const info = readMessage(data, offset);
-    if (!info || offset + info.size > data.length) {
-      if (offset < data.length) {
-        partialMessages.set(key, new Uint8Array(data.buffer, offset));
-      }
-      break;
+function makeInfallible(fn, thisv) {
+  return (...args) => {
+    try {
+      fn.apply(thisv, args);
+    } catch (e) {
+      postError(e);
     }
-    // Copy the message into its own ArrayBuffer.
-    const msg = new Uint8Array(info.size);
-    msg.set(new Uint8Array(data.buffer, offset, info.size));
-
-    maybeLogMessage("SocketRecv", id, msg, ++gConnections[id].numRecvs);
-
-    messages.push(msg.buffer);
-    offset += info.size;
-  }
-  return messages;
-}
-
-function onMessage(id, evt, bulk) {
-  const connection = gConnections[id];
-
-  // When we have heard back from the replayer, we are fully connected to it.
-  if (!connection.connected && !bulk) {
-    connection.connected = true;
-    doLog(`ReplayerConnected ${id} ${elapsedTime()}\n`);
-    postMessage({ kind: "connected", id });
-  }
-
-  const messages = extractCompleteMessages(id, bulk, new Uint8Array(evt.data));
-  messages.forEach(msg => ChromeUtils.recordReplayOnMessage(id, msg));
-}
-
-function onError(id, evt) {
-  PostError("SocketError", id);
-}
-
-function PostError(why, id) {
-  doLog(`ReplayerConnectionError ${id} ${why.toString()} ${elapsedTime()}\n`);
-  postMessage({ kind: "error", why: why.toString(), id });
+  };
 }
 
 function doLog(text) {
