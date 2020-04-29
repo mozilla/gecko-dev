9ddc7aaad023b58e8f91ecf5a6a78600ed6866dd
Brian Hackett <bhackett1024@gmail.com>
2020-04-28 15:49:38 -1000

    Add inspector test that iframe contents are picked / marked up correctly.

--- devtools/client/webreplay/mochitest/browser_rr_inspector-02.js
+++ devtools/client/webreplay/mochitest/browser_rr_inspector-02.js
@@ -19,7 +19,7 @@ add_task(async function() {
   await addBreakpoint(dbg, "doc_inspector_basic.html", 9);
   await rewindToLine(dbg, 9);
 
-  const { testActor } = await openInspector();
+  const { testActor, inspector } = await openInspector();
 
   info("Waiting for element picker to become active.");
   toolbox.win.focus();
@@ -34,6 +34,19 @@ add_task(async function() {
   info("Performing checks");
   await testActor.isNodeCorrectlyHighlighted("#maindiv", is);
 
+  // EventUtils.synthesizeMouse() can't figure out the exact position on the
+  // screen of elements in iframes when replaying. Pass in some suitable values
+  // so that the right position is clicked. This is pretty cheesy.
+  await moveMouseOver(["#iframe", "#iframediv"], 10, 48);
+  await testActor.isNodeCorrectlyHighlighted(["#iframe", "#iframediv"], is);
+
+  mouseClick(["#iframe", "#iframediv"], 10, 48);
+  await waitUntil(() => {
+    const container = inspector.markup.getSelectedContainer();
+    return container && container.editor.textEditor &&
+      container.editor.textEditor.textNode.state.value == "IFRAME";
+  });
+
   await shutdownDebugger(dbg);
 
   function moveMouseOver(selector, x, y) {
@@ -45,5 +58,14 @@ add_task(async function() {
       options: { type: "mousemove" },
     });
     return toolbox.nodePicker.once("picker-node-hovered");
+  }
+
+  function mouseClick(selector, x, y) {
+    testActor.synthesizeMouse({
+      selector,
+      x,
+      y,
+      options: {},
+    });
   }
 });
