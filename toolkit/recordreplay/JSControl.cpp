05a1d12c22234406efe5cdae64adb48d66524bee
Brian Hackett <bhackett1024@gmail.com>
2020-12-10 13:06:42 -0700

    Watch for new message text when recordings finish, fixes #243

--- toolkit/recordreplay/JSControl.cpp
+++ toolkit/recordreplay/JSControl.cpp
@@ -164,7 +164,7 @@ MOZ_EXPORT ProgressCounter RecordReplayInterface_NewTimeWarpTarget() {
 
 void OnTestCommand(const char* aString) {
   // Ignore commands to finish the current test if we aren't recording/replaying.
-  if (!strcmp(aString, "RecReplaySendAsyncMessage RecordingFinished") &&
+  if (!strcmp(aString, "RecReplaySendAsyncMessage Example__Finished") &&
       !IsRecordingOrReplaying()) {
     return;
   }
