c8fb2a3779d78de22ead674a5d435d64fa0a3ced
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2021-06-09 10:13:10 +0000

    Add ARCH_CPU_* for riscv

--- security/sandbox/chromium/build/build_config.h
+++ security/sandbox/chromium/build/build_config.h
@@ -166,6 +166,18 @@
 #define ARCH_CPU_32_BITS 1
 #define ARCH_CPU_BIG_ENDIAN 1
 #endif
+#elif defined(__riscv)
+#if __riscv_xlen == 32
+#  define ARCH_CPU_RISCV_FAMILY 1
+#  define ARCH_CPU_RISCV32 1
+#  define ARCH_CPU_32_BITS 1
+#  define ARCH_CPU_LITTLE_ENDIAN 1
+#elif __riscv_xlen == 64
+#  define ARCH_CPU_RISCV_FAMILY 1
+#  define ARCH_CPU_RISCV64 1
+#  define ARCH_CPU_64_BITS 1
+#  define ARCH_CPU_LITTLE_ENDIAN 1
+#endif
 #else
 #error Please add support for your architecture in build/build_config.h
 #endif
