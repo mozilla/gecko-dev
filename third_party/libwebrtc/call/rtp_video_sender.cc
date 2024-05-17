a3fa8f4fc0cde5fafa128c96d468189b1b6bbfa9
Michael Froman <mfroman@mozilla.com>
2024-05-17 11:44:57 -0500

    Bug 1897501 - Vendor libwebrtc from 626edea852
    Upstream commit: https://webrtc.googlesource.com/src/+/626edea852db77b87e432854017a78ca6a0f26b5
        Use independet frame IDs between simulcast streams when `WebRTC-GenericDescriptorAuth` is disabled.
    
        Implemented behind `WebRTC-Video-SimulcastIndependentFrameIds`.
    
        Bug: b/329063481
        Change-Id: I683e567bb5b449f998be57ec3a11bb3b95e3ace4
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/343382
        Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#41927}

--- third_party/libwebrtc/call/rtp_video_sender.cc
+++ third_party/libwebrtc/call/rtp_video_sender.cc
@@ -402,6 +402,10 @@ RtpVideoSender::RtpVideoSender(
       rtp_config_(rtp_config),
       codec_type_(GetVideoCodecType(rtp_config)),
       transport_(transport),
+      independent_frame_ids_(
+          field_trials_.IsEnabled(
+              "WebRTC-Video-SimulcastIndependentFrameIds") &&
+          field_trials_.IsDisabled("WebRTC-GenericDescriptorAuth")),
       transport_overhead_bytes_per_packet_(0),
       encoder_target_rate_bps_(0),
       frame_counts_(rtp_config.ssrcs.size()),
@@ -575,11 +579,16 @@ EncodedImageCallback::Result RtpVideoSender::OnEncodedImage(
     }
   }
 
+  absl::optional<int64_t> frame_id;
+  if (!independent_frame_ids_) {
+    frame_id = shared_frame_id_;
+  }
+
   bool send_result =
       rtp_streams_[simulcast_index].sender_video->SendEncodedImage(
           rtp_config_.payload_type, codec_type_, rtp_timestamp, encoded_image,
           params_[simulcast_index].GetRtpVideoHeader(
-              encoded_image, codec_specific_info, shared_frame_id_),
+              encoded_image, codec_specific_info, frame_id),
           expected_retransmission_time);
   if (frame_count_observer_) {
     FrameCounts& counts = frame_counts_[simulcast_index];
