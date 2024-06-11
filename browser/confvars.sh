871d25f61ec02b2059ef49730d99ea1b5e251067
serge-sans-paille <sguelton@mozilla.com>
2024-06-11 16:30:58 +0000

    Bug 1898177 - Move MOZ_APP_VENDOR from confvars.sh to moz.configure r=glandium,geckoview-reviewers,m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D212338

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -3,8 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_VENDOR=Mozilla
-
 if test "$OS_ARCH" = "WINNT"; then
   if ! test "$HAVE_64BIT_BUILD"; then
     if test "$MOZ_UPDATE_CHANNEL" = "nightly" -o \
