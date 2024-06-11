d004e6dda31d2233706369b82f84aa6e00d1f562
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-06-11 17:11:03 -0500

    Delete config directory

--- config/external/nspr/_pr_bld.h
+++
@@ -1,6 +0,0 @@
-/*
- * This file was previously generated at build time to set some preprocessor
- * macros for use by prvrsion.c, but that file will set the values to (empty)
- * defaults if they're not set, so this is just a stub to avoid having to
- * generate another header file at build time.
- */
