c4ad019d5a7b92079ec737c2f475049d4484295e
sarah541 <sarahkhan1107@hotmail.com>
2024-06-12 16:25:51 +0000

    Bug 1899640 - Fix display toolbar paddings r=android-reviewers,Roger,harrisono,mavduevskiy
    Differential Revision: https://phabricator.services.mozilla.com/D212058

--- mobile/android/android-components/components/browser/toolbar/src/main/java/mozilla/components/browser/toolbar/display/DisplayToolbar.kt
+++ mobile/android/android-components/components/browser/toolbar/src/main/java/mozilla/components/browser/toolbar/display/DisplayToolbar.kt
@@ -669,8 +669,8 @@ class DisplayToolbar internal constructor(
     fun setHorizontalPadding(horizontalPadding: Int) {
         val background = views.background
         (background.layoutParams as? ConstraintLayout.LayoutParams)?.apply {
-            marginStart = horizontalPadding
-            marginEnd = horizontalPadding
+            goneStartMargin = horizontalPadding
+            goneEndMargin = horizontalPadding
             background.layoutParams = this
         }
     }
