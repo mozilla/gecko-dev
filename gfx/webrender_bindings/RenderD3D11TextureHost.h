b4bd4ed4c0d5386ff2eadbff270bf317e9dcb213
sotaro <sotaro.ikeda.g@gmail.com>
2024-06-12 17:29:38 +0000

    Bug 1901947 - Clean up some DXGITextureHostD3D11 code r=gfx-reviewers,lsalzman
    Removed unused members and clean up around DXGITextureHostD3D11::GetAsSurface().
    
    This is a preparation for Bug 1889275.
    
    Differential Revision: https://phabricator.services.mozilla.com/D213389

--- gfx/webrender_bindings/RenderD3D11TextureHost.h
+++ gfx/webrender_bindings/RenderD3D11TextureHost.h
@@ -27,12 +27,13 @@ namespace wr {
 class RenderDXGITextureHost final : public RenderTextureHostSWGL {
  public:
   RenderDXGITextureHost(
-      RefPtr<gfx::FileHandleWrapper> aHandle,
-      Maybe<layers::GpuProcessTextureId>& aGpuProcessTextureId,
-      uint32_t aArrayIndex, gfx::SurfaceFormat aFormat, gfx::ColorSpace2,
-      gfx::ColorRange aColorRange, gfx::IntSize aSize, bool aHasKeyedMutex,
-      gfx::FenceInfo& aAcquireFenceInfo,
-      Maybe<layers::GpuProcessQueryId>& aGpuProcessQueryId);
+      const RefPtr<gfx::FileHandleWrapper> aHandle,
+      const Maybe<layers::GpuProcessTextureId>& aGpuProcessTextureId,
+      const uint32_t aArrayIndex, const gfx::SurfaceFormat aFormat,
+      const gfx::ColorSpace2 aColorSpace, const gfx::ColorRange aColorRange,
+      const gfx::IntSize aSize, const bool aHasKeyedMutex,
+      const gfx::FenceInfo& aAcquireFenceInfo,
+      const Maybe<layers::GpuProcessQueryId>& aGpuProcessQueryId);
 
   wr::WrExternalImage Lock(uint8_t aChannelIndex, gl::GLContext* aGL) override;
   void Unlock() override;
@@ -103,11 +104,11 @@ class RenderDXGITextureHost final : public RenderTextureHostSWGL {
 
   RefPtr<gl::GLContext> mGL;
 
-  RefPtr<gfx::FileHandleWrapper> mHandle;
-  Maybe<layers::GpuProcessTextureId> mGpuProcessTextureId;
-  Maybe<layers::GpuProcessQueryId> mGpuProcessQueryId;
+  const RefPtr<gfx::FileHandleWrapper> mHandle;
+  const Maybe<layers::GpuProcessTextureId> mGpuProcessTextureId;
+  const Maybe<layers::GpuProcessQueryId> mGpuProcessQueryId;
   RefPtr<ID3D11Texture2D> mTexture;
-  uint32_t mArrayIndex = 0;
+  const uint32_t mArrayIndex;
   RefPtr<IDXGIKeyedMutex> mKeyedMutex;
 
   // Temporary state between MapPlane and UnmapPlanes.
