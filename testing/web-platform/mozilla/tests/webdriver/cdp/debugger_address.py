130b05b763550b96dab07a5fb3af5b94f4b4eb1c
Henrik Skupin <mail@hskupin.info>
2024-06-12 08:57:37 +0000

    Bug 1882089 - [remote] Update tests for explictly enabling the CDP protocol if needed. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D212908

--- testing/web-platform/mozilla/tests/webdriver/cdp/debugger_address.py
+++ testing/web-platform/mozilla/tests/webdriver/cdp/debugger_address.py
@@ -4,21 +4,55 @@ import json
 
 import pytest
 from support.context import using_context
+from support.helpers import clear_pref
 from tests.support.http_request import HTTPRequest
 
 
+@pytest.fixture(autouse=True)
+def clear_protocol_pref(session):
+    yield
+    clear_pref(session, "remote.active-protocols")
+
+
+@pytest.mark.capabilities(
+    {
+        "moz:firefoxOptions": {
+            "prefs": {
+                "remote.active-protocols": 2,
+            },
+        },
+    }
+)
 def test_debugger_address_not_set(session):
     debugger_address = session.capabilities.get("moz:debuggerAddress")
     assert debugger_address is None
 
 
-@pytest.mark.capabilities({"moz:debuggerAddress": False})
+@pytest.mark.capabilities(
+    {
+        "moz:debuggerAddress": False,
+        "moz:firefoxOptions": {
+            "prefs": {
+                "remote.active-protocols": 2,
+            },
+        },
+    }
+)
 def test_debugger_address_false(session):
     debugger_address = session.capabilities.get("moz:debuggerAddress")
     assert debugger_address is None
 
 
-@pytest.mark.capabilities({"moz:debuggerAddress": True})
+@pytest.mark.capabilities(
+    {
+        "moz:debuggerAddress": True,
+        "moz:firefoxOptions": {
+            "prefs": {
+                "remote.active-protocols": 2,
+            },
+        },
+    }
+)
 @pytest.mark.parametrize("fission_enabled", [True, False], ids=["enabled", "disabled"])
 def test_debugger_address_true_with_fission(session, fission_enabled):
     debugger_address = session.capabilities.get("moz:debuggerAddress")
