0261f9b42b8f1daedf592413a83a899797377b2e
Nikki Sharpley <nsharpley@mozilla.com>
2024-06-12 15:06:01 +0000

    Bug 1891777 - Add a tools pref to the sidebar revamp r=sidebar-reviewers,kcochrane
    Rather than persisting via SessionStore, as the original bug suggests, use a pref to globally
    store the disabled/enabled state of the sidebar tools.
    
    Not entirely unrelated, I added a test for adopting the sidebar from an opener window as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D212853

--- browser/components/sidebar/browser-sidebar.js
+++ browser/components/sidebar/browser-sidebar.js
@@ -7,6 +7,12 @@
  * dynamically adding menubar menu items for the View -> Sidebar menu,
  * and provides APIs for sidebar extensions, etc.
  */
+const defaultTools = {
+  viewHistorySidebar: "history",
+  viewTabsSidebar: "syncedtabs",
+  viewBookmarksSidebar: "bookmarks",
+};
+
 var SidebarController = {
   makeSidebar({ elementId, ...rest }) {
     return {
@@ -154,6 +160,7 @@ var SidebarController = {
   },
   POSITION_START_PREF: "sidebar.position_start",
   DEFAULT_SIDEBAR_ID: "viewBookmarksSidebar",
+  TOOLS_PREF: "sidebar.main.tools",
 
   // lastOpenedId is set in show() but unlike currentID it's not cleared out on hide
   // and isn't persisted across windows
@@ -632,6 +639,19 @@ var SidebarController = {
       this.toolsAndExtensions.delete(commandID);
       this.toolsAndExtensions.set(commandID, toggledTool);
     }
+    // Tools are persisted via a pref.
+    if (!Object.hasOwn(toggledTool, "extensionId")) {
+      const tools = new Set(this.sidebarRevampTools.split(","));
+      const updatedTools = tools.has(defaultTools[commandID])
+        ? Array.from(tools).filter(
+            tool => !!tool && tool != defaultTools[commandID]
+          )
+        : [
+            ...Array.from(tools).filter(tool => !!tool),
+            defaultTools[commandID],
+          ];
+      Services.prefs.setStringPref(this.TOOLS_PREF, updatedTools.join());
+    }
     window.dispatchEvent(new CustomEvent("SidebarItemChanged"));
   },
 
@@ -803,19 +823,17 @@ var SidebarController = {
    * @returns {Array}
    */
   getTools() {
-    const toolIds = [
-      "viewHistorySidebar",
-      "viewTabsSidebar",
-      "viewBookmarksSidebar",
-    ];
-    return toolIds.map(commandID => {
+    return Object.keys(defaultTools).map(commandID => {
       const sidebar = this.sidebars.get(commandID);
+      const disabled = !this.sidebarRevampTools
+        .split(",")
+        .includes(defaultTools[commandID]);
       return {
         commandID,
         view: commandID,
         iconUrl: sidebar.iconUrl,
         l10nId: sidebar.revampL10nId,
-        disabled: sidebar.disabled ?? false,
+        disabled,
       };
     });
   },
@@ -1044,4 +1062,10 @@ XPCOMUtils.defineLazyPreferenceGetter(
   "sidebarRevampEnabled",
   "sidebar.revamp",
   false
+);
+XPCOMUtils.defineLazyPreferenceGetter(
+  SidebarController,
+  "sidebarRevampTools",
+  "sidebar.main.tools",
+  "history, syncedtabs"
 );
