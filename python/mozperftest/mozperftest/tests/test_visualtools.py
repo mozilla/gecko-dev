c01916dbacb451bde6d7efd755c8976535eeb3f4
ahochheiden <ahochheiden@mozilla.com>
2024-06-12 16:10:34 +0000

    Bug 1898817 - Replace `distutils.spawn.find_executable` with `shutil.which` within `mozperftest` r=mozperftest-reviewers,sparky
    Differential Revision: https://phabricator.services.mozilla.com/D211631

--- python/mozperftest/mozperftest/tests/test_visualtools.py
+++ python/mozperftest/mozperftest/tests/test_visualtools.py
@@ -9,9 +9,7 @@ from mozperftest.test.browsertime.visualtools import get_dependencies, xvfb
 from mozperftest.utils import temporary_env
 
 
-@mock.patch(
-    "mozperftest.test.browsertime.visualtools.find_executable", new=lambda name: "Xvfb"
-)
+@mock.patch("mozperftest.test.browsertime.visualtools.which", new=lambda name: "Xvfb")
 def test_xvfb(*mocked):
     with temporary_env(DISPLAY="ME"):
         with mock.patch("subprocess.Popen") as mocked, xvfb():
@@ -19,9 +17,7 @@ def test_xvfb(*mocked):
         assert os.environ["DISPLAY"] == "ME"
 
 
-@mock.patch(
-    "mozperftest.test.browsertime.visualtools.find_executable", new=lambda name: "Xvfb"
-)
+@mock.patch("mozperftest.test.browsertime.visualtools.which", new=lambda name: "Xvfb")
 def test_xvfb_env(*mocked):
     with temporary_env(DISPLAY=None):
         with mock.patch("subprocess.Popen") as mocked, xvfb():
@@ -29,9 +25,7 @@ def test_xvfb_env(*mocked):
         assert "DISPLAY" not in os.environ
 
 
-@mock.patch(
-    "mozperftest.test.browsertime.visualtools.find_executable", new=lambda name: None
-)
+@mock.patch("mozperftest.test.browsertime.visualtools.which", new=lambda name: None)
 def test_xvfb_none(*mocked):
     with pytest.raises(FileNotFoundError), xvfb():
         pass
