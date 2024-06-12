8b4f2ccd5be86a17e8f812ec28c8a3daf788dc64
Frédéric Wang <fwang@igalia.com>
2024-06-12 07:50:42 +0000

    Bug 1881040 - Add WPT tests for global fetch with a destination. r=valentin
    Add a new fetchpriority-urgency-destination.https.h2.html test that
    is essentially combining features for
    
      /fetch/api/request/destination/fetch-destination.https.html
      /fetch/fetchpriority/fetchpriority-urgency.h2.html
    
    in order to test urgency of a global fetch with a destination. Also add
    a comment in fetch-tests-data.js to explain why we can't do a direct
    testing of the internal priority.
    
    Differential Revision: https://phabricator.services.mozilla.com/D210286

--- testing/web-platform/mozilla/tests/fetch/fetchpriority/support/priority-dependent-content.js
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/priority-dependent-content.js
@@ -1,6 +1,5 @@
 // See get_content in priority-dependent-content.py
 
-
 function convertSizeToUrgency(size) {
     return Math.round(size / 10 - 1);
 }
@@ -10,6 +9,11 @@ function convertRectToUrgency(rect) {
         throw "not a square";
     }
     return convertSizeToUrgency(rect.width);
+}
+
+function convertDurationToUrgency(duration) {
+    // Duration is not accurate, but floor gives better result than round.
+    return Math.floor(duration * 100 - 1);
 }
 
 function getElementUrgencyFromSize(id) {
--- testing/web-platform/mozilla/tests/fetch/fetchpriority/support/resources/README.md
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/resources/README.md
@@ -1,3 +1,5 @@
 This directory contains some resources loaded by fetchpriority tests.
 
-font_*em.woff2 files were generated using the `generate_fonts.py` Python script.
+* `generate_fonts.py` is used to generate `font_*.woff2` files.
+* `generate_media_files.py` is used to generate `audio_*.opus` and
+  `video_*.webm` files.
