cd67cc3619d6d9956256ff9f3f3bb6437a86dc84
serge-sans-paille <sguelton@mozilla.com>
2024-06-13 07:23:53 +0000

    Bug 1901986 - set MOZ_APP_VENDOR and MOZ_APP_ID value for iOS r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D213525

---
+++ mobile/ios/confvars.sh
@@ -0,0 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_VENDOR=Mozilla
+MOZ_APP_ID={8e3ba928-3798-4bcd-9b64-c788f2d954d3}
