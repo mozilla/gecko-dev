b159013d09cb49759f98a8578609e8ac6d7d98ce
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2024-06-13 13:07:10 +0000

    Bug 1901406 - JS Shell: os.file.listDir: Report UTF8 errors. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D213240

--- js/src/shell/OSObject.cpp
+++ js/src/shell/OSObject.cpp
@@ -459,7 +459,7 @@ static bool ListDir(JSContext* cx, unsigned argc, Value* vp,
 
   UniqueChars pathname = JS_EncodeStringToUTF8(cx, str);
   if (!pathname) {
-    JS_ReportErrorASCII(cx, "os.file.listDir cannot convert path to Latin1");
+    JS_ReportErrorASCII(cx, "os.file.listDir cannot convert path to UTF8");
     return false;
   }
 
@@ -479,8 +479,8 @@ static bool ListDir(JSContext* cx, unsigned argc, Value* vp,
   {
     DIR* dir = opendir(pathname.get());
     if (!dir) {
-      JS_ReportErrorASCII(cx, "os.file.listDir is unable to open: %s",
-                          pathname.get());
+      JS_ReportErrorUTF8(cx, "os.file.listDir is unable to open: %s",
+                         pathname.get());
       return false;
     }
     auto close = mozilla::MakeScopeExit([&] {
@@ -507,6 +507,11 @@ static bool ListDir(JSContext* cx, unsigned argc, Value* vp,
 
     WIN32_FIND_DATAA FindFileData;
     HANDLE hFind = FindFirstFileA(pattern.begin(), &FindFileData);
+    if (hFind == INVALID_HANDLE_VALUE) {
+      JS_ReportErrorUTF8(cx, "os.file.listDir is unable to open: %s",
+                         pathname.get());
+      return false;
+    }
     auto close = mozilla::MakeScopeExit([&] {
       if (!FindClose(hFind)) {
         MOZ_CRASH("Could not close Find");
