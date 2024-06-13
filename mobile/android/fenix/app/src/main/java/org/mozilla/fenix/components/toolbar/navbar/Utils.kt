1dbc5a02aca2f238e9aaa6fcb9aa4f203dd71811
mike a. <mavduevskiy@gmail.com>
2024-06-13 05:28:45 +0000

    Bug 1893526 – Refactor shouldAddNavigationBar into an extension function r=android-reviewers,petru
    The function is duplicated in Home and Browser fragments and being used to determine in the nav bar should be displayed. Moving it into extensions reduces code fragility, as well as allowing other components, not just fragments, to reliably check if the nav bar should be displayed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211583

---
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/toolbar/navbar/Utils.kt
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.components.toolbar.navbar
+
+import android.content.Context
+import mozilla.components.support.utils.ext.isLandscape
+import org.mozilla.fenix.components.toolbar.IncompleteRedesignToolbarFeature
+import org.mozilla.fenix.ext.isTablet
+import org.mozilla.fenix.ext.settings
+
+/**
+ * Returns true if navigation bar should be displayed. The returned value depends on the feature state, as well as the
+ * device type and orientation – we don't show the navigation bar for tablets and in landscape mode.
+ * NB: don't use it with the app context – it doesn't get recreated when a foldable changes its modes.
+ */
+fun shouldAddNavigationBar(context: Context) =
+    IncompleteRedesignToolbarFeature(context.settings()).isEnabled && !context.isLandscape() && !context.isTablet()
