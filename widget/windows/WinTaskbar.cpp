e8d2c5ea1c1582401ca37590053d4396d1974b2b
Mike Conley <mconley@mozilla.com>
2024-06-13 01:17:59 +0000

    Bug 1880109 - Remove legacy JumpListBuilder usage in WinTaskbar. r=rkraesig,win-reviewers
    Depends on D210543
    
    Differential Revision: https://phabricator.services.mozilla.com/D210544

--- widget/windows/WinTaskbar.cpp
+++ widget/windows/WinTaskbar.cpp
@@ -18,13 +18,11 @@
 #include <nsIBaseWindow.h>
 #include <nsServiceManagerUtils.h>
 #include "nsIXULAppInfo.h"
-#include "nsILegacyJumpListBuilder.h"
 #include "nsUXThemeData.h"
 #include "nsWindow.h"
 #include "WinUtils.h"
 #include "TaskbarTabPreview.h"
 #include "TaskbarWindowPreview.h"
-#include "LegacyJumpListBuilder.h"
 #include "nsWidgetsCID.h"
 #include "nsPIDOMWindow.h"
 #include "nsAppDirectoryServiceDefs.h"
@@ -36,9 +34,6 @@
 #include <propvarutil.h>
 #include <propkey.h>
 #include <shellapi.h>
-
-static NS_DEFINE_CID(kLegacyJumpListBuilderCID,
-                     NS_WIN_LEGACYJUMPLISTBUILDER_CID);
 
 namespace {
 
@@ -408,26 +403,6 @@ WinTaskbar::GetOverlayIconController(
   NS_ENSURE_SUCCESS(rv, rv);
 
   return CallQueryInterface(preview, _retval);
-}
-
-NS_IMETHODIMP
-WinTaskbar::CreateLegacyJumpListBuilder(
-    bool aPrivateBrowsing, nsILegacyJumpListBuilder** aJumpListBuilder) {
-  nsresult rv;
-
-  if (LegacyJumpListBuilder::sBuildingList) return NS_ERROR_ALREADY_INITIALIZED;
-
-  nsCOMPtr<nsILegacyJumpListBuilder> builder =
-      do_CreateInstance(kLegacyJumpListBuilderCID, &rv);
-  if (NS_FAILED(rv)) return NS_ERROR_UNEXPECTED;
-
-  NS_IF_ADDREF(*aJumpListBuilder = builder);
-
-  nsAutoString aumid;
-  GenerateAppUserModelID(aumid, aPrivateBrowsing);
-  builder->SetAppUserModelID(aumid);
-
-  return NS_OK;
 }
 
 NS_IMETHODIMP
