fa8546dc3a47b48d6dba9d3f6f091008589698a9
serge-sans-paille <sguelton@mozilla.com>
2024-06-13 09:32:38 +0000

    Bug 1898177 - Move MOZ_APP_VENDOR from confvars.sh to moz.configure r=glandium,geckoview-reviewers,m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D212338

--- mobile/android/confvars.sh
+++ mobile/android/confvars.sh
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_VENDOR=Mozilla
-
 MOZ_APP_UA_NAME=Firefox
 
 BROWSER_CHROME_URL=chrome://geckoview/content/geckoview.xhtml
