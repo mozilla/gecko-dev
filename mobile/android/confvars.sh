d31f750ad9e5b6c8f7b62d387dbea3f804044350
Tamas Szentpeteri <tszentpeteri@mozilla.com>
2024-06-14 01:56:03 +0300

    Backed out 2 changesets (bug 1898060, bug 1898177) for causing BR bustages related to MOZ_APP_ID. a=backout
    Backed out changeset d31b4c115176 (bug 1898177)
    Backed out changeset 8cda65a28dd1 (bug 1898060)

--- mobile/android/confvars.sh
+++ mobile/android/confvars.sh
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+MOZ_APP_VENDOR=Mozilla
+
 MOZ_APP_UA_NAME=Firefox
 
 BROWSER_CHROME_URL=chrome://geckoview/content/geckoview.xhtml
@@ -11,3 +13,5 @@ MOZ_OFFICIAL_BRANDING_DIRECTORY=mobile/android/branding/official
 # MOZ_APP_DISPLAYNAME is set by branding/configure.sh
 
 MOZ_RAW=1
+
+MOZ_APP_ID={aa3c5121-dab2-40e2-81ca-7ea25febc110}
