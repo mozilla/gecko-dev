060245685e8626374962c3d8a877a90875096544
Randell Jesup <rjesup@mozilla.com>
2024-06-14 14:25:11 +0000

    Bug 1891506: Update tests for net_CoalesceDirs r=valentin,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D207638

--- netwerk/test/gtest/TestStandardURL.cpp
+++ netwerk/test/gtest/TestStandardURL.cpp
@@ -449,6 +449,12 @@ TEST(TestStandardURL, CoalescePath)
     ASSERT_EQ(nsCString(buf.get()), nsCString(expected));
   };
 
+#ifndef DEBUG
+  // invalid input
+  testCoalescing("", "");
+#endif
+
+  testCoalescing("/", "/");
   testCoalescing("/.", "/");
   testCoalescing("/..", "/");
   testCoalescing("/foo/foo1/.", "/foo/foo1/");
