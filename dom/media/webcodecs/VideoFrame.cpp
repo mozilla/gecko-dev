00059c6284ff6fac19bf1930f986ad515d60b760
Chun-Min Chang <chun.m.chang@gmail.com>
2024-06-14 17:39:28 +0000

    Bug 1902157 - Update VideoFrame::Format::SampleBytes r=media-playback-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D213645

--- dom/media/webcodecs/VideoFrame.cpp
+++ dom/media/webcodecs/VideoFrame.cpp
@@ -2303,8 +2303,23 @@ uint32_t VideoFrame::Format::SampleBytes(const Plane& aPlane) const {
     case VideoPixelFormat::I420:
     case VideoPixelFormat::I420A:
     case VideoPixelFormat::I422:
+    case VideoPixelFormat::I422A:
     case VideoPixelFormat::I444:
+    case VideoPixelFormat::I444A:
       return 1;  // 8 bits/sample on the Y, U, V, A plane.
+    case VideoPixelFormat::I420P10:
+    case VideoPixelFormat::I420P12:
+    case VideoPixelFormat::I420AP10:
+    case VideoPixelFormat::I420AP12:
+    case VideoPixelFormat::I422P10:
+    case VideoPixelFormat::I422P12:
+    case VideoPixelFormat::I422AP10:
+    case VideoPixelFormat::I422AP12:
+    case VideoPixelFormat::I444P10:
+    case VideoPixelFormat::I444P12:
+    case VideoPixelFormat::I444AP10:
+    case VideoPixelFormat::I444AP12:
+      return 2;  // 10 or 12 bits/sample on the Y, U, V, A plane.
     case VideoPixelFormat::NV12:
       switch (aPlane) {
         case Plane::Y:
