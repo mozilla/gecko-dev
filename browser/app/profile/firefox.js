a26e972e97fbec860400d80df625193f4c88d64e
Cristian Tuns <ctuns@mozilla.com>
2024-06-16 04:55:47 -0400

    Backed out changeset 5d8fe1ab20de (bug 1900175) as requested by bobowen on element. CLOSED TREE

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1441,7 +1441,7 @@ pref("browser.bookmarks.editDialog.maxRecentFolders", 7);
   // See - security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
   // SetSecurityLevelForContentProcess() for what the different settings mean.
   #if defined(NIGHTLY_BUILD)
-    pref("security.sandbox.content.level", 8);
+    pref("security.sandbox.content.level", 7);
   #else
     pref("security.sandbox.content.level", 7);
   #endif
