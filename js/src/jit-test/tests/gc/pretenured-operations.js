8499ae7aca0ad29f07ed2f8289ed6d9d48b94e80
Jon Coppeard <jcoppeard@mozilla.com>
2024-06-17 08:39:34 +0000

    Bug 1902308 - Part 2: Add a test for the specific operations we pretenure r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D213587

---
+++ js/src/jit-test/tests/gc/pretenured-operations.js
@@ -0,0 +1,30 @@
+// Check individual operations that support pretenuring their result.
+
+load(libdir + "pretenure.js");
+
+let a;
+
+function run(op) {
+  a = [];
+  for (let i = 0; i < 1000; i++) {
+    a.push(op());
+  }
+  return a[0];
+}
+
+function check(op) {
+  assertEq(isNurseryAllocated(run(op)), true);
+  minorgc();
+  assertEq(isNurseryAllocated(run(op)), false);
+
+  a = undefined;
+  gc();
+}
+
+setupPretenureTest();
+
+// Object allocation.
+check(() => { return {}; });
+
+// Array allocation.
+check(() => { return []; });
