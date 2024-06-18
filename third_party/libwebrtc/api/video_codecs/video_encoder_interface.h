3dda90a8fcb51eb54d512a9fe6adfc92a0f54f87
Michael Froman <mfroman@mozilla.com>
2024-06-18 17:27:11 -0500

    Bug 1903098 - Vendor libwebrtc from b85b4c0f29
    Upstream commit: https://webrtc.googlesource.com/src/+/b85b4c0f29f15776d776ef75a46baf82ebbb9c7f
        Reland "New video encoder API."
    
        This reverts commit 56e63097498d0ad6fe2eca779b7df173cc6a0fca.
    
        Reason for revert: Preparing for reland
    
        Original change's description:
        > Revert "New video encoder API."
        >
        > This reverts commit 42f12d5183016060dcddc0b515a53294853559fe.
        >
        > Reason for revert: tests fails downstream
        >
        > Original change's description:
        > > New video encoder API.
        > >
        > > Also initial implementation wrapping the libaom AV1 encoder.
        > >
        > > Note that for now this is intended for prototype purposes.
        > >
        > > Bug: none
        > > Change-Id: Iac42ca4aecb6a204601c9f00bfb300e3eda3c4f4
        > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/306181
        > > Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        > > Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        > > Reviewed-by: Erik Språng <sprang@webrtc.org>
        > > Cr-Commit-Position: refs/heads/main@{#42108}
        >
        > Bug: none
        > Change-Id: I927260353afb91df6c7650364baee4f13a098efd
        > No-Presubmit: true
        > No-Tree-Checks: true
        > No-Try: true
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/347883
        > Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
        > Reviewed-by: Philip Eliasson <philipel@webrtc.org>
        > Owners-Override: Philip Eliasson <philipel@webrtc.org>
        > Auto-Submit: Danil Chapovalov <danilchap@webrtc.org>
        > Cr-Commit-Position: refs/heads/main@{#42111}
    
        Bug: none
        Change-Id: Ib72ef5359ead697d27301e2ca2408e8b27165931
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/349001
        Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#42172}

---
+++ third_party/libwebrtc/api/video_codecs/video_encoder_interface.h
@@ -0,0 +1,89 @@
+/*
+ *  Copyright (c) 2023 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_VIDEO_CODECS_VIDEO_ENCODER_INTERFACE_H_
+#define API_VIDEO_CODECS_VIDEO_ENCODER_INTERFACE_H_
+
+#include <map>
+#include <memory>
+// #include <unordered_set>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/functional/any_invocable.h"
+#include "absl/types/optional.h"
+#include "absl/types/variant.h"
+#include "api/units/data_rate.h"
+#include "api/units/time_delta.h"
+#include "api/units/timestamp.h"
+#include "api/video/encoded_image.h"
+#include "api/video/resolution.h"
+#include "api/video/video_frame.h"
+#include "api/video_codecs/video_codec.h"
+#include "api/video_codecs/video_encoding_general.h"
+#include "rtc_base/numerics/rational.h"
+
+namespace webrtc {
+// NOTE: This class is still under development and may change without notice.
+class VideoEncoderInterface {
+ public:
+  virtual ~VideoEncoderInterface() = default;
+  enum class FrameType { kKeyframe, kStartFrame, kDeltaFrame };
+
+  struct TemporalUnitSettings {
+    VideoCodecMode content_hint = VideoCodecMode::kRealtimeVideo;
+    Timestamp presentation_timestamp;
+    int effort_level = 0;
+  };
+
+  struct FrameEncodeSettings {
+    struct Cbr {
+      TimeDelta duration;
+      DataRate target_bitrate;
+    };
+
+    struct Cqp {
+      int target_qp;
+    };
+
+    absl::variant<Cqp, Cbr> rate_options;
+
+    FrameType frame_type = FrameType::kDeltaFrame;
+    int temporal_id = 0;
+    int spatial_id = 0;
+    Resolution resolution;
+    std::vector<int> reference_buffers;
+    absl::optional<int> update_buffer;
+  };
+
+  // Results from calling Encode. Called once for each configured frame.
+  struct EncodingError {};
+
+  struct EncodedData {
+    rtc::scoped_refptr<EncodedImageBufferInterface> bitstream_data;
+    FrameType frame_type;
+    int spatial_id;
+    int encoded_qp;
+    std::vector<int> referenced_buffers;
+  };
+
+  using EncodeResult = std::variant<EncodingError, EncodedData>;
+  using EncodeResultCallback =
+      absl::AnyInvocable<void(const EncodeResult& result)>;
+
+  virtual void Encode(rtc::scoped_refptr<webrtc::VideoFrameBuffer> frame_buffer,
+                      const TemporalUnitSettings& settings,
+                      const std::vector<FrameEncodeSettings>& frame_settings,
+                      EncodeResultCallback encode_result_callback) = 0;
+};
+
+}  // namespace webrtc
+#endif  // API_VIDEO_CODECS_VIDEO_ENCODER_INTERFACE_H_
