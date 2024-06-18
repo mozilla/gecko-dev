3f71f91c2bc71b53729303e8c61a5be75f862d3e
Steve Fink <sfink@mozilla.com>
2024-06-18 08:11:40 +0000

    Bug 1895951 - Switch IndexedDB to newer ArrayBuffer (or view) APIs. r=dom-storage-reviewers,sfink,jari
    Differential Revision: https://phabricator.services.mozilla.com/D211140

--- dom/indexedDB/Key.h
+++ dom/indexedDB/Key.h
@@ -19,6 +19,10 @@ struct ParamTraits;
 
 }  // namespace IPC
 
+namespace JS {
+class ArrayBufferOrView;
+}  // namespace JS
+
 namespace mozilla::dom::indexedDB {
 
 class Key {
@@ -226,6 +230,9 @@ class Key {
 
   Result<Ok, nsresult> EncodeNumber(double aFloat, uint8_t aType);
 
+  Result<Ok, nsresult> EncodeBinary(
+      const JS::ArrayBufferOrView& aArrayBufferOrView, uint8_t aTypeOffset);
+
   // Decoding functions. aPos points into mBuffer and is adjusted to point
   // past the consumed value. (Note: this may be beyond aEnd).
   static nsresult DecodeJSVal(const EncodedDataType*& aPos,
@@ -238,9 +245,8 @@ class Key {
   static double DecodeNumber(const EncodedDataType*& aPos,
                              const EncodedDataType* aEnd);
 
-  static JSObject* GetArrayBufferObjectFromDataRange(
-      const EncodedDataType*& aPos, const EncodedDataType* aEnd,
-      JSContext* aCx);
+  static JSObject* DecodeBinary(const EncodedDataType*& aPos,
+                                const EncodedDataType* aEnd, JSContext* aCx);
 
   // Returns the size of the decoded data for stringy (string or binary),
   // excluding a null terminator.
