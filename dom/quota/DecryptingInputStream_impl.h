1e766112efee01d0605e794e243173fcc8f50d67
Jan Varga <jvarga@mozilla.com>
2024-06-18 07:17:16 +0000

    Bug 1901525 - Remove extra Seek and ParseNextChunk from DecryptingInputStream::Seek; r=hsingh
    Now when we have proper boundary checks, the extra Seek and ParseNextChunk can
    be safely removed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D213068

--- dom/quota/DecryptingInputStream_impl.h
+++ dom/quota/DecryptingInputStream_impl.h
@@ -433,37 +433,6 @@ NS_IMETHODIMP DecryptingInputStream<CipherStrategy>::Seek(const int32_t aWhence,
     return rv;
   }
 
-  // We positioned after the last block, we must read that to know its size.
-  // XXX We could know earlier if we positioned us after the last block.
-  if (!readBytes) {
-    // XXX It seems that this check was added to prevent seeking before the
-    // start of the base stream during the Seek call below.
-    if (baseBlocksOffset == 0) {
-      // The stream is empty.
-      return NS_OK;
-    }
-
-    nsresult rv = (*mBaseSeekableStream)->Seek(NS_SEEK_CUR, -*mBlockSize);
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      return rv;
-    }
-
-    rv = ParseNextChunk(&readBytes);
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      return rv;
-    }
-
-    // XXX Investigate if we can actually remove the extra Seek and
-    // ParseNextChecck above given we don't use readBytes anymore. It seems
-    // this code was added in
-    // https://phabricator.services.mozilla.com/D74669?vs=276120&id=276956#toc
-    // and then there was another revision
-    // https://phabricator.services.mozilla.com/D74669?vs=293270&id=294180#toc
-    // which properly fixed Tell implementation and the Seek and ParseNextCunk
-    // were somehow forgotten to be removed here.
-    readBytes = 0;
-  }
-
   mPlainBytes = readBytes;
   mNextByte = nextByteOffset;
 
