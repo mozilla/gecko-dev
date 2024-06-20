d651d07b948ccd0d209d25bc2d133e02a695e502
Nikki Sharpley <nsharpley@mozilla.com>
2024-06-20 14:39:00 +0000

    Bug 1896987 - Fix browser_sidebar_context_menu.js intermittent r=sidebar-reviewers,kcochrane
    Differential Revision: https://phabricator.services.mozilla.com/D213807

--- browser/components/sidebar/tests/browser/browser_sidebar_context_menu.js
+++ browser/components/sidebar/tests/browser/browser_sidebar_context_menu.js
@@ -10,11 +10,8 @@ add_task(async function test_sidebar_extension_context_menu() {
   await waitForBrowserWindowActive(win);
   const { document } = win;
   const sidebar = document.querySelector("sidebar-main");
+  await sidebar.updateComplete;
   ok(BrowserTestUtils.isVisible(sidebar), "Sidebar is shown.");
-  await BrowserTestUtils.waitForCondition(
-    async () => await sidebar.updateComplete,
-    "The sidebar-main component has fully rendered."
-  );
 
   const manageStub = sinon.stub(sidebar, "manageExtension");
   const reportStub = sinon.stub(sidebar, "reportExtension");
