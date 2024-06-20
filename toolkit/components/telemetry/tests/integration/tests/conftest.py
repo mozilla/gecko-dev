41e85f9a9d5ec9ce21df410ebe9c27550adf8c17
Dave Townsend <dtownsend@oxymoronical.com>
2024-06-20 09:32:25 +0000

    Bug 1903572: Make SERVER_ROOT an absolute path. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D214269

--- toolkit/components/telemetry/tests/integration/tests/conftest.py
+++ toolkit/components/telemetry/tests/integration/tests/conftest.py
@@ -84,7 +84,9 @@ def fixture_marionette(binary, ping_server):
 @pytest.fixture(name="ping_server")
 def fixture_ping_server():
     """Run a ping server on localhost on a free port assigned by the OS"""
-    server = PingServer(SERVER_ROOT, "http://localhost:0")
+    server = PingServer(
+        os.path.join(build.topsrcdir, SERVER_ROOT), "http://localhost:0"
+    )
     server.start()
     yield server
     server.stop()
