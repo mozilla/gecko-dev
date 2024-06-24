cb35780bd4988f2f12571036295bc8dec10f1214
Karandeep <klubana@mozilla.com>
2024-06-24 14:49:11 +0000

    Bug 1902330 - Implement UrlbarSearchTermsPersistence component and add test cases. r=jteow,urlbar-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D213603

---
+++ browser/components/urlbar/tests/unit/test_urlbar_search_terms_persistence.js
@@ -0,0 +1,210 @@
+/*
+ * This test checks we can determine the type of search page and whether the
+ * page is a default search page.
+ */
+
+ChromeUtils.defineESModuleGetters(this, {
+  UrlbarSearchTermsPersistence:
+    "resource:///modules/UrlbarSearchTermsPersistence.sys.mjs",
+});
+
+const PROVIDERS = [
+  {
+    id: "example",
+    queryParamNames: ["q"],
+    searchPageRegexp: "^https://example\\.com/",
+    includeParams: [
+      {
+        key: "page",
+        values: ["web"],
+        canBeMissing: true,
+      },
+    ],
+    excludeParams: [
+      {
+        key: "excludeKey",
+        values: ["hello"],
+      },
+    ],
+  },
+  {
+    id: "example2",
+    queryParamNames: ["q"],
+    searchPageRegexp: "^https://example2\\.com/",
+    includeParams: [
+      {
+        key: "page",
+        values: ["web"],
+        canBeMissing: false,
+      },
+    ],
+    excludeParams: [
+      {
+        key: "excludeKey",
+      },
+    ],
+  },
+];
+
+const TESTS = [
+  {
+    title: "Tracked provider",
+    name: "example",
+    search_url: "https://example.com/",
+    search_url_get_params: "q={searchTerms}",
+    cases: [
+      {
+        title: "No search parameters",
+        currentURI: "https://example.com/",
+        originalURI: "https://example.com/",
+        expected: "",
+      },
+      {
+        title: "No search query param value",
+        currentURI: "https://example.com/?q",
+        originalURI: "https://example.com/?q",
+        expected: "",
+      },
+      {
+        title: "With search query param value",
+        currentURI: "https://example.com/?q=foo",
+        originalURI: "https://example.com/?q=foo",
+        expected: "foo",
+      },
+      {
+        title: "With search query param value and a fake value key-value pair",
+        currentURI: "https://example.com/?q=foo&page=fake_code",
+        originalURI: "https://example.com/?q=foo&page=fake_code",
+        expected: "",
+      },
+      {
+        title: "With search query param value and valid key-value pair",
+        currentURI: "https://example.com/?q=foo&page=web",
+        originalURI: "https://example.com/?q=foo&page=web",
+        expected: "foo",
+      },
+      {
+        title: "With search query param value and unknown key-value pair",
+        currentURI: "https://example.com/?q=foo&key=unknown",
+        originalURI: "https://example.com/?q=foo&key=unknown",
+        expected: "foo",
+      },
+      {
+        title:
+          "With search query param value and valid excludeParams key-value pair",
+        currentURI: "https://example.com/?q=foo&excludeKey=hello",
+        originalURI: "https://example.com/?q=foo&excludeKey=hello",
+        expected: "",
+      },
+      {
+        title:
+          "With search query param value and invalid excludeParams key-value pair",
+        currentURI: "https://example.com/?q=foo&page=web&excludeKey=hi",
+        originalURI: "https://example.com/?q=foo&page=web&excludeKey=hi",
+        expected: "foo",
+      },
+    ],
+  },
+  {
+    title: "Untracked provider",
+    name: "example1",
+    search_url: "https://example1.com/",
+    search_url_get_params: "q={searchTerms}&default=all",
+    cases: [
+      {
+        title: "With search query param",
+        currentURI: "https://example1.com/?q=foo",
+        originalURI: "https://example1.com/?q=foo",
+        expected: "",
+      },
+      {
+        title: "No search parameters",
+        currentURI: "https://example1.com/",
+        originalURI: "https://example1.com/",
+        expected: "",
+      },
+      {
+        title: "No search query param value",
+        currentURI: "https://example1.com/?q",
+        originalURI: "https://example1.com/?q",
+        expected: "",
+      },
+      {
+        title: "With search query param value and unknown key-value pair",
+        currentURI: "https://example1.com/?q=foo&key=unknown",
+        originalURI: "https://example1.com/?q=foo&key=unknown",
+        expected: "",
+      },
+      {
+        title: "with search query param value and default key-value pair",
+        currentURI: "https://example1.com/?q=foo&default=all",
+        originalURI: "https://example1.com/?q=foo&default=all",
+        expected: "foo",
+      },
+    ],
+  },
+  {
+    title: "Tracked provider with mandatory key-value pair",
+    name: "example2",
+    search_url: "https://example2.com/",
+    search_url_get_params: "q={searchTerms}&page=web",
+    cases: [
+      {
+        title: "With search query param value",
+        currentURI: "https://example2.com/?q=foo",
+        originalURI: "https://example2.com/?q=foo",
+        expected: "",
+      },
+      {
+        title: "With search query param value",
+        currentURI: "https://example2.com/?q=foo&page=web",
+        originalURI: "https://example2.com/?q=foo&page=web",
+        expected: "foo",
+      },
+      {
+        title: "With search query param value and excludeParams key-value pair",
+        currentURI: "https://example2.com/?q=foo&page=web&excludeKey=web",
+        originalURI: "https://example2.com/?q=foo&page=web&excludeKey=web",
+        expected: "",
+      },
+      {
+        title:
+          "With search query param value and presence of excludeParams key",
+        currentURI: "https://example2.com/?q=foo&excludeKey",
+        originalURI: "https://example2.com/?q=foo&excludeKey",
+        expected: "",
+      },
+    ],
+  },
+];
+
+add_setup(async function () {
+  await UrlbarSearchTermsPersistence.overrideSearchTermsPersistenceForTests(
+    PROVIDERS
+  );
+});
+
+add_task(async function test_parsing_extracted_urls() {
+  for (let test of TESTS) {
+    let { name, search_url, search_url_get_params } = test;
+    await SearchTestUtils.installSearchExtension(
+      {
+        name,
+        search_url,
+        search_url_get_params,
+      },
+      { setAsDefault: true }
+    );
+
+    for (let { title, currentURI, originalURI, expected } of test.cases) {
+      info(`${test.name} - ${title}`);
+      Assert.equal(
+        UrlbarSearchTermsPersistence.getSearchTermIfDefaultSerpUri(
+          Services.io.newURI(originalURI),
+          Services.io.newURI(currentURI)
+        ),
+        expected
+      );
+    }
+  }
+});
