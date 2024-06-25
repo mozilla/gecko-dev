153cfd316fc2472acaa4240ae9fd94474255a30a
Dimi <dlee@mozilla.com>
2024-06-25 09:53:52 +0000

    Bug 1898745 - P4. Remove active section and active field related code in formautofill r=credential-management-reviewers,NeilDeakin
    The new architecture no longer has the notion of an "active" section.
    When autocomplete is triggered, we pass the "element id" of the triggered element and
    use this id to indicate which form we are currently processing.
    
    Because of this change, we no longer need the code that deals with an active section, field, etc.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211553

--- toolkit/components/formautofill/shared/FormStateManager.sys.mjs
+++ toolkit/components/formautofill/shared/FormStateManager.sys.mjs
@@ -15,78 +15,8 @@ export class FormStateManager {
      * @type {WeakMap} mapping FormLike root HTML elements to FormAutofillHandler objects.
      */
     this._formsDetails = new WeakMap();
-    /**
-     * @type {object} The object where to store the active items, e.g. element,
-     * handler, section, and field detail.
-     */
-    this._activeItems = {};
 
     this.onFilledModifiedCallback = onFilledModifiedCallback;
-  }
-
-  /**
-   * Get the active input's information from cache which is created after page
-   * identified.
-   *
-   * @returns {object | null}
-   *          Return the active input's information that cloned from content cache
-   *          (or return null if the information is not found in the cache).
-   */
-  get activeFieldDetail() {
-    if (!this._activeItems.fieldDetail) {
-      let formDetails = this.activeFormDetails;
-      if (!formDetails) {
-        return null;
-      }
-      for (let detail of formDetails) {
-        let detailElement = detail.element;
-        if (detailElement && this.activeInput == detailElement) {
-          this._activeItems.fieldDetail = detail;
-          break;
-        }
-      }
-    }
-    return this._activeItems.fieldDetail;
-  }
-
-  /**
-   * Get the active form's information from cache which is created after page
-   * identified.
-   *
-   * @returns {Array<object> | null}
-   *          Return target form's information from content cache
-   *          (or return null if the information is not found in the cache).
-   *
-   */
-  get activeFormDetails() {
-    let formHandler = this.activeHandler;
-    return formHandler ? formHandler.fieldDetails : null;
-  }
-
-  get activeInput() {
-    return this._activeItems.elementWeakRef?.deref();
-  }
-
-  get activeHandler() {
-    const activeInput = this.activeInput;
-    if (!activeInput) {
-      return null;
-    }
-
-    // XXX: We are recomputing the activeHandler every time to avoid keeping a
-    // reference on the active element. This might be called quite frequently
-    // so if _getFormHandler/findRootForField become more costly, we should
-    // look into caching this result (eg by adding a weakmap).
-    let handler = this._getFormHandler(activeInput);
-    if (handler) {
-      handler.focusedInput = activeInput;
-    }
-    return handler;
-  }
-
-  get activeSection() {
-    let formHandler = this.activeHandler;
-    return formHandler ? formHandler.activeSection : null;
   }
 
   /**
@@ -132,35 +62,6 @@ export class FormStateManager {
 
     handler.collectFormFields();
     return { handler, newFieldsIdentified: true };
-  }
-
-  updateActiveInput(element) {
-    if (!element) {
-      this._activeItems = {};
-      return;
-    }
-    this._activeItems = {
-      elementWeakRef: new WeakRef(element),
-      fieldDetail: null,
-    };
-  }
-
-  getElementValues(formElement, handler) {
-    handler = handler || this._formsDetails.get(formElement);
-    const records = handler?.createRecords();
-
-    if (
-      !handler ||
-      !records ||
-      !Object.values(records).some(typeRecords => typeRecords.length)
-    ) {
-      return null;
-    }
-    return records;
-  }
-
-  didDestroy() {
-    this._activeItems = {};
   }
 }
 
