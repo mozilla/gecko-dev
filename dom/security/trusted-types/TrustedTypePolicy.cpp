897c70e12021265a6d20b396573795bdd3d59250
Mirko Brodesser <mbrodesser@igalia.com>
2024-06-25 08:43:14 +0000

    Bug 1903551: when creating a trusted type convert null to the empty string and activate all WPTs for TT. r=peterv
    Using the empty string implements the updated spec and avoids some
    crashes when activating the WPTs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D214266

--- dom/security/trusted-types/TrustedTypePolicy.cpp
+++ dom/security/trusted-types/TrustedTypePolicy.cpp
@@ -66,6 +66,10 @@ UniquePtr<T> TrustedTypePolicy::CreateTrustedType(
     return nullptr;
   }
 
+  if (policyValue.IsVoid()) {
+    policyValue = EmptyString();
+  }
+
   UniquePtr<T> trustedObject = MakeUnique<T>(std::move(policyValue));
 
   // TODO: add special handling for `TrustedScript` when default policy support
@@ -80,9 +84,7 @@ void TrustedTypePolicy::DetermineTrustedPolicyValue(
     const Sequence<JS::Value>& aArguments, bool aThrowIfMissing,
     ErrorResult& aErrorResult, nsAString& aResult) const {
   if (!aCallbackObject) {
-    // The spec lacks a definition for stringifying null, see
-    // <https://github.com/w3c/trusted-types/issues/469>.
-    aResult = EmptyString();
+    aResult.SetIsVoid(true);
 
     if (aThrowIfMissing) {
       aErrorResult.ThrowTypeError("Function missing.");
