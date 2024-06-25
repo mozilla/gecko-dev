cfe4c720d6d739787a4481df3252f9bff079ead5
Jon Coppeard <jcoppeard@mozilla.com>
2024-06-25 13:46:19 +0000

    Bug 1889355 - Add testcase r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D206661

---
+++ js/src/jit-test/tests/gc/bug-1889355.js
@@ -0,0 +1,15 @@
+let global = newGlobal({newCompartment: true});
+let original = new FakeDOMObject();
+original['bar'] = {};
+
+let {object, transplant} = transplantableObject({object: original});
+assertEq(object, original)
+
+enqueueMark(object);
+enqueueMark("yield");
+
+gczeal(0);
+gczeal(8);
+startgc(1);
+gcslice(1);
+transplant(global);
