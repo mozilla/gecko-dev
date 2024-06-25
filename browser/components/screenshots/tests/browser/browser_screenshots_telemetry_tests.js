2f48061aef8c8976b73749ee845e7b85751f5f2f
Niklas Baumgardner <nbaumgardner@mozilla.com>
2024-06-25 16:16:32 +0000

    Bug 1902303 - Focus browser before taking screenshot. r=sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D213673

--- browser/components/screenshots/tests/browser/browser_screenshots_telemetry_tests.js
+++ browser/components/screenshots/tests/browser/browser_screenshots_telemetry_tests.js
@@ -427,6 +427,8 @@ add_task(async function test_extra_telemetry() {
       let retryButton = helper.getDialogButton("retry");
       retryButton.click();
 
+      await helper.waitForOverlay();
+
       info("waiting for panel");
       panel = await helper.waitForPanel();
 
