98aab068e793ccfe78fd4815a08a6d1d43c7b970
Mike Hommey <mh+mozilla@glandium.org>
2024-06-25 19:52:14 +0000

    Bug 1904480 - Disable in-tree robustcheckout on linux workers. r=releng-reviewers,taskgraph-reviewers,jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D214790

--- taskcluster/gecko_taskgraph/transforms/job/run_task.py
+++ taskcluster/gecko_taskgraph/transforms/job/run_task.py
@@ -163,6 +163,7 @@ def generic_worker_run_task(config, job, taskdesc):
     worker = taskdesc["worker"] = job["worker"]
     is_win = worker["os"] == "windows"
     is_mac = worker["os"] == "macosx"
+    is_linux = worker["os"] == "linux"
     is_bitbar = worker["os"] == "linux-bitbar"
 
     if run["tooltool-downloads"]:
@@ -203,9 +204,9 @@ def generic_worker_run_task(config, job, taskdesc):
                 "file": "./fetch-content",
             }
         )
-    # Mac workers are still using a python2-based Mercurial not compatible with the
-    # in-tree robustcheckout. bug #1626357
-    if run.get("checkout") and not is_mac:
+    # Mac and Linux workers are still using a python2-based Mercurial not compatible
+    # with the in-tree robustcheckout. bug #1626357
+    if run.get("checkout") and not is_mac and not is_linux:
         worker["mounts"].append(
             {
                 "content": {
