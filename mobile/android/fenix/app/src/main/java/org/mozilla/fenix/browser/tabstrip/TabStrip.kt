fa7a127f84cd879bd5fa5014753ecf67b5b80a08
Butkovits Atila <abutkovits@mozilla.com>
2024-06-25 21:13:52 +0300

    Backed out changeset a31e53c97151 (bug 1903827) for causing lint failures. CLOSED TREE

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/tabstrip/TabStrip.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/tabstrip/TabStrip.kt
@@ -336,10 +336,7 @@ private fun TabItem(
                 Icon(
                     painter = painterResource(R.drawable.mozac_ic_cross_20),
                     tint = FirefoxTheme.colors.iconPrimary,
-                    contentDescription = stringResource(
-                        id = R.string.close_tab_title,
-                        state.title,
-                    ),
+                    contentDescription = stringResource(R.string.close_tab),
                 )
             }
         }
