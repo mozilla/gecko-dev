4ac7168eae4d81aaf6720d0a86736abc84bb8102
Cristian Tuns <ctuns@mozilla.com>
2024-06-26 11:43:21 -0400

    Backed out changeset e705ed34801d (bug 1885566) for causing ktlint-fenix failures CLOSED TREE

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/header/MenuHeader.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/header/MenuHeader.kt
@@ -17,7 +17,6 @@ import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
