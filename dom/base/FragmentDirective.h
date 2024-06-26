84ba155f2d4fdd1597e11828c040bd4efe77831b
Jan-Niklas Jaeschke <jjaschke@mozilla.com>
2024-06-26 14:24:33 +0000

    Bug 1888756, part 5 - Text Fragments: Implemented user activation. r=edgar,dom-core
    This commit implements the rules given in the spec [0]
    to determine if a text directive is allowed to be scrolled to.
    
    This includes checking and consuming the text directive user activation
    which was introduced in the previous parts of this patch set.
    
    Additionally, a number of checks given in [1] are implemented.
    The newly added method is injected into our scroll-to-fragment
    algorithms in `Document::ScrollToRef()` and `nsDocShell::ScrollToAnchor()`.
    
    [0] https://wicg.github.io/scroll-to-text-fragment/#restricting-the-text-fragment
    [1] https://wicg.github.io/scroll-to-text-fragment/#check-if-a-text-directive-can-be-scrolled
    
    Differential Revision: https://phabricator.services.mozilla.com/D212820

--- dom/base/FragmentDirective.h
+++ dom/base/FragmentDirective.h
@@ -116,6 +116,15 @@ class FragmentDirective final : public nsISupports, public nsWrapperCache {
       nsCString& aFragment, nsTArray<TextDirective>* aTextDirectives = nullptr,
       nsIURI* aURI = nullptr);
 
+  /** Performs various checks to determine if a text directive is allowed to be
+   * scrolled to.
+   *
+   * This follows the algorithm "check if a text directive can be scrolled" in
+   * section 3.5.4 of the text fragment spec
+   * (https://wicg.github.io/scroll-to-text-fragment/#restricting-the-text-fragment).
+   */
+  bool IsTextDirectiveAllowedToBeScrolledTo();
+
  private:
   RefPtr<nsRange> FindRangeForTextDirective(
       const TextDirective& aTextDirective);
