73aa7d3a467799b061d0222207606c13e97fa86c
James Teh <jteh@mozilla.com>
2024-06-26 02:03:36 +0000

    Bug 1901456 part 1: Add stub ITextRangeProvider implementation. r=nlapre
    Differential Revision: https://phabricator.services.mozilla.com/D214220

---
+++ accessible/windows/uia/UiaTextRange.cpp
@@ -0,0 +1,118 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "UiaTextRange.h"
+
+namespace mozilla::a11y {
+
+// UiaTextRange
+
+UiaTextRange::UiaTextRange(TextLeafRange& aRange) {}
+
+// IUnknown
+IMPL_IUNKNOWN1(UiaTextRange, ITextRangeProvider)
+
+// ITextRangeProvider methods
+
+STDMETHODIMP
+UiaTextRange::Clone(__RPC__deref_out_opt ITextRangeProvider** aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::Compare(__RPC__in_opt ITextRangeProvider* aRange,
+                      __RPC__out BOOL* aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::CompareEndpoints(enum TextPatternRangeEndpoint aEndpoint,
+                               __RPC__in_opt ITextRangeProvider* aTargetRange,
+                               enum TextPatternRangeEndpoint aTargetEndpoint,
+                               __RPC__out int* aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::ExpandToEnclosingUnit(enum TextUnit aUnit) { return E_NOTIMPL; }
+
+STDMETHODIMP
+UiaTextRange::FindAttribute(TEXTATTRIBUTEID aAttributeId, VARIANT aVal,
+                            BOOL aBackward,
+                            __RPC__deref_out_opt ITextRangeProvider** aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::FindText(__RPC__in BSTR aText, BOOL aBackward, BOOL aIgnoreCase,
+                       __RPC__deref_out_opt ITextRangeProvider** aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::GetAttributeValue(TEXTATTRIBUTEID aAttributeId,
+                                __RPC__out VARIANT* aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::GetBoundingRectangles(__RPC__deref_out_opt SAFEARRAY** aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::GetEnclosingElement(
+    __RPC__deref_out_opt IRawElementProviderSimple** aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::GetText(int aMaxLength, __RPC__deref_out_opt BSTR* aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::Move(enum TextUnit aUnit, int aCount, __RPC__out int* aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::MoveEndpointByUnit(enum TextPatternRangeEndpoint aEndpoint,
+                                 enum TextUnit aUnit, int aCount,
+                                 __RPC__out int* aRetVal) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::MoveEndpointByRange(
+    enum TextPatternRangeEndpoint aEndpoint,
+    __RPC__in_opt ITextRangeProvider* aTargetRange,
+    enum TextPatternRangeEndpoint aTargetEndpoint) {
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP
+UiaTextRange::Select() { return E_NOTIMPL; }
+
+STDMETHODIMP
+UiaTextRange::AddToSelection() { return E_NOTIMPL; }
+
+STDMETHODIMP
+UiaTextRange::RemoveFromSelection() { return E_NOTIMPL; }
+
+STDMETHODIMP
+UiaTextRange::ScrollIntoView(BOOL aAlignToTop) { return E_NOTIMPL; }
+
+STDMETHODIMP
+UiaTextRange::GetChildren(__RPC__deref_out_opt SAFEARRAY** aRetVal) {
+  if (!aRetVal) {
+    return E_INVALIDARG;
+  }
+  *aRetVal = nullptr;
+  return S_OK;
+}
+
+}  // namespace mozilla::a11y
---
+++ accessible/windows/uia/UiaTextRange.h
@@ -0,0 +1,104 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_a11y_UiaTextRange_h__
+#define mozilla_a11y_UiaTextRange_h__
+
+#include "IUnknownImpl.h"
+#include "mozilla/Assertions.h"
+#include "objbase.h"
+#include "uiautomation.h"
+
+namespace mozilla::a11y {
+class TextLeafRange;
+
+/**
+ * ITextRangeProvider implementation.
+ */
+class UiaTextRange : public ITextRangeProvider {
+ public:
+  explicit UiaTextRange(TextLeafRange& aRange);
+  virtual ~UiaTextRange() = default;
+
+  // IUnknown
+  DECL_IUNKNOWN
+
+  // ITextRangeProvider
+  virtual HRESULT STDMETHODCALLTYPE Clone(
+      /* [retval][out] */ __RPC__deref_out_opt ITextRangeProvider** aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE Compare(
+      /* [in] */ __RPC__in_opt ITextRangeProvider* aRange,
+      /* [retval][out] */ __RPC__out BOOL* aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE CompareEndpoints(
+      /* [in] */ enum TextPatternRangeEndpoint aEndpoint,
+      /* [in] */ __RPC__in_opt ITextRangeProvider* aTargetRange,
+      /* [in] */ enum TextPatternRangeEndpoint aTargetEndpoint,
+      /* [retval][out] */ __RPC__out int* aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE ExpandToEnclosingUnit(
+      /* [in] */ enum TextUnit aUnit);
+
+  virtual HRESULT STDMETHODCALLTYPE FindAttribute(
+      /* [in] */ TEXTATTRIBUTEID aAttributeId,
+      /* [in] */ VARIANT aVal,
+      /* [in] */ BOOL aBackward,
+      /* [retval][out] */ __RPC__deref_out_opt ITextRangeProvider** aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE FindText(
+      /* [in] */ __RPC__in BSTR aText,
+      /* [in] */ BOOL aBackward,
+      /* [in] */ BOOL aIgnoreCase,
+      /* [retval][out] */ __RPC__deref_out_opt ITextRangeProvider** aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE GetAttributeValue(
+      /* [in] */ TEXTATTRIBUTEID aAttributeId,
+      /* [retval][out] */ __RPC__out VARIANT* aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE GetBoundingRectangles(
+      /* [retval][out] */ __RPC__deref_out_opt SAFEARRAY** aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE GetEnclosingElement(
+      /* [retval][out] */ __RPC__deref_out_opt IRawElementProviderSimple**
+          aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE GetText(
+      /* [in] */ int aMaxLength,
+      /* [retval][out] */ __RPC__deref_out_opt BSTR* aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE Move(
+      /* [in] */ enum TextUnit aUnit,
+      /* [in] */ int aCount,
+      /* [retval][out] */ __RPC__out int* aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE MoveEndpointByUnit(
+      /* [in] */ enum TextPatternRangeEndpoint aEndpoint,
+      /* [in] */ enum TextUnit aUnit,
+      /* [in] */ int aCount,
+      /* [retval][out] */ __RPC__out int* aRetVal);
+
+  virtual HRESULT STDMETHODCALLTYPE MoveEndpointByRange(
+      /* [in] */ enum TextPatternRangeEndpoint aEndpoint,
+      /* [in] */ __RPC__in_opt ITextRangeProvider* aTargetRange,
+      /* [in] */ enum TextPatternRangeEndpoint aTargetEndpoint);
+
+  virtual HRESULT STDMETHODCALLTYPE Select(void);
+
+  virtual HRESULT STDMETHODCALLTYPE AddToSelection(void);
+
+  virtual HRESULT STDMETHODCALLTYPE RemoveFromSelection(void);
+
+  virtual HRESULT STDMETHODCALLTYPE ScrollIntoView(
+      /* [in] */ BOOL aAlignToTop);
+
+  virtual HRESULT STDMETHODCALLTYPE GetChildren(
+      /* [retval][out] */ __RPC__deref_out_opt SAFEARRAY** aRetVal);
+};
+
+}  // namespace mozilla::a11y
+
+#endif
