d21978e2ef06027b6e311202dd09990b5e2b0af3
edgul <edgul@mozilla.com>
2024-06-28 20:50:45 +0000

    Bug 1878391 - Added tests for chips cookie purging at partition capacity r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D203521

---
+++ netwerk/cookie/test/browser/browser_chips_partition_cap_child.js
@@ -0,0 +1,312 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const URL_EXAMPLE =
+  "https://example.com/browser/netwerk/cookie/test/browser/cookie-set-dom.html";
+
+registerCleanupFunction(() => {
+  Services.prefs.clearUserPref("network.cookie.cookieBehavior");
+  Services.prefs.clearUserPref(
+    "network.cookie.cookieBehavior.optInPartitioning"
+  );
+  Services.prefs.clearUserPref("network.cookie.chips.partitionLimitEnabled");
+  Services.prefs.clearUserPref(
+    "network.cookie.chips.partitionLimitByteCapacity"
+  );
+  Services.prefs.clearUserPref("network.cookie.CHIPS.enabled");
+  Services.prefs.clearUserPref("network.cookie.chips.partitionLimitDryRun");
+  Services.cookies.removeAll();
+});
+
+// enable chips and chips partition limit
+add_setup(async () => {
+  Services.prefs.setIntPref("network.cookie.cookieBehavior", 5);
+  Services.prefs.setBoolPref(
+    "network.cookie.cookieBehavior.optInPartitioning",
+    true
+  );
+  Services.prefs.setBoolPref("network.cookie.CHIPS.enabled", true);
+  Services.prefs.setBoolPref(
+    "network.cookie.chips.partitionLimitEnabled",
+    true
+  );
+  Services.prefs.setBoolPref(
+    "network.cookie.chips.partitionLimitDryRun",
+    false
+  );
+});
+
+const PATH = "/browser/netwerk/cookie/test/browser/";
+const PATH_EMPTY = PATH + "file_empty.html";
+
+const FIRST_PARTY = "example.com";
+const THIRD_PARTY = "example.org";
+
+const URL_DOCUMENT_FIRSTPARTY = "https://" + FIRST_PARTY + PATH_EMPTY;
+const URL_DOCUMENT_THIRDPARTY = "https://" + THIRD_PARTY + PATH_EMPTY;
+
+const COOKIE_PARTITIONED =
+  "cookie=partitioned; Partitioned; Secure; SameSite=None;";
+const COOKIE_UNPARTITIONED = "cookie=unpartitioned; Secure; SameSite=None;";
+
+function createOriginAttributes(partitionKey) {
+  return JSON.stringify({
+    firstPartyDomain: "",
+    geckoViewSessionContextId: "",
+    inIsolatedMozBrowser: false,
+    partitionKey,
+    privateBrowsingId: 0,
+    userContextId: 0,
+  });
+}
+
+function createPartitionKey(url) {
+  let uri = NetUtil.newURI(url);
+  return `(${uri.scheme},${uri.host})`;
+}
+
+function createSameSiteForeignPartitionKey(url) {
+  let uri = NetUtil.newURI(url);
+  return `(${uri.scheme},${uri.host},f)`;
+}
+
+// OriginAttributes used to access partitioned and unpartitioned cookie jars
+// in all tests.
+const partitionedOAs = createOriginAttributes(
+  createPartitionKey(URL_DOCUMENT_FIRSTPARTY)
+);
+const partitionedSameSiteForeignOAs = createOriginAttributes(
+  createSameSiteForeignPartitionKey(URL_DOCUMENT_FIRSTPARTY)
+);
+const unpartitionedOAs = createOriginAttributes("");
+
+// cookie bytes >= 4013
+var staticCount = 0;
+function uniqueLargeCookie(partitioned) {
+  ++staticCount;
+  var cookie = "largecookie"
+    .concat(staticCount)
+    .concat("=")
+    .concat("1234567890".repeat(400))
+    .concat("; Secure; SameSite=None;");
+  if (partitioned) {
+    return cookie.concat("; Partitioned");
+  }
+  return cookie;
+}
+
+async function setCookieViaDomOnChild(browser, cookie) {
+  await SpecialPowers.spawn(browser, [cookie], cookie => {
+    content.document.cookie = cookie;
+  });
+}
+
+// child-side document sets are byte-limited
+add_task(async function test_chips_limit_document_first_party_child() {
+  const tab = BrowserTestUtils.addTab(gBrowser, URL_DOCUMENT_FIRSTPARTY);
+  const browser = gBrowser.getBrowserForTab(tab);
+  await BrowserTestUtils.browserLoaded(browser);
+
+  // Set partitioned and unpartitioned cookie from document child-side
+  // third should fail to set (> 10K bytes)
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(true));
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(true));
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(true));
+
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(false));
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(false));
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(false));
+
+  // Get cookies from partitioned jar
+  let partitioned = Services.cookies.getCookiesWithOriginAttributes(
+    partitionedOAs,
+    FIRST_PARTY
+  );
+
+  // Get cookies from unpartitioned jar
+  let unpartitioned = Services.cookies.getCookiesWithOriginAttributes(
+    unpartitionedOAs,
+    FIRST_PARTY
+  );
+
+  // check that partitioned cookies are purged at maximum
+  // check that unpartitioned cookies are not
+  Assert.equal(partitioned.length, 2);
+  Assert.equal(unpartitioned.length, 3);
+
+  // Cleanup
+  BrowserTestUtils.removeTab(tab);
+  Services.cookies.removeAll();
+});
+
+// chips partitioned 3rd party cookies are subject to chips partition limit
+add_task(async function test_chips_limit_third_party() {
+  const tab = BrowserTestUtils.addTab(gBrowser, URL_DOCUMENT_FIRSTPARTY);
+  const browser = gBrowser.getBrowserForTab(tab);
+  await BrowserTestUtils.browserLoaded(browser);
+
+  // Spawn document bc
+  await SpecialPowers.spawn(browser, [URL_DOCUMENT_THIRDPARTY], async url => {
+    let ifr = content.document.createElement("iframe");
+    let loadPromise = ContentTaskUtils.waitForEvent(ifr, "load");
+    ifr.src = url;
+    content.document.body.appendChild(ifr);
+    await loadPromise;
+
+    // Spawn iframe bc
+    await SpecialPowers.spawn(await ifr.browsingContext, [], async () => {
+      function largeCookie(partitioned, index) {
+        var cookie = "largecookie"
+          .concat(index)
+          .concat("=")
+          .concat("1234567890".repeat(400))
+          .concat("; Secure; SameSite=None;");
+        if (partitioned) {
+          return cookie.concat("; Partitioned");
+        }
+        return cookie;
+      }
+      content.document.cookie = largeCookie(true, 0);
+      content.document.cookie = largeCookie(true, 1);
+      content.document.cookie = largeCookie(true, 2);
+      content.document.cookie = largeCookie(false, 0);
+      content.document.cookie = largeCookie(false, 1);
+      content.document.cookie = largeCookie(false, 2);
+    });
+  });
+
+  // Get cookies from partitioned jar
+  let partitioned = Services.cookies.getCookiesWithOriginAttributes(
+    partitionedOAs,
+    THIRD_PARTY
+  );
+  // Get cookies from unpartitioned jar
+  let unpartitioned = Services.cookies.getCookiesWithOriginAttributes(
+    unpartitionedOAs,
+    THIRD_PARTY
+  );
+
+  // third party partitioned cookies are subject to the byte limit
+  // while unpartitioned are blocked
+  Assert.equal(partitioned.length, 2);
+  Assert.equal(unpartitioned.length, 0);
+
+  // Cleanup
+  BrowserTestUtils.removeTab(tab);
+  Services.cookies.removeAll();
+});
+
+// same-site foreign partitioned cookies are subject to the limit
+// This is ABA scenario: A contains B iframe, which contains A iframe
+add_task(async function test_chips_limit_samesite_foreign() {
+  const tab = BrowserTestUtils.addTab(gBrowser, URL_DOCUMENT_FIRSTPARTY);
+  const browser = gBrowser.getBrowserForTab(tab);
+  await BrowserTestUtils.browserLoaded(browser);
+
+  // content process (top-level)
+  await SpecialPowers.spawn(
+    browser,
+    [URL_DOCUMENT_THIRDPARTY, URL_DOCUMENT_FIRSTPARTY],
+    async (url, urlInner) => {
+      let ifr = content.document.createElement("iframe");
+      let loadPromise = ContentTaskUtils.waitForEvent(ifr, "load");
+      ifr.src = url;
+      content.document.body.appendChild(ifr);
+      await loadPromise;
+
+      // Spawn iframe bc (third-party to top-level)
+      await SpecialPowers.spawn(
+        await ifr.browsingContext,
+        [urlInner],
+        async urlInner => {
+          let ifr = content.document.createElement("iframe");
+          let loadPromise = ContentTaskUtils.waitForEvent(ifr, "load");
+          ifr.src = urlInner;
+          content.document.body.appendChild(ifr);
+          await loadPromise;
+
+          // spawn inner iframe (same-site as top-level)
+          await SpecialPowers.spawn(await ifr.browsingContext, [], async () => {
+            function largeCookie(partitioned, index) {
+              var cookie = "largecookie"
+                .concat(index)
+                .concat("=")
+                .concat("1234567890".repeat(400))
+                .concat("; Secure; SameSite=None;");
+              if (partitioned) {
+                return cookie.concat("; Partitioned");
+              }
+              return cookie;
+            }
+            content.document.cookie = largeCookie(true, 0);
+            content.document.cookie = largeCookie(true, 1);
+            content.document.cookie = largeCookie(true, 2);
+            content.document.cookie = largeCookie(false, 0);
+            content.document.cookie = largeCookie(false, 1);
+            content.document.cookie = largeCookie(false, 2);
+          });
+        }
+      );
+    }
+  );
+
+  // Get cookies from partitioned jar
+  let partitioned = Services.cookies.getCookiesWithOriginAttributes(
+    partitionedSameSiteForeignOAs,
+    FIRST_PARTY
+  );
+  // Get cookies from unpartitioned jar
+  let unpartitioned = Services.cookies.getCookiesWithOriginAttributes(
+    unpartitionedOAs, // no such thing as unpartitioned with foreign bit
+    FIRST_PARTY
+  );
+
+  // same-site foreign partitioned cookies are subject to the byte limit
+  // unpartitioned are blocked
+  Assert.equal(partitioned.length, 2);
+  Assert.equal(unpartitioned.length, 0);
+
+  // Cleanup
+  BrowserTestUtils.removeTab(tab);
+  Services.cookies.removeAll();
+});
+
+// reducing the byte limit pref affects the amount of cookie bytes we trigger at
+add_task(async function test_chips_limit_change_byte_limit() {
+  Services.prefs.setIntPref(
+    "network.cookie.chips.partitionLimitByteCapacity",
+    5120 // half the default
+  );
+  const tab = BrowserTestUtils.addTab(gBrowser, URL_DOCUMENT_FIRSTPARTY);
+  const browser = gBrowser.getBrowserForTab(tab);
+  await BrowserTestUtils.browserLoaded(browser);
+
+  // two cookies of this size will exceed the new limit
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(true));
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(true));
+  // unpartitioned will be unaffected
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(false));
+  await setCookieViaDomOnChild(browser, uniqueLargeCookie(false));
+
+  // Get cookies from partitioned jar
+  let partitioned = Services.cookies.getCookiesWithOriginAttributes(
+    partitionedOAs,
+    FIRST_PARTY
+  );
+
+  // Get cookies from unpartitioned jar
+  let unpartitioned = Services.cookies.getCookiesWithOriginAttributes(
+    unpartitionedOAs,
+    FIRST_PARTY
+  );
+
+  // check that partitioned cookies are purged at maximum
+  // check that unpartitioned cookies are not
+  Assert.equal(partitioned.length, 1);
+  Assert.equal(unpartitioned.length, 2);
+
+  // Cleanup
+  BrowserTestUtils.removeTab(tab);
+  Services.cookies.removeAll();
+});
---
+++ netwerk/cookie/test/unit/test_chips_partition_capping.js
@@ -0,0 +1,397 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { NetUtil } = ChromeUtils.importESModule(
+  "resource://gre/modules/NetUtil.sys.mjs"
+);
+
+registerCleanupFunction(() => {
+  Services.prefs.clearUserPref("network.cookie.cookieBehavior");
+  Services.prefs.clearUserPref(
+    "network.cookieJarSettings.unblocked_for_testing"
+  );
+  Services.prefs.clearUserPref("network.cookie.CHIPS.enabled");
+  Services.prefs.clearUserPref("network.cookie.chips.partitionLimitEnabled");
+  Services.prefs.clearUserPref(
+    "network.cookie.chips.partitionLimitByteCapacity"
+  );
+  Services.prefs.clearUserPref("network.cookie.chips.partitionLimitDryRun");
+
+  Services.cookies.removeAll();
+});
+
+// enable chips and chips partition limit
+add_setup(async () => {
+  Services.prefs.setIntPref("network.cookie.cookieBehavior", 5);
+  Services.prefs.setBoolPref(
+    "network.cookieJarSettings.unblocked_for_testing",
+    true
+  );
+  Services.prefs.setBoolPref("network.cookie.CHIPS.enabled", true);
+  Services.prefs.setBoolPref(
+    "network.cookie.chips.partitionLimitEnabled",
+    true
+  );
+  Services.prefs.setBoolPref(
+    "network.cookie.chips.partitionLimitDryRun",
+    false
+  );
+
+  // FOG needs a profile directory to put its data in.
+  do_get_profile();
+  // FOG needs to be initialized in order for data to flow.
+  Services.fog.initializeFOG();
+});
+
+function headerify(cookie, index, partitioned) {
+  let maxAge = 9000 + index; // use index so cookies can be ordered by age
+  // there is no way to test insecure purging first with a partitioned cookie without `Secure`
+  // these cookies would be immediately rejected
+  let mostHeaders = `; Max-Age=${maxAge}; SameSite=None; Secure`;
+  let temp = cookie.concat(mostHeaders);
+  if (partitioned) {
+    temp = temp.concat("; Partitioned");
+  }
+  return temp;
+}
+
+async function checkReportedOverflow(expected) {
+  let reported =
+    await Glean.networking.cookieChipsPartitionLimitOverflow.testGetValue();
+  if (expected == 0) {
+    Assert.equal(reported, null);
+    return;
+  }
+  // verify the telemetry by summing, but the individual reports are not aggregated
+  Assert.equal(reported.sum, expected);
+}
+
+function channelMaybePartitioned(uri, partition) {
+  let channel = NetUtil.newChannel({
+    uri,
+    contentPolicyType: Ci.nsIContentPolicy.TYPE_DOCUMENT,
+    securityFlags: Ci.nsILoadInfo.SEC_ALLOW_CROSS_ORIGIN_INHERITS_SEC_CONTEXT,
+    loadingPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
+  });
+  if (partition) {
+    let cookieJarSettings = Cc[
+      "@mozilla.org/cookieJarSettings;1"
+    ].createInstance(Ci.nsICookieJarSettings);
+    cookieJarSettings.initWithURI(uri, false);
+    channel.loadInfo.cookieJarSettings = cookieJarSettings;
+  }
+  return channel;
+}
+
+const COOKIES_TO_SET_COUNT = 102;
+
+// set 102 cookies at 100 Bytes each -> 10200B
+// partition maximum is 10KiB or 10240B (only for partitioned cookies)
+// so after this function is called any partitioned cookie > 40Bytes will exceed
+function setManyCookies(uri, channel, partitioned) {
+  let cookieString = "";
+  let cookieNames = [];
+  for (let i = 0; i < COOKIES_TO_SET_COUNT; i++) {
+    // each cookie will be 100 Bytes
+    let name = "c" + i.toString();
+    let value = i + "_".repeat(100 - i.toString().length - name.length);
+    let cookie = name + "=" + value;
+    cookieNames.push(name);
+
+    Services.cookies.setCookieStringFromHttp(
+      uri,
+      headerify(cookie, i, partitioned),
+      channel
+    );
+
+    // prep the expected value
+    cookieString += cookie;
+    if (i < COOKIES_TO_SET_COUNT - 1) {
+      cookieString += "; ";
+    }
+  }
+
+  return { cookieString, cookieNames };
+}
+
+// unpartitioned cookies should not be purged
+add_task(async function test_chips_limit_parent_http_unpartitioned() {
+  let baseDomain = "example.org";
+  let uri = NetUtil.newURI("https://" + baseDomain + "/");
+  let channel = channelMaybePartitioned(uri, baseDomain, false);
+
+  let expected = setManyCookies(uri, channel, false);
+  expected.cookieNames.push("exceeded");
+
+  // pre-condition: check that all got added as expected
+  let actual = Services.cookies.getCookieStringFromHttp(uri, channel);
+  Assert.equal(actual, expected.cookieString);
+  await checkReportedOverflow(0);
+
+  // unpartitioned cookies, no limit here
+  let cookie = "exceeded".concat("=").concat("x".repeat(240));
+  Services.cookies.setCookieStringFromHttp(
+    uri,
+    headerify(cookie, COOKIES_TO_SET_COUNT, false), // use count for uniqueness
+    channel
+  );
+
+  await checkReportedOverflow(0);
+
+  // extract cookie names from string and compare to expected values
+  let second = Services.cookies.getCookieStringFromHttp(uri, channel);
+  let cookies = second.split("; ");
+  for (let i = 0; i < cookies.length; i++) {
+    cookies[i] = cookies[i].substr(0, cookies[i].indexOf("="));
+  }
+  Assert.deepEqual(cookies, expected.cookieNames);
+  Assert.equal(cookies.length, expected.cookieNames.length);
+  Services.cookies.removeAll();
+  Services.fog.testResetFOG();
+});
+
+// parent http partition cookies exceeding capacity should purge in FIFO manner
+add_task(async function test_chips_limit_parent_http_partitioned() {
+  let baseDomain = "example.org";
+  let uri = NetUtil.newURI("https://" + baseDomain + "/");
+  let channel = channelMaybePartitioned(uri, baseDomain, true);
+
+  let expected = setManyCookies(uri, channel, true);
+  expected.cookieNames.push("exceeded");
+  expected.cookieNames.shift(); // remove first three elements
+  expected.cookieNames.shift();
+  expected.cookieNames.shift();
+
+  // pre-condition: check that all got added as expected
+  let actual = Services.cookies.getCookieStringFromHttp(uri, channel);
+  Assert.equal(actual, expected.cookieString);
+  await checkReportedOverflow(0);
+
+  // adding 248 Bytes has excess of 208Bytes (3 cookies will be purged (FIFO))
+  let cookie = "exceeded".concat("=").concat("x".repeat(240));
+  Services.cookies.setCookieStringFromHttp(
+    uri,
+    headerify(cookie, COOKIES_TO_SET_COUNT, true), // use count for uniqueness
+    channel
+  );
+
+  let expectedOverflow = 208;
+  await checkReportedOverflow(expectedOverflow);
+
+  // extract cookie names from string and compare to expected values
+  let second = Services.cookies.getCookieStringFromHttp(uri, channel);
+  let cookies = second.split("; ");
+  for (let i = 0; i < cookies.length; i++) {
+    cookies[i] = cookies[i].substr(0, cookies[i].indexOf("="));
+  }
+  Assert.deepEqual(cookies, expected.cookieNames);
+  Assert.equal(cookies.length, expected.cookieNames.length);
+  Services.cookies.removeAll();
+  Services.fog.testResetFOG();
+});
+
+// partition limit should still work for cookie overwrites
+add_task(async function test_chips_limit_overwrites_can_purge() {
+  let baseDomain = "example.org";
+  let uri = NetUtil.newURI("https://" + baseDomain + "/");
+  let channel = channelMaybePartitioned(uri, baseDomain, true);
+
+  let expected = setManyCookies(uri, channel, true);
+  expected.cookieNames.shift(); // remove first two elements
+  expected.cookieNames.shift();
+
+  // pre-condition: check that all got added as expected
+  let actual = Services.cookies.getCookieStringFromHttp(uri, channel);
+  Assert.equal(actual, expected.cookieString);
+  await checkReportedOverflow(0);
+
+  // cookie which already exists also triggers purge
+  // 244 (new cookie) - 100 (existing cookie) -> 144 newly added bytes
+  // So we are in excess by 104 bytes, means 2 cookies need purging (FIFO)
+  let cookie = "c101".concat("=").concat("x".repeat(240)); // 244
+  Services.cookies.setCookieStringFromHttp(
+    uri,
+    headerify(cookie, COOKIES_TO_SET_COUNT, true), // use count for uniqueness
+    channel
+  );
+
+  let expectedOverflow = 104;
+  await checkReportedOverflow(expectedOverflow);
+
+  // extract cookie names from string and compare to expected values
+  let second = Services.cookies.getCookieStringFromHttp(uri, channel);
+  let cookies = second.split("; ");
+  for (let i = 0; i < cookies.length; i++) {
+    cookies[i] = cookies[i].substr(0, cookies[i].indexOf("="));
+  }
+  Assert.deepEqual(cookies, expected.cookieNames);
+  Assert.equal(cookies.length, expected.cookieNames.length);
+  Services.cookies.removeAll();
+  Services.fog.testResetFOG();
+});
+
+// dry run mode should not purge, but still report excess via telemetry
+add_task(async function test_chips_limit_dry_run_no_purge() {
+  Services.prefs.setBoolPref("network.cookie.chips.partitionLimitDryRun", true);
+
+  let baseDomain = "example.org";
+  let uri = NetUtil.newURI("https://" + baseDomain + "/");
+  let channel = channelMaybePartitioned(uri, baseDomain, true);
+  let expected = setManyCookies(uri, channel, true);
+  expected.cookieNames.push("exceeded");
+
+  // pre-condition: check that all got added as expected
+  let actual = Services.cookies.getCookieStringFromHttp(uri, channel);
+  Assert.equal(actual, expected.cookieString);
+  await checkReportedOverflow(0);
+
+  // adding 248 Bytes has excess of 208Bytes (3 cookies will be purged (FIFO))
+  let cookie = "exceeded".concat("=").concat("x".repeat(240));
+  Services.cookies.setCookieStringFromHttp(
+    uri,
+    headerify(cookie, COOKIES_TO_SET_COUNT, true), // use count for uniqueness
+    channel
+  );
+  expected.cookieString += "; ".concat(cookie);
+
+  let expectedOverflow = 208;
+  await checkReportedOverflow(expectedOverflow);
+
+  // extract cookie names from string and compare to expected values
+  let second = Services.cookies.getCookieStringFromHttp(uri, channel);
+  let cookies = second.split("; ");
+  for (let i = 0; i < cookies.length; i++) {
+    cookies[i] = cookies[i].substr(0, cookies[i].indexOf("="));
+  }
+  Assert.deepEqual(cookies, expected.cookieNames);
+  Assert.equal(cookies.length, expected.cookieNames.length);
+  Assert.equal(second, expected.cookieString);
+  Services.cookies.removeAll();
+  Services.fog.testResetFOG();
+});
+
+add_task(async function test_chips_limit_chips_off() {
+  Services.prefs.setBoolPref(
+    "network.cookie.chips.partitionLimitDryRun",
+    false
+  );
+  Services.prefs.setBoolPref("network.cookie.CHIPS.enabled", false);
+
+  let baseDomain = "example.org";
+  let uri = NetUtil.newURI("https://" + baseDomain + "/");
+  let channel = channelMaybePartitioned(uri, baseDomain, true);
+  let expected = setManyCookies(uri, channel, true);
+  expected.cookieNames.push("exceeded");
+
+  // pre-condition: check that all got added as expected
+  let actual = Services.cookies.getCookieStringFromHttp(uri, channel);
+  Assert.equal(actual, expected.cookieString);
+  await checkReportedOverflow(0);
+
+  // shouldn't trigger purge when CHIPS disabled
+  let cookie = "exceeded".concat("=").concat("x".repeat(240));
+  Services.cookies.setCookieStringFromHttp(
+    uri,
+    headerify(cookie, COOKIES_TO_SET_COUNT, true), // use count for uniqueness
+    channel
+  );
+  expected.cookieString += "; ".concat(cookie);
+
+  await checkReportedOverflow(0);
+
+  // extract cookie names from string and compare to expected values
+  let second = Services.cookies.getCookieStringFromHttp(uri, channel);
+  let cookies = second.split("; ");
+  for (let i = 0; i < cookies.length; i++) {
+    cookies[i] = cookies[i].substr(0, cookies[i].indexOf("="));
+  }
+  Assert.deepEqual(cookies, expected.cookieNames);
+  Assert.equal(cookies.length, expected.cookieNames.length);
+  Assert.equal(second, expected.cookieString);
+  Services.cookies.removeAll();
+  Services.fog.testResetFOG();
+});
+
+add_task(async function test_chips_limit_chips_limit_off() {
+  Services.prefs.setBoolPref(
+    "network.cookie.chips.partitionLimitDryRun",
+    false
+  );
+  Services.prefs.setBoolPref("network.cookie.CHIPS.enabled", true);
+
+  Services.prefs.setBoolPref(
+    "network.cookie.chips.partitionLimitEnabled",
+    false
+  );
+
+  let baseDomain = "example.org";
+  let uri = NetUtil.newURI("https://" + baseDomain + "/");
+  let channel = channelMaybePartitioned(uri, baseDomain, true);
+  let expected = setManyCookies(uri, channel, true);
+  expected.cookieNames.push("exceeded");
+
+  // pre-condition: check that all got added as expected
+  let actual = Services.cookies.getCookieStringFromHttp(uri, channel);
+  Assert.equal(actual, expected.cookieString);
+  await checkReportedOverflow(0);
+
+  // shouldn't trigger purge when CHIPS limit disabled
+  let cookie = "exceeded".concat("=").concat("x".repeat(240));
+  Services.cookies.setCookieStringFromHttp(
+    uri,
+    headerify(cookie, COOKIES_TO_SET_COUNT, true), // use count for uniqueness
+    channel
+  );
+  expected.cookieString += "; ".concat(cookie);
+
+  await checkReportedOverflow(0);
+
+  // extract cookie names from string and compare to expected values
+  let second = Services.cookies.getCookieStringFromHttp(uri, channel);
+  let cookies = second.split("; ");
+  for (let i = 0; i < cookies.length; i++) {
+    cookies[i] = cookies[i].substr(0, cookies[i].indexOf("="));
+  }
+  Assert.deepEqual(cookies, expected.cookieNames);
+  Assert.equal(cookies.length, expected.cookieNames.length);
+  Assert.equal(second, expected.cookieString);
+  Services.cookies.removeAll();
+  Services.fog.testResetFOG();
+});
+
+// non-chips-partitioned cookies do not trigger the limit
+add_task(async function test_chips_limit_non_chips_partitioned() {
+  // channel is partitioned, not cookie header -> non-chips-partition cookies
+  let baseDomain = "example.org";
+  let uri = NetUtil.newURI("https://" + baseDomain + "/");
+  let channel = channelMaybePartitioned(uri, baseDomain, true);
+  let expected = setManyCookies(uri, channel, false);
+  expected.cookieNames.push("exceeded");
+
+  // pre-condition: check that all got added as expected
+  let actual = Services.cookies.getCookieStringFromHttp(uri, channel);
+  Assert.equal(actual, expected.cookieString);
+  await checkReportedOverflow(0);
+
+  // non-chips-partitioned cookies, should also have no limit
+  let cookie = "exceeded".concat("=").concat("x".repeat(240));
+  Services.cookies.setCookieStringFromHttp(
+    uri,
+    headerify(cookie, COOKIES_TO_SET_COUNT, false), // use count for uniqueness
+    channel
+  );
+
+  await checkReportedOverflow(0);
+
+  // extract cookie names from string and compare to expected values
+  let second = Services.cookies.getCookieStringFromHttp(uri, channel);
+  let cookies = second.split("; ");
+  for (let i = 0; i < cookies.length; i++) {
+    cookies[i] = cookies[i].substr(0, cookies[i].indexOf("="));
+  }
+  Assert.deepEqual(cookies, expected.cookieNames);
+  Assert.equal(cookies.length, expected.cookieNames.length);
+  Services.cookies.removeAll();
+  Services.fog.testResetFOG();
+});
