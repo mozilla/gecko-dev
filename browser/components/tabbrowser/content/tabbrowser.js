ecd9936d5568095db4541b87cc3df62fd3d77950
Anna Yeddi <ayeddi@mozilla.com>
2024-06-28 17:19:31 +0000

    Bug 1884970 - Close current tab button is missing an accessible name and role. r=tabbrowser-reviewers,fluent-reviewers,dao,bolsson,flod
    The [tab-close-button](https://searchfox.org/mozilla-central/rev/f9157a03835653cd3ece8d2dc713a782b7e4374e/browser/base/content/tabbrowser-tab.js#40) is not labeled and is missing an interactive role of button, while it is functioning as one.
    
    Note: we do not want this control to be keyboard focusable, because keyboard-only user could close the tab via the context menu and we don't want to create an additional tab stop for the navigation as well, but making sure the control is marked up as a button with an accessible name would allow it to be actionable with speech-to-text software, with touch devices, with switch controls in scan mode, and for screen readers via their navigation shortcuts as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D204413

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -5056,6 +5056,9 @@
 
       this.tabContainer._updateCloseButtons();
       this.tabContainer._updateHiddenTabsStatus();
+      if (aTab.multiselected) {
+        this._updateMultiselectedTabCloseButtonTooltip();
+      }
 
       let event = document.createEvent("Events");
       event.initEvent("TabShow", true, false);
@@ -5079,6 +5082,9 @@
 
       this.tabContainer._updateCloseButtons();
       this.tabContainer._updateHiddenTabsStatus();
+      if (aTab.multiselected) {
+        this._updateMultiselectedTabCloseButtonTooltip();
+      }
 
       // Splice this tab out of any lines of succession before any events are
       // dispatched.
@@ -5412,6 +5418,19 @@
       );
     },
 
+    /**
+     * Update accessible names of close buttons in the (multi) selected tabs
+     * collection with how many tabs they will close
+     */
+    _updateMultiselectedTabCloseButtonTooltip() {
+      const tabCount = gBrowser.selectedTabs.length;
+      gBrowser.selectedTabs.forEach(selectedTab => {
+        document.l10n.setArgs(selectedTab.querySelector(".tab-close-button"), {
+          tabCount,
+        });
+      });
+    },
+
     addToMultiSelectedTabs(aTab) {
       if (aTab.multiselected) {
         return;
@@ -5426,6 +5445,8 @@
       } else {
         this._multiSelectChangeAdditions.add(aTab);
       }
+
+      this._updateMultiselectedTabCloseButtonTooltip();
     },
 
     /**
@@ -5448,6 +5469,8 @@
       for (let i = lowerIndex; i <= higherIndex; i++) {
         this.addToMultiSelectedTabs(tabs[i]);
       }
+
+      this._updateMultiselectedTabCloseButtonTooltip();
     },
 
     removeFromMultiSelectedTabs(aTab) {
@@ -5463,6 +5486,13 @@
       } else {
         this._multiSelectChangeRemovals.add(aTab);
       }
+      // Update labels for Close buttons of the remaining multiselected tabs:
+      this._updateMultiselectedTabCloseButtonTooltip();
+      // Update the label for the Close button of the tab being removed
+      // from the multiselection:
+      document.l10n.setArgs(aTab.querySelector(".tab-close-button"), {
+        tabCount: 1,
+      });
     },
 
     clearMultiSelectedTabs() {
@@ -5973,12 +6003,7 @@
       const tabCount = this.selectedTabs.includes(tab)
         ? this.selectedTabs.length
         : 1;
-      if (tab.mOverCloseButton) {
-        tooltip.label = "";
-        document.l10n.setAttributes(tooltip, "tabbrowser-close-tabs-tooltip", {
-          tabCount,
-        });
-      } else if (tab._overPlayingIcon) {
+      if (tab._overPlayingIcon) {
         let l10nId;
         const l10nArgs = { tabCount };
         if (tab.selected) {
