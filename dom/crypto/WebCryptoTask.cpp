c681d22e0b3ba075178716fa92d8c65e5a7c75a3
Anna Weine <anna.weine@mozilla.com>
2024-07-01 08:58:14 +0000

    Bug 1905120 - WebCrypto: EC (ECDH mostly) should not check the alg field in jwk r=keeler
    Differential Revision: https://phabricator.services.mozilla.com/D215130

--- dom/crypto/WebCryptoTask.cpp
+++ dom/crypto/WebCryptoTask.cpp
@@ -1387,7 +1387,8 @@ class ImportKeyTask : public WebCryptoTask {
 
   static bool JwkCompatible(const JsonWebKey& aJwk, const CryptoKey* aKey) {
     // Check 'alg'
-    if (!aJwk.mKty.EqualsLiteral(JWK_TYPE_OKP) && aJwk.mAlg.WasPassed() &&
+    if (!aJwk.mKty.EqualsLiteral(JWK_TYPE_OKP) &&
+        !aJwk.mKty.EqualsLiteral(JWK_TYPE_EC) && aJwk.mAlg.WasPassed() &&
         aJwk.mAlg.Value() != aKey->Algorithm().JwkAlg()) {
       return false;
     }
