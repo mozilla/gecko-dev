9cefe2edadec05c456f493d03b1bdcd0df0606c4
Stanca Serban <sstanca@mozilla.com>
2024-07-03 00:05:37 +0300

    Backed out changeset 94a2d660b4c5 (bug 1822975) for causing browser_net_accessibility related devtools failures. CLOSED TREE

--- devtools/client/netmonitor/test/head.js
+++ devtools/client/netmonitor/test/head.js
@@ -334,6 +334,14 @@ function initNetMonitor(
   }
 
   return (async function () {
+    await SpecialPowers.pushPrefEnv({
+      set: [
+        // Capture all stacks so that the timing of devtools opening
+        // doesn't affect the stack trace results.
+        ["javascript.options.asyncstack_capture_debuggee_only", false],
+      ],
+    });
+
     let tab = null;
     let privateWindow = null;
 
