7f510a58bbde59047cbb34808c6edaef5187690e
Mark Banner <standard8@mozilla.com>
2024-07-08 08:51:18 +0000

    Bug 1906047 - Move toolkit/profile/ tests into a test sub-directory. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D215736

--- .eslintrc-test-paths.js
+++ .eslintrc-test-paths.js
@@ -66,7 +66,6 @@ const extraXpcshellTestPaths = [
   "toolkit/components/thumbnails/test/",
   "toolkit/modules/tests/modules/",
   "toolkit/mozapps/update/tests/data/",
-  "toolkit/profile/xpcshell/",
   "toolkit/xre/test/",
   "widget/headless/tests/",
 ];
@@ -266,7 +265,6 @@ const extraChromeTestPaths = [
   "toolkit/components/windowwatcher/test/",
   "toolkit/components/workerloader/tests/",
   "toolkit/content/tests/widgets/",
-  "toolkit/profile/test/",
   "widget/tests/",
   "xpfe/appshell/test/",
 ];
