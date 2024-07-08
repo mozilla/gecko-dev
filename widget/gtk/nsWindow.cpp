266feaed899204d6888ed0678682dc6a65a1d25f
stransky <stransky@redhat.com>
2024-07-08 08:01:08 +0000

    Bug 1898476 [Wayland] Provide surface lock by GtkCompositorWidget r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D214884

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -10276,3 +10276,10 @@ void nsWindow::SetDragSource(GdkDragContext* aSourceDragContext) {
     }
   }
 }
+
+UniquePtr<MozContainerSurfaceLock> nsWindow::LockSurface() {
+  if (mIsDestroyed) {
+    return nullptr;
+  }
+  return MakeUnique<MozContainerSurfaceLock>(mContainer);
+}
