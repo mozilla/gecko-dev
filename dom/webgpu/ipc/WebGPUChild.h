f2d8f7579cd59ff803e4dd45c35ca3f4d9e362ce
Otto Länd <bind-autoland@mozilla.com>
2024-07-15 23:52:58 +0000

    Bug 1906679: apply code formatting via Lando
    # ignore-this-changeset

--- dom/webgpu/ipc/WebGPUChild.h
+++ dom/webgpu/ipc/WebGPUChild.h
@@ -60,8 +60,8 @@ class WebGPUChild final : public PWebGPUChild, public SupportsWeakPtr {
 
   RefPtr<AdapterPromise> InstanceRequestAdapter(
       const dom::GPURequestAdapterOptions& aOptions);
-  Maybe<DeviceRequest> AdapterRequestDevice(RawId aSelfId,
-                                            const ffi::WGPUFfiDeviceDescriptor&);
+  Maybe<DeviceRequest> AdapterRequestDevice(
+      RawId aSelfId, const ffi::WGPUFfiDeviceDescriptor&);
   RawId RenderBundleEncoderFinish(ffi::WGPURenderBundleEncoder& aEncoder,
                                   RawId aDeviceId,
                                   const dom::GPURenderBundleDescriptor& aDesc);
