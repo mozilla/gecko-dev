764411347ef4eb2c1ee305e1328e2e0d2f4d9111
Sean Feng <sean@seanfeng.dev>
2024-07-16 21:14:28 +0000

    Bug 1907228 - Fix a crash about when Element::FromNode is used for textnode r=jjaschke,dom-core
    Differential Revision: https://phabricator.services.mozilla.com/D216742

--- dom/base/ContentIterator.cpp
+++ dom/base/ContentIterator.cpp
@@ -1158,7 +1158,7 @@ void ContentSubtreeIterator::Next() {
     // as long as we are finding ancestors of the endpoint of the range,
     // dive down into their children
     ShadowRoot* root = ShadowDOMSelectionHelpers::GetShadowRoot(
-        Element::FromNode(nextNode), IterAllowCrossShadowBoundary());
+        nextNode, IterAllowCrossShadowBoundary());
     if (!root) {
       nextNode = nextNode->GetFirstChild();
     } else {
