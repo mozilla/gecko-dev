1c3525cd58b796134e3af7bb2416394258dcb7e8
Jeremy Swinarton <jswinarton@mozilla.com>
2024-07-18 17:01:32 +0000

    Bug 1889880: temporarily cancel tab hover UI delay when leaving tabstrip r=dwalker,tabbrowser-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D209167

--- browser/components/tabbrowser/test/browser/tabs/browser_tab_preview.js
+++ browser/components/tabbrowser/test/browser/tabs/browser_tab_preview.js
@@ -577,6 +577,38 @@ add_task(async function urlBarInputTests() {
 });
 
 /**
+ * Quickly moving the mouse off and back on to the tab strip should
+ * not reset the delay
+ */
+add_task(async function zeroDelayTests() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["ui.tooltip.delay_ms", 1000]],
+  });
+
+  const tabUrl =
+    "data:text/html,<html><head><title>First New Tab</title></head><body>Hello</body></html>";
+  const tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, tabUrl);
+
+  await openPreview(tab);
+  await closePreviews();
+
+  let resolved = false;
+  let openPreviewPromise = openPreview(tab).then(() => {
+    resolved = true;
+  });
+  // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+  let timeoutPromise = new Promise(resolve => setTimeout(resolve, 300));
+  await Promise.race([openPreviewPromise, timeoutPromise]);
+
+  Assert.ok(resolved, "Zero delay is set immediately after leaving tab strip");
+
+  await closePreviews();
+  BrowserTestUtils.removeTab(tab);
+
+  await SpecialPowers.popPrefEnv();
+});
+
+/**
  * The panel should be configured to roll up on wheel events if
  * the tab strip is overflowing.
  */
