2a4dc0272cdf067bd255146bd07171bc8c4f65fc
Stanca Serban <sstanca@mozilla.com>
2024-07-20 18:36:32 +0300

    Backed out 9 changesets (bug 1908814, bug 1908756, bug 1908728, bug 1908737, bug 1908748, bug 1908948) for causing wpt failures in idlharness.https.html.
    Backed out changeset 930f6e63df50 (bug 1908948)
    Backed out changeset d77351fa728c (bug 1908948)
    Backed out changeset d8c081b36e07 (bug 1908756)
    Backed out changeset d90964750cd3 (bug 1908814)
    Backed out changeset 9840929bd7be (bug 1908737)
    Backed out changeset b52f1aa8abdf (bug 1908737)
    Backed out changeset be58d54e04a5 (bug 1908748)
    Backed out changeset 5a753dc93dbe (bug 1908748)
    Backed out changeset 1c31513c5e29 (bug 1908728)

--- testing/web-platform/tests/storage-access-api/resources/embedded_responder.js
+++ testing/web-platform/tests/storage-access-api/resources/embedded_responder.js
@@ -95,9 +95,3 @@ window.addEventListener("message", async (event) => {
     default:
   }
 });
-
-// The document that loads this script will define `should_ack_load` based on
-// the query parameters it received from the test.
-if (should_ack_load) {
-  parent.postMessage('loaded', '*');
-}
