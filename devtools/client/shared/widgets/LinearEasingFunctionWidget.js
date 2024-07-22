cb1c0e2a75740f2ae471cdb8a6ce3573dddc202d
Masayuki Nakano <masayuki@d-toybox.com>
2024-07-22 08:05:35 +0000

    Bug 1907494 - Make `LinearEasingFunctionWidget` listen to `pointerdown` to capture the pointer r=nchevobbe,devtools-reviewers
    `MouseEvent.pointerId` is `undefined` so that `setPointerCapture(undefined)`
    means `setPointerCapture(0)` and it captures both click of mouse and first
    touch of touch input devices.  Therefore, for getting correct `pointerId`,
    it needs to listen to `pointerdown`.  Then, we make it listen to `pointerdown`
    and `pointerup` events because `mousedown` and `mouseup` are always fired after
    `pointerdown` and `pointerup` (`mousemove` and `pointermove` are really
    different, therefore, it needs to keep listening to `mousemove`).  Finally,
    they are handling a drag during a button press.  Therefore, they are designed
    only for mouse devices.  Therefore, this patch makes the `pointerdown` listener
    start to handle a drag only when the device type is "mouse".
    
    Note that the pointer capture will be released when all pointers of the device
    are inactivated.  Therefore, it does not need explicitly to release the capture.
    
    Depends on D217215
    
    Differential Revision: https://phabricator.services.mozilla.com/D217216

--- devtools/client/shared/widgets/LinearEasingFunctionWidget.js
+++ devtools/client/shared/widgets/LinearEasingFunctionWidget.js
@@ -43,9 +43,14 @@ class LinearEasingFunctionWidget extends EventEmitter {
     this.parent = parent;
     this.#initMarkup();
 
-    this.#svgEl.addEventListener("mousedown", this.#onMouseDown.bind(this), {
-      signal: this.#abortController.signal,
-    });
+    this.#svgEl.addEventListener(
+      "pointerdown",
+      this.#onPointerDown.bind(this),
+      {
+        signal: this.#abortController.signal,
+        passive: true,
+      }
+    );
     this.#svgEl.addEventListener("dblclick", this.#onDoubleClick.bind(this), {
       signal: this.#abortController.signal,
     });
@@ -179,12 +184,15 @@ class LinearEasingFunctionWidget extends EventEmitter {
   }
 
   /**
-   * Handle mousedown event on the svg
+   * Handle pointerdown event on the svg
    *
-   * @param {MouseEvent} event
+   * @param {PointerEvent} event
    */
-  #onMouseDown(event) {
+  #onPointerDown(event) {
     if (
+      // We want to handle a drag during a mouse button is pressed.  So, we can
+      // ignore pointer events which are caused by other devices.
+      event.pointerType != "mouse" ||
       !event.target.classList.contains(
         LinearEasingFunctionWidget.CONTROL_POINTS_CLASSNAME
       )
@@ -196,14 +204,20 @@ class LinearEasingFunctionWidget extends EventEmitter {
     this.#draggedEl.setPointerCapture(event.pointerId);
 
     this.#dragAbortController = new AbortController();
+    // Note that "pointermove" is also fired when the button state is changed.
+    // Therefore, we should listen to "mousemove".
     this.#draggedEl.addEventListener(
       "mousemove",
       this.#onMouseMove.bind(this),
       { signal: this.#dragAbortController.signal }
     );
-    this.#draggedEl.addEventListener("mouseup", this.#onMouseUp.bind(this), {
-      signal: this.#dragAbortController.signal,
-    });
+    this.#draggedEl.addEventListener(
+      "pointerup",
+      this.#onPointerUp.bind(this),
+      {
+        signal: this.#dragAbortController.signal,
+      }
+    );
   }
 
   /**
@@ -255,13 +269,10 @@ class LinearEasingFunctionWidget extends EventEmitter {
   }, 20);
 
   /**
-   * Handle mouseup event on a control point. Only active when there's a control point
+   * Handle pointerup event on a control point. Only active when there's a control point
    * being dragged.
-   *
-   * @param {MouseEvent} event
    */
-  #onMouseUp(event) {
-    this.#draggedEl.releasePointerCapture(event.pointerId);
+  #onPointerUp() {
     this.#draggedEl = null;
     this.#dragAbortController.abort();
     this.#dragAbortController = null;
