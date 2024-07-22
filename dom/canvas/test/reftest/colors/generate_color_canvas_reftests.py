35df88146833e85e521aa14bb10214c8a444bc2d
Kelsey Gilbert <kelsey.gilbert@mozilla.com>
2024-07-22 22:43:09 +0000

    Bug 1908740 - Enable ES3 webgl1 on Windows. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D217149

--- dom/canvas/test/reftest/colors/generate_color_canvas_reftests.py
+++ dom/canvas/test/reftest/colors/generate_color_canvas_reftests.py
@@ -275,13 +275,13 @@ def reftests_from_config(test_config: Config) -> Iterable[ColorReftest]:
             ["skip-if(cocoaWidget||winWidget)  "], expected_color_srgb
         )
     elif test_config["e_context"] == "webgl":
-        # Mac
+        # Win, Mac
         yield from reftests_from_expected_color(
-            ["skip-if(!cocoaWidget)"], correct_color
+            ["skip-if(!cocoaWidget&&!winWidget)"], correct_color
         )
-        # Win, Lin, Android
+        # Lin, Android
         yield from reftests_from_expected_color(
-            ["skip-if(cocoaWidget) "], expected_color_srgb
+            ["skip-if(cocoaWidget||winWidget)  "], expected_color_srgb
         )
     else:
         assert False, test_config["e_context"]
