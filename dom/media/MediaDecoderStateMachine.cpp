63ac6ada8a0a2bf3f6a203462162662a5c274ae7
alwu <alwu@mozilla.com>
2024-07-22 17:11:05 +0000

    Bug 1905231 - part2 : duration should not be negative. r=media-playback-reviewers,karlt
    The root cause of this problem is because a negative end time [1]
    buffered range would be updated to the duration, which causes a seeking
    [2] to a negative position.
    
    [1] https://searchfox.org/mozilla-central/rev/b220e40ff2ee3d10ce68e07d8a8a577d5558e2a2/dom/media/MediaFormatReader.cpp#3278
    [2] https://searchfox.org/mozilla-central/rev/b220e40ff2ee3d10ce68e07d8a8a577d5558e2a2/dom/media/MediaDecoder.cpp#1189
    
    Differential Revision: https://phabricator.services.mozilla.com/D217042

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -3928,8 +3928,9 @@ void MediaDecoderStateMachine::BufferedRangeUpdated() {
 
   // Use estimated duration from buffer ranges when mDuration is unknown or
   // the estimated duration is larger.
-  if (mDuration.Ref().isNothing() || mDuration.Ref()->IsInfinite() ||
-      end > mDuration.Ref().ref()) {
+  if ((mDuration.Ref().isNothing() || mDuration.Ref()->IsInfinite() ||
+       end > mDuration.Ref().ref()) &&
+      end.IsPositiveOrZero()) {
     nsPrintfCString msg{
         "duration:%" PRId64 "->%" PRId64,
         mDuration.Ref().isNothing() ? 0 : mDuration.Ref()->ToMicroseconds(),
