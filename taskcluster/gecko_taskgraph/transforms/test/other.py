0c88112d01578a3a18e2cfa07d98bfcf0dda4ec8
KS <kshampur@mozilla.com>
2024-07-23 13:32:18 +0000

    Bug 1901039 - Add chromedriver 127. r=perftest-reviewers,taskgraph-reviewers,bhearsum,aglavic
    Differential Revision: https://phabricator.services.mozilla.com/D217038

--- taskcluster/gecko_taskgraph/transforms/test/other.py
+++ taskcluster/gecko_taskgraph/transforms/test/other.py
@@ -395,20 +395,20 @@ def setup_browsertime(config, tasks):
 
         cd_fetches = {
             "android.*": [
-                "linux64-chromedriver-125",
                 "linux64-chromedriver-126",
+                "linux64-chromedriver-127",
             ],
             "linux.*": [
-                "linux64-chromedriver-125",
                 "linux64-chromedriver-126",
+                "linux64-chromedriver-127",
             ],
             "macosx1015.*": [
-                "mac64-chromedriver-125",
                 "mac64-chromedriver-126",
+                "mac64-chromedriver-127",
             ],
             "macosx1400.*": [
-                "mac-arm-chromedriver-125",
                 "mac-arm-chromedriver-126",
+                "mac-arm-chromedriver-127",
             ],
             "windows.*aarch64.*": [
                 "win32-chromedriver-121",
@@ -416,8 +416,8 @@ def setup_browsertime(config, tasks):
                 "win32-chromedriver-123",
             ],
             "windows.*-64.*": [
-                "win64-chromedriver-125",
                 "win64-chromedriver-126",
+                "win64-chromedriver-127",
             ],
         }
 
