beeb7ef80744fb4aa2eceb3f13346eefbb990f16
Erich Gubler <erichdongubler@gmail.com>
2024-07-24 13:40:45 +0000

    Bug 1909265 - test(webgpu): note actual repo. tech. in Gecko checkout find errs. r=webgpu-reviewers,teoxoy
    Differential Revision: https://phabricator.services.mozilla.com/D217537

--- dom/webgpu/tests/cts/vendor/src/main.rs
+++ dom/webgpu/tests/cts/vendor/src/main.rs
@@ -63,14 +63,15 @@ fn run(args: CliArgs) -> miette::Result<()> {
             cwd: Path::new("."),
             kind: FindUpKind::Dir,
         };
-        let find_up = |root_dir_name| {
+        let find_up = |repo_tech_name, root_dir_name| {
             let err = || {
                 miette!(
                     concat!(
-                        "failed to find a Mercurial repository ({:?}) in any of current ",
+                        "failed to find a {} repository ({:?}) in any of current ",
                         "working directory and its parent directories",
                     ),
-                    root_dir_name
+                    repo_tech_name,
+                    root_dir_name,
                 )
             };
             find_up_with(root_dir_name, find_up_opts())
@@ -82,8 +83,8 @@ fn run(args: CliArgs) -> miette::Result<()> {
                     dir
                 })
         };
-        let gecko_source_root = find_up(".hg").or_else(|hg_err| {
-            find_up(".git").or_else(|git_err| match find_up(".jj") {
+        let gecko_source_root = find_up("Mercurial", ".hg").or_else(|hg_err| {
+            find_up("Git", ".git").or_else(|git_err| match find_up("Jujutsu", ".jj") {
                 Ok(path) => {
                     log::debug!("{hg_err:?}");
                     Ok(path)
