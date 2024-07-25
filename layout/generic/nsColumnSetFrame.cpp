337eaaf06b01b112897476826447ecc0189133f0
Ting-Yu Lin <tlin@mozilla.com>
2024-07-25 18:26:48 +0000

    Bug 1909786 Part 5 - Remove obsolete comments in nsColumnSetFrame. r=layout-reviewers,jfkthame
    After [1], nsColumnSetFrame becomes the inner frame of ColumnSetWrapperFrame, so
    we don't need to consider size containment for nsColumnSetFrame.
    
    [1] https://hg.mozilla.org/mozilla-central/rev/62a41cc1264ebceedd1458b60f1c1500f0565794
    
    Differential Revision: https://phabricator.services.mozilla.com/D217681

--- layout/generic/nsColumnSetFrame.cpp
+++ layout/generic/nsColumnSetFrame.cpp
@@ -415,9 +415,6 @@ nscoord nsColumnSetFrame::GetMinISize(gfxContext* aRenderingContext) {
   nscoord iSize = 0;
 
   if (mFrames.FirstChild()) {
-    // We want to ignore this in the case that we're size contained
-    // because our children should not contribute to our
-    // intrinsic size.
     iSize = mFrames.FirstChild()->GetMinISize(aRenderingContext);
   }
   const nsStyleColumn* colStyle = StyleColumn();
@@ -455,9 +452,6 @@ nscoord nsColumnSetFrame::GetPrefISize(gfxContext* aRenderingContext) {
     colISize =
         ColumnUtils::ClampUsedColumnWidth(colStyle->mColumnWidth.AsLength());
   } else if (mFrames.FirstChild()) {
-    // We want to ignore this in the case that we're size contained
-    // because our children should not contribute to our
-    // intrinsic size.
     colISize = mFrames.FirstChild()->GetPrefISize(aRenderingContext);
   } else {
     colISize = 0;
