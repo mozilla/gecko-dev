465912000d02f3f474d28466f5df010bc8f93a4f
Benjamin VanderSloot <bvandersloot@mozilla.com>
2024-07-25 15:34:40 +0000

    Bug 1892021, part 3 - Credential chosen callback update to match new semantic - r=anti-tracking-reviewers,pbz
    Down the stack I need to change the argument to `notify` in the nsICredentialChosenCallback so it
    can run in Javascript for testing. I make the change here that allows consumers of this class to
    still get the `IPCIdentityCredential` in the resolve callback of the argument promise.
    
    Differential Revision: https://phabricator.services.mozilla.com/D215007

--- toolkit/components/credentialmanagement/CredentialChosenCallback.h
+++ toolkit/components/credentialmanagement/CredentialChosenCallback.h
@@ -10,21 +10,27 @@
 #include "nsICredentialChosenCallback.h"
 #include "nsINamed.h"
 #include "nsStringFwd.h"
+#include "nsTArray.h"
 
+#include "mozilla/dom/IdentityCredentialSerializationHelpers.h"
 #include "mozilla/dom/Promise.h"
 
 namespace mozilla {
 
-using dom::Credential;
+using dom::IPCIdentityCredential;
 using dom::Promise;
 
 class CredentialChosenCallback final : public nsICredentialChosenCallback,
                                        public nsINamed {
  public:
-  explicit CredentialChosenCallback(Promise* aPromise) : mPromise(aPromise) {}
+  explicit CredentialChosenCallback(
+      CopyableTArray<IPCIdentityCredential>&& aOptions,
+      const RefPtr<MozPromise<IPCIdentityCredential, nsresult, true>::Private>&
+          aResult)
+      : mOptions(aOptions), mResult(aResult) {}
 
   NS_IMETHOD
-  Notify(Credential* aCredential) override;
+  Notify(const nsACString& aCredential) override;
 
   NS_IMETHOD
   GetName(nsACString& aName) override;
@@ -32,8 +38,21 @@ class CredentialChosenCallback final : public nsICredentialChosenCallback,
   NS_DECL_ISUPPORTS
 
  private:
-  ~CredentialChosenCallback() = default;
-  RefPtr<Promise> mPromise;
+  ~CredentialChosenCallback() {
+    if (mResult) {
+      mResult->Reject(NS_ERROR_FAILURE, __func__);
+    }
+  };
+
+  // mOptions is the list of credentials presented to the user in the credential
+  // chooser. We maintain this list so we can resolve with the user's choice of
+  // credential.
+  CopyableTArray<IPCIdentityCredential> mOptions;
+
+  // mResult is a promise that will fulfill once the user has made a choice.
+  // Dismissal is represented as a reject(NS_OK), and selection resolves with
+  // an entry of mOptions.
+  RefPtr<MozPromise<IPCIdentityCredential, nsresult, true>::Private> mResult;
 };
 
 }  // namespace mozilla
