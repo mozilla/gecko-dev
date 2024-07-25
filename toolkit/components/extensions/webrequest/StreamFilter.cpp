b99c0c779d707dea2361878a9875c472da9095ff
Peter Van der Beken <peterv@propagandism.org>
2024-07-25 07:40:41 +0000

    Bug 1900930 - Use IgnoreErrors() from ArrayBuffer::Create in StreamFilter::FireEvent. r=dom-core,extension-reviewers,robwu,farre
    Slightly simpler (and avoids calling WouldReportJSException).
    
    Differential Revision: https://phabricator.services.mozilla.com/D214795

--- toolkit/components/extensions/webrequest/StreamFilter.cpp
+++ toolkit/components/extensions/webrequest/StreamFilter.cpp
@@ -201,11 +201,9 @@ void StreamFilter::FireDataEvent(const nsTArray<uint8_t>& aData) {
   init.mBubbles = false;
   init.mCancelable = false;
 
-  ErrorResult error;
-  auto buffer = ArrayBuffer::Create(cx, aData, error);
-  if (error.Failed()) {
-    // TODO: There is no way to recover from this. This chunk of data is lost.
-    error.SuppressException();
+  // TODO: There is no way to recover from this. This chunk of data is lost.
+  auto* buffer = ArrayBuffer::Create(cx, aData, IgnoreErrors());
+  if (!buffer) {
     FireErrorEvent(u"Out of memory"_ns);
     return;
   }
