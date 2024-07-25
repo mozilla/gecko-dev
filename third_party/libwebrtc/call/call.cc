45facb46d94d4729a7d7ac9c1afd65bb5ebaa033
Byron Campen <docfaraday@gmail.com>
2024-07-25 10:45:45 -0500

    Bug 1909234 - Vendor libwebrtc from c157f29216
    Upstream commit: https://webrtc.googlesource.com/src/+/c157f29216a908949e4723a1cb9c9da49b840ce7
        Pass Environment into audio ChannelSend
    
        To make it available for creating AudioEncoders in follow ups
    
        Bug: webrtc:343086059
        Change-Id: I24bb8f7e0494e392210cb1001ea0421030d2766b
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/352601
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Reviewed-by: Jakob Ivarsson‎ <jakobi@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#42396}

--- third_party/libwebrtc/call/call.cc
+++ third_party/libwebrtc/call/call.cc
@@ -744,10 +744,10 @@ webrtc::AudioSendStream* Call::CreateAudioSendStream(
     }
   }
 
-  AudioSendStream* send_stream = new AudioSendStream(
-      &env_.clock(), config, config_.audio_state, &env_.task_queue_factory(),
-      transport_send_.get(), bitrate_allocator_.get(), &env_.event_log(),
-      call_stats_->AsRtcpRttStats(), suspended_rtp_state, trials());
+  AudioSendStream* send_stream =
+      new AudioSendStream(env_, config, config_.audio_state,
+                          transport_send_.get(), bitrate_allocator_.get(),
+                          call_stats_->AsRtcpRttStats(), suspended_rtp_state);
   RTC_DCHECK(audio_send_ssrcs_.find(config.rtp.ssrc) ==
              audio_send_ssrcs_.end());
   audio_send_ssrcs_[config.rtp.ssrc] = send_stream;
