0dfb26d1622a7f3499b62bd8d691686aa3349e4b
Sean <sekim@mozilla.com>
2024-07-25 16:39:48 +0000

    Bug 1908994 - Migrate NETWORK_CACHE_METADATA_* probes to Glean r=kershaw,acreskey,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D217177

--- netwerk/cache2/CacheFileMetadata.cpp
+++ netwerk/cache2/CacheFileMetadata.cpp
@@ -17,6 +17,7 @@
 #include "mozilla/Telemetry.h"
 #include "mozilla/DebugOnly.h"
 #include "mozilla/IntegerPrintfMacros.h"
+#include "mozilla/glean/GleanMetrics.h"
 #include "prnetdb.h"
 
 namespace mozilla::net {
@@ -635,13 +636,16 @@ nsresult CacheFileMetadata::OnDataRead(CacheFileHandle* aHandle, char* aBuf,
     return NS_OK;
   }
 
+#ifndef ANDROID
+  mozilla::TimeStamp readEnd = mozilla::TimeStamp::Now();
   if (mFirstRead) {
-    Telemetry::AccumulateTimeDelta(
-        Telemetry::NETWORK_CACHE_METADATA_FIRST_READ_TIME_MS, mReadStart);
+    mozilla::glean::networking::cache_metadata_first_read_time
+        .AccumulateRawDuration(readEnd - mReadStart);
   } else {
-    Telemetry::AccumulateTimeDelta(
-        Telemetry::NETWORK_CACHE_METADATA_SECOND_READ_TIME_MS, mReadStart);
+    mozilla::glean::networking::cache_metadata_second_read_time
+        .AccumulateRawDuration(readEnd - mReadStart);
   }
+#endif
 
   // check whether we have read all necessary data
   uint32_t realOffset =
@@ -726,8 +730,9 @@ nsresult CacheFileMetadata::OnDataRead(CacheFileHandle* aHandle, char* aBuf,
     return NS_OK;
   }
 
-  Telemetry::Accumulate(Telemetry::NETWORK_CACHE_METADATA_SIZE_2,
-                        size - realOffset);
+#ifndef ANDROID
+  mozilla::glean::networking::cache_metadata_size.Accumulate(size - realOffset);
+#endif
 
   // We have all data according to offset information at the end of the entry.
   // Try to parse it.
