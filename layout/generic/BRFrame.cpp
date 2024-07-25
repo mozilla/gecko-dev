fe1641c15e93e742a6ce4ad161d546685d9066fb
Ting-Yu Lin <tlin@mozilla.com>
2024-07-25 18:26:47 +0000

    Bug 1909786 Part 3 - Delete GetMinISize/GetPrefISize overrides for BRFrame. r=layout-reviewers,jfkthame
    BRFrame inherits from nsIFrame, and nsIFrame's default implementation for both
    methods return 0.
    
    Differential Revision: https://phabricator.services.mozilla.com/D217679

--- layout/generic/BRFrame.cpp
+++ layout/generic/BRFrame.cpp
@@ -57,8 +57,6 @@ class BRFrame final : public nsIFrame {
                          InlineMinISizeData* aData) override;
   void AddInlinePrefISize(gfxContext* aRenderingContext,
                           InlinePrefISizeData* aData) override;
-  nscoord GetMinISize(gfxContext* aRenderingContext) override;
-  nscoord GetPrefISize(gfxContext* aRenderingContext) override;
 
   Maybe<nscoord> GetNaturalBaselineBOffset(
       WritingMode aWM, BaselineSharingGroup aBaselineGroup,
@@ -183,12 +181,6 @@ void BRFrame::AddInlinePrefISize(gfxContext* aRenderingContext,
     aData->ForceBreak();
   }
 }
-
-/* virtual */
-nscoord BRFrame::GetMinISize(gfxContext* aRenderingContext) { return 0; }
-
-/* virtual */
-nscoord BRFrame::GetPrefISize(gfxContext* aRenderingContext) { return 0; }
 
 Maybe<nscoord> BRFrame::GetNaturalBaselineBOffset(
     WritingMode aWM, BaselineSharingGroup aBaselineGroup,
