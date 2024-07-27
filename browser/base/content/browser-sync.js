4a6e1e6b469ee89ce6b2a823ca988e7fcc1f8d34
Sammy Khamis <skhamis@mozilla.com>
2024-07-27 15:14:54 +0000

    Bug 1908686: FxA sign in entrypoint should match clients  a=dmeehan
    Original Revision: https://phabricator.services.mozilla.com/D217048
    
    Differential Revision: https://phabricator.services.mozilla.com/D217868

--- browser/base/content/browser-sync.js
+++ browser/base/content/browser-sync.js
@@ -1426,7 +1426,10 @@ var gSync = {
 
   async openFxAEmailFirstPageFromFxaMenu(sourceElement, extraParams = {}) {
     this.emitFxaToolbarTelemetry("login", sourceElement);
-    this.openFxAEmailFirstPage("fxa_toolbar_button", extraParams);
+    this.openFxAEmailFirstPage(
+      this._getEntryPointForElement(sourceElement),
+      extraParams
+    );
   },
 
   async openFxAManagePage(entryPoint) {
