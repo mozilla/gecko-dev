720c0d45ad45a8c9b6e670ca941ceceb0745e0d2
Brad Werth <bwerth@mozilla.com>
2024-07-31 16:35:48 +0000

    Bug 1896000: Make WebRenderCommandBuilder calculate a dirty rect without considering visibility. r=gfx-reviewers,nical
    This ensures the invalid rect always encompasses the items that
    invalidate it. The preserved/visible rect is still used to determine
    if an update needs to be sent at all, which hopefully retains the
    intended optimization benefits.
    
    This patch also removes an incorrect comment that is confusing. The
    preserved rect does not contain the invalid rect.
    
    Differential Revision: https://phabricator.services.mozilla.com/D218140

--- gfx/layers/wr/WebRenderCommandBuilder.cpp
+++ gfx/layers/wr/WebRenderCommandBuilder.cpp
@@ -310,7 +310,6 @@ struct DIGroup {
   LayerIntRect mLastVisibleRect;
 
   // This is the intersection of mVisibleRect and mLastVisibleRect
-  // we ensure that mInvalidRect is contained in mPreservedRect
   LayerIntRect mPreservedRect;
   // mHitTestBounds is the same as mActualBounds except for the bounds
   // of invisible items which are accounted for in the former but not
@@ -336,11 +335,7 @@ struct DIGroup {
         mHitInfo(CompositorHitTestInvisibleToHit) {}
 
   void InvalidateRect(const LayerIntRect& aRect) {
-    auto r = aRect.Intersect(mPreservedRect);
-    // Empty rects get dropped
-    if (!r.IsEmpty()) {
-      mInvalidRect = mInvalidRect.Union(r);
-    }
+    mInvalidRect = mInvalidRect.Union(aRect);
   }
 
   LayerIntRect ItemBounds(nsDisplayItem* aItem) {
