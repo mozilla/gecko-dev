3778f80cd0bf6734c9f45e284326e35fa3c77de5
Chun-Min Chang <chun.m.chang@gmail.com>
2024-07-31 00:09:55 +0000

    Bug 1865896 - Alphabetize headers in FFmpegRuntimeLinker.cpp r=media-playback-reviewers,padenot
    Depends on D194551
    
    Differential Revision: https://phabricator.services.mozilla.com/D204449

--- dom/media/platforms/ffmpeg/FFmpegRuntimeLinker.cpp
+++ dom/media/platforms/ffmpeg/FFmpegRuntimeLinker.cpp
@@ -4,10 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "FFmpegRuntimeLinker.h"
 #include "FFmpegLibWrapper.h"
-#include "mozilla/ArrayUtils.h"
 #include "FFmpegLog.h"
+#include "FFmpegRuntimeLinker.h"
+#include "mozilla/ArrayUtils.h"
 #include "prlink.h"
 
 namespace mozilla {
