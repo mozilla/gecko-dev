760edb221fc338f8cd88e6efebb0788cf07379e9
Jared Hirsch <ohai@6a68.net>
2024-07-31 21:13:30 +0000

    Bug 1893666 Part 1 - make the nsToolkitProfile rootDir writable. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D209751

--- toolkit/profile/test/xpcshell/test_update_paths.js
+++ toolkit/profile/test/xpcshell/test_update_paths.js
@@ -6,52 +6,57 @@
  * profile directory, including the case where the profile is the dedicated
  * default for the current install.
  */
-add_task(async () => {
-  let hash = xreDirProvider.getInstallHash();
-  let defaultProfile = makeRandomProfileDir("default");
-  let updatedProfile = makeRandomProfileDir("updated");
-  let profilesIni = {
-    profiles: [
-      {
-        name: "default",
-        path: defaultProfile.leafName,
-        default: true,
+add_task(
+  {
+    skip_if: () => !AppConstants.MOZ_SELECTABLE_PROFILES,
+  },
+  async () => {
+    let hash = xreDirProvider.getInstallHash();
+    let defaultProfile = makeRandomProfileDir("default");
+    let updatedProfile = makeRandomProfileDir("updated");
+    let profilesIni = {
+      profiles: [
+        {
+          name: "default",
+          path: defaultProfile.leafName,
+          default: true,
+        },
+      ],
+      installs: {
+        [hash]: {
+          default: defaultProfile.leafName,
+        },
       },
-    ],
-    installs: {
-      [hash]: {
-        default: defaultProfile.leafName,
-      },
-    },
-  };
-  writeProfilesIni(profilesIni);
+    };
+    writeProfilesIni(profilesIni);
 
-  // Ensure the database and local data are consistent at the outset.
-  checkProfileService(profilesIni);
+    // Ensure the database and local data are consistent at the outset.
+    checkProfileService(profilesIni);
 
-  // Update the rootDir and flush changes to disk.
-  let { profile } = selectStartupProfile();
-  profile.rootDir = updatedProfile;
-  let service = getProfileService();
-  service.flush();
+    // Update the rootDir and flush changes to disk.
+    let { profile } = selectStartupProfile();
+    profile.rootDir = updatedProfile;
+    let service = getProfileService();
+    service.flush();
 
-  // These are the database changes we expected.
-  profilesIni.profiles[0].path = updatedProfile.leafName;
-  profilesIni.installs[hash].default = updatedProfile.leafName;
+    // These are the database changes we expected.
+    profilesIni.profiles[0].path = updatedProfile.leafName;
+    profilesIni.installs[hash].default = updatedProfile.leafName;
 
-  // Verify expected changes were made to the database.
-  checkProfileService(profilesIni, false);
+    // Verify expected changes were made to the database.
+    checkProfileService(profilesIni, false);
 
-  // Finally, verify the profile object was updated as expected, including
-  // updating the localDir (which is not saved to the database).
-  Assert.ok(
-    profile.rootDir.equals(updatedProfile),
-    "rootDir should have been updated."
-  );
-  let expectedLocalDir = gDataHomeLocal.clone();
-  expectedLocalDir.append(updatedProfile.leafName);
-  Assert.ok(
-    profile.localDir.equals(expectedLocalDir),
-    "localDir should have been updated."
-  );
-});
+    // Finally, verify the profile object was updated as expected, including
+    // updating the localDir (which is not saved to the database).
+    Assert.ok(
+      profile.rootDir.equals(updatedProfile),
+      "rootDir should have been updated."
+    );
+    let expectedLocalDir = gDataHomeLocal.clone();
+    expectedLocalDir.append(updatedProfile.leafName);
+    Assert.ok(
+      profile.localDir.equals(expectedLocalDir),
+      "localDir should have been updated."
+    );
+  }
+);
