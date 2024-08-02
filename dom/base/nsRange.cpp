027dd359b18593bbc8f6cb630d4a69d811453b0a
Sean Feng <sefeng@mozilla.com>
2024-08-02 13:46:18 +0000

    Bug 1911009 - Ensure nsRange::mCrossShadowBoundaryRange can only be created when both nodes are in the document r=jjaschke,dom-core
    Differential Revision: https://phabricator.services.mozilla.com/D218352

--- dom/base/nsRange.cpp
+++ dom/base/nsRange.cpp
@@ -3512,6 +3512,12 @@ void nsRange::CreateOrUpdateCrossShadowBoundaryRangeIfNeeded(
     return;
   }
 
+  // Nodes at least needs to be in the same document.
+  if (startNode && endNode &&
+      startNode->GetComposedDoc() != endNode->GetComposedDoc()) {
+    return;
+  }
+
   auto CanBecomeCrossShadowBoundaryPoint = [](nsINode* aContainer) -> bool {
     if (!aContainer) {
       return true;
