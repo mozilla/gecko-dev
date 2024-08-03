e5e392581b5f00b19640a3829d155fb84a294942
KENZ <KENZ.gelsoft@gmail.com>
2024-08-03 10:47:57 +0900

    Don't fail bootstrap when zstandard installation skipped.

--- python/mozbuild/mozbuild/action/tooltool.py
+++ python/mozbuild/mozbuild/action/tooltool.py
@@ -985,7 +985,11 @@ def unpack_file(filename):
         with tarfile.open(filename) as tar:
             safe_extract(tar)
     elif os.path.isfile(filename) and filename.endswith(".tar.zst"):
-        import zstandard
+        try:
+            import zstandard
+        except:
+            log.error("Didn't install zstandard. Can't extract '%s'" % filename)
+            return False
 
         base_file = filename.replace(".tar.zst", "")
         clean_path(base_file)
