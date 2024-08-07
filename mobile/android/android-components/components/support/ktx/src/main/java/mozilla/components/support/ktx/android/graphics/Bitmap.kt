66e21bb372afb74098291584b7dc75806f1e432f
mcarare <mcarare@mozilla.com>
2024-08-07 14:20:46 +0000

    Bug 1880805 - Handle Bitmap.config nullability. r=android-reviewers,petru
    Differential Revision: https://phabricator.services.mozilla.com/D217430

--- mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/graphics/Bitmap.kt
+++ mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/graphics/Bitmap.kt
@@ -5,6 +5,7 @@
 package mozilla.components.support.ktx.android.graphics
 
 import android.graphics.Bitmap
+import android.graphics.Bitmap.Config
 import android.graphics.BitmapShader
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -35,7 +36,7 @@ private const val BITMAP_COMPRESSION_QUALITY = 100
  * - Wrap your bitmap's ImageView with a layout that masks your view with rounded corners (e.g. CardView)
  */
 @CheckResult
-fun Bitmap.withRoundedCorners(cornerRadiusPx: Float): Bitmap {
+fun Bitmap.withRoundedCorners(cornerRadiusPx: Float, config: Config): Bitmap {
     val roundedBitmap = Bitmap.createBitmap(width, height, config)
     val canvas = Canvas(roundedBitmap)
     val paint = Paint().apply {
