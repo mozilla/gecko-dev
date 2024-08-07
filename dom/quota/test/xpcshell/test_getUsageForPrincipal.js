378f5dc3a7988e749513b3e5302f336bb7b67075
Jan Varga <jvarga@mozilla.com>
2024-08-07 07:53:15 +0000

    Bug 1910848 - Add a test for nsIQuotaManagerService::GetUsageForPrincipal; r=hsingh
    This test is needed for making sure that upcoming refactoring doesn't regress
    getting origin usage, especially the cancelling of requests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D194073

---
+++ dom/quota/test/xpcshell/test_getUsageForPrincipal.js
@@ -0,0 +1,79 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+async function testSteps() {
+  const principal = getPrincipal("http://localhost");
+
+  const resultBeforeInstall = {
+    databaseUsage: 0,
+    fileUsage: 0,
+    usage: 0,
+  };
+
+  const resultAfterInstall = {
+    databaseUsage: 147456,
+    fileUsage: 0,
+    usage: 147456,
+  };
+
+  function verifyResult(result, expectedResult) {
+    ok(
+      result instanceof Ci.nsIQuotaOriginUsageResult,
+      "The result is nsIQuotaOriginUsageResult instance"
+    );
+
+    Assert.strictEqual(
+      result.databaseUsage,
+      expectedResult.databaseUsage,
+      "Database usage equals"
+    );
+    Assert.strictEqual(
+      result.fileUsage,
+      expectedResult.fileUsage,
+      "File usage equals"
+    );
+    Assert.strictEqual(
+      result.usage,
+      expectedResult.usage,
+      "Total usage equals"
+    );
+  }
+
+  info("Clearing");
+
+  let request = clear();
+  await requestFinished(request);
+
+  info("Getting origin usage");
+
+  request = getOriginUsage(principal);
+  let result = await requestFinished(request);
+
+  info("Verifying result");
+
+  verifyResult(result, resultBeforeInstall);
+
+  info("Clearing");
+
+  request = clear();
+  await requestFinished(request);
+
+  info("Installing package");
+
+  // The profile contains IndexedDB databases placed across the repositories.
+  // The file make_getUsageForPrincipal.js was run locally, specifically it was
+  // temporarily enabled in xpcshell.ini and then executed:
+  // mach test --interactive dom/quota/test/xpcshell/make_getUsageForPrincipal.js
+  installPackage("getUsageForPrincipal_profile");
+
+  info("Getting origin usage");
+
+  request = getOriginUsage(principal);
+  result = await requestFinished(request);
+
+  info("Verifying result");
+
+  verifyResult(result, resultAfterInstall);
+}
