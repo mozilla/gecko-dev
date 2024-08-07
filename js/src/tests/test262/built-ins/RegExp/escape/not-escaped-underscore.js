767e3b48e84566f7a3cbe733c987372f254b8cc0
André Bargull <andre.bargull@gmail.com>
2024-08-07 13:49:14 +0000

    Bug 1911097 - Part 3: Reimport test262. r=spidermonkey-reviewers,dminor
    Differential Revision: https://phabricator.services.mozilla.com/D218323

--- js/src/tests/test262/built-ins/RegExp/escape/not-escaped-underscore.js
+++ js/src/tests/test262/built-ins/RegExp/escape/not-escaped-underscore.js
@@ -1,4 +1,4 @@
-// |reftest| skip -- RegExp.escape is not supported
+// |reftest| shell-option(--enable-regexp-escape) skip-if(!RegExp.escape||!xulRuntime.shell) -- RegExp.escape is not enabled unconditionally, requires shell-options
 // Copyright (C) 2024 Leo Balter. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
