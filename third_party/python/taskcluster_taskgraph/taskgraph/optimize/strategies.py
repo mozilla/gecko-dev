2aa9aade9f1fc47a1c9bfc8885be766eb14f5039
Mike Hommey <mh+mozilla@glandium.org>
2024-08-07 21:18:05 +0000

    Bug 1910501 - Update taskgraph vendor to v10.0.2.  a=dmeehan
    Original Revision: https://phabricator.services.mozilla.com/D218586
    
    Differential Revision: https://phabricator.services.mozilla.com/D218792

--- third_party/python/taskcluster_taskgraph/taskgraph/optimize/strategies.py
+++ third_party/python/taskcluster_taskgraph/taskgraph/optimize/strategies.py
@@ -50,7 +50,7 @@ class IndexSearch(OptimizationStrategy):
                 # (e.g. test-action-callback)
                 if not status or status.get("state") in ("exception", "failed"):
                     logger.debug(
-                        f'not replacing {task["label"]} with {task_id} because it is in failed or exception state'
+                        f"not replacing {task.label} with {task_id} because it is in failed or exception state"
                     )
                     continue
 
@@ -58,7 +58,7 @@ class IndexSearch(OptimizationStrategy):
                     status["expires"], self.fmt
                 ) < datetime.strptime(deadline, self.fmt):
                     logger.debug(
-                        f'not replacing {task["label"]} with {task_id} because it expires before {deadline}'
+                        f"not replacing {task.label} with {task_id} because it expires before {deadline}"
                     )
                     continue
 
