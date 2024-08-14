8f6ed663ea983d75f80a09db5966b657089647dd
Mathew Hodson <mathew.hodson@gmail.com>
2024-08-14 13:44:14 +0000

    Bug 1894758 - Fix developer tools learn more link for headers test r=nchevobbe,devtools-reviewers
    This test was broken and always passed previously, because the Headers
    panel was never opened and the loop containing the asserts was skipped.
    
    Send a mousedown event to open the panel and assert some header rows
    were found.
    
    Differential Revision: https://phabricator.services.mozilla.com/D219020

--- devtools/client/netmonitor/test/browser_net_header-docs.js
+++ devtools/client/netmonitor/test/browser_net_header-docs.js
@@ -7,70 +7,55 @@
  * Tests if "Learn More" links are correctly displayed
  * next to headers.
  */
-add_task(async function () {
+add_task(async function testHeadersLearnMoreLink() {
   const { tab, monitor } = await initNetMonitor(POST_DATA_URL, {
     requestCount: 1,
   });
-  info("Starting test... ");
-
   const { document, store, windowRequire } = monitor.panelWin;
   const Actions = windowRequire("devtools/client/netmonitor/src/actions/index");
-  const { getSortedRequests } = windowRequire(
-    "devtools/client/netmonitor/src/selectors/index"
+
+  store.dispatch(Actions.batchEnable(false));
+
+  await performRequests(monitor, tab, 2);
+
+  // Open Headers panel for the customized request sent by html_post-data-test-page.html.
+  EventUtils.sendMouseEvent(
+    { type: "mousedown" },
+    document.querySelectorAll(".request-list-item")[1]
   );
+
+  await waitForDOMIfNeeded(document, "#responseHeaders, #requestHeaders", 2);
+
+  testShowLearnMore(document);
+
+  await teardown(monitor);
+});
+
+/*
+ * Tests that a "Learn More" button is only shown if
+ * and only if a header is documented in MDN.
+ */
+function testShowLearnMore(document) {
   const {
     getHeadersURL,
   } = require("resource://devtools/client/netmonitor/src/utils/doc-utils.js");
 
-  store.dispatch(Actions.batchEnable(false));
-
-  // Execute requests.
-  await performRequests(monitor, tab, 2);
-
-  AccessibilityUtils.setEnv({
-    // Keyboard users will will see the sidebar when the request row is
-    // selected. Accessibility is handled on the container level.
-    actionCountRule: false,
-    interactiveRule: false,
-    labelRule: false,
-  });
-  EventUtils.sendMouseEvent(
-    { type: "click" },
-    document.querySelectorAll(".request-list-item")[0]
+  const headerRows = document.querySelectorAll(
+    ".properties-view .treeRow.stringRow"
   );
-  AccessibilityUtils.resetEnv();
+  Assert.greater(headerRows.length, 0);
 
-  testShowLearnMore(getSortedRequests(store.getState())[0]);
-
-  return teardown(monitor);
-
-  /*
-   * Tests that a "Learn More" button is only shown if
-   * and only if a header is documented in MDN.
-   */
-  function testShowLearnMore() {
-    const selector = ".properties-view .treeRow.stringRow";
-    document.querySelectorAll(selector).forEach(rowEl => {
-      const headerName = rowEl.querySelectorAll(".treeLabelCell .treeLabel")[0]
-        .textContent;
-      const headerDocURL = getHeadersURL(headerName);
-      const learnMoreEl = rowEl.querySelectorAll(
-        ".treeValueCell .learn-more-link"
-      );
-
-      if (headerDocURL === null) {
-        Assert.strictEqual(
-          learnMoreEl.length,
-          0,
-          'undocumented header does not include a "Learn More" button'
-        );
-      } else {
-        Assert.strictEqual(
-          learnMoreEl[0].getAttribute("title"),
-          headerDocURL,
-          'documented header includes a "Learn More" button with a link to MDN'
-        );
-      }
-    });
+  for (const rowEl of headerRows) {
+    const headerName = rowEl.querySelectorAll(".treeLabelCell .treeLabel")[0]
+      .textContent;
+    const headerDocURL = getHeadersURL(headerName);
+    const learnMoreEl = rowEl.querySelectorAll(
+      ".treeValueCell .learn-more-link"
+    );
+    Assert.equal(
+      learnMoreEl.length,
+      headerDocURL ? 1 : 0,
+      'Only a documented header should include a "Learn More" button'
+    );
   }
-});
+}
