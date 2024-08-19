5d2062b8b5d731b912c17d42139e0eb9fe5fd60c
Tom Schuster <tschuster@mozilla.com>
2024-08-19 10:55:25 +0000

    Bug 1912405 - Use individual event listeners for gProtectionsHandler click handlers. r=Gijs a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D219422

--- browser/base/content/browser-siteProtections.js
+++ browser/base/content/browser-siteProtections.js
@@ -1384,7 +1384,6 @@ var gProtectionsHandler = {
         blocker.updateCategoryItem();
       }
 
-      this._protectionsPopup.addEventListener("click", this);
       this._protectionsPopup.addEventListener("command", this);
       this._protectionsPopup.addEventListener("popupshown", this);
       this._protectionsPopup.addEventListener("popuphidden", this);
@@ -1402,6 +1401,24 @@ var gProtectionsHandler = {
       notBlockingWhy.addEventListener("focus", openTooltip);
       notBlockingWhy.addEventListener("mouseout", closeTooltip);
       notBlockingWhy.addEventListener("blur", closeTooltip);
+
+      document
+        .getElementById("protections-popup-mainView-panel-header")
+        .addEventListener("click", event =>
+          gProtectionsHandler.onHeaderClicked(event)
+        );
+      document
+        .getElementById(
+          "protections-popup-trackers-blocked-counter-description"
+        )
+        .addEventListener("click", () =>
+          gProtectionsHandler.openProtections(true)
+        );
+      document
+        .getElementById("protections-popup-cookie-banner-switch")
+        .addEventListener("click", () =>
+          gProtectionsHandler.onCookieBannerClick()
+        );
     }
   },
 
@@ -2040,20 +2057,6 @@ var gProtectionsHandler = {
     this.reportBlockingEventTelemetry(event, isSimulated, previousState);
   },
 
-  onClick(event) {
-    switch (event.target.id) {
-      case "protections-popup-mainView-panel-header":
-        gProtectionsHandler.onHeaderClicked(event);
-        break;
-      case "protections-popup-trackers-blocked-counter-description":
-        gProtectionsHandler.openProtections(true);
-        break;
-      case "protections-popup-cookie-banner-switch":
-        gProtectionsHandler.onCookieBannerClick();
-        break;
-    }
-  },
-
   onCommand(event) {
     switch (event.target.id) {
       case "protections-popup-category-trackers":
@@ -2121,9 +2124,6 @@ var gProtectionsHandler = {
   // We handle focus here when the panel is shown.
   handleEvent(event) {
     switch (event.type) {
-      case "click":
-        this.onClick(event);
-        break;
       case "command":
         this.onCommand(event);
         break;
