41cd6b26181d8262444167735bdd092efdafe46b
Julian Descottes <jdescottes@mozilla.com>
2024-08-20 19:45:08 +0000

    Bug 1912239 - [puppeteer] Early exit clean.mjs if not in a git repository r=webdriver-reviewers,whimboo
    Depends on D219557
    https://github.com/puppeteer/puppeteer/pull/12981
    
    Differential Revision: https://phabricator.services.mozilla.com/D219558

--- remote/test/puppeteer/tools/clean.mjs
+++ remote/test/puppeteer/tools/clean.mjs
@@ -17,6 +17,14 @@ const execAsync = promisify(exec);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const puppeteerRoot = path.join(__dirname, '../');
 
+try {
+  await execAsync('git status');
+} catch (e) {
+  // If `git status` threw an error, we are not in a git repository, bail out.
+  console.log('Not inside a .git repository');
+  process.exit(0);
+}
+
 const {stdout} = await execAsync('git rev-parse --show-toplevel');
 const gitTopLevel = stdout.replaceAll('\n', '').trim();
 
