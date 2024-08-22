22f3bddb583ae98553f3f9c12e870229560c9408
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2024-08-22 14:48:07 +0000

    Bug 1822975 - clean up places that manually enable broader async stacks which is now the default in tests, r=devtools-reviewers,firefox-desktop-core-reviewers ,geckoview-reviewers,mconley,ohall
    Differential Revision: https://phabricator.services.mozilla.com/D215204

--- devtools/client/shared/test/shared-head.js
+++ devtools/client/shared/test/shared-head.js
@@ -279,15 +279,6 @@ Services.prefs.setBoolPref("devtools.inspector.three-pane-enabled", true);
 // requests occuring after a process is created/destroyed. See Bug 1620983.
 Services.prefs.setBoolPref("dom.ipc.processPrelaunch.enabled", false);
 
-// Disable this preference to capture async stacks across all locations during
-// DevTools mochitests. Async stacks provide very valuable information to debug
-// intermittents, but come with a performance overhead, which is why they are
-// only captured in Debuggees by default.
-Services.prefs.setBoolPref(
-  "javascript.options.asyncstack_capture_debuggee_only",
-  false
-);
-
 // On some Linux platforms, prefers-reduced-motion is enabled, which would
 // trigger the notification to be displayed in the toolbox. Dismiss the message
 // by default.
@@ -304,9 +295,6 @@ registerCleanupFunction(() => {
   Services.prefs.clearUserPref("devtools.toolbox.previousHost");
   Services.prefs.clearUserPref("devtools.toolbox.splitconsole.open");
   Services.prefs.clearUserPref("devtools.toolbox.splitconsoleHeight");
-  Services.prefs.clearUserPref(
-    "javascript.options.asyncstack_capture_debuggee_only"
-  );
   Services.prefs.clearUserPref(
     "devtools.inspector.simple-highlighters.message-dismissed"
   );
