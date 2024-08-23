edfed89db22f841ad37400053bebed7e0b524f15
Mark Banner <standard8@mozilla.com>
2024-08-23 08:55:47 +0000

    Bug 1870226 - Fix missing jsdoc @returns for search code. r=search-reviewers,jteow
    Differential Revision: https://phabricator.services.mozilla.com/D219507

--- toolkit/components/search/SearchEngineSelector.sys.mjs
+++ toolkit/components/search/SearchEngineSelector.sys.mjs
@@ -54,6 +54,9 @@ export class SearchEngineSelector {
 
   /**
    * Handles getting the configuration from remote settings.
+   *
+   * @returns {object}
+   *   The configuration data.
    */
   async getEngineConfiguration() {
     if (this._getConfigurationPromise) {
@@ -90,6 +93,9 @@ export class SearchEngineSelector {
 
   /**
    * Used by tests to get the configuration overrides.
+   *
+   * @returns {object}
+   *   The engine overrides data.
    */
   async getEngineConfigurationOverrides() {
     await this.getEngineConfiguration();
