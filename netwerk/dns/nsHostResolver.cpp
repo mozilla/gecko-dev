1d4c27f9f166ce6e967fb0e8c8d6e0795dbbd12e
Tamas Szentpeteri <tszentpeteri@mozilla.com>
2024-08-26 12:42:24 +0300

    Backed out changeset 5daa91615f4c (bug 1910593) for causing bug 1914796. CLOSED TREE

--- netwerk/dns/nsHostResolver.cpp
+++ netwerk/dns/nsHostResolver.cpp
@@ -2035,13 +2035,20 @@ void nsHostResolver::GetDNSCacheEntries(nsTArray<DNSCacheEntries>* args) {
       continue;
     }
 
-    DNSCacheEntries info;
-    info.resolveType = rec->type;
-    info.hostname = rec->host;
-    info.family = rec->af;
-    if (rec->mValidEnd.IsNull()) {
+    // For now we only show A/AAAA records.
+    if (!rec->IsAddrRecord()) {
       continue;
     }
+
+    RefPtr<AddrHostRecord> addrRec = do_QueryObject(rec);
+    MOZ_ASSERT(addrRec);
+    if (!addrRec || !addrRec->addr_info) {
+      continue;
+    }
+
+    DNSCacheEntries info;
+    info.hostname = rec->host;
+    info.family = rec->af;
     info.expiration =
         (int64_t)(rec->mValidEnd - TimeStamp::NowLoRes()).ToSeconds();
     if (info.expiration <= 0) {
@@ -2049,12 +2056,7 @@ void nsHostResolver::GetDNSCacheEntries(nsTArray<DNSCacheEntries>* args) {
       continue;
     }
 
-    info.originAttributesSuffix = recordEntry.GetKey().originSuffix;
-    info.flags = nsPrintfCString("%u|0x%x|%u|%d|%s", rec->type, rec->flags,
-                                 rec->af, rec->pb, rec->mTrrServer.get());
-
-    RefPtr<AddrHostRecord> addrRec = do_QueryObject(rec);
-    if (addrRec && addrRec->addr_info) {
+    {
       MutexAutoLock lock(addrRec->addr_info_lock);
       for (const auto& addr : addrRec->addr_info->Addresses()) {
         char buf[kIPv6CStrBufSize];
@@ -2064,6 +2066,10 @@ void nsHostResolver::GetDNSCacheEntries(nsTArray<DNSCacheEntries>* args) {
       }
       info.TRR = addrRec->addr_info->IsTRR();
     }
+
+    info.originAttributesSuffix = recordEntry.GetKey().originSuffix;
+    info.flags = nsPrintfCString("%u|0x%x|%u|%d|%s", rec->type, rec->flags,
+                                 rec->af, rec->pb, rec->mTrrServer.get());
 
     args->AppendElement(std::move(info));
   }
