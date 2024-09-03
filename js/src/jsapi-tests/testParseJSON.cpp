c97a81d82b9c7955bb9c88744bc176445a7fcc1f
Bryan Thrall <bthrall@mozilla.com>
2024-09-03 15:58:04 +0000

    Bug 1913085 - Fix jsapi-test argument count assertion r=dminor
    Since JSON.parse with source is enabled, revivers have three arguments now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D220474

--- js/src/jsapi-tests/testParseJSON.cpp
+++ js/src/jsapi-tests/testParseJSON.cpp
@@ -321,7 +321,7 @@ END_TEST(testParseJSON_error)
 
 static bool Censor(JSContext* cx, unsigned argc, JS::Value* vp) {
   JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
-  MOZ_RELEASE_ASSERT(args.length() == 2);
+  MOZ_RELEASE_ASSERT(args.length() == 3);
   MOZ_RELEASE_ASSERT(args[0].isString());
   args.rval().setNull();
   return true;
