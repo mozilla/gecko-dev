ccecd50f697af0bb11863c34608a408b4cf23509
Mike Hommey <mh+mozilla@glandium.org>
2024-09-04 08:48:59 +0000

    Bug 1916600 - Pass all arguments to build-clang. r=firefox-build-system-reviewers,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D220991

--- taskcluster/scripts/misc/build-clang-tidy-external.sh
+++ taskcluster/scripts/misc/build-clang-tidy-external.sh
@@ -7,5 +7,5 @@ rm -rf $GECKO_PATH/build/clang-plugin/external/*
 # Move external repository into its place
 cp -r $MOZ_FETCHES_DIR/civet.git/* $GECKO_PATH/build/clang-plugin/external
 
-# Call build-clang.sh with this script's first argument (our JSON config)
-$GECKO_PATH/taskcluster/scripts/misc/build-clang.sh $1
+# Call build-clang.sh with this script's arguments
+$GECKO_PATH/taskcluster/scripts/misc/build-clang.sh "$@"
