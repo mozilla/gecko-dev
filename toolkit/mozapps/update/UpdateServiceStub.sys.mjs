ab8c88c814ebc7991e35ef540ee077d91b818f5c
Eric Chen <erchen@mozilla.com>
2024-09-07 22:33:03 +0000

    Bug 1642039 - Part 1: Merge updateManager constructor with an async #reload function r=bytesized,application-update-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D216604

--- toolkit/mozapps/update/UpdateServiceStub.sys.mjs
+++ toolkit/mozapps/update/UpdateServiceStub.sys.mjs
@@ -121,7 +121,9 @@ export class UpdateServiceStub {
     const aus = Cc["@mozilla.org/updates/update-service;1"].getService(
       Ci.nsIApplicationUpdateService
     );
-    Cc["@mozilla.org/updates/update-manager;1"].getService(Ci.nsIUpdateManager);
+    await Cc["@mozilla.org/updates/update-manager;1"]
+      .getService(Ci.nsIUpdateManager)
+      .internal.reload(false);
     Cc["@mozilla.org/updates/update-checker;1"].getService(Ci.nsIUpdateChecker);
 
     // Run update service initialization
