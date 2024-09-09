baa3d9e8585f5ffde22a8d8b423e76f4cc772519
André Bargull <andre.bargull@gmail.com>
2024-09-09 06:16:16 +0000

    Bug 1916990 - Part 3: Use a switch-statement in EvaluateInEnv to handle options more clearly. r=spidermonkey-reviewers,sfink
    All different options compute a script and an environment and then call `ExecuteKernel`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D221209

--- js/src/debugger/Frame.cpp
+++ js/src/debugger/Frame.cpp
@@ -1034,83 +1034,83 @@ static bool EvaluateInEnv(
     return false;
   }
 
-  JS::Rooted<JSObject*> env(cx, envArg);
-  if (evalOptions.kind() == EvalOptions::EnvKind::FrameWithExtraBindings ||
-      evalOptions.kind() ==
-          EvalOptions::EnvKind::GlobalWithExtraInnerBindings) {
-    // NOTE: GlobalWithExtraOuterBindings case is handled in separately below.
-    env = CreateBindingsEnv(cx, env, bindingKeys, bindingValues);
-    if (!env) {
-      return false;
+  // Compile the script and compute the environment object.
+  JS::Rooted<JSScript*> script(cx);
+  JS::Rooted<JSObject*> env(cx);
+  switch (evalOptions.kind()) {
+    case EvalOptions::EnvKind::FrameWithExtraBindings: {
+      env = CreateBindingsEnv(cx, envArg, bindingKeys, bindingValues);
+      if (!env) {
+        return false;
+      }
+      [[fallthrough]];
     }
-  }
+    case EvalOptions::EnvKind::Frame: {
+      // Default to |envArg| when no extra bindings are used.
+      if (!env) {
+        env = envArg;
+      }
 
-  if (evalOptions.kind() == EvalOptions::EnvKind::Frame ||
-      evalOptions.kind() == EvalOptions::EnvKind::FrameWithExtraBindings) {
-    options.setNonSyntacticScope(true);
+      options.setNonSyntacticScope(true);
 
-    Rooted<Scope*> scope(cx,
-                         GlobalScope::createEmpty(cx, ScopeKind::NonSyntactic));
-    if (!scope) {
-      return false;
+      Rooted<Scope*> scope(
+          cx, GlobalScope::createEmpty(cx, ScopeKind::NonSyntactic));
+      if (!scope) {
+        return false;
+      }
+
+      script = frontend::CompileEvalScript(cx, options, srcBuf, scope, env);
+      if (!script) {
+        return false;
+      }
+      break;
     }
+    case EvalOptions::EnvKind::Global: {
+      AutoReportFrontendContext fc(cx);
+      script = frontend::CompileGlobalScript(cx, &fc, options, srcBuf,
+                                             ScopeKind::Global);
+      if (!script) {
+        return false;
+      }
 
-    RootedScript script(
-        cx, frontend::CompileEvalScript(cx, options, srcBuf, scope, env));
-    if (!script) {
-      return false;
+      env = envArg;
+      break;
     }
+    case EvalOptions::EnvKind::GlobalWithExtraOuterBindings: {
+      // Do not consider executeInGlobal{,WithBindings} as an eval, but instead
+      // as executing a series of statements at the global level. This is to
+      // circumvent the fresh lexical scope that all eval have, so that the
+      // users of executeInGlobal{,WithBindings}, like the web console, may add
+      // new bindings to the global scope.
 
-    return ExecuteKernel(cx, script, env, frame, rval);
-  }
+      MOZ_ASSERT(envArg == &cx->global()->lexicalEnvironment());
 
-  // Do not consider executeInGlobal{,WithBindings} as an eval, but instead
-  // as executing a series of statements at the global level. This is to
-  // circumvent the fresh lexical scope that all eval have, so that the
-  // users of executeInGlobal{,WithBindings}, like the web console, may add new
-  // bindings to the global scope.
+      options.setNonSyntacticScope(true);
 
-  if (evalOptions.kind() ==
-      EvalOptions::EnvKind::GlobalWithExtraOuterBindings) {
-    options.setNonSyntacticScope(true);
-
-    MOZ_ASSERT(env == &cx->global()->lexicalEnvironment());
-
-    JS::Rooted<JSObject*> bindingsEnv(cx);
-
-    AutoReportFrontendContext fc(cx);
-    RootedScript script(cx, frontend::CompileGlobalScriptWithExtraBindings(
-                                cx, &fc, options, srcBuf, bindingKeys,
-                                bindingValues, &bindingsEnv));
-    if (!script) {
-      return false;
+      AutoReportFrontendContext fc(cx);
+      script = frontend::CompileGlobalScriptWithExtraBindings(
+          cx, &fc, options, srcBuf, bindingKeys, bindingValues, &env);
+      if (!script) {
+        return false;
+      }
+      break;
     }
+    case EvalOptions::EnvKind::GlobalWithExtraInnerBindings: {
+      env = CreateBindingsEnv(cx, envArg, bindingKeys, bindingValues);
+      if (!env) {
+        return false;
+      }
 
-    return ExecuteKernel(cx, script, bindingsEnv, frame, rval);
-  }
+      options.setNonSyntacticScope(true);
 
-  if (evalOptions.kind() ==
-      EvalOptions::EnvKind::GlobalWithExtraInnerBindings) {
-    options.setNonSyntacticScope(true);
-
-    AutoReportFrontendContext fc(cx);
-    RootedScript script(cx,
-                        frontend::CompileGlobalScript(cx, &fc, options, srcBuf,
-                                                      ScopeKind::NonSyntactic));
-    if (!script) {
-      return false;
+      AutoReportFrontendContext fc(cx);
+      script = frontend::CompileGlobalScript(cx, &fc, options, srcBuf,
+                                             ScopeKind::NonSyntactic);
+      if (!script) {
+        return false;
+      }
+      break;
     }
-
-    return ExecuteKernel(cx, script, env, frame, rval);
-  }
-
-  MOZ_ASSERT(evalOptions.kind() == EvalOptions::EnvKind::Global);
-
-  AutoReportFrontendContext fc(cx);
-  RootedScript script(cx, frontend::CompileGlobalScript(
-                              cx, &fc, options, srcBuf, ScopeKind::Global));
-  if (!script) {
-    return false;
   }
 
   return ExecuteKernel(cx, script, env, frame, rval);
