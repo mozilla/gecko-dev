dec419b4de85d3559ac3f816c2b6d6757bab5304
Sammy Khamis <skhamis@mozilla.com>
2024-09-10 20:25:47 +0000

    Bug 1911626: Add closing tabs remotely to sidebar r=markh,lina,fluent-reviewers,desktop-theme-reviewers,fxview-reviewers,sync-reviewers,sidebar-reviewers,dao,jsudiaman
    Differential Revision: https://phabricator.services.mozilla.com/D218902

--- services/sync/modules/SyncedTabs.sys.mjs
+++ services/sync/modules/SyncedTabs.sys.mjs
@@ -21,6 +21,12 @@ ChromeUtils.defineLazyGetter(lazy, "weaveXPCService", function () {
   ).wrappedJSObject;
 });
 
+ChromeUtils.defineLazyGetter(lazy, "fxAccounts", () => {
+  return ChromeUtils.importESModule(
+    "resource://gre/modules/FxAccounts.sys.mjs"
+  ).getFxAccountsSingleton();
+});
+
 // from MDN...
 function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
@@ -105,6 +111,12 @@ let SyncedTabsInternal = {
     return reFilter.test(tab.url) || reFilter.test(tab.title);
   },
 
+  // A wrapper for grabbing the fxaDeviceId, to make it easier for stubbing
+  // for tests
+  _getClientFxaDeviceId(clientId) {
+    return lazy.Weave.Service.clientsEngine.getClientFxaDeviceId(clientId);
+  },
+
   _createRecentTabsList(
     clients,
     maxCount,
@@ -116,9 +128,21 @@ let SyncedTabsInternal = {
       if (extraParams.removeDeviceDupes) {
         client.tabs = this._filterRecentTabsDupes(client.tabs);
       }
+
+      // We have the client obj but we need the FxA device obj so we use the clients
+      // engine to get us the FxA device
+      let device =
+        lazy.fxAccounts.device.recentDeviceList &&
+        lazy.fxAccounts.device.recentDeviceList.find(
+          d => d.id === this._getClientFxaDeviceId(client.id)
+        );
+
       for (let tab of client.tabs) {
         tab.device = client.name;
         tab.deviceType = client.clientType;
+        // Surface broadcasted commmands for things like close remote tab
+        tab.fxaDeviceId = device.id;
+        tab.availableCommands = device.availableCommands;
       }
       tabs = [...tabs, ...client.tabs.reverse()];
     }
