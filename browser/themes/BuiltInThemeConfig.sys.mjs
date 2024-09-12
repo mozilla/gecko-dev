6f2990ffa66e34df06862dd066e9d81a12ac423c
Butkovits Atila <abutkovits@mozilla.com>
2024-09-12 14:14:29 +0300

    Backed out changeset a2fa22990548 (bug 1918317) for causing failures at browser_html_detail_view.js. CLOSED TREE

--- browser/themes/BuiltInThemeConfig.sys.mjs
+++ browser/themes/BuiltInThemeConfig.sys.mjs
@@ -22,14 +22,14 @@ export const BuiltInThemeConfig = new Map([
   [
     "firefox-compact-light@mozilla.org",
     {
-      version: "1.3",
+      version: "1.2",
       path: "resource://builtin-themes/light/",
     },
   ],
   [
     "firefox-compact-dark@mozilla.org",
     {
-      version: "1.3",
+      version: "1.2",
       path: "resource://builtin-themes/dark/",
     },
   ],
