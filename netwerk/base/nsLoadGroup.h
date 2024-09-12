ecedc850d7cfb81c15453431bf5b8aa830fdb7bc
Kershaw Chang <kershaw@mozilla.com>
2024-09-12 09:58:42 +0000

    Bug 1678312 - Make sure page load telemetry probes are reported properly, r=necko-reviewers,valentin
    Before this patch, `TelemetryReportChannel(timedChannel, true)` was only called within `nsLoadGroup::TelemetryReport()` when all requests were completed in the load group.
    However, `SetDefaultLoadRequest(nullptr)` could be called before this, resulting in `mDefaultLoadRequest` being set to null and causing page load metrics to sometimes not be reported.
    To fix this, this patch moves `TelemetryReportChannel(timedChannel, true)` to the point where `mDefaultLoadRequest` is completed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D221597

--- netwerk/base/nsLoadGroup.h
+++ netwerk/base/nsLoadGroup.h
@@ -93,6 +93,7 @@ class nsLoadGroup : public nsILoadGroup,
   nsWeakPtr mParentLoadGroup;
 
   nsresult mStatus{NS_OK};
+  nsresult mDefaultStatus{NS_OK};
   bool mIsCanceling{false};
   bool mDefaultLoadIsTimed{false};
   bool mBrowsingContextDiscarded{false};
