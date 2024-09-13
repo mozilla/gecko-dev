93184d92e609ad929d87fb943e6bf84ccdf25ad2
Jeff Boek <jeff@jeffboek.com>
2024-09-13 17:48:26 +0000

    Bug 1918750 - Disables the new crash reporter r=android-reviewers,matt-tighe
    Differential Revision: https://phabricator.services.mozilla.com/D222152

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -2265,6 +2265,6 @@ class Settings(private val appContext: Context) : PreferencesHolder {
      */
     var useNewCrashReporter by booleanPreference(
         appContext.getPreferenceKey(R.string.pref_key_use_new_crash_reporter),
-        default = Config.channel.isNightlyOrDebug,
+        default = false,
     )
 }
