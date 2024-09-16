ac33fb14b282f7f09ef25612a652ff29f3f5f7c9
Amy Churchwell <achurchwell@mozilla.com>
2024-09-16 21:50:25 +0000

    Bug 1918444 – create placeholder & loading styles for list feed cards. r=home-newtab-reviewers,nbarrett
    Differential Revision: https://phabricator.services.mozilla.com/D222319

--- browser/components/newtab/content-src/components/DiscoveryStreamComponents/ListFeed/ListFeed.jsx
+++ browser/components/newtab/content-src/components/DiscoveryStreamComponents/ListFeed/ListFeed.jsx
@@ -20,40 +20,51 @@ function ListFeed({ type, firstVisibleTimestamp, recs }) {
           role="menu"
           aria-labelledby="list-feed-title"
         >
-          {recs.map(rec => (
-            <DSCard
-              key={`list-card-${rec.id}`}
-              pos={rec.pos}
-              flightId={rec.flight_id}
-              image_src={rec.image_src}
-              raw_image_src={rec.raw_image_src}
-              word_count={rec.word_count}
-              time_to_read={rec.time_to_read}
-              title={rec.title}
-              topic={rec.topic}
-              excerpt={rec.excerpt}
-              url={rec.url}
-              id={rec.id}
-              shim={rec.shim}
-              fetchTimestamp={rec.fetchTimestamp}
-              type={type}
-              context={rec.context}
-              sponsor={rec.sponsor}
-              sponsored_by_override={rec.sponsored_by_override}
-              dispatch={dispatch}
-              source={rec.domain}
-              publisher={rec.publisher}
-              pocket_id={rec.pocket_id}
-              context_type={rec.context_type}
-              bookmarkGuid={rec.bookmarkGuid}
-              recommendation_id={rec.recommendation_id}
-              firstVisibleTimestamp={firstVisibleTimestamp}
-              scheduled_corpus_item_id={rec.scheduled_corpus_item_id}
-              recommended_at={rec.recommended_at}
-              received_rank={rec.received_rank}
-              isListCard={true}
-            />
-          ))}
+          {recs.map(rec => {
+            if (!rec || rec.placeholder) {
+              return (
+                <DSCard
+                  key={`list-card-${rec.id}`}
+                  placeholder={true}
+                  isListCard={true}
+                />
+              );
+            }
+            return (
+              <DSCard
+                key={`list-card-${rec.id}`}
+                pos={rec.pos}
+                flightId={rec.flight_id}
+                image_src={rec.image_src}
+                raw_image_src={rec.raw_image_src}
+                word_count={rec.word_count}
+                time_to_read={rec.time_to_read}
+                title={rec.title}
+                topic={rec.topic}
+                excerpt={rec.excerpt}
+                url={rec.url}
+                id={rec.id}
+                shim={rec.shim}
+                fetchTimestamp={rec.fetchTimestamp}
+                type={type}
+                context={rec.context}
+                sponsor={rec.sponsor}
+                sponsored_by_override={rec.sponsored_by_override}
+                dispatch={dispatch}
+                source={rec.domain}
+                publisher={rec.publisher}
+                pocket_id={rec.pocket_id}
+                context_type={rec.context_type}
+                bookmarkGuid={rec.bookmarkGuid}
+                recommendation_id={rec.recommendation_id}
+                firstVisibleTimestamp={firstVisibleTimestamp}
+                scheduled_corpus_item_id={rec.scheduled_corpus_item_id}
+                recommended_at={rec.recommended_at}
+                received_rank={rec.received_rank}
+                isListCard={true}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
