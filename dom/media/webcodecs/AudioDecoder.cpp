b93303e6c213180dc5aeeace8f7160c03cc100fb
Paul Adenot <paul@paul.cx>
2024-09-16 15:46:59 +0000

    Bug 1915143 - Reject opus decoding without description when there's more than 2 channels. r=chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D220431

--- dom/media/webcodecs/AudioDecoder.cpp
+++ dom/media/webcodecs/AudioDecoder.cpp
@@ -6,6 +6,7 @@
 
 #include "mozilla/dom/AudioDecoder.h"
 #include "mozilla/dom/AudioDecoderBinding.h"
+#include "mozilla/dom/TypedArray.h"
 
 #include "DecoderTraits.h"
 #include "MediaContainerType.h"
@@ -155,6 +156,7 @@ static bool CanDecodeAudio(const Config& aConfig) {
   if (IsOnAndroid() && IsAACCodecString(param.mParsedCodec)) {
     return false;
   }
+  bool typeSupported = false;
   // TODO: Instead of calling CanHandleContainerType with the guessed the
   // containers, DecoderTraits should provide an API to tell if a codec is
   // decodable or not.
@@ -164,11 +166,30 @@ static bool CanDecodeAudio(const Config& aConfig) {
       if (DecoderTraits::CanHandleContainerType(
               *containerType, nullptr /* DecoderDoctorDiagnostics */) !=
           CANPLAY_NO) {
-        return true;
+        typeSupported = true;
       }
     }
   }
-  return false;
+
+  if (!typeSupported) {
+    return false;
+  }
+
+  // Perform additional checks, often codec-specific.
+  // This is to error out only when attempting to `configure(...)` the decoder,
+  // not when calling `isConfigSupported(...)`
+  if constexpr (std::is_same_v<Config, AudioDecoderConfigInternal>) {
+    if (aConfig.mCodec.EqualsLiteral("opus")) {
+      if (aConfig.mNumberOfChannels > 2 &&
+          (!aConfig.mDescription || aConfig.mDescription->Length() < 10)) {
+        LOG("Opus needs a description of at least 10 bytes when decoding > 2 "
+            "channels");
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 static nsTArray<UniquePtr<TrackInfo>> GetTracksInfo(
