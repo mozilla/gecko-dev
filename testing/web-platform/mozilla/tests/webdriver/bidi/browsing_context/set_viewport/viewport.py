3ff5af8fa9ebcff4db17c0fc24c27f2ccf8ee217
Henrik Skupin <mail@hskupin.info>
2024-09-17 12:30:14 +0000

    Bug 1918287 - [wdspec] Test for "browsingContext.setViewport" to check it's not failing on browsing context replacement. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D222369

--- testing/web-platform/mozilla/tests/webdriver/bidi/browsing_context/set_viewport/viewport.py
+++ testing/web-platform/mozilla/tests/webdriver/bidi/browsing_context/set_viewport/viewport.py
@@ -1,4 +1,8 @@
+import asyncio
 import pytest
+
+from tests.bidi import get_viewport_dimensions
+
 
 pytestmark = pytest.mark.asyncio
 
@@ -16,3 +20,33 @@ async def test_params_viewport_max_value(bidi_session, new_tab, viewport):
     await bidi_session.browsing_context.set_viewport(
         context=new_tab["context"], viewport=viewport
     )
+
+
+@pytest.mark.parametrize("type_hint", ["tab", "window"])
+async def test_when_context_created(
+    bidi_session, wait_for_event, wait_for_future_safe, type_hint
+):
+    test_viewport = {"width": 499, "height": 599}
+
+    await bidi_session.session.subscribe(
+        events=["browsingContext.contextCreated"]
+    )
+    on_context_created = wait_for_event("browsingContext.contextCreated")
+
+    # Save the task to await for it later.
+    task = asyncio.create_task(
+        bidi_session.browsing_context.create(type_hint=type_hint)
+    )
+
+    context_info = await wait_for_future_safe(on_context_created)
+
+    # Bug 1918287 - Don't fail the command when the initial about:blank page is loaded
+    await bidi_session.browsing_context.set_viewport(
+        context=context_info["context"], viewport=test_viewport
+    )
+
+    new_tab = await task
+
+    assert await get_viewport_dimensions(bidi_session, new_tab) == test_viewport
+
+    await bidi_session.browsing_context.close(context=new_tab["context"])
