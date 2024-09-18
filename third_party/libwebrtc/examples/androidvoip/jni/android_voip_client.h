82c1c26836ca5b7b2235e5b50308daafc08c1746
Michael Froman <mfroman@mozilla.com>
2024-09-18 12:19:43 -0500

    Bug 1918268 - Vendor libwebrtc from 1ceff3d442
    Upstream commit: https://webrtc.googlesource.com/src/+/1ceff3d442782c950fc5efca9c715564d63f35c7
        Reland "Remove jni_zero type aliases in jni_generator_helper.h."
    
        This is a reland of commit 6cb7b3a424455f82ad5df78a7295a3e5253b1445
    
        Original change's description:
        > Reland "Remove jni_zero type aliases in jni_generator_helper.h."
        >
        > This is a reland of commit 9fcaa034bc032da9de5d6fcdd45528169f44d343
        >
        > Original change's description:
        > > Remove jni_zero type aliases in jni_generator_helper.h.
        > >
        > > This CL removes some type alias from
        > > sdk/android/src/jni/jni_generator_helper.h and make sure all the
        > > jni_zero types are referred to using the jni_zero:: namespace.
        > >
        > > The goal is to remove sdk/android/src/jni/jni_generator_helper.h
        > > in future CLs.
        > >
        > > Bug: b/319078685, b/351773023
        > > Change-Id: Ief60fce3e8f301f09ac5392d143aa5a82a445bcb
        > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/356882
        > > Reviewed-by: Zoé Lepaul <xalep@webrtc.org>
        > > Reviewed-by: Jeremy Leconte <jleconte@google.com>
        > > Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        > > Cr-Commit-Position: refs/heads/main@{#42618}
        >
        > No-Presubmit: True
        > Bug: b/319078685, b/351773023
        > Change-Id: I605a726788ee84efdbdc9e75098b404cbd7c1f30
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/358922
        > Reviewed-by: Zoé Lepaul <xalep@webrtc.org>
        > Reviewed-by: Christoffer Dewerin <jansson@webrtc.org>
        > Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        > Cr-Commit-Position: refs/heads/main@{#42751}
    
        No-Presubmit: True
        Bug: b/319078685, b/351773023
        Change-Id: I8294d8c5c49bc330d8158684a60fd4e6c88627d8
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/359320
        Reviewed-by: Zoé Lepaul <xalep@webrtc.org>
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Mohamed Heikal <mheikal@google.com>
        Reviewed-by: Christoffer Dewerin <jansson@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#42768}

--- third_party/libwebrtc/examples/androidvoip/jni/android_voip_client.h
+++ third_party/libwebrtc/examples/androidvoip/jni/android_voip_client.h
@@ -49,8 +49,8 @@ class AndroidVoipClient : public webrtc::Transport {
   // they are done with it (this class provides a Delete() method).
   static AndroidVoipClient* Create(
       JNIEnv* env,
-      const webrtc::JavaParamRef<jobject>& application_context,
-      const webrtc::JavaParamRef<jobject>& j_voip_client);
+      const jni_zero::JavaParamRef<jobject>& application_context,
+      const jni_zero::JavaParamRef<jobject>& j_voip_client);
 
   ~AndroidVoipClient() override;
 
@@ -66,22 +66,23 @@ class AndroidVoipClient : public webrtc::Transport {
 
   // Sets the encoder used by the VoIP API.
   void SetEncoder(JNIEnv* env,
-                  const webrtc::JavaParamRef<jstring>& j_encoder_string);
+                  const jni_zero::JavaParamRef<jstring>& j_encoder_string);
 
   // Sets the decoders used by the VoIP API.
   void SetDecoders(JNIEnv* env,
-                   const webrtc::JavaParamRef<jobject>& j_decoder_strings);
+                   const jni_zero::JavaParamRef<jobject>& j_decoder_strings);
 
   // Sets two local/remote addresses, one for RTP packets, and another for
   // RTCP packets. The RTP address will have IP address j_ip_address_string
   // and port number j_port_number_int, the RTCP address will have IP address
   // j_ip_address_string and port number j_port_number_int+1.
-  void SetLocalAddress(JNIEnv* env,
-                       const webrtc::JavaParamRef<jstring>& j_ip_address_string,
-                       jint j_port_number_int);
+  void SetLocalAddress(
+      JNIEnv* env,
+      const jni_zero::JavaParamRef<jstring>& j_ip_address_string,
+      jint j_port_number_int);
   void SetRemoteAddress(
       JNIEnv* env,
-      const webrtc::JavaParamRef<jstring>& j_ip_address_string,
+      const jni_zero::JavaParamRef<jstring>& j_ip_address_string,
       jint j_port_number_int);
 
   // Starts a VoIP session, then calls a callback method with a boolean
@@ -128,12 +129,12 @@ class AndroidVoipClient : public webrtc::Transport {
 
  private:
   AndroidVoipClient(JNIEnv* env,
-                    const webrtc::JavaParamRef<jobject>& j_voip_client)
+                    const jni_zero::JavaParamRef<jobject>& j_voip_client)
       : voip_thread_(rtc::Thread::CreateWithSocketServer()),
         j_voip_client_(env, j_voip_client) {}
 
   void Init(JNIEnv* env,
-            const webrtc::JavaParamRef<jobject>& application_context);
+            const jni_zero::JavaParamRef<jobject>& application_context);
 
   // Overloaded methods having native C++ variables as arguments.
   void SetEncoder(const std::string& encoder);
@@ -153,7 +154,7 @@ class AndroidVoipClient : public webrtc::Transport {
   std::unique_ptr<rtc::Thread> voip_thread_;
   // Reference to the VoipClient java instance used to
   // invoke callbacks when operations are finished.
-  webrtc::ScopedJavaGlobalRef<jobject> j_voip_client_
+  jni_zero::ScopedJavaGlobalRef<jobject> j_voip_client_
       RTC_GUARDED_BY(voip_thread_);
   // A list of AudioCodecSpec supported by the built-in
   // encoder/decoder factories.
