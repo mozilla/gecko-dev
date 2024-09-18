78d959f86656a4b737d805cbadfa26d64181b254
Matt Harding <majaharding@gmail.com>
2024-09-18 17:13:08 +0000

    Bug 1913613 - Fix CookieBannerChild.sys.mjs mixing up timeout IDs from different implementations r=pbz
    There are two implementations of (set|clear)(Timeout|Interval) at play here: a Javascript implementation
    from toolkit/modules/Timer.sys.mjs used by browser components, and a C++ implementation from
    dom/base/TimeoutManager.cpp that is exposed to web pages.
    
    CookieBannerChild.sys.mjs was creating timers with the C++ implementation, taking those IDs, and then
    trying to clear them in the Javascript implementation. The two implementations have individual ID namespaces
    so this resulted in unrelated timeouts being inadvertantly cancelled and the intended one continuing on.
    
    Differential Revision: https://phabricator.services.mozilla.com/D221261

--- toolkit/components/cookiebanners/CookieBannerChild.sys.mjs
+++ toolkit/components/cookiebanners/CookieBannerChild.sys.mjs
@@ -8,6 +8,7 @@ import { XPCOMUtils } from "resource://gre/modules/XPCOMUtils.sys.mjs";
 const lazy = {};
 
 ChromeUtils.defineESModuleGetters(lazy, {
+  setTimeout: "resource://gre/modules/Timer.sys.mjs",
   clearTimeout: "resource://gre/modules/Timer.sys.mjs",
   setInterval: "resource://gre/modules/Timer.sys.mjs",
   clearInterval: "resource://gre/modules/Timer.sys.mjs",
@@ -364,7 +365,7 @@ export class CookieBannerChild extends JSWindowActorChild {
       }
     );
 
-    this.#observerCleanUpTimer = this.contentWindow?.setTimeout(() => {
+    this.#observerCleanUpTimer = lazy.setTimeout(() => {
       lazy.logConsole.debug(
         "#startOrResetCleanupTimer: Cleanup timeout triggered",
         {
