3f84808593136771120ad07e2c243682d3606248
Michael Froman <mfroman@mozilla.com>
2024-09-18 12:28:51 -0500

    Bug 1918268 - Vendor libwebrtc from cfd83744d9
    Upstream commit: https://webrtc.googlesource.com/src/+/cfd83744d93dbf7a6932c66e88cc457f35082598
        Misc OpenSSL fixes
    
        improving the error message from PEM parsing and adding a few DCHECKs
        Tested locally with OpenSSL 3.x
    
        BUG=webrtc:42225468
    
        Change-Id: Ia2ff1e5826f486060db73bee979e2703fc6c5823
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/358441
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Philipp Hancke <phancke@meta.com>
        Reviewed-by: David Benjamin <davidben@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#42776}

--- third_party/libwebrtc/rtc_base/ssl_stream_adapter_unittest.cc
+++ third_party/libwebrtc/rtc_base/ssl_stream_adapter_unittest.cc
@@ -493,6 +493,8 @@ class SSLStreamAdapterTestBase : public ::testing::Test,
             : rtc::SSLPeerCertificateDigestError::VERIFICATION_FAILED;
 
     RTC_LOG(LS_INFO) << "Setting peer identities by digest";
+    RTC_DCHECK(server_identity());
+    RTC_DCHECK(client_identity());
 
     rv = server_identity()->certificate().ComputeDigest(
         digest_algorithm_, server_digest, digest_length_, &server_digest_len);
