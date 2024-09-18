a174e516ccacb748094df064ffaec06c7a7e1e07
Sean <sekim@mozilla.com>
2024-09-18 23:23:16 +0000

    Bug 1768724 - Part 1: Add a preference and policy for Microsoft SSO on macOS (Non-Necko Part) r=fluent-reviewers,mkaply,mossop,bolsson
    Differential Revision: https://phabricator.services.mozilla.com/D212445

--- browser/components/enterprisepolicies/Policies.sys.mjs
+++ browser/components/enterprisepolicies/Policies.sys.mjs
@@ -1638,6 +1638,12 @@ export var Policies = {
     },
   },
 
+  MicrosoftEntraSSO: {
+    onBeforeAddons(manager, param) {
+      setAndLockPref("network.http.microsoft-entra-sso.enabled", param);
+    },
+  },
+
   NetworkPrediction: {
     onBeforeAddons(manager, param) {
       setAndLockPref("network.dns.disablePrefetch", !param);
