ede1c4e4ff86b1900086a3549161e5d0ba045ad0
Sean <sekim@mozilla.com>
2024-09-20 16:32:32 +0000

    Bug 1905939 - Migrate HTTP3_0RTT_STATE and HTTP3_0RTT_STATE_DURATION probes to glean r=necko-reviewers,kershaw,djackson
    Differential Revision: https://phabricator.services.mozilla.com/D216204

--- netwerk/protocol/http/Http3Session.cpp
+++ netwerk/protocol/http/Http3Session.cpp
@@ -19,6 +19,7 @@
 #include "mozilla/RandomNum.h"
 #include "mozilla/RefPtr.h"
 #include "mozilla/Telemetry.h"
+#include "mozilla/glean/GleanMetrics.h"
 #include "mozilla/net/DNS.h"
 #include "nsHttpHandler.h"
 #include "nsIHttpActivityObserver.h"
@@ -211,9 +212,11 @@ nsresult Http3Session::Init(const nsHttpConnectionInfo* aConnInfo,
     }
   }
 
+#ifndef ANDROID
   if (mState != ZERORTT) {
     ZeroRttTelemetry(ZeroRttOutcome::NOT_USED);
   }
+#endif
 
   auto config = mConnInfo->GetEchConfig();
   if (config.IsEmpty()) {
@@ -367,8 +370,9 @@ void Http3Session::Shutdown() {
 
 Http3Session::~Http3Session() {
   LOG3(("Http3Session::~Http3Session %p", this));
-
+#ifndef ANDROID
   EchOutcomeTelemetry();
+#endif
   Telemetry::Accumulate(Telemetry::HTTP3_REQUEST_PER_CONN, mTransactionCount);
   Telemetry::Accumulate(Telemetry::HTTP3_BLOCKED_BY_STREAM_LIMIT_PER_CONN,
                         mBlockedByStreamLimitCount);
@@ -597,7 +601,9 @@ nsresult Http3Session::ProcessEvents() {
           mState = INITIALIZING;
           mTransactionCount = 0;
           Finish0Rtt(true);
+#ifndef ANDROID
           ZeroRttTelemetry(ZeroRttOutcome::USED_REJECTED);
+#endif
         }
         break;
       case Http3Event::Tag::ResumptionToken: {
@@ -621,7 +627,9 @@ nsresult Http3Session::ProcessEvents() {
         mSocketControl->HandshakeCompleted();
         if (was0RTT) {
           Finish0Rtt(false);
+#ifndef ANDROID
           ZeroRttTelemetry(ZeroRttOutcome::USED_SUCCEEDED);
+#endif
         }
 
         OnTransportStatus(nullptr, NS_NET_STATUS_CONNECTED_TO, 0);
@@ -1734,10 +1742,12 @@ void Http3Session::CloseInternal(bool aCallNeqoClose) {
     mBeforeConnectedError = true;
   }
 
+#ifndef ANDROID
   if (mState == ZERORTT) {
     ZeroRttTelemetry(aCallNeqoClose ? ZeroRttOutcome::USED_CONN_CLOSED_BY_NECKO
                                     : ZeroRttOutcome::USED_CONN_ERROR);
   }
+#endif
 
   mState = CLOSING;
   Shutdown();
@@ -2486,6 +2496,7 @@ void Http3Session::ReportHttp3Connection() {
   }
 }
 
+#ifndef ANDROID
 void Http3Session::EchOutcomeTelemetry() {
   MOZ_ASSERT(OnSocketThread(), "not on socket thread");
 
@@ -2507,8 +2518,6 @@ void Http3Session::EchOutcomeTelemetry() {
 }
 
 void Http3Session::ZeroRttTelemetry(ZeroRttOutcome aOutcome) {
-  Telemetry::Accumulate(Telemetry::HTTP3_0RTT_STATE, aOutcome);
-
   nsAutoCString key;
 
   switch (aOutcome) {
@@ -2528,12 +2537,18 @@ void Http3Session::ZeroRttTelemetry(ZeroRttOutcome aOutcome) {
       break;
   }
 
-  if (!key.IsEmpty()) {
+  if (key.IsEmpty()) {
+    mozilla::glean::netwerk::http3_0rtt_state.Get("not_used"_ns).Add(1);
+  } else {
     MOZ_ASSERT(mZeroRttStarted);
-    Telemetry::AccumulateTimeDelta(Telemetry::HTTP3_0RTT_STATE_DURATION, key,
-                                   mZeroRttStarted, TimeStamp::Now());
+    mozilla::TimeStamp zeroRttEnded = mozilla::TimeStamp::Now();
+    mozilla::glean::netwerk::http3_0rtt_state_duration.Get(key)
+        .AccumulateRawDuration(zeroRttEnded - mZeroRttStarted);
+
+    mozilla::glean::netwerk::http3_0rtt_state.Get(key).Add(1);
   }
 }
+#endif
 
 nsresult Http3Session::GetTransactionTLSSocketControl(
     nsITLSSocketControl** tlsSocketControl) {
