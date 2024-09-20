6acc36c26b620ac74bc726be97917fbf2e9cf357
Fatih <fkilic@mozilla.com>
2024-09-20 21:11:45 +0000

    Bug 1507280: Change GetLocalizedString to GetMaybeLocalizedString in deprecation warnings. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D222633

--- dom/bindings/BindingUtils.cpp
+++ dom/bindings/BindingUtils.cpp
@@ -4046,7 +4046,7 @@ static const char* kDeprecatedOperations[] = {
     nullptr};
 #undef DEPRECATED_OPERATION
 
-void ReportDeprecation(nsIGlobalObject* aGlobal, nsIURI* aURI,
+void ReportDeprecation(nsIGlobalObject* aGlobal, Document* aDoc, nsIURI* aURI,
                        DeprecatedOperations aOperation,
                        const nsACString& aFileName,
                        const Nullable<uint32_t>& aLineNumber,
@@ -4070,8 +4070,8 @@ void ReportDeprecation(nsIGlobalObject* aGlobal, nsIURI* aURI,
   key.AppendASCII("Warning");
 
   nsAutoString msg;
-  rv = nsContentUtils::GetLocalizedString(nsContentUtils::eDOM_PROPERTIES,
-                                          key.get(), msg);
+  rv = nsContentUtils::GetMaybeLocalizedString(nsContentUtils::eDOM_PROPERTIES,
+                                               key.get(), aDoc, msg);
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return;
   }
@@ -4133,7 +4133,7 @@ void MaybeShowDeprecationWarning(const GlobalObject& aGlobal,
 void MaybeReportDeprecation(const GlobalObject& aGlobal,
                             DeprecatedOperations aOperation) {
   nsCOMPtr<nsIURI> uri;
-
+  nsCOMPtr<Document> doc;
   if (NS_IsMainThread()) {
     nsCOMPtr<nsPIDOMWindowInner> window =
         do_QueryInterface(aGlobal.GetAsSupports());
@@ -4141,7 +4141,8 @@ void MaybeReportDeprecation(const GlobalObject& aGlobal,
       return;
     }
 
-    uri = window->GetExtantDoc()->GetDocumentURI();
+    doc = window->GetExtantDoc();
+    uri = doc->GetDocumentURI();
   } else {
     WorkerPrivate* workerPrivate =
         GetWorkerPrivateFromContext(aGlobal.Context());
@@ -4167,8 +4168,8 @@ void MaybeReportDeprecation(const GlobalObject& aGlobal,
   nsCOMPtr<nsIGlobalObject> global = do_QueryInterface(aGlobal.GetAsSupports());
   MOZ_ASSERT(global);
 
-  ReportDeprecation(global, uri, aOperation, location.FileName(), lineNumber,
-                    columnNumber);
+  ReportDeprecation(global, doc, uri, aOperation, location.FileName(),
+                    lineNumber, columnNumber);
 }
 
 }  // anonymous namespace
