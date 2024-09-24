2b0c9155e500b4297418a38c80029930834c8cd7
Valentin Gosu <valentin.gosu@gmail.com>
2024-09-24 12:29:08 +0000

    Bug 1900561 - Add fetchPriority to classOfService r=acreskey,necko-reviewers,kershaw
    Knowing the actual fetchPriority of the initial request is useful information
    when computing the urgency. This allows us to not worry about the priority
    being clobbered by another caller of nsISupportsPriority.
    
    Differential Revision: https://phabricator.services.mozilla.com/D221957

--- netwerk/protocol/http/ClassOfService.h
+++ netwerk/protocol/http/ClassOfService.h
@@ -7,13 +7,12 @@
 
 #include "nsIClassOfService.h"
 #include "nsPrintfCString.h"
-#include "ipc/IPCMessageUtils.h"
 
 namespace mozilla::net {
 
 class ClassOfService {
  public:
-  ClassOfService() : mClassFlags(0), mIncremental(false) {}
+  ClassOfService() = default;
   ClassOfService(unsigned long flags, bool incremental)
       : mClassFlags(flags), mIncremental(incremental) {}
 
@@ -25,6 +24,11 @@ class ClassOfService {
   bool Incremental() const { return mIncremental; }
   void SetIncremental(bool incremental) { mIncremental = incremental; }
 
+  nsIClassOfService::FetchPriority FetchPriority() { return mFetchPriority; }
+  void SetFetchPriority(nsIClassOfService::FetchPriority aPriority) {
+    mFetchPriority = aPriority;
+  }
+
   static void ToString(const ClassOfService aCos, nsACString& aOut) {
     return ToString(aCos.Flags(), aOut);
   }
@@ -34,8 +38,10 @@ class ClassOfService {
   }
 
  private:
-  unsigned long mClassFlags;
-  bool mIncremental;
+  unsigned long mClassFlags = 0;
+  bool mIncremental = false;
+  nsIClassOfService::FetchPriority mFetchPriority =
+      nsIClassOfService::FETCHPRIORITY_UNSET;
   friend IPC::ParamTraits<mozilla::net::ClassOfService>;
   friend bool operator==(const ClassOfService& lhs, const ClassOfService& rhs);
   friend bool operator!=(const ClassOfService& lhs, const ClassOfService& rhs);
@@ -43,7 +49,8 @@ class ClassOfService {
 
 inline bool operator==(const ClassOfService& lhs, const ClassOfService& rhs) {
   return lhs.mClassFlags == rhs.mClassFlags &&
-         lhs.mIncremental == rhs.mIncremental;
+         lhs.mIncremental == rhs.mIncremental &&
+         lhs.mFetchPriority == rhs.mFetchPriority;
 }
 
 inline bool operator!=(const ClassOfService& lhs, const ClassOfService& rhs) {
@@ -51,26 +58,5 @@ inline bool operator!=(const ClassOfService& lhs, const ClassOfService& rhs) {
 }
 
 }  // namespace mozilla::net
-
-namespace IPC {
-template <>
-struct ParamTraits<mozilla::net::ClassOfService> {
-  typedef mozilla::net::ClassOfService paramType;
-
-  static void Write(MessageWriter* aWriter, const paramType& aParam) {
-    WriteParam(aWriter, aParam.mClassFlags);
-    WriteParam(aWriter, aParam.mIncremental);
-  }
-
-  static bool Read(MessageReader* aReader, paramType* aResult) {
-    if (!ReadParam(aReader, &aResult->mClassFlags) ||
-        !ReadParam(aReader, &aResult->mIncremental))
-      return false;
-
-    return true;
-  }
-};
-
-}  // namespace IPC
 
 #endif
