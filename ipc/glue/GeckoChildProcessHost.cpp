b53bf33baee1afcde8fc2d9c3e482903705226c8
Nika Layzell <nika@thelayzells.com>
2024-09-24 19:28:11 +0000

    Bug 1440207 - Part 3f: Windows support for file geckoargs, r=ipc-reviewers,mccr8
    Windows is the simplest platform to support, as HANDLEs are inherited by
    identity into the child process, and there's less variance between different
    OSes.
    
    This effectively re-implements the existing behaviour used for HANDLE
    passing on Windows by passing the handle directly on the command line.
    
    Differential Revision: https://phabricator.services.mozilla.com/D221376

--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -1699,6 +1699,11 @@ Result<Ok, LaunchError> WindowsProcessLauncher::DoSetup() {
   // Process type
   mCmdLine->AppendLooseValue(UTF8ToWide(ChildProcessType()));
 
+  // Add any files which need to be transferred to handles_to_inherit.
+  for (auto& file : mChildArgs.mFiles) {
+    mLaunchOptions->handles_to_inherit.push_back(file.get());
+  }
+
 #  ifdef MOZ_SANDBOX
   if (mUseSandbox) {
     // Mark the handles to inherit as inheritable.
