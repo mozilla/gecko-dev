76cf4a997322b42075b8998079f61a595967d55f
Chun-Min Chang <chun.m.chang@gmail.com>
2024-09-24 23:04:42 +0000

    Bug 1920740 - Remove empty comment r=media-playback-reviewers,aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D222883

--- dom/media/platforms/apple/AppleUtils.h
+++ dom/media/platforms/apple/AppleUtils.h
@@ -10,7 +10,7 @@
 #include "mozilla/Attributes.h"
 #include <CoreFoundation/CFBase.h>  // For CFRelease()
 #include <CoreVideo/CVBuffer.h>     // For CVBufferRelease()
-                                    //
+
 #if TARGET_OS_IPHONE
 inline bool OSSupportsSVC() {
   // TODO
