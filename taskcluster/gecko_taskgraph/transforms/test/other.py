a4171b71af250b3c26ef4d5b256aef171ef5355d
Greg Mierzwinski <gmierz2@outlook.com>
2024-09-24 19:32:06 +0000

    Bug 1919760 - Update chromedriver to v129. r=perftest-reviewers,taskgraph-reviewers,kshampur,ahal DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D222779

--- taskcluster/gecko_taskgraph/transforms/test/other.py
+++ taskcluster/gecko_taskgraph/transforms/test/other.py
@@ -395,20 +395,20 @@ def setup_browsertime(config, tasks):
 
         cd_fetches = {
             "android.*": [
-                "linux64-chromedriver-127",
                 "linux64-chromedriver-128",
+                "linux64-chromedriver-129",
             ],
             "linux.*": [
                 "linux64-chromedriver-127",
-                "linux64-chromedriver-128",
+                "linux64-chromedriver-129",
             ],
             "macosx1015.*": [
-                "mac64-chromedriver-127",
                 "mac64-chromedriver-128",
+                "mac64-chromedriver-129",
             ],
             "macosx1400.*": [
-                "mac-arm-chromedriver-127",
                 "mac-arm-chromedriver-128",
+                "mac-arm-chromedriver-129",
             ],
             "windows.*aarch64.*": [
                 "win32-chromedriver-121",
@@ -416,8 +416,8 @@ def setup_browsertime(config, tasks):
                 "win32-chromedriver-123",
             ],
             "windows.*-64.*": [
-                "win64-chromedriver-127",
                 "win64-chromedriver-128",
+                "win64-chromedriver-129",
             ],
         }
 
