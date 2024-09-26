c0983f0bd554cd8a204223b2a1f72717155c75c5
Andrew Creskey <acreskey@mozilla.com>
2024-09-26 10:29:41 +0000

    Bug 1920222 - Modify the fetchpriority adjustments for <img> elements so that priority="low" is given a lower priority than auto / no attribute r=necko-reviewers,fredw,kershaw
    Set adjustment for fetchpriority="low" on images to deprioritize so developers can reduce bandwidth shared to tagged images.
    
    Differential Revision: https://phabricator.services.mozilla.com/D223003

--- testing/web-platform/mozilla/tests/fetch/fetchpriority/support/image-tests-data.js
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/image-tests-data.js
@@ -6,7 +6,7 @@ export const kTestFolderName = "image-tests";
 // (https://web.dev/articles/fetch-priority#browser_priority_and_fetchpriority).
 const kExpectedRequestsOfInitialLoad = [
     {   fileNameAndSuffix: "square_25px_x_25px.png?1",
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOWEST
     },
     {   fileNameAndSuffix: "square_25px_x_25px.png?2",
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
