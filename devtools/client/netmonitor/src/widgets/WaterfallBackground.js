6a510b95d3391eaae976f05b88f31188c364dc1c
Iulian Moraru <imoraru@mozilla.com>
2024-09-26 16:43:23 +0300

    Backed out changeset d2cab005f94a (bug 1916355) for causing dt failures on browser_parsable_css.js. CLOSED TREE

--- devtools/client/netmonitor/src/widgets/WaterfallBackground.js
+++ devtools/client/netmonitor/src/widgets/WaterfallBackground.js
@@ -155,7 +155,7 @@ class WaterfallBackground {
       colorVariableName,
       document.ownerGlobal
     );
-    const { r, g, b, a } = InspectorUtils.colorToRGBA(colorStr, document);
+    const { r, g, b, a } = InspectorUtils.colorToRGBA(colorStr);
     return [r, g, b, a * 255];
   }
 
