735d4aa5d5d11ac54f2eb24849bc81d404ab6c64
Jan Varga <jvarga@mozilla.com>
2024-09-30 17:01:49 +0000

    Bug 1919428 - Add a test for origin clearing requested right after starting database work for SDB; r=dom-storage-reviewers,jari
    Differential Revision: https://phabricator.services.mozilla.com/D220109

---
+++ dom/simpledb/test/xpcshell/test_slowDatabaseInitialization.js
@@ -0,0 +1,73 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+const { PrincipalUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/dom/quota/test/modules/PrincipalUtils.sys.mjs"
+);
+const { QuotaUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/dom/quota/test/modules/QuotaUtils.sys.mjs"
+);
+const { SimpleDBUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/dom/simpledb/test/modules/SimpleDBUtils.sys.mjs"
+);
+const { TestUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/TestUtils.sys.mjs"
+);
+
+add_task(
+  {
+    pref_set: [["dom.simpledb.databaseInitialization.pauseOnIOThreadMs", 2000]],
+  },
+  async function testSteps() {
+    const principal = PrincipalUtils.createPrincipal("https://example.com");
+    const name = "test_slowStorageInitialization.js";
+
+    info("Testing origin clearing requested after starting database work");
+
+    info("Starting database opening");
+
+    const [dbConnection, openPromise] = (function () {
+      const connection = SimpleDBUtils.createConnection(principal);
+      const request = connection.open(name);
+      const promise = SimpleDBUtils.requestFinished(request);
+      return [connection, promise];
+    })();
+
+    info("Waiting for database work to start");
+
+    await TestUtils.topicObserved("SimpleDB::DatabaseWorkStarted");
+
+    info("Starting origin clearing");
+
+    const clearPromise = (async function () {
+      const request = Services.qms.clearStoragesForPrincipal(principal);
+      const promise = QuotaUtils.requestFinished(request);
+      return promise;
+    })();
+
+    info("Waiting for database to finish opening");
+
+    // XXX This should throw!
+    await openPromise;
+
+    info("Waiting for origin to finish clearing");
+
+    await clearPromise;
+
+    info("Reading from the database");
+
+    try {
+      dbConnection.seek(0);
+      ok(false, "Should have thrown");
+    } catch (e) {
+      ok(true, "Should have thrown");
+      Assert.strictEqual(
+        e.result,
+        Cr.NS_ERROR_ABORT,
+        "Threw right result code"
+      );
+    }
+  }
+);
