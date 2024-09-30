04045ee31525fd377b88d5812a4289a8643423d6
Tim Huang <tihuang@mozilla.com>
2024-09-30 15:04:03 +0000

    Bug 1921491 - Disable QueryParameterStripping in Nightly. r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D223975

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2175,11 +2175,6 @@ pref("privacy.trackingprotection.cryptomining.enabled", true);
 
 pref("browser.contentblocking.database.enabled", true);
 
-// Enable URL query stripping in Nightly.
-#ifdef NIGHTLY_BUILD
-pref("privacy.query_stripping.enabled", true);
-#endif
-
 // Enable Strip on Share by default on desktop
 pref("privacy.query_stripping.strip_on_share.enabled", true);
 
