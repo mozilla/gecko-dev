859cd830958b2d68a3913385d2af541996a8c41b
Frédéric Wang <fwang@igalia.com>
2024-09-30 12:18:38 +0000

    Bug 1916988 - Set vertical offset of mfrac denominator from the baseline. r=emilio
    Currently, we have
    
    ```
    aDesiredSize.Height() = aDesiredSize.BlockStartAscent() +
      sizeDen.Height() + denMargin.bottom - sizeDen.BlockStartAscent() +
      denShift;
    ```
    
    Then a call to `InflateReflowAndBoundingMetrics` respectively increments
    `aDesiredSize.BlockStartAscent()` and `aDesiredSize.Height()` by
    `borderPadding.top` and `borderPadding.top + borderPadding.bottom`.
    
    Finally, we set
    
    ```
    dy = aDesiredSize.Height() - sizeDen.Height() - denMargin.bottom -
         borderPadding.bottom;
    ```
    
    which is equivalent to
    
    ```
    aDesiredSize.BlockStartAscent() + denShift - sizeDen.BlockStartAscent()
    ```
    
    Differential Revision: https://phabricator.services.mozilla.com/D222456

--- layout/mathml/nsMathMLmfracFrame.cpp
+++ layout/mathml/nsMathMLmfracFrame.cpp
@@ -377,8 +377,8 @@ nsresult nsMathMLmfracFrame::Place(DrawTarget* aDrawTarget,
                       ReflowChildFlags::Default);
     // place denominator
     dxDen += denMargin.left;
-    dy = aDesiredSize.Height() - sizeDen.Height() - denMargin.bottom -
-         borderPadding.bottom;
+    dy =
+        aDesiredSize.BlockStartAscent() + denShift - sizeDen.BlockStartAscent();
     FinishReflowChild(frameDen, presContext, sizeDen, nullptr, dxDen, dy,
                       ReflowChildFlags::Default);
     // place the fraction bar - dy is top of bar
