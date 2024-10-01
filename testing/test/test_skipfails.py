98aab8bfe5f8039ead13a23a057fb12130748302
Joel Maher <jmaher@mozilla.com>
2024-10-01 11:23:27 +0000

    Bug 1920961 - remove line number tests from skip-fails. r=aryx a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D224166

--- testing/test/test_skipfails.py
+++ testing/test/test_skipfails.py
@@ -888,7 +888,7 @@ def test_reftest_get_lineno():
     ]
     allmods = " ".join(mods)
     lineno = sf.reftest_find_lineno(
-        "layout/reftests/font-face/reftest.list", mods, allmods
+        "testing/test/data/fontface_reftest.list", mods, allmods
     )
     assert lineno == 171
 
@@ -909,7 +909,7 @@ def test_reftest_get_lineno2():
     ]
     allmods = " ".join(mods)
     lineno = sf.reftest_find_lineno(
-        "image/test/reftest/downscaling/reftest.list", mods, allmods
+        "testing/test/data/downscaling_reftest.list", mods, allmods
     )
     assert lineno == 183
 
@@ -928,7 +928,7 @@ def test_reftest_get_lineno3():
     ]
     allmods = " ".join(mods)
     lineno = sf.reftest_find_lineno(
-        "dom/canvas/test/reftest/reftest.list", mods, allmods
+        "testing/test/data/dom_canvas_reftest.list", mods, allmods
     )
     assert lineno == 233
 
