4e1c74fc93751941081040bb1ce557af66a2e4eb
Mugurell <Mugurell@users.noreply.github.com>
2024-10-01 12:39:21 +0000

    Bug 1918416 - part 1 - Prevent leaking DynamicDownloadDialogBehaviors r=android-reviewers,skhan a=RyanVM
    For each new download dialog to be shown we set a new DynamicDownloadDialogBehavior.
    But a previous one may now be leaked because it continues to observe store updates and
    reacting to when the orientation changes.
    The leak is made possible because the "download complete dialog" is always attached to
    the displayed layout, only it's visibility is updated depending on if it needs to be shown.
    
    Differential Revision: https://phabricator.services.mozilla.com/D222281

--- mobile/android/fenix/app/src/test/java/org/mozilla/fenix/downloads/dialog/DynamicDownloadDialogTest.kt
+++ mobile/android/fenix/app/src/test/java/org/mozilla/fenix/downloads/dialog/DynamicDownloadDialogTest.kt
@@ -4,19 +4,37 @@
 
 package org.mozilla.fenix.downloads.dialog
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.webkit.MimeTypeMap
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import io.mockk.every
+import io.mockk.mockk
 import mozilla.components.browser.state.state.content.DownloadState
 import mozilla.components.support.test.robolectric.testContext
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertSame
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mozilla.fenix.R
+import org.mozilla.fenix.databinding.DownloadDialogLayoutBinding
 import org.mozilla.fenix.downloads.dialog.DynamicDownloadDialog.Companion.getCannotOpenFileErrorMessage
+import org.mozilla.fenix.ext.components
+import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.helpers.FenixRobolectricTestRunner
 import org.robolectric.Shadows.shadowOf
 
 @RunWith(FenixRobolectricTestRunner::class)
 class DynamicDownloadDialogTest {
+
+    @Before
+    fun setUp() {
+        every { testContext.settings() } returns mockk(relaxed = true)
+        every { testContext.components.appStore } returns mockk(relaxed = true)
+    }
 
     @Test
     fun `WHEN calling getCannotOpenFileErrorMessage THEN should return the error message for the download file type`() {
@@ -34,4 +52,72 @@ class DynamicDownloadDialogTest {
         val result = getCannotOpenFileErrorMessage(testContext, download)
         assertEquals(expected, result)
     }
+
+    @Test
+    fun `GIVEN a custom behavior not set WHEN showing a new download complete dialog THEN set a new behavior`() {
+        val binding = createDynamicDownloadBinding()
+        val dialog = createDynamicDownloadDialog(binding = binding)
+
+        assertNull(binding.behavior)
+
+        dialog.show()
+
+        assertTrue(binding.behavior is DynamicDownloadDialogBehavior)
+    }
+
+    @Test
+    fun `GIVEN a custom behavior already set WHEN showing a new download complete dialog THEN don't set a new behavior`() {
+        val binding = createDynamicDownloadBinding()
+        val dialog = createDynamicDownloadDialog(binding = binding)
+
+        dialog.show()
+        val initialBehavior = binding.behavior
+
+        dialog.show()
+
+        assertSame(initialBehavior, binding.behavior)
+    }
+
+    @Test
+    fun `GIVEN a custom behavior already set WHEN dismissing the download complete dialog THEN remove any set behaviors`() {
+        val binding = createDynamicDownloadBinding()
+        val dialog = createDynamicDownloadDialog(binding = binding)
+        dialog.show()
+
+        dialog.dismiss()
+
+        assertNull(binding.behavior)
+    }
+
+    private val DownloadDialogLayoutBinding.behavior
+        get() = (root.layoutParams as? CoordinatorLayout.LayoutParams)?.behavior
+
+    private fun createDynamicDownloadBinding(): DownloadDialogLayoutBinding {
+        val downloadDialog = LayoutInflater.from(testContext).inflate(
+            R.layout.download_dialog_layout,
+            CoordinatorLayout(testContext),
+            false,
+        )
+        CoordinatorLayout(testContext).addView(downloadDialog)
+
+        return DownloadDialogLayoutBinding.bind(downloadDialog)
+    }
+
+    private fun createDynamicDownloadDialog(
+        context: Context = testContext,
+        downloadState: DownloadState? = mockk(relaxed = true),
+        didFail: Boolean = false,
+        tryAgain: (String) -> Unit = { },
+        onCannotOpenFile: (DownloadState) -> Unit = { },
+        onDismiss: () -> Unit = { },
+        binding: DownloadDialogLayoutBinding,
+    ) = DynamicDownloadDialog(
+        context = context,
+        downloadState = downloadState,
+        didFail = didFail,
+        tryAgain = tryAgain,
+        onCannotOpenFile = onCannotOpenFile,
+        binding = binding,
+        onDismiss = onDismiss,
+    )
 }
