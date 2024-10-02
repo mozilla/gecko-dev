54dae7614eed488bedca4973a83a77ad0ee4352d
Iulian Moraru <imoraru@mozilla.com>
2024-10-02 05:04:06 +0300

    Backed out changeset 572fcd78f07f (bug 1316571) for causing android Gtest failures. CLOSED TREE

--- dom/media/mediasink/VideoSink.h
+++ dom/media/mediasink/VideoSink.h
@@ -84,13 +84,13 @@ class VideoSink : public MediaSink {
 
   void EnsureHighResTimersOnOnlyIfPlaying();
 
-  // Sets images and frame dimensions into the VideoFrameContainer. Called on
-  // the shared state machine thread.
+  // Sets VideoQueue images into the VideoFrameContainer. Called on the shared
+  // state machine thread. The first aMaxFrames (at most) are set.
   // aClockTime and aClockTimeStamp are used as the baseline for deriving
   // timestamps for the frames.
-  // If aFrames is empty, this does nothing.
-  void RenderVideoFrames(Span<const RefPtr<VideoData>> aFrames,
-                         int64_t aClockTime, const TimeStamp& aClockTimeStamp);
+  // If the VideoQueue is empty, this does nothing.
+  void RenderVideoFrames(int32_t aMaxFrames, int64_t aClockTime,
+                         const TimeStamp& aClockTimeStamp);
 
   // Triggered while videosink is started, videosink becomes "playing" status,
   // or VideoQueue event arrived.
