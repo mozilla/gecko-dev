6f46d58a91a41c7e3977a18bd00a48184d3a1481
ahochheiden <ahochheiden@mozilla.com>
2024-10-03 17:28:17 +0000

    Bug 1920152 - Prevent the absolute path from being written in `requirements.txt` and re-vendor r=ahal,mach-reviewers
    We can continue using `-o` to generate the `requiriements.txt` file by just using the filename instead of the absolute path. This works because we're already setting the `cwd` to be where it needs to be generated. This makes the comment at the top of the generated `requirements.txt` file always be the same, regardless of who ran the vendor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D224508

--- python/mozbuild/mozbuild/vendor/vendor_python.py
+++ python/mozbuild/mozbuild/vendor/vendor_python.py
@@ -59,7 +59,8 @@ class VendorPython(MozbuildObject):
         self.log_manager.enable_unstructured()
 
         vendor_dir = Path(self.topsrcdir) / "third_party" / "python"
-        requirements_txt = vendor_dir / "requirements.txt"
+        requirements_file_name = "requirements.txt"
+        requirements_path = vendor_dir / requirements_file_name
         uv_lock_file = vendor_dir / "uv.lock"
         vendored_lock_file_hash_file = vendor_dir / "uv.lock.hash"
 
@@ -109,7 +110,7 @@ class VendorPython(MozbuildObject):
                 "--format",
                 "requirements-txt",
                 "-o",
-                requirements_txt,
+                requirements_file_name,
                 "-q",
             ],
             cwd=vendor_dir,
@@ -122,7 +123,7 @@ class VendorPython(MozbuildObject):
         # fetch them as requested, or intelligently raise an error if that's not
         # possible (e.g.: if different versions of Python would result in different
         # packages/package versions).
-        remove_environment_markers_from_requirements_txt(requirements_txt)
+        remove_environment_markers_from_requirements_txt(requirements_path)
 
         with TemporaryDirectory() as tmp:
             # use requirements.txt to download archived source distributions of all
@@ -134,7 +135,7 @@ class VendorPython(MozbuildObject):
                     "pip",
                     "download",
                     "-r",
-                    str(requirements_txt),
+                    str(requirements_path),
                     "--no-deps",
                     "--dest",
                     tmp,
