0e27a4fc22b80073d220ac6a42d8d20099e8bbe9
Sam Foster <sfoster@mozilla.com>
2024-10-03 21:42:05 +0000

    Bug 1922546 - Add back the z-index for #navigator-toolbox. r=emilio,desktop-theme-reviewers,sidebar-reviewers,nsharpley
    * Adds a z-index for the #navigator-toolbox (with and without sidebar.verticalTabs)
    * This will regress Bug 1921811, and will need follow-up in bug Bug 1921257
    
    Differential Revision: https://phabricator.services.mozilla.com/D224521

---
+++ browser/components/sidebar/tests/browser/browser_vertical_tabs_stacking_context.js
@@ -0,0 +1,44 @@
+/* Any copyright is dedicated to the Public Domain.
+   https://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Check that when enabling vertical tabs, we can still receive a click on the urlbar results view
+ */
+add_task(async function test_click_urlbar_results() {
+  await SpecialPowers.pushPrefEnv({ set: [["sidebar.verticalTabs", true]] });
+
+  await TestUtils.waitForCondition(() => {
+    return BrowserTestUtils.isVisible(document.querySelector("sidebar-main"));
+  }, "The new sidebar is shown.");
+
+  await BrowserTestUtils.withNewTab(
+    {
+      url: "about:blank",
+      gBrowser,
+    },
+    async () => {
+      let urlbarResultsElem = document.querySelector("#urlbar .urlbarView");
+
+      EventUtils.synthesizeMouseAtCenter(
+        document.querySelector("#urlbar .urlbar-input-box"),
+        {}
+      );
+      await TestUtils.waitForCondition(() => {
+        return BrowserTestUtils.isVisible(urlbarResultsElem);
+      });
+
+      let promiseClicked = BrowserTestUtils.waitForEvent(
+        urlbarResultsElem,
+        "click",
+        true
+      );
+
+      EventUtils.synthesizeMouseAtCenter(urlbarResultsElem, {});
+
+      await promiseClicked;
+      Assert.ok(true, "urlbar results view received a click");
+    }
+  );
+});
