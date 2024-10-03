8fcbda187e0671f1a4ac077d93584ced62213887
Jan Varga <jvarga@mozilla.com>
2024-10-03 22:38:28 +0000

    Bug 1919556 - Add a test for origin clearing requested right after starting database work for FS; r=dom-storage-reviewers,jari
    Differential Revision: https://phabricator.services.mozilla.com/D220916

---
+++ dom/fs/test/xpcshell/test_slowDatabaseInitialization.js
@@ -0,0 +1,69 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+// This test doesn't use the shared module system (running the same test in
+// multiple test suites) on purpose because it needs to create an unprivileged
+// sandbox which is not possible if the test is already running in a sandbox.
+
+const { PrincipalUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/dom/quota/test/modules/PrincipalUtils.sys.mjs"
+);
+const { QuotaUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/dom/quota/test/modules/QuotaUtils.sys.mjs"
+);
+const { TestUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/TestUtils.sys.mjs"
+);
+
+add_task(
+  { pref_set: [["dom.fs.databaseInitialization.pauseOnIOThreadMs", 2000]] },
+  async function testSteps() {
+    const principal = PrincipalUtils.createPrincipal("https://example.com");
+
+    info("Testing origin clearing requested after starting database work");
+
+    info("Starting database opening");
+
+    const openPromise = new Promise(function (resolve, reject) {
+      const sandbox = new Cu.Sandbox(principal, {
+        wantGlobalProperties: ["storage"],
+        forceSecureContext: true,
+      });
+      sandbox.resolve = resolve;
+      sandbox.reject = reject;
+      Cu.evalInSandbox(
+        `storage.getDirectory().then(resolve, reject);`,
+        sandbox
+      );
+    });
+
+    info("Waiting for database work to start");
+
+    await TestUtils.topicObserved("BucketFS::DatabaseWorkStarted");
+
+    info("Starting origin clearing");
+
+    const clearPromise = (async function () {
+      const request = Services.qms.clearStoragesForPrincipal(principal);
+      const promise = QuotaUtils.requestFinished(request);
+      return promise;
+    })();
+
+    info("Waiting for database to finish opening");
+
+    try {
+      await openPromise;
+      ok(false, "Should have thrown");
+    } catch (e) {
+      ok(true, "Should have thrown");
+      // XXX It should have thrown "AbortError"!
+      Assert.strictEqual(e, undefined, "Threw right result code");
+    }
+
+    info("Waiting for origin to finish clearing");
+
+    await clearPromise;
+  }
+);
