78b232fba858b697544c37fb20f83499aa2b90d0
alwu <alwu@mozilla.com>
2024-10-04 04:27:43 +0000

    Bug 1908258 - part2 : no need to delay wakelock releasing for testing. r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D218685

--- testing/profiles/common/user.js
+++ testing/profiles/common/user.js
@@ -67,6 +67,8 @@ user_pref("media.block-autoplay-until-in-foreground", false);
 user_pref("toolkit.telemetry.coverage.endpoint.base", "http://localhost");
 // Don't ask for a request in testing unless explicitly set this as true.
 user_pref("media.geckoview.autoplay.request", false);
+// No need to delay wakelock releasing for testing
+user_pref("media.wakelock.audio.delay-releasing.ms", 0);
 user_pref("geo.provider.network.compare.url", "");
 user_pref("browser.region.network.url", "");
 // Do not unload tabs on low memory when testing
