6145a71e34d2006e8ae51f88700bbedffcfd0f24
Jan Varga <jvarga@mozilla.com>
2024-10-08 07:34:07 +0000

    Bug 1923056 - Add a new test for shutdown requested after starting request finalization; r=dom-storage-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D224888

---
+++ dom/localstorage/test/unit/test_shutdownDuringRequestFinalization.js
@@ -0,0 +1,73 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+const { PrincipalUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/dom/quota/test/modules/PrincipalUtils.sys.mjs"
+);
+const { QuotaUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/dom/quota/test/modules/QuotaUtils.sys.mjs"
+);
+const { TestUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/TestUtils.sys.mjs"
+);
+
+add_task(
+  {
+    pref_set: [
+      ["dom.storage.requestFinalization.pauseOnDOMFileThreadMs", 2000],
+    ],
+  },
+  async function testSteps() {
+    const principal = PrincipalUtils.createPrincipal("https://example.com");
+
+    info("Testing shutdown requested after starting request finalization");
+
+    // We need some existing data on disk, otherwise the preloading won't create
+    // a datastore in memory that holds a directory lock.
+
+    info("Clearing");
+
+    {
+      const request = Services.qms.clear();
+      await QuotaUtils.requestFinished(request);
+    }
+
+    info("Installing package");
+
+    installPackage("somedata_profile");
+
+    info("Preloading");
+
+    await Services.domStorageManager.preload(principal);
+
+    info("Starting database opening");
+
+    const openPromise = Services.domStorageManager.preload(principal);
+
+    info("Waiting for request finalization to start");
+
+    await TestUtils.topicObserved("LocalStorage::RequestFinalizationStarted");
+
+    info("Starting shutdown");
+
+    Services.startup.advanceShutdownPhase(
+      Services.startup.SHUTDOWN_PHASE_APPSHUTDOWNQM
+    );
+
+    info("Waiting for database to finish opening");
+
+    try {
+      await openPromise;
+      ok(false, "Should have thrown");
+    } catch (e) {
+      ok(true, "Should have thrown");
+      Assert.strictEqual(
+        e.result,
+        Cr.NS_ERROR_ABORT,
+        "Threw right result code"
+      );
+    }
+  }
+);
