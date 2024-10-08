5f1e7ae5d34d29c4b867c698be59f1fcf7428770
Byron Campen [:bwc] <docfaraday@gmail.com>
2024-10-08 15:05:32 +0000

    Bug 1922959: Triage these disabled tests. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D224645

--- dom/media/webrtc/tests/mochitests/pc.js
+++ dom/media/webrtc/tests/mochitests/pc.js
@@ -2260,11 +2260,11 @@ PeerConnectionWrapper.prototype = {
       if (testOptions.rtcpmux) {
         is(numIceConnections, 1, "stats reports exactly 1 ICE connection");
       } else {
-        is(
-          numIceConnections,
-          2,
-          "stats report exactly 2 ICE connections for media and RTCP"
+        ok(
+          numIceConnections >= 1,
+          `stats ICE connections should be at least 1`
         );
+        ok(numIceConnections <= 2, `stats ICE connections should be at most 2`);
       }
     } else {
       var numAudioTransceivers = this._pc
@@ -2284,9 +2284,6 @@ PeerConnectionWrapper.prototype = {
         }).length;
 
       var numExpectedTransports = numAudioTransceivers + numVideoTransceivers;
-      if (!testOptions.rtcpmux) {
-        numExpectedTransports *= 2;
-      }
 
       if (this.dataChannels.length) {
         ++numExpectedTransports;
@@ -2295,11 +2292,25 @@ PeerConnectionWrapper.prototype = {
       info(
         "expected audio + video + data transports: " + numExpectedTransports
       );
-      is(
-        numIceConnections,
-        numExpectedTransports,
-        "stats ICE connections matches expected A/V transports"
-      );
+      if (!testOptions.rtcpmux) {
+        // Without rtcp mux, the expected number of transports doubles, but
+        // there's no good way to check whether the rtcp transports are ready,
+        // since there's no way to expose the state of those extra transports.
+        ok(
+          numIceConnections >= numExpectedTransports,
+          `stats ICE connections should be at least ${numExpectedTransports}`
+        );
+        ok(
+          numIceConnections <= numExpectedTransports * 2,
+          `stats ICE connections should be at most ${numExpectedTransports * 2}`
+        );
+      } else {
+        is(
+          numIceConnections,
+          numExpectedTransports,
+          "stats ICE connections matches expected A/V transports"
+        );
+      }
     }
   },
 
