f89adfac1942eb2c8201fdff2a8b77c28592d875
Julien Cristau <jcristau@mozilla.com>
2024-10-09 15:19:07 +0000

    Bug 1923604. r=taskgraph-reviewers,jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D225058

--- taskcluster/gecko_taskgraph/transforms/job/mozharness_test.py
+++ taskcluster/gecko_taskgraph/transforms/job/mozharness_test.py
@@ -110,7 +110,7 @@ def mozharness_test_on_docker(config, job, taskdesc):
     worker["max-run-time"] = test["max-run-time"]
     worker["retry-exit-status"] = test["retry-exit-status"]
     if "android-em-7.0-x86" in test["test-platform"]:
-        worker["privileged"] = True
+        worker["kvm"] = True
 
     artifacts = [
         # (artifact name prefix, in-image path)
