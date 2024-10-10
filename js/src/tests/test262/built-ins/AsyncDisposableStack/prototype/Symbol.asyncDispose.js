1d98b5a5a0f2636a631cb4843bc96ebbb70099db
Syed Bariman Jan <syedbarimanjan@gmail.com>
2024-10-10 15:02:44 +0000

    Bug 1918706 - Update test262 tests; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D224461

---
+++ js/src/tests/test262/built-ins/AsyncDisposableStack/prototype/Symbol.asyncDispose.js
@@ -0,0 +1,24 @@
+// |reftest| skip-if(!(this.hasOwnProperty('getBuildConfiguration')&&getBuildConfiguration('explicit-resource-management'))) -- explicit-resource-management is not enabled unconditionally
+// Copyright (C) 2023 Ron Buckton. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-asyncdisposablestack.prototype-@@asyncDispose
+description: Initial state of the Symbol.asyncDispose property
+info: |
+  The initial value of the @@asyncDispose property is the same function object as
+  the initial value of the disposeAsync property.
+
+  Per ES6 section 17, the method should exist on the Array prototype, and it
+  should be writable and configurable, but not enumerable.
+includes: [propertyHelper.js]
+features: [explicit-resource-management]
+---*/
+
+verifyProperty(AsyncDisposableStack.prototype, Symbol.asyncDispose, {
+  value: AsyncDisposableStack.prototype.disposeAsync,
+  enumerable: false,
+  writable: true,
+  configurable: true,
+});
+
+reportCompare(0, 0);
---
+++ js/src/tests/test262/built-ins/NativeErrors/SuppressedError/proto.js
@@ -0,0 +1,19 @@
+// |reftest| skip-if(!(this.hasOwnProperty('getBuildConfiguration')&&getBuildConfiguration('explicit-resource-management'))) -- explicit-resource-management is not enabled unconditionally
+// Copyright (C) 2023 Ron Buckton. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: The prototype of SuppressedError constructor is Error
+esid: sec-properties-of-the-suppressederror-constructors
+info: |
+  Properties of the SuppressedError Constructor
+
+  - has a [[Prototype]] internal slot whose value is the intrinsic object %Error%.
+features: [explicit-resource-management]
+---*/
+
+var proto = Object.getPrototypeOf(SuppressedError);
+
+assert.sameValue(proto, Error);
+
+reportCompare(0, 0);
