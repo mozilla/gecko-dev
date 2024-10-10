1b49f95d1ca97283987565de2603ccb29291185d
Florian Quèze <florian@queze.net>
2024-10-10 19:32:52 +0000

    Bug 1923015 - Remove no longer needed addonsManager.reportSuspiciousSite telemetry event, r=chutten,bholley.
    Differential Revision: https://phabricator.services.mozilla.com/D223502

--- toolkit/mozapps/extensions/AddonManager.sys.mjs
+++ toolkit/mozapps/extensions/AddonManager.sys.mjs
@@ -5201,12 +5201,6 @@ AMTelemetry = {
    */
   recordSuspiciousSiteEvent({ displayURI }) {
     let site = displayURI?.displayHost ?? "(unknown)";
-    this.recordEvent({
-      method: "reportSuspiciousSite",
-      object: "suspiciousSite",
-      value: site,
-      extra: {},
-    });
     Glean.addonsManager.reportSuspiciousSite.record(
       this.formatExtraVars({ suspicious_site: site })
     );
