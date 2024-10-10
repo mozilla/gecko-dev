b27d53e33dc387d9387185434493079bbb524e54
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2024-10-10 03:43:47 +0000

    Bug 1918906 - Set `TriggerByScript` to AnchorScrollFlags to ignore scroll anchor adjustments during (smooth) scrolling to anchor. r=botond
    Since bug 1789930 we have allowed scroll anchor adjustments during scroll
    operations by user. To tell whether the given scroll operation is done by user
    or by script, we introduced ScrollFlags::TriggerByScript. Though scrolling to
    anchor is kinda user triggered scroll operation, it would make sense to not allow
    scroll anchor adjustments during scrolling to anchor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D224284

--- layout/base/PresShellForwards.h
+++ layout/base/PresShellForwards.h
@@ -146,8 +146,10 @@ enum class ScrollFlags {
   ScrollSmooth = 1 << 3,
   ScrollSmoothAuto = 1 << 4,
   TriggeredByScript = 1 << 5,
-  // ScrollOverflowHidden | ScrollNoParentFrames
-  AnchorScrollFlags = (1 << 1) | (1 << 2),
+  // ScrollOverflowHidden | ScrollNoParentFrames | TriggeredByScript
+  // NOTE: `Anchor` means here is "scrolling to an anchor", not "CSS scroll
+  // anchoring".
+  AnchorScrollFlags = (1 << 1) | (1 << 2) | (1 << 5),
   ALL_BITS = (1 << 6) - 1,
 };
 
