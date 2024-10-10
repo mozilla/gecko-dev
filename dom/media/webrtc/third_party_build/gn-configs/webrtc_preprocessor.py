bb0d33215fdba04c246cd5bc61844d02888a3f36
Michael Froman <mfroman@mozilla.com>
2024-10-10 13:42:39 +0000

    Bug 1921707 - libwebrtc preprocessor, .json, .gni, and *.gn changes r=ng,webrtc-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D224076

---
+++ dom/media/webrtc/third_party_build/gn-configs/webrtc_preprocessor.py
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+def main(gn_config_file):
+    target_dir = "libwebrtc"
+    raw_file_contents = ""
+    with open(gn_config_file, "r") as fh:
+        raw_file_contents = fh.read()
+    raw_file_contents = raw_file_contents.replace(f"{target_dir}/", "")
+    raw_file_contents = raw_file_contents.replace(f"{target_dir}:", ":")
+    with open(gn_config_file, "w") as fh:
+        fh.write(raw_file_contents)
