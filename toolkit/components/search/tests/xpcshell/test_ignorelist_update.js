c49e6ea04ebb2395d78dc4e678cec4eb7c692c08
Moritz Beier <mbeier@mozilla.com>
2024-10-10 13:35:37 +0000

    Bug 1895934 - Improve handling of search term parameter names. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D223749

--- toolkit/components/search/tests/xpcshell/test_ignorelist_update.js
+++ toolkit/components/search/tests/xpcshell/test_ignorelist_update.js
@@ -6,11 +6,9 @@
 const kSearchEngineID1 = "ignorelist_test_engine1";
 const kSearchEngineID2 = "ignorelist_test_engine2";
 const kSearchEngineID3 = "ignorelist_test_engine3";
-const kSearchEngineURL1 =
-  "https://example.com/?search={searchTerms}&ignore=true";
-const kSearchEngineURL2 =
-  "https://example.com/?search={searchTerms}&IGNORE=TRUE";
-const kSearchEngineURL3 = "https://example.com/?search={searchTerms}";
+const kSearchEngineURL1 = "https://example.com/?ignore=true";
+const kSearchEngineURL2 = "https://example.com/?IGNORE=TRUE";
+const kSearchEngineURL3 = "https://example.com/";
 const kExtensionID = "searchignore@mozilla.com";
 
 add_task(async function test_ignoreList() {
