79c49a05564639edf46c5c0f4a03713c630b8ba8
andrej <aglavic@mozilla.com>
2024-10-11 18:43:29 +0000

    Bug 1919628 - Improve CVNE test implementation and rename to shopify-applink-startup.  a=RyanVM
    This bug addresses a few small issues discovered upon initial merging:
    - 50 runs is too much, 5 will suffice(done)
    - enable android animations(done)
    - close tabs, don't let them build up to 50(done - altered the test so it's consistent. 4 tabs are opened and then closed then the applink test is run)
    - ensure video recording videos have timestamps(done)
    - ensure replicates are there(done)
    - change the testname to something more readable(done)
    
    Original Revision: https://phabricator.services.mozilla.com/D223143
    
    Differential Revision: https://phabricator.services.mozilla.com/D225402

--- testing/performance/mobile-startup/cvne.sh
+++ testing/performance/mobile-startup/cvne.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#name: cold_view_nav_end mobile startup script
+#name: applink-startup-cnve
 #owner: perftest
-#description: Runs the cold_view_nav_end(cvne) startup app link test for fenix
+#description: Runs the applink startup(cvne) test for chrome/fenix
 
 # Path to the Python script
 SCRIPT_PATH="testing/performance/mobile-startup/android_startup_videoapplink.py"
