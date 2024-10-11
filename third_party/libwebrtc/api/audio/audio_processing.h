80dc05812952e52ba5a3e2a9ad5b9bb3cc3acff6
Michael Froman <mfroman@mozilla.com>
2024-10-11 13:49:11 -0500

    Bug 1924098 - Vendor libwebrtc from f2487c0d4f
    Upstream commit: https://webrtc.googlesource.com/src/+/f2487c0d4f6b28ea8e56dbc74ff1f33ab09fdbd4
        [audio] Adjust the order of some definitions in audio_processing
    
        Moving defines before they are used with
        unique_ptr allows to compile this file with
        -std=c++2b flag.
    
        Bug: webrtc:339074792
        Change-Id: Ie7c37ab724800aea4545b72b4d2a779e12a2026a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/360860
        Auto-Submit: Ho Cheung <hocheung@chromium.org>
        Commit-Queue: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#42879}

--- third_party/libwebrtc/api/audio/audio_processing.h
+++ third_party/libwebrtc/api/audio/audio_processing.h
@@ -49,8 +49,6 @@ class StreamConfig;
 class ProcessingConfig;
 
 class EchoDetector;
-class CustomAudioAnalyzer;
-class CustomProcessing;
 
 // The Audio Processing Module (APM) provides a collection of voice processing
 // components designed for real-time communications software.
@@ -735,6 +733,35 @@ class RTC_EXPORT AudioProcessing : public RefCountInterface {
   static int GetFrameSize(int sample_rate_hz) { return sample_rate_hz / 100; }
 };
 
+// Experimental interface for a custom analysis submodule.
+class CustomAudioAnalyzer {
+ public:
+  // (Re-) Initializes the submodule.
+  virtual void Initialize(int sample_rate_hz, int num_channels) = 0;
+  // Analyzes the given capture or render signal.
+  virtual void Analyze(const AudioBuffer* audio) = 0;
+  // Returns a string representation of the module state.
+  virtual std::string ToString() const = 0;
+
+  virtual ~CustomAudioAnalyzer() {}
+};
+
+// Interface for a custom processing submodule.
+class CustomProcessing {
+ public:
+  // (Re-)Initializes the submodule.
+  virtual void Initialize(int sample_rate_hz, int num_channels) = 0;
+  // Processes the given capture or render signal.
+  virtual void Process(AudioBuffer* audio) = 0;
+  // Returns a string representation of the module state.
+  virtual std::string ToString() const = 0;
+  // Handles RuntimeSettings. TODO(webrtc:9262): make pure virtual
+  // after updating dependencies.
+  virtual void SetRuntimeSetting(AudioProcessing::RuntimeSetting setting);
+
+  virtual ~CustomProcessing() {}
+};
+
 class RTC_EXPORT AudioProcessingBuilder {
  public:
   AudioProcessingBuilder();
@@ -885,35 +912,6 @@ class ProcessingConfig {
   }
 
   StreamConfig streams[StreamName::kNumStreamNames];
-};
-
-// Experimental interface for a custom analysis submodule.
-class CustomAudioAnalyzer {
- public:
-  // (Re-) Initializes the submodule.
-  virtual void Initialize(int sample_rate_hz, int num_channels) = 0;
-  // Analyzes the given capture or render signal.
-  virtual void Analyze(const AudioBuffer* audio) = 0;
-  // Returns a string representation of the module state.
-  virtual std::string ToString() const = 0;
-
-  virtual ~CustomAudioAnalyzer() {}
-};
-
-// Interface for a custom processing submodule.
-class CustomProcessing {
- public:
-  // (Re-)Initializes the submodule.
-  virtual void Initialize(int sample_rate_hz, int num_channels) = 0;
-  // Processes the given capture or render signal.
-  virtual void Process(AudioBuffer* audio) = 0;
-  // Returns a string representation of the module state.
-  virtual std::string ToString() const = 0;
-  // Handles RuntimeSettings. TODO(webrtc:9262): make pure virtual
-  // after updating dependencies.
-  virtual void SetRuntimeSetting(AudioProcessing::RuntimeSetting setting);
-
-  virtual ~CustomProcessing() {}
 };
 
 // Interface for an echo detector submodule.
