bd603f44f4030cc98615c8af00756a3c040ecea0
KS <kshampur@mozilla.com>
2024-10-15 00:44:51 +0000

    Bug 1923978 - Build chromium-as-release with better symbol information and remove some unecessary files. r=mstange
    This patch enables better symbol information specifically for Windows.
    We can consider other platforms later if requested.
    
    Additionally, it was found that some files are not necessary for the
    built binaries so these are removed. This isn't an exhaustive list of
    files to be removed, and further work to track that is done in Bug
    1923966.
    
    Differential Revision: https://phabricator.services.mozilla.com/D225418

--- taskcluster/scripts/misc/build-custom-car.sh
+++ taskcluster/scripts/misc/build-custom-car.sh
@@ -11,6 +11,7 @@ shift
 CONFIG=$(echo $* | tr -d "'")
 
 # Android build flag
+IS_ANDROID=false
 if [[ "$ARTIFACT_NAME" == *"android"* ]]; then
   IS_ANDROID=true
 fi
@@ -195,6 +196,19 @@ CONFIG=$(echo $CONFIG pgo_data_path='"'$PGO_FILE'"')
 # Set up then build chrome
 gn gen out/Default --args="$CONFIG"
 autoninja -C out/Default $FINAL_BIN
+
+# Make artifact smaller for win/linux
+if [[ $(uname -s) == "Linux" ]] || [[ $(uname -o) == "Msys" ]]; then
+  if [ "$IS_ANDROID" = false ]; then
+    rm -rf out/Default/.ninjadeps
+    rm -rf out/Default/*_deps
+    rm -rf out/Default/gen/*
+    rm -rf out/Default/obj/*
+    rm -rf out/Default/thinlto-cache
+    rm -rf out/Default/*/gen/*
+    rm -rf out/Default/*/obj/*
+  fi
+fi
 
 # Gather binary and related files into a zip, and upload it
 cd ..
