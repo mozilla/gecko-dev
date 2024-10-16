0b8cf7862ce070afee2bfbdc6310ee50fbbdad3c
Eitan Isaacson <eitan@monotonous.org>
2024-10-16 16:52:03 +0000

    Bug 1920082 - Check if mAttrElementsMap before CC traversing it. r=dom-core,jjaschke
    I'm not sure why this helps, but it looks like checking if the
    nsTHashMap is empty before iterating speeds things up.
    
    Differential Revision: https://phabricator.services.mozilla.com/D225720

--- dom/base/FragmentOrElement.cpp
+++ dom/base/FragmentOrElement.cpp
@@ -662,12 +662,14 @@ void FragmentOrElement::nsExtendedDOMSlots::TraverseExtendedSlots(
   NS_CYCLE_COLLECTION_NOTE_EDGE_NAME(aCb, "mSlots->mPart");
   aCb.NoteXPCOMChild(mPart.get());
 
-  for (auto& tableEntry : mAttrElementsMap) {
-    auto& [explicitlySetElements, cachedAttrElements] =
-        *tableEntry.GetModifiableData();
-    if (cachedAttrElements) {
-      ImplCycleCollectionTraverse(aCb, *cachedAttrElements,
-                                  "cached attribute elements entry", 0);
+  if (!mAttrElementsMap.IsEmpty()) {
+    for (auto& tableEntry : mAttrElementsMap) {
+      auto& [explicitlySetElements, cachedAttrElements] =
+          *tableEntry.GetModifiableData();
+      if (cachedAttrElements) {
+        ImplCycleCollectionTraverse(aCb, *cachedAttrElements,
+                                    "cached attribute elements entry", 0);
+      }
     }
   }
 
