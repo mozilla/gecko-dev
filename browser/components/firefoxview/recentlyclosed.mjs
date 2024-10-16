e7d9977cfb64ebcee2bb3c6ad6f379f8e3adcddd
Stuart Colville <scolville@mozilla.com>
2024-10-16 18:46:35 +0000

    Bug 1911749 - Update History and Tabs from other devices strings shared with FxView. r=sidebar-reviewers,fluent-reviewers,bolsson,fxview-reviewers,nsharpley
    Differential Revision: https://phabricator.services.mozilla.com/D225550

--- browser/components/firefoxview/recentlyclosed.mjs
+++ browser/components/firefoxview/recentlyclosed.mjs
@@ -296,9 +296,9 @@ class RecentlyClosedTabsInView extends ViewPage {
     let descriptionLink;
     if (Services.prefs.getBoolPref(NEVER_REMEMBER_HISTORY_PREF, false)) {
       // History pref set to never remember history
-      descriptionHeader = "firefoxview-dont-remember-history-empty-header";
+      descriptionHeader = "firefoxview-dont-remember-history-empty-header-2";
       descriptionLabels = [
-        "firefoxview-dont-remember-history-empty-description",
+        "firefoxview-dont-remember-history-empty-description-one",
         "firefoxview-dont-remember-history-empty-description-two",
       ];
       descriptionLink = {
