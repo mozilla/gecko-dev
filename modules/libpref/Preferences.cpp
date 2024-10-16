b7a34f9ced74b6b5241f693aaaf2d6e930606db2
Florian Quèze <florian@queze.net>
2024-10-16 20:46:09 +0000

    Bug 1923028 - Migrate scalars to use Glean APIs for preferences.prefs_file_was_invalid, r=chutten.
    Differential Revision: https://phabricator.services.mozilla.com/D225373

--- modules/libpref/Preferences.cpp
+++ modules/libpref/Preferences.cpp
@@ -42,7 +42,6 @@
 #include "mozilla/StaticPrefsAll.h"
 #include "mozilla/StaticPtr.h"
 #include "mozilla/SyncRunnable.h"
-#include "mozilla/Telemetry.h"
 #include "mozilla/Try.h"
 #include "mozilla/UniquePtrExtensions.h"
 #include "mozilla/URLPreloader.h"
@@ -4376,8 +4375,7 @@ already_AddRefed<nsIFile> Preferences::ReadSavedPrefs() {
       // Save a backup copy of the current (invalid) prefs file, since all prefs
       // from the error line to the end of the file will be lost (bug 361102).
       // TODO we should notify the user about it (bug 523725).
-      Telemetry::ScalarSet(
-          Telemetry::ScalarID::PREFERENCES_PREFS_FILE_WAS_INVALID, true);
+      glean::preferences::prefs_file_was_invalid.Set(true);
       MakeBackupPrefFile(file);
     }
   }
