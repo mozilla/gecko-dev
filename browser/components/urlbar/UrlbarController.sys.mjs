259ae08b0942f828e2a4278802a16e0ae539007b
Adi <agoloman@mozilla.com>
2024-10-16 16:54:55 +0300

    Backed out changeset c03123359b41 (bug 1086524) for causing browser_persist_searchMode.js failures. CLOSED TREE

--- browser/components/urlbar/UrlbarController.sys.mjs
+++ browser/components/urlbar/UrlbarController.sys.mjs
@@ -276,7 +276,6 @@ export class UrlbarController {
    *   will be deferred by the event bufferer, but preventDefault() and friends
    *   should still happen synchronously.
    */
-  // eslint-disable-next-line complexity
   handleKeyNavigation(event, executeAction = true) {
     const isMac = AppConstants.platform == "macosx";
     // Handle readline/emacs-style navigation bindings on Mac.
@@ -322,12 +321,6 @@ export class UrlbarController {
         if (executeAction) {
           if (this.view.isOpen) {
             this.view.close();
-          } else if (lazy.UrlbarPrefs.get("focusContentDocumentOnEsc")) {
-            if (this.browserWindow.gBrowser.userTypedValue) {
-              this.input.handleRevert(true);
-            } else {
-              this.browserWindow.gBrowser.selectedBrowser.focus();
-            }
           } else {
             this.input.handleRevert({
               escapeSearchMode: true,
