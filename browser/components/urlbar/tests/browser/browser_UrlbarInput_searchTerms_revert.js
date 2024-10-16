3c01bc72a3336023f8b5cc1df885a6b837593131
Collin Richards <richardscollin@gatech.edu>
2024-10-16 15:55:01 +0000

    Bug 1086524 - Focus window on Esc in address bar r=dao,urlbar-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D218893

--- browser/components/urlbar/tests/browser/browser_UrlbarInput_searchTerms_revert.js
+++ browser/components/urlbar/tests/browser/browser_UrlbarInput_searchTerms_revert.js
@@ -21,8 +21,8 @@ add_setup(async function () {
 
 function synthesizeRevert() {
   gURLBar.focus();
-  info("Double escape and revert Urlbar.");
-  EventUtils.synthesizeKey("KEY_Escape", { repeat: 2 });
+  info("Escape to revert Urlbar.");
+  EventUtils.synthesizeKey("KEY_Escape");
 }
 
 // Users should be able to revert the URL bar
