af06d4f89218d8582c3b015e7ec0686924a95298
Butkovits Atila <abutkovits@mozilla.com>
2024-10-17 12:08:51 +0300

    Backed out 4 changesets (bug 1920468, bug 1920674) for causing gtest failures in TestWindowGfx.CreateIcon_SVG_Context. CLOSED TREE
    Backed out changeset 51a61309e309 (bug 1920468)
    Backed out changeset bd830a73c821 (bug 1920674)
    Backed out changeset 03f5f21e011f (bug 1920468)
    Backed out changeset a1dac924abab (bug 1920674)

--- browser/components/profiles/SelectableProfileService.sys.mjs
+++ browser/components/profiles/SelectableProfileService.sys.mjs
@@ -32,66 +32,6 @@ XPCOMUtils.defineLazyServiceGetter(
 
 const PROFILES_CRYPTO_SALT_LENGTH_BYTES = 16;
 
-function loadImage(url) {
-  return new Promise((resolve, reject) => {
-    let imageTools = Cc["@mozilla.org/image/tools;1"].getService(Ci.imgITools);
-    let imageContainer;
-    let observer = imageTools.createScriptedObserver({
-      sizeAvailable() {
-        resolve(imageContainer);
-      },
-    });
-
-    imageTools.decodeImageFromChannelAsync(
-      url,
-      Services.io.newChannelFromURI(
-        url,
-        null,
-        Services.scriptSecurityManager.getSystemPrincipal(),
-        null, // aTriggeringPrincipal
-        Ci.nsILoadInfo.SEC_ALLOW_CROSS_ORIGIN_SEC_CONTEXT_IS_NULL,
-        Ci.nsIContentPolicy.TYPE_IMAGE
-      ),
-      (image, status) => {
-        if (!Components.isSuccessCode(status)) {
-          reject(new Components.Exception("Image loading failed", status));
-        } else {
-          imageContainer = image;
-        }
-      },
-      observer
-    );
-  });
-}
-
-async function updateTaskbar(iconUrl, profileName, strokeColor, fillColor) {
-  try {
-    let image = await loadImage(iconUrl);
-
-    if ("nsIMacDockSupport" in Ci) {
-      Cc["@mozilla.org/widget/macdocksupport;1"]
-        .getService(Ci.nsIMacDockSupport)
-        .setBadgeImage(image, { fillColor, strokeColor });
-    } else if ("nsIWinTaskbar" in Ci) {
-      lazy.EveryWindow.registerCallback(
-        "profiles",
-        win => {
-          let iconController = Cc["@mozilla.org/windows-taskbar;1"]
-            .getService(Ci.nsIWinTaskbar)
-            .getOverlayIconController(win.docShell);
-          iconController.setOverlayIcon(image, profileName, {
-            fillColor,
-            strokeColor,
-          });
-        },
-        () => {}
-      );
-    }
-  } catch (e) {
-    console.error(e);
-  }
-}
-
 /**
  * The service that manages selectable profiles
  */
