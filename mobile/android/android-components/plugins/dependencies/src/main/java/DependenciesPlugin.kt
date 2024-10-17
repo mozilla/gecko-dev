ceb07d897effc42f1a62cfe33555271bf561818a
Ryan VanderMeulen <ryanvm@gmail.com>
2024-10-17 12:14:25 +0000

    Bug 1925141 - Update AndroidX Benchmark to version 1.3.3. r=android-reviewers,mcarare
    Differential Revision: https://phabricator.services.mozilla.com/D225910

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -56,7 +56,7 @@ object Versions {
         const val appcompat = "1.7.0"
         const val arch = "2.2.0"
         const val autofill = "1.1.0"
-        const val benchmark = "1.3.1"
+        const val benchmark = "1.3.3"
         const val biometric = "1.1.0"
         const val browser = "1.8.0"
         const val cardview = "1.0.0"
