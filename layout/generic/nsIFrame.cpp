51d461e4b905979780466f535295780c593a5723
Butkovits Atila <abutkovits@mozilla.com>
2024-10-17 13:21:58 +0300

    Backed out 2 changesets (bug 1924277) for causing failures at test_restyles.html. CLOSED TREE
    Backed out changeset 491e19c84114 (bug 1924277)
    Backed out changeset f612bcda5bdd (bug 1924277)

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -11320,17 +11320,22 @@ static bool IsFrameRectScrolledOutOfView(const nsIFrame* aTarget,
   nsRect transformedRect = nsLayoutUtils::TransformFrameRectToAncestor(
       aTarget, aTargetRect, clipParent);
 
-  Maybe<nsRect> intersection =
-      transformedRect.EdgeInclusiveIntersection(clipRect);
-  if (intersection.isNothing()) {
+  if (transformedRect.IsEmpty()) {
+    // If the transformed rect is empty it represents a line or a point that we
+    // should check is outside the the scrollable rect.
+    if (transformedRect.x > clipRect.XMost() ||
+        transformedRect.y > clipRect.YMost() ||
+        clipRect.x > transformedRect.XMost() ||
+        clipRect.y > transformedRect.YMost()) {
+      return true;
+    }
+  } else if (!transformedRect.Intersects(clipRect)) {
     return true;
   }
-  transformedRect = *intersection;
 
   nsIFrame* parent = clipParent->GetParent();
   if (!parent) {
-    return nsLayoutUtils::FrameRectIsScrolledOutOfViewInCrossProcess(
-        clipParent, transformedRect);
+    return false;
   }
 
   return IsFrameRectScrolledOutOfView(clipParent, transformedRect, parent);
