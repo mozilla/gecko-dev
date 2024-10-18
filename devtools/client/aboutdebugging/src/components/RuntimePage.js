6c4d716ba403130e894089d29ec61e2a783160c5
Abhijeet Chawla <ff2400t@proton.me>
2024-10-18 14:18:28 +0000

    Bug 1810486 - Stop using UNSAFE_componentWillMount in devtools/client/aboutdebugging/src/components/RuntimePage.js r=devtools-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D226113

--- devtools/client/aboutdebugging/src/components/RuntimePage.js
+++ devtools/client/aboutdebugging/src/components/RuntimePage.js
@@ -104,9 +104,7 @@ class RuntimePage extends PureComponent {
     };
   }
 
-  // TODO: avoid the use of this method
-  // https://bugzilla.mozilla.org/show_bug.cgi?id=1774507
-  UNSAFE_componentWillMount() {
+  componentDidMount() {
     const { dispatch, runtimeId } = this.props;
     dispatch(Actions.selectPage(PAGE_TYPES.RUNTIME, runtimeId));
   }
