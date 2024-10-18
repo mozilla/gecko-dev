b89a7d6f52ea3f1c0908d9e343c7cc6045058c71
Greg Stoll <gstoll@mozilla.com>
2024-10-18 19:30:29 +0000

    Bug 1921079 - make DLP agent name bold in panel r=dlp-reviewers,fluent-reviewers,handyman
    I tried this for the hover text as well but unsurprisingly bold text
    isn't styled any differently there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D225856

--- browser/components/contentanalysis/content/ContentAnalysis.sys.mjs
+++ browser/components/contentanalysis/content/ContentAnalysis.sys.mjs
@@ -420,7 +420,7 @@ export const ContentAnalysis = {
         element.ownerDocument,
         "content-analysis-panel-description"
       ),
-      "content-analysis-panel-text",
+      "content-analysis-panel-text-styled",
       { agentName: lazy.agentName }
     );
     panelUI.showSubView("content-analysis-panel", element);
