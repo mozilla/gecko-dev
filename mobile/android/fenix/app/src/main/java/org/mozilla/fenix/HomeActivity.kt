34930fe74cda729c217e40e3982f1e71e6b8124f
John Oberhauser <j.git-global@obez.io>
2024-10-18 00:08:55 +0000

    Bug 1917939: Using metrics.distribution_id and first_session.distribution_id metrics to help track vivo distributions.  Removing the deal_id extra key from the app_opened event because it's no longer needed.  Updating some naming of classes.  a=RyanVM
    Original Revision: https://phabricator.services.mozilla.com/D222046
    
    Differential Revision: https://phabricator.services.mozilla.com/D226072

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -135,7 +135,6 @@ import org.mozilla.fenix.messaging.FenixMessageSurfaceId
 import org.mozilla.fenix.messaging.MessageNotificationWorker
 import org.mozilla.fenix.nimbus.FxNimbus
 import org.mozilla.fenix.onboarding.ReEngagementNotificationWorker
-import org.mozilla.fenix.partnerships.PartnershipDealIdUtil
 import org.mozilla.fenix.perf.MarkersActivityLifecycleCallbacks
 import org.mozilla.fenix.perf.MarkersFragmentLifecycleCallbacks
 import org.mozilla.fenix.perf.Performance
@@ -376,14 +375,11 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity {
             safeIntent
                 ?.let(::getIntentSource)
                 ?.also { source ->
-                    lifecycleScope.launch {
-                        Events.appOpened.record(
-                            Events.AppOpenedExtra(
-                                source = source,
-                                dealId = PartnershipDealIdUtil.getPartnershipDealId(),
-                            ),
-                        )
-                    }
+                    Events.appOpened.record(
+                        Events.AppOpenedExtra(
+                            source = source,
+                        ),
+                    )
                     // This will record an event in Nimbus' internal event store. Used for behavioral targeting
                     recordEventInNimbus("app_opened")
 
