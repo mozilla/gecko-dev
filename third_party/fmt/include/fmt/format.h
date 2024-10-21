014904e8153e243b9ccb3d5eb21bdc6d949ece72
Paul Adenot <paul@paul.cx>
2024-10-21 12:54:53 +0000

    Bug 1717448 - Make `{fmt}` use `double-conversion` for converting float to string internally. r=glandium,nika
    Differential Revision: https://phabricator.services.mozilla.com/D217724

--- third_party/fmt/include/fmt/format.h
+++ third_party/fmt/include/fmt/format.h
@@ -33,6 +33,8 @@
 #ifndef FMT_FORMAT_H_
 #define FMT_FORMAT_H_
 
+#include "double-conversion/double-to-string.h"
+
 #ifndef _LIBCPP_REMOVE_TRANSITIVE_INCLUDES
 #  define _LIBCPP_REMOVE_TRANSITIVE_INCLUDES
 #  define FMT_REMOVE_TRANSITIVE_INCLUDES
@@ -3146,6 +3148,13 @@ template <typename Float>
 FMT_CONSTEXPR20 auto format_float(Float value, int precision,
                                   const format_specs& specs, bool binary32,
                                   buffer<char>& buf) -> int {
+  // GCC and old clang seem to hit this even though this function isn't called.
+  #ifdef __clang__
+  #if __clang_major__ > 8
+    static_assert(false,
+      "This method is not to be used in Gecko, use format_float_gecko");
+  #endif
+  #endif
   // float is passed as double to reduce the number of instantiations.
   static_assert(!std::is_same<Float, float>::value, "");
   auto converted_value = convert_float(value);
@@ -3441,6 +3450,43 @@ FMT_CONSTEXPR20 auto format_float(Float value, int precision,
   return exp;
 }
 
+// See https://bugzilla.mozilla.org/show_bug.cgi?id=1717448#c2 for the reason
+// we're doing this. This copied from and should be kept in sync with
+// PrintfTarget::cvt_f in Gecko's mozglue/misc/Printf.cpp.
+template <typename Float>
+FMT_CONSTEXPR20 auto format_float_gecko(Float value, int precision, format_specs specs,
+                                  buffer<char>& buf) -> int {
+  FMT_ASSERT(detail::isfinite(value),
+    "Non-finite values are to be handled ahead of calling this");
+  using double_conversion::DoubleToStringConverter;
+  using DTSC = DoubleToStringConverter;
+  // Printf.cpp seem to use UNIQUE_ZERO here, but then adds its own `-` further
+  // in the code.
+  char e = specs.upper() ? 'E' : 'e';
+  DTSC converter(DTSC::NO_TRAILING_ZERO |
+                 DTSC::EMIT_POSITIVE_EXPONENT_SIGN,
+                 "inf", "nan", e, 0, 0, 4, 0, 2);
+  buf.try_resize(64);
+  double_conversion::StringBuilder builder(buf.data(), buf.size());
+  // Negative precision defaults to 6.
+  if (precision == -1) { precision = 6; }
+  bool success = false;
+  if (specs.type() == presentation_type::exp) {
+      success = converter.ToExponential(value, precision, &builder);
+  } else if (specs.type() == presentation_type::fixed) {
+      success = converter.ToFixed(value, precision, &builder);
+  } else if (specs.type() == presentation_type::general ||
+      specs.type() == presentation_type::none) {
+      // "If an explicit precision is zero, it shall be taken as 1."
+      success = converter.ToPrecision(value, precision ? precision : 1, &builder);
+  } else {
+    FMT_ASSERT(false, "Unhandled");
+  }
+  FMT_ASSERT(success, "");
+  buf.try_resize(builder.position());
+  return builder.position();
+}
+
 template <typename Char, typename OutputIt, typename T>
 FMT_CONSTEXPR20 auto write_float(OutputIt out, T value, format_specs specs,
                                  locale_ref loc) -> OutputIt {
@@ -3457,17 +3503,6 @@ FMT_CONSTEXPR20 auto write_float(OutputIt out, T value, format_specs specs,
   }
 
   int precision = specs.precision;
-  if (precision < 0) {
-    if (specs.type() != presentation_type::none) {
-      precision = 6;
-    } else if (is_fast_float<T>::value && !is_constant_evaluated()) {
-      // Use Dragonbox for the shortest format.
-      using floaty = conditional_t<sizeof(T) >= sizeof(double), double, float>;
-      auto dec = dragonbox::to_decimal(static_cast<floaty>(value));
-      return write_float<Char>(out, dec, specs, s, loc);
-    }
-  }
-
   memory_buffer buffer;
   if (specs.type() == presentation_type::hexfloat) {
     if (s != sign::none) buffer.push_back(detail::getsign<char>(s));
@@ -3477,22 +3512,37 @@ FMT_CONSTEXPR20 auto write_float(OutputIt out, T value, format_specs specs,
   }
 
   if (specs.type() == presentation_type::exp) {
-    if (precision == max_value<int>())
-      report_error("number is too big");
-    else
-      ++precision;
     if (specs.precision != 0) specs.set_alt();
   } else if (specs.type() == presentation_type::fixed) {
     if (specs.precision != 0) specs.set_alt();
   } else if (precision == 0) {
     precision = 1;
   }
-  int exp = format_float(convert_float(value), precision, specs,
-                         std::is_same<T, float>(), buffer);
 
   specs.precision = precision;
-  auto f = big_decimal_fp{buffer.data(), static_cast<int>(buffer.size()), exp};
-  return write_float<Char>(out, f, specs, s, loc);
+  value = abs(value);
+  format_float_gecko(convert_float(value), precision, specs, buffer);
+  size_t size = buffer.size();
+  if (s != sign::none) {
+    // Space for the sign, this influences the padding calculations below.
+    size++;
+    // If padding with zeros, the sign goes here, e.g. -0004.3
+    if (*specs.fill<Char>() == '0') {
+      *out++ = detail::getsign<Char>(s);
+    }
+  }
+  return write_padded<Char, align::right>(
+      out, specs, size, size, [s, &buffer, specs](reserve_iterator<OutputIt> it) {
+        // If padding with something other than zeros, the sign goes here, e.g.
+        // '   -4.3' (quotes added for clarity).
+        if (s != sign::none) {
+          if (*specs.fill<Char>() != '0') {
+            *it++ = detail::getsign<Char>(s);
+          }
+        }
+        const char* data = buffer.data();
+        return copy<Char>(data, data + buffer.size(), it);
+      });
 }
 
 template <typename Char, typename OutputIt, typename T,
@@ -3518,8 +3568,7 @@ FMT_CONSTEXPR20 auto write(OutputIt out, T value) -> OutputIt {
   if ((bit_cast<floaty_uint>(value) & mask) == mask)
     return write_nonfinite<Char>(out, std::isnan(value), specs, s);
 
-  auto dec = dragonbox::to_decimal(static_cast<floaty>(value));
-  return write_float<Char>(out, dec, specs, s, {});
+  return write_float<Char>(out, value, specs, {});
 }
 
 template <typename Char, typename OutputIt, typename T,
