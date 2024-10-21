3343db1c1690746134210c9f9073e1477bf6ca79
Chris H-C <chutten@mozilla.com>
2024-10-21 15:37:00 +0000

    Bug 1926034 - Move widget metrics definitions to firefox_desktop_metrics from gecko_metrics r=janerik
    Differential Revision: https://phabricator.services.mozilla.com/D226327

--- toolkit/components/glean/metrics_index.py
+++ toolkit/components/glean/metrics_index.py
@@ -73,8 +73,6 @@ gecko_metrics = [
     "toolkit/mozapps/handling/metrics.yaml",
     "toolkit/profile/metrics.yaml",
     "toolkit/xre/metrics.yaml",
-    "widget/gtk/metrics.yaml",
-    "widget/metrics.yaml",
     "xpcom/metrics.yaml",
 ]
 
@@ -115,6 +113,8 @@ firefox_desktop_metrics = [
     "toolkit/components/telemetry/metrics.yaml",
     "toolkit/modules/metrics.yaml",
     "widget/cocoa/metrics.yaml",
+    "widget/gtk/metrics.yaml",
+    "widget/metrics.yaml",
     "widget/windows/metrics.yaml",
 ]
 
