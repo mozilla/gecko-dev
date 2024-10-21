cfcd66bac90dbe8abecf1a0de999cace95880492
KS <kshampur@mozilla.com>
2024-10-21 22:29:32 +0000

    Bug 1925145 - Add updated Windows SDK and fix CaR builds. r=firefox-build-system-reviewers,glandium
    The windows chromium-as-release (CaR) builds recently updated the
    minimum windows SDK version required to complete the builds. This patch
    adds this new sdk toolchain and also keeps it separate for just these
    builds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D226332

--- taskcluster/scripts/misc/vs-setup.sh
+++ taskcluster/scripts/misc/vs-setup.sh
@@ -8,6 +8,9 @@ fi
 if [ ! -d "${VSPATH}/${VCDIR}" ]; then
     VCDIR=VC/Tools/MSVC/14.39.33519
 fi
+if [ ! -d "${VSPATH}/${VCDIR}" ]; then
+    VCDIR=VC/Tools/MSVC/14.41.34120
+fi
 SDKDIR="Windows Kits/10"
 SDK_VERSION=10.0.17134.0
 if [ ! -d "${VSPATH}/${SDKDIR}/Lib/${SDK_VERSION}" ]; then
@@ -15,6 +18,9 @@ if [ ! -d "${VSPATH}/${SDKDIR}/Lib/${SDK_VERSION}" ]; then
 fi
 if [ ! -d "${VSPATH}/${SDKDIR}/Lib/${SDK_VERSION}" ]; then
     SDK_VERSION=10.0.22621.0
+fi
+if [ ! -d "${VSPATH}/${SDKDIR}/Lib/${SDK_VERSION}" ]; then
+    SDK_VERSION=10.0.26100.0
 fi
 
 case "$TARGET" in
