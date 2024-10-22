246d7f3b602576764769840f1bebfec3aa10cddd
Dave Townsend <dtownsend@oxymoronical.com>
2024-10-22 14:10:12 +0000

    Bug 1925774: Browser does not start after choosing different profile from -p. r=jhirsch
    We must delay closing the profile selection UI until after the asynchronous flush completes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D226322

--- toolkit/profile/content/profileSelection.js
+++ toolkit/profile/content/profileSelection.js
@@ -75,8 +75,30 @@ function startup() {
     window.close();
     throw e;
   }
-  document.addEventListener("dialogaccept", acceptDialog);
-  document.addEventListener("dialogcancel", exitDialog);
+
+  document.addEventListener("dialogaccept", dialogClosing);
+  document.addEventListener("dialogcancel", dialogClosing);
+}
+
+function dialogClosing(event) {
+  // We want to handle closing the dialog ourselves once the async operation has
+  // completed. Note that this means the `dialogclosing` event is not dispatched
+  // but we don't use that anyway.
+  event.preventDefault();
+
+  let promise = event.type == "dialogaccept" ? acceptDialog() : exitDialog();
+  promise
+    .then(shouldClose => {
+      if (shouldClose === false) {
+        return;
+      }
+
+      window.close();
+    })
+    .catch(e => {
+      console.error(e);
+      window.close();
+    });
 }
 
 async function flush(cancelled) {
@@ -140,7 +162,7 @@ async function flush(cancelled) {
   );
 }
 
-function acceptDialog(event) {
+async function acceptDialog() {
   var appName = gBrandBundle.getString("brandShortName");
 
   var profilesElement = document.getElementById("profiles");
@@ -153,8 +175,8 @@ function acceptDialog(event) {
       [appName]
     );
     Services.prompt.alert(window, pleaseSelectTitle, pleaseSelect);
-    event.preventDefault();
-    return;
+
+    return false;
   }
 
   gDialogParams.objects.insertElementAt(selectedProfile.profile.rootDir, 0);
@@ -169,11 +191,13 @@ function acceptDialog(event) {
       // profile based on the lock's directories.
     }
   }
-  flush(false);
+
+  await flush(false);
+  return true;
 }
 
-function exitDialog() {
-  flush(true);
+async function exitDialog() {
+  await flush(true);
 }
 
 function updateStartupPrefs() {
