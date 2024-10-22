3a38c100a5a018ab8e390952cd0cb34b16891c44
Gregory Pappas <gp3033@protonmail.com>
2024-10-22 08:37:03 +0000

    Bug 1926068 - Re-enable YouTube flash embed rewriting in nightly and early beta r=emilio
    Use counters in Firefox and Chromium are showing that
    this mechanism can't be disabled in release.
    
    Let's re-enable in nightly and early beta.
    
    See https://github.com/whatwg/html/issues/2390 for more info
    
    Differential Revision: https://phabricator.services.mozilla.com/D226355

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3705,11 +3705,7 @@ pref("webextensions.storage.sync.serverURL", "https://webextensions.settings.ser
 pref("dom.input.fallbackUploadDir", "");
 
 // Turn rewriting of youtube embeds on/off
-#if defined(EARLY_BETA_OR_EARLIER)
-  pref("plugins.rewrite_youtube_embeds", false);
-#else
-  pref("plugins.rewrite_youtube_embeds", true);
-#endif
+pref("plugins.rewrite_youtube_embeds", true);
 
 // Default media volume
 pref("media.default_volume", "1.0");
