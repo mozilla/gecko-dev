e932754708d789a7d17a649416a6b70a6905a17e
Jan Varga <jvarga@mozilla.com>
2024-10-23 18:02:07 +0000

    Bug 1925832 - LSNG: Cache the pref value for enabling migration; r=dom-storage-reviewers,jari
    Differential Revision: https://phabricator.services.mozilla.com/D226246

--- dom/localstorage/ActorsParent.cpp
+++ dom/localstorage/ActorsParent.cpp
@@ -2276,6 +2276,7 @@ class PrepareDatastoreOp
   uint64_t mDatastoreId;
   NestedState mNestedState;
   const bool mForPreload;
+  const bool mEnableMigration;
   bool mDatabaseNotAvailable;
   // Set when the Datastore has been registered with gPrivateDatastores so that
   // it can be unregistered if an error is encountered in PrepareDatastoreOp.
@@ -6563,6 +6564,9 @@ PrepareDatastoreOp::PrepareDatastoreOp(
       mNestedState(NestedState::BeforeNesting),
       mForPreload(aParams.type() ==
                   LSRequestParams::TLSRequestPreloadDatastoreParams),
+      mEnableMigration(
+          StaticPrefs::
+              dom_storage_enable_migration_from_unsupported_legacy_implementation()),
       mDatabaseNotAvailable(false),
       mInvalidated(false)
 #ifdef DEBUG
@@ -6997,9 +7001,7 @@ nsresult PrepareDatastoreOp::DatabaseWork() {
     }
 
     bool hasDataForMigration =
-        StaticPrefs::
-            dom_storage_enable_migration_from_unsupported_legacy_implementation() &&
-        mArchivedOriginScope->HasMatches(gArchivedOrigins);
+        mEnableMigration && mArchivedOriginScope->HasMatches(gArchivedOrigins);
 
     // If there's nothing to preload (except the case when we want to migrate
     // data during preloading), then we can finish the operation without
