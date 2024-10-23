c63511a216d144147b8280ae4d4e4886f081950f
Serah Nderi <sarahngima77@gmail.com>
2024-10-23 12:36:30 +0000

    Bug 1924608 - Add non262 tests for Error.isError; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D226557

---
+++ js/src/tests/non262/Error/isError.js
@@ -0,0 +1,19 @@
+// |reftest| shell-option(--enable-error-iserror) skip-if(!Error.isError)
+
+// Test non-object input should return false
+assertEq(Error.isError(null), false);
+assertEq(Error.isError(undefined), false);
+assertEq(Error.isError(123), false);
+assertEq(Error.isError("string"), false);
+
+// Test plain objects should return false
+assertEq(Error.isError({}), false);
+assertEq(Error.isError(new Object()), false);
+
+// Test various error objects should return true
+assertEq(Error.isError(new Error()), true);
+assertEq(Error.isError(new TypeError()), true);
+assertEq(Error.isError(new RangeError()), true);
+
+if (typeof reportCompare === 'function')
+    reportCompare(0, 0);
