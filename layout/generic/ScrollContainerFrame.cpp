fddb54c15df96bc579075bb1cb2b2f7efba03e44
Tamas Szentpeteri <tszentpeteri@mozilla.com>
2024-10-23 09:42:33 +0300

    Backed out changeset 3033cffb3c50 (bug 1911457) for causing Wr failures on transform-3d-scales-different-x-y-dynamic-002.html. CLOSED TREE

--- layout/generic/ScrollContainerFrame.cpp
+++ layout/generic/ScrollContainerFrame.cpp
@@ -6361,10 +6361,7 @@ bool ScrollContainerFrame::ComputeCustomOverflow(
   bool needReflow = false;
   nsPoint scrollPosition = GetScrollPosition();
   if (overflowChange.contains(ScrollDirection::eHorizontal)) {
-    if (ss.mHorizontal != StyleOverflow::Hidden || scrollPosition.x ||
-        // If we are in minimum-scale size mode, we need to do a reflow to
-        // re-compute the minimum-scale size.
-        mIsUsingMinimumScaleSize) {
+    if (ss.mHorizontal != StyleOverflow::Hidden || scrollPosition.x) {
       needReflow = true;
     }
   }
