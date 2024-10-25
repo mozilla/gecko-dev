481bda16db4cf814fe55ca981c8d11db6595d362
Charlie Humphreys <chumphreys@mozilla.com>
2024-10-25 21:12:37 +0000

    Bug 1923495: Update RecordedNimbusContext to support behavioral event queries. r=beth,android-reviewers,boek
    Differential Revision: https://phabricator.services.mozilla.com/D224994

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/experiments/NimbusSetup.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/experiments/NimbusSetup.kt
@@ -42,6 +42,10 @@ fun createNimbus(context: Context, urlString: String?): NimbusApi {
     if (isAppFirstRun) {
         context.settings().isFirstNimbusRun = false
     }
+    val recordedNimbusContext = RecordedNimbusContext.create(
+        context = context,
+        isFirstRun = isAppFirstRun,
+    )
 
     // The name "fenix" here corresponds to the app_name defined for the family of apps
     // that encompasses all of the channels for the Fenix app.  This is defined upstream in
@@ -70,14 +74,7 @@ fun createNimbus(context: Context, urlString: String?): NimbusApi {
         onFetchCallback = {
             context.settings().nimbusExperimentsFetched = true
         }
-        recordedContext = RecordedNimbusContext(
-            isFirstRun = isAppFirstRun,
-            utmSource = context.settings().utmSource,
-            utmMedium = context.settings().utmMedium,
-            utmCampaign = context.settings().utmCampaign,
-            utmTerm = context.settings().utmTerm,
-            utmContent = context.settings().utmContent,
-        )
+        recordedContext = recordedNimbusContext
     }.build(appInfo).also { nimbusApi ->
         nimbusApi.recordIsReady(FxNimbus.features.nimbusIsReady.value().eventCount)
     }
--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/experiments/RecordedNimbusContext.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/experiments/RecordedNimbusContext.kt
@@ -4,10 +4,25 @@
 
 package org.mozilla.fenix.experiments
 
+import android.content.Context
+import androidx.annotation.VisibleForTesting
 import org.json.JSONObject
 import org.mozilla.experiments.nimbus.internal.JsonObject
 import org.mozilla.experiments.nimbus.internal.RecordedContext
 import org.mozilla.fenix.GleanMetrics.NimbusSystem
+import org.mozilla.fenix.ext.settings
+
+/**
+ * The following constants are string constants of the keys that appear in the [EVENT_QUERIES] map.
+ */
+const val DAYS_OPENED_IN_LAST_28 = "days_opened_in_last_28"
+
+/**
+ * [EVENT_QUERIES] is a map of keys to Nimbus SDK EventStore queries.
+ */
+private val EVENT_QUERIES = mapOf(
+    DAYS_OPENED_IN_LAST_28 to "'events.app_opened'|eventCountNonZero('Days', 28, 0)",
+)
 
 /**
  * The RecordedNimbusContext class inherits from an internal Nimbus interface that provides methods
@@ -19,16 +34,37 @@ import org.mozilla.fenix.GleanMetrics.NimbusSystem
  */
 class RecordedNimbusContext(
     val isFirstRun: Boolean,
+    private val eventQueries: Map<String, String> = mapOf(),
+    private var eventQueryValues: Map<String, Double> = mapOf(),
     val utmSource: String,
     val utmMedium: String,
     val utmCampaign: String,
     val utmTerm: String,
     val utmContent: String,
 ) : RecordedContext {
+    /**
+     * [getEventQueries] is called by the Nimbus SDK Rust code to retrieve the map of event
+     * queries. The are then executed against the Nimbus SDK's EventStore to retrieve their values.
+     *
+     * @return Map<String, String>
+     */
+    override fun getEventQueries(): Map<String, String> {
+        return eventQueries
+    }
+
+    /**
+     * [record] is called when experiment enrollments are evolved. It should apply the
+     * [RecordedNimbusContext]'s values to a [NimbusSystem.RecordedNimbusContextObject] instance,
+     * and use that instance to record the values to Glean.
+     */
     override fun record() {
+        val eventQueryValuesObject = NimbusSystem.RecordedNimbusContextObjectItemEventQueryValuesObject(
+            daysOpenedInLast28 = eventQueryValues[DAYS_OPENED_IN_LAST_28]?.toInt(),
+        )
         NimbusSystem.recordedNimbusContext.set(
             NimbusSystem.RecordedNimbusContextObject(
                 isFirstRun = isFirstRun,
+                eventQueryValues = eventQueryValuesObject,
                 installReferrerResponseUtmSource = utmSource,
                 installReferrerResponseUtmMedium = utmMedium,
                 installReferrerResponseUtmCampaign = utmCampaign,
@@ -38,14 +74,87 @@ class RecordedNimbusContext(
         )
     }
 
+    /**
+     * [setEventQueryValues] is called by the Nimbus SDK Rust code after the event queries have been
+     * executed. The [eventQueryValues] should be written back to the Kotlin object.
+     *
+     * @param [eventQueryValues] The values for each query after they have been executed in the
+     * Nimbus SDK Rust environment.
+     */
+    override fun setEventQueryValues(eventQueryValues: Map<String, Double>) {
+        this.eventQueryValues = eventQueryValues
+    }
+
+    /**
+     * [toJson] is called by the Nimbus SDK Rust code after the event queries have been executed,
+     * and before experiment enrollments have been evolved. The value returned from this method
+     * will be applied directly to the Nimbus targeting context, and its keys/values take
+     * precedence over those in the main Nimbus targeting context.
+     *
+     * @return JSONObject
+     */
     override fun toJson(): JsonObject {
-        val obj = JSONObject()
-        obj.put("is_first_run", isFirstRun)
-        obj.put("install_referrer_response_utm_source", utmSource)
-        obj.put("install_referrer_response_utm_medium", utmMedium)
-        obj.put("install_referrer_response_utm_campaign", utmCampaign)
-        obj.put("install_referrer_response_utm_term", utmTerm)
-        obj.put("install_referrer_response_utm_content", utmContent)
+        val obj = JSONObject(
+            mapOf(
+                "is_first_run" to isFirstRun,
+                "events" to JSONObject(eventQueryValues),
+                "install_referrer_response_utm_source" to utmSource,
+                "install_referrer_response_utm_medium" to utmMedium,
+                "install_referrer_response_utm_campaign" to utmCampaign,
+                "install_referrer_response_utm_term" to utmTerm,
+                "install_referrer_response_utm_content" to utmContent,
+            ),
+        )
         return obj
+    }
+
+    /**
+     * Companion object for RecordedNimbusContext
+     */
+    companion object {
+
+        /**
+         * Creates a RecordedNimbusContext instance, populated with the application-defined
+         * eventQueries
+         *
+         * @return RecordedNimbusContext
+         */
+        fun create(
+            context: Context,
+            isFirstRun: Boolean,
+        ): RecordedNimbusContext {
+            return RecordedNimbusContext(
+                isFirstRun = isFirstRun,
+                eventQueries = EVENT_QUERIES,
+                utmSource = context.settings().utmSource,
+                utmMedium = context.settings().utmMedium,
+                utmCampaign = context.settings().utmCampaign,
+                utmTerm = context.settings().utmTerm,
+                utmContent = context.settings().utmContent,
+            )
+        }
+
+        /**
+         * Creates a RecordedNimbusContext instance for test purposes
+         *
+         * @return RecordedNimbusContext
+         */
+        @VisibleForTesting
+        internal fun createForTest(
+            isFirstRun: Boolean = false,
+            eventQueries: Map<String, String> = EVENT_QUERIES,
+            eventQueryValues: Map<String, Double> = mapOf(),
+        ): RecordedNimbusContext {
+            return RecordedNimbusContext(
+                isFirstRun = isFirstRun,
+                eventQueries = eventQueries,
+                eventQueryValues = eventQueryValues,
+                utmSource = "",
+                utmMedium = "",
+                utmCampaign = "",
+                utmTerm = "",
+                utmContent = "",
+            )
+        }
     }
 }
