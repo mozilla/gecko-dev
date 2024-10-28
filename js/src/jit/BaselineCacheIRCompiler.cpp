99d0e7462d504a70a6645ac97dfa77a7939b33ad
Chris Fallin <chris@cfallin.org>
2024-10-28 15:56:20 +0000

    Bug 1922981 part 21 / 39: PBL: switch to uniform indirect-call interface to all ICs including fallbacks. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D224673

--- js/src/jit/BaselineCacheIRCompiler.cpp
+++ js/src/jit/BaselineCacheIRCompiler.cpp
@@ -23,6 +23,7 @@
 #include "proxy/DeadObjectProxy.h"
 #include "proxy/Proxy.h"
 #include "util/Unicode.h"
+#include "vm/PortableBaselineInterpret.h"
 #include "vm/StaticStrings.h"
 
 #include "jit/JitScript-inl.h"
@@ -2722,6 +2723,11 @@ ICAttachResult js::jit::AttachBaselineCacheIRStub(
   auto newStub = new (newStubMem) ICCacheIRStub(code, stubInfo);
   writer.copyStubData(newStub->stubDataStart());
   newStub->setTypeData(writer.typeData());
+
+#ifdef ENABLE_PORTABLE_BASELINE_INTERP
+  newStub->updateRawJitCode(pbl::GetICInterpreter());
+#endif
+
   stub->addNewStub(icEntry, newStub);
 
   JSScript* owningScript = icScript->isInlined()
