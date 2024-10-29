e9b42785788bac58100d488f1353d5efe9fe208d
Luca Greco <lgreco@mozilla.com>
2024-10-29 13:20:59 +0000

    Bug 1926106 - Add test case loading a moz-extension background page subframe in packed and unpacked extension. r=willdurand,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D226376

--- toolkit/components/extensions/test/xpcshell/test_ext_background_sub_windows.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_background_sub_windows.js
@@ -2,6 +2,23 @@
 /* vim: set sts=2 sw=2 et tw=80: */
 "use strict";
 
+const { AddonManager } = ChromeUtils.importESModule(
+  "resource://gre/modules/AddonManager.sys.mjs"
+);
+
+AddonTestUtils.init(this);
+AddonTestUtils.overrideCertDB();
+AddonTestUtils.createAppInfo(
+  "xpcshell@tests.mozilla.org",
+  "XPCShell",
+  "1",
+  "43"
+);
+
+add_setup(async () => {
+  await AddonTestUtils.promiseStartupManager();
+});
+
 add_task(async function testBackgroundWindow() {
   let extension = ExtensionTestUtils.loadExtension({
     background() {
@@ -43,4 +60,134 @@ add_task(async function testBackgroundWindow() {
   equal(loadCount, 1, "background script loaded only once");
 
   await extension.unload();
+});
+
+add_task(async function testBackgroundMozExtSubframe() {
+  const id = "bg-subframe@test-addon";
+  let xpi = AddonTestUtils.createTempWebExtensionFile({
+    manifest: {
+      background: { scripts: ["bg.js"] },
+      browser_specific_settings: { gecko: { id } },
+    },
+    files: {
+      "bg.js": function () {
+        const subframe = document.createElement("iframe");
+        subframe.src = "iframe.html";
+        subframe.onload = () =>
+          browser.test.sendMessage("frame-load-result", {
+            type: "load",
+            url: subframe.contentWindow.location.href,
+          });
+        subframe.onerror = () =>
+          browser.test.sendMessage("frame-load-result", {
+            type: "error",
+            url: subframe.contentWindow.location.href,
+          });
+        document.body.append(subframe);
+      },
+      "iframe.html": `<!DOCTYPE html><html><body>subframe</body></html>`,
+      "extpage.html": `<!DOCTYPE html><html><body>extpage</body></html>`,
+    },
+  });
+
+  info(
+    "Test bgpage moz-extension subframe in addon temporarily installed as packed"
+  );
+  let extPacked = ExtensionTestUtils.expectExtension(id);
+  await AddonManager.installTemporaryAddon(xpi);
+  await extPacked.awaitStartup();
+  Assert.deepEqual(
+    await extPacked.awaitMessage("frame-load-result"),
+    {
+      type: "load",
+      url: `moz-extension://${extPacked.uuid}/iframe.html`,
+    },
+    "Expect moz-extension subframe to be loaded successfully in zipped extension"
+  );
+  await extPacked.unload();
+
+  // Repeat the test case again on an unpacked extension, which is
+  // where Bug 1926106 was actually originally hit.
+  info(
+    "Test bgpage moz-extension subframe in addon temporarily installed as unpacked"
+  );
+  // This temporary directory is going to be removed from the
+  // cleanup function, but also make it unique as we do for the
+  // other temporary files (e.g. like getTemporaryFile as defined
+  // in XPIInstall.sys.mjs).
+  const random = Math.round(Math.random() * 36 ** 3).toString(36);
+  const tmpDirName = `mochitest_unpacked_addons_${random}`;
+  let tmpExtPath = FileUtils.getDir("TmpD", [tmpDirName]);
+  tmpExtPath.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+  registerCleanupFunction(() => {
+    tmpExtPath.remove(true);
+  });
+  // Unpacking the xpi file into the temporary directory.
+  const extDir = await AddonTestUtils.manuallyInstall(
+    xpi,
+    tmpExtPath,
+    null,
+    /* unpacked */ true
+  );
+  let extUnpacked = ExtensionTestUtils.expectExtension(id);
+  await AddonManager.installTemporaryAddon(extDir);
+  await extUnpacked.awaitStartup();
+  Assert.deepEqual(
+    await extUnpacked.awaitMessage("frame-load-result"),
+    {
+      type: "load",
+      url: `moz-extension://${extUnpacked.uuid}/iframe.html`,
+    },
+    "Expect moz-extension subframe to be loaded successfully in unpacked extension"
+  );
+
+  // This is the opposite case where the nested frame url is one that
+  // we wouldn't find on disk (instead of the parent frame), that isn't
+  // an actual scenario that developers should hit, but it would ensure
+  // that scenario works in the short term (and it may become useful if
+  // we would get another generated moz-extension page that would make
+  // sense for developers to use as a subframe url in practice).
+  info("Test _generated_background_page.html set as subframe url");
+  const extpage = await ExtensionTestUtils.loadContentPage(
+    extUnpacked.extension.baseURI.resolve("extpage.html")
+  );
+  const result = await extpage.spawn([], () => {
+    return new Promise(resolve => {
+      const doc = this.content.document;
+      const frame = doc.createElement("iframe");
+      frame.src = "_generated_background_page.html";
+      frame.onload = () =>
+        resolve({
+          type: "load",
+          url: frame.contentWindow.location.href,
+        });
+      frame.onerror = () =>
+        resolve({
+          type: "error",
+          url: frame.contentWindow.location.href,
+        });
+      doc.body.append(frame);
+    });
+  });
+  await extpage.close();
+
+  Assert.deepEqual(
+    result,
+    {
+      type: "load",
+      url: `moz-extension://${extUnpacked.uuid}/_generated_background_page.html`,
+    },
+    "Expect _generated_background_page.html to be loaded successfully as a subframe"
+  );
+
+  Assert.deepEqual(
+    await extUnpacked.awaitMessage("frame-load-result"),
+    {
+      type: "load",
+      url: `moz-extension://${extUnpacked.uuid}/iframe.html`,
+    },
+    "Expect moz-extension subframe to be loaded successfully in _generated_background_page subframe"
+  );
+
+  await extUnpacked.unload();
 });
