b7edb77774e27db3a43e66dde91e6fde22d27d21
Hubert Boma Manilla <hmanilla@mozilla.com>
2024-10-29 08:29:19 +0000

    Bug 1832017 - [devtools] Wait for updates to complete so popups do not lose focus mid assertion r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D227038

--- devtools/client/debugger/test/mochitest/browser_dbg-blackbox.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-blackbox.js
@@ -535,6 +535,12 @@ async function assertGutterBlackBoxBoxContextMenuItems(dbg, testFixtures) {
     info(
       "Asserts that the gutter context menu items when clicking on the gutter of a nonBlackboxed line"
     );
+
+    // Wait for CM6 to complete any updates so the popup
+    // does not lose focus while asserting the context menu items due
+    // late updates completion.
+    await waitForDocumentLoadComplete(dbg);
+
     const popup = await openContextMenuInDebugger(
       dbg,
       "gutterElement",
