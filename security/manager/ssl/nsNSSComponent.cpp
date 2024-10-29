ea048083e0c4164b6912ce9be97e29104d63d582
Otto Länd <bind-autoland@mozilla.com>
2024-10-29 16:49:44 +0000

    Bug 1927501: apply code formatting via Lando
    # ignore-this-changeset

--- security/manager/ssl/nsNSSComponent.cpp
+++ security/manager/ssl/nsNSSComponent.cpp
@@ -1308,14 +1308,14 @@ static nsresult GetNSSProfilePath(nsAutoCString& aProfilePath) {
 // |profilePath| is encoded in UTF-8.
 static nsresult AttemptToRenamePKCS11ModuleDB(const nsACString& profilePath) {
   nsCOMPtr<nsIFile> profileDir;
-#ifdef XP_WIN
+#  ifdef XP_WIN
   // |profilePath| is encoded in UTF-8 because SQLite always takes UTF-8 file
   // paths regardless of the current system code page.
   MOZ_TRY(NS_NewLocalFile(u""_ns, getter_AddRefs(profileDir)));
   MOZ_TRY(profileDir->InitWithPath(NS_ConvertUTF8toUTF16(profilePath)));
-#else
+#  else
   MOZ_TRY(NS_NewNativeLocalFile(profilePath, getter_AddRefs(profileDir)));
-#endif
+#  endif
   const char* moduleDBFilename = "pkcs11.txt";
   nsAutoCString destModuleDBFilename(moduleDBFilename);
   destModuleDBFilename.Append(".fips");
