6cb7d5308c1f5dd01a0b7569b8f3d4e546cdc48c
Nicolas Chevobbe <nchevobbe@mozilla.com>
2024-10-29 15:05:28 +0000

    Bug 1921758 - [devtools] Use aria-pressed for selected breadcrumb item. r=devtools-reviewers,ochameau.
    Differential Revision: https://phabricator.services.mozilla.com/D226670

--- devtools/client/inspector/breadcrumbs.js
+++ devtools/client/inspector/breadcrumbs.js
@@ -681,10 +681,13 @@ HTMLBreadcrumbs.prototype = {
       this.currentIndex > -1 &&
       this.currentIndex < this.nodeHierarchy.length
     ) {
-      this.nodeHierarchy[this.currentIndex].button.removeAttribute("checked");
+      this.nodeHierarchy[this.currentIndex].button.setAttribute(
+        "aria-pressed",
+        "false"
+      );
     }
     if (index > -1) {
-      this.nodeHierarchy[index].button.setAttribute("checked", "true");
+      this.nodeHierarchy[index].button.setAttribute("aria-pressed", "true");
     } else {
       // Unset active active descendant when all buttons are unselected.
       this.outer.removeAttribute("aria-activedescendant");
