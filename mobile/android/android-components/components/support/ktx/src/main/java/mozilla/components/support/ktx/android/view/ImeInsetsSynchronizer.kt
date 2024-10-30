5b259f1bb5f5cba5aee580522f40925d440b8283
Mugurell <Mugurell@users.noreply.github.com>
2024-10-30 13:45:39 +0000

    Bug 1926874 - Don't crash if ime insets get set before window insets r=android-reviewers,skhan
    Differential Revision: https://phabricator.services.mozilla.com/D227042

--- mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/view/ImeInsetsSynchronizer.kt
+++ mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/view/ImeInsetsSynchronizer.kt
@@ -133,7 +133,8 @@ class ImeInsetsSynchronizer private constructor(
     }
 
     override fun onEnd(animation: WindowInsetsAnimationCompat) {
-        if (areKeyboardInsetsDeferred && (animation.typeMask and ime()) != 0) {
+        val currentInsets = getCurrentInsets()
+        if (currentInsets != null && areKeyboardInsetsDeferred && (animation.typeMask and ime()) != 0) {
             // If we deferred the IME insets and an IME animation has finished, we need to reset the flag
             areKeyboardInsetsDeferred = false
 
@@ -141,7 +142,7 @@ class ImeInsetsSynchronizer private constructor(
             // Ideally we would just call view.requestApplyInsets() and let the normal dispatch
             // cycle happen, but this happens too late resulting in a visual flicker.
             // Instead we manually dispatch the most recent WindowInsets to the view.
-            ViewCompat.dispatchApplyWindowInsets(targetView, lastWindowInsets)
+            ViewCompat.dispatchApplyWindowInsets(targetView, currentInsets)
         }
     }
 
@@ -159,6 +160,11 @@ class ImeInsetsSynchronizer private constructor(
 
     private fun getNavbarHeight() = ViewCompat.getRootWindowInsets(targetView)
         ?.getInsets(systemBars())?.bottom ?: lastWindowInsets.navigationBarInsetHeight
+
+    private fun getCurrentInsets() = when (::lastWindowInsets.isInitialized) {
+        true -> lastWindowInsets
+        false -> ViewCompat.getRootWindowInsets(targetView)
+    }
 
     private fun calculateBottomMargin(
         keyboardHeight: Int,
