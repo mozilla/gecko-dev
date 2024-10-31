ac4b108879995342baf5c2fd82446720396a933d
Ryan VanderMeulen <ryanvm@gmail.com>
2024-10-31 11:30:42 +0000

    Bug 1928246 - Update AndroidX Compose BOM to 2024.10.01. r=android-reviewers,mcarare
    Differential Revision: https://phabricator.services.mozilla.com/D227423

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -59,7 +59,7 @@ object Versions {
         const val browser = "1.8.0"
         const val cardview = "1.0.0"
         const val collection = "1.4.5"
-        const val compose_bom = "2024.10.00"
+        const val compose_bom = "2024.10.01"
         const val constraintlayout = "2.1.4"
         const val constraintlayout_compose = "1.0.1"
         const val coordinatorlayout = "1.2.0"
