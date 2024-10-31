523d20562d77749b70d0661be82656d13c7d32b1
Ryan Hunt <rhunt@eqrion.net>
2024-10-31 20:22:12 +0000

    Bug 1928122 - Add setPrefValue to JS shell. r=jandem
    Adds a setPrefValue that mirrors the getPrefValue function. It is only
    available in the JS shell because the browser has its own way of setting
    a pref.
    
    I had to either define this builtin away from all the other JS pref functions
    in js.cpp, or else move all the JS pref functions up in the file so this
    builtin can be used in shell_functions. I chose to move them all up, but
    I'm fine the other way.
    
    I also renamed the functions from JSPref to just Pref to match the name of
    the builtin, but again I can go the other way too.
    
    Differential Revision: https://phabricator.services.mozilla.com/D227376

--- js/src/jit-test/tests/basic/shell-prefs.js
+++ js/src/jit-test/tests/basic/shell-prefs.js
@@ -1,5 +1,7 @@
 // |jit-test| --setpref=site_based_pretenuring=false; --setpref=tests.uint32-pref=123450
 
+load(libdir + "asserts.js");
+
 let names = getAllPrefNames();
 assertEq(names.length > 0, true, "Expected at least one pref!");
 assertEq(new Set(names).size, names.length, "Unexpected duplicate pref name");
@@ -13,6 +15,15 @@ for (let name of names) {
 // remove it, change this test to check a different one.
 assertEq(getPrefValue("site_based_pretenuring"), false);
 assertEq(getPrefValue("tests.uint32-pref"), 123450);
+
+// We can set prefs at runtime
+setPrefValue("tests.uint32-pref", 54321);
+assertEq(getPrefValue("tests.uint32-pref"), 54321);
+
+// But not prefs that are only set at startup
+assertErrorMessage(() => setPrefValue("site_based_pretenuring", true),
+    Error, /startup pref/);
+assertEq(getPrefValue("site_based_pretenuring"), false);
 
 // Must throw an exception for unknown pref names.
 let ex;
