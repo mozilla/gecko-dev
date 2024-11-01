b285d7983bdbe2e5d2e6f2e6beb1e1daf4fc0260
Alexandru Marc <amarc@mozilla.com>
2024-11-01 16:03:56 +0200

    Backed out changeset 2064995033ce (bug 1918490) for causing mochitest failures @ test_moz_radio_group.html. CLOSED TREE

--- toolkit/content/widgets/lit-utils.mjs
+++ toolkit/content/widgets/lit-utils.mjs
@@ -284,17 +284,14 @@ export class MozBaseInputElement extends MozLitElement {
         rel="stylesheet"
         href="chrome://global/content/elements/moz-input-common.css"
       />
-      <span class="label-wrapper">
-        <label
-          is="moz-label"
-          part="label"
-          for="input"
-          shownaccesskey=${ifDefined(this.accessKey)}
-        >
-          ${this.inputTemplate()}${this.labelTemplate()}
-        </label>
-        ${this.supportLinkTemplate()}
-      </span>
+      <label
+        is="moz-label"
+        part="label"
+        for="input"
+        shownaccesskey=${ifDefined(this.accessKey)}
+      >
+        ${this.inputTemplate()}${this.labelTemplate()}
+      </label>
       ${this.descriptionTemplate()}
     `;
   }
@@ -303,6 +300,7 @@ export class MozBaseInputElement extends MozLitElement {
     return html`<span class="label-content">
       ${this.iconTemplate()}
       <span class="text">${this.label}</span>
+      ${this.supportLinkTemplate()}
     </span>`;
   }
 
