f1a27dc942ac667593679880e0d488909adb69f6
alwu <alwu@mozilla.com>
2024-11-02 19:29:58 +0000

    Bug 1928798 - do not return fatal error if the initialization failed, try again with another state machine. r=jrmuizel, a=dmeehan
    It's possible that the initialization failure is caused by the user's
    device not supporting the media engine pipeline. In this case, we
    should attempt to use another state machine for playback instead of
    simply returning an error.
    
    Differential Revision: https://phabricator.services.mozilla.com/D227773

--- xpcom/base/ErrorList.py
+++ xpcom/base/ErrorList.py
@@ -1191,6 +1191,7 @@ with modules["DOM_MEDIA"]:
     errors["NS_ERROR_DOM_MEDIA_DENIED_IN_NON_UTILITY"] = FAILURE(104)
     errors["NS_ERROR_DOM_MEDIA_RANGE_ERR"] = FAILURE(105)
     errors["NS_ERROR_DOM_MEDIA_TYPE_ERR"] = FAILURE(106)
+    errors["NS_ERROR_DOM_MEDIA_MEDIA_ENGINE_INITIALIZATION_ERR"] = FAILURE(107)
 
 # =======================================================================
 # 42: NS_ERROR_MODULE_URL_CLASSIFIER
