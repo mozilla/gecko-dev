fcfc10db25f317e19d27937b532699a64c6cad9a
Debadree Chatterjee <debadree333@gmail.com>
2024-11-03 08:32:29 +0000

    Bug 1927195 - Part 9: Update existing test262 tests to pass shell option. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D227718

--- js/src/tests/test262/language/statements/await-using/throws-if-initializer-not-object.js
+++ js/src/tests/test262/language/statements/await-using/throws-if-initializer-not-object.js
@@ -1,4 +1,4 @@
-// |reftest| skip-if(!(this.hasOwnProperty('getBuildConfiguration')&&getBuildConfiguration('explicit-resource-management'))) async -- explicit-resource-management is not enabled unconditionally
+// |reftest| shell-option(--enable-explicit-resource-management) skip-if(!(this.hasOwnProperty('getBuildConfiguration')&&getBuildConfiguration('explicit-resource-management'))||!xulRuntime.shell) async -- explicit-resource-management is not enabled unconditionally, requires shell-options
 // Copyright (C) 2023 Ron Buckton. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
