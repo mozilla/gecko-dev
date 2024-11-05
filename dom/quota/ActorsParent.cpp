b6eeaab53e844417f385c2031ace53f2ba31b6bd
Jan Varga <jvarga@mozilla.com>
2024-11-05 18:46:38 +0000

    Bug 1928028 - QM: Add a new artificial failure category; r=dom-storage-reviewers,jstutte,asuth, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D227796

--- dom/quota/ActorsParent.cpp
+++ dom/quota/ActorsParent.cpp
@@ -89,6 +89,7 @@
 #include "mozilla/dom/indexedDB/ActorsParent.h"
 #include "mozilla/dom/ipc/IdType.h"
 #include "mozilla/dom/localstorage/ActorsParent.h"
+#include "mozilla/dom/quota/ArtificialFailure.h"
 #include "mozilla/dom/quota/AssertionsImpl.h"
 #include "mozilla/dom/quota/CheckedUnsafePtr.h"
 #include "mozilla/dom/quota/Client.h"
@@ -3854,6 +3855,9 @@ nsresult QuotaManager::InitializeOrigin(PersistenceType aPersistenceType,
           aOriginMetadata.mOrigin.get()));
 
   AssertIsOnIOThread();
+
+  QM_TRY(
+      ArtificialFailure(nsIQuotaArtificialFailure::CATEGORY_INITIALIZE_ORIGIN));
 
   // The ScopedLogExtraInfo is not set here on purpose, so the callers can
   // decide if they want to set it. The extra info can be set sooner this way
