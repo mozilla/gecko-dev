2b4c3640228fe1e9e0b29f3e4971564a73b41d87
Butkovits Atila <abutkovits@mozilla.com>
2024-11-06 13:42:35 +0200

    Backed out changeset 5696b75841af (bug 1929269) for causing Linting failures. CLOSED TREE

--- ipc/glue/IdleSchedulerParent.cpp
+++ ipc/glue/IdleSchedulerParent.cpp
@@ -105,8 +105,9 @@ void IdleSchedulerParent::CalculateNumIdleTasks() {
   // On one and two processor (or hardware thread) systems this will
   // allow one concurrent idle task.
   sMaxConcurrentIdleTasksInChildProcesses = int32_t(std::max(sNumCPUs, 1u));
-  sMaxConcurrentGCs = std::clamp(sNumCPUs / sPrefConcurrentGCsCPUDivisor, 1u,
-                                 sPrefConcurrentGCsMax);
+  sMaxConcurrentGCs =
+      std::min(std::max(sNumCPUs / sPrefConcurrentGCsCPUDivisor, 1u),
+               sPrefConcurrentGCsMax);
 
   if (sActiveChildCounter && sActiveChildCounter->Memory()) {
     static_cast<Atomic<int32_t>*>(
