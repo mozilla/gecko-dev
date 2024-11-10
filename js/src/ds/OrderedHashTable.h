9981e4c0bbeadfe3b3be5e7b8f7489f26905d6c9
Jan de Mooij <jdemooij@mozilla.com>
2024-11-10 09:06:33 +0000

    Bug 1927405 - Simplify and optimize OrderedHashTable::clear. r=jonco
    Change `clear` to destroy all elements and then shrink the table only if needed,
    similar to how `remove` is implemented.
    
    This eliminates a `free` and `malloc` if there are only a few elements but it's also
    simpler because it avoids calling `init` more than once which is a footgun.
    
    Differential Revision: https://phabricator.services.mozilla.com/D227011

--- js/src/ds/OrderedHashTable.h
+++ js/src/ds/OrderedHashTable.h
@@ -121,6 +121,8 @@ class OrderedHashTable {
   // Logarithm base 2 of the number of buckets in the hash table initially.
   static constexpr uint32_t InitialBucketsLog2 = 1;
   static constexpr uint32_t InitialBuckets = 1 << InitialBucketsLog2;
+  static constexpr uint32_t InitialHashShift =
+      js::kHashNumberBits - InitialBucketsLog2;
 
   // The maximum load factor (mean number of entries per bucket).
   // It is an invariant that
@@ -202,14 +204,12 @@ class OrderedHashTable {
 
     std::uninitialized_fill_n(tableAlloc, buckets, nullptr);
 
-    // clear() requires that members are assigned only after all allocation
-    // has succeeded, and that this->ranges_ is left untouched.
     hashTable_ = tableAlloc;
     data_ = dataAlloc;
     dataLength_ = 0;
     dataCapacity_ = capacity;
     liveCount_ = 0;
-    hashShift_ = js::kHashNumberBits - InitialBucketsLog2;
+    hashShift_ = InitialHashShift;
     MOZ_ASSERT(hashBuckets() == buckets);
     return true;
   }
@@ -344,8 +344,9 @@ class OrderedHashTable {
   /*
    * Remove all entries.
    *
-   * Returns false on OOM, leaving the OrderedHashTable and any live Ranges
-   * in the old state.
+   * Return true on success, false if we tried to shrink the table and hit an
+   * allocation failure. Even if this returns false, the table was cleared.
+   * Shrinking is an optimization and it's OK for it to fail.
    *
    * The effect on live Ranges is the same as removing all entries; in
    * particular, those Ranges are still live and will see any entries added
@@ -353,21 +354,21 @@ class OrderedHashTable {
    */
   [[nodiscard]] bool clear() {
     if (dataLength_ != 0) {
-      Data** oldHashTable = hashTable_;
-      Data* oldData = data_;
-      uint32_t oldHashBuckets = hashBuckets();
-      uint32_t oldDataLength = dataLength_;
-      uint32_t oldDataCapacity = dataCapacity_;
+      destroyData(data_, dataLength_);
+      dataLength_ = 0;
+      liveCount_ = 0;
 
-      hashTable_ = nullptr;
-      if (!init()) {
-        // init() only mutates members on success; see comment above.
-        hashTable_ = oldHashTable;
-        return false;
-      }
+      size_t buckets = hashBuckets();
+      std::fill_n(hashTable_, buckets, nullptr);
 
-      freeData(oldData, oldDataLength, oldDataCapacity, oldHashBuckets);
       forEachRange([](Range* range) { range->onClear(); });
+
+      // Try to shrink the table.
+      if (buckets > InitialBuckets) {
+        if (!rehash(InitialHashShift)) {
+          return false;
+        }
+      }
     }
 
     MOZ_ASSERT(hashTable_);
