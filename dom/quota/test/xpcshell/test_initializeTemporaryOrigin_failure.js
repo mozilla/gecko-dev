a6f73e6ce116775a4777db192352d1cc51612f6c
Jan Varga <jvarga@mozilla.com>
2024-11-13 00:38:29 +0000

    Bug 1929190 - QM: Add a disabled subtest for a failure during temporary origin initialization; r=dom-storage-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D228144

--- dom/quota/test/xpcshell/test_initializeTemporaryOrigin_failure.js
+++ dom/quota/test/xpcshell/test_initializeTemporaryOrigin_failure.js
@@ -9,6 +9,9 @@ const { PrincipalUtils } = ChromeUtils.importESModule(
 const { QuotaUtils } = ChromeUtils.importESModule(
   "resource://testing-common/dom/quota/test/modules/QuotaUtils.sys.mjs"
 );
+const { SimpleDBUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/dom/simpledb/test/modules/SimpleDBUtils.sys.mjs"
+);
 
 async function testCachedOrigins() {
   const origin = "https://example.com";
@@ -93,7 +96,89 @@ async function testCachedOrigins() {
   }
 }
 
+async function testCreateConnection() {
+  const principal = PrincipalUtils.createPrincipal("https://example.com");
+  const name = "test_initializeTemporaryOrigin_failure.js";
+
+  info("Clearing");
+
+  {
+    const request = Services.qms.clear();
+    await QuotaUtils.requestFinished(request);
+  }
+
+  info("Initializing");
+
+  {
+    const request = Services.qms.init();
+    await QuotaUtils.requestFinished(request);
+  }
+
+  info("Initializing temporary storage");
+
+  {
+    const request = Services.qms.initTemporaryStorage();
+    await QuotaUtils.requestFinished(request);
+  }
+
+  info("Initializing group");
+
+  {
+    const request = Services.qms.initializeTemporaryGroup(principal);
+    await QuotaUtils.requestFinished(request);
+  }
+
+  info("Initializing origin");
+
+  const ex = await QuotaUtils.withArtificialFailures(
+    Ci.nsIQuotaArtificialFailure.CATEGORY_CREATE_DIRECTORY_METADATA2,
+    /* probability */ 100,
+    Cr.NS_ERROR_UNEXPECTED,
+    async function () {
+      const request = Services.qms.initializeTemporaryOrigin(
+        "default",
+        principal,
+        /* aCreateIfNonExistent */ true
+      );
+      try {
+        await QuotaUtils.requestFinished(request);
+        return null;
+      } catch (ex) {
+        return ex;
+      }
+    }
+  );
+
+  Assert.ok(ex, "Should have thrown");
+  Assert.strictEqual(
+    ex.resultCode,
+    NS_ERROR_UNEXPECTED,
+    "Threw right result code"
+  );
+
+  info("Opening database");
+
+  {
+    const connection = SimpleDBUtils.createConnection(principal);
+    const request = connection.open(name);
+    await SimpleDBUtils.requestFinished(request);
+  }
+
+  info("Clearing storage");
+
+  {
+    const request = Services.qms.clear();
+    await QuotaUtils.requestFinished(request);
+  }
+}
+
 /* exported testSteps */
 async function testSteps() {
   add_task(testCachedOrigins);
+  add_task(
+    {
+      skip_if: () => true,
+    },
+    testCreateConnection
+  );
 }
