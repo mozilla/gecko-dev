c160b9197b92542ce889e61f1c941d876ae18430
Beth Rennie <beth@brennie.ca>
2024-11-13 20:36:31 +0000

    Bug 1929116 - Update NimbusEnrollment.schema.json and store new Firefox Labs fields in saved enrolments r=chumphreys
    Differential Revision: https://phabricator.services.mozilla.com/D228723

--- toolkit/components/nimbus/lib/ExperimentManager.sys.mjs
+++ toolkit/components/nimbus/lib/ExperimentManager.sys.mjs
@@ -529,6 +529,9 @@ export class _ExperimentManager {
       featureIds,
       isRollout,
       localizations,
+      isFirefoxLabsOptIn,
+      firefoxLabsTitle,
+      firefoxLabsDescription,
     },
     branch,
     source,
@@ -578,6 +581,14 @@ export class _ExperimentManager {
 
     if (localizations) {
       experiment.localizations = localizations;
+    }
+
+    if (typeof isFirefoxLabsOptIn !== "undefined") {
+      Object.assign(experiment, {
+        isFirefoxLabsOptIn,
+        firefoxLabsTitle,
+        firefoxLabsDescription,
+      });
     }
 
     if (typeof isRollout !== "undefined") {
