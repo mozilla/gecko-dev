d120154203b1007a09e446c7b3d9621b1e49ba35
Debadree Chatterjee <debadree333@gmail.com>
2024-11-13 06:03:50 +0000

    Bug 1929055 - Add tests for xray on explicit resource management globals. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D228434

--- toolkit/modules/AppConstants.sys.mjs
+++ toolkit/modules/AppConstants.sys.mjs
@@ -19,6 +19,13 @@ export var AppConstants = Object.freeze({
   false,
 #endif
 
+  ENABLE_EXPLICIT_RESOURCE_MANAGEMENT:
+#ifdef ENABLE_EXPLICIT_RESOURCE_MANAGEMENT
+  true,
+#else
+  false,
+#endif
+
   RELEASE_OR_BETA:
 #ifdef RELEASE_OR_BETA
   true,
