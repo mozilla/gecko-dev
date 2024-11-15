ff33e4dde6d5595e6770a119cf34aae31ff1aff0
Yannis Juglaret <yjuglaret@mozilla.com>
2024-11-15 17:49:50 +0000

    Bug 1900726 - Take the update mutex through a child process in the canCheckForAndApplyUpdates.js test. r=application-update-reviewers,nalexander,bytesized
    In order to properly test the new update mutex, we need at least one
    test that acquires it from a different process. This patch adapts the
    canCheckForAndApplyUpdates.js test so that it does. We achieve this by
    refactoring and recycling the Subprocess helpers used in the
    updateSyncManager.js test.
    
    Depends on D222157
    
    Differential Revision: https://phabricator.services.mozilla.com/D226748

--- toolkit/mozapps/update/tests/data/syncManagerTestChild.js
+++ toolkit/mozapps/update/tests/data/syncManagerTestChild.js
@@ -7,47 +7,15 @@
 // The main thing this script does is override the child's directory service
 // so that it ends up with the same fake binary path that the parent test runner
 // has opened its update lock with.
-// This requires that we have already been passed a constant on our command
-// line which contains the relevant fake binary path, which is called:
-/* global customExePath */
+// This requires that our command line defines of the relevant helper functions
+// from xpcshellUtilsAUS.js, which are:
+/* global registerCustomDirProvider */
+/* global resetSyncManagerLock */
 
 print("child process is running");
 
-// This function is copied from xpcshellUtilsAUS.js so that we can have our
-// xpcshell subprocess call it without having to load that whole file, because
-// it turns out that needs a bunch of infrastructure that normally the testing
-// framework would provide, and that also requires a bunch of setup, and it's
-// just not worth all that. This is a cut down version that only includes the
-// directory provider functionality that the subprocess really needs.
-function adjustGeneralPaths() {
-  let dirProvider = {
-    getFile: function AGP_DP_getFile(aProp, aPersistent) {
-      // Set the value of persistent to false so when this directory provider is
-      // unregistered it will revert back to the original provider.
-      aPersistent.value = false;
-      // The sync manager only needs XREExeF, so that's all we provide.
-      if (aProp == "XREExeF") {
-        let file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
-        file.initWithPath(customExePath);
-        return file;
-      }
-      return null;
-    },
-    QueryInterface: ChromeUtils.generateQI(["nsIDirectoryServiceProvider"]),
-  };
-  let ds = Services.dirsvc.QueryInterface(Ci.nsIDirectoryService);
-  ds.QueryInterface(Ci.nsIProperties).undefine("XREExeF");
-  ds.registerProvider(dirProvider);
-
-  // Now that we've overridden the directory provider, the name of the update
-  // lock needs to be changed to match the overridden path.
-  let syncManager = Cc["@mozilla.org/updates/update-sync-manager;1"].getService(
-    Ci.nsIUpdateSyncManager
-  );
-  syncManager.resetLock();
-}
-
-adjustGeneralPaths();
+registerCustomDirProvider();
+resetSyncManagerLock();
 
 // Wait a few seconds for the parent to do what it needs to do, then exit.
 print("child process should now have the lock; will exit in 5 seconds");
