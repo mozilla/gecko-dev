290918b09942f95223dc02aac918fbe901e3ce1d
Markus Stange <mstange.moz@gmail.com>
2024-11-19 14:36:52 +0000

    Bug 1929913 - Bump the Gecko profile format version to 31. r=profiler-reviewers,julienw
    The corresponding front-end bump is happening in https://github.com/firefox-devtools/profiler/pull/5186 .
    
    Bumping the version to 31 means that our code can emit profiles which
    use the marker field formats 'flow-id' and 'terminating-flow-id' without
    running into trouble on the front-end side.
    
    Differential Revision: https://phabricator.services.mozilla.com/D228348

--- mozglue/baseprofiler/public/BaseAndGeckoProfilerDetail.h
+++ mozglue/baseprofiler/public/BaseAndGeckoProfilerDetail.h
@@ -24,7 +24,7 @@ namespace mozilla {
 class ProfileBufferChunkManagerWithLocalLimit;
 
 // Centrally defines the version of the gecko profiler JSON format.
-const int GECKO_PROFILER_FORMAT_VERSION = 30;
+const int GECKO_PROFILER_FORMAT_VERSION = 31;
 
 namespace baseprofiler::detail {
 
