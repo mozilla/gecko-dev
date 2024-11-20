609db0d83bdcbb854404d3fd43d692dfbdb40714
Nico Grunbaum <na-g@nostrum.com>
2024-11-20 22:32:49 +0000

    Bug 1932488 - update trampoline headers with current mode-line;r=mjf,webrtc-reviewers DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D229701

--- third_party/libwebrtc/third_party/opus/src/include/opus.h
+++ third_party/libwebrtc/third_party/opus/src/include/opus.h
@@ -1,7 +1,7 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * vim: set ts=8 sts=2 et sw=2 tw=80:
- *
- * Copyright 2023 Mozilla Foundation
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/*
+ * Copyright 2024 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
