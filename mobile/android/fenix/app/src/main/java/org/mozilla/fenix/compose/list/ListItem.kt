8c51927dbdbf15779995ce8ebd024ee24fa75cc0
Norisz Fay <nfay@mozilla.com>
2024-11-20 13:00:43 +0200

    Backed out changeset f7e09524c9bd (bug 1915867) for causing multiple wpt failures CLOSED TREE

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/compose/list/ListItem.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/compose/list/ListItem.kt
@@ -44,8 +44,6 @@ import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.selected
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -807,11 +805,7 @@ private fun ListItem(
                     color = if (enabled) descriptionTextColor else FirefoxTheme.colors.textDisabled,
                     overflow = TextOverflow.Ellipsis,
                     maxLines = maxDescriptionLines,
-                    // Bug 1915867 - We must force the text direction to correctly truncate a LTR description that is
-                    // too long when the app in RTL mode - at least until this bug gets fixed in Compose. This isn't
-                    // the most optional solution but it should have less side-effects than forcing no letter spacing
-                    // (which would be the best approach here).
-                    style = FirefoxTheme.typography.body2.merge(TextStyle(textDirection = TextDirection.Content)),
+                    style = FirefoxTheme.typography.body2,
                 )
             }
         }
