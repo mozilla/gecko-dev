f810b391acaff1179e9d3aef75a1a09da8f744e8
Peter Van der Beken <peterv@propagandism.org>
2024-11-21 04:39:14 +0000

    Bug 1769586 - Small build bustage fix: replace ArrayLength with std::size. r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D229484

--- accessible/base/ARIAMap.cpp
+++ accessible/base/ARIAMap.cpp
@@ -1741,7 +1741,7 @@ bool AttrIterator::ExposeAttr(AccAttributes* aTargetAttrs) const {
 ////////////////////////////////////////////////////////////////////////////////
 // AttrWithCharacteristicsIterator class
 bool AttrWithCharacteristicsIterator::Next() {
-  for (mIdx++; mIdx < static_cast<int32_t>(ArrayLength(gWAIUnivAttrMap));
+  for (mIdx++; mIdx < static_cast<int32_t>(std::size(gWAIUnivAttrMap));
        mIdx++) {
     if (gWAIUnivAttrMap[mIdx].characteristics & mCharacteristics) {
       return true;
