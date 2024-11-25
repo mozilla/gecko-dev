b03c1944ee9ddf75d1c863deb96953418a1e58ae
longsonr <longsonr@gmail.com>
2024-11-25 01:16:48 +0000

    Bug 1933071 - Use NS_Hypot to calculate the transform list distance r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D230042

--- dom/svg/SVGTransformListSMILType.cpp
+++ dom/svg/SVGTransformListSMILType.cpp
@@ -8,6 +8,7 @@
 
 #include "mozilla/SMILValue.h"
 #include "nsCRT.h"
+#include "nsMathUtils.h"
 #include "SVGTransformList.h"
 #include "SVGTransform.h"
 #include <math.h>
@@ -199,7 +200,7 @@ nsresult SVGTransformListSMILType::ComputeDistance(const SMILValue& aFrom,
       const float& a_ty = fromTransform.mParams[1];
       const float& b_tx = toTransform.mParams[0];
       const float& b_ty = toTransform.mParams[1];
-      aDistance = sqrt(pow(a_tx - b_tx, 2) + (pow(a_ty - b_ty, 2)));
+      aDistance = NS_hypot(a_tx - b_tx, a_ty - b_ty);
     } break;
 
     case SVG_TRANSFORM_ROTATE:
