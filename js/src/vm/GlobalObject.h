a5490e9568f77feb8eb520db4bcb610cfc240b77
Matthew Gaudet <mgaudet@mozilla.com>
2024-11-25 18:49:32 +0000

    Bug 1928852 - Guard against double initialization of protos r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D228736

--- js/src/vm/GlobalObject.h
+++ js/src/vm/GlobalObject.h
@@ -290,6 +290,9 @@ class GlobalObject : public NativeObject {
 
   void initBuiltinProto(ProtoKind kind, JSObject* proto) {
     MOZ_ASSERT(proto);
+    // Catch double-initialization; however if this is too much of a burden due to
+    // OOM handling it could be removed. 
+    MOZ_ASSERT(!hasBuiltinProto(kind));
     data().builtinProtos[kind].init(proto);
   }
   bool hasBuiltinProto(ProtoKind kind) const {
