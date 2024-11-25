af9ea16aa653930008f231e82d2852faa8c00c96
Ryan VanderMeulen <ryanvm@gmail.com>
2024-11-25 23:36:53 +0000

    Bug 1932464 - Update Android bundletool and commandlinetools to the latest releases. r=geckoview-reviewers,calu
    Differential Revision: https://phabricator.services.mozilla.com/D229672

--- python/mozboot/mozboot/android.py
+++ python/mozboot/mozboot/android.py
@@ -21,10 +21,10 @@ from tqdm import tqdm
 from mozboot.bootstrap import MOZCONFIG_SUGGESTION_TEMPLATE
 
 NDK_VERSION = "r27c"
-CMDLINE_TOOLS_VERSION_STRING = "12.0"
-CMDLINE_TOOLS_VERSION = "11076708"
+CMDLINE_TOOLS_VERSION_STRING = "16.0"
+CMDLINE_TOOLS_VERSION = "12266719"
 
-BUNDLETOOL_VERSION = "1.17.0"
+BUNDLETOOL_VERSION = "1.17.2"
 
 # We expect the emulator AVD definitions to be platform agnostic
 LINUX_X86_64_ANDROID_AVD = "linux64-android-avd-x86_64-repack"
