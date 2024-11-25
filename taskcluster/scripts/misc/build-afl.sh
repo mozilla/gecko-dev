7c6d8e389a412e3ba8493709e8d4be59e2f30c02
Jesse Schwartzentruber <truber@mozilla.com>
2024-11-25 14:32:05 +0000

    Bug 1932656 - Add all AFL++ programs to the afl toolchain r=firefox-build-system-reviewers,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D229801

--- taskcluster/scripts/misc/build-afl.sh
+++ taskcluster/scripts/misc/build-afl.sh
@@ -6,10 +6,22 @@ artifact="$(basename "$TOOLCHAIN_ARTIFACT")"
 dir="${artifact%.tar.*}"
 scripts="$(realpath "${0%/*}")"
 
+# these lists are copied from AFLplusplus/GNUmakefile
+PROGS="afl-fuzz afl-showmap afl-tmin afl-gotcpu afl-analyze"
+SH_PROGS="afl-plot afl-cmin afl-cmin.bash afl-whatsup afl-addseeds afl-system-config afl-persistent-config"
+
 cd "$MOZ_FETCHES_DIR/AFLplusplus"
 patch -p1 -i "$scripts/afl-nyx.patch"
-make -f GNUmakefile afl-showmap \
-    CC="$MOZ_FETCHES_DIR/clang/bin/clang"
+
+make -f GNUmakefile $PROGS \
+    CC="$MOZ_FETCHES_DIR/clang/bin/clang" \
+    CFLAGS="--sysroot $MOZ_FETCHES_DIR/sysroot" \
+    CODE_COVERAGE=1 \
+    NO_PYTHON=1 \
+    PREFIX=/
+mkdir -p "$dir/bin"
+install -m 755 $PROGS $SH_PROGS "$dir/bin"
+
 make -f GNUmakefile.llvm install \
     CODE_COVERAGE=1 \
     CPPFLAGS="--sysroot $MOZ_FETCHES_DIR/sysroot" \
