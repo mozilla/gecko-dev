285bd0204d2b53def700896976a8ec2d0eaf7575
Zac McKenney <zmckenney@mozilla.com>
2024-11-26 17:08:56 +0000

    Bug 1929525 - Add ellipses to email overflow in menu redesign r=android-reviewers,giorga
    Differential Revision: https://phabricator.services.mozilla.com/D228748

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/header/MozillaAccountMenuButton.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/header/MozillaAccountMenuButton.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import mozilla.components.service.fxa.manager.AccountState
@@ -110,6 +111,7 @@ internal fun MozillaAccountMenuButton(
                         color = FirefoxTheme.colors.textSecondary,
                         maxLines = 1,
                         style = FirefoxTheme.typography.headline7,
+                        overflow = TextOverflow.Ellipsis,
                     )
                 }
 
