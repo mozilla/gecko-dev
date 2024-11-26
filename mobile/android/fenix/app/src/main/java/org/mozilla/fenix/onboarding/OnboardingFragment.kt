c4de3aede9bcbfc4255796b32d3031a29e4377c5
Sandor Molnar <smolnar@mozilla.com>
2024-11-26 05:25:02 +0200

    Backed out changeset cef47336c034 (bug 1933232) for causing Fenix ui-test failures CLOSED TREE

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/onboarding/OnboardingFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/onboarding/OnboardingFragment.kt
@@ -29,7 +29,6 @@ import mozilla.components.support.base.log.logger.Logger
 import mozilla.components.support.utils.BrowsersCache
 import org.mozilla.fenix.R
 import org.mozilla.fenix.components.accounts.FenixFxAEntryPoint
-import org.mozilla.fenix.components.initializeGlean
 import org.mozilla.fenix.components.lazyStore
 import org.mozilla.fenix.compose.LinkTextState
 import org.mozilla.fenix.ext.components
@@ -255,14 +254,6 @@ class OnboardingFragment : Fragment() {
         }
 
         requireComponents.fenixOnboarding.finish()
-
-        initializeGlean(
-            requireContext().applicationContext,
-            logger,
-            requireContext().settings().isTelemetryEnabled,
-            requireComponents.core.client,
-        )
-
         findNavController().nav(
             id = R.id.onboardingFragment,
             directions = OnboardingFragmentDirections.actionHome(),
