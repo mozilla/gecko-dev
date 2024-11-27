83728637a456b2cb69b639107ed67599002cd116
Nico Grunbaum <na-g@nostrum.com>
2024-11-27 03:41:26 +0000

    Bug 1932488 - update trampoline headers with current mode-line;r=mjf,webrtc-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D229701

--- third_party/libwebrtc/third_party/libaom/source/libaom/aom/aom_codec.h
+++ third_party/libwebrtc/third_party/libaom/source/libaom/aom/aom_codec.h
@@ -1,6 +1,6 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * vim: set ts=8 sts=2 et sw=2 tw=80:
- *
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/*
  * Copyright 2024 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
