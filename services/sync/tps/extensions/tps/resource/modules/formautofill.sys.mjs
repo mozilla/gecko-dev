af7de05319ff39a4b5beb262ca8531dc6082fedd
Tooru Fujisawa <arai_a@mac.com>
2024-11-28 17:50:12 +0000

    Bug 1934043 - Update the TPS comment. r=markh,sync-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D230558

--- services/sync/tps/extensions/tps/resource/modules/formautofill.sys.mjs
+++ services/sync/tps/extensions/tps/resource/modules/formautofill.sys.mjs
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* This is a JavaScript module (JSM) to be imported via
- * ChromeUtils.import() and acts as a singleton. Only the following
+/* This is a JavaScript module to be imported via
+ * ChromeUtils.importESModule() and acts as a singleton. Only the following
  * listed symbols will exposed on import, and only when and where imported.
  */
 
