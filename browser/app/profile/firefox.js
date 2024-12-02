613b94fd54a31df3ef1c2d00fe1865542e8cba1e
Emilio Cobos Álvarez <emilio@crisal.io>
2024-12-02 20:19:46 +0000

    Bug 1934640 - Don't let the rounded content area ride the trains for now. r=sidebar-reviewers,sfoster
    It breaks picture caching (bug 1933527), and has a known issue with
    backdrop-filter (bug 1934039), so let's put behind a pref for now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D230807

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2021,6 +2021,12 @@ pref("pdfjs.handleOctetStream", true);
 // Is the sidebar positioned ahead of the content browser
 pref("sidebar.position_start", true);
 pref("sidebar.revamp", false);
+// This is nightly only for now, as we need to address bug 1933527 and bug 1934039.
+#ifdef NIGHTLY_BUILD
+pref("sidebar.revamp.round-content-area", true);
+#else
+pref("sidebar.revamp.round-content-area", false);
+#endif
 pref("sidebar.animation.enabled", true);
 pref("sidebar.animation.duration-ms", 200);
 pref("sidebar.main.tools", "aichat,syncedtabs,history");
