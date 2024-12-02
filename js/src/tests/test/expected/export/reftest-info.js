71a7d7ff78d35eb07089810da8dd5e1edab172f3
Ms2ger <Ms2ger@gmail.com>
2024-12-02 15:58:04 +0000

    Bug 1928619 - test262-export: improve formatting of generated info in frontmatter; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D227646

---
+++ js/src/tests/test/expected/export/reftest-info.js
@@ -0,0 +1,18 @@
+// Copyright (C) 2024 Igalia S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+info: |
+  Information
+
+  foo bar  baz
+flags:
+- noStrict
+description: |
+  pending
+esid: pending
+---*/
+function f() {
+    let
+    await 0;
+}
---
+++ js/src/tests/test/fixtures/export/reftest-info.js
@@ -0,0 +1,12 @@
+// |reftest| -- foo bar  baz
+// Copyright (C) 2024 Igalia S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+info: Information
+---*/
+
+function f() {
+    let
+    await 0;
+}
