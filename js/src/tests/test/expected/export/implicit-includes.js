7ee33017584054171cc2740f611a76fc392fd941
Ms2ger <Ms2ger@gmail.com>
2024-12-02 15:58:04 +0000

    Bug 1928619 - test262-export: automatically translate some helper methods; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D227647

---
+++ js/src/tests/test/expected/export/implicit-includes.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2024 Igalia S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+includes: [compareArray.js, deepEqual.js]
+flags:
+- noStrict
+description: |
+  pending
+esid: pending
+---*/
+assert.sameValue("", "");
+assert.deepEqual("", "");
+assert.compareArray([], []);
+$262.detachArrayBuffer(new ArrayBuffer());
+createNewGlobal();
