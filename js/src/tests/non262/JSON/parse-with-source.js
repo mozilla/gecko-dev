b0440e633184e0b1bc71310a84a41eb418531c46
Ms2ger <Ms2ger@gmail.com>
2024-12-03 15:55:07 +0000

    Bug 1928619 - non262 tests: split proprietary tests for JSON.rawJSON into their own file; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D227614

--- js/src/tests/non262/JSON/parse-with-source.js
+++ js/src/tests/non262/JSON/parse-with-source.js
@@ -103,34 +103,6 @@
     assertEq(true, JSON.isRawJSON(JSON.rawJSON(1), JSON.rawJSON(2)));
 })();
 
-(function checkCrossCompartmentWrappers() {
-    var gbl = newGlobal({newCompartment: true});
-
-    // the created context object should be wrapped in this compartment
-    gbl.eval("context = JSON.parse('4.0', (k,v,context) => context);");
-    assertEq(isCCW(gbl.context), true);
-    // an object created in the reviver function should be wrapped in this compartment
-    gbl.eval("sourceV = JSON.parse('4.0', (k,v,context) => { return {}; });");
-    assertEq(isCCW(gbl.sourceV), true);
-
-    // objects created by a reviver in this compartment should be wrapped in
-    // the other compartment
-    gbl.rev = (k,v,c) => { return {v}};
-    gbl.eval("v2 = JSON.parse('4.0', rev);");
-    assertEq(gbl.eval("isCCW(v2)"), true);
-
-    gbl.eval("objCCW = {};");
-    assertEq(JSON.isRawJSON(gbl.objCCW), false, "isRawJSON() should accept CCW arguments");
-    rawJSONCCW = gbl.eval("JSON.rawJSON(455);");
-    assertEq(JSON.isRawJSON(rawJSONCCW), true, "isRawJSON() should return true for wrapped rawJSON objects");
-
-    assertEq(rawJSONCCW.rawJSON, "455", "rawJSON object enumerable property should be visible through CCW");
-
-    objWithCCW = { ccw: rawJSONCCW, raw: JSON.rawJSON(true) };
-    assertEq(JSON.stringify(objWithCCW), '{"ccw":455,"raw":true}');
-    assertEq(isCCW(rawJSONCCW), true);
-})();
-
 (function checkUseAsPrototype() {
     var p = JSON.rawJSON(false);
     var obj = { a: "hi" };
