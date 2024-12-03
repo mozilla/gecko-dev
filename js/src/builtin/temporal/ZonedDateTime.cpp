4b786900e9e8214a7a57d6116c6ba720de162b7e
André Bargull <andre.bargull@gmail.com>
2024-12-03 07:45:39 +0000

    Bug 1929284 - Part 21: Nanoseconds difference between two consecutive days fits into int64. r=dminor
    We don't have to use `Int128`, because without user-defined time zones, it's guaranteed
    that the values fit into `int64_t`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D228022

--- js/src/builtin/temporal/ZonedDateTime.cpp
+++ js/src/builtin/temporal/ZonedDateTime.cpp
@@ -1819,11 +1819,13 @@ static bool ZonedDateTime_hoursInDay(JSContext* cx, const CallArgs& args) {
 
   // Step 9.
   auto diff = tomorrowNs - todayNs;
-  MOZ_ASSERT(IsValidInstantSpan(diff));
+  MOZ_ASSERT(diff.abs().toNanoseconds() <
+             Int128{ToNanoseconds(TemporalUnit::Day) * 2});
 
   // Step 10.
-  constexpr auto nsPerHour = Int128{ToNanoseconds(TemporalUnit::Hour)};
-  args.rval().setNumber(FractionToDouble(diff.toNanoseconds(), nsPerHour));
+  constexpr auto nsPerHour = ToNanoseconds(TemporalUnit::Hour);
+  args.rval().setNumber(
+      FractionToDouble(int64_t(diff.toNanoseconds()), nsPerHour));
   return true;
 }
 
