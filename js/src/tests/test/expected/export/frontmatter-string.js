7ae45e640b41d3d3a8cd62299e533c91a1b32554
Ms2ger <Ms2ger@gmail.com>
2024-12-03 15:55:18 +0000

    Bug 1928619 - test262-export: support plain-text frontmatter; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D227641

---
+++ js/src/tests/test/expected/export/frontmatter-string.js
@@ -0,0 +1,18 @@
+// Copyright (C) 2024 Igalia S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+info: |
+  Information
+flags:
+- noStrict
+description: |
+  pending
+esid: pending
+---*/
+async function f() {
+    let
+    await 0;
+}
+
+assert.sameValue(true, f instanceof Function);
