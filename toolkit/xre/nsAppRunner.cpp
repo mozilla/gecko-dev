0761c5dbf9b82a29227100185887c578a7804cbd
Dave Townsend <dtownsend@oxymoronical.com>
2024-12-03 15:32:07 +0000

    Bug 1933261: Fix the profile missing and downgrade UIs to not hang on startup.  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D230256
    
    Differential Revision: https://phabricator.services.mozilla.com/D230963

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -2626,6 +2626,10 @@ static nsresult ProfileMissingDialog(nsINativeAppSupport* aNative) {
   rv = xpcom.SetWindowCreator(aNative);
   NS_ENSURE_SUCCESS(rv, NS_ERROR_FAILURE);
 
+#  ifdef XP_MACOSX
+  InitializeMacApp();
+#  endif
+
   {  // extra scoping is needed so we release these components before xpcom
      // shutdown
     nsCOMPtr<nsIStringBundleService> sbs =
@@ -2656,7 +2660,7 @@ static nsresult ProfileMissingDialog(nsINativeAppSupport* aNative) {
 
     return NS_ERROR_ABORT;
   }
-#endif    // MOZ_WIDGET_ANDROID
+#endif  // MOZ_WIDGET_ANDROID
 }
 
 static ReturnAbortOnError ProfileLockedDialog(nsIFile* aProfileDir,
@@ -3232,6 +3236,10 @@ static ReturnAbortOnError CheckDowngrade(nsIFile* aProfileDir,
 
     rv = xpcom.SetWindowCreator(aNative);
     NS_ENSURE_SUCCESS(rv, rv);
+
+#  ifdef XP_MACOSX
+    InitializeMacApp();
+#  endif
 
     {  // extra scoping is needed so we release these components before xpcom
        // shutdown
