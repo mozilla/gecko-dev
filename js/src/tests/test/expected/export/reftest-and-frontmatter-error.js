9e13ac805798c503a2621c12ba544de87bdb9f15
Ms2ger <Ms2ger@gmail.com>
2024-12-03 15:55:19 +0000

    Bug 1928619 - test262-export: make frontmatter formatting more robust; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D227643

--- js/src/tests/test/expected/export/reftest-and-frontmatter-error.js
+++ js/src/tests/test/expected/export/reftest-and-frontmatter-error.js
@@ -4,9 +4,7 @@
 /*---
 esid: sec-let-and-const-declarations
 description: |
-  Outside AsyncFunction, |await| is a perfectly cromulent LexicalDeclaration variable
-  name.  Therefore ASI doesn't apply, and so the |0| where a |=| was expected is a
-  syntax error.
+  Outside AsyncFunction, |await| is a perfectly cromulent LexicalDeclaration variable name.  Therefore ASI doesn't apply, and so the |0| where a |=| was expected is a syntax error.
 negative:
   phase: runtime
   type: SyntaxError
--- js/src/tests/test/expected/export/reftest-error-syntaxerror.js
+++ js/src/tests/test/expected/export/reftest-error-syntaxerror.js
@@ -5,9 +5,7 @@
 author: Jeff Walden <jwalden+code@mit.edu>
 esid: sec-let-and-const-declarations
 description: |
-  Outside AsyncFunction, |await| is a perfectly cromulent LexicalDeclaration variable
-  name.  Therefore ASI doesn't apply, and so the |0| where a |=| was expected is a
-  syntax error.
+  Outside AsyncFunction, |await| is a perfectly cromulent LexicalDeclaration variable name.  Therefore ASI doesn't apply, and so the |0| where a |=| was expected is a syntax error.
 flags:
 - noStrict
 features: []
