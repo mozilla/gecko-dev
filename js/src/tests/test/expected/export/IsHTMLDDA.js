2731ff404ce99b74df1c4601f354d4e037fc14c2
Ms2ger <Ms2ger@gmail.com>
2024-12-03 15:55:21 +0000

    Bug 1928619 - test262-export: automatically flag IsHTMLDDA use; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D227648

---
+++ js/src/tests/test/expected/export/IsHTMLDDA.js
@@ -0,0 +1,13 @@
+// Copyright (C) 2024 Igalia S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+features:
+- IsHTMLDDA
+flags:
+- noStrict
+description: |
+  pending
+esid: pending
+---*/
+var dda = createIsHTMLDDA();
