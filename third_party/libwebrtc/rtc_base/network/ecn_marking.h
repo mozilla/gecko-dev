f457ade5b41936d362104139e3c160da57813385
Michael Froman <mfroman@mozilla.com>
2024-12-04 21:03:08 -0600

    Bug 1934695 - Vendor libwebrtc from 3073c4809c
    Upstream commit: https://webrtc.googlesource.com/src/+/3073c4809cd41081b08f8f2aed61d18df9612360
        Move rtc_base/network/ecn_marking.h to api/transport
    
        For now, old file forward include api/transport/ecn_marking.h
        Done in preparation for more usage of this enum when handling received
        RFC8888 feedback.
    
        Bug: webrtc:42225697
        Change-Id: I022c2b7f1e7f986b24aa32b8911ad67c6640a5c0
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/366440
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Per Kjellander <perkj@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#43282}

--- third_party/libwebrtc/rtc_base/network/ecn_marking.h
+++ third_party/libwebrtc/rtc_base/network/ecn_marking.h
@@ -7,36 +7,14 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
-
 #ifndef RTC_BASE_NETWORK_ECN_MARKING_H_
 #define RTC_BASE_NETWORK_ECN_MARKING_H_
 
+// // TODO: bugs.webrtc.org/42225697 - delete this file.
+#include "api/transport/ecn_marking.h"
+
 namespace rtc {
-
-// TODO(https://bugs.webrtc.org/15368): L4S support is slowly being developed.
-// Help is appreciated.
-
-// L4S Explicit Congestion Notification (ECN) .
-// https://www.rfc-editor.org/rfc/rfc9331.html ECT stands for ECN-Capable
-// Transport and CE stands for Congestion Experienced.
-
-// RFC-3168, Section 5
-// +-----+-----+
-// | ECN FIELD |
-// +-----+-----+
-//   ECT   CE         [Obsolete] RFC 2481 names for the ECN bits.
-//    0     0         Not-ECT
-//    0     1         ECT(1)
-//    1     0         ECT(0)
-//    1     1         CE
-
-enum class EcnMarking {
-  kNotEct = 0,  // Not ECN-Capable Transport
-  kEct1 = 1,    // ECN-Capable Transport
-  kEct0 = 2,    // Not used by L4s (or webrtc.)
-  kCe = 3,      // Congestion experienced
-};
-
+using ::webrtc::EcnMarking;
 }  // namespace rtc
 
 #endif  // RTC_BASE_NETWORK_ECN_MARKING_H_
