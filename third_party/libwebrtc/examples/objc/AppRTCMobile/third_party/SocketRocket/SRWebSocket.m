b8868f1c13542bf1610b971f1145c3267c60b2eb
Michael Froman <mfroman@mozilla.com>
2024-12-05 15:50:08 -0600

    Bug 1934695 - Vendor libwebrtc from 04d97b6c52
    Upstream commit: https://webrtc.googlesource.com/src/+/04d97b6c52313376182232c238ad773dc93605ae
        Fix "arithmetic on a pointer to void" warning.
    
        Error appears when rolling Chromium in WebRTC:
        https://ci.chromium.org/ui/p/webrtc/builders/try/mac_rel/76983/overview
    
        Change-Id: Ibe9b15680efb00cac2333639a12c71cb76f6e71b
        Bug: None
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/367781
        Commit-Queue: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Auto-Submit: Jeremy Leconte <jleconte@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#43370}

--- third_party/libwebrtc/examples/objc/AppRTCMobile/third_party/SocketRocket/SRWebSocket.m
+++ third_party/libwebrtc/examples/objc/AppRTCMobile/third_party/SocketRocket/SRWebSocket.m
@@ -1080,12 +1080,19 @@ static const uint8_t SRPayloadLenMask   = 0x7F;
     
     NSUInteger dataLength = _outputBuffer.length;
     if (dataLength - _outputBufferOffset > 0 && _outputStream.hasSpaceAvailable) {
-        NSInteger bytesWritten = [_outputStream write:_outputBuffer.bytes + _outputBufferOffset maxLength:dataLength - _outputBufferOffset];
-        if (bytesWritten == -1) {
-            [self _failWithError:[NSError errorWithDomain:SRWebSocketErrorDomain code:2145 userInfo:[NSDictionary dictionaryWithObject:@"Error writing to stream" forKey:NSLocalizedDescriptionKey]]];
-             return;
-        }
-        
+      NSInteger bytesWritten =
+          [_outputStream write:(const unsigned char *)_outputBuffer.bytes + _outputBufferOffset
+                     maxLength:dataLength - _outputBufferOffset];
+      if (bytesWritten == -1) {
+        [self _failWithError:
+                  [NSError errorWithDomain:SRWebSocketErrorDomain
+                                      code:2145
+                                  userInfo:[NSDictionary
+                                               dictionaryWithObject:@"Error writing to stream"
+                                                             forKey:NSLocalizedDescriptionKey]]];
+        return;
+      }
+
         _outputBufferOffset += bytesWritten;
         
         if (_outputBufferOffset > 4096 && _outputBufferOffset > (_outputBuffer.length >> 1)) {
