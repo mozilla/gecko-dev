2a0764e6609d763e9899c2725d52b310cecc11fc
Nicolas Chevobbe <nchevobbe@mozilla.com>
2024-12-05 11:04:28 +0000

    Bug 1934382 - [devtools] Only assert SuppressedError in browser_webconsole_errors.js  when they're supported. r=devtools-reviewers,jdescottes.
    The pref that we use to enable explicit resource management only works
    on Nightly, which is why we're getting test failures on beta simulation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D230931

--- devtools/client/webconsole/test/browser/browser_webconsole_errors.js
+++ devtools/client/webconsole/test/browser/browser_webconsole_errors.js
@@ -320,7 +320,12 @@ add_task(async function () {
         );
       },
     },
-    {
+  ];
+
+  // javascript.options.experimental.explicit_resource_management is set to true, but it's
+  // only supported on Nightly at the moment
+  if (AppConstants.ENABLE_EXPLICIT_RESOURCE_MANAGEMENT) {
+    TEST_DATA.push({
       desc: `SuppressedError`,
       expression: `throw new SuppressedError(
           new Error("foo"),
@@ -328,8 +333,8 @@ add_task(async function () {
           "the suppressed error message"
         )`,
       expected: `Uncaught SuppressedError: the suppressed error message`,
-    },
-  ];
+    });
+  }
 
   for (const { desc, expression, expected, assert } of TEST_DATA) {
     info(`Check error: ${desc}`);
