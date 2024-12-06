e0c53dc74a6cee1cee09a93c7d9c4e5404f279f1
Chris H-C <chutten@mozilla.com>
2024-12-06 16:42:26 +0000

    Bug 1935419 - Instrument histograms that use AccumulateTimeDelta backwards r=TravisLong
    Differential Revision: https://phabricator.services.mozilla.com/D231275

--- toolkit/components/telemetry/tests/gtest/TestHistograms.cpp
+++ toolkit/components/telemetry/tests/gtest/TestHistograms.cpp
@@ -821,6 +821,12 @@ TEST_F(TelemetryTestFixture, AccumulateTimeDelta) {
   Telemetry::AccumulateTimeDelta(Telemetry::TELEMETRY_TEST_COUNT, start + delta,
                                  start);
 
+  // The zero contribution is instrumented
+  auto result = mozilla::glean::telemetry::clamping_time_hgrams
+                    .Get("TELEMETRY_TEST_COUNT"_ns)
+                    .TestGetValue();
+  ASSERT_EQ(1, result.unwrap().ref());
+
   // Get a snapshot for all the histograms
   JS::Rooted<JS::Value> snapshot(cx.GetJSContext());
   GetSnapshots(cx.GetJSContext(), mTelemetry, "TELEMETRY_TEST_COUNT", &snapshot,
