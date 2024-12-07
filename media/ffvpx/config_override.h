2f7de869c075e2c3502f6253acbd76009d5af8e6
alwu <alwu@mozilla.com>
2024-12-07 03:10:26 +0000

    Bug 1932772 - part1 : add d3d11va2 vp9 decode path from ffmpeg 7.0 to bundled ffvpx on Windows. r=media-playback-reviewers,chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D226067

--- media/ffvpx/config_override.h
+++ media/ffvpx/config_override.h
@@ -12,6 +12,9 @@
 #define EXTERN_ASM
 #endif
 
+/**
+ * Linux Hardware Video Decoding
+ **/
 #undef CONFIG_VAAPI
 #undef CONFIG_VAAPI_1
 #undef CONFIG_VP8_VAAPI_HWACCEL
@@ -30,4 +33,21 @@
 #define CONFIG_VP8_VAAPI_HWACCEL 0
 #define CONFIG_VP9_VAAPI_HWACCEL 0
 #define CONFIG_AV1_VAAPI_HWACCEL 0
+#endif
+
+/**
+ * Windows Hardware Video Decoding
+ **/
+#undef CONFIG_D3D11VA
+#undef CONFIG_VP9_D3D11VA_HWACCEL
+#undef CONFIG_VP9_D3D11VA2_HWACCEL
+
+#if defined (XP_WIN) && !defined(MOZ_FFVPX_AUDIOONLY)
+  #define CONFIG_D3D11VA 1
+  #define CONFIG_VP9_D3D11VA_HWACCEL 1
+  #define CONFIG_VP9_D3D11VA2_HWACCEL 1
+#else
+  #define CONFIG_D3D11VA 0
+  #define CONFIG_VP9_D3D11VA_HWACCEL 0
+  #define CONFIG_VP9_D3D11VA2_HWACCEL 0
 #endif
