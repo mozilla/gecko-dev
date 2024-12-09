9f1aa04d71c17a6fbe300e2881c39f70638217e7
ffxbld <ffxbld@mozilla.com>
2024-12-09 15:04:39 +0000

    No Bug, mozilla-central repo-update HSTS HPKP tld-suffixes mobile-experiments ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D231520

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1739445523000000);
+static const PRTime kCTExpirationTime = INT64_C(1739791056000000);
 
 namespace mozilla::ct {
 
