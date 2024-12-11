9c2e9481bd04ff72f578418e48e8941423850c34
Tooru Fujisawa <arai_a@mac.com>
2024-12-11 15:09:33 +0000

    Bug 1936085 - Part 3: Use ESM in the worker test. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D231596

--- dom/workers/test/WorkerTest.sys.mjs
+++ dom/workers/test/WorkerTest.sys.mjs
@@ -2,9 +2,7 @@
  * Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
-var EXPORTED_SYMBOLS = ["WorkerTest"];
-
-var WorkerTest = {
+export var WorkerTest = {
   go(message, messageCallback, errorCallback) {
     let worker = new ChromeWorker("WorkerTest_worker.js");
     worker.onmessage = messageCallback;
