52a48cc200f3b23ac6f10ba7aef525faba775861
Daniel Holbert <dholbert@cs.stanford.edu>
2024-12-12 08:38:32 +0000

    Bug 1929147: Make VectorImage::GetWidth/GetHeight gracefully fail (rather than crashing) if internal doc's root element is nullptr. r=tnikkel
    Based on intermittent crashes in automation, it seems like during shutdown, two
    things may happen independently:
    (A) we gather a memory report that asks our SVG images for their width/height.
    (B) we tear down the internal SVG document for our SVG images.
    
    If they happen in that order (A before B), then everything's fine; but if B
    happens *before* A, then the width/height queries in A will find an SVG
    document with a null pointer for the root element. So we need to handle that
    case gracefully.
    
    Differential Revision: https://phabricator.services.mozilla.com/D231825

--- image/VectorImage.cpp
+++ image/VectorImage.cpp
@@ -450,9 +450,14 @@ VectorImage::GetWidth(int32_t* aWidth) {
   }
 
   SVGSVGElement* rootElem = mSVGDocumentWrapper->GetRootSVGElem();
-  MOZ_ASSERT(rootElem,
-             "Should have a root SVG elem, since we finished "
-             "loading without errors");
+  if (MOZ_UNLIKELY(!rootElem)) {
+    // Unlikely to reach this code; we should have a root SVG elem, since we
+    // finished loading without errors. But we can sometimes get here during
+    // shutdown (as part of gathering a memory report) if the internal SVG
+    // document has already been torn down by a shutdown listener.
+    *aWidth = 0;
+    return NS_ERROR_FAILURE;
+  }
   LengthPercentage rootElemWidth = rootElem->GetIntrinsicWidth();
 
   if (!rootElemWidth.IsLength()) {
@@ -542,9 +547,14 @@ VectorImage::GetHeight(int32_t* aHeight) {
   }
 
   SVGSVGElement* rootElem = mSVGDocumentWrapper->GetRootSVGElem();
-  MOZ_ASSERT(rootElem,
-             "Should have a root SVG elem, since we finished "
-             "loading without errors");
+  if (MOZ_UNLIKELY(!rootElem)) {
+    // Unlikely to reach this code; we should have a root SVG elem, since we
+    // finished loading without errors. But we can sometimes get here during
+    // shutdown (as part of gathering a memory report) if the internal SVG
+    // document has already been torn down by a shutdown listener.
+    *aHeight = 0;
+    return NS_ERROR_FAILURE;
+  }
   LengthPercentage rootElemHeight = rootElem->GetIntrinsicHeight();
 
   if (!rootElemHeight.IsLength()) {
