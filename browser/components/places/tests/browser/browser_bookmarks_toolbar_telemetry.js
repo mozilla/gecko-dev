f44a5e3f1df67ee412084b264c77ba8ba50e124e
Mark Banner <standard8@mozilla.com>
2024-12-13 18:59:17 +0000

    Bug 1935530 - Fix TV failure in browser_bookmarks_toolbar_telemetry.js by ensuring the test is set up correctly. r=places-reviewers,mak
    Differential Revision: https://phabricator.services.mozilla.com/D231781

--- browser/components/places/tests/browser/browser_bookmarks_toolbar_telemetry.js
+++ browser/components/places/tests/browser/browser_bookmarks_toolbar_telemetry.js
@@ -24,24 +24,25 @@ const bookmarksInfo = [
   },
 ];
 
-// Setup.
-add_task(async function test_bookmarks_toolbar_telemetry() {
-  await SpecialPowers.pushPrefEnv({
-    set: [["browser.toolbars.bookmarks.visibility", "newtab"]],
+add_setup(async function () {
+  let toolbarVisibility = Services.prefs.getCharPref(
+    "browser.toolbars.bookmarks.visibility",
+    "newtab"
+  );
+  if (toolbarVisibility != "newtab") {
+    // The first test assumes that we will be changing from the `newtab` state
+    // to `never`. Hence ensure we are on the expected state.
+    await changeToolbarVisibilityViaContextMenu("newtab");
+  }
+
+  registerCleanupFunction(async () => {
+    await changeToolbarVisibilityViaContextMenu(toolbarVisibility);
   });
 
-  // This is added during startup
-  await TestUtils.waitForCondition(
-    () =>
-      keyedScalarExists(
-        "browser.ui.toolbar_widgets",
-        "bookmarks-bar_pinned_newtab",
-        true
-      ),
-    `Waiting for "bookmarks-bar_pinned_newtab" to appear in Telemetry snapshot`
-  );
-  ok(true, `"bookmarks-bar_pinned_newtab"=true found in Telemetry`);
+  Services.telemetry.clearScalars();
+});
 
+add_task(async function test_bookmarks_toolbar_visibility() {
   await changeToolbarVisibilityViaContextMenu("never");
   await assertUIChange(
     "bookmarks-bar_move_newtab_never_toolbar-context-menu",
@@ -59,7 +60,9 @@ add_task(async function test_bookmarks_toolbar_telemetry() {
     "bookmarks-bar_move_newtab_always_toolbar-context-menu",
     1
   );
+});
 
+add_task(async function test_bookmarks_counts_across_multiple_windows() {
   // Extra windows are opened to make sure telemetry numbers aren't
   // double counted since there will be multiple instances of the
   // bookmarks toolbar.
