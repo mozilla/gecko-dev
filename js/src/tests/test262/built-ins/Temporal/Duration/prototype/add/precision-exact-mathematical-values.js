c5eda65e559a61ad6b256889743e1f3a986cd0e2
Daniel Minor <dminor@mozilla.com>
2024-12-13 16:09:05 +0000

    Bug 1912757 - Use --enable-temporal flag for test262 tests; r=anba
    Differential Revision: https://phabricator.services.mozilla.com/D231839

--- js/src/tests/test262/built-ins/Temporal/Duration/prototype/add/precision-exact-mathematical-values.js
+++ js/src/tests/test262/built-ins/Temporal/Duration/prototype/add/precision-exact-mathematical-values.js
@@ -1,4 +1,4 @@
-// |reftest| skip-if(!this.hasOwnProperty('Temporal')) -- Temporal is not enabled unconditionally
+// |reftest| shell-option(--enable-temporal) skip-if(!this.hasOwnProperty('Temporal')||!xulRuntime.shell) -- Temporal is not enabled unconditionally, requires shell-options
 // Copyright (C) 2022 André Bargull. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
