5b53850fb96c75618f970bdf3b8b4dd566f1313d
Robin Steuber <bytesized@mozilla.com>
2024-12-13 00:18:33 +0000

    Bug 1907127 - Add support for writing update.timestamp r=nrishel,nalexander,application-update-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D224890

--- browser/branding/nightly/pref/firefox-branding.js
+++ browser/branding/nightly/pref/firefox-branding.js
@@ -30,6 +30,14 @@ pref("app.update.checkInstallTime.days", 2);
 // Give the user x seconds to reboot before showing a badge on the hamburger
 // button. default=immediately
 pref("app.update.badgeWaitTime", 0);
+// This represents the duration between an update being ready and it being
+// possible to install it while other sessions are running. Note that
+// having this pref's duration differ from `app.update.badgeWaitTime` may result
+// in undefined behavior such as showing an update prompt that does not result
+// in an update when the "Restart to Update" button is clicked. Keep in mind
+// that this is in milliseconds and `app.update.badgeWaitTime` is in seconds.
+// Note that the effective value of this pref is limited to 1 week, maximum.
+pref("app.update.multiSessionInstallLockout.timeoutMs", 0);
 
 // Number of usages of the web console.
 // If this is less than 5, then pasting code into the web console is disabled
