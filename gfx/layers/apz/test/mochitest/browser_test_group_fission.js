01962e4f93a13c82a065c1f7847f6729f1d92625
Alex Jakobi <ajakobi@mozilla.com>
2024-12-14 09:11:16 +0000

    Bug 1841896 - Port helper_fission_large_subframe to regular mochitests r=hiro
    Differential Revision: https://phabricator.services.mozilla.com/D231822

--- gfx/layers/apz/test/mochitest/browser_test_group_fission.js
+++ gfx/layers/apz/test/mochitest/browser_test_group_fission.js
@@ -36,7 +36,6 @@ add_task(async function test_main() {
       },
     },
     { file: "helper_fission_force_empty_hit_region.html" },
-    { file: "helper_fission_large_subframe.html" },
     { file: "helper_fission_initial_displayport.html" },
     { file: "helper_fission_checkerboard_severity.html" },
     // add additional tests here
