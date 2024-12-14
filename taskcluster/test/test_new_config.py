32539b5fff46eaa7b061f9a6d72dc1742538516a
Goloman Adrian <agoloman@mozilla.com>
2024-12-14 10:03:37 +0200

    Backed out changeset bca9e1e0626a (bug 1902032) for causing bc jobs to not run any tests. CLOSED TREE

--- taskcluster/test/test_new_config.py
+++ taskcluster/test/test_new_config.py
@@ -48,11 +48,7 @@ PARAMS_NEW_CONFIG.update(
                 t.kind == "test"
                 and t.attributes["unittest_suite"] == "mochitest-browser-chrome"
                 and t.attributes["test_platform"] == "linux1804-64-qr/opt"
-                and (
-                    "spi-nw" not in t.label
-                    and "a11y-checks" not in t.label
-                    and "vt" not in t.label
-                )
+                and ("spi-nw" not in t.label and "a11y-checks" not in t.label)
             ),
             32,
             id="mochitest-browser-chrome",
@@ -78,11 +74,7 @@ def test_tasks_new_config_false(full_task_graph, filter_tasks, func, min_expecte
                 t.kind == "test"
                 and t.attributes["unittest_suite"] == "mochitest-browser-chrome"
                 and t.attributes["test_platform"] == "linux1804-64-qr/opt"
-                and (
-                    "spi-nw" not in t.label
-                    and "a11y-checks" not in t.label
-                    and "vt" not in t.label
-                )
+                and ("spi-nw" not in t.label and "a11y-checks" not in t.label)
             ),
             32,
             id="mochitest-browser-chrome",
