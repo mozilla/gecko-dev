15e89023ae5b14dfadd1feec9da16294c9f9e7c6
Beth Rennie <beth@brennie.ca>
2024-12-17 20:20:26 +0000

    Bug 1937626 - Add a schema for the nimbusTelemetry feature r=chumphreys
    Differential Revision: https://phabricator.services.mozilla.com/D232271

--- toolkit/components/nimbus/test/unit/test_nimbusTelemetry.js
+++ toolkit/components/nimbus/test/unit/test_nimbusTelemetry.js
@@ -6,10 +6,23 @@ const { ExperimentAPI } = ChromeUtils.importESModule(
   "resource://nimbus/ExperimentAPI.sys.mjs"
 );
 
+const { JsonSchema } = ChromeUtils.importESModule(
+  "resource://gre/modules/JsonSchema.sys.mjs"
+);
+
 add_setup(function setup() {
   do_get_profile();
   Services.fog.initializeFOG();
 });
+
+const SCHEMAS = {
+  get nimbusTelemetry() {
+    return fetch(
+      "resource://nimbus/schemas/NimbusTelemetryFeature.schema.json",
+      { credentials: "omit" }
+    ).then(rsp => rsp.json());
+  },
+};
 
 add_task(async function test_enrollAndUnenroll() {
   const experiment = ExperimentFakes.recipe("experiment", {
@@ -171,4 +184,51 @@ add_task(async function test_enrollAndUnenroll() {
   manager.store._deleteForTests("rollout");
 
   await assertEmptyStore(manager.store);
+});
+
+add_task(async function test_featureConfigSchema() {
+  function metricConfig(metric, value) {
+    return {
+      gleanMetricConfiguration: {
+        metrics_enabled: {
+          [metric]: value,
+        },
+      },
+    };
+  }
+
+  const validator = new JsonSchema.Validator(await SCHEMAS.nimbusTelemetry);
+
+  const ALLOWED = [
+    "nimbus_targeting_environment.targeting_context_value",
+    "nimbus_targeting_environment.pref_type_errors",
+    "nimbus_targeting_environment.attr_eval_errors",
+    "nimbus_targeting_environment.user_set_prefs",
+    "nimbus_targeting_environment.pref_values",
+    "nimbus_targeting_context.active_experiments",
+    "nimbus_targeting_context.active_rollouts",
+    "nimbus_targeting_context.addresses_saved",
+    "nimbus_events.enrollment",
+    "nimbus_events.is_ready",
+    "nimbus_events.enrollment_status",
+  ];
+
+  for (const metric of ALLOWED) {
+    for (const value of [true, false]) {
+      Assert.ok(
+        validator.validate(metricConfig(metric, value)).valid,
+        `Can control ${metric} with nimbusTelemetry`
+      );
+    }
+  }
+
+  Assert.ok(
+    !validator.validate(metricConfig("nimbus_events.enrollment", "true")).valid,
+    "Schema enforces boolean values"
+  );
+
+  Assert.ok(
+    !validator.validate(metricConfig("bogus", true)).valid,
+    "Schema restricts metrics to nimbus metrics"
+  );
 });
