7e4d985bcb47b87e2a4017808e1ae14f3bf42d21
Jan de Mooij <jdemooij@mozilla.com>
2024-12-19 10:35:19 +0000

    Bug 1937570 part 1 - Assert canonical function has not resolved length/name. r=iain
    The code for `LLambda` already relied on this because it copies the flags from
    the canonical function to the new function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D232346

--- js/src/vm/JSFunction.cpp
+++ js/src/vm/JSFunction.cpp
@@ -1845,14 +1845,12 @@ static inline JSFunction* NewFunctionClone(JSContext* cx, HandleFunction fun,
     return nullptr;
   }
 
-  constexpr uint16_t NonCloneableFlags =
-      FunctionFlags::RESOLVED_LENGTH | FunctionFlags::RESOLVED_NAME;
-
-  FunctionFlags flags = fun->flags();
-  flags.clearFlags(NonCloneableFlags);
+  // The following flags shouldn't be set or cloned.
+  MOZ_ASSERT(!fun->flags().hasResolvedLength());
+  MOZ_ASSERT(!fun->flags().hasResolvedName());
 
   clone->setArgCount(fun->nargs());
-  clone->setFlags(flags);
+  clone->setFlags(fun->flags());
 
   // Note: |clone| and |fun| are same-zone so we don't need to call markAtom.
   clone->initAtom(fun->maybePartialDisplayAtom());
