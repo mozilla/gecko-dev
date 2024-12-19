46d2e5dbd0112ba2ce900ca3bcf6d491f84f2680
Stanca Serban <sstanca@mozilla.com>
2024-12-19 02:07:56 +0200

    Backed out changeset ca6305692cfb (bug 1936275) for causing bp-nu build bustages in GMPVideoDecoderChild.cpp. CLOSED TREE

--- dom/media/gmp/GMPPlatform.h
+++ dom/media/gmp/GMPPlatform.h
@@ -25,31 +25,12 @@ namespace gmp {
 class GMPChild;
 
 void InitPlatformAPI(GMPPlatformAPI& aPlatformAPI, GMPChild* aChild);
-void ShutdownPlatformAPI();
 
 GMPErr RunOnMainThread(GMPTask* aTask);
 
 GMPTask* NewGMPTask(std::function<void()>&& aFunction);
 
 GMPErr SetTimerOnMainThread(GMPTask* aTask, int64_t aTimeoutMS);
-
-/**
- * This is intended to be used by encoders/decoders that will make a GMP call
- * that is a synchronous post to the GMP worker thread. Because the GMP worker
- * threads can synchronously callback to the main thread, this has the potential
- * for a deadlock. If the encoder/decoder tracks any outstanding requests that
- * will result in a synchronous callback to the main thread, we can simply spin
- * the event loop on those callbacks until they are completed. Then we can
- * safefully make our own synchronous call to the GMP worker thread without fear
- * of a deadlock.
- *
- * Note that each encoder/decoder has its own worker thread, so assuming we
- * drain the synchronous events for that specific encoder/decoder, we know there
- * are no more forthcoming to cause us to deadlock.
- */
-using SpinPendingPredicate = std::function<bool()>;
-bool SpinPendingGmpEventsUntil(const SpinPendingPredicate& aPred,
-                               uint32_t aTimeoutMs);
 
 void SendFOGData(ipc::ByteBuf&& buf);
 
