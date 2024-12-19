c7ad8a30c24cd539400b8c6a7f45bcdf42a81225
Chris H-C <chutten@mozilla.com>
2024-12-19 13:46:27 +0000

    Bug 1937587 - Exclude instrumentation from Android for clamping time histograms  a=RyanVM
    Original Revision: https://phabricator.services.mozilla.com/D232290
    
    Differential Revision: https://phabricator.services.mozilla.com/D232527

--- toolkit/components/telemetry/core/Telemetry.cpp
+++ toolkit/components/telemetry/core/Telemetry.cpp
@@ -1825,10 +1825,12 @@ void AccumulateCategorical(HistogramID id, const nsTArray<nsCString>& labels) {
 void AccumulateTimeDelta(HistogramID aHistogram, TimeStamp start,
                          TimeStamp end) {
   if (start > end) {
+#if !defined(MOZ_WIDGET_ANDROID)
     mozilla::glean::telemetry::clamping_time_hgrams
         .Get(nsDependentCString(GetHistogramName(aHistogram)))
         .Add(1);
     Accumulate(aHistogram, 0);
+#endif  // !defined(MOZ_WIDGET_ANDROID)
     return;
   }
   Accumulate(aHistogram, static_cast<uint32_t>((end - start).ToMilliseconds()));
@@ -1837,10 +1839,12 @@ void AccumulateTimeDelta(HistogramID aHistogram, TimeStamp start,
 void AccumulateTimeDelta(HistogramID aHistogram, const nsCString& key,
                          TimeStamp start, TimeStamp end) {
   if (start > end) {
+#if !defined(MOZ_WIDGET_ANDROID)
     mozilla::glean::telemetry::clamping_time_hgrams
         .Get(nsDependentCString(GetHistogramName(aHistogram)))
         .Add(1);
     Accumulate(aHistogram, key, 0);
+#endif  // !defined(MOZ_WIDGET_ANDROID)
     return;
   }
   Accumulate(aHistogram, key,
