7ea095d481be9dbf834742f7fa1d4762a76c59a9
kpatenio <kpatenio@mozilla.com>
2024-12-20 01:55:56 +0000

    Bug 1916012 - Add empty state UI for sidebar migration. r=shopping-reviewers,fluent-reviewers,fchasen
    - Adds new illustrations and copy to RC sidebar panel for non PDPs, which will act as empty states
    - To test the feature, set browser.shopping.experience2023.integratedSidebar=true, browser.shopping.experience2023.shoppingSidebar=false
    - This is for users already opted-in to RC, so browser.shopping.experience2023.optedIn=true. See D226075 for the non-opted in versions of the empty states.
    - Adds a new ShoppingProduct static method called getSupportedDomains, which allows us to read supported domains from our ProductConfig and display a list of domains in the empty state
    - Adds browser tests for empty states and ShoppingProduct.getSupportedDomains
    - Figma: https://www.figma.com/design/GnCtw7DmPtlSEIfjatHtSI/Review-Checker-in-sidebar-migration?node-id=9832-47109&node-type=section&m=dev
    
    Differential Revision: https://phabricator.services.mozilla.com/D222891

--- browser/components/shopping/content/shopping-container.mjs
+++ browser/components/shopping/content/shopping-container.mjs
@@ -53,6 +53,8 @@ export class ShoppingContainer extends MozLitElement {
     showingKeepClosedMessage: { type: Boolean },
     isProductPage: { type: Boolean },
     isSupportedSite: { type: Boolean },
+    supportedDomains: { type: Object },
+    formattedDomainList: { type: Object, state: true },
   };
 
   static get queries() {
@@ -68,6 +70,11 @@ export class ShoppingContainer extends MozLitElement {
       loadingEl: "#loading-wrapper",
       closeButtonEl: "#close-button",
       keepClosedMessageBarEl: "#keep-closed-message-bar",
+      emptyStateImgEl: "#shopping-empty-state-img",
+      emptyStateHeaderEl: "#shopping-empty-state-header",
+      emptyStateTextEl: "#shopping-empty-state-text",
+      emptyStateSupportedListEl: "#shopping-empty-list-of-supported-domains",
+      containerContentEl: "#content",
     };
   }
 
@@ -98,7 +105,23 @@ export class ShoppingContainer extends MozLitElement {
     );
   }
 
-  updated() {
+  updated(changedProperties) {
+    if (changedProperties.has("supportedDomains")) {
+      let oldVal = changedProperties.get("supportedDomains");
+      /**
+       * We expect the domains object to be passed in consistently and not change often.
+       * A shallow comparison seems to be enough.
+       */
+      try {
+        if (JSON.stringify(oldVal) !== JSON.stringify(this.supportedDomains)) {
+          // Let the render function deal with recreating the formatted list.
+          this.formattedDomainList = null;
+        }
+      } catch (e) {
+        console.error(e);
+        this.formattedDomainList = null;
+      }
+    }
     if (this.focusCloseButton) {
       this.closeButtonEl.focus();
     }
@@ -118,6 +141,7 @@ export class ShoppingContainer extends MozLitElement {
     autoOpenEnabledByUser,
     isProductPage,
     isSupportedSite,
+    supportedDomains,
   }) {
     // If we're not opted in or there's no shopping URL in the main browser,
     // the actor will pass `null`, which means this will clear out any existing
@@ -137,6 +161,7 @@ export class ShoppingContainer extends MozLitElement {
       autoOpenEnabledByUser ?? this.autoOpenEnabledByUser;
     this.isProductPage = isProductPage ?? true;
     this.isSupportedSite = isSupportedSite;
+    this.supportedDomains = supportedDomains ?? this.supportedDomains;
   }
 
   _updateRecommendations({ recommendationData }) {
@@ -240,78 +265,72 @@ export class ShoppingContainer extends MozLitElement {
     `;
   }
 
-  contentTemplate() {
+  hasDataTemplate() {
+    let dataBodyTemplate = null;
+
     // The user requested an analysis which is not done yet.
     if (
       this.analysisEvent?.productUrl == this.productUrl &&
       this.isAnalysisInProgress
     ) {
       const isReanalysis = this.analysisEvent.type === "ReanalysisRequested";
-      return html`<shopping-message-bar
+      dataBodyTemplate = html`<shopping-message-bar
           type=${isReanalysis
             ? "reanalysis-in-progress"
             : "analysis-in-progress"}
           progress=${this.analysisProgress}
         ></shopping-message-bar>
         ${isReanalysis ? this.analysisDetailsTemplate() : null}`;
-    }
-
-    if (this.data?.error) {
-      return html`<shopping-message-bar
+    } else if (this.data?.error) {
+      dataBodyTemplate = html`<shopping-message-bar
         type="generic-error"
       ></shopping-message-bar>`;
-    }
-
-    if (this.data.page_not_supported) {
-      return html`<shopping-message-bar
+    } else if (this.data.page_not_supported) {
+      dataBodyTemplate = html`<shopping-message-bar
         type="page-not-supported"
       ></shopping-message-bar>`;
-    }
-
-    if (this.data.deleted_product_reported) {
-      return html`<shopping-message-bar
+    } else if (this.data.deleted_product_reported) {
+      dataBodyTemplate = html`<shopping-message-bar
         type="product-not-available-reported"
       ></shopping-message-bar>`;
-    }
-
-    if (this.data.deleted_product) {
-      return this.userReportedAvailable
+    } else if (this.data.deleted_product) {
+      dataBodyTemplate = this.userReportedAvailable
         ? html`<shopping-message-bar
             type="thanks-for-reporting"
           ></shopping-message-bar>`
         : html`<shopping-message-bar
             type="product-not-available"
           ></shopping-message-bar>`;
-    }
-
-    if (this.data.needs_analysis) {
+    } else if (this.data.needs_analysis) {
       if (!this.data.product_id || typeof this.data.grade != "string") {
         // Product is new to us.
-        return html`<unanalyzed-product-card
+        dataBodyTemplate = html`<unanalyzed-product-card
           productUrl=${ifDefined(this.productUrl)}
         ></unanalyzed-product-card>`;
+      } else {
+        // We successfully analyzed the product before, but the current analysis is outdated and can be updated
+        // via a re-analysis.
+        dataBodyTemplate = html`
+          <shopping-message-bar
+            type="stale"
+            .productUrl=${this.productUrl}
+          ></shopping-message-bar>
+          ${this.analysisDetailsTemplate()}
+        `;
       }
-
-      // We successfully analyzed the product before, but the current analysis is outdated and can be updated
-      // via a re-analysis.
-      return html`
-        <shopping-message-bar
-          type="stale"
-          .productUrl=${this.productUrl}
-        ></shopping-message-bar>
-        ${this.analysisDetailsTemplate()}
-      `;
-    }
-
-    if (this.data.not_enough_reviews) {
+    } else if (this.data.not_enough_reviews) {
       // We already saw and tried to analyze this product before, but there are not enough reviews
       // to make a detailed analysis.
-      return html`<shopping-message-bar
+      dataBodyTemplate = html`<shopping-message-bar
         type="not-enough-reviews"
       ></shopping-message-bar>`;
+    } else {
+      dataBodyTemplate = this.analysisDetailsTemplate();
     }
 
-    return this.analysisDetailsTemplate();
+    return html`
+      ${dataBodyTemplate}${this.explainerTemplate()}${this.recommendationTemplate()}
+    `;
   }
 
   recommendationTemplate() {
@@ -352,7 +371,127 @@ export class ShoppingContainer extends MozLitElement {
     `;
   }
 
-  renderContainer(sidebarContent, hideFooter = false) {
+  noDataTemplate({ animate = true } = {}) {
+    if (this.isAnalysisInProgress) {
+      return html`<shopping-message-bar
+          type="analysis-in-progress"
+          progress=${this.analysisProgress}
+        ></shopping-message-bar
+        >${this.explainerTemplate()}${this.recommendationTemplate()}`;
+    }
+    return this.loadingTemplate({ animate });
+  }
+
+  nonPDPTemplate() {
+    // TODO: (Bug 1937924) settings template will throw a warning since this.productUrl is null when viewing a non-PDP
+    const bodyTextTemplate = this.isSupportedSite
+      ? html` <p
+          id="shopping-empty-state-text"
+          data-l10n-id="shopping-empty-state-supported-site"
+        ></p>`
+      : html`
+          <p
+            id="shopping-empty-state-text"
+            data-l10n-id="shopping-empty-state-non-supported-site"
+          ></p>
+        `;
+
+    if (!this.formattedDomainList) {
+      this.formattedDomainList = this._formattedDomainListTemplate();
+    }
+
+    const listTemplate = !this.isSupportedSite
+      ? html`<ul id="shopping-empty-list-of-supported-domains">
+          ${this.formattedDomainList}
+        </ul>`
+      : null;
+
+    // Anything wrapped by #shopping-empty-wrapper will be centered on sidebar width changes.
+    return html`<div id="shopping-empty-wrapper">
+        <img
+          id="shopping-empty-state-img"
+          src="chrome://browser/content/shopping/assets/emptyStateA.svg"
+          alt=""
+          role="presentation"
+        />
+        <h2
+          id="shopping-empty-state-header"
+          data-l10n-id="shopping-empty-state-header"
+        ></h2>
+        ${bodyTextTemplate} ${listTemplate}
+      </div>
+      ${this.isSupportedSite
+        ? this.explainerTemplate({ className: "first-footer-card" })
+        : null}`;
+  }
+
+  _formattedDomainListTemplate() {
+    if (!this.supportedDomains) {
+      return null;
+    }
+
+    let template = [];
+    let formatter = new Intl.ListFormat(undefined, {
+      style: "narrow",
+      type: "conjunction",
+    });
+
+    Object.keys(this.supportedDomains)
+      .sort()
+      .forEach(sitename => {
+        let domainsFromSite = this.supportedDomains[sitename];
+
+        // List of supported domains per sitename, per row, as a string.
+        let anchorsString = domainsFromSite.map(domain => {
+          try {
+            let url = new URL(domain);
+            let hostname = url.hostname;
+            /** ShoppingProduct should already validate the URLs in the ProductConfig for us.
+             * As an extra precaution though, let's verify that we've been passed a valid URL, in case
+             * something goes awry in messages between actors and the shopping-container.
+             *
+             * @see ShoppingProduct
+             */
+            let validProtocolRegex = /^(https:\/\/\w+.*)/;
+            return validProtocolRegex.test(url)
+              ? `<a class="shopping-supported-domain-link" href=${url.href} target="_blank">${hostname}</a>`
+              : null;
+          } catch (e) {
+            // Somehow, we got an invalid URL.
+            console.error(e);
+            return null;
+          }
+        });
+
+        // Now format the string as a list suitable for the current locale.
+        anchorsString = formatter.format(anchorsString);
+
+        // Convert the formatted string into an element that can be inserted into our litElement template.
+        const parser = new DOMParser();
+        let anchorsDOMDoc = parser.parseFromString(anchorsString, "text/html");
+        /**
+         * litElement will lose a reference to the childNodes on re-render if we use a DocumentFragment.
+         * Instead, add the nodes as an array in our template so that we preserve them.
+         */
+        let anchorsTemplate = [];
+        Array.from(anchorsDOMDoc.body.childNodes).forEach(childNode => {
+          anchorsTemplate.push(childNode);
+        });
+
+        let listTemplate = html` <li
+          id="shopping-empty-state-domains-list-${sitename}"
+          class="shopping-supported-domain-list"
+        >
+          ${anchorsTemplate}
+        </li>`;
+
+        template.push(listTemplate);
+      });
+
+    return template;
+  }
+
+  renderContainer(sidebarContent, { showSettings = false } = {}) {
     return html`<link
         rel="stylesheet"
         href="chrome://browser/content/shopping/shopping-container.css"
@@ -384,29 +523,44 @@ export class ShoppingContainer extends MozLitElement {
             @click=${this.handleCloseButtonClick}
           ></button>
         </div>
-        <div id="content" aria-live="polite" aria-busy=${!this.data}>
+        <div
+          id="content"
+          class=${!this.isProductPage && !this.isOffline
+            ? "is-empty-state"
+            : ""}
+          aria-live="polite"
+          aria-busy=${!this.data}
+        >
           <slot name="multi-stage-message-slot"></slot>
           ${this.keepClosedMessageTemplate()}${sidebarContent}
-          ${!hideFooter ? this.footerTemplate() : null}
+          ${showSettings
+            ? this.settingsTemplate(
+                !this.isSupportedSite && !this.isProductPage
+                  ? { className: "first-footer-card" }
+                  : ""
+              )
+            : null}
         </div>
       </div>`;
   }
 
-  footerTemplate() {
+  explainerTemplate({ className = "" } = {}) {
+    return html`<analysis-explainer
+      productUrl=${ifDefined(this.productUrl)}
+      class=${className}
+    ></analysis-explainer>`;
+  }
+
+  settingsTemplate({ className = "" } = {}) {
     let hostname = this.getHostnameFromProductUrl();
-    return html`
-      <analysis-explainer
-        productUrl=${ifDefined(this.productUrl)}
-      ></analysis-explainer>
-      ${this.recommendationTemplate()}
-      <shopping-settings
-        ?adsEnabled=${this.adsEnabled}
-        ?adsEnabledByUser=${this.adsEnabledByUser}
-        ?autoOpenEnabled=${this.autoOpenEnabled}
-        ?autoOpenEnabledByUser=${this.autoOpenEnabledByUser}
-        .hostname=${hostname}
-      ></shopping-settings>
-    `;
+    return html` <shopping-settings
+      ?adsEnabled=${this.adsEnabled}
+      ?adsEnabledByUser=${this.adsEnabledByUser}
+      ?autoOpenEnabled=${this.autoOpenEnabled}
+      ?autoOpenEnabledByUser=${this.autoOpenEnabledByUser}
+      .hostname=${hostname}
+      class=${className}
+    ></shopping-settings>`;
   }
 
   keepClosedMessageTemplate() {
@@ -426,27 +580,26 @@ export class ShoppingContainer extends MozLitElement {
 
   render() {
     let content;
-    let hideFooter;
+    // this.data may be null because we're viewing a non PDP, or a PDP that does not have data yet.
+    // Use isProductPage and isSupported to distinguish between the two.
+    const isLoadingData =
+      !this.data && this.isProductPage && !this.isSupportedSite;
+
     if (this.showOnboarding) {
       content = html``;
-      hideFooter = true;
-    } else if (this.isOffline) {
-      content = this.loadingTemplate({ animate: false });
-      hideFooter = true;
-    } else if (!this.data) {
-      if (this.isAnalysisInProgress) {
-        content = html`<shopping-message-bar
-          type="analysis-in-progress"
-          progress=${this.analysisProgress}
-        ></shopping-message-bar>`;
-      } else {
-        content = this.loadingTemplate();
-        hideFooter = true;
-      }
+    } else if (isLoadingData || this.isOffline) {
+      content = this.noDataTemplate({ animate: !this.isOffline });
+    } else if (!this.isProductPage || this.isSupportedSite) {
+      content = this.nonPDPTemplate();
     } else {
-      content = this.contentTemplate();
+      content = this.hasDataTemplate();
     }
-    return this.renderContainer(content, hideFooter);
+
+    const showSettings =
+      !this.showOnboarding &&
+      !this.isOffline &&
+      (!isLoadingData || (isLoadingData && this.isAnalysisInProgress));
+    return this.renderContainer(content, { showSettings });
   }
 
   handleCloseButtonClick() {
---
+++ browser/components/shopping/tests/browser/browser_empty_states.js
@@ -0,0 +1,176 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* import-globals-from head.js */
+
+const CONTENT_PAGE = "https://example.com";
+
+add_setup(async function setup() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["sidebar.revamp", true],
+      ["browser.shopping.experience2023.integratedSidebar", true],
+      ["browser.shopping.experience2023.shoppingSidebar", false],
+      ["sidebar.main.tools", "aichat,reviewchecker,syncedtabs,history"],
+      ["toolkit.shopping.ohttpRelayURL", ""],
+      ["toolkit.shopping.ohttpConfigURL", ""],
+    ],
+  });
+  registerCleanupFunction(async () => {
+    SidebarController.hide();
+  });
+});
+
+add_task(
+  async function test_integrated_sidebar_empty_state_opted_in_supported_site() {
+    await BrowserTestUtils.withNewTab(CONTENT_PAGE, async function (_browser) {
+      await SidebarController.show("viewReviewCheckerSidebar");
+
+      await withReviewCheckerSidebar(async () => {
+        let shoppingContainer = await ContentTaskUtils.waitForCondition(
+          () =>
+            content.document.querySelector("shopping-container")
+              ?.wrappedJSObject,
+          "Review Checker is loaded."
+        );
+        await shoppingContainer.updateComplete;
+
+        Assert.ok(shoppingContainer, "Review Checker is loaded");
+        Assert.ok(
+          shoppingContainer.emptyStateImgEl,
+          "Empty state should have an image"
+        );
+        Assert.ok(
+          shoppingContainer.emptyStateHeaderEl,
+          "Empty state should have a header"
+        );
+        Assert.ok(
+          shoppingContainer.emptyStateTextEl,
+          "Empty state should have text"
+        );
+        Assert.ok(
+          !shoppingContainer.emptyStateSupportedListEl,
+          "Empty state should not have a list of sites for a supported site"
+        );
+        Assert.ok(
+          shoppingContainer.analysisExplainerEl,
+          "Empty state should show the analysis-explainer for a supported site"
+        );
+        Assert.ok(
+          shoppingContainer.settingsEl,
+          "Empty state should have the settings card"
+        );
+        Assert.equal(
+          shoppingContainer.containerContentEl?.className,
+          "is-empty-state",
+          "There should be a classname for empty state so that styles are applied"
+        );
+      });
+    });
+  }
+);
+
+add_task(
+  async function test_integrated_sidebar_empty_state_opted_in_not_supported_site() {
+    await BrowserTestUtils.withNewTab("about:about", async function (_browser) {
+      await SidebarController.show("viewReviewCheckerSidebar");
+
+      await withReviewCheckerSidebar(async () => {
+        let shoppingContainer = await ContentTaskUtils.waitForCondition(
+          () =>
+            content.document.querySelector("shopping-container")
+              ?.wrappedJSObject,
+          "Review Checker is loaded."
+        );
+        await shoppingContainer.updateComplete;
+
+        Assert.ok(shoppingContainer, "Review Checker is loaded");
+        Assert.ok(
+          shoppingContainer.emptyStateImgEl,
+          "Empty state should have an image"
+        );
+        Assert.ok(
+          shoppingContainer.emptyStateHeaderEl,
+          "Empty state should have a header"
+        );
+        Assert.ok(
+          shoppingContainer.emptyStateTextEl,
+          "Empty state should have text"
+        );
+        Assert.ok(
+          shoppingContainer.emptyStateSupportedListEl,
+          "Empty state should have a list of sites for an unsupported site"
+        );
+        Assert.ok(
+          !shoppingContainer.analysisExplainerEl,
+          "Empty state should not show the analysis-explainer for an unsupported site"
+        );
+        Assert.ok(
+          shoppingContainer.settingsEl,
+          "Empty state should have the settings card"
+        );
+        Assert.equal(
+          shoppingContainer.containerContentEl?.className,
+          "is-empty-state",
+          "There should be a classname for empty state so that styles are applied"
+        );
+      });
+    });
+  }
+);
+
+add_task(async function test_integrated_sidebar_no_empty_state_opted_in_pdp() {
+  await BrowserTestUtils.withNewTab(
+    PRODUCT_TEST_URL,
+    async function (_browser) {
+      await SidebarController.show("viewReviewCheckerSidebar");
+
+      await withReviewCheckerSidebar(async () => {
+        let shoppingContainer = await ContentTaskUtils.waitForCondition(
+          () =>
+            content.document.querySelector("shopping-container")
+              ?.wrappedJSObject,
+          "Review Checker is loaded."
+        );
+        await shoppingContainer.updateComplete;
+
+        Assert.ok(shoppingContainer, "Review Checker is loaded");
+        Assert.ok(
+          !shoppingContainer.emptyStateImgEl,
+          "There should be no empty state image"
+        );
+        Assert.ok(
+          !shoppingContainer.emptyStateHeaderEl,
+          "There should be no empty state header"
+        );
+        Assert.ok(
+          !shoppingContainer.emptyStateTextEl,
+          "There should be no empty state text"
+        );
+        Assert.ok(
+          !shoppingContainer.emptyStateSupportedListEl,
+          "There should be no empty state list of supported sites"
+        );
+        Assert.equal(
+          shoppingContainer.shadowRoot.querySelectorAll("analysis-explainer")
+            .length,
+          1,
+          "There should be only one analysis-explainer"
+        );
+        Assert.equal(
+          shoppingContainer.shadowRoot.querySelectorAll("shopping-settings")
+            .length,
+          1,
+          "There should be only one shopping-settings"
+        );
+        Assert.notEqual(
+          shoppingContainer.containerContentEl?.className,
+          "is-empty-state",
+          "There should be no classname for empty state"
+        );
+      });
+    }
+  );
+});
