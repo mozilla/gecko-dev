b6718bf263ae780289da471f0ea35797a09e2f05
ffxbld <ffxbld@mozilla.com>
2024-12-30 13:32:51 +0000

    No Bug, mozilla-central repo-update HSTS HPKP ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D232974

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1741259803000000);
+static const PRTime kCTExpirationTime = INT64_C(1741605333000000);
 
 namespace mozilla::ct {
 
--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -727,4 +727,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1743678990382000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1744024518573000);
