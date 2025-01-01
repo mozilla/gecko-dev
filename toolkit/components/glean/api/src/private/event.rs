15c0f408aee216fbb2080082180d147ab08d6f79
asthetik <aestheticslee@outlook.com>
2025-01-01 10:21:42 +0000

    Bug 1868296 - Optimize EventMetric::Child match arm in record method. r=sylvestre
    Simplify the handling of the `extra` variable by combining the conversion and default assignment into a single `if let` statement.
    This reduces unnecessary variable assignments and makes the code more concise.
    
    Differential Revision: https://phabricator.services.mozilla.com/D232795

--- toolkit/components/glean/api/src/private/event.rs
+++ toolkit/components/glean/api/src/private/event.rs
@@ -192,8 +192,11 @@ impl<K: 'static + ExtraKeys + Send + Sync + Clone> Event for EventMetric<K> {
             }
             EventMetric::Child(_) => {
                 // No need to add a marker here, as we dispatch to `record_with_time` above.
-                let extra = extra.into().map(|extra| extra.into_ffi_extra());
-                let extra = extra.unwrap_or_default();
+                let extra = if let Some(extra) = extra.into() {
+                    extra.into_ffi_extra()
+                } else {
+                    HashMap::new()
+                };
                 self.record_with_time(now, extra);
             }
         }
