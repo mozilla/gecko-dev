a234bcc77f736ae2cd1cfacb6f4036d6fbf7f8e7
Jonathan Kew <jkew@mozilla.com>
2025-01-02 10:17:53 +0000

    Bug 1935148 - Remove newline (instead of transforming to space) if adjacent to East-Asian punctuation character. r=m_kato
    Includes the examples from the report as a testcase, though there is not yet
    any formal spec for the exact behavior of segment break transformation.
    (But nevertheless there is an existing collection of tests, so this just adds
    one for the punctuation case.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D231476

--- layout/generic/nsTextFrameUtils.h
+++ layout/generic/nsTextFrameUtils.h
@@ -10,6 +10,7 @@
 #include "gfxSkipChars.h"
 #include "nsBidiUtils.h"
 
+class nsAtom;
 class nsIContent;
 struct nsStyleText;
 
@@ -121,12 +122,15 @@ class nsTextFrameUtils {
    * @param aIncomingFlags a flag indicating whether there was whitespace
    * or an Arabic character preceding this text. We set it to indicate if
    * there's an Arabic character or whitespace preceding the end of this text.
+   * @param aLanguage Content language (used to select Japanese/Chinese behavior
+   * at punctuation, see https://bugzilla.mozilla.org/show_bug.cgi?id=1935148).
    */
   template <class CharT>
   static CharT* TransformText(const CharT* aText, uint32_t aLength,
                               CharT* aOutput, CompressionMode aCompression,
                               uint8_t* aIncomingFlags, gfxSkipChars* aSkipChars,
-                              nsTextFrameUtils::Flags* aAnalysisFlags);
+                              nsTextFrameUtils::Flags* aAnalysisFlags,
+                              const nsAtom* aLanguage);
 
   /**
    * Returns whether aChar is a character that nsTextFrameUtils::TransformText
