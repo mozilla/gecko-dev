b3fd22a6b09ed7c62e205200529313dda661f389
Calixte <cdenizet@mozilla.com>
2025-01-06 20:30:01 +0000

    Bug 1940123 - Don't update the pdf.js revision for talos tests r=pdfjs-reviewers,marco
    Differential Revision: https://phabricator.services.mozilla.com/D233288

--- toolkit/components/pdfjs/update.sh
+++ toolkit/components/pdfjs/update.sh
@@ -44,8 +44,10 @@ rsync -a -v --delete "$ROOT/build/mozcentral/browser/extensions/pdfjs/content/we
 ls -R "$ROOT/build/mozcentral/browser/"
 cp "$ROOT"/build/mozcentral/browser/locales/en-US/pdfviewer/*.ftl "$GECKO_PATH/toolkit/locales/en-US/toolkit/pdfviewer/" || true
 
+# For now we don't update the revision because we need to reduce the number of files to test
+# (cf https://bugzilla.mozilla.org/show_bug.cgi?id=1940144 for re-enabling).
 # Update the revision in the toolchains.yml file for the Talos tests.
-sed -i -z "s/\(mozilla-pdf\.js.*revision: \)[0-9a-f]*/\1$1/g" "$GECKO_PATH/taskcluster/kinds/fetch/toolchains.yml"
+# sed -i -z "s/\(mozilla-pdf\.js.*revision: \)[0-9a-f]*/\1$1/g" "$GECKO_PATH/taskcluster/kinds/fetch/toolchains.yml"
 
 if [ -v PDFJS_TMPDIR ]; then
 	rm -rf "$PDFJS_TMPDIR"
