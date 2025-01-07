a4920b01f5b7fe4609c82bc467c746ac4fe1431d
Ryan VanderMeulen <ryanvm@gmail.com>
2025-01-07 14:47:01 +0000

    Bug 1940130 - Update JUnit to version 5.11.4. r=android-reviewers,sfamisa
    Differential Revision: https://phabricator.services.mozilla.com/D233290

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -109,7 +109,7 @@ object Versions {
         const val androidx_test_uiautomator = "2.3.0"
         const val falcon = "2.2.0"
         const val fastlane = "2.1.1"
-        const val junit = "5.11.3"
+        const val junit = "5.11.4"
         const val mockito = "5.14.2"
         const val mockk = "1.13.13"
         const val robolectric = "4.14.1"
