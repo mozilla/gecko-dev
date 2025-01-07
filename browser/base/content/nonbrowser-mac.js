64d3d2c4a5f273034a37cdc8cb4b97f468a86d78
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2025-01-07 15:15:53 +0000

    Bug 1939584 - clean up nonbrowser-mac.js and stop using inline event handlers in the mac hidden window, r=tschuster,firefox-desktop-core-reviewers ,mossop
    Differential Revision: https://phabricator.services.mozilla.com/D233045

--- browser/base/content/nonbrowser-mac.js
+++ browser/base/content/nonbrowser-mac.js
@@ -5,158 +5,168 @@
 
 /* eslint-env mozilla/browser-window */
 
-ChromeUtils.defineESModuleGetters(this, {
-  NimbusFeatures: "resource://nimbus/ExperimentAPI.sys.mjs",
-});
+var NonBrowserWindow = {
+  delayedStartupTimeoutId: null,
+  MAC_HIDDEN_WINDOW: "chrome://browser/content/hiddenWindowMac.xhtml",
 
-let delayedStartupTimeoutId = null;
+  openBrowserWindowFromDockMenu(options = {}) {
+    let existingWindow = BrowserWindowTracker.getTopWindow();
+    options.openerWindow = existingWindow || window;
+    let win = OpenBrowserWindow(options);
+    win.addEventListener(
+      "load",
+      () => this.dockSupport.activateApplication(true),
+      { once: true }
+    );
 
-function OpenBrowserWindowFromDockMenu(options = {}) {
-  let existingWindow = BrowserWindowTracker.getTopWindow();
-  options.openerWindow = existingWindow || window;
-  let win = OpenBrowserWindow(options);
-  win.addEventListener(
-    "load",
-    function () {
-      let dockSupport = Cc["@mozilla.org/widget/macdocksupport;1"].getService(
-        Ci.nsIMacDockSupport
-      );
-      dockSupport.activateApplication(true);
-    },
-    { once: true }
-  );
+    return win;
+  },
 
-  return win;
-}
-
-function nonBrowserWindowStartup() {
-  // Disable inappropriate commands / submenus
-  var disabledItems = [
-    "Browser:SavePage",
-    "Browser:SendLink",
-    "cmd_pageSetup",
-    "cmd_print",
-    "cmd_find",
-    "cmd_findAgain",
-    "viewToolbarsMenu",
-    "viewSidebarMenuMenu",
-    "Browser:Reload",
-    "viewFullZoomMenu",
-    "pageStyleMenu",
-    "repair-text-encoding",
-    "View:PageSource",
-    "View:FullScreen",
-    "enterFullScreenItem",
-    "viewHistorySidebar",
-    "Browser:AddBookmarkAs",
-    "Browser:BookmarkAllTabs",
-    "View:PageInfo",
-    "History:UndoCloseTab",
-    "menu_openFirefoxView",
-  ];
-  var element;
-
-  for (let disabledItem of disabledItems) {
-    element = document.getElementById(disabledItem);
-    if (element) {
-      element.setAttribute("disabled", "true");
-    }
-  }
-
-  // Show menus that are only visible in non-browser windows
-  let shownItems = ["menu_openLocation"];
-  for (let shownItem of shownItems) {
-    element = document.getElementById(shownItem);
-    if (element) {
-      element.removeAttribute("hidden");
-    }
-  }
-
-  if (
-    window.location.href == "chrome://browser/content/hiddenWindowMac.xhtml"
-  ) {
-    // If no windows are active (i.e. we're the hidden window), disable the
-    // close, minimize and zoom menu commands as well.
-    var hiddenWindowDisabledItems = [
-      "cmd_close",
-      "cmd_minimizeWindow",
-      "zoomWindow",
+  startup() {
+    // Disable inappropriate commands / submenus
+    var disabledItems = [
+      "Browser:SavePage",
+      "Browser:SendLink",
+      "cmd_pageSetup",
+      "cmd_print",
+      "cmd_find",
+      "cmd_findAgain",
+      "viewToolbarsMenu",
+      "viewSidebarMenuMenu",
+      "Browser:Reload",
+      "viewFullZoomMenu",
+      "pageStyleMenu",
+      "repair-text-encoding",
+      "View:PageSource",
+      "View:FullScreen",
+      "enterFullScreenItem",
+      "viewHistorySidebar",
+      "Browser:AddBookmarkAs",
+      "Browser:BookmarkAllTabs",
+      "View:PageInfo",
+      "History:UndoCloseTab",
+      "menu_openFirefoxView",
     ];
-    for (let hiddenWindowDisabledItem of hiddenWindowDisabledItems) {
-      element = document.getElementById(hiddenWindowDisabledItem);
+    var element;
+
+    for (let disabledItem of disabledItems) {
+      element = document.getElementById(disabledItem);
       if (element) {
         element.setAttribute("disabled", "true");
       }
     }
 
-    // Also hide the window-list separator.
-    element = document.getElementById("sep-window-list");
-    element.hidden = true;
-
-    // Setup the dock menu.
-    let dockMenuElement = document.getElementById("menu_mac_dockmenu");
-    if (dockMenuElement != null) {
-      let nativeMenu = Cc[
-        "@mozilla.org/widget/standalonenativemenu;1"
-      ].createInstance(Ci.nsIStandaloneNativeMenu);
-
-      try {
-        nativeMenu.init(dockMenuElement);
-
-        let dockSupport = Cc["@mozilla.org/widget/macdocksupport;1"].getService(
-          Ci.nsIMacDockSupport
-        );
-        dockSupport.dockMenu = nativeMenu;
-      } catch (e) {}
+    // Show menus that are only visible in non-browser windows
+    let shownItems = ["menu_openLocation"];
+    for (let shownItem of shownItems) {
+      element = document.getElementById(shownItem);
+      if (element) {
+        element.removeAttribute("hidden");
+      }
     }
 
-    // Hide menuitems that don't apply to private contexts.
-    if (PrivateBrowsingUtils.permanentPrivateBrowsing) {
-      document.getElementById("macDockMenuNewWindow").hidden = true;
+    if (window.location.href == this.MAC_HIDDEN_WINDOW) {
+      // If no windows are active (i.e. we're the hidden window), disable the
+      // close, minimize and zoom menu commands as well.
+      var hiddenWindowDisabledItems = [
+        "cmd_close",
+        "cmd_minimizeWindow",
+        "zoomWindow",
+      ];
+      for (let hiddenWindowDisabledItem of hiddenWindowDisabledItems) {
+        element = document.getElementById(hiddenWindowDisabledItem);
+        if (element) {
+          element.setAttribute("disabled", "true");
+        }
+      }
+
+      // Also hide the window-list separator.
+      element = document.getElementById("sep-window-list");
+      element.hidden = true;
+
+      // Setup the dock menu.
+      let dockMenuElement = document.getElementById("menu_mac_dockmenu");
+      if (dockMenuElement != null) {
+        let nativeMenu = Cc[
+          "@mozilla.org/widget/standalonenativemenu;1"
+        ].createInstance(Ci.nsIStandaloneNativeMenu);
+
+        try {
+          nativeMenu.init(dockMenuElement);
+          this.dockSupport.dockMenu = nativeMenu;
+        } catch (e) {}
+      }
+
+      dockMenuElement.addEventListener("command", this);
+
+      // Hide menuitems that don't apply to private contexts.
+      if (PrivateBrowsingUtils.permanentPrivateBrowsing) {
+        document.getElementById("macDockMenuNewWindow").hidden = true;
+      }
+      if (!PrivateBrowsingUtils.enabled) {
+        document.getElementById("macDockMenuNewPrivateWindow").hidden = true;
+      }
+      if (BrowserUIUtils.quitShortcutDisabled) {
+        document.getElementById("key_quitApplication").remove();
+        document.getElementById("menu_FileQuitItem").removeAttribute("key");
+      }
     }
-    if (!PrivateBrowsingUtils.enabled) {
-      document.getElementById("macDockMenuNewPrivateWindow").hidden = true;
+
+    this.delayedStartupTimeoutId = setTimeout(() => this.delayedStartup(), 0);
+  },
+
+  delayedStartup() {
+    this.delayedStartupTimeoutId = null;
+
+    // initialise the offline listener
+    BrowserOffline.init();
+
+    // initialize the private browsing UI
+    gPrivateBrowsingUI.init();
+  },
+
+  shutdown() {
+    // If this is the hidden window being closed, release our reference to
+    // the dock menu element to prevent leaks on shutdown
+    if (window.location.href == this.MAC_HIDDEN_WINDOW) {
+      this.dockSupport.dockMenu = null;
     }
-    if (BrowserUIUtils.quitShortcutDisabled) {
-      document.getElementById("key_quitApplication").remove();
-      document.getElementById("menu_FileQuitItem").removeAttribute("key");
+
+    // If nonBrowserWindowDelayedStartup hasn't run yet, we have no work to do -
+    // just cancel the pending timeout and return;
+    if (this.delayedStartupTimeoutId) {
+      clearTimeout(this.delayedStartupTimeoutId);
+      return;
     }
-  }
 
-  delayedStartupTimeoutId = setTimeout(nonBrowserWindowDelayedStartup, 0);
-}
+    BrowserOffline.uninit();
+  },
 
-function nonBrowserWindowDelayedStartup() {
-  delayedStartupTimeoutId = null;
+  handleEvent(event) {
+    switch (event.type) {
+      case "load":
+        this.startup();
+        break;
+      case "unload":
+        this.shutdown();
+        break;
+      case "command":
+        if (event.target.id.startsWith("macDockMenuNew")) {
+          let wantPrivate = event.target.id == "macDockMenuNewPrivateWindow";
+          this.openBrowserWindowFromDockMenu(
+            wantPrivate ? { private: true } : {}
+          );
+        }
+        break;
+    }
+  },
+};
 
-  // initialise the offline listener
-  BrowserOffline.init();
-
-  // initialize the private browsing UI
-  gPrivateBrowsingUI.init();
-}
-
-function nonBrowserWindowShutdown() {
-  // If this is the hidden window being closed, release our reference to
-  // the dock menu element to prevent leaks on shutdown
-  if (
-    window.location.href == "chrome://browser/content/hiddenWindowMac.xhtml"
-  ) {
-    let dockSupport = Cc["@mozilla.org/widget/macdocksupport;1"].getService(
-      Ci.nsIMacDockSupport
-    );
-    dockSupport.dockMenu = null;
-  }
-
-  // If nonBrowserWindowDelayedStartup hasn't run yet, we have no work to do -
-  // just cancel the pending timeout and return;
-  if (delayedStartupTimeoutId) {
-    clearTimeout(delayedStartupTimeoutId);
-    return;
-  }
-
-  BrowserOffline.uninit();
-}
-
-addEventListener("load", nonBrowserWindowStartup, false);
-addEventListener("unload", nonBrowserWindowShutdown, false);
+addEventListener("load", NonBrowserWindow, false);
+addEventListener("unload", NonBrowserWindow, false);
+XPCOMUtils.defineLazyServiceGetter(
+  NonBrowserWindow,
+  "dockSupport",
+  "@mozilla.org/widget/macdocksupport;1",
+  Ci.nsIMacDockSupport
+);
