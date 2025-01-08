a6cc5ddf0d3869d42b25e5df8f0eca1be773e69d
Michael Froman <mfroman@mozilla.com>
2025-01-08 19:21:05 +0000

    Bug 1936422 - fix extract-for-git.py for new third_party/chromium/build directory. r=dbaker DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D233500

--- dom/media/webrtc/third_party_build/extract-for-git.py
+++ dom/media/webrtc/third_party_build/extract-for-git.py
@@ -77,7 +77,6 @@ def filter_nonwebrtc(commit):
         # moz.build files which are code generated.
         if (
             line.startswith("diff --git a/" + LIBWEBRTC_DIR)
-            and not line.startswith("diff --git a/" + LIBWEBRTC_DIR + "/build/")
             and not line.startswith("diff --git a/" + LIBWEBRTC_DIR + "/third_party/")
             and not line.startswith("diff --git a/" + LIBWEBRTC_DIR + "/README.moz")
             and not line.startswith(
@@ -131,7 +130,9 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    if args.target != "libwebrtc":
+    if args.target == "build":
+        LIBWEBRTC_DIR = "third_party/chromium/build"
+    elif args.target == "third_party":
         LIBWEBRTC_DIR = os.path.join(LIBWEBRTC_DIR, args.target)
 
     # must run 'hg' with HGPLAIN=1 to ensure aliases don't interfere with
