5d94a708b91970e81dd459a1588fb57e7f49126c
Alexandru Marc <amarc@mozilla.com>
2025-01-08 17:25:51 +0200

    Backed out 1 changesets (bug 1940434) for causing SM bustages @ denormals-1.js CLOSED TREE
    Backed out changeset 2cf7471462a9 (bug 1940434)

--- js/src/jit-test/tests/self-test/denormals-0.js
+++
@@ -1,3 +0,0 @@
-// |jit-test| error: Assertion failed: got 5e-324, expected 0
-
-assertEq(5E-324, 0);
--- js/src/jit-test/tests/self-test/denormals-1.js
+++
@@ -1,3 +0,0 @@
-// |jit-test| --disable-main-thread-denormals; skip-if: !getBuildConfiguration("x64") && !getBuildConfiguration("arm") && !getBuildConfiguration("arm64");
-
-assertEq(5E-324, 0);
