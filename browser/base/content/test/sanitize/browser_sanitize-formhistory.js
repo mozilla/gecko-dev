2483b714df80bd77f254b856bf33a74eedf196c1
Harshit Sohaney <73911621+HarshitSohaney@users.noreply.github.com>
2025-01-09 12:52:46 +0000

    Bug 1888466 - (part 5) Fix the browser_sanitize-formhistory test to test using the relevant cleaners. r=emz,manuel
    Differential Revision: https://phabricator.services.mozilla.com/D222734

--- browser/base/content/test/sanitize/browser_sanitize-formhistory.js
+++ browser/base/content/test/sanitize/browser_sanitize-formhistory.js
@@ -3,30 +3,26 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 add_task(async function test() {
-  let prefs = ["history", "historyFormDataAndDownloads"];
+  // This test relies on the form history being empty to start with delete
+  // all the items first.
+  // The TabContextMenu initializes its strings only on a focus or mouseover event.
+  // Calls focus event on the TabContextMenu early in the test.
+  gBrowser.selectedTab.focus();
+  await FormHistory.update({ op: "remove" });
 
-  for (let pref of prefs) {
-    // This test relies on the form history being empty to start with delete
-    // all the items first.
-    // The TabContextMenu initializes its strings only on a focus or mouseover event.
-    // Calls focus event on the TabContextMenu early in the test.
-    gBrowser.selectedTab.focus();
-    await FormHistory.update({ op: "remove" });
+  // Sanitize now so we can test the baseline point.
+  await Sanitizer.sanitize(["formdata"]);
+  await gFindBarPromise;
+  ok(!gFindBar.hasTransactions, "pre-test baseline for sanitizer");
 
-    // Sanitize now so we can test the baseline point.
-    await Sanitizer.sanitize([pref]);
-    await gFindBarPromise;
-    ok(!gFindBar.hasTransactions, "pre-test baseline for sanitizer");
+  gFindBar.getElement("findbar-textbox").value = "m";
+  ok(gFindBar.hasTransactions, "formdata can be cleared after input");
 
-    gFindBar.getElement("findbar-textbox").value = "m";
-    ok(gFindBar.hasTransactions, "formdata can be cleared after input");
-
-    await Sanitizer.sanitize(["formdata"]);
-    is(
-      gFindBar.getElement("findbar-textbox").value,
-      "",
-      "findBar textbox should be empty after sanitize"
-    );
-    ok(!gFindBar.hasTransactions, "No transactions after sanitize");
-  }
+  await Sanitizer.sanitize(["formdata"]);
+  is(
+    gFindBar.getElement("findbar-textbox").value,
+    "",
+    "findBar textbox should be empty after sanitize"
+  );
+  ok(!gFindBar.hasTransactions, "No transactions after sanitize");
 });
