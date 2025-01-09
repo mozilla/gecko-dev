5c00bdc9de2952ac20f48ca8b7b13a78561649c9
James Teh <jteh@mozilla.com>
2025-01-09 13:44:15 +0000

    Bug 1936294: Add a11y test for focus when changing a tab from non-remote to remote. r=nlapre
    Differential Revision: https://phabricator.services.mozilla.com/D232437

--- accessible/tests/browser/events/browser_focus_document.js
+++ accessible/tests/browser/events/browser_focus_document.js
@@ -19,3 +19,27 @@ addAccessibleTask(``, async function testRemoteThenLocal(browser) {
   });
   await focused;
 });
+
+/**
+ * Test that switching from a non-remote document to a remote document fires
+ * focus correctly.
+ */
+addAccessibleTask(
+  ``,
+  async function testLocalThenRemote(browser) {
+    info("Loading example.com into same tab");
+    let focused = waitForEvent(EVENT_FOCUS, event => {
+      const acc = event.accessible;
+      acc.QueryInterface(nsIAccessibleDocument);
+      return acc.URL == "https://example.com/";
+    });
+    // The accessibility test harness removes maychangeremoteness when we run a
+    // chrome test, but we explicitly want to change remoteness now.
+    browser.setAttribute("maychangeremoteness", "true");
+    browser.loadURI(Services.io.newURI("https://example.com/"), {
+      triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
+    });
+    await focused;
+  },
+  { chrome: true, topLevel: false }
+);
