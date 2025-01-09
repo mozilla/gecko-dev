07a6500dc12ccb78418a7a2716657ea5ff11e461
Mike Hommey <mh+mozilla@glandium.org>
2025-01-09 01:27:54 +0000

    Bug 1940602 - Don't declare AnnotationType consts when they are #define'd. r=Jamie
    Newer versions of mingw started #define'ing them, which conflicts with
    the consts we declare for older versions that don't have them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D233594

--- accessible/windows/uia/supplementalMinGWDefinitions.h
+++ accessible/windows/uia/supplementalMinGWDefinitions.h
@@ -13,31 +13,81 @@
 #define mozilla_a11y_supplementalMinGWDefinitions_h__
 
 // UIA Annotation Type Identifiers
+#ifndef AnnotationType_Unknown
 const long AnnotationType_Unknown = 60000;
+#endif
+#ifndef AnnotationType_SpellingError
 const long AnnotationType_SpellingError = 60001;
+#endif
+#ifndef AnnotationType_GrammarError
 const long AnnotationType_GrammarError = 60002;
+#endif
+#ifndef AnnotationType_Comment
 const long AnnotationType_Comment = 60003;
+#endif
+#ifndef AnnotationType_FormulaError
 const long AnnotationType_FormulaError = 60004;
+#endif
+#ifndef AnnotationType_TrackChanges
 const long AnnotationType_TrackChanges = 60005;
+#endif
+#ifndef AnnotationType_Header
 const long AnnotationType_Header = 60006;
+#endif
+#ifndef AnnotationType_Footer
 const long AnnotationType_Footer = 60007;
+#endif
+#ifndef AnnotationType_Highlighted
 const long AnnotationType_Highlighted = 60008;
+#endif
+#ifndef AnnotationType_Endnote
 const long AnnotationType_Endnote = 60009;
+#endif
+#ifndef AnnotationType_Footnote
 const long AnnotationType_Footnote = 60010;
+#endif
+#ifndef AnnotationType_InsertionChange
 const long AnnotationType_InsertionChange = 60011;
+#endif
+#ifndef AnnotationType_DeletionChange
 const long AnnotationType_DeletionChange = 60012;
+#endif
+#ifndef AnnotationType_MoveChange
 const long AnnotationType_MoveChange = 60013;
+#endif
+#ifndef AnnotationType_FormatChange
 const long AnnotationType_FormatChange = 60014;
+#endif
+#ifndef AnnotationType_UnsyncedChange
 const long AnnotationType_UnsyncedChange = 60015;
+#endif
+#ifndef AnnotationType_EditingLockedChange
 const long AnnotationType_EditingLockedChange = 60016;
+#endif
+#ifndef AnnotationType_ExternalChange
 const long AnnotationType_ExternalChange = 60017;
+#endif
+#ifndef AnnotationType_ConflictingChange
 const long AnnotationType_ConflictingChange = 60018;
+#endif
+#ifndef AnnotationType_Author
 const long AnnotationType_Author = 60019;
+#endif
+#ifndef AnnotationType_AdvancedProofingIssue
 const long AnnotationType_AdvancedProofingIssue = 60020;
+#endif
+#ifndef AnnotationType_DataValidationError
 const long AnnotationType_DataValidationError = 60021;
+#endif
+#ifndef AnnotationType_CircularReferenceError
 const long AnnotationType_CircularReferenceError = 60022;
+#endif
+#ifndef AnnotationType_Mathematics
 const long AnnotationType_Mathematics = 60023;
+#endif
+#ifndef AnnotationType_Sensitive
 const long AnnotationType_Sensitive = 60024;
+#endif
 
 // UIA Style Identifiers
 const long StyleId_Custom = 70000;
