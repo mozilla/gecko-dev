8c1db650217a2e9c3dc746238caccbe1ee6d3119
Tooru Fujisawa <arai_a@mac.com>
2025-01-10 10:18:28 +0000

    Bug 1937699 - Part 1: Fix chrome-map.json entry for preprocessed files. r=firefox-build-system-reviewers,glandium
    chrome-map.json was using the .in filename also in the objdir path, where it
    should be using the .in filename only in the source dir path.
    
    Differential Revision: https://phabricator.services.mozilla.com/D232593

--- python/mozbuild/mozbuild/codecoverage/chrome_map.py
+++ python/mozbuild/mozbuild/codecoverage/chrome_map.py
@@ -97,9 +97,9 @@ class ChromeMapBackend(CommonBackend):
         for path, files in obj.files.walk():
             for f in files:
                 dest = mozpath.join(obj.install_target, path, f.target_basename)
+                if dest.endswith(".in"):
+                    dest = dest[:-3]
                 obj_path = mozpath.join(self.environment.topobjdir, dest)
-                if obj_path.endswith(".in"):
-                    obj_path = obj_path[:-3]
                 if isinstance(obj, FinalTargetPreprocessedFiles):
                     assert os.path.exists(obj_path), "%s should exist" % obj_path
                     pp_info = generate_pp_info(obj_path, obj.topsrcdir)
