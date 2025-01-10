b76a0509e0bd4ce810890c6ade53d26fa4afd38e
Alexandru Marc <amarc@mozilla.com>
2025-01-10 22:36:52 +0200

    Backed out changeset 22aed046ea10 (bug 1940988) for causing linting failures @ syncedtabs-tab-list.mjs CLOSED TREE

--- browser/components/firefoxview/syncedtabs-tab-list.mjs
+++ browser/components/firefoxview/syncedtabs-tab-list.mjs
@@ -145,8 +145,10 @@ export class SyncedTabsTabRow extends FxviewTabRowBase {
       () =>
         html`<moz-button
           type="icon ghost"
-          class="fxview-tab-row-button"
-          iconSrc=${this.getIconSrc(this.secondaryActionClass)}
+          class=${classMap({
+            "fxview-tab-row-button": true,
+            [this.secondaryActionClass]: this.secondaryActionClass,
+          })}
           ?disabled=${this.closeRequested}
           id="fxview-tab-row-secondary-button"
           data-l10n-id=${this.secondaryL10nId}
