19e5b76f66675f6f16dfe03f027eb068de991f63
Karl Tomlinson <karlt+@karlt.net>
2025-01-10 22:23:44 +0000

    Bug 1940883 return early even when an internal seek is waiting r=media-playback-reviewers,padenot
    UpdateReceivedNewData() would have aborted Update() early on
    mSeekRequest.Exists() if there were an external seek, so at this point any
    seek request would be internal.
    
    This doesn't change behavior because `needInput` is false when
    `HasInternalSeekPending()`, but merely clarifies that nothing more will
    happen.
    
    Differential Revision: https://phabricator.services.mozilla.com/D233809

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -2470,8 +2470,10 @@ void MediaFormatReader::Update(TrackType aTrack) {
       // Now that draining has completed, we check if we have received
       // new data again as the result may now be different from the earlier
       // run.
-      if (UpdateReceivedNewData(aTrack) || decoder.mSeekRequest.Exists()) {
-        LOGV("Nothing more to do");
+      if (UpdateReceivedNewData(aTrack) || decoder.HasInternalSeekPending()) {
+        LOGV("%s: Nothing more to do", decoder.HasInternalSeekWaiting()
+                                           ? "Waiting during internal seek"
+                                           : "Completed drain");
         return;
       }
     } else if (decoder.mDemuxEOS && !decoder.HasPendingDrain() &&
