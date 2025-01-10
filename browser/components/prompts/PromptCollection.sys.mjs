c944a1e5d494a4b12eb0a37cc33ab6933870fdd6
Alexandru Marc <amarc@mozilla.com>
2025-01-10 22:07:55 +0200

    Backed out changeset 562e1030923b (bug 1927587) for causing bc failures @ browser_confirmFolderUpload.js CLOSED TREE

--- browser/components/prompts/PromptCollection.sys.mjs
+++ browser/components/prompts/PromptCollection.sys.mjs
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { SelectableProfileService } from "resource:///modules/profiles/SelectableProfileService.sys.mjs";
-
 /**
  * Implements nsIPromptCollection
  *
@@ -74,13 +72,9 @@ export class PromptCollection {
       return true;
     }
 
-    let originNoSuffix =
-      browsingContext.embedderElement?.contentPrincipal.originNoSuffix;
-    const isPDFjs = originNoSuffix === "resource://pdf.js";
-    const isProfilePage =
-      originNoSuffix === "about:newprofile" ||
-      originNoSuffix === "about:editprofile";
-
+    const isPDFjs =
+      browsingContext.embedderElement?.contentPrincipal.originNoSuffix ===
+      "resource://pdf.js";
     let title, message, leaveLabel, stayLabel, buttonFlags;
     let args = {
       // Tell the prompt service that this is a permit unload prompt
@@ -105,28 +99,6 @@ export class PromptCollection {
         args.useTitle = true;
         args.headerIconCSSValue =
           "url('chrome://branding/content/document_pdf.svg')";
-      } else if (isProfilePage) {
-        title = this.stringBundles.dom.GetStringFromName(
-          "OnBeforeUnloadAboutNewProfileTitle"
-        );
-        let defaultName = SelectableProfileService.currentProfile.name;
-        message = this.stringBundles.dom.formatStringFromName(
-          "OnBeforeUnloadAboutNewProfileMessage",
-          [defaultName]
-        );
-        leaveLabel = this.stringBundles.dom.GetStringFromName(
-          "OnBeforeUnloadAboutNewProfileLeaveButton"
-        );
-        stayLabel = this.stringBundles.dom.GetStringFromName(
-          "OnBeforeUnloadAboutNewProfileStayButton"
-        );
-        buttonFlags =
-          Ci.nsIPromptService.BUTTON_POS_0_DEFAULT |
-          (Ci.nsIPromptService.BUTTON_TITLE_IS_STRING *
-            Ci.nsIPromptService.BUTTON_POS_0) |
-          (Ci.nsIPromptService.BUTTON_TITLE_IS_STRING *
-            Ci.nsIPromptService.BUTTON_POS_1);
-        args.useTitle = true;
       } else {
         title = this.stringBundles.dom.GetStringFromName("OnBeforeUnloadTitle");
         message = this.stringBundles.dom.GetStringFromName(
@@ -184,14 +156,6 @@ export class PromptCollection {
         const actor = browsingContext.currentWindowGlobal.getActor("Pdfjs");
         actor.sendAsyncMessage("PDFJS:Save");
         await savePdfPromise;
-      }
-      return buttonNumClicked !== 1;
-    } else if (isProfilePage) {
-      if (buttonNumClicked === 0) {
-        Services.prefs.setBoolPref(
-          "browser.profiles.profile-name.updated",
-          true
-        );
       }
       return buttonNumClicked !== 1;
     }
