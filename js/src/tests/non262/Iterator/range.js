84736d56e61b370e8f3dff4221d21dad4ee75882
Serah Nderi <sarahngima77@gmail.com>
2025-01-13 18:37:29 +0000

    Bug 1939732 - Support other sequences in Iterator.range; r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D233814

--- js/src/tests/non262/Iterator/range.js
+++ js/src/tests/non262/Iterator/range.js
@@ -65,7 +65,59 @@ assertDeepEq(result3, []);
 const result4 = Array.from(Iterator.range(5, 5));
 assertDeepEq(result4, []);
 
-//TODO: support/test other sequences
+// testing reverse range
+const result5 = Array.from(Iterator.range(10, 0, -2));
+assertDeepEq(result5, [10, 8, 6, 4, 2]);
+
+// inclusive end
+const result6 = Array.from(Iterator.range(0, 10, { step: 2, inclusiveEnd: true }));
+assertDeepEq(result6, [0, 2, 4, 6, 8, 10]);
+
+// overlapping step with range limits
+const result7 = Array.from(Iterator.range(0, 10, 15));
+assertDeepEq(result7, [0]);
+
+// negative numbers in the range
+const result8 = Array.from(Iterator.range(-10, 0));
+assertDeepEq(result8, [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]);
+
+// floating point
+const resultFloat1 = Array.from(Iterator.range(0.5, 3.5));
+assertDeepEq(resultFloat1, [0.5, 1.5, 2.5]);
+
+const resultFloat3 = Array.from(Iterator.range(0, 0.3, 0.1));
+assertDeepEq(resultFloat3, [0, 0.1, 0.2]);
+
+
+// floating point precision
+function approximatelyEqual(a, b) {
+    // If inputs are arrays, compare each element
+    if (Array.isArray(a) && Array.isArray(b)) {
+        assertEq(a.length, b.length);
+        // Compare each element
+        for (let i = 0; i < a.length; i++) {
+            approximatelyEqual(a[i], b[i]);
+        }
+        return true;
+    }
+
+    // For single numbers
+    var r = (a != a && b != b) || Math.abs(a - b) < 0.001;
+    if (!r) {
+        print('Got', a, ', to be approximately equal to', b);
+        assertEq(false, true);
+    }
+    return true;
+}
+
+const resultFloat2 = Array.from(Iterator.range(0, 1, 0.2));
+approximatelyEqual(resultFloat2, [0, 0.2, 0.4, 0.6, 0.8]);
+
+const resultStep2 = Array.from(Iterator.range(0, 10, 3.3));
+approximatelyEqual(resultStep2, [0, 3.3, 6.6, 9.9]);
+
+// invalid mixed-type parameters
+assertThrowsInstanceOf(() => Iterator.range(0, 10, { step: NaN, inclusiveEnd: true }), RangeError);
 
 if (typeof reportCompare === 'function')
     reportCompare(0, 0);
