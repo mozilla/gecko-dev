fab3284999f4144b384ed71db484a1103242adf2
Mike Hommey <mh+mozilla@glandium.org>
2025-01-14 21:18:19 +0000

    Bug 1867845 - Split rlbox.wasm.c in multiple parts in local builds. r=firefox-build-system-reviewers,sergesanspaille
    We pick an arbitrary number of files to split the wasm2c output.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195276

--- config/wasm2c.py
+++ config/wasm2c.py
@@ -6,9 +6,9 @@ import os
 import subprocess
 
 
-def wasm2c(output, wasm2c_bin, wasm_lib):
+def wasm2c(output, wasm2c_bin, wasm_lib, *flags):
     output.close()
     module_name = os.path.basename(os.path.splitext(wasm_lib)[0])
     return subprocess.run(
-        [wasm2c_bin, "-n", module_name, "-o", output.name, wasm_lib]
+        [wasm2c_bin, "-n", module_name] + list(flags) + ["-o", output.name, wasm_lib]
     ).returncode
