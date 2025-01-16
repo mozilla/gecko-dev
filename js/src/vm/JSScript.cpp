e31b8e7f52353a0e00dc000d7d5e503d3740ebcb
Tooru Fujisawa <arai_a@mac.com>
2025-01-16 03:20:37 +0000

    Bug 1939710 - Add ScriptSource reader before getting the units. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D234130

--- js/src/vm/JSScript.cpp
+++ js/src/vm/JSScript.cpp
@@ -1255,9 +1255,11 @@ ScriptSource::PinnedUnits<Unit>::PinnedUnits(
     : PinnedUnitsBase(source) {
   MOZ_ASSERT(source->hasSourceType<Unit>(), "must pin units of source's type");
 
+  addReader();
+
   units_ = source->units<Unit>(cx, holder, begin, len);
-  if (units_) {
-    addReader();
+  if (!units_) {
+    removeReader<Unit>();
   }
 }
 
@@ -1270,9 +1272,11 @@ ScriptSource::PinnedUnitsIfUncompressed<Unit>::PinnedUnitsIfUncompressed(
     : PinnedUnitsBase(source) {
   MOZ_ASSERT(source->hasSourceType<Unit>(), "must pin units of source's type");
 
+  addReader();
+
   units_ = source->uncompressedUnits<Unit>(begin, len);
-  if (units_) {
-    addReader();
+  if (!units_) {
+    removeReader<Unit>();
   }
 }
 
