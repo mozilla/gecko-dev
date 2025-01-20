56ee5ff6fb6482fff3c91b420fec8592ace75b44
ffxbld <ffxbld@mozilla.com>
2025-01-20 17:41:08 +0000

    No Bug, mozilla-beta repo-update HSTS HPKP remote-settings tld-suffixes mobile-experiments ct-logs -  a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D234823

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1743073952000000);
+static const PRTime kCTExpirationTime = INT64_C(1743419727000000);
 
 namespace mozilla::ct {
 
--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -727,4 +727,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1745493137876000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1745838912931000);
