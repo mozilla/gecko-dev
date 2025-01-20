5089982c13271f633b5936934233ee7d28812579
aglavic <aglavic@mozilla.com>
2025-01-20 16:04:44 +0000

    Bug 1929960 - Enable non-essential tp6m tests on chrome-m for android crons. r=perftest-reviewers,taskgraph-reviewers,fbilt,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D234407

--- taskcluster/gecko_taskgraph/target_tasks.py
+++ taskcluster/gecko_taskgraph/target_tasks.py
@@ -818,7 +818,8 @@ def target_tasks_general_perf_testing(full_task_graph, parameters, graph_config)
                 return False
             if "chrome-m" in try_name and "-nofis" not in try_name:
                 return False
-            if "chrome-m" in try_name and "essential" in try_name:
+            # Bug 1929960 - Enable all chrome-m tp6m tests on a55 only
+            if "chrome-m" in try_name and "tp6m" in try_name and "hw-a55" in platform:
                 return True
             if "chrome-m" in try_name and (
                 ("ebay" in try_name and "live" not in try_name)
