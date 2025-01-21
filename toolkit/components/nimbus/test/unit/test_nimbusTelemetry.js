1fbbab5f737473508aceb31b24d46996468f1b54
Beth Rennie <beth@brennie.ca>
2025-01-21 18:07:44 +0000

    Bug 1942637 - Skip tests that call recordTargetingContext() in artifact builds r=emcminn
    Differential Revision: https://phabricator.services.mozilla.com/D234885

--- toolkit/components/nimbus/test/unit/test_nimbusTelemetry.js
+++ toolkit/components/nimbus/test/unit/test_nimbusTelemetry.js
@@ -28,167 +28,186 @@ const SCHEMAS = {
   },
 };
 
-add_task(async function test_enrollAndUnenroll_gleanMetricConfiguration() {
-  const experiment = ExperimentFakes.recipe("experiment", {
-    bucketConfig: {
-      ...ExperimentFakes.recipe.bucketConfig,
-      count: 1000,
-    },
-    branches: [
-      {
-        slug: "control",
-        ratio: 1,
-        features: [
-          {
-            featureId: "nimbusTelemetry",
-            value: {
-              gleanMetricConfiguration: {
-                metrics_enabled: {
-                  "nimbus_targeting_environment.targeting_context_value": true,
+function nimbusTargetingContextTelemetryDisabled() {
+  return !Services.prefs.getBoolPref(
+    "nimbus.telemetry.targetingContextEnabled"
+  );
+}
+
+add_task(
+  { skip_if: nimbusTargetingContextTelemetryDisabled },
+  async function test_enrollAndUnenroll_gleanMetricConfiguration() {
+    const experiment = ExperimentFakes.recipe("experiment", {
+      bucketConfig: {
+        ...ExperimentFakes.recipe.bucketConfig,
+        count: 1000,
+      },
+      branches: [
+        {
+          slug: "control",
+          ratio: 1,
+          features: [
+            {
+              featureId: "nimbusTelemetry",
+              value: {
+                gleanMetricConfiguration: {
+                  metrics_enabled: {
+                    "nimbus_targeting_environment.targeting_context_value": true,
+                  },
                 },
               },
             },
-          },
-        ],
-      },
-    ],
-  });
+          ],
+        },
+      ],
+    });
 
-  const rollout = ExperimentFakes.recipe("rollout", {
-    bucketConfig: experiment.bucketConfig,
-    isRollout: true,
-    branches: [
-      {
-        slug: "control",
-        ratio: 1,
-        features: [
-          {
-            featureId: "nimbusTelemetry",
-            value: {
-              gleanMetricConfiguration: {
-                metrics_enabled: {
-                  "nimbus_events.enrollment_status": true,
+    const rollout = ExperimentFakes.recipe("rollout", {
+      bucketConfig: experiment.bucketConfig,
+      isRollout: true,
+      branches: [
+        {
+          slug: "control",
+          ratio: 1,
+          features: [
+            {
+              featureId: "nimbusTelemetry",
+              value: {
+                gleanMetricConfiguration: {
+                  metrics_enabled: {
+                    "nimbus_events.enrollment_status": true,
+                  },
                 },
               },
             },
-          },
-        ],
-      },
-    ],
-  });
+          ],
+        },
+      ],
+    });
 
-  const manager = ExperimentFakes.manager();
+    const manager = ExperimentFakes.manager();
 
-  sinon.stub(ExperimentAPI, "_manager").get(() => manager);
-  sinon.stub(ExperimentAPI, "_store").get(() => manager.store);
+    sinon.stub(ExperimentAPI, "_manager").get(() => manager);
+    sinon.stub(ExperimentAPI, "_store").get(() => manager.store);
 
-  await manager.onStartup();
-  await manager.store.ready();
+    await manager.onStartup();
+    await manager.store.ready();
 
-  Glean.nimbusTargetingEnvironment.targetingContextValue.set(
-    "nothing-active-0"
-  );
-  Glean.nimbusEvents.enrollmentStatus.record({ reason: "nothing-active-0" });
-
-  Assert.equal(
-    Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
-    null,
-    "targetingContextValue not recorded by default"
-  );
-  Assert.equal(
-    Glean.nimbusEvents.enrollmentStatus.testGetValue("events"),
-    null,
-    "enrollmentStatus not recorded by default"
-  );
-
-  await manager.enroll(rollout, "rs-loader");
-
-  Glean.nimbusTargetingEnvironment.targetingContextValue.set(
-    "rollout-active-1"
-  );
-  Glean.nimbusEvents.enrollmentStatus.record({ reason: "rollout-active-1" });
-
-  Assert.equal(
-    Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
-    null,
-    "targetingContextValue not recorded by default"
-  );
-
-  {
-    const events = Glean.nimbusEvents.enrollmentStatus.testGetValue("events");
-    Assert.equal(events?.length ?? 0, 1, "enrollmentStatus recorded once");
-    Assert.equal(
-      events[0].extra.reason,
-      "rollout-active-1",
-      "enrollmentStatus recorded once"
+    Glean.nimbusTargetingEnvironment.targetingContextValue.set(
+      "nothing-active-0"
     );
-  }
+    Glean.nimbusEvents.enrollmentStatus.record({ reason: "nothing-active-0" });
 
-  await manager.enroll(experiment, "rs-loader");
-
-  Glean.nimbusTargetingEnvironment.targetingContextValue.set(
-    "experiment-active-2"
-  );
-  Glean.nimbusEvents.enrollmentStatus.record({ reason: "experiment-active-2" });
-
-  Assert.equal(
-    Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
-    "experiment-active-2"
-  );
-
-  {
-    const events = Glean.nimbusEvents.enrollmentStatus.testGetValue("events");
-    Assert.equal(events?.length ?? 0, 1, "enrollmentStatus not recorded again");
-  }
-
-  await manager.unenroll("experiment");
-
-  Glean.nimbusTargetingEnvironment.targetingContextValue.set(
-    "rollout-active-3"
-  );
-  Glean.nimbusEvents.enrollmentStatus.record({ reason: "rollout-active-3" });
-
-  Assert.equal(
-    Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
-    "experiment-active-2",
-    "targetingContextValue was not recorded again"
-  );
-
-  {
-    const events = Glean.nimbusEvents.enrollmentStatus.testGetValue("events");
-    Assert.equal(events?.length ?? 0, 2, "enrollmentStatus recorded again");
     Assert.equal(
-      events[1].extra.reason,
-      "rollout-active-3",
-      "enrollmentStatus recorded with correct value"
+      Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
+      null,
+      "targetingContextValue not recorded by default"
     );
+    Assert.equal(
+      Glean.nimbusEvents.enrollmentStatus.testGetValue("events"),
+      null,
+      "enrollmentStatus not recorded by default"
+    );
+
+    await manager.enroll(rollout, "rs-loader");
+
+    Glean.nimbusTargetingEnvironment.targetingContextValue.set(
+      "rollout-active-1"
+    );
+    Glean.nimbusEvents.enrollmentStatus.record({ reason: "rollout-active-1" });
+
+    Assert.equal(
+      Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
+      null,
+      "targetingContextValue not recorded by default"
+    );
+
+    {
+      const events = Glean.nimbusEvents.enrollmentStatus.testGetValue("events");
+      Assert.equal(events?.length ?? 0, 1, "enrollmentStatus recorded once");
+      Assert.equal(
+        events[0].extra.reason,
+        "rollout-active-1",
+        "enrollmentStatus recorded once"
+      );
+    }
+
+    await manager.enroll(experiment, "rs-loader");
+
+    Glean.nimbusTargetingEnvironment.targetingContextValue.set(
+      "experiment-active-2"
+    );
+    Glean.nimbusEvents.enrollmentStatus.record({
+      reason: "experiment-active-2",
+    });
+
+    Assert.equal(
+      Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
+      "experiment-active-2"
+    );
+
+    {
+      const events = Glean.nimbusEvents.enrollmentStatus.testGetValue("events");
+      Assert.equal(
+        events?.length ?? 0,
+        1,
+        "enrollmentStatus not recorded again"
+      );
+    }
+
+    await manager.unenroll("experiment");
+
+    Glean.nimbusTargetingEnvironment.targetingContextValue.set(
+      "rollout-active-3"
+    );
+    Glean.nimbusEvents.enrollmentStatus.record({ reason: "rollout-active-3" });
+
+    Assert.equal(
+      Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
+      "experiment-active-2",
+      "targetingContextValue was not recorded again"
+    );
+
+    {
+      const events = Glean.nimbusEvents.enrollmentStatus.testGetValue("events");
+      Assert.equal(events?.length ?? 0, 2, "enrollmentStatus recorded again");
+      Assert.equal(
+        events[1].extra.reason,
+        "rollout-active-3",
+        "enrollmentStatus recorded with correct value"
+      );
+    }
+
+    await manager.unenroll("rollout");
+
+    Glean.nimbusTargetingEnvironment.targetingContextValue.set(
+      "nothing-active-0"
+    );
+    Glean.nimbusEvents.enrollmentStatus.record({ reason: "nothing-active-0" });
+
+    Assert.equal(
+      Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
+      "experiment-active-2",
+      "targetingContextValue was not recorded again"
+    );
+
+    {
+      const events = Glean.nimbusEvents.enrollmentStatus.testGetValue("events");
+      Assert.equal(
+        events?.length ?? 0,
+        2,
+        "enrollmentStatus not recorded again"
+      );
+    }
+
+    Services.fog.testResetFOG();
+
+    manager.store._deleteForTests("experiment");
+    manager.store._deleteForTests("rollout");
+
+    await assertEmptyStore(manager.store);
   }
-
-  await manager.unenroll("rollout");
-
-  Glean.nimbusTargetingEnvironment.targetingContextValue.set(
-    "nothing-active-0"
-  );
-  Glean.nimbusEvents.enrollmentStatus.record({ reason: "nothing-active-0" });
-
-  Assert.equal(
-    Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
-    "experiment-active-2",
-    "targetingContextValue was not recorded again"
-  );
-
-  {
-    const events = Glean.nimbusEvents.enrollmentStatus.testGetValue("events");
-    Assert.equal(events?.length ?? 0, 2, "enrollmentStatus not recorded again");
-  }
-
-  Services.fog.testResetFOG();
-
-  manager.store._deleteForTests("experiment");
-  manager.store._deleteForTests("rollout");
-
-  await assertEmptyStore(manager.store);
-});
+);
 
 add_task(async function test_featureConfigSchema_gleanMetricConfiguration() {
   function metricConfig(metric, value) {
@@ -259,69 +278,72 @@ add_task(async function test_featureConfigSchema_nimbusTargetingEnvironment() {
   );
 });
 
-add_task(async function test_nimbusTargetingEnvironment_recordAttrs() {
-  const sandbox = sinon.createSandbox();
-  const manager = ExperimentFakes.manager();
+add_task(
+  { skip_if: nimbusTargetingContextTelemetryDisabled },
+  async function test_nimbusTargetingEnvironment_recordAttrs() {
+    const sandbox = sinon.createSandbox();
+    const manager = ExperimentFakes.manager();
 
-  sandbox.stub(ExperimentAPI, "_manager").get(() => manager);
-  sandbox.stub(ExperimentAPI, "_store").get(() => manager.store);
+    sandbox.stub(ExperimentAPI, "_manager").get(() => manager);
+    sandbox.stub(ExperimentAPI, "_store").get(() => manager.store);
 
-  await manager.onStartup();
-  await manager.store.ready();
+    await manager.onStartup();
+    await manager.store.ready();
 
-  const cleanup = await ExperimentFakes.enrollWithFeatureConfig(
-    {
-      featureId: "nimbusTelemetry",
-      value: {
-        gleanMetricConfiguration: {
-          metrics_enabled: {
-            "nimbus_targeting_environment.targeting_context_value": true,
+    const cleanup = await ExperimentFakes.enrollWithFeatureConfig(
+      {
+        featureId: "nimbusTelemetry",
+        value: {
+          gleanMetricConfiguration: {
+            metrics_enabled: {
+              "nimbus_targeting_environment.targeting_context_value": true,
+            },
           },
-        },
-        nimbusTargetingEnvironment: {
-          recordAttrs: [
-            "activeExperiments",
-            "activeRollouts",
-            "enrollmentsMap",
-          ],
+          nimbusTargetingEnvironment: {
+            recordAttrs: [
+              "activeExperiments",
+              "activeRollouts",
+              "enrollmentsMap",
+            ],
+          },
         },
       },
-    },
-    {
-      manager,
-      source: "test",
-      slug: "config",
-    }
-  );
-
-  Assert.equal(
-    Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
-    null,
-    "The targetingContextValue metric has not been recorded yet."
-  );
-
-  GleanPings.nimbusTargetingContext.testBeforeNextSubmit(() => {
-    Assert.deepEqual(
-      JSON.parse(
-        Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue()
-      ),
       {
-        activeExperiments: ["config"],
-        activeRollouts: [],
-        enrollmentsMap: [
-          {
-            experimentSlug: "config",
-            branchSlug: "control",
-          },
-        ],
+        manager,
+        source: "test",
+        slug: "config",
       }
     );
-  });
-  await recordTargetingContext();
 
-  await cleanup();
-  await assertEmptyStore(manager.store);
-  await sandbox.restore();
+    Assert.equal(
+      Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue(),
+      null,
+      "The targetingContextValue metric has not been recorded yet."
+    );
 
-  Services.fog.testResetFOG();
-});
+    GleanPings.nimbusTargetingContext.testBeforeNextSubmit(() => {
+      Assert.deepEqual(
+        JSON.parse(
+          Glean.nimbusTargetingEnvironment.targetingContextValue.testGetValue()
+        ),
+        {
+          activeExperiments: ["config"],
+          activeRollouts: [],
+          enrollmentsMap: [
+            {
+              experimentSlug: "config",
+              branchSlug: "control",
+            },
+          ],
+        }
+      );
+    });
+    await recordTargetingContext();
+
+    await cleanup();
+    await assertEmptyStore(manager.store);
+    await sandbox.restore();
+
+    Services.fog.testResetFOG();
+  }
+);
