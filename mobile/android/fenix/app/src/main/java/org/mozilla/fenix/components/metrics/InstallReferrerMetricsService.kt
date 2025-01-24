2afc36d9ae9c06a33e32732dda3c4463fd1c8232
Roger Yang <royang@mozilla.com>
2025-01-24 20:57:42 +0000

    Revert "Bug 1940774 - Wait till Adjust attribution data before triggering first session ping."  a=RyanVM
    This reverts commit 2a0812488e898e302455c3bb01e1e3f976e5707b.
    
    Differential Revision: https://phabricator.services.mozilla.com/D235424

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/metrics/InstallReferrerMetricsService.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/metrics/InstallReferrerMetricsService.kt
@@ -45,6 +45,7 @@ class InstallReferrerMetricsService(private val context: Context) : MetricsServi
             object : InstallReferrerStateListener {
                 override fun onInstallReferrerSetupFinished(responseCode: Int) {
                     PlayStoreAttribution.attributionTime.stopAndAccumulate(timerId)
+                    val firstSession = FirstSessionPing(context, context.components.core.store)
                     when (responseCode) {
                         InstallReferrerClient.InstallReferrerResponse.OK -> {
                             // Connection established.
@@ -73,6 +74,8 @@ class InstallReferrerMetricsService(private val context: Context) : MetricsServi
 
                             utmParams.recordInstallReferrer(context.settings())
                             context.settings().utmParamsKnown = true
+
+                            firstSession.checkAndSend()
                         }
 
                         InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED,
@@ -81,6 +84,7 @@ class InstallReferrerMetricsService(private val context: Context) : MetricsServi
                         -> {
                             // unrecoverable errors, but we still want to send the first-session ping.
                             context.settings().utmParamsKnown = true
+                            firstSession.checkAndSend()
                         }
 
                         InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE -> {
