ddbe2dcc11864bf7079854658fba98fecf278d46
aglavic <aglavic@mozilla.com>
2025-01-24 01:50:57 +0000

    Bug 1942922 - Enable power measurements for all a55 speedometer3 tests on CI. r=perftest-reviewers,sparky
    Differential Revision: https://phabricator.services.mozilla.com/D235071

--- taskcluster/gecko_taskgraph/transforms/test/raptor.py
+++ taskcluster/gecko_taskgraph/transforms/test/raptor.py
@@ -361,10 +361,15 @@ def add_extra_options(config, tests):
                     "--test-url-params={}".format(param.replace(" ", ""))
                 )
 
-        if "android-hw-p6" in test_platform or "android-hw-s24" in test_platform:
-            if "--power-test" not in extra_options:
-                extra_options.append("--power-test")
-        elif "android-hw-a55" in test_platform and "tp6" in test["test-name"]:
+        if (
+            "android-hw-p6" in test_platform
+            or "android-hw-s24" in test_platform
+            or (
+                "android-hw-a55" in test_platform
+                and any(t in test["test-name"] for t in ("tp6", "speedometer3"))
+                # Bug 1919024 remove tp6 and sp3 restrictions once benchmark parsing is done in the support scripts
+            )
+        ):
             if "--power-test" not in extra_options:
                 extra_options.append("--power-test")
         elif "windows" in test_platform and any(
