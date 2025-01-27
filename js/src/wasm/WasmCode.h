595760dd5159863c3d8766b3020aa8f221890c03
Julian Seward <jseward@acm.org>
2025-01-27 15:15:41 +0000

    Bug 1938968 - use atomic store in wasm::JumpTables::setJitEntry.  r=rhunt.
    JumpTables::setJitEntry doesn't use an atomic store to write the `jit_` array,
    and so TSan reports a race against ::setJitEntryIfNull, which does use suitable
    atomic primitives.  The resulting race (if any) is harmless since we are not
    relying on the synchronisation here anyways, but the TSan complaint blocks
    fuzzing.  This patch fixes ::setJitEntry.
    
    Differential Revision: https://phabricator.services.mozilla.com/D235305

--- js/src/wasm/WasmCode.h
+++ js/src/wasm/WasmCode.h
@@ -895,7 +895,7 @@ class JumpTables {
     // Make sure that write is atomic; see comment in wasm::Module::finishTier2
     // to that effect.
     MOZ_ASSERT(i < numFuncs_);
-    jit_.get()[i] = target;
+    __atomic_store_n(&jit_.get()[i], target, __ATOMIC_RELAXED);
   }
   void setJitEntryIfNull(size_t i, void* target) const {
     // Make sure that compare-and-write is atomic; see comment in
@@ -903,8 +903,9 @@ class JumpTables {
     MOZ_ASSERT(i < numFuncs_);
     void* expected = nullptr;
     (void)__atomic_compare_exchange_n(&jit_.get()[i], &expected, target,
-                                      /*weak=*/false, __ATOMIC_RELAXED,
-                                      __ATOMIC_RELAXED);
+                                      /*weak=*/false,
+                                      /*success_memorder=*/__ATOMIC_RELAXED,
+                                      /*failure_memorder=*/__ATOMIC_RELAXED);
   }
   void** getAddressOfJitEntry(size_t i) const {
     MOZ_ASSERT(i < numFuncs_);
