59a420ad89ad49346e282dbac477d9114d656e40
André Bargull <andre.bargull@gmail.com>
2025-01-28 10:55:56 +0000

    Bug 1944011 - Part 10: Remove unused LUnaryMath. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D235655

--- js/src/jit/shared/LIR-shared.h
+++ js/src/jit/shared/LIR-shared.h
@@ -45,16 +45,6 @@ class LBinaryMath : public LInstructionHelper<1, 2 + ExtraUses, Temps> {
   const LAllocation* rhs() { return this->getOperand(1); }
 };
 
-template <size_t Temps, size_t ExtraUses = 0>
-class LUnaryMath : public LInstructionHelper<1, 1 + ExtraUses, Temps> {
- protected:
-  explicit LUnaryMath(LNode::Opcode opcode)
-      : LInstructionHelper<1, 1 + ExtraUses, Temps>(opcode) {}
-
- public:
-  const LAllocation* input() { return this->getOperand(0); }
-};
-
 // An LOsiPoint captures a snapshot after a call and ensures enough space to
 // patch in a call to the invalidation mechanism.
 //
