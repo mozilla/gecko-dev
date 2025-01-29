7deac3db48a7544a8227ed890bb4e636a25a250f
Julian Descottes <jdescottes@mozilla.com>
2025-01-29 17:29:05 +0000

    Bug 1944361 - [devtools] Add test for netmonitor waterfall column resize r=devtools-reviewers,ochameau
    Depends on D235838
    
    Differential Revision: https://phabricator.services.mozilla.com/D235852

--- devtools/client/netmonitor/test/head.js
+++ devtools/client/netmonitor/test/head.js
@@ -1603,3 +1603,93 @@ async function selectThrottle(monitor, profileId) {
   info(`Waiting for the '${profileId}' profile to be applied`);
   await monitor.panelWin.api.once(TEST_EVENTS.THROTTLING_CHANGED);
 }
+
+/**
+ * Resize a netmonitor column.
+ *
+ * @param {Element} columnHeader
+ * @param {number} newPercent
+ * @param {number} parentWidth
+ * @param {string} dir
+ */
+function resizeColumn(columnHeader, newPercent, parentWidth, dir = "ltr") {
+  const newWidthInPixels = (newPercent * parentWidth) / 100;
+  const win = columnHeader.ownerDocument.defaultView;
+  const currentWidth = columnHeader.getBoundingClientRect().width;
+  const mouseDown = dir === "rtl" ? 0 : currentWidth;
+  const mouseMove =
+    dir === "rtl" ? currentWidth - newWidthInPixels : newWidthInPixels;
+
+  EventUtils.synthesizeMouse(
+    columnHeader,
+    mouseDown,
+    1,
+    { type: "mousedown" },
+    win
+  );
+  EventUtils.synthesizeMouse(
+    columnHeader,
+    mouseMove,
+    1,
+    { type: "mousemove" },
+    win
+  );
+  EventUtils.synthesizeMouse(
+    columnHeader,
+    mouseMove,
+    1,
+    { type: "mouseup" },
+    win
+  );
+}
+
+/**
+ * Resize the waterfall netmonitor column.
+ * Uses slightly different logic than for the other columns.
+ *
+ * @param {Element} columnHeader
+ * @param {number} newPercent
+ * @param {number} parentWidth
+ * @param {string} dir
+ */
+function resizeWaterfallColumn(
+  columnHeader,
+  newPercent,
+  parentWidth,
+  dir = "ltr"
+) {
+  const newWidthInPixels = (newPercent * parentWidth) / 100;
+  const win = columnHeader.ownerDocument.defaultView;
+  const mouseDown =
+    dir === "rtl"
+      ? columnHeader.getBoundingClientRect().right
+      : columnHeader.getBoundingClientRect().left;
+  const mouseMove =
+    dir === "rtl"
+      ? mouseDown +
+        (newWidthInPixels - columnHeader.getBoundingClientRect().width)
+      : mouseDown +
+        (columnHeader.getBoundingClientRect().width - newWidthInPixels);
+
+  EventUtils.synthesizeMouse(
+    columnHeader.parentElement,
+    mouseDown,
+    1,
+    { type: "mousedown" },
+    win
+  );
+  EventUtils.synthesizeMouse(
+    columnHeader.parentElement,
+    mouseMove,
+    1,
+    { type: "mousemove" },
+    win
+  );
+  EventUtils.synthesizeMouse(
+    columnHeader.parentElement,
+    mouseMove,
+    1,
+    { type: "mouseup" },
+    win
+  );
+}
