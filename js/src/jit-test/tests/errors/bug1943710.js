911af295dc9a5d11f6c7070f7c9b3feb1278ce43
Matthew Gaudet <mgaudet@mozilla.com>
2025-01-29 18:30:32 +0000

    Bug 1944517 - Guard against missing Error.captureStackTrace r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D236007

--- js/src/jit-test/tests/errors/bug1943710.js
+++ js/src/jit-test/tests/errors/bug1943710.js
@@ -1,9 +1,10 @@
 // |jit-test| --ion-offthread-compile=off; --setpref=experimental.error_capture_stack_trace
-
-run(`
-  a16 = {};
-  Error.captureStackTrace(a16, Error)
-`);
-function run(code) {
+if ('captureStackTrace' in Error) {
+  run(`
+    a16 = {};
+    Error.captureStackTrace(a16, Error)
+  `);
+  function run(code) {
     evaluate(code)
+  }
 }
