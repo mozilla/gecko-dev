78d181426be628d3cab7c24a512641413a2e58ba
pollymce <pmceldowney@mozilla.com>
2025-01-29 17:43:09 +0000

    Bug 1923902: set the values in the focus daily usage ping. r=android-reviewers,twhite
    Set values for duration, os, os version, app display version and app channel.
    Also pulled out the glean specific calls behind an interface for easier unit testing of the lifecycle observer actions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D235287

---
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/telemetry/GleanUsageReporting.kt
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.telemetry
+
+import android.os.Build
+import org.mozilla.focus.BuildConfig
+import org.mozilla.focus.GleanMetrics.Pings
+import org.mozilla.focus.GleanMetrics.Usage
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+
+/**
+ * Makes the glean calls we need to manage the usage reporting ping.
+ */
+class GleanUsageReporting : GleanUsageReportingApi {
+
+    override fun setDuration(durationMillis: Long) {
+        val duration = durationMillis.toDuration(DurationUnit.MILLISECONDS)
+        Usage.duration.setRawNanos(duration.inWholeNanoseconds)
+    }
+
+    override fun setUsageReason(usageReason: GleanUsageReportingApi.UsageReason) {
+        Usage.reason.set(usageReason.name.lowercase())
+    }
+
+    override fun submitPing() {
+        setUsageConstantValues()
+        Pings.usageReporting.submit()
+    }
+
+    private fun setUsageConstantValues() {
+        Usage.os.set("android")
+        Usage.osVersion.set(Build.VERSION.RELEASE)
+        Usage.appDisplayVersion.set(BuildConfig.VERSION_NAME)
+        Usage.appChannel.set(BuildConfig.BUILD_TYPE)
+    }
+}
