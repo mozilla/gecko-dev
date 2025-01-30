00f3a5b97bd811e1495c4fa9250bd5246eeef436
Alex Jakobi <ajakobi@mozilla.com>
2025-01-30 18:44:37 +0000

    Bug 1841896 - Port helper_fission_initial_displayport to regular mochitests r=hiro
    Differential Revision: https://phabricator.services.mozilla.com/D231277

--- gfx/layers/apz/test/mochitest/apz_test_utils.js
+++ gfx/layers/apz/test/mochitest/apz_test_utils.js
@@ -1266,23 +1266,27 @@ function compareVisualViewport(
 }
 
 // Loads a URL in an iframe and waits until APZ is stable
-async function setupIframe(aIFrame, aURL) {
+async function setupIframe(aIFrame, aURL, aIsOffScreen = false) {
   const iframeLoadPromise = promiseOneEvent(aIFrame, "load", null);
   aIFrame.src = aURL;
   await iframeLoadPromise;
 
-  await SpecialPowers.spawn(aIFrame, [], async () => {
-    await content.wrappedJSObject.waitUntilApzStable();
-  });
+  if (!aIsOffScreen) {
+    await SpecialPowers.spawn(aIFrame, [], async () => {
+      await content.wrappedJSObject.waitUntilApzStable();
+    });
+  }
 }
 
 // Loads a URL in an iframe and replaces its origin to
 // create an out-of-process iframe
-async function setupCrossOriginIFrame(aIFrame, aUrl) {
+async function setupCrossOriginIFrame(aIFrame, aUrl, aIsOffScreen = false) {
   let iframeURL = SimpleTest.getTestFileURL(aUrl);
   iframeURL = iframeURL.replace(window.location.origin, "https://example.com");
-  await setupIframe(aIFrame, iframeURL);
-  await SpecialPowers.spawn(aIFrame, [], async () => {
-    await SpecialPowers.contentTransformsReceived(content);
-  });
+  await setupIframe(aIFrame, iframeURL, aIsOffScreen);
+  if (!aIsOffScreen) {
+    await SpecialPowers.spawn(aIFrame, [], async () => {
+      await SpecialPowers.contentTransformsReceived(content);
+    });
+  }
 }
