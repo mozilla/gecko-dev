ade9ed0fa648a61c1dcd22ab4b17a273c68af2d2
sotaro <sotaro.ikeda.g@gmail.com>
2025-01-30 05:02:16 +0000

    Bug 1944716 - Fix taking snapshot of WebGPU when no readback is enabled r=gfx-reviewers,lsalzman
    Fix simple mistake of Bug 1940374 fix.
    
    Differential Revision: https://phabricator.services.mozilla.com/D236074

--- dom/webgpu/ipc/WebGPUParent.cpp
+++ dom/webgpu/ipc/WebGPUParent.cpp
@@ -1269,7 +1269,7 @@ ipc::IPCResult WebGPUParent::GetFrontBufferSnapshot(
   }
 
   // External texture is already invalid and posted to RemoteTextureMap
-  if (it != mExternalTextures.end()) {
+  if (it == mExternalTextures.end()) {
     if (!mRemoteTextureOwner || !mRemoteTextureOwner->IsRegistered(aOwnerId)) {
       MOZ_ASSERT_UNREACHABLE("unexpected to be called");
       return IPC_OK();
