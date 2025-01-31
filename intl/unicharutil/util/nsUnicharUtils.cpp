68e94b67987af7d69d3fda69a77702d13807ce9f
Jonathan Kew <jkew@mozilla.com>
2025-01-31 12:26:07 +0000

    Bug 1940947 - Exclude Korean currency symbol (WON SIGN) from being treated like Chinese/Japanese characters during CSS segment break transformation. r=dshin
    Differential Revision: https://phabricator.services.mozilla.com/D235669

--- intl/unicharutil/util/nsUnicharUtils.cpp
+++ intl/unicharutil/util/nsUnicharUtils.cpp
@@ -515,9 +515,16 @@ uint32_t HashUTF8AsUTF16(const char* aUTF8, size_t aLength, bool* aErr) {
   return hash;
 }
 
+// The Korean Won currency sign has East Asian Width = HALFWIDTH, and
+// Script = COMMON (rather than HANGUL), but we don't want to treat it like
+// Chinese/Japanese half-width characters for segment break transformation,
+// so we exclude it individually in the two functions here.
+static constexpr uint32_t kWonCurrencySign = 0x20A9;
+
 bool IsSegmentBreakSkipChar(uint32_t u) {
   return intl::UnicodeProperties::IsEastAsianWidthFHWexcludingEmoji(u) &&
-         intl::UnicodeProperties::GetScriptCode(u) != intl::Script::HANGUL;
+         intl::UnicodeProperties::GetScriptCode(u) != intl::Script::HANGUL &&
+         u != kWonCurrencySign;
 }
 
 bool IsEastAsianPunctuation(uint32_t u) {
@@ -527,7 +534,9 @@ bool IsEastAsianPunctuation(uint32_t u) {
   // two characters consistent behavior.
   constexpr uint32_t kFullwidthTilde = 0xFF5E;
   return intl::UnicodeProperties::IsEastAsianWidthFHW(u) &&
-         (intl::UnicodeProperties::IsPunctuation(u) || u == kFullwidthTilde);
+         ((intl::UnicodeProperties::IsPunctuation(u) &&
+           u != kWonCurrencySign) ||
+          u == kFullwidthTilde);
 }
 
 bool IsPunctuationForWordSelect(char16_t aCh) {
