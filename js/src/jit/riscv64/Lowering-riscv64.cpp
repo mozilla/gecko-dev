4c34be5cd351e761f3d8667cf1eb3ff8b252d8da
André Bargull <andre.bargull@gmail.com>
2025-01-31 18:52:58 +0000

    Bug 1944011 - Part 83: Fix register allocation for LAtomicTypedArrayElementBinop64 on RISCV. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D236204

--- js/src/jit/riscv64/Lowering-riscv64.cpp
+++ js/src/jit/riscv64/Lowering-riscv64.cpp
@@ -669,9 +669,9 @@ void LIRGenerator::visitAtomicTypedArrayElementBinop(
   const LUse elements = useRegister(ins->elements());
   const LAllocation index =
       useRegisterOrIndexConstant(ins->index(), ins->arrayType());
-  const LAllocation value = useRegister(ins->value());
 
   if (Scalar::isBigIntType(ins->arrayType())) {
+    LInt64Allocation value = useInt64Register(ins->value());
     LInt64Definition temp = tempInt64();
 
     // Case 1: the result of the operation is not used.
@@ -690,6 +690,8 @@ void LIRGenerator::visitAtomicTypedArrayElementBinop(
     defineInt64(lir, ins);
     return;
   }
+
+  const LAllocation value = useRegister(ins->value());
 
   LDefinition valueTemp = LDefinition::BogusTemp();
   LDefinition offsetTemp = LDefinition::BogusTemp();
