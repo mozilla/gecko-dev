725fc3179ce123a9ea4896ebf8299b841fff5b22
Julian Descottes <jdescottes@mozilla.com>
2025-02-03 17:20:36 +0000

    Bug 1767711 - [devtools] Extract failing subtest from browser_dbg-preview.js r=devtools-reviewers,ochameau
    At the moment the test fails because of testHoveringInvalidTargetTokens
    Extract this to a different test file and re-enable the main test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D236158

---
+++ devtools/client/debugger/test/mochitest/browser_dbg-preview-invalid-tokens.js
@@ -0,0 +1,123 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// Test hovering on an object, which will show a popup and on a
+// simple value, which will show a tooltip.
+
+"use strict";
+
+add_task(async function () {
+  const dbg = await initDebugger("doc-preview.html", "preview.js");
+
+  await selectSource(dbg, "preview.js");
+
+  // Test hovering tokens for which we shouldn't have a preview popup displayed
+  invokeInTab("invalidTargets");
+  await waitForPaused(dbg);
+  // CodeMirror refreshes after inline previews are displayed, so wait until they're rendered.
+  await waitForInlinePreviews(dbg);
+
+  await assertNoPreviews(dbg, `"a"`, 69, 4);
+  await assertNoPreviews(dbg, `false`, 70, 4);
+  await assertNoPreviews(dbg, `undefined`, 71, 4);
+  await assertNoPreviews(dbg, `null`, 72, 4);
+  await assertNoPreviews(dbg, `42`, 73, 4);
+  await assertNoPreviews(dbg, `const`, 74, 4);
+
+  // checking inline preview widget
+  // Move the cursor to the top left corner to have a clean state
+  resetCursorPositionToTopLeftCorner(dbg);
+
+  // Wait for all the updates to the document to complete to make all
+  // token elements have been rendered
+  await waitForDocumentLoadComplete(dbg);
+
+  const inlinePreviewEl = findElement(dbg, "inlinePreview");
+  is(inlinePreviewEl.innerText, `myVar:"foo"`, "got expected inline preview");
+
+  const racePromise = Promise.any([
+    waitForElement(dbg, "previewPopup"),
+    wait(500).then(() => "TIMEOUT"),
+  ]);
+  // Hover over the inline preview element
+  hoverToken(inlinePreviewEl);
+  const raceResult = await racePromise;
+  is(raceResult, "TIMEOUT", "No popup was displayed over the inline preview");
+
+  await resume(dbg);
+
+  info("Test hovering element not in a line");
+  await getDebuggerSplitConsole(dbg);
+  const { hud } = dbg.toolbox.getPanel("webconsole");
+  evaluateExpressionInConsole(
+    hud,
+    `
+      a = 1;
+      debugger;
+      b = 2;`
+  );
+  await waitForPaused(dbg);
+  await dbg.toolbox.toggleSplitConsole();
+
+  resetCursorPositionToTopLeftCorner(dbg);
+
+  const racePromiseLines = Promise.any([
+    waitForElement(dbg, "previewPopup"),
+    wait(500).then(() => "TIMEOUT_LINES"),
+  ]);
+  // We don't want to use hoverToken, as it synthesize the event at the center of the element,
+  // which wouldn't reproduce the original issue we want to check
+  EventUtils.synthesizeMouse(
+    findElement(dbg, "CodeMirrorLines"),
+    0,
+    0,
+    {
+      type: "mousemove",
+    },
+    dbg.win
+  );
+  is(
+    await racePromiseLines,
+    "TIMEOUT_LINES",
+    "No popup was displayed over the content container element"
+  );
+
+  // Trigger a preview popup on an element which actually will show a popup
+  // to avoid test document leaks linked to the earlier mousemove events which
+  // did not trigger any popup.
+  const aTokenEl = await getTokenElAtLine(dbg, "a", 2, 8);
+  await tryHoverToken(dbg, aTokenEl, "previewPopup");
+
+  // Resume
+  await resume(dbg);
+  await selectSource(dbg, "preview.js");
+});
+
+async function assertNoPreviews(dbg, expression, line, column) {
+  // Move the cursor to the top left corner to have a clean state
+  resetCursorPositionToTopLeftCorner(dbg);
+
+  // Wait for all the updates to the document to complete to make all
+  // token elements have been rendered
+  await waitForDocumentLoadComplete(dbg);
+
+  // Hover the token
+  const result = await Promise.race([
+    tryHoverTokenAtLine(dbg, expression, line, column, "previewPopup"),
+    wait(500).then(() => "TIMEOUT"),
+  ]);
+  is(result, "TIMEOUT", `No popup was displayed when hovering "${expression}"`);
+}
+
+function resetCursorPositionToTopLeftCorner(dbg) {
+  EventUtils.synthesizeMouse(
+    findElement(dbg, "codeMirror"),
+    0,
+    0,
+    {
+      type: "mousemove",
+    },
+    dbg.win
+  );
+}
--- devtools/client/debugger/test/mochitest/browser_dbg-preview.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-preview.js
@@ -138,8 +138,6 @@ add_task(async function () {
   }
 
   await selectSource(dbg, "preview.js");
-  await testHoveringInvalidTargetTokens(dbg);
-
   info(
     "Check that closing the preview tooltip doesn't release the underlying object actor"
   );
@@ -185,111 +183,6 @@ async function testPreviews(dbg, fnName, previews) {
   await resume(dbg);
 
   info(`Ran tests for ${fnName}`);
-}
-
-async function testHoveringInvalidTargetTokens(dbg) {
-  // Test hovering tokens for which we shouldn't have a preview popup displayed
-  invokeInTab("invalidTargets");
-  await waitForPaused(dbg);
-  // CodeMirror refreshes after inline previews are displayed, so wait until they're rendered.
-  await waitForInlinePreviews(dbg);
-
-  await assertNoPreviews(dbg, `"a"`, 69, 4);
-  await assertNoPreviews(dbg, `false`, 70, 4);
-  await assertNoPreviews(dbg, `undefined`, 71, 4);
-  await assertNoPreviews(dbg, `null`, 72, 4);
-  await assertNoPreviews(dbg, `42`, 73, 4);
-  await assertNoPreviews(dbg, `const`, 74, 4);
-
-  // checking inline preview widget
-  // Move the cursor to the top left corner to have a clean state
-  resetCursorPositionToTopLeftCorner(dbg);
-
-  // Wait for all the updates to the document to complete to make all
-  // token elements have been rendered
-  await waitForDocumentLoadComplete(dbg);
-
-  const inlinePreviewEl = findElement(dbg, "inlinePreview");
-  is(inlinePreviewEl.innerText, `myVar:"foo"`, "got expected inline preview");
-
-  const racePromise = Promise.any([
-    waitForElement(dbg, "previewPopup"),
-    wait(500).then(() => "TIMEOUT"),
-  ]);
-  // Hover over the inline preview element
-  hoverToken(inlinePreviewEl);
-  const raceResult = await racePromise;
-  is(raceResult, "TIMEOUT", "No popup was displayed over the inline preview");
-
-  await resume(dbg);
-
-  info("Test hovering element not in a line");
-  await getDebuggerSplitConsole(dbg);
-  const { hud } = dbg.toolbox.getPanel("webconsole");
-  evaluateExpressionInConsole(
-    hud,
-    `
-      a = 1;
-      debugger;
-      b = 2;`
-  );
-  await waitForPaused(dbg);
-  await dbg.toolbox.toggleSplitConsole();
-
-  resetCursorPositionToTopLeftCorner(dbg);
-
-  const racePromiseLines = Promise.any([
-    waitForElement(dbg, "previewPopup"),
-    wait(500).then(() => "TIMEOUT_LINES"),
-  ]);
-  // We don't want to use hoverToken, as it synthesize the event at the center of the element,
-  // which wouldn't reproduce the original issue we want to check
-  EventUtils.synthesizeMouse(
-    findElement(dbg, "CodeMirrorLines"),
-    0,
-    0,
-    {
-      type: "mousemove",
-    },
-    dbg.win
-  );
-  is(
-    await racePromiseLines,
-    "TIMEOUT_LINES",
-    "No popup was displayed over the content container element"
-  );
-
-  // Resume and select back the main JS file that is used by the other assertions
-  await resume(dbg);
-  await selectSource(dbg, "preview.js");
-}
-
-async function assertNoPreviews(dbg, expression, line, column) {
-  // Move the cursor to the top left corner to have a clean state
-  resetCursorPositionToTopLeftCorner(dbg);
-
-  // Wait for all the updates to the document to complete to make all
-  // token elements have been rendered
-  await waitForDocumentLoadComplete(dbg);
-
-  // Hover the token
-  const result = await Promise.race([
-    tryHoverTokenAtLine(dbg, expression, line, column, "previewPopup"),
-    wait(500).then(() => "TIMEOUT"),
-  ]);
-  is(result, "TIMEOUT", `No popup was displayed when hovering "${expression}"`);
-}
-
-function resetCursorPositionToTopLeftCorner(dbg) {
-  EventUtils.synthesizeMouse(
-    findElement(dbg, "codeMirror"),
-    0,
-    0,
-    {
-      type: "mousemove",
-    },
-    dbg.win
-  );
 }
 
 async function testMovingFromATokenToAnother(dbg) {
