082c7e2eca6df8a27cce127e7bb755a52a3dee92
James Teh <jteh@mozilla.com>
2025-02-03 23:29:31 +0000

    Bug 1938768: Include DocAccessible-inl.h in CssAltContent.cpp. r=nlapre
    CssAltContent uses FireDelayedEvent, which is defined in DocAccessible-inl.h.
    
    Differential Revision: https://phabricator.services.mozilla.com/D236492

--- accessible/base/CssAltContent.cpp
+++ accessible/base/CssAltContent.cpp
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "CssAltContent.h"
-#include "mozilla/a11y/DocAccessible.h"
+#include "DocAccessible-inl.h"
 #include "mozilla/a11y/DocManager.h"
 #include "mozilla/dom/Document.h"
 #include "mozilla/dom/Element.h"
