6bb121e4f080f11bf2d161f6eeef3290d1ae0ede
Rika <tjj.rikap@gmail.com>
2025-02-04 10:59:18 +0000

    Bug 1943846 - Fix nsFind::IsSpace to handle UTF-32 characters correctly. r=emilio
    The original IsSpace macro uses nsCRT::IsAsciiSpace which doesn't handle
    UTF-32 characters (characters above U+FFFF) correctly. Add a check for UTF-32
    characters to ensure correct matching behavior. Also add a test case to
    test_nsFind.html to verify the fix for Bug 1943846.
    
    Differential Revision: https://phabricator.services.mozilla.com/D236455

--- toolkit/components/find/nsFind.cpp
+++ toolkit/components/find/nsFind.cpp
@@ -574,7 +574,16 @@ char32_t nsFind::PeekNextChar(State& aState, bool aAlreadyMatching) const {
 }
 
 #define NBSP_CHARCODE (CHAR_TO_UNICHAR(160))
-#define IsSpace(c) (nsCRT::IsAsciiSpace(c) || (c) == NBSP_CHARCODE)
+// Bug 1943846: The char16_t version of nsCRT::IsAsciiSpace is not working as
+// expected for characters with UTF32 > 0xFFFF. Add a check for UTF32 characters
+
+static bool IsSpace(char16_t aChar) {
+  return nsCRT::IsAsciiSpace(aChar) || aChar == NBSP_CHARCODE;
+}
+static bool IsSpace(char32_t aChar) {
+  return aChar <= 0xffff && IsSpace(char16_t(aChar));
+}
+
 #define OVERFLOW_PINDEX (mFindBackward ? pindex < 0 : pindex > patLen)
 #define DONE_WITH_PINDEX (mFindBackward ? pindex <= 0 : pindex >= patLen)
 
