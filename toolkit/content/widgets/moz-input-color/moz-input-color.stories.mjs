3ff50534d9fbe0dd2a3f474553102725e27095b6
mark <mkennedy@mozilla.com>
2025-02-05 17:10:47 +0000

    Bug 1937639 - Migrate color-input element to moz-color-input reusable component r=reusable-components-reviewers,desktop-theme-reviewers,hjones
    Differential Revision: https://phabricator.services.mozilla.com/D232402

--- toolkit/content/widgets/moz-input-color/moz-input-color.stories.mjs
+++ toolkit/content/widgets/moz-input-color/moz-input-color.stories.mjs
@@ -2,30 +2,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { html } from "../../content/widgets/vendor/lit.all.mjs";
-import "chrome://global/content/reader/color-input.mjs";
+import { html } from "../vendor/lit.all.mjs";
+import "./moz-input-color.mjs";
 
 export default {
-  title: "Domain-specific UI Widgets/Reader View/Color Input",
-  component: "color-input",
+  title: "UI Widgets/Color Input",
+  component: "moz-input-color",
   argTypes: {},
   parameters: {
     status: "stable",
-    fluent: `moz-color-input-label = Background`,
+    fluent: `moz-input-color-label = Background`,
   },
 };
 
 const Template = ({ color, propName, labelL10nId }) => html`
-  <color-input
+  <moz-input-color
     color=${color}
     data-l10n-id=${labelL10nId}
     prop-name=${propName}
-  ></color-input>
+  ></moz-input-color>
 `;
 
 export const Default = Template.bind({});
 Default.args = {
   propName: "background",
   color: "#7293C9",
-  labelL10nId: "moz-color-input-label",
+  labelL10nId: "moz-input-color-label",
 };
