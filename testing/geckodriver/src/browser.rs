b9c1caf81567ff3ed5fa01a7bd275802622a6589
Henrik Skupin <mail@hskupin.info>
2025-02-05 16:34:23 +0000

    Bug 1359472 - [geckodriver] Add "--allow-system-access" argument to allow testing in "chrome" context. r=webdriver-reviewers,Sasha,jgraham
    Adds a command line argument that gets forwarded to Firefox
    and will control in future versions of Firefox when access
    to the browser's parent process for eg. browser ui testing
    is allowed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D236176

--- testing/geckodriver/src/browser.rs
+++ testing/geckodriver/src/browser.rs
@@ -107,6 +107,7 @@ impl LocalBrowser {
         options: FirefoxOptions,
         marionette_port: u16,
         jsdebugger: bool,
+        system_access: bool,
         profile_root: Option<&Path>,
     ) -> WebDriverResult<LocalBrowser> {
         let binary = options.binary.ok_or_else(|| {
@@ -153,6 +154,9 @@ impl LocalBrowser {
         runner.arg("--marionette");
         if jsdebugger {
             runner.arg("--jsdebugger");
+        }
+        if system_access {
+            runner.arg("--remote-allow-system-access");
         }
         if let Some(args) = options.args.as_ref() {
             runner.args(args);
@@ -274,11 +278,12 @@ impl RemoteBrowser {
         options: FirefoxOptions,
         marionette_port: u16,
         websocket_port: Option<u16>,
+        system_access: bool,
         profile_root: Option<&Path>,
     ) -> WebDriverResult<RemoteBrowser> {
         let android_options = options.android.unwrap();
 
-        let handler = AndroidHandler::new(&android_options, marionette_port, websocket_port)?;
+        let handler = AndroidHandler::new(&android_options, marionette_port, system_access, websocket_port)?;
 
         // Profile management.
         let (mut profile, is_custom_profile) = match options.profile {
