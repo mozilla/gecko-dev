5c3dd2a1bca7fb2696136c25417776a871ac814c
Thomas Wisniewski <twisniewski@mozilla.com>
2025-02-06 20:56:10 +0000

    Bug 1937817 - remove intervention for unimedlitoral.com.br; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D237052

--- testing/webcompat/interventions/tests/test_1903970_www_unimedlitoral_com_br.py
+++ testing/webcompat/interventions/tests/test_1903970_www_unimedlitoral_com_br.py
@@ -1,21 +1,21 @@
 import pytest
 
 URL = "https://www.unimedlitoral.com.br/agendaonline/index.php?c=Authentication"
-BLOCKED_CSS = ".navigators-page"
-NOT_BLOCKED_CSS = "#loginEmail"
+FILE_NOT_FOUND_MSG = "File not found."
+
+# Note this page is now a 404, but this appears to not be what the site intends,
+# so in case it ever returns we should confirm whether Firefox is still blocked.
 
 
 @pytest.mark.asyncio
 @pytest.mark.with_interventions
 async def test_enabled(client):
     await client.navigate(URL)
-    assert client.await_css(NOT_BLOCKED_CSS)
-    assert not client.find_css(BLOCKED_CSS)
+    assert client.await_text(FILE_NOT_FOUND_MSG)
 
 
 @pytest.mark.asyncio
 @pytest.mark.without_interventions
 async def test_disabled(client):
     await client.navigate(URL)
-    assert client.await_css(BLOCKED_CSS)
-    assert not client.find_css(NOT_BLOCKED_CSS)
+    assert client.await_text(FILE_NOT_FOUND_MSG)
