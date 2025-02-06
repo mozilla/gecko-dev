06bfd40a0f389f17b53af1e1895f6b5154384c04
stransky <stransky@redhat.com>
2025-02-06 06:27:06 +0000

    Bug 1922776 [Linux] Use mShell (toplevel GtkWindow) to get IME colors r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D236844

--- widget/gtk/IMContextWrapper.cpp
+++ widget/gtk/IMContextWrapper.cpp
@@ -232,18 +232,11 @@ class SelectionStyleProvider final {
     sHasShutDown = true;
   }
 
-  // aGDKWindow is a GTK window which will be associated with an IM context.
-  void AttachTo(GdkWindow* aGDKWindow) {
-    GtkWidget* widget = nullptr;
-    // gdk_window_get_user_data() typically returns pointer to widget that
-    // window belongs to.  If it's widget, fcitx retrieves selection colors
-    // of them.  So, we need to overwrite its style.
-    gdk_window_get_user_data(aGDKWindow, (gpointer*)&widget);
-    if (GTK_IS_WIDGET(widget)) {
-      gtk_style_context_add_provider(gtk_widget_get_style_context(widget),
-                                     GTK_STYLE_PROVIDER(mProvider),
-                                     GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-    }
+  // GtkWidget is a GTK window which will be associated with an IM context.
+  void AttachTo(GtkWidget* aWidget) {
+    gtk_style_context_add_provider(gtk_widget_get_style_context(aWidget),
+                                   GTK_STYLE_PROVIDER(mProvider),
+                                   GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
   }
 
   void OnThemeChanged() {
@@ -424,7 +417,7 @@ void IMContextWrapper::Init() {
   // Overwrite selection colors of the window before associating the window
   // with IM context since IME may look up selection colors via IM context
   // to support any colored widgets.
-  SelectionStyleProvider::GetInstance()->AttachTo(gdkWindow);
+  SelectionStyleProvider::GetInstance()->AttachTo(mOwnerWindow->GetGtkWidget());
 
   // NOTE: gtk_im_*_new() abort (kill) the whole process when it fails.
   //       So, we don't need to check the result.
