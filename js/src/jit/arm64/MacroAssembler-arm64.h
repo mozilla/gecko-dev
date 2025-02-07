da326b27e3bcbea3d4f05cc55036a0d72bbf217a
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2025-02-07 18:01:29 +0000

    Bug 1940716 - convertDoubleToInt32: Clobber destination register when zero.  a=dmeehan
    Original Revision: https://phabricator.services.mozilla.com/D234893
    
    Differential Revision: https://phabricator.services.mozilla.com/D236699

--- js/src/jit/arm64/MacroAssembler-arm64.h
+++ js/src/jit/arm64/MacroAssembler-arm64.h
@@ -622,8 +622,11 @@ class MacroAssemblerCompat : public vixl::MacroAssembler {
       if (negativeZeroCheck) {
         Label nonzero;
         Cbnz(dest32, &nonzero);
+        // dest32 is 0, fail if fsrc64 is negative.
         Fmov(dest64, fsrc64);
-        Cbnz(dest64, fail);
+        Cmp(dest64, xzr);
+        B(fail, Assembler::Signed);
+        Mov(dest64, xzr);
         bind(&nonzero);
       }
     }
@@ -647,8 +650,11 @@ class MacroAssemblerCompat : public vixl::MacroAssembler {
     if (negativeZeroCheck) {
       Label nonzero;
       Cbnz(dest32, &nonzero);
+      // dest32 is 0, fail if fsrc64 is negative.
       Fmov(dest32, fsrc);
-      Cbnz(dest32, fail);
+      Cmp(dest32, xzr);
+      B(fail, Assembler::Signed);
+      Mov(dest32, xzr);
       bind(&nonzero);
     }
     Uxtw(dest64, dest64);
@@ -674,8 +680,11 @@ class MacroAssemblerCompat : public vixl::MacroAssembler {
     if (negativeZeroCheck) {
       Label nonzero;
       Cbnz(dest64, &nonzero);
+      // dest64 is 0, fail if fsrc64 is negative.
       Fmov(dest64, fsrc64);
-      Cbnz(dest64, fail);
+      Cmp(dest64, xzr);
+      B(fail, Assembler::Signed);
+      Mov(dest64, xzr);
       bind(&nonzero);
     }
   }
