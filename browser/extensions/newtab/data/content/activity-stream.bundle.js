fbc70a84178cd9e6c9575dd0dc2a8687b09df2a9
scottdowne <sdowne@mozilla.com>
2025-02-07 19:27:03 +0000

    Bug 1945804 - newtab save to pocket button in cards again if pocket is turned off r=home-newtab-reviewers,nbarrett
    Differential Revision: https://phabricator.services.mozilla.com/D237253

--- browser/extensions/newtab/data/content/activity-stream.bundle.js
+++ browser/extensions/newtab/data/content/activity-stream.bundle.js
@@ -2296,7 +2296,7 @@ class DSLinkMenu extends (external_React_default()).PureComponent {
     if (!this.props.isRecentSave) {
       // Show Pocket context menu options if applicable.
       // Additionally, show these menu options for all section cards.
-      if (this.props.pocket_button_enabled && this.props.saveToPocketCard || this.props.isSectionsCard) {
+      if (this.props.pocket_button_enabled && (this.props.saveToPocketCard || this.props.isSectionsCard)) {
         pocketMenuOptions = ["CheckSavedToPocket"];
       }
       TOP_STORIES_CONTEXT_MENU_OPTIONS = ["CheckBookmark", "CheckArchiveFromPocket", ...pocketMenuOptions, "Separator", "OpenInNewWindow", "OpenInPrivateWindow", "Separator", "BlockUrl", ...(this.props.showPrivacyInfo ? ["ShowPrivacyInfo"] : [])];
