214d69ec7480d87746f191070dedbc8844c4803a
Michael Froman <mfroman@mozilla.com>
2025-02-07 15:53:57 -0600

    Bug 1942428 - (fix-882b32d00f) fix -Wtautological-unsigned-zero-compare in payload_type.h

--- third_party/libwebrtc/call/payload_type.h
+++ third_party/libwebrtc/call/payload_type.h
@@ -33,7 +33,11 @@ class PayloadType : public StrongAlias<class PayloadTypeTag, uint8_t> {
     if (rtcp_mux && (id > 63 && id < 96)) {
       return false;
     }
+#if defined(WEBRTC_MOZILLA_BUILD)
+    return id <= 127; // id is unsigned, therefor always >= 0
+#else
     return id >= 0 && id <= 127;
+#endif
   }
   template <typename Sink>
   friend void AbslStringify(Sink& sink, const PayloadType pt) {
