d348f3ffec38c081a45aa2cec6b99d0fcf21ecb7
Emilio Cobos Álvarez <emilio@crisal.io>
2025-02-10 19:09:00 +0000

    Bug 1888298 - Null-check pres shell on teardown. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D237605

--- accessible/generic/DocAccessible.cpp
+++ accessible/generic/DocAccessible.cpp
@@ -674,7 +674,9 @@ nsresult DocAccessible::RemoveEventListeners() {
     NS_RELEASE_THIS();  // Kung fu death grip
   }
 
-  SelectionMgr()->RemoveDocSelectionListener(mPresShell);
+  if (mPresShell) {
+    SelectionMgr()->RemoveDocSelectionListener(mPresShell);
+  }
   return NS_OK;
 }
 
