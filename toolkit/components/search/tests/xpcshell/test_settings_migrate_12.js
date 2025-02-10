c12c771988bdd8290e9a2bfe12e83789bc1a95cf
Moritz Beier <mbeier@mozilla.com>
2025-02-10 18:00:05 +0000

    Bug 1946156 - Ensure the icons don't exceed the maximum size in the migration to search settings v12.  a=dmeehan
    Original Revision: https://phabricator.services.mozilla.com/D237195
    
    Differential Revision: https://phabricator.services.mozilla.com/D237559

--- toolkit/components/search/tests/xpcshell/test_settings_migrate_12.js
+++ toolkit/components/search/tests/xpcshell/test_settings_migrate_12.js
@@ -28,7 +28,7 @@ const SEARCH_SETTINGS = {
       _iconMapObj: {
         16: "data:image/x-icon;base64,ico16",
         32: "data:image/x-icon;base64,ico32",
-        74: "data:image/png;base64,ico74",
+        74: "data:image/x-icon;base64,ico74",
       },
       _metaData: {
         loadPathHash: "OixanEC3I3fSEnZY/YeX1YndC9qdzkqotEEKsodghLY=",
@@ -55,15 +55,13 @@ const SEARCH_SETTINGS = {
 };
 
 const CONFIG = [{ identifier: "engine1" }];
-let icon16;
 
-add_setup(async function () {
-  useHttpServer();
-  icon16 = await SearchTestUtils.fetchAsDataUrl(
-    `${gHttpURL}/icons/remoteIcon.ico`
-  );
-  SEARCH_SETTINGS.engines[1]._iconURL = icon16;
+async function useIcon(url, contentType) {
+  let icon = await SearchTestUtils.fetchAsDataUrl(url, contentType);
+  SEARCH_SETTINGS.engines[1]._iconURL = icon;
   SearchTestUtils.setRemoteSettingsConfig(CONFIG);
+
+  await Services.search.wrappedJSObject.reset();
 
   await IOUtils.writeJSON(
     PathUtils.join(PathUtils.profileDir, SETTINGS_FILENAME),
@@ -72,23 +70,55 @@ add_setup(async function () {
   );
 
   await Services.search.init();
+  return icon;
+}
+
+add_setup(async function () {
+  useHttpServer();
 });
 
 add_task(async function test_migration() {
+  let icon32 = await useIcon(`${gHttpURL}/icons/multipleSizes.ico`);
   let engine = Services.search.getEngineByName("IconsTest");
   let iconMapObj = engine.wrappedJSObject._iconMapObj;
+
   Assert.equal(
     Object.keys(iconMapObj).length,
     3,
     "Still 3 icons in _iconMapObj"
   );
-
   Assert.equal(
-    await engine.getIconURL(16),
-    icon16,
+    await engine.getIconURL(32),
+    icon32,
     "_iconURL overwrote the _iconMapObj icon"
   );
-  info("Checking if the other icons are correct.");
-  Assert.ok((await engine.getIconURL(32)).includes("ico32"));
+
+  info("Checking if the other icons are still correct.");
+  Assert.ok((await engine.getIconURL(16)).includes("ico16"));
+  Assert.ok((await engine.getIconURL(74)).includes("ico74"));
+});
+
+add_task(async function test_migration_rescale() {
+  await useIcon(`${gHttpURL}/icons/bigIcon.ico`);
+  let engine = Services.search.getEngineByName("IconsTest");
+  let iconMapObj = engine.wrappedJSObject._iconMapObj;
+
+  Assert.equal(
+    Object.keys(iconMapObj).length,
+    3,
+    "Still 3 icons in _iconMapObj"
+  );
+  Assert.equal(
+    iconMapObj[256],
+    undefined,
+    "No icon exists with original size."
+  );
+  Assert.ok(
+    (await engine.getIconURL(32)).startsWith("data:image/png;base64,"),
+    "Icon was rescaled to 32x32."
+  );
+
+  info("Checking if the other icons are still correct.");
+  Assert.ok((await engine.getIconURL(16)).includes("ico16"));
   Assert.ok((await engine.getIconURL(74)).includes("ico74"));
 });
