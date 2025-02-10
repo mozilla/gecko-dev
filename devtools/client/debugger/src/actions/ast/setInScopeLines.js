c53a79845a710cb08f0070e2615ab586c951793c
Hubert Boma Manilla <hmanilla@mozilla.com>
2025-02-10 14:47:56 +0000

    Bug 1908327 - [devtools] Migrate getting in scope locations from CM6 parser instead of the parser worker r=devtools-reviewers,ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D217926

--- devtools/client/debugger/src/actions/ast/setInScopeLines.js
+++ devtools/client/debugger/src/actions/ast/setInScopeLines.js
@@ -9,6 +9,7 @@ import {
 } from "../../selectors/index";
 
 import { getSourceLineCount } from "../../utils/source";
+import { features } from "../../utils/prefs";
 
 import { isFulfilled } from "../../utils/async-value";
 
@@ -59,8 +60,13 @@ async function getInScopeLines(location, sourceTextContent, { parserWorker }) {
   // out of scope lines.
   return sourceLines.filter(i => i != undefined);
 }
-
-export function setInScopeLines() {
+/**
+ * Get and store the in scope lines in the reducer
+ * @param {Object} editor - The editor provides an API to retrieve the in scope location
+ *                          details based on lezer in CM6.
+ * @returns
+ */
+export function setInScopeLines(editor) {
   return async thunkArgs => {
     const { getState, dispatch } = thunkArgs;
     const visibleFrame = getVisibleSelectedFrame(getState());
@@ -78,12 +84,16 @@ export function setInScopeLines() {
     if (
       hasInScopeLines(getState(), location) ||
       !sourceTextContent ||
-      !isFulfilled(sourceTextContent)
+      !isFulfilled(sourceTextContent) ||
+      !editor
     ) {
       return;
     }
 
-    const lines = await getInScopeLines(location, sourceTextContent, thunkArgs);
+    const lines =
+      features.codemirrorNext && editor
+        ? await editor.getInScopeLines(location)
+        : await getInScopeLines(location, sourceTextContent, thunkArgs);
 
     dispatch({
       type: "IN_SCOPE_LINES",
