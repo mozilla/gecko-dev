3259c6d4bd31fb7bd25136b6fc3f29140caf3e9e
Roger Yang <royang@mozilla.com>
2025-02-10 13:13:48 +0000

    Bug 1946865 - Do not show always checkbox in app links private tabs prompt. r=android-reviewers,petru, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D237271

--- mobile/android/android-components/components/feature/app-links/src/main/java/mozilla/components/feature/app/links/AppLinksInterceptor.kt
+++ mobile/android/android-components/components/feature/app-links/src/main/java/mozilla/components/feature/app/links/AppLinksInterceptor.kt
@@ -55,7 +55,7 @@ private const val MAPS = "maps."
  * have registered to open.
  * @param launchFromInterceptor If {true} then the interceptor will prompt and launch the link in
  * third-party apps if available.  Do not use this in conjunction with [AppLinksFeature]
- * @param showCheckbox if {true} then the checkbox will be visible on [SimpleRedirectDialogFragment]
+ * @param showCheckbox if {true} then the checkbox will be visible on normal tabs within [SimpleRedirectDialogFragment]
  * @param store [BrowserStore] containing the information about the currently open tabs.
  * @param shouldPrompt If {true} then we should prompt the user before redirect.
  * @param failedToLaunchAction Action to perform when failing to launch in third party app.
@@ -314,7 +314,7 @@ class AppLinksInterceptor(
         return SimpleRedirectDialogFragment.newInstance(
             dialogTitleText = dialogTitle,
             dialogMessageString = dialogMessage,
-            showCheckbox = showCheckbox,
+            showCheckbox = if (isPrivate) false else showCheckbox,
             maxSuccessiveDialogMillisLimit = MAX_SUCCESSIVE_DIALOG_MILLIS_LIMIT,
         )
     }
