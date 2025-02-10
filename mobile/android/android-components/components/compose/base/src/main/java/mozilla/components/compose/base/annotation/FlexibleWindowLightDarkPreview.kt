b9fb000bcc73653c58cf845030b4e52b3bcf034d
t-p-white <towhite@mozilla.com>
2025-02-10 10:45:26 +0000

    Bug 1946773 - Added landscape form factor to the FlexibleWindowLightDarkPreview r=android-reviewers,007
    Differential Revision: https://phabricator.services.mozilla.com/D237224

--- mobile/android/android-components/components/compose/base/src/main/java/mozilla/components/compose/base/annotation/FlexibleWindowLightDarkPreview.kt
+++ mobile/android/android-components/components/compose/base/src/main/java/mozilla/components/compose/base/annotation/FlexibleWindowLightDarkPreview.kt
@@ -20,35 +20,47 @@ private const val LARGE_WINDOW_WIDTH = 1000
 // The device parameter is needed in order to force the `LocalConfiguration.current.screenWidth`
 // to work properly. See: https://issuetracker.google.com/issues/300116108#comment1
 @Preview(
-    name = "Small Window Light",
+    name = "Small Window Light Portrait",
     widthDp = SMALL_WINDOW_WIDTH,
     uiMode = Configuration.UI_MODE_NIGHT_NO,
 )
 @Preview(
-    name = "Small Window Dark",
+    name = "Small Window Dark Portrait",
     widthDp = SMALL_WINDOW_WIDTH,
     uiMode = Configuration.UI_MODE_NIGHT_YES,
 )
 @Preview(
-    name = "Medium Window Light",
+    name = "Small Window Light Landscape",
+    heightDp = SMALL_WINDOW_WIDTH,
+    widthDp = SMALL_WINDOW_WIDTH * 2,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Preview(
+    name = "Small Window Dark Landscape",
+    heightDp = SMALL_WINDOW_WIDTH,
+    widthDp = SMALL_WINDOW_WIDTH * 2,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Preview(
+    name = "Medium Window Light Portrait",
     widthDp = MEDIUM_WINDOW_WIDTH,
     device = Devices.NEXUS_7,
     uiMode = Configuration.UI_MODE_NIGHT_NO,
 )
 @Preview(
-    name = "Medium Window Dark",
+    name = "Medium Window Dark Portrait",
     widthDp = MEDIUM_WINDOW_WIDTH,
     device = Devices.NEXUS_7,
     uiMode = Configuration.UI_MODE_NIGHT_YES,
 )
 @Preview(
-    name = "Large Window Light",
+    name = "Large Window Light Landscape",
     widthDp = LARGE_WINDOW_WIDTH,
     device = Devices.AUTOMOTIVE_1024p,
     uiMode = Configuration.UI_MODE_NIGHT_NO,
 )
 @Preview(
-    name = "Large Window Dark",
+    name = "Large Window Dark Landscape",
     widthDp = LARGE_WINDOW_WIDTH,
     device = Devices.AUTOMOTIVE_1024p,
     uiMode = Configuration.UI_MODE_NIGHT_YES,
