c3c06526a314b29ca7b1e13726c5e22e84ecf3d1
Sandor Molnar <smolnar@mozilla.com>
2025-02-11 19:23:09 +0200

    Backed out 9 changesets (bug 1620998) for causing multiple xpc failures. CLOSED TREE
    Backed out changeset d261d33c1778 (bug 1620998)
    Backed out changeset 159fa2982a24 (bug 1620998)
    Backed out changeset 7b2fd4d66216 (bug 1620998)
    Backed out changeset 81bf2c760466 (bug 1620998)
    Backed out changeset bd4719db23d2 (bug 1620998)
    Backed out changeset 074b06a16323 (bug 1620998)
    Backed out changeset 0d6ef1c8e871 (bug 1620998)
    Backed out changeset 0a7a0876a987 (bug 1620998)
    Backed out changeset fe27920e71da (bug 1620998)

--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -109,9 +109,8 @@ extern crate rust_minidump_writer_linux;
 
 #[cfg(feature = "crashreporter")]
 extern crate mozannotation_client;
-
 #[cfg(feature = "crashreporter")]
-extern crate crash_helper_client;
+extern crate mozannotation_server;
 
 #[cfg(feature = "webmidi_midir_impl")]
 extern crate midir_impl;
