bba1bf12270af4794a595542ef267ae6cb78bce2
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2025-02-11 19:44:31 +0000

    Bug 1553850 - Add LLVM profiling to Linux Sandbox r=perftest-reviewers,afinder,jld
    Differential Revision: https://phabricator.services.mozilla.com/D217167

--- security/sandbox/linux/broker/SandboxBrokerPolicyFactory.cpp
+++ security/sandbox/linux/broker/SandboxBrokerPolicyFactory.cpp
@@ -35,6 +35,10 @@
 #include "nsNetCID.h"
 #include "prenv.h"
 
+#if defined(MOZ_PROFILE_GENERATE)
+#  include <string>
+#endif
+
 #ifdef ANDROID
 #  include "cutils/properties.h"
 #endif
@@ -417,6 +421,25 @@ static void AddGLDependencies(SandboxBroker::Policy* policy) {
   // server, because headless GL (e.g., Mesa GBM) may not need it.
 }
 
+// Assums this is an absolute path, SandboxBroker does not like relative paths:
+// RealPath() will try to get the absolute path of the llvm profile path to open
+// for writing but this will return errno=2 because the file does not exists, so
+// sandbox will not allow for its creation.
+//
+// Forcing expecting an absolute path will be enough to make sure it can be
+// allowed.
+//
+// It should only be allowed on instrumented builds, never on production
+// builds.
+#if defined(MOZ_PROFILE_GENERATE)
+static void AddLLVMProfilePathDirectory(SandboxBroker::Policy* aPolicy) {
+  std::string parentPath;
+  if (GetLlvmProfileDir(parentPath)) {
+    aPolicy->AddFutureDir(rdwrcr, parentPath.c_str());
+  }
+}
+#endif  // defined(MOZ_PROFILE_GENERATE)
+
 void SandboxBrokerPolicyFactory::InitContentPolicy() {
   const bool headless =
       StaticPrefs::security_sandbox_content_headless_AtStartup();
@@ -773,6 +796,10 @@ void SandboxBrokerPolicyFactory::InitContentPolicy() {
     policy->AddPath(rdonly, "/sys/module/amdgpu");
   }
 
+#if defined(MOZ_PROFILE_GENERATE)
+  AddLLVMProfilePathDirectory(policy);
+#endif
+
   mCommonContentPolicy.reset(policy);
 }
 
@@ -962,6 +989,10 @@ SandboxBrokerPolicyFactory::GetRDDPolicy(int aPid) {
   policy->AddPath(rdwr, "/dev/nvhost-vic");
 #endif  // defined(__aarch64__)
 
+#if defined(MOZ_PROFILE_GENERATE)
+  AddLLVMProfilePathDirectory(policy.get());
+#endif
+
   if (policy->IsEmpty()) {
     policy = nullptr;
   }
@@ -1016,6 +1047,10 @@ SandboxBrokerPolicyFactory::GetSocketProcessPolicy(int aPid) {
       policy->AddTree(rdonly, tmpPath.get());
     }
   }
+
+#if defined(MOZ_PROFILE_GENERATE)
+  AddLLVMProfilePathDirectory(policy.get());
+#endif
 
   if (policy->IsEmpty()) {
     policy = nullptr;
@@ -1074,6 +1109,10 @@ SandboxBrokerPolicyFactory::GetUtilityProcessPolicy(int aPid) {
       policy->AddTree(rdonly, tmpPath.get());
     }
   }
+
+#if defined(MOZ_PROFILE_GENERATE)
+  AddLLVMProfilePathDirectory(policy.get());
+#endif
 
   if (policy->IsEmpty()) {
     policy = nullptr;
