63c5af0098dd003b669d707f080c34ee41b50d14
Thomas Wisniewski <twisniewski@mozilla.com>
2025-02-12 18:05:13 +0000

    Bug 1943898 - add an Android-only webcompat intervention for www.capital.gr to prevent endless reloading cycles; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D237852

---
+++ testing/webcompat/interventions/tests/test_1943898_capital_gr.py
@@ -0,0 +1,29 @@
+import asyncio
+
+import pytest
+
+URL = "https://www.capital.gr/"
+
+
+async def is_reload_cycle_detected(client):
+    await client.navigate(URL)
+    client.await_css("#alwaysFetch")
+    client.execute_script("location.reload()")
+    await asyncio.sleep(1)
+    try:
+        await (await client.promise_navigation_begins(URL, timeout=3))
+        return True
+    except asyncio.exceptions.TimeoutError:
+        return False
+
+
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    assert not await is_reload_cycle_detected(client)
+
+
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    assert await is_reload_cycle_detected(client)
