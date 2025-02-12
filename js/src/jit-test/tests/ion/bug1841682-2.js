e1d9957169f285ec53e6d6c57b8b01eb4242872b
Jan de Mooij <jdemooij@mozilla.com>
2025-02-12 12:45:16 +0000

    Bug 1841682 - Add tests. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D183584

---
+++ js/src/jit-test/tests/ion/bug1841682-2.js
@@ -0,0 +1,20 @@
+// |jit-test| --fast-warmup
+
+function f(a) {
+  var vals = Object.getOwnPropertyNames(a).map(p => a[p]);
+  for (var i = 0; i < vals.length; i++) {
+    var v = vals[i];
+    queue.push([v]);
+  }
+}
+
+gczeal(12);
+gczeal(19, 5);
+
+var a = [{x:{0:0}}, {x:{0:0}}];
+var queue = [a];
+
+while (queue.length > 0 && queue.length < 100) {
+  var arr = queue.shift();
+  f(arr);
+}
