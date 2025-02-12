9d60566f2c4e4cdc984ca01820cb8a6f9ecbe49c
Thomas Wisniewski <twisniewski@mozilla.com>
2025-02-12 18:05:10 +0000

    Bug 1906630 - add a UA override for watch.foxtel.com.au on desktop; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D237841

---
+++ testing/webcompat/interventions/tests/test_1906630_watch_foxtel_com_au.py
@@ -0,0 +1,23 @@
+import pytest
+
+URL = "https://watch.foxtel.com.au/app/#/live/fs1"
+SUPPORTED_CSS = ".login-page"
+UNSUPPORTED_CSS = ".ua-barrier"
+
+
+@pytest.mark.skip_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL)
+    assert client.await_css(SUPPORTED_CSS)
+    assert not client.find_css(UNSUPPORTED_CSS, is_displayed=True)
+
+
+@pytest.mark.skip_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL)
+    assert client.await_css(UNSUPPORTED_CSS, is_displayed=True)
+    assert not client.find_css(SUPPORTED_CSS)
