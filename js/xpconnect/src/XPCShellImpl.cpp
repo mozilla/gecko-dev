0914ba4976a00c2126b44d2429d11342c2d5c6c1
Gabriele Svelto <gsvelto@mozilla.com>
2025-02-12 14:06:11 +0000

    Bug 1620998 - Fix XPCShell tests that need to run the crash helper r=afranchuk
    Differential Revision: https://phabricator.services.mozilla.com/D231525

--- js/xpconnect/src/XPCShellImpl.cpp
+++ js/xpconnect/src/XPCShellImpl.cpp
@@ -1198,7 +1198,16 @@ int XRE_XPCShellMain(int argc, char** argv, char** envp,
 
     const char* val = getenv("MOZ_CRASHREPORTER");
     if (val && *val && !CrashReporter::IsDummy()) {
-      rv = CrashReporter::SetExceptionHandler(greDir, true);
+      nsCOMPtr<nsIFile> greBinDir;
+      bool persistent = false;
+      rv = dirprovider.GetFile(NS_GRE_BIN_DIR, &persistent,
+                               getter_AddRefs(greBinDir));
+      if (NS_FAILED(rv)) {
+        printf("Could not get the GreBinD directory\n");
+        return 1;
+      }
+
+      rv = CrashReporter::SetExceptionHandler(greBinDir, true);
       if (NS_FAILED(rv)) {
         printf("CrashReporter::SetExceptionHandler failed!\n");
         return 1;
