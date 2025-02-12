13b44389bff8414046717e6daa0f4c330fae9247
Thomas Wisniewski <twisniewski@mozilla.com>
2025-02-12 18:05:07 +0000

    Bug 1902452 - add eternalslots.com to our Android intervention for casino sites; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D237787

---
+++ testing/webcompat/interventions/tests/test_1830739_1902452_eternalslots_com.py
@@ -0,0 +1,31 @@
+import pytest
+
+URL = "https://eternalslots.com/games/3"
+FIRST_GAME_CSS = ".gameGrid .AsyncImage-image[src]"
+PLAY_FOR_FUN_CSS = "#BtnUserPlayNow"
+IFRAME_CSS = "iframe[src*=gameLauncher]"
+UNSUPPORTED_CSS = ".unsupported-device-box"
+SUPPORTED_CSS = "#game_main"
+
+
+async def get_to_page(client):
+    await client.navigate(URL)
+    client.click(client.await_css(FIRST_GAME_CSS, is_displayed=True))
+    client.soft_click(client.await_css(PLAY_FOR_FUN_CSS, is_displayed=True))
+    client.switch_to_frame(client.await_css(IFRAME_CSS))
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await get_to_page(client)
+    assert client.await_css(SUPPORTED_CSS, timeout=30)
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await get_to_page(client)
+    assert client.await_css(UNSUPPORTED_CSS, timeout=30)
