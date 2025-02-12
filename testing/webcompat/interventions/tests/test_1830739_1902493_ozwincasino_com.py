c24f99a2848e800c90785564b735f6881716fc7f
Thomas Wisniewski <twisniewski@mozilla.com>
2025-02-12 18:05:08 +0000

    Bug 1902493 - add ozwincasino.com to our Android intervention for casino sites; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D237790

---
+++ testing/webcompat/interventions/tests/test_1830739_1902493_ozwincasino_com.py
@@ -0,0 +1,31 @@
+import pytest
+
+URL = "https://play.ozwincasino.com:4072/lobby.aspx?skinid=1"
+FIRST_GAME_CSS = ".home-view-games-row .gametile"
+FIRST_GAME_PRACTICE_CSS = (
+    ".detailDialog button:not(#dialogTitleBarCloseBtn).bg-buttonSecondary"
+)
+UNSUPPORTED_CSS = ".unsupported-device-box"
+SUPPORTED_CSS = "#game_main"
+
+
+async def get_to_page(client):
+    await client.navigate(URL)
+    client.click(client.await_css(FIRST_GAME_CSS, is_displayed=True))
+    client.soft_click(client.await_css(FIRST_GAME_PRACTICE_CSS, is_displayed=True))
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await get_to_page(client)
+    assert client.await_css(SUPPORTED_CSS, timeout=30)
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await get_to_page(client)
+    assert client.await_css(UNSUPPORTED_CSS, timeout=30)
