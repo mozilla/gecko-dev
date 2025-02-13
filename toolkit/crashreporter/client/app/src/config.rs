4e1a0de8db19150f423ef2e9456d28bb582a633e
Stanca Serban <sstanca@mozilla.com>
2025-02-13 04:28:21 +0200

    Backed out 2 changesets (bug 1946542) for causing multiple failures.
    Backed out changeset 9ab0d012d1c9 (bug 1946542)
    Backed out changeset ff91adc5cf49 (bug 1946542)

--- toolkit/crashreporter/client/app/src/config.rs
+++ toolkit/crashreporter/client/app/src/config.rs
@@ -19,10 +19,6 @@ pub mod test {
     pub const MINIDUMP_PRUNE_SAVE_COUNT: usize = super::MINIDUMP_PRUNE_SAVE_COUNT;
 }
 
-mod buildid_section {
-    include!(concat!(env!("OUT_DIR"), "/buildid_section.rs"));
-}
-
 const VENDOR_KEY: &str = "Vendor";
 const PRODUCT_KEY: &str = "ProductName";
 const DEFAULT_VENDOR: &str = "Mozilla";
@@ -564,37 +560,6 @@ pub fn installation_path() -> &'static Path {
         dir_path
     });
     &*PATH
-}
-
-/// Read the buildid from the installation.
-///
-/// This may fail if installation files are not found.
-pub fn buildid() -> Option<&'static str> {
-    static BUILDID: Lazy<Option<String>> = Lazy::new(|| {
-        let section_name = buildid_section::MOZ_BUILDID_SECTION_NAME.to_str().ok()?;
-        let xul_path = installation_path().join(if cfg!(target_os = "macos") {
-            "XUL"
-        } else if cfg!(target_os = "windows") {
-            "xul.dll"
-        } else {
-            "libxul.so"
-        });
-        #[cfg(mock)]
-        let xul_path = xul_path.as_ref();
-        match buildid_reader::BuildIdReader::new(&xul_path)
-            .and_then(|mut reader| reader.read_string_build_id(section_name))
-        {
-            Ok(s) => {
-                log::info!("read build id from XUL: {s}");
-                Some(s)
-            }
-            Err(e) => {
-                log::warn!("failed to read build id from XUL: {e}");
-                None
-            }
-        }
-    });
-    BUILDID.as_deref()
 }
 
 fn self_path() -> &'static Path {
