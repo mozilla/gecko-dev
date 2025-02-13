64f1d4ee26000cec58f3e1aa13c67099f553c360
Ryan VanderMeulen <ryanvm@gmail.com>
2025-02-13 13:22:35 +0000

    Bug 1947886 - Update AndroidX Navigation to version 2.8.7. r=android-reviewers,mcarare
    Differential Revision: https://phabricator.services.mozilla.com/D237961

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -71,7 +71,7 @@ object Versions {
         const val lifecycle = "2.8.7"
         const val localbroadcastmanager = "1.0.0"
         const val media = "1.7.0"
-        const val navigation = "2.8.6"
+        const val navigation = "2.8.7"
         const val paging = "3.3.5"
         const val palette = "1.0.0"
         const val preferences = "1.2.1"
