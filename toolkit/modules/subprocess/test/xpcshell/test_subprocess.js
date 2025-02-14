93d27e891871c4a22e24c36b9307db64351b9db7
aiunusov@mozilla.com <aiunusov@mozilla.com>
2025-02-14 17:24:33 +0000

    Bug 1945470 - enable dom.workers.timeoutmanager for Nightly, r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D237064

--- toolkit/modules/subprocess/test/xpcshell/test_subprocess.js
+++ toolkit/modules/subprocess/test/xpcshell/test_subprocess.js
@@ -5,7 +5,17 @@ const { setTimeout } = ChromeUtils.importESModule(
   "resource://gre/modules/Timer.sys.mjs"
 );
 
-const MAX_ROUND_TRIP_TIME_MS = AppConstants.DEBUG || AppConstants.ASAN ? 18 : 9;
+const { Preferences } = ChromeUtils.importESModule(
+  "resource://gre/modules/Preferences.sys.mjs"
+);
+
+let max_round_trip_time_ms = AppConstants.DEBUG || AppConstants.ASAN ? 18 : 9;
+if (Preferences.get("dom.workers.timeoutmanager") === true) {
+  max_round_trip_time_ms = 90;
+}
+
+const MAX_ROUND_TRIP_TIME_MS = max_round_trip_time_ms;
+
 const MAX_RETRIES = 5;
 
 let PYTHON;
