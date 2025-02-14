dcd46847f77f9f210415fa92b1177bafe5aac86c
Jed Davis <jld@mozilla.com>
2025-02-14 01:33:08 +0000

    Bug 1944669 - Suppress ProcessWatcher logspam introduced in earlier rewrite. r=ipc-reviewers,mccr8
    The case in question is where the process exits before the timeout
    expires and it's a forkserver child.  Previously (before bug 1658072)
    there wouldn't be a log message in this case, but otherwise the effect
    was the same: if a process with the given pid was still running, it
    would be sent `SIGKILL`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D237857

--- ipc/chromium/src/chrome/common/process_watcher_posix_sigchld.cc
+++ ipc/chromium/src/chrome/common/process_watcher_posix_sigchld.cc
@@ -156,7 +156,20 @@ already_AddRefed<nsITimer> DelayedKill(pid_t aPid) {
       getter_AddRefs(timer),
       [aPid](nsITimer*) {
         if (kill(aPid, SIGKILL) != 0) {
-          CHROMIUM_LOG(ERROR) << "failed to send SIGKILL to process " << aPid;
+          const int err = errno;
+      // Bug 1944669: suppress logging if it's a forkserver child
+      // process that already exited.  (Before bug 1658072 we
+      // would kill(pid, 0) first, but that doesn't change
+      // anything.)  This can be removed with bug 1752638.
+#ifdef MOZ_ENABLE_FORKSERVER
+          const bool forkServed = mozilla::ipc::ForkServiceChild::WasUsed();
+#else
+          constexpr bool forkServed = false;
+#endif
+          if (err != ESRCH || !forkServed) {
+            CHROMIUM_LOG(ERROR) << "failed to send SIGKILL to process " << aPid
+                                << strerror(err);
+          }
         }
       },
       kMaxWaitMs, nsITimer::TYPE_ONE_SHOT, "ProcessWatcher::DelayedKill",
