ab201321706762910066d5d330f03a246a5acfc6
Kagami Sascha Rosylight <saschanaz@outlook.com>
2025-02-17 21:06:09 +0000

    Bug 1890560 - Call UnregisterHandler earlier for the old handler r=asuth,nalexander,win-reviewers,gstoll
    This makes sure we get the consistent behavior for every platform (except Android for now).
    
    Differential Revision: https://phabricator.services.mozilla.com/D237022

--- widget/windows/ToastNotification.cpp
+++ widget/windows/ToastNotification.cpp
@@ -487,7 +487,10 @@ ToastNotification::ShowAlert(nsIAlertNotification* aAlert,
     }
   }
 
-  RefPtr<ToastNotificationHandler> oldHandler = mActiveHandlers.Get(name);
+  // If there was a previous handler with the same name then unregister it.
+  if (RefPtr<ToastNotificationHandler> oldHandler = mActiveHandlers.Get(name)) {
+    oldHandler->UnregisterHandler();
+  }
 
   NS_ENSURE_TRUE(mAumid.isSome(), NS_ERROR_UNEXPECTED);
   RefPtr<ToastNotificationHandler> handler = new ToastNotificationHandler(
@@ -510,11 +513,6 @@ ToastNotification::ShowAlert(nsIAlertNotification* aAlert,
     mActiveHandlers.Remove(name);
     handler->UnregisterHandler();
     return rv;
-  }
-
-  // If there was a previous handler with the same name then unregister it.
-  if (oldHandler) {
-    oldHandler->UnregisterHandler();
   }
 
   return NS_OK;
