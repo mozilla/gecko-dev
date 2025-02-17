a2f5b0d845b8ca44e14ad9e6126099dfa8ad5655
Ting-Yu Lin <tlin@mozilla.com>
2025-02-17 17:38:15 +0000

    Bug 421436 - Fix y coordinate when calling focusEditableField in browser_rules_grid-template-areas.js. r=devtools-reviewers,nchevobbe
    After apply the previous patch D238162, the return value of
    `getBoundingClientRect()` can be different since the `<br>` elements do not have
    any width.
    
    This patch does not change any test expectation. It only fixes the y-coordinate
    passing to `focusEditableField` so that the call succeeds on the
    `grid-template-area` value span stored in `gridRuleProperty`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D238388

--- devtools/client/inspector/rules/test/browser_rules_grid-template-areas.js
+++ devtools/client/inspector/rules/test/browser_rules_grid-template-areas.js
@@ -90,14 +90,8 @@ add_task(async function () {
 
   // Calculate offsets to click in the value line which is below the property name line .
   const rect = gridRuleProperty.getBoundingClientRect();
-  const previousProperty = await getRuleViewProperty(
-    view,
-    "#testid",
-    "display"
-  ).nameSpan.getBoundingClientRect();
-
   const x = rect.width / 2;
-  const y = rect.y - previousProperty.y + 1;
+  const y = rect.height / 2;
 
   info("Focusing the css property editable value");
   await focusEditableField(view, gridRuleProperty, x, y);
