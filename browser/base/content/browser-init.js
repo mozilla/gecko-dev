c76d81104db14db43724bd0d006d79511015d6b3
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2025-02-18 16:17:28 +0000

    Bug 1946400 - move nsBrowserAccess (nsIBrowserDOMWindow impl) out of browser.js into its own module, r=firefox-desktop-core-reviewers ,mossop
    Differential Revision: https://phabricator.services.mozilla.com/D238533

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -127,11 +127,14 @@ var gBrowserInit = {
   },
 
   onDOMContentLoaded() {
-    // This needs setting up before we create the first remote browser.
+    // All of this needs setting up before we create the first remote browser.
     window.docShell.treeOwner
       .QueryInterface(Ci.nsIInterfaceRequestor)
       .getInterface(Ci.nsIAppWindow).XULBrowserWindow = window.XULBrowserWindow;
-    window.browserDOMWindow = new nsBrowserAccess();
+    BrowserUtils.callModulesFromCategory(
+      { categoryName: "browser-window-domcontentloaded-before-tabbrowser" },
+      window
+    );
 
     gBrowser = new window.Tabbrowser();
     gBrowser.init();
@@ -1146,6 +1149,10 @@ var gBrowserInit = {
     window.docShell.treeOwner
       .QueryInterface(Ci.nsIInterfaceRequestor)
       .getInterface(Ci.nsIAppWindow).XULBrowserWindow = null;
-    window.browserDOMWindow = null;
+
+    BrowserUtils.callModulesFromCategory(
+      { categoryName: "browser-window-unload" },
+      window
+    );
   },
 };
