effd1e03a9a2d1c6815f55a005fa7a7fb0c8345e
Nika Layzell <nika@thelayzells.com>
2025-02-18 21:19:20 +0000

    Bug 1945252 - Part 6: Temporarily use a headless clipboard on iOS, r=glandium
    In the future we'll want to hook up to the system clipboard on iOS, but for now
    we can use the headless clipboard to shim out the functionality.
    
    Differential Revision: https://phabricator.services.mozilla.com/D236407

--- widget/uikit/nsWidgetFactory.mm
+++ widget/uikit/nsWidgetFactory.mm
@@ -3,13 +3,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "nsISupports.h"
+#include "mozilla/Components.h"
+#include "mozilla/ModuleUtils.h"
+#include "mozilla/WidgetUtils.h"
+
+#include "nsWidgetsCID.h"
+
 #include "nsAppShell.h"
 #include "nsAppShellSingleton.h"
 #include "nsLookAndFeel.h"
-#include "nsWidgetFactory.h"
-#include "mozilla/WidgetUtils.h"
 
+#include "HeadlessClipboard.h"
+
+using namespace mozilla;
 using namespace mozilla::widget;
+
+NS_IMPL_COMPONENT_FACTORY(nsIClipboard) {
+  nsCOMPtr<nsIClipboard> inst = new HeadlessClipboard();
+
+  return inst.forget();
+}
 
 void nsWidgetUIKitModuleCtor() { nsAppShellInit(); }
 
