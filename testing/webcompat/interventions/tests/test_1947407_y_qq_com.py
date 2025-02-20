3c6cbd9b43507d39918e394903acd0b17a3244ce
Thomas Wisniewski <twisniewski@mozilla.com>
2025-02-20 18:39:34 +0000

    Bug 1947407 - add an desktop CSS intervention for y.qq.com/artists to unblock Firefox; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D238701

---
+++ testing/webcompat/interventions/tests/test_1947407_y_qq_com.py
@@ -0,0 +1,24 @@
+import pytest
+
+URL = "https://y.qq.com/artists/"
+
+SUPPORTED_CSS = "#app"
+UNSUPPORTED_CSS = ".popup_browser .icon_chrome"
+
+
+@pytest.mark.skip_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL)
+    assert client.await_css(SUPPORTED_CSS)
+    assert not client.find_css(UNSUPPORTED_CSS, is_displayed=True)
+
+
+@pytest.mark.skip_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL)
+    assert client.await_css(UNSUPPORTED_CSS, is_displayed=True)
+    assert not client.find_css(SUPPORTED_CSS, is_displayed=True)
