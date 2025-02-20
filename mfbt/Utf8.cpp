12c956a3cf18e6c1c875d533319a9039fa082230
Mike Hommey <mh+mozilla@glandium.org>
2025-02-20 05:08:22 +0000

    Bug 1945507 - Use FINAL_LIBRARY to link mfbt to mozglue. r=firefox-build-system-reviewers,sergesanspaille
    The difference it makes is that it makes m mfbt inherit the
    LIBRARY_DEFINES from mozglue, which were intended to propagate
    there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D238539

--- mfbt/Utf8.cpp
+++ mfbt/Utf8.cpp
@@ -4,6 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#define MOZ_PRETEND_NO_JSRUST 1
+
 #include "mozilla/Maybe.h"
 #include "mozilla/TextUtils.h"
 #include "mozilla/Types.h"
