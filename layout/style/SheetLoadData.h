d9ac4bb4828fff071e2707443eadf98c367724b2
Emilio Cobos Álvarez <emilio@crisal.io>
2025-02-20 16:53:04 +0000

    Bug 1944052 - Set stylesheet metadata on OnStartRequest. r=dshin, a=dmeehan
    SKIP_BMO_CHECK
    
    Differential Revision: https://phabricator.services.mozilla.com/D238592

--- layout/style/SheetLoadData.h
+++ layout/style/SheetLoadData.h
@@ -94,13 +94,12 @@ class SheetLoadData final
   NotNull<const Encoding*> DetermineNonBOMEncoding(const nsACString& aSegment,
                                                    nsIChannel*) const;
 
+  void OnStartRequest(nsIRequest*);
+
   // The caller may have the bytes for the stylesheet split across two strings,
   // so aBytes1 and aBytes2 refer to those pieces.
   nsresult VerifySheetReadyToParse(nsresult aStatus, const nsACString& aBytes1,
-                                   const nsACString& aBytes2,
-                                   nsIChannel* aChannel,
-                                   nsIURI* aFinalChannelURI,
-                                   nsIPrincipal* aPrincipal);
+                                   const nsACString& aBytes2, nsIChannel*);
 
   NS_DECL_ISUPPORTS
 
