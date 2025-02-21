b375c765c1061dfc58ff5e8b86b22e6b56056001
Mike Hommey <mh+mozilla@glandium.org>
2025-02-21 22:38:37 +0000

    Bug 1949095 - Adjust browser_net_content-type.js for when using libz-rs. r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D239087

--- devtools/client/netmonitor/test/browser_net_content-type.js
+++ devtools/client/netmonitor/test/browser_net_content-type.js
@@ -136,7 +136,10 @@ add_task(async function () {
       statusText: "OK",
       type: "plain",
       fullMimeType: "text/plain",
-      transferred: L10N.getFormatStrWithNumbers("networkMenu.sizeB", 324),
+      transferred: L10N.getFormatStrWithNumbers(
+        "networkMenu.sizeB",
+        AppConstants.USE_LIBZ_RS ? 333 : 324
+      ),
       size: L10N.getFormatStrWithNumbers("networkMenu.size.kB", 10.99),
       time: true,
     }
