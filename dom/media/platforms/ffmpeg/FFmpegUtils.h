b6f398bff2df76b958d389b4f77b73abfbac72f8
alwu <alwu@mozilla.com>
2025-02-21 06:50:15 +0000

    Bug 1894818 - part3 : support HEVC for ffmpeg video decoder. r=stransky
    Differential Revision: https://phabricator.services.mozilla.com/D238155

--- dom/media/platforms/ffmpeg/FFmpegUtils.h
+++ dom/media/platforms/ffmpeg/FFmpegUtils.h
@@ -41,6 +41,7 @@ inline bool IsVideoCodec(AVCodecID aCodecID) {
 #endif
 #if LIBAVCODEC_VERSION_MAJOR >= 55
     case AV_CODEC_ID_VP9:
+    case AV_CODEC_ID_HEVC:
 #endif
 #if LIBAVCODEC_VERSION_MAJOR >= 59
     case AV_CODEC_ID_AV1:
