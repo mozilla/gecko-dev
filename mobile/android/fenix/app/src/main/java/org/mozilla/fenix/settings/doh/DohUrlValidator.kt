c0102190324b9e6a6a4ac9a7e2133c0a7fe509dd
Sean <sekim@mozilla.com>
2025-02-24 20:18:14 +0000

    Bug 1934262 - [DoH on Android] Part 2: Add a url validator for the DoH settings r=valentin,android-reviewers,sfamisa
    Differential Revision: https://phabricator.services.mozilla.com/D237627

---
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/doh/DohUrlValidator.kt
@@ -0,0 +1,87 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.settings.doh
+
+import java.net.URI
+import java.net.URISyntaxException
+
+/**
+ * An object for DoH custom provider and exception urls.
+ */
+object DohUrlValidator {
+    /**
+     * Validates that the provided [url] is a valid absolute https url.
+     *
+     * @param url The URL to be validated.
+     * @return The normalized url as a string.
+     *
+     * @throws UrlValidationException.NonHttpsUrlException If the url scheme is not "https".
+     * @throws UrlValidationException.InvalidUrlException If the url is invalid or does not have a valid host.
+     */
+    @Suppress("ThrowsCount")
+    fun validate(url: String): String {
+        // This check is necessary because urls starting with "https:/" or "https:\"
+        // would be considered "invalid" in java.net.URI
+        if (!url.startsWith("https://")) {
+            throw UrlValidationException.NonHttpsUrlException
+        }
+        return try {
+            val uri = URI.create(url)
+            when {
+                uri.scheme != "https" -> throw UrlValidationException.NonHttpsUrlException
+                uri.host.isNullOrBlank() || !uri.isAbsolute -> throw UrlValidationException.InvalidUrlException
+                else -> uri.toString()
+            }
+        } catch (e: URISyntaxException) {
+            throw UrlValidationException.InvalidUrlException
+        } catch (e: IllegalArgumentException) {
+            throw UrlValidationException.InvalidUrlException
+        }
+    }
+
+    /**
+     * Removes the scheme (e.g. "https://", "http://") from the given [url].
+     *
+     * For instance:
+     * - "http://example.com" -> "c.com"
+     * - "https://foo"    -> "foo"
+     * - "ftp://foo/bar"  -> "foo/bar"
+     * - "example.com"    -> "example.com"
+     *
+     * @param url The original URL string.
+     * @return The [url] without its leading scheme and "://", if present.
+     */
+    @Suppress("TooGenericExceptionCaught")
+    fun dropScheme(url: String): String {
+        // Only drop the scheme if the url is valid
+        return try {
+            val uri = URI.create(url)
+            val prefix = "${uri.scheme}://"
+            uri.toString().removePrefix(prefix)
+        } catch (e: NullPointerException) {
+            // a scheme does not exist
+            url
+        } catch (e: URISyntaxException) {
+            url
+        } catch (e: IllegalArgumentException) {
+            url
+        }
+    }
+}
+
+/**
+ * Represents the possible exceptions thrown during DoH url validation.
+ */
+sealed class UrlValidationException : RuntimeException() {
+    /**
+     * Indicates that the provided url does not use https as its scheme.
+     */
+    data object NonHttpsUrlException : UrlValidationException()
+
+    /**
+     * Indicates that the provided url is invalid (e.g. malformed, missing host).
+     */
+    data object InvalidUrlException : UrlValidationException()
+}
---
+++ mobile/android/fenix/app/src/test/java/org/mozilla/fenix/settings/doh/DohUrlValidatorTest.kt
@@ -0,0 +1,83 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.settings.doh
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class DohUrlValidatorTest {
+    @Test
+    fun `WHEN a valid url is passed THEN the validator should return itself without any exception`() {
+        assertEquals(
+            DohUrlValidator.validate("https://example.com"),
+            "https://example.com",
+        )
+    }
+
+    @Test
+    fun `WHEN a non-https url is passed THEN the validator should throw NonHttpsUrl exception`() {
+        assertThrows(
+            UrlValidationException.NonHttpsUrlException::class.java,
+        ) {
+            DohUrlValidator.validate("http://example.com")
+        }
+    }
+
+    @Test
+    fun `WHEN url does not start with a scheme followed by colon and two backslashes THEN the validator should throw NonHttpsUrl exception`() {
+        // This is intended, unfortunately due to the nature of java.utils.URI
+        assertThrows(
+            UrlValidationException.NonHttpsUrlException::class.java,
+        ) {
+            DohUrlValidator.validate("https:/example.com")
+        }
+    }
+
+    @Test
+    fun `WHEN url has a blank host THEN the validator should throw InvalidUrl exception`() {
+        assertThrows(
+            UrlValidationException.InvalidUrlException::class.java,
+        ) {
+            DohUrlValidator.validate("https://?q=abc")
+        }
+    }
+
+    @Test
+    fun `WHEN url contains an invalid character THEN the validator should throw InvalidUrl exception`() {
+        assertThrows(
+            UrlValidationException.InvalidUrlException::class.java,
+        ) {
+            DohUrlValidator.validate("https://e@.com")
+        }
+    }
+
+    @Test
+    fun `WHEN dropScheme is called with a url already without a scheme THEN no change should occur`() {
+        val url = "foo.bar"
+        assertEquals(
+            DohUrlValidator.dropScheme(url),
+            url,
+        )
+    }
+
+    @Test
+    fun `WHEN dropScheme is called with a valid url with a scheme THEN the scheme should be dropped`() {
+        val url = "https://foo.bar"
+        assertEquals(
+            DohUrlValidator.dropScheme(url),
+            "foo.bar",
+        )
+    }
+
+    @Test
+    fun `WHEN dropScheme is called with an invalid url THEN no change should occur`() {
+        val url = "totally&!@#invalid://host"
+        assertEquals(
+            DohUrlValidator.dropScheme(url),
+            url,
+        )
+    }
+}
