9c5505d5043140b0a2d52c8661e71a7198085b6f
Stanca Serban <sstanca@mozilla.com>
2025-02-25 12:59:31 +0200

    Backed out 3 changesets (bug 1946404) as requested by plingurar for enabling rendering issues, pending more investigation. CLOSED TREE
    Backed out changeset 83ed17be8e13 (bug 1946404)
    Backed out changeset ac46b0f1f7d3 (bug 1946404)
    Backed out changeset 3355377fb4b1 (bug 1946404)

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/search/SearchDialogFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/search/SearchDialogFragment.kt
@@ -71,10 +71,8 @@ import mozilla.components.support.ktx.android.content.hasCamera
 import mozilla.components.support.ktx.android.content.isPermissionGranted
 import mozilla.components.support.ktx.android.content.res.getSpanned
 import mozilla.components.support.ktx.android.net.isHttpOrHttps
-import mozilla.components.support.ktx.android.view.ImeInsetsSynchronizer
 import mozilla.components.support.ktx.android.view.findViewInHierarchy
 import mozilla.components.support.ktx.android.view.hideKeyboard
-import mozilla.components.support.ktx.android.view.setupPersistentInsets
 import mozilla.components.support.ktx.android.view.showKeyboard
 import mozilla.components.support.ktx.kotlin.toNormalizedUrl
 import mozilla.components.ui.autocomplete.InlineAutocompleteEditText
@@ -215,8 +213,6 @@ class SearchDialogFragment : AppCompatDialogFragment(), UserInteractionHandler {
                     this@SearchDialogFragment.onBackPressed()
                 }
             }
-
-            window?.setupPersistentInsets()
         }
     }
 
@@ -480,7 +476,6 @@ class SearchDialogFragment : AppCompatDialogFragment(), UserInteractionHandler {
             updateAccessibilityTraversalOrder()
         }
 
-        ImeInsetsSynchronizer.setup(view)
         observeClipboardState()
         observeSuggestionProvidersState()
 
