384aba85abcc3c89dd2281d0be7b8c2bf811961f
Tooru Fujisawa <arai_a@mac.com>
2025-02-25 01:46:52 +0000

    Bug 1948162 - Reject out-of-range column number in Debugger API. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D238392

--- js/src/debugger/Script.cpp
+++ js/src/debugger/Script.cpp
@@ -739,6 +739,9 @@ class DebuggerScript::GetPossibleBreakpointsMatcher {
       return false;
     }
     *result->addressOfValueForTranscode() = tmp;
+    if (!result->valid()) {
+      return false;
+    }
     return true;
   }
   bool parseSizeTValue(HandleValue value, size_t* result) {
@@ -867,9 +870,10 @@ class DebuggerScript::GetPossibleBreakpointsMatcher {
       }
 
       if (!parseColumnValue(minColumnValue, &minColumn)) {
-        JS_ReportErrorNumberASCII(
-            cx_, GetErrorMessage, nullptr, JSMSG_UNEXPECTED_TYPE,
-            "getPossibleBreakpoints' 'minColumn'", "not a positive integer");
+        JS_ReportErrorNumberASCII(cx_, GetErrorMessage, nullptr,
+                                  JSMSG_UNEXPECTED_TYPE,
+                                  "getPossibleBreakpoints' 'minColumn'",
+                                  "not a positive integer in valid range");
         return false;
       }
     }
@@ -893,9 +897,10 @@ class DebuggerScript::GetPossibleBreakpointsMatcher {
       }
 
       if (!parseColumnValue(maxColumnValue, &maxColumn)) {
-        JS_ReportErrorNumberASCII(
-            cx_, GetErrorMessage, nullptr, JSMSG_UNEXPECTED_TYPE,
-            "getPossibleBreakpoints' 'maxColumn'", "not a positive integer");
+        JS_ReportErrorNumberASCII(cx_, GetErrorMessage, nullptr,
+                                  JSMSG_UNEXPECTED_TYPE,
+                                  "getPossibleBreakpoints' 'maxColumn'",
+                                  "not a positive integer in valid range");
         return false;
       }
     }
