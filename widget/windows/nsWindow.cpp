67cdaa0c3fc73e7a06d4226b024817c66f1a422c
Emilio Cobos Álvarez <emilio@crisal.io>
2025-02-25 18:25:44 +0000

    Bug 1935894 - Pref out hidden taskbar window workarounds. r=win-reviewers,rkraesig
    This seems to fix the issue according to the bug comments (see
    comment 61 and comment 17).
    
    Put this behind a pref in case it causes worse regressions. Idea is to
    remove the code in a release or two.
    
    Differential Revision: https://phabricator.services.mozilla.com/D239425

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -2664,14 +2664,16 @@ bool nsWindow::UpdateNonClientMargins(bool aReflowWindow) {
     // position the edges "offscreen" for maximized windows.
     metrics.mOffset.top = metrics.mCaptionHeight;
 
-    if (mozilla::Maybe<UINT> maybeEdge = GetHiddenTaskbarEdge()) {
-      auto edge = maybeEdge.value();
-      if (ABE_LEFT == edge) {
-        metrics.mOffset.left -= kHiddenTaskbarSize;
-      } else if (ABE_RIGHT == edge) {
-        metrics.mOffset.right -= kHiddenTaskbarSize;
-      } else if (ABE_BOTTOM == edge || ABE_TOP == edge) {
-        metrics.mOffset.bottom -= kHiddenTaskbarSize;
+    if (StaticPrefs::widget_windows_hidden_taskbar_hack_size()) {
+      if (mozilla::Maybe<UINT> maybeEdge = GetHiddenTaskbarEdge()) {
+        auto edge = maybeEdge.value();
+        if (ABE_LEFT == edge) {
+          metrics.mOffset.left -= kHiddenTaskbarSize;
+        } else if (ABE_RIGHT == edge) {
+          metrics.mOffset.right -= kHiddenTaskbarSize;
+        } else if (ABE_BOTTOM == edge || ABE_TOP == edge) {
+          metrics.mOffset.bottom -= kHiddenTaskbarSize;
+        }
       }
     }
   } else if (mPIPWindow &&
@@ -2682,9 +2684,11 @@ bool nsWindow::UpdateNonClientMargins(bool aReflowWindow) {
   }
 
   UpdateOpaqueRegionInternal();
-  // We probably shouldn't need to clear the NC-area, but we need to in order
-  // to work around bug 642851.
-  mNeedsNCAreaClear = true;
+  if (StaticPrefs::widget_windows_hidden_taskbar_hack_paint()) {
+    // We probably shouldn't need to clear the NC-area, but we need to in
+    // order to work around bug 642851.
+    mNeedsNCAreaClear = true;
+  }
 
   if (aReflowWindow) {
     // Force a reflow of content based on the new client
