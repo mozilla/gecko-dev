a2ed45e494414a4a849aea0319dabebac386ff6f
Andrew Sutherland <asutherland@asutherland.org>
2025-02-26 22:21:51 +0000

    Bug 1837969 - StorageAllowedForDocument() shouldn't return ePartitionForeignOrDeny for top-level windows. r=anti-tracking-reviewers,timhuang
    This is a slight variation on :twisniewski's prototype fix.
    
    Differential Revision: https://phabricator.services.mozilla.com/D235399

--- toolkit/components/antitracking/StorageAccess.cpp
+++ toolkit/components/antitracking/StorageAccess.cpp
@@ -316,7 +316,10 @@ StorageAccess StorageAllowedForWindow(nsPIDOMWindowInner* aWindow,
 
 StorageAccess StorageAllowedForDocument(const Document* aDoc) {
   StorageAccess cookieAllowed = CookieAllowedForDocument(aDoc);
-  if (StaticPrefs::
+  // If this isn't a top-level window, check if we should apply the policy to
+  // always partition non-cookie storage.
+  if (!aDoc->IsTopLevelContentDocument() &&
+      StaticPrefs::
           privacy_partition_always_partition_third_party_non_cookie_storage() &&
       cookieAllowed > StorageAccess::eDeny) {
     return StorageAccess::ePartitionForeignOrDeny;
