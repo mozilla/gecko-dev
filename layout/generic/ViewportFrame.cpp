70c8f61f187af8122b52b91dc9b0206d4fb4f93d
Emilio Cobos Álvarez <emilio@crisal.io>
2025-02-26 12:29:47 +0000

    Bug 1922300 - View display list transition pseudo-tree in a pseudo-top layer. r=view-transitions-reviewers,boris
    On top of the regular top layer but under DevTools and other UA content.
    
    Differential Revision: https://phabricator.services.mozilla.com/D239378

--- layout/generic/ViewportFrame.cpp
+++ layout/generic/ViewportFrame.cpp
@@ -17,6 +17,7 @@
 #include "mozilla/RestyleManager.h"
 #include "mozilla/ScrollContainerFrame.h"
 #include "nsGkAtoms.h"
+#include "mozilla/dom/ViewTransition.h"
 #include "nsAbsoluteContainingBlock.h"
 #include "nsCanvasFrame.h"
 #include "nsLayoutUtils.h"
@@ -163,7 +164,9 @@ nsDisplayWrapList* ViewportFrame::BuildDisplayListForTopLayer(
     nsDisplayListBuilder* aBuilder, bool* aIsOpaque) {
   nsDisplayList topLayerList(aBuilder);
 
-  nsTArray<dom::Element*> topLayer = PresContext()->Document()->GetTopLayer();
+  auto* doc = PresContext()->Document();
+
+  nsTArray<dom::Element*> topLayer = doc->GetTopLayer();
   for (dom::Element* elem : topLayer) {
     nsIFrame* frame = elem->GetPrimaryFrame();
     if (!frame) {
@@ -213,6 +216,17 @@ nsDisplayWrapList* ViewportFrame::BuildDisplayListForTopLayer(
       }
     }
     BuildDisplayListForTopLayerFrame(aBuilder, frame, &topLayerList);
+  }
+
+  if (dom::ViewTransition* vt = doc->GetActiveViewTransition()) {
+    if (dom::Element* root = vt->GetRoot()) {
+      if (nsIFrame* frame = root->GetPrimaryFrame()) {
+        MOZ_ASSERT(frame->StyleDisplay()->mTopLayer != StyleTopLayer::None,
+                   "ua.css should ensure this");
+        MOZ_ASSERT(frame->HasAnyStateBits(NS_FRAME_OUT_OF_FLOW));
+        BuildDisplayListForTopLayerFrame(aBuilder, frame, &topLayerList);
+      }
+    }
   }
 
   if (nsCanvasFrame* canvasFrame = PresShell()->GetCanvasFrame()) {
