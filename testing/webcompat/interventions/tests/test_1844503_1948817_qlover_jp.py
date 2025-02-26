48ac51e64998c74d814539451716646bf188d8d7
Thomas Wisniewski <twisniewski@mozilla.com>
2025-02-26 18:08:03 +0000

    Bug 1948817 - add qlover.jp to our webcompat intervention for nicochannel sites, and fix that intervention; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D239765

---
+++ testing/webcompat/interventions/tests/test_1844503_1948817_qlover_jp.py
@@ -0,0 +1,15 @@
+import pytest
+
+URL = "https://qlover.jp/limosiru/video/sm6cfPqfj3dzX3GVT97YHQxG"
+
+
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.test_nicochannel_like_site(URL, shouldPass=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.test_nicochannel_like_site(URL, shouldPass=False)
