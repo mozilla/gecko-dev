1319ffe77714ab93a9e4c4836962c496a3c679e5
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2025-02-27 17:07:04 +0000

    Bug 1853108 - Add testing of interesting crash nightly only enabling r=afranchuk,gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D226876

---
+++ toolkit/components/crashes/tests/xpcshell/test_remote_settings_crash_pull_nightly.js
@@ -0,0 +1,75 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* const { AppConstants } = ChromeUtils.importESModule(
+  "resource://gre/modules/AppConstants.sys.mjs"
+); */
+
+const { RemoteSettingsCrashPull } = ChromeUtils.importESModule(
+  "resource://gre/modules/RemoteSettingsCrashPull.sys.mjs"
+);
+
+add_setup(async function setup_test() {
+  do_get_profile();
+});
+
+add_task(function test_pref_value() {
+  const prefValue = Services.prefs.getBoolPref(
+    "browser.crashReports.crashPull"
+  );
+  // Disabled by default until further notice
+  /* if (AppConstants.NIGHTLY_BUILD) {
+    Assert.ok(prefValue, "RemoteSettingsCrashPull pref enabled on nightly");
+  } else { */
+  Assert.ok(!prefValue, "RemoteSettingsCrashPull pref disabled on nightly");
+  /* } */
+});
+
+add_task(function test_pref_disabled() {
+  const originalPref = Services.prefs.getBoolPref(
+    "browser.crashReports.crashPull"
+  );
+  Services.prefs.setBoolPref("browser.crashReports.crashPull", false);
+
+  const originalCollection = RemoteSettingsCrashPull.collection;
+  let collectionCalled = false;
+  RemoteSettingsCrashPull.collection = function () {
+    console.debug("Calling collection()");
+    collectionCalled = true;
+  };
+
+  RemoteSettingsCrashPull.start();
+
+  Assert.ok(RemoteSettingsCrashPull, "RemoteSettingsCrashPull obtained.");
+  Assert.ok(
+    !collectionCalled,
+    "Method collection() should not have been called"
+  );
+
+  RemoteSettingsCrashPull.collection = originalCollection;
+  Services.prefs.setBoolPref("browser.crashReports.crashPull", originalPref);
+});
+
+add_task(function test_pref_enabled() {
+  const originalPref = Services.prefs.getBoolPref(
+    "browser.crashReports.crashPull"
+  );
+  Services.prefs.setBoolPref("browser.crashReports.crashPull", true);
+
+  const originalCollection = RemoteSettingsCrashPull.collection;
+  let collectionCalled = false;
+  RemoteSettingsCrashPull.collection = function () {
+    console.debug("Calling collection()");
+    collectionCalled = true;
+  };
+
+  RemoteSettingsCrashPull.start();
+
+  Assert.ok(RemoteSettingsCrashPull, "RemoteSettingsCrashPull obtained.");
+  Assert.ok(collectionCalled, "Method collection() should have been called");
+
+  RemoteSettingsCrashPull.collection = originalCollection;
+  Services.prefs.setBoolPref("browser.crashReports.crashPull", originalPref);
+});
