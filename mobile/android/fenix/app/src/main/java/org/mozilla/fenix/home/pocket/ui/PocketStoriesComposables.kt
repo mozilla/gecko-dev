48ee8fdd06f1573828c984f75ed95995613aed9c
Gabriel Luong <gabriel.luong@gmail.com>
2025-02-27 22:09:41 +0000

    Bug 1950122 - Record pocket.home_recs_spoc_shown telemetry event when a MARS sponsored content is shown r=android-reviewers,devota, a=dmeehan
    - Apply `Modifier.onShown` around `SponsoredContent` composable to invoke `onStoryShown` which will record the pocket.home_recs_spoc_shown telemetry event.
    
    Differential Revision: https://phabricator.services.mozilla.com/D239953

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/pocket/ui/PocketStoriesComposables.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/pocket/ui/PocketStoriesComposables.kt
@@ -363,7 +363,7 @@ fun ContentRecommendation(
  * @param onDiscoverMoreClicked Callback for when the user taps an element which contains an
  */
 @OptIn(ExperimentalComposeUiApi::class)
-@Suppress("LongMethod")
+@Suppress("CyclomaticComplexMethod", "LongMethod")
 @Composable
 fun PocketStories(
     @PreviewParameter(PocketStoryProvider::class) stories: List<PocketStory>,
@@ -495,14 +495,44 @@ fun PocketStories(
                             }
 
                             is SponsoredContent -> {
-                                SponsoredContent(
-                                    sponsoredContent = story,
-                                    backgroundColor = backgroundColor,
+                                val screenBounds = Rect()
+                                    .apply { LocalView.current.getWindowVisibleDisplayFrame(this) }
+                                    .apply {
+                                        // Check if this is in a preview because `settings()` breaks previews
+                                        if (!inComposePreview) {
+                                            val verticalOffset = LocalContext.current.resources.getDimensionPixelSize(
+                                                R.dimen.browser_toolbar_height,
+                                            )
+
+                                            if (LocalContext.current.settings().shouldUseBottomToolbar) {
+                                                bottom -= verticalOffset
+                                            } else {
+                                                top += verticalOffset
+                                            }
+                                        }
+                                    }
+
+                                Box(
+                                    modifier = Modifier.onShown(
+                                        threshold = 0.5f,
+                                        onVisible = {
+                                            onStoryShown(
+                                                story,
+                                                Triple(rowIndex, columnIndex, stories.indexOf(story)),
+                                            )
+                                        },
+                                        screenBounds = screenBounds,
+                                    ),
                                 ) {
-                                    onStoryClicked(
-                                        story,
-                                        Triple(rowIndex, columnIndex, stories.indexOf(story)),
-                                    )
+                                    SponsoredContent(
+                                        sponsoredContent = story,
+                                        backgroundColor = backgroundColor,
+                                    ) {
+                                        onStoryClicked(
+                                            story,
+                                            Triple(rowIndex, columnIndex, stories.indexOf(story)),
+                                        )
+                                    }
                                 }
                             }
                         }
