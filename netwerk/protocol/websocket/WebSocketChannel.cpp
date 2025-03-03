3c82fef6f074f6fc2b5d660c41086aaec0159450
Valentin Gosu <valentin.gosu@gmail.com>
2025-03-03 09:16:21 +0000

    Bug 1948605 - Refactor websocket code to avoid GCC warning r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D239179

--- netwerk/protocol/websocket/WebSocketChannel.cpp
+++ netwerk/protocol/websocket/WebSocketChannel.cpp
@@ -1436,15 +1436,21 @@ bool WebSocketChannel::UpdateReadBuffer(uint8_t* buffer, uint32_t count,
     mFramePtr = mBuffer + accumulatedFragments;
   } else {
     // existing buffer is not sufficient, extend it
-    mBufferSize += count + 8192 + mBufferSize / 3;
-    LOG(("WebSocketChannel: update read buffer extended to %u\n", mBufferSize));
-    uint8_t* old = mBuffer;
-    mBuffer = (uint8_t*)realloc(mBuffer, mBufferSize);
-    if (!mBuffer) {
-      mBuffer = old;
+    uint32_t newBufferSize = mBufferSize;
+    newBufferSize += count + 8192 + mBufferSize / 3;
+    ptrdiff_t frameIndex = mFramePtr - mBuffer;
+    LOG(("WebSocketChannel: update read buffer extended to %u\n",
+         newBufferSize));
+    uint8_t* newBuffer = (uint8_t*)realloc(mBuffer, newBufferSize);
+    if (!newBuffer) {
+      // Reallocation failed.
       return false;
     }
-    mFramePtr = mBuffer + (mFramePtr - old);
+    mBuffer = newBuffer;
+    mBufferSize = newBufferSize;
+
+    // mBuffer was reallocated, so we need to update mFramePtr
+    mFramePtr = mBuffer + frameIndex;
   }
 
   ::memcpy(mBuffer + mBuffered, buffer, count);
