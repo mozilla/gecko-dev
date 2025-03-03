7376e73b219aa8882ecc78d0247dbe0afd4bbdc0
Sandor Molnar <smolnar@mozilla.com>
2025-03-03 13:05:40 +0200

    Backed out changeset 70a25f7b9f48 (bug 1928109) for causing bc failures @ browser_address_doorhanger_ui.js CLOSED TREE

--- toolkit/components/formautofill/shared/FormAutofillUtils.sys.mjs
+++ toolkit/components/formautofill/shared/FormAutofillUtils.sys.mjs
@@ -802,7 +802,6 @@ FormAutofillUtils = {
         sub_keys: subKeys,
         sub_names: subNames,
         sub_lnames: subLnames,
-        sub_isoids: subIsoids,
       } = metadata;
       if (!subKeys) {
         // Not all regions have sub_keys. e.g. DE
@@ -816,7 +815,6 @@ FormAutofillUtils = {
         let identifiedValue = this.identifyValue(
           subKeys,
           subNames,
-          subIsoids,
           val,
           collators
         );
@@ -898,7 +896,6 @@ FormAutofillUtils = {
           }
           // Apply sub_lnames if sub_names does not exist
           let names = dataset.sub_names || dataset.sub_lnames;
-          let isoids = dataset.sub_isoids;
 
           // Go through options one by one to find a match.
           // Also check if any option contain the address-level1 key.
@@ -910,14 +907,12 @@ FormAutofillUtils = {
             let optionValue = this.identifyValue(
               keys,
               names,
-              isoids,
               option.value,
               collators
             );
             let optionText = this.identifyValue(
               keys,
               names,
-              isoids,
               option.text,
               collators,
               true
@@ -1076,13 +1071,12 @@ FormAutofillUtils = {
    *
    * @param   {Array<string>} keys
    * @param   {Array<string>} names
-   * @param   {Array<string>} isoids
    * @param   {string} value
    * @param   {Array} collators
    * @param   {bool} inexactMatch
    * @returns {string}
    */
-  identifyValue(keys, names, isoids, value, collators, inexactMatch = false) {
+  identifyValue(keys, names, value, collators, inexactMatch = false) {
     let resultKey = keys.find(key => this.strCompare(value, key, collators));
     if (resultKey) {
       return resultKey;
@@ -1093,15 +1087,11 @@ FormAutofillUtils = {
         ? this.strInclude(value, name, collators)
         : this.strCompare(value, name, collators)
     );
-    if (index === -1) {
-      index = isoids.findIndex(isoid =>
-        inexactMatch
-          ? this.strInclude(value, isoid, collators)
-          : this.strCompare(value, isoid, collators)
-      );
+    if (index !== -1) {
+      return keys[index];
     }
 
-    return index !== -1 ? keys[index] : null;
+    return null;
   },
 
   /**
