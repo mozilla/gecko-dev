f2f607e9c5fb22fb90c32ebd4251775f145fa43d
Valentin Gosu <valentin.gosu@gmail.com>
2025-03-03 09:16:22 +0000

    Bug 1948606 - Ignore GCC warning r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D239206

--- netwerk/streamconv/converters/nsHTTPCompressConv.cpp
+++ netwerk/streamconv/converters/nsHTTPCompressConv.cpp
@@ -509,12 +509,17 @@ nsHTTPCompressConv::OnDataAvailable(nsIRequest* request, nsIInputStream* iStr,
       [[fallthrough]];
 
     case HTTP_COMPRESS_DEFLATE:
-
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wuse-after-free"
+      // The return value of realloc is null in case of failure, and the old
+      // buffer will stay valid but GCC isn't smart enough to figure that out.
+      // See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=110501
       if (mInpBuffer != nullptr && streamLen > mInpBufferLen) {
         unsigned char* originalInpBuffer = mInpBuffer;
         if (!(mInpBuffer = (unsigned char*)realloc(
-                  originalInpBuffer, mInpBufferLen = streamLen))) {
+                  mInpBuffer, mInpBufferLen = streamLen))) {
           free(originalInpBuffer);
+          mInpBufferLen = 0;
         }
 
         if (mOutBufferLen < streamLen * 2) {
@@ -522,9 +527,10 @@ nsHTTPCompressConv::OnDataAvailable(nsIRequest* request, nsIInputStream* iStr,
           if (!(mOutBuffer = (unsigned char*)realloc(
                     mOutBuffer, mOutBufferLen = streamLen * 3))) {
             free(originalOutBuffer);
+            mOutBufferLen = 0;
           }
         }
-
+#pragma GCC diagnostic pop
         if (mInpBuffer == nullptr || mOutBuffer == nullptr) {
           return NS_ERROR_OUT_OF_MEMORY;
         }
