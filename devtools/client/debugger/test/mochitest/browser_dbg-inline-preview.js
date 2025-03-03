ec8008935a8b622b14f9cb316c19edfa7358a202
Nicolas Chevobbe <nchevobbe@mozilla.com>
2025-03-03 12:35:07 +0000

    Bug 1951341 - [devtools] Add Debugger support for scripts using import attributes. r=devtools-reviewers,ochameau.
    We need to add the @babel/plugin-syntax-import-attributes plugin to make sure
    the outline panel and inline previews are working when using import attributes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D240122

--- devtools/client/debugger/test/mochitest/browser_dbg-inline-preview.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-inline-preview.js
@@ -8,6 +8,7 @@
 
 add_task(async function testInlinePreviews() {
   await pushPref("devtools.debugger.features.inline-preview", true);
+  await pushPref("javascript.options.experimental.import_attributes", true);
 
   const dbg = await initDebugger(
     "doc-inline-preview.html",
@@ -52,6 +53,7 @@ add_task(async function testInlinePreviews() {
   // Check inline previews for values within a module script
   await checkInlinePreview(dbg, "runInModule", [
     { identifier: "val:", value: "4" },
+    { identifier: "ids:", value: "Array [ 1, 2 ]" },
   ]);
 
   // Checks that open in inspector button works in inline preview
