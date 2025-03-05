e44e0b83f464de9ab7c1c6e80b65c15f337b628f
Julian Descottes <jdescottes@mozilla.com>
2025-03-05 09:35:21 +0000

    Bug 1946860 - [devtools] Update browser_net_error-boundary-01 to trigger error boundary reliably r=devtools-reviewers,nchevobbe
    The current reducer update would only trigger a component error during a waterfall redraw,
    which might not happen if the request list is empty (which seems to randomly occur because
    the state is damaged).
    
    The new version tries to clearly explain what we attempt to break and only performs a request
    instead of fully reloading the page.
    
    Differential Revision: https://phabricator.services.mozilla.com/D240448

--- devtools/client/netmonitor/test/browser_net_error-boundary-01.js
+++ devtools/client/netmonitor/test/browser_net_error-boundary-01.js
@@ -6,6 +6,7 @@
  * Test that top-level net monitor error boundary catches child errors.
  */
 add_task(async function () {
+  await pushPref("devtools.netmonitor.persistlog", true);
   const { monitor } = await initNetMonitor(SIMPLE_URL, {
     requestCount: 1,
   });
@@ -17,9 +18,20 @@ add_task(async function () {
 
   // Intentionally damage the store to cause a child component error
   const state = store.getState();
-  state.ui = "foo";
 
-  await reloadBrowser();
+  // Do NOT nullify state.ui as it is used by the App.js component which is not
+  // wrapped in the AppErrorBoundary component.
+  // Do NOT nullify state.requests as it will just bypass rendering the requests
+  // list.
+  // requestBlocking should make the RequestListContent.js component throw when
+  // rendering the new request sent right after.
+  // In general, this test is very much linked to the specific implementation of
+  // the components and might break if said implementation changes.
+  state.requestBlocking = null;
+
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [SIMPLE_URL], url => {
+    content.fetch(url);
+  });
 
   // Wait for the panel to fall back to the error UI
   const errorPanel = await waitUntil(() =>
