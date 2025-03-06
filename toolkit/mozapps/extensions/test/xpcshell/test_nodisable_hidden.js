c1dbb90382269386ba407d1cdfaa2ac600defba5
Luca Greco <lgreco@mozilla.com>
2025-03-06 16:27:14 +0000

    Bug 1948908 - Cover system builtin location in test_nodisable_hidden.js. r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D238673

--- toolkit/mozapps/extensions/test/xpcshell/test_nodisable_hidden.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_nodisable_hidden.js
@@ -9,6 +9,10 @@ const distroDir = FileUtils.getDir("ProfD", ["sysfeatures"]);
 distroDir.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
 registerDirectory("XREAppFeat", distroDir);
 
+// Enable SCOPE_APPLICATION for builtin testing.  Default in tests is only SCOPE_PROFILE.
+let scopes = AddonManager.SCOPE_PROFILE | AddonManager.SCOPE_APPLICATION;
+Services.prefs.setIntPref("extensions.enabledScopes", scopes);
+
 const NORMAL_ID = "normal@tests.mozilla.org";
 const SYSTEM_ID = "system@tests.mozilla.org";
 
@@ -59,8 +63,9 @@ add_task(async function () {
   await promiseShutdownManager();
 });
 
-// system add-ons can never be user disabled.
-add_task(async function () {
+// system add-ons installed in the app-system-defaults directory location can never be user disabled.
+// TODO(Bug 1949847): remove this test along with removing the app-system-defaults location.
+add_task(async function test_legacy_system_defaults_location() {
   let xpi = createTempWebExtensionFile({
     manifest: {
       name: "Test disabling hidden add-ons, hidden system add-on case.",
@@ -70,6 +75,57 @@ add_task(async function () {
   });
   xpi.copyTo(distroDir, `${SYSTEM_ID}.xpi`);
   await overrideBuiltIns({ system: [SYSTEM_ID] });
+
+  await promiseStartupManager();
+
+  let addon = await promiseAddonByID(SYSTEM_ID);
+  Assert.notEqual(addon, null);
+  Assert.equal(addon.version, "1.0");
+  Assert.equal(
+    addon.name,
+    "Test disabling hidden add-ons, hidden system add-on case."
+  );
+  Assert.ok(addon.isCompatible);
+  Assert.ok(!addon.appDisabled);
+  Assert.ok(!addon.userDisabled);
+  Assert.ok(addon.isActive);
+  Assert.equal(addon.type, "extension");
+
+  // system add-ons cannot be disabled by the user.
+  await Assert.rejects(
+    addon.disable(),
+    err => err.message == `Cannot disable system add-on ${SYSTEM_ID}`,
+    "disable() on a hidden add-on should fail"
+  );
+
+  Assert.ok(!addon.userDisabled);
+  Assert.ok(addon.isActive);
+
+  await promiseShutdownManager();
+});
+
+// system add-ons installed in the system builtin location can never be user disabled.
+add_task(async function test_legacy_system_defaults_builtin_location() {
+  const addon_res_url_path = "test-builtin-systemaddon";
+  await setupBuiltinExtension(
+    {
+      manifest: {
+        name: "Test disabling hidden add-ons, hidden system add-on case.",
+        version: "1.0",
+        browser_specific_settings: { gecko: { id: SYSTEM_ID } },
+      },
+    },
+    addon_res_url_path
+  );
+  await overrideBuiltIns({
+    builtins: [
+      {
+        addon_id: SYSTEM_ID,
+        addon_version: "1.0",
+        res_url: `resource://${addon_res_url_path}/`,
+      },
+    ],
+  });
 
   await promiseStartupManager();
 
