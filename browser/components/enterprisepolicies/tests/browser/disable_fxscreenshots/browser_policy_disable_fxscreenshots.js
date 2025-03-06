cd4d862292acd64d632736a9dedebec8b9c1cfd7
Luca Greco <lgreco@mozilla.com>
2025-03-06 16:24:03 +0000

    Bug 1948364 - part 4: adjust Enterprise Policies DisableFirefoxScreenshots setting mochitest. r=mkaply
    Differential Revision: https://phabricator.services.mozilla.com/D238316

--- browser/components/enterprisepolicies/tests/browser/disable_fxscreenshots/browser_policy_disable_fxscreenshots.js
+++ browser/components/enterprisepolicies/tests/browser/disable_fxscreenshots/browser_policy_disable_fxscreenshots.js
@@ -3,19 +3,35 @@
 
 "use strict";
 
+ChromeUtils.defineESModuleGetters(this, {
+  ScreenshotsUtils: "resource:///modules/ScreenshotsUtils.sys.mjs",
+});
+
 const PREF_DISABLE_FX_SCREENSHOTS = "extensions.screenshots.disabled";
 
 async function checkScreenshots(shouldBeEnabled) {
-  return BrowserTestUtils.waitForCondition(() => {
-    return (
-      !!PageActions.actionForID("screenshots_mozilla_org") == shouldBeEnabled
-    );
-  }, "Expecting screenshots to be " + shouldBeEnabled);
+  let menu = document.getElementById("contentAreaContextMenu");
+  let popupshown = BrowserTestUtils.waitForPopupEvent(menu, "shown");
+  EventUtils.synthesizeMouseAtCenter(document.body, {
+    type: "contextmenu",
+  });
+  await popupshown;
+  Assert.equal(menu.state, "open", "Context menu is open");
+
+  is(
+    menu.querySelector("#context-take-screenshot").hidden,
+    !shouldBeEnabled,
+    `Screenshots context menu item should be ${shouldBeEnabled ? "shown" : "hidden"}`
+  );
+
+  let popuphidden = BrowserTestUtils.waitForPopupEvent(menu, "hidden");
+  menu.hidePopup();
+  await popuphidden;
 }
 
 add_task(async function test_disable_firefox_screenshots() {
   // Dynamically toggling the PREF_DISABLE_FX_SCREENSHOTS is very finicky, because
-  // that pref is being watched, and it makes the Firefox Screenshots system add-on
+  // that pref is being watched, and it makes the Firefox Screenshots component
   // to start or stop, causing intermittency.
   //
   // Firefox Screenshots is disabled by default on tests (in
@@ -27,6 +43,15 @@ add_task(async function test_disable_firefox_screenshots() {
     Services.prefs.getBoolPref(PREF_DISABLE_FX_SCREENSHOTS),
     true,
     "Screenshots pref is disabled"
+  );
+
+  await BrowserTestUtils.waitForCondition(
+    () => !ScreenshotsUtils.initialized,
+    "Wait for the screenshot component to be uninitialized"
+  );
+  ok(
+    !ScreenshotsUtils.initialized,
+    "The screenshot component is uninitialized"
   );
 
   await BrowserTestUtils.withNewTab("data:text/html,Test", async function () {
