833a3cb541e579c4c1fcbbd609b6078fbcba3d98
Butkovits Atila <abutkovits@mozilla.com>
2025-03-06 13:53:35 +0200

    Backed out changeset 79e3d8d9befa (bug 1949808) for causing failures at browser_tab_grouping_telemetry.js. CLOSED TREE

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -99,6 +99,7 @@
       this.tabContainer = document.getElementById("tabbrowser-tabs");
       this.tabGroupMenu = document.getElementById("tab-group-editor");
       this.tabbox = document.getElementById("tabbrowser-tabbox");
+      this.tabGroupNameField = document.getElementById("tab-group-name");
       this.tabpanels = document.getElementById("tabbrowser-tabpanels");
       this.verticalPinnedTabsContainer = document.getElementById(
         "vertical-pinned-tabs-container"
@@ -167,6 +168,11 @@
         "security.notification_enable_delay",
         500
       );
+      XPCOMUtils.defineLazyPreferenceGetter(
+        this,
+        "_smartTabGroupsEnabled",
+        "browser.tabs.groups.smart.enabled"
+      );
 
       if (AppConstants.MOZ_CRASHREPORTER) {
         ChromeUtils.defineESModuleGetters(this, {
@@ -183,6 +189,7 @@
       window.addEventListener("activate", this);
       window.addEventListener("deactivate", this);
       window.addEventListener("TabGroupCreate", this);
+      window.addEventListener("MlLabelCreate", this);
 
       this.tabContainer.init();
       this._setupInitialBrowserAndTab();
@@ -2953,6 +2960,21 @@
       if (!group.tabs.length) {
         group.remove();
         return null;
+      }
+
+      if (this._smartTabGroupsEnabled) {
+        gBrowser.getGroupTitleForTabs(tabs).then(newLabel => {
+          group.label = newLabel;
+          if (this.tabGroupMenu.panel.state !== "closed") {
+            this.tabGroupNameField.value = newLabel;
+            group.dispatchEvent(
+              new CustomEvent("MlLabelCreate", {
+                bubbles: true,
+                detail: { mlLabel: newLabel },
+              })
+            );
+          }
+        });
       }
 
       group.dispatchEvent(
@@ -6746,6 +6768,11 @@
         case "TabGroupCreate":
           if (aEvent.detail.isUserCreated) {
             this.tabGroupMenu.openCreateModal(aEvent.target);
+          }
+          break;
+        case "MlLabelCreate":
+          if (aEvent.detail.mlLabel) {
+            this.tabGroupMenu.mlLabel = aEvent.detail.mlLabel;
           }
           break;
         case "activate":
