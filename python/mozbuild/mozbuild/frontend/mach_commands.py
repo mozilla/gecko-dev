36aa1a474d797e859b53c2b82de25a81c5b0ab19
Joel Maher <jmaher@mozilla.com>
2025-03-07 14:56:01 +0000

    Bug 1950648 - Adjust mach file-info bugzilla-components to work on windows. r=marco
    Differential Revision: https://phabricator.services.mozilla.com/D240705

--- python/mozbuild/mozbuild/frontend/mach_commands.py
+++ python/mozbuild/mozbuild/frontend/mach_commands.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 
 import mozpack.path as mozpath
 from mach.decorators import Command, CommandArgument, SubCommand
+from mozpack.files import FileFinder
 
 TOPSRCDIR = os.path.abspath(os.path.join(__file__, "../../../../../"))
 
@@ -281,12 +282,14 @@ def _get_files_info(command_context, paths, rev=None):
 
     # Normalize to relative from topsrcdir.
     relpaths = []
-    for p in paths:
-        a = mozpath.abspath(p)
-        if not mozpath.basedir(a, [command_context.topsrcdir]):
-            raise InvalidPathException("path is outside topsrcdir: %s" % p)
+    finder = FileFinder(command_context.topsrcdir)
+    for path in paths:
+        for p, _ in finder.find(path):
+            a = mozpath.abspath(p)
+            if not mozpath.basedir(a, [command_context.topsrcdir]):
+                raise InvalidPathException("path is outside topsrcdir: %s" % p)
 
-        relpaths.append(mozpath.relpath(a, command_context.topsrcdir))
+            relpaths.append(mozpath.relpath(a, command_context.topsrcdir))
 
     # Expand wildcards.
     # One variable is for ordering. The other for membership tests.
