f40a91b1d71d3184aac7abb48a054995488b5cdb
Nicolas Silva <nsilva@mozilla.com>
2025-03-10 13:20:58 +0000

    Bug 1952863 - Ensure non-requested frames go through the normal present path. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D239904

--- gfx/wr/webrender/src/render_backend.rs
+++ gfx/wr/webrender/src/render_backend.rs
@@ -1375,7 +1375,7 @@ impl RenderBackend {
         mut frame_ops: Vec<FrameMsg>,
         mut notifications: Vec<NotificationRequest>,
         mut render_frame: bool,
-        present: bool,
+        mut present: bool,
         render_reasons: RenderReasons,
         generated_frame_id: Option<u64>,
         invalidate_rendered_frame: bool,
@@ -1454,6 +1454,14 @@ impl RenderBackend {
         }
 
         if build_frame {
+            if !requested_frame {
+                // When we don't request a frame, present defaults to false. If for some
+                // reason we did not request the frame but must render it anyway, set
+                // present to true (it was false as a byproduct of expecting we wouldn't
+                // produce the frame but we did not explicitly opt out of it).
+                present = true;
+            }
+
             if start_time.is_some() {
               Telemetry::record_time_to_frame_build(Duration::from_nanos(precise_time_ns() - start_time.unwrap()));
             }
