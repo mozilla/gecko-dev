421d159652b865785ced390ece0fb3129ae6aa8b
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2025-03-10 14:05:19 +0000

    Bug 1951212 - Fix register size in convertFloat32ToInt32. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D240199

--- js/src/jit/arm64/MacroAssembler-arm64.h
+++ js/src/jit/arm64/MacroAssembler-arm64.h
@@ -620,9 +620,9 @@ class MacroAssemblerCompat : public vixl::MacroAssembler {
       Cbnz(dest32, &nonzero);
       // dest32 is 0, fail if fsrc64 is negative.
       Fmov(dest32, fsrc);
-      Cmp(dest32, xzr);
+      Cmp(dest32, wzr);
       B(fail, Assembler::Signed);
-      Mov(dest32, xzr);
+      Mov(dest32, wzr);
       bind(&nonzero);
     }
     Uxtw(dest64, dest64);
