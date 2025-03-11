eb4d4fc5575a3298bc9e151359141fef6548da77
Nico Burns <nico@nicoburns.com>
2025-03-11 11:52:25 +0000

    Bug 1952776: impl SubAssign for CSSPixelLength (r=emilio)
    Signed-off-by: Nico Burns <nico@nicoburns.com>
    
    Differential Revision: https://phabricator.services.mozilla.com/D240806

--- servo/components/style/values/computed/length.rs
+++ servo/components/style/values/computed/length.rs
@@ -19,7 +19,7 @@ use crate::values::{specified, CSSFloat};
 use crate::Zero;
 use app_units::Au;
 use std::fmt::{self, Write};
-use std::ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub};
+use std::ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub, SubAssign};
 use style_traits::{CSSPixel, CssWriter, ToCss};
 
 pub use super::image::Image;
@@ -456,6 +456,13 @@ impl Sub for CSSPixelLength {
         Self::new(self.px() - other.px())
     }
 }
+
+impl SubAssign for CSSPixelLength {
+     #[inline]
+     fn sub_assign(&mut self, other: Self) {
+         self.0 -= other.0;
+     }
+ }
 
 impl From<CSSPixelLength> for Au {
     #[inline]
