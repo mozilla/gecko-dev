4ae17b47b602a0966a3d9e8318cba02796e30ba8
ffxbld <ffxbld@mozilla.com>
2025-03-11 09:32:24 +0000

    No Bug, mozilla-beta repo-update HSTS HPKP tld-suffixes ct-logs -  a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D240849

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1747307884000000);
+static const PRTime kCTExpirationTime = INT64_C(1747657856000000);
 
 namespace mozilla::ct {
 
