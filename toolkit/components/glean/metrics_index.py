edaf26b481c49d12823b46b6867dba23c5a6dd48
Jan-Erik Rediger <jrediger@mozilla.com>
2025-03-11 15:21:31 +0000

    Bug 1934321 - Ensure the background-task process only registers pings it needs r=TravisLong
    The background task process initializes Glean with the `firefox.desktop.background.tasks` app ID,
    but because this one was not known in `register_pings` it initialized
    ALL pings.
    Most of which will _never_ be sent by the background task process.
    
    Making the app ID known reduces the number of registered pings and
    should stop accidentally sending any other.
    
    Differential Revision: https://phabricator.services.mozilla.com/D241044

--- toolkit/components/glean/metrics_index.py
+++ toolkit/components/glean/metrics_index.py
@@ -242,6 +242,7 @@ pings_by_app_or_lib_id = {
     "firefox.desktop.background.update": gecko_pings
     + background_update_pings
     + test_pings,
+    "firefox.desktop.background.tasks": gecko_pings + background_tasks_pings,
     # Not an _actual_ app ID in use,
     # but needed to differentiate ping registration in geckoview builds
     "gecko": gecko_pings + test_pings,
