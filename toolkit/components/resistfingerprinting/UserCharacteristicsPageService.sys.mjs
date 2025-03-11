feb8d2628e4049454e2e635ac032841bd3fea561
Fatih Kilic <fkilic@mozilla.com>
2025-03-11 20:10:24 +0000

    Bug 1953226 - Use Date.UTC instead of new Date when parsing buildId into buildDate. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D241070

--- toolkit/components/resistfingerprinting/UserCharacteristicsPageService.sys.mjs
+++ toolkit/components/resistfingerprinting/UserCharacteristicsPageService.sys.mjs
@@ -728,14 +728,14 @@ export class UserCharacteristicsPageService {
   async populateClientInfo() {
     const buildID = Services.appinfo.appBuildID;
     const buildDate =
-      new Date(
+      Date.UTC(
         buildID.slice(0, 4),
         buildID.slice(4, 6) - 1,
         buildID.slice(6, 8),
         buildID.slice(8, 10),
         buildID.slice(10, 12),
         buildID.slice(12, 14)
-      ).getTime() / 1000;
+      ) / 1000;
 
     Glean.characteristics.version.set(Services.appinfo.version);
     Glean.characteristics.channel.set(AppConstants.MOZ_UPDATE_CHANNEL);
--- toolkit/components/resistfingerprinting/nsUserCharacteristics.cpp
+++ toolkit/components/resistfingerprinting/nsUserCharacteristics.cpp
@@ -681,7 +681,7 @@ const RefPtr<PopulatePromise>& TimoutPromise(
 // metric is set, this variable should be incremented. It'll be a lot. It's
 // okay. We're going to need it to know (including during development) what is
 // the source of the data we are looking at.
-const int kSubmissionSchema = 21;
+const int kSubmissionSchema = 22;
 
 const auto* const kUUIDPref =
     "toolkit.telemetry.user_characteristics_ping.uuid";
