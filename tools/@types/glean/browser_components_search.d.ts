55355f6473e0ec71279603fd0076d6881d7b3b3f
Mark Banner <standard8@mozilla.com>
2025-03-11 20:54:10 +0000

    Bug 1938929 - Add TypeScript definitions for browser/components/search/metrics.yaml. r=search-reviewers,scunnane
    Differential Revision: https://phabricator.services.mozilla.com/D240623

---
+++ tools/@types/glean/browser_components_search.d.ts
@@ -0,0 +1,112 @@
+/**
+ * NOTE: Do not modify this file by hand.
+ * Content was generated from source metrics.yaml files.
+ * If you're updating some of the sources, see README for instructions.
+ */
+
+interface GleanImpl {
+
+  newtabSearch: {
+    issued: GleanEvent;
+  }
+
+  newtabSearchAd: {
+    impression: GleanEvent;
+    click: GleanEvent;
+  }
+
+  serp: {
+    impression: GleanEvent;
+    engagement: GleanEvent;
+    adImpression: GleanEvent;
+    abandonment: GleanEvent;
+    categorizationDuration: GleanTimingDistribution;
+    categorization: GleanEvent;
+    adsBlockedCount: Record<string, GleanCounter>;
+    experimentInfo: GleanObject;
+    categorizationNoMapFound: GleanCounter;
+  }
+
+  searchWith: {
+    reportingUrl: GleanUrl;
+    contextId: GleanUuid;
+  }
+
+  browserEngagementNavigation: {
+    urlbar: Record<string, GleanCounter>;
+    urlbarHandoff: Record<string, GleanCounter>;
+    urlbarPersisted: Record<string, GleanCounter>;
+    urlbarSearchmode: Record<string, GleanCounter>;
+    searchbar: Record<string, GleanCounter>;
+    aboutHome: Record<string, GleanCounter>;
+    aboutNewtab: Record<string, GleanCounter>;
+    contextmenu: Record<string, GleanCounter>;
+    webextension: Record<string, GleanCounter>;
+  }
+
+  browserSearchContent: {
+    urlbar: Record<string, GleanCounter>;
+    urlbarHandoff: Record<string, GleanCounter>;
+    urlbarPersisted: Record<string, GleanCounter>;
+    urlbarSearchmode: Record<string, GleanCounter>;
+    searchbar: Record<string, GleanCounter>;
+    aboutHome: Record<string, GleanCounter>;
+    aboutNewtab: Record<string, GleanCounter>;
+    contextmenu: Record<string, GleanCounter>;
+    webextension: Record<string, GleanCounter>;
+    system: Record<string, GleanCounter>;
+    tabhistory: Record<string, GleanCounter>;
+    reload: Record<string, GleanCounter>;
+    unknown: Record<string, GleanCounter>;
+  }
+
+  browserSearchWithads: {
+    urlbar: Record<string, GleanCounter>;
+    urlbarHandoff: Record<string, GleanCounter>;
+    urlbarPersisted: Record<string, GleanCounter>;
+    urlbarSearchmode: Record<string, GleanCounter>;
+    searchbar: Record<string, GleanCounter>;
+    aboutHome: Record<string, GleanCounter>;
+    aboutNewtab: Record<string, GleanCounter>;
+    contextmenu: Record<string, GleanCounter>;
+    webextension: Record<string, GleanCounter>;
+    system: Record<string, GleanCounter>;
+    tabhistory: Record<string, GleanCounter>;
+    reload: Record<string, GleanCounter>;
+    unknown: Record<string, GleanCounter>;
+  }
+
+  browserSearchAdclicks: {
+    urlbar: Record<string, GleanCounter>;
+    urlbarHandoff: Record<string, GleanCounter>;
+    urlbarPersisted: Record<string, GleanCounter>;
+    urlbarSearchmode: Record<string, GleanCounter>;
+    searchbar: Record<string, GleanCounter>;
+    aboutHome: Record<string, GleanCounter>;
+    aboutNewtab: Record<string, GleanCounter>;
+    contextmenu: Record<string, GleanCounter>;
+    webextension: Record<string, GleanCounter>;
+    system: Record<string, GleanCounter>;
+    tabhistory: Record<string, GleanCounter>;
+    reload: Record<string, GleanCounter>;
+    unknown: Record<string, GleanCounter>;
+  }
+
+  urlbarSearchmode: {
+    bookmarkmenu: Record<string, GleanCounter>;
+    handoff: Record<string, GleanCounter>;
+    keywordoffer: Record<string, GleanCounter>;
+    oneoff: Record<string, GleanCounter>;
+    searchbutton: Record<string, GleanCounter>;
+    shortcut: Record<string, GleanCounter>;
+    tabmenu: Record<string, GleanCounter>;
+    tabtosearch: Record<string, GleanCounter>;
+    tabtosearchOnboard: Record<string, GleanCounter>;
+    topsitesNewtab: Record<string, GleanCounter>;
+    topsitesUrlbar: Record<string, GleanCounter>;
+    touchbar: Record<string, GleanCounter>;
+    typed: Record<string, GleanCounter>;
+    historymenu: Record<string, GleanCounter>;
+    other: Record<string, GleanCounter>;
+  }
+}
--- tools/@types/lib.gecko.glean.d.ts
+++ tools/@types/lib.gecko.glean.d.ts
@@ -4,6 +4,7 @@
  * If you're updating some of the sources, see README for instructions.
  */
 
+/// <reference types="./glean/browser_components_search.d.ts" />
 /// <reference types="./glean/browser_components_search_pings.d.ts" />
 /// <reference types="./glean/toolkit_components_extensions.d.ts" />
 /// <reference types="./glean/toolkit_components_search.d.ts" />
