294aefea1789c4b664a5ede52a93abb6b5d7c66d
Sebastian Hengst <archaeopteryx@coole-files.de>
2025-03-12 14:40:12 +0000

    Bug 1953368 - set CookieStore interfaces as Nightly-only. r=baku, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D241212

--- dom/serviceworkers/test/test_serviceworker_interfaces.js
+++ dom/serviceworkers/test/test_serviceworker_interfaces.js
@@ -152,7 +152,7 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "CountQueuingStrategy",
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  "CookieStore",
+  { name: "CookieStore", nightly: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "Crypto",
   // IMPORTANT: Do not change this list without review from a DOM peer!
@@ -506,7 +506,7 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "console",
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  "cookieStore",
+  { name: "cookieStore", nightly: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "onactivate",
   // IMPORTANT: Do not change this list without review from a DOM peer!
