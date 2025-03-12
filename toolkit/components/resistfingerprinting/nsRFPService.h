0e9bb0adb656e67fa8d08fd5e020533714c762e2
Tim Huang <tihuang@mozilla.com>
2025-03-12 07:33:35 +0000

    Bug 1951017 - Dispatch nsRFPService::MaybeReportFontFingerprinter() to the main thread.  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D240306
    
    Differential Revision: https://phabricator.services.mozilla.com/D240832

--- toolkit/components/resistfingerprinting/nsRFPService.h
+++ toolkit/components/resistfingerprinting/nsRFPService.h
@@ -365,7 +365,7 @@ class nsRFPService final : public nsIObserver, public nsIRFPService {
                                              nsACString& aOriginNoSuffix);
 
   static void MaybeReportFontFingerprinter(nsIChannel* aChannel,
-                                           nsACString& aOriginNoSuffix);
+                                           const nsACString& aOriginNoSuffix);
 
   // --------------------------------------------------------------------------
 
