51ebe8a195aeee414d9c1c3fb126a826bfe4e16f
Thomas Wisniewski <twisniewski@mozilla.com>
2025-03-12 22:03:34 +0000

    Bug 1905069 - add a desktop-only webcompat JS intervention for one.geekie.com.br; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D240239

---
+++ testing/webcompat/interventions/tests/test_1905069_one_geekie_com_br.py
@@ -0,0 +1,24 @@
+import pytest
+
+URL = "https://one.geekie.com.br/"
+
+SUPPORTED_CSS = "#splashscreen"
+UNSUPPORTED_CSS = "#not-chrome"
+
+
+@pytest.mark.skip_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL)
+    assert client.await_css(SUPPORTED_CSS, is_displayed=True)
+    assert not client.find_css(UNSUPPORTED_CSS, is_displayed=True)
+
+
+@pytest.mark.skip_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL)
+    assert client.await_css(UNSUPPORTED_CSS, is_displayed=True)
+    assert not client.find_css(SUPPORTED_CSS, is_displayed=True)
