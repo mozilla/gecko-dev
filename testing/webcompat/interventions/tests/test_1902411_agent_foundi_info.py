16a9b24c4844e101e1e9c26e2bbfd31c0946117f
Thomas Wisniewski <twisniewski@mozilla.com>
2025-03-12 22:03:32 +0000

    Bug 1902411 - add a UA override for agent.foundi.info; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D240235

---
+++ testing/webcompat/interventions/tests/test_1902411_agent_foundi_info.py
@@ -0,0 +1,22 @@
+import pytest
+
+URL = "https://agent.foundi.info/"
+
+SUPPORTED_CSS = ".main-content"
+UNSUPPORTED_TEXT = "Google Chrome"
+
+
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(SUPPORTED_CSS, is_displayed=True)
+    assert not client.find_text(UNSUPPORTED_TEXT, is_displayed=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_text(UNSUPPORTED_TEXT, is_displayed=True)
+    assert not client.find_css(SUPPORTED_CSS, is_displayed=True)
