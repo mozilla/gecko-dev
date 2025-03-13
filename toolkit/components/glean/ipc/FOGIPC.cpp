f8eaee8f082adea830daa10ac085bf2fad547916
Travis Long <tlong@mozilla.com>
2025-03-13 13:17:26 +0000

    Bug 1947604 - Part 2: Glam Experiment temporary metric instrumentation (process tools component). r=janerik
    Differential Revision: https://phabricator.services.mozilla.com/D241251

--- toolkit/components/glean/ipc/FOGIPC.cpp
+++ toolkit/components/glean/ipc/FOGIPC.cpp
@@ -387,12 +387,24 @@ void RecordPowerMetrics() {
     int32_t nNewCpuTime = int32_t(newCpuTime);
     if (newCpuTime < std::numeric_limits<int32_t>::max()) {
       power::total_cpu_time_ms.Add(nNewCpuTime);
+      // GLAM EXPERIMENT
+      // This metric is temporary, disabled by default, and will be enabled only
+      // for the purpose of experimenting with client-side sampling of data for
+      // GLAM use. See Bug 1947604 for more information.
+      glam_experiment::total_cpu_time_ms.Add(nNewCpuTime);
+      // END GLAM EXPERIMENT
       power::cpu_time_per_process_type_ms.Get(type).Add(nNewCpuTime);
       if (!trackerType.IsEmpty()) {
         power::cpu_time_per_tracker_type_ms.Get(trackerType).Add(nNewCpuTime);
       }
     } else {
       power::cpu_time_bogus_values.Add(1);
+      // GLAM EXPERIMENT
+      // This metric is temporary, disabled by default, and will be enabled only
+      // for the purpose of experimenting with client-side sampling of data for
+      // GLAM use. See Bug 1947604 for more information.
+      glam_experiment::cpu_time_bogus_values.Add(1);
+      // END GLAM EXPERIMENT
     }
     PROFILER_MARKER("Process CPU Time", OTHER, {}, ProcessingTimeMarker,
                     nNewCpuTime, type, trackerType);
