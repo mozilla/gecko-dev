ce903750e6db033d2ed7534197d2572aecaf8f63
Jon Coppeard <jcoppeard@mozilla.com>
2025-03-14 17:46:44 +0000

    Bug 1953446 - Part 5: Remove barrier wrappers from WeakMap template parameters r=sfink
    All clients use HeapPtr anyway. Adding the barriers internally will allow us to
    elide them on destruction.
    
    Differential Revision: https://phabricator.services.mozilla.com/D241201

--- js/src/debugger/Debugger.h
+++ js/src/debugger/Debugger.h
@@ -336,10 +336,10 @@ extern void CheckDebuggeeThing(JSObject* obj, bool invisibleOk);
  * beacomes a debuggee again later, new Frame objects are created.)
  */
 template <class Referent, class Wrapper, bool InvisibleKeysOk = false>
-class DebuggerWeakMap : private WeakMap<HeapPtr<Referent*>, HeapPtr<Wrapper*>> {
+class DebuggerWeakMap : private WeakMap<Referent*, Wrapper*> {
  private:
-  using Key = HeapPtr<Referent*>;
-  using Value = HeapPtr<Wrapper*>;
+  using Key = Referent*;
+  using Value = Wrapper*;
 
   JS::Compartment* compartment;
 
