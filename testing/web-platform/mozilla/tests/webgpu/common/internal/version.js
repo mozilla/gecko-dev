23bb7a1df2d742d3dc100aa8150920504f677eb8
Erich Gubler <erichdongubler@gmail.com>
2025-03-14 11:32:32 +0000

    Bug 1943776 - test(webgpu): update CTS to 593e7c8526f220f76e0f047670ba2854cb5bee8b r=webgpu-reviewers,nical
    The only change this patch brings is the consumption of a PR I authored
    to work around runtime errors when a failure incorrectly tries to return
    an error object with methods inside its `extra` field to the content
    context.
    
    See [gpuweb/cts#4287] for more details.
    
    [gpuweb/cts#4287]: https://github.com/gpuweb/cts/pull/4287
    
    Differential Revision: https://phabricator.services.mozilla.com/D241076

--- testing/web-platform/mozilla/tests/webgpu/common/internal/version.js
+++ testing/web-platform/mozilla/tests/webgpu/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'fe6ac50b97644a06e78166e7d3e89265940f4308';
+export const version = '593e7c8526f220f76e0f047670ba2854cb5bee8b';
