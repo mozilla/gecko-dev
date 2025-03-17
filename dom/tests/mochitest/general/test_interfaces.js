3e3fb0d8370f25cffe7cac846b3584cbf8d107c0
Erich Gubler <erichdongubler@gmail.com>
2025-03-17 15:41:45 +0000

    Bug 1952345 - feat(webgpu): stub out `GPUExternalTexture` r=webgpu-reviewers,webidl,smaug,teoxoy
    Do this by populating a trivial `ExtTex` type in `ExternalTexture.h`. Do not use `ExternalTexture`, since that's currently used by the internals that actually implement interesting operations for external texture usage with WGPU; punt on figuring out the perfect name for later.
    
    This allows many CTS tests, which use `x instanceof GPUExternalTexture` without actually consuming the external texture API, to make interesting forward progress.
    
    This should be strictly safe to do, since we do not permit the user to actually construct or use a `GPUExternalTexture`.
    
    Analysis of CTS outcome changes:
    
    - New `PASS`es resulting in promotion to tier 2:
    	- `webgpu:shader,execution,expression,call,builtin,textureDimensions:depth:*`
    	- `webgpu:shader,execution,expression,call,builtin,textureDimensions:sampled_and_multisampled:*` on Linux and macOS
    	- `webgpu:shader,execution,expression,call,builtin,textureLoad:sampled_1d:*` on Windows and Linux
    	- `webgpu:shader,execution,expression,call,builtin,textureLoad:sampled_3d:*` on Windows and Linux
    	- `webgpu:shader,execution,expression,call,builtin,textureNumLayers:arrayed:*` on Linux and macOS
    	- `webgpu:shader,execution,expression,call,builtin,textureNumLayers:sampled:*` on Linux and macOS
    	- `webgpu:shader,execution,expression,call,builtin,textureNumLevels:depth:*`
    	- `webgpu:shader,execution,expression,call,builtin,textureNumLevels:sampled:*` on Linux and macOS
    	- `webgpu:shader,execution,expression,call,builtin,textureNumSamples:depth:*`
    	- `webgpu:shader,execution,expression,call,builtin,textureNumSamples:sampled:*`
    	- `webgpu:shader,execution,expression,call,builtin,textureSample:sampled_1d_coords:*` on Windows and Linux
    
    	Otherwise are _many_ new `PASS`es in test variants that don't quite make entire test(s) pass, so this is definitely a good change. 🙂
    - Some tests' variants have been deleted, since they are no longer reported due to _all_ runs of them `CRASH`ing due to timeout (rather than a stack crash). This will be dealt with in follow-up work. Since these tests are in tier 3, I don't see a hard problem with letting these be deleted for now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D240673

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -540,6 +540,8 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "GPUError", nightly: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "GPUExternalTexture", nightly: true },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "GPUInternalError", nightly: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "GPUMapMode", nightly: true },
