cb2fb6e32179daa266130ab0a0443fc84710e5ab
Norisz Fay <nfay@mozilla.com>
2025-03-18 01:38:16 +0200

    Backed out 2 changesets (bug 1953920, bug 1953927) for causing searchModeSwitcher related failures CLOSED TREE
    Backed out changeset 8b74e1a3e693 (bug 1953920)
    Backed out changeset 4dfd7b14198d (bug 1953927)

--- browser/components/urlbar/SearchModeSwitcher.sys.mjs
+++ browser/components/urlbar/SearchModeSwitcher.sys.mjs
@@ -82,13 +82,19 @@ export class SearchModeSwitcher {
 
     await this.#buildSearchModeList(this.#input.window);
 
-    this.#input.view.close({ showFocusBorder: false });
+    if (anchor.getAttribute("open") == "true") {
+      lazy.PanelMultiView.hidePopup(this.#popup);
+      return;
+    }
+
+    this.#input.view.hideTemporarily();
 
     this.#popup.addEventListener(
       "popuphidden",
       () => {
         anchor.removeAttribute("open");
         anchor.setAttribute("aria-expanded", false);
+        this.#input.view.restoreVisibility();
       },
       { once: true }
     );
