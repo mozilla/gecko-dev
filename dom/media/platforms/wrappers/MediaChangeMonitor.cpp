cd22cdcd923c8e77772bb707eb81cc7c5af391ec
Alexandru Marc <amarc@mozilla.com>
2025-03-18 20:31:01 +0200

    Backed out changeset 5d5ab2e5d2ec (bug 1954585) for causing build bustages @ MediaChangeMonitor.cpp CLOSED TREE

--- dom/media/platforms/wrappers/MediaChangeMonitor.cpp
+++ dom/media/platforms/wrappers/MediaChangeMonitor.cpp
@@ -31,9 +31,6 @@ extern LazyLogModule gMediaDecoderLog;
 #define LOG(x, ...) \
   MOZ_LOG(gMediaDecoderLog, LogLevel::Debug, (x, ##__VA_ARGS__))
 
-#define LOGV(x, ...) \
-  MOZ_LOG(gMediaDecoderLog, LogLevel::Verbose, (x, ##__VA_ARGS__))
-
 // Gets the pixel aspect ratio from the decoded video size and the rendered
 // size.
 inline double GetPixelAspectRatio(const gfx::IntSize& aImage,
@@ -247,6 +244,8 @@ class HEVCChangeMonitor : public MediaChangeMonitor::CodecChangeMonitor {
             : nullptr;
     // Sample doesn't contain any SPS and we already have SPS, do nothing.
     auto curConfig = HVCCConfig::Parse(mCurrentConfig.mExtraData);
+    LOG("current config: %s",
+        curConfig.isOk() ? curConfig.inspect().ToString().get() : "invalid");
     if ((!extraData || extraData->IsEmpty()) && curConfig.unwrap().HasSPS()) {
       return NS_OK;
     }
@@ -258,9 +257,7 @@ class HEVCChangeMonitor : public MediaChangeMonitor::CodecChangeMonitor {
       return NS_OK;
     }
     const HVCCConfig newConfig = rv.unwrap();
-    LOGV("Current config: %s, new config: %s",
-         curConfig.isOk() ? curConfig.inspect().ToString().get() : "invalid",
-         newConfig.ToString().get());
+    LOG("new config: %s", newConfig.ToString().get());
 
     if (!newConfig.HasSPS() && !curConfig.unwrap().HasSPS()) {
       // We don't have inband data and the original config didn't contain a SPS.
