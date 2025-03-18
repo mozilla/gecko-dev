0783f8c6174470a71a4d7dd250137b320bb3adf7
Jonathan Kew <jkew@mozilla.com>
2025-03-18 16:27:36 +0000

    Bug 1951174 - Check for shutdown before trying to create a timer in RLBoxSandboxPool, regardless of what thread we're on. r=xpcom-reviewers,jstutte
    Differential Revision: https://phabricator.services.mozilla.com/D241992

--- xpcom/base/RLBoxSandboxPool.cpp
+++ xpcom/base/RLBoxSandboxPool.cpp
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "nsThreadUtils.h"
-#include "mozilla/ClearOnShutdown.h"
+#include "mozilla/AppShutdown.h"
 #include "mozilla/DebugOnly.h"
 #include "mozilla/RLBoxSandboxPool.h"
 #ifdef MOZ_USING_WASM_SANDBOXING
@@ -19,13 +19,9 @@ NS_IMPL_ISUPPORTS(RLBoxSandboxPool, nsITimerCallback, nsINamed)
 void RLBoxSandboxPool::StartTimer() {
   mMutex.AssertCurrentThreadOwns();
   MOZ_ASSERT(!mTimer, "timer already initialized");
-  if (NS_IsMainThread() &&
-      PastShutdownPhase(ShutdownPhase::AppShutdownConfirmed)) {
+  if (AppShutdown::IsInOrBeyond(ShutdownPhase::AppShutdownConfirmed)) {
     // If we're shutting down, setting the time might fail, and we don't need it
-    // (since all the memory will be cleaned up soon anyway). Note that
-    // PastShutdownPhase() can only be called on the main thread, but that's
-    // fine, because other threads will have joined already by the point timers
-    // start failing to register.
+    // (since all the memory will be cleaned up soon anyway).
     mPool.Clear();
     return;
   }
