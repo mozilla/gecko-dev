1ed807576f7ae8e594fa1f3bb2d5b5e878ef52d9
Rebecca King <rking@mozilla.com>
2025-03-20 17:41:24 +0000

    Bug 1942373 - Add support for badging to moz-button r=shopping-reviewers,reusable-components-reviewers,desktop-theme-reviewers,tgiles,kpatenio,sidebar-reviewers,nsharpley,hjones
    Differential Revision: https://phabricator.services.mozilla.com/D239943

--- toolkit/content/widgets/moz-button/moz-button.mjs
+++ toolkit/content/widgets/moz-button/moz-button.mjs
@@ -53,6 +53,7 @@ export default class MozButton extends MozLitElement {
     iconSrc: { type: String },
     hasVisibleLabel: { type: Boolean, state: true },
     accessKey: { type: String, mapped: true, fluent: true },
+    attention: { type: Boolean },
   };
 
   static queries = {
@@ -67,6 +68,7 @@ export default class MozButton extends MozLitElement {
     this.size = "default";
     this.disabled = false;
     this.hasVisibleLabel = !!this.label;
+    this.attention = false;
   }
 
   // Delegate clicks on host to the button element.
@@ -103,6 +105,8 @@ export default class MozButton extends MozLitElement {
           class=${classMap({
             labelled: this.label || this.hasVisibleLabel,
             "button-background": true,
+            badged:
+              (this.iconSrc || this.type.includes("icon")) && this.attention,
           })}
           part="button"
           type=${this.type}
