498801d41817da307cf380c59032bbf4bef754db
Hubert Boma Manilla <hmanilla@mozilla.com>
2025-03-20 11:32:51 +0000

    Bug 1945467 - [devtools] Clarify how platform scope information is determined r=devtools-reviewers,perftest-reviewers,ochameau,sparky
    Differential Revision: https://phabricator.services.mozilla.com/D241027

--- devtools/client/debugger/src/selectors/pause.js
+++ devtools/client/debugger/src/selectors/pause.js
@@ -202,22 +202,27 @@ export function getSelectedFrameBindings(state, thread) {
   return frameBindings;
 }
 
-function getFrameScope(state, frame) {
-  return (
-    getOriginalFrameScope(state, frame) || getGeneratedFrameScope(state, frame)
-  );
-}
+export function getSelectedScope(state) {
+  const frame = getSelectedFrame(state);
 
-// This is only used by tests
-export function getSelectedScope(state, thread) {
-  const frame = getSelectedFrame(state, thread);
-
-  const frameScope = getFrameScope(state, frame);
-  if (!frameScope) {
-    return null;
+  let scopes;
+  // For non-pretty printed original sources
+  if (
+    frame.location.source.isOriginal &&
+    !frame.location.source.isPrettyPrinted &&
+    !frame.generatedLocation?.source.isWasm
+  ) {
+    scopes = getOriginalFrameScope(state, frame)?.scope;
+    // Fallback to the generated scopes if there are no original scopes
+    if (!scopes) {
+      scopes = getGeneratedFrameScope(state, frame)?.scope;
+    }
+  } else {
+    // For generated sources
+    // For pretty printed sources - Even though are seen as original sources they do not include any rename of variables/function names.
+    scopes = getGeneratedFrameScope(state, frame)?.scope;
   }
-
-  return frameScope.scope || null;
+  return scopes;
 }
 
 export function getSelectedOriginalScope(state, thread) {
