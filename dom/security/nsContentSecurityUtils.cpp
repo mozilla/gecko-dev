34597789d38d9b22704f55284df9d18ec1b35f95
Simon Friedberger <simon@mozilla.com>
2025-03-20 15:30:10 +0000

    Bug 1954939. Add CSP to editcacert.xhtml r=tschuster
    Differential Revision: https://phabricator.services.mozilla.com/D242285

--- dom/security/nsContentSecurityUtils.cpp
+++ dom/security/nsContentSecurityUtils.cpp
@@ -1292,6 +1292,7 @@ static nsLiteralCString sStyleSrcUnsafeInlineAllowList[] = {
     "chrome://pippki/content/deletecert.xhtml"_ns,
     "chrome://pippki/content/device_manager.xhtml"_ns,
     "chrome://pippki/content/downloadcert.xhtml"_ns,
+    "chrome://pippki/content/editcacert.xhtml"_ns,
 };
 // img-src data: blob:
 static nsLiteralCString sImgSrcDataBlobAllowList[] = {
@@ -1934,7 +1935,6 @@ void nsContentSecurityUtils::AssertChromePageHasCSP(Document* aDocument) {
       "chrome://global/content/win.xhtml"_ns,
       "chrome://global/skin/in-content/info-pages.css"_ns,
       "chrome://layoutdebug/content/layoutdebug.xhtml"_ns,
-      "chrome://pippki/content/editcacert.xhtml"_ns,
       "chrome://pippki/content/exceptionDialog.xhtml"_ns,
       "chrome://pippki/content/load_device.xhtml"_ns,
       "chrome://pippki/content/setp12password.xhtml"_ns,
