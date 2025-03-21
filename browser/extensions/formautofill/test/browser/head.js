45decc48d9e6447b3556b59568d428e92aa6f385
Dimi <dlee@mozilla.com>
2025-03-21 09:30:48 +0000

    Bug 1928604 - P1. Support textarea in formautofill module r=NeilDeakin,credential-management-reviewers,issammani
    Differential Revision: https://phabricator.services.mozilla.com/D240334

--- browser/extensions/formautofill/test/browser/head.js
+++ browser/extensions/formautofill/test/browser/head.js
@@ -524,7 +524,10 @@ async function focusUpdateSubmitForm(target, args, submit = true) {
 
     for (const [selector, value] of Object.entries(obj.newValues)) {
       element = form.querySelector(selector);
-      if (content.HTMLInputElement.isInstance(element)) {
+      if (
+        content.HTMLInputElement.isInstance(element) ||
+        content.HTMLTextAreaElement.isInstance(element)
+      ) {
         element.setUserInput(value);
       } else if (
         content.HTMLSelectElement.isInstance(element) &&
@@ -1011,7 +1014,7 @@ function verifySectionAutofillResult(section, result, expectedSection) {
 function getSelectorFromFieldDetail(fieldDetail) {
   // identifier is set with `${element.id}/${element.name}`;
   const id = fieldDetail.identifier.split("/")[0];
-  return `input#${id}, select#${id}`;
+  return `input#${id}, select#${id}, textarea#${id}`;
 }
 
 /**
@@ -1128,7 +1131,11 @@ async function findContext(browser, selector) {
         // TODO: replace the following with an approach that can precisely find the
         // element we want without basing on visibility.
         const e = content.document.querySelector(selector);
-        if (e && content.HTMLInputElement.isInstance(e)) {
+        if (
+          e &&
+          (content.HTMLInputElement.isInstance(e) ||
+            content.HTMLTextAreaElement.isInstance(e))
+        ) {
           return !!(
             e.checkVisibility({
               checkOpacity: true,
--- toolkit/components/formautofill/shared/FormAutofillHeuristics.sys.mjs
+++ toolkit/components/formautofill/shared/FormAutofillHeuristics.sys.mjs
@@ -943,6 +943,11 @@ export const FormAutofillHeuristics = {
       fieldNames = fieldNames.filter(name =>
         FIELDNAMES_FOR_SELECT_ELEMENT.includes(name)
       );
+    } else if (HTMLTextAreaElement.isInstance(element)) {
+      const FIELDNAMES_FOR_TEXT_AREA_ELEMENT = ["street-address"];
+      fieldNames = fieldNames.filter(name =>
+        FIELDNAMES_FOR_TEXT_AREA_ELEMENT.includes(name)
+      );
     }
 
     return fieldNames;
@@ -1050,6 +1055,7 @@ export const FormAutofillHeuristics = {
 
     // Find a matched field name using regexp-based heuristics
     const matchedFieldNames = this._findMatchedFieldNames(element, fields);
+
     return [matchedFieldNames, inferredInfo];
   },
 
