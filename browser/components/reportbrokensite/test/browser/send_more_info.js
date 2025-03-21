77f04f6e76b01e3414289c5aad3abcad61ba5aa0
Thomas Wisniewski <twisniewski@mozilla.com>
2025-03-21 13:00:34 +0000

    Bug 1952334 - Remove the old Report Site Issue fallback and its system addon, and have Report Broken Site perform its function; r=Gijs,mossop,webcompat-reviewers,zeid,frontend-codestyle-reviewers,denschub
    Differential Revision: https://phabricator.services.mozilla.com/D240671

--- browser/components/reportbrokensite/test/browser/send_more_info.js
+++ browser/components/reportbrokensite/test/browser/send_more_info.js
@@ -223,13 +223,41 @@ async function testSendMoreInfo(tab, menu, expectedOverrides = {}) {
   let rbs = await menu.openAndPrefillReportBrokenSite(url, description);
 
   const receivedData = await rbs.clickSendMoreInfo();
-  const { message } = receivedData;
+  await checkWebcompatComPayload(
+    tab,
+    url,
+    description,
+    expectedOverrides,
+    receivedData
+  );
 
+  // re-opening the panel, the url and description should be reset
+  rbs = await menu.openReportBrokenSite();
+  rbs.isMainViewResetToCurrentTab();
+  rbs.close();
+}
+
+async function testWebcompatComFallback(tab, menu) {
+  const url = menu.win.gBrowser.currentURI.spec;
+  const receivedData =
+    await menu.clickReportBrokenSiteAndAwaitWebCompatTabData();
+  await checkWebcompatComPayload(tab, url, "", {}, receivedData);
+  menu.close();
+}
+
+async function checkWebcompatComPayload(
+  tab,
+  url,
+  description,
+  expectedOverrides,
+  receivedData
+) {
   const expected = await reformatExpectedWebCompatInfo(tab, expectedOverrides);
   expected.url = url;
   expected.description = description;
 
   // sanity checks
+  const { message } = receivedData;
   const { details } = message;
   const { additionalData } = details;
   ok(message.url?.length, "Got a URL");
@@ -256,9 +284,4 @@ async function testSendMoreInfo(tab, menu, expectedOverrides = {}) {
   }
 
   ok(areObjectsEqual(message, expected), "sent info matches expectations");
-
-  // re-opening the panel, the url and description should be reset
-  rbs = await menu.openReportBrokenSite();
-  rbs.isMainViewResetToCurrentTab();
-  rbs.close();
 }
