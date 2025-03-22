423e114cb27ae67c88e0377b99ed9f9486cbeb74
Tooru Fujisawa <arai_a@mac.com>
2025-03-22 11:54:13 +0000

    Bug 1954641 - Part 2: Do not cache inline scripts. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D242317

--- dom/script/ScriptLoader.cpp
+++ dom/script/ScriptLoader.cpp
@@ -3059,6 +3059,10 @@ ScriptLoader::CacheBehavior ScriptLoader::GetCacheBehavior(
     return CacheBehavior::DoNothing;
   }
 
+  if (aRequest->GetScriptLoadContext()->mIsInline) {
+    return CacheBehavior::DoNothing;
+  }
+
   if (!aRequest->mURI->SchemeIs("http") && !aRequest->mURI->SchemeIs("https")) {
     // Internal resources can be exposed to the web content, but they don't
     // have to be cached.
