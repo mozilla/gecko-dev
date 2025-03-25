b3237653647d9fe02b6e74ddb37b58cd2b1cddac
Mugurell <Mugurell@users.noreply.github.com>
2025-03-25 19:08:56 +0000

    Bug 1950628 - Improve usability and developer experience for the sample browser r=android-reviewers,gl
    This wasn't supporting Android 15's edge-to-edge functionality and was using
    MaterialTheme instead of AcornTheme.
    
    Differential Revision: https://phabricator.services.mozilla.com/D239783

--- mobile/android/android-components/samples/compose-browser/src/main/java/org/mozilla/samples/compose/browser/BrowserComposeActivity.kt
+++ mobile/android/android-components/samples/compose-browser/src/main/java/org/mozilla/samples/compose/browser/BrowserComposeActivity.kt
@@ -6,11 +6,18 @@ package org.mozilla.samples.compose.browser
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import mozilla.components.compose.base.theme.AcornTheme
+import mozilla.components.support.ktx.android.view.setupPersistentInsets
 import org.mozilla.samples.compose.browser.browser.BrowserScreen
 import org.mozilla.samples.compose.browser.ext.components
 import org.mozilla.samples.compose.browser.settings.SettingsScreen
@@ -26,14 +33,20 @@ class BrowserComposeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        window.setupPersistentInsets()
 
         setContent {
             val navController = rememberNavController()
 
-            MaterialTheme {
-                NavHost(navController, startDestination = ROUTE_BROWSER) {
-                    composable(ROUTE_BROWSER) { BrowserScreen(navController) }
-                    composable(ROUTE_SETTINGS) { SettingsScreen() }
+            AcornTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        NavHost(navController, startDestination = ROUTE_BROWSER) {
+                            composable(ROUTE_BROWSER) { BrowserScreen(navController) }
+                            composable(ROUTE_SETTINGS) { SettingsScreen() }
+                        }
+                    }
                 }
             }
         }
