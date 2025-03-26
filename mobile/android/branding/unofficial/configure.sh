e7ecbbd0d0b639f981f64109bf5b26de40b80454
Alexandru Marc <amarc@mozilla.com>
2025-03-26 12:01:30 +0200

    Backed out changeset 598632c3c68f (bug 1943622) for causing build bustages CLOSED TREE

--- mobile/android/branding/unofficial/configure.sh
+++ mobile/android/branding/unofficial/configure.sh
@@ -2,6 +2,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ANDROID_APPEND_USERNAME_TO_PACKAGE_NAME=1
-ANDROID_PACKAGE_NAME="org.mozilla.fennec"
-MOZ_APP_DISPLAYNAME="Fennec"
+ANDROID_PACKAGE_NAME=org.mozilla.fennec_`echo ${USER:-unknown} | sed 's/-/_/g'`
+MOZ_APP_DISPLAYNAME="Fennec `echo ${USER:-unknown} | sed 's/-/_/g'`"
