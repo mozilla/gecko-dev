8f8495d72657737caeddac7088cba910bd090795
Benjamin VanderSloot <bvandersloot@mozilla.com>
2025-03-26 18:09:50 +0000

    Bug 1945576, part 1 - Add DOM plumbing for Login Status JS hooks - r=anti-tracking-reviewers,webidl,smaug,timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D240217

---
+++ dom/credentialmanagement/identity/NavigatorLogin.h
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_NavigatorLogin_h
+#define mozilla_dom_NavigatorLogin_h
+
+#include "mozilla/dom/LoginStatusBinding.h"
+#include "nsISupports.h"
+#include "nsWrapperCache.h"
+#include "nsCOMPtr.h"
+
+namespace mozilla::dom {
+
+class NavigatorLogin : public nsWrapperCache {
+ public:
+  NS_INLINE_DECL_CYCLE_COLLECTING_NATIVE_REFCOUNTING(NavigatorLogin)
+  NS_DECL_CYCLE_COLLECTION_NATIVE_WRAPPERCACHE_CLASS(NavigatorLogin)
+
+  explicit NavigatorLogin(nsIGlobalObject* aGlobal);
+  nsIGlobalObject* GetParentObject() const { return mOwner; }
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aGivenProto) override;
+
+  already_AddRefed<mozilla::dom::Promise> SetStatus(const LoginStatus& aStatus,
+                                                    mozilla::ErrorResult& aRv);
+
+ protected:
+  virtual ~NavigatorLogin();
+
+ private:
+  nsCOMPtr<nsIGlobalObject> mOwner;
+};
+
+}  // namespace mozilla::dom
+
+#endif  // mozilla_dom_NavigatorLogin_h
