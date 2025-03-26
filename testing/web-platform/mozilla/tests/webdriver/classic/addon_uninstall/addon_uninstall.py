7ea28595f323f874855ae30d9f49b9a902e8fd71
Cosmin Sabou <csabou@mozilla.com>
2025-03-26 23:17:20 +0200

    Backed out changeset f4d143b19597 (bug 1956510) for causing lint failures on addon_uninstall.py.

--- testing/web-platform/mozilla/tests/webdriver/classic/addon_uninstall/addon_uninstall.py
+++ testing/web-platform/mozilla/tests/webdriver/classic/addon_uninstall/addon_uninstall.py
@@ -9,3 +9,26 @@ from . import install_addon, uninstall_addon
 def test_uninstall_nonexistent_addon(session):
     response = uninstall_addon(session, "i-do-not-exist-as-an-id")
     assert_error(response, "unknown error")
+
+
+@pytest.mark.allow_system_access
+@pytest.mark.parametrize(
+    "filename, temporary",
+    [("firefox/signed.xpi", True), ("firefox/unsigned.xpi", False)],
+)
+def test_uninstall_addon(session, filename, temporary):
+    response = install_addon(
+        session, "addon", get_base64_for_extension_file(filename), temporary
+    )
+    addon_id = assert_success(response)
+
+    installed_addon_ids = get_ids_for_installed_addons(session)
+
+    assert addon_id in installed_addon_ids
+
+    response = uninstall_addon(session, addon_id)
+    assert_success(response)
+
+    installed_addon_ids = get_ids_for_installed_addons(session)
+
+    assert addon_id not in installed_addon_ids
