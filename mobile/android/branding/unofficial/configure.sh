02cf85ef40f681f3d4df3df737d36a13b675edec
serge-sans-paille <sguelton@mozilla.com>
2025-03-26 09:20:03 +0000

    Bug 1943622 - Move configure.sh to a simple key/value format r=glandium
    Reuse the parse from confvars.sh to do so. This restricts the
    expressiveness of the format as it limits it to variable declared as
    confvar, but that's not a location where we actually want this kind of
    customisation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D235770

--- mobile/android/branding/unofficial/configure.sh
+++ mobile/android/branding/unofficial/configure.sh
@@ -2,5 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ANDROID_PACKAGE_NAME=org.mozilla.fennec_`echo ${USER:-unknown} | sed 's/-/_/g'`
-MOZ_APP_DISPLAYNAME="Fennec `echo ${USER:-unknown} | sed 's/-/_/g'`"
+ANDROID_APPEND_USERNAME_TO_PACKAGE_NAME=1
+ANDROID_PACKAGE_NAME="org.mozilla.fennec"
+MOZ_APP_DISPLAYNAME="Fennec"
