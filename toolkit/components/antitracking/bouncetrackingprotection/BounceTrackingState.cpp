b6aa3a956280e8dd7b446b201eeaf703201bd840
Goloman Adrian <agoloman@mozilla.com>
2025-03-31 21:09:35 +0300

    Backed out changeset 36a69a7cc60d (bug 1956302) for causing build bustages @BounceTrackingState.cpp.

--- toolkit/components/antitracking/bouncetrackingprotection/BounceTrackingState.cpp
+++ toolkit/components/antitracking/bouncetrackingprotection/BounceTrackingState.cpp
@@ -509,18 +509,6 @@ BounceTrackingState::OnStateChange(nsIWebProgress* aWebProgress,
     return NS_OK;
   }
 
-  MOZ_LOG(gBounceTrackingProtectionLog, LogLevel::Verbose,
-          ("%s: Top level window load: aStateFlags: %d, aStatus: %u",
-           __PRETTY_FUNCTION__, aStateFlags, aStatus));
-
-  // Discard failed loads. Those are not valid destinations.
-  if (NS_FAILED(aStatus)) {
-    MOZ_LOG(gBounceTrackingProtectionLog, LogLevel::Verbose,
-            ("%s: Discarding failed load. aStatus: %u", __PRETTY_FUNCTION__,
-             aStatus));
-    return NS_OK;
-  }
-
   // Get the document principal via the current window global.
   dom::BrowsingContext* browsingContext = aWebProgress->GetBrowsingContext();
   NS_ENSURE_TRUE(browsingContext, NS_ERROR_FAILURE);
