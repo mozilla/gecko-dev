bedadde95bd57ddd513a488f8ab9677b5d54933c
Kershaw Chang <kershaw@mozilla.com>
2025-03-31 17:43:02 +0000

    Bug 1874101 - Make Http2WebTransportSession be able to send and receive capsules, r=necko-reviewers,valentin,jesup
    Differential Revision: https://phabricator.services.mozilla.com/D239915

--- netwerk/protocol/http/Http2WebTransportSession.cpp
+++ netwerk/protocol/http/Http2WebTransportSession.cpp
@@ -7,15 +7,24 @@
 // HttpLog.h should generally be included first
 #include "HttpLog.h"
 
+#include "Capsule.h"
+#include "CapsuleEncoder.h"
 #include "Http2WebTransportSession.h"
 #include "Http2Session.h"
+#include "mozilla/net/NeqoHttp3Conn.h"
+#include "nsIWebTransport.h"
+#include "nsIOService.h"
 
 namespace mozilla::net {
+
+NS_IMPL_ISUPPORTS_INHERITED(Http2WebTransportSession, Http2StreamTunnel,
+                            nsIOutputStreamCallback, nsIInputStreamCallback)
 
 Http2WebTransportSession::Http2WebTransportSession(
     Http2Session* aSession, int32_t aPriority, uint64_t aBcId,
     nsHttpConnectionInfo* aConnectionInfo)
-    : Http2StreamTunnel(aSession, aPriority, aBcId, aConnectionInfo) {
+    : Http2StreamTunnel(aSession, aPriority, aBcId, aConnectionInfo),
+      mCapsuleParser(MakeUnique<CapsuleParser>(this)) {
   LOG(("Http2WebTransportSession ctor:%p", this));
 }
 
@@ -54,7 +63,182 @@ nsresult Http2WebTransportSession::GenerateHeaders(nsCString& aCompressedData,
   NS_ENSURE_SUCCESS(rv, rv);
 
   mRequestBodyLenRemaining = 0x0fffffffffffffffULL;
+
+  if (mInput) {
+    mInput->AsyncWait(this, 0, 0, nullptr);
+  }
   return NS_OK;
 }
+
+bool Http2WebTransportSession::OnCapsule(Capsule&& aCapsule) {
+  switch (aCapsule.Type()) {
+    case CapsuleType::CLOSE_WEBTRANSPORT_SESSION:
+      LOG(("Handling CLOSE_WEBTRANSPORT_SESSION\n"));
+      break;
+    case CapsuleType::DRAIN_WEBTRANSPORT_SESSION:
+      LOG(("Handling DRAIN_WEBTRANSPORT_SESSION\n"));
+      break;
+    case CapsuleType::PADDING:
+      LOG(("Handling PADDING\n"));
+      break;
+    case CapsuleType::WT_RESET_STREAM:
+      LOG(("Handling WT_RESET_STREAM\n"));
+      break;
+    case CapsuleType::WT_STOP_SENDING:
+      LOG(("Handling WT_STOP_SENDING\n"));
+      break;
+    case CapsuleType::WT_STREAM:
+      LOG(("Handling WT_STREAM\n"));
+      break;
+    case CapsuleType::WT_STREAM_FIN:
+      LOG(("Handling WT_STREAM_FIN\n"));
+      break;
+    case CapsuleType::WT_MAX_DATA:
+      LOG(("Handling WT_MAX_DATA\n"));
+      break;
+    case CapsuleType::WT_MAX_STREAM_DATA:
+      LOG(("Handling WT_MAX_STREAM_DATA\n"));
+      break;
+    case CapsuleType::WT_MAX_STREAMS_BIDI:
+      LOG(("Handling WT_MAX_STREAMS_BIDI\n"));
+      break;
+    case CapsuleType::WT_MAX_STREAMS_UNIDI:
+      LOG(("Handling WT_MAX_STREAMS_UNIDI\n"));
+      break;
+    case CapsuleType::WT_DATA_BLOCKED:
+      LOG(("Handling WT_DATA_BLOCKED\n"));
+      break;
+    case CapsuleType::WT_STREAM_DATA_BLOCKED:
+      LOG(("Handling WT_STREAM_DATA_BLOCKED\n"));
+      break;
+    case CapsuleType::WT_STREAMS_BLOCKED_BIDI:
+      LOG(("Handling WT_STREAMS_BLOCKED_BIDI\n"));
+      break;
+    case CapsuleType::WT_STREAMS_BLOCKED_UNIDI:
+      LOG(("Handling WT_STREAMS_BLOCKED_UNIDI\n"));
+      break;
+    default:
+      LOG(("Unhandled capsule type\n"));
+      break;
+  }
+  return true;
+}
+
+void Http2WebTransportSession::OnCapsuleParseFailure(nsresult aError) {}
+
+NS_IMETHODIMP
+Http2WebTransportSession::OnInputStreamReady(nsIAsyncInputStream* aIn) {
+  char buffer[nsIOService::gDefaultSegmentSize];
+  uint32_t remainingCapacity = sizeof(buffer);
+  uint32_t read = 0;
+
+  while (remainingCapacity > 0) {
+    uint32_t count = 0;
+    nsresult rv = mInput->Read(buffer + read, remainingCapacity, &count);
+    if (rv == NS_BASE_STREAM_WOULD_BLOCK) {
+      break;
+    }
+
+    if (NS_FAILED(rv)) {
+      LOG(("Http2WebTransportSession::OnInputStreamReady %p failed %u\n", this,
+           static_cast<uint32_t>(rv)));
+      // TODO: close connection
+      return rv;
+    }
+
+    // base stream closed
+    if (count == 0) {
+      LOG((
+          "Http2WebTransportSession::OnInputStreamReady %p connection closed\n",
+          this));
+      // Close with NS_BASE_STREAM_CLOSED
+      return NS_OK;
+    }
+
+    remainingCapacity -= count;
+    read += count;
+  }
+
+  if (read > 0) {
+    Http2Session::LogIO(nullptr, this, "Http2WebTransportSession", buffer,
+                        read);
+
+    mCapsuleParser->ProcessCapsuleData(reinterpret_cast<uint8_t*>(buffer),
+                                       read);
+  }
+
+  mInput->AsyncWait(this, 0, 0, nullptr);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+Http2WebTransportSession::OnOutputStreamReady(nsIAsyncOutputStream* aOut) {
+  while (!mOutgoingQueue.empty() && mOutput) {
+    CapsuleEncoder& data = mOutgoingQueue.front();
+    auto buffer = data.GetBuffer();
+    const char* writeBuffer =
+        reinterpret_cast<const char*>(buffer.Elements()) + mWriteOffset;
+    uint32_t toWrite = buffer.Length() - mWriteOffset;
+
+    uint32_t wrote = 0;
+    nsresult rv = mOutput->Write(writeBuffer, toWrite, &wrote);
+    if (rv == NS_BASE_STREAM_WOULD_BLOCK) {
+      mOutput->AsyncWait(this, 0, 0, nullptr);
+      return NS_OK;
+    }
+
+    if (NS_FAILED(rv)) {
+      LOG(("Http2WebTransportSession::OnOutputStreamReady %p failed %u\n", this,
+           static_cast<uint32_t>(rv)));
+      // TODO: close connection
+      return NS_OK;
+    }
+
+    mWriteOffset += wrote;
+
+    if (toWrite == wrote) {
+      mWriteOffset = 0;
+      mOutgoingQueue.pop_front();
+    }
+  }
+
+  if (mInput) {
+    mInput->AsyncWait(this, 0, 0, nullptr);
+  }
+  return NS_OK;
+}
+
+void Http2WebTransportSession::SendCapsule(CapsuleEncoder&& aEncoder) {
+  LOG(("Http2WebTransportSession::SendCapsule %p mSendClosed=%d", this,
+       mSendClosed));
+  if (mSendClosed) {
+    return;
+  }
+
+  mOutgoingQueue.emplace_back(std::move(aEncoder));
+
+  if (mOutput) {
+    OnOutputStreamReady(mOutput);
+  }
+}
+
+void Http2WebTransportSession::CloseSession(uint32_t aStatus,
+                                            const nsACString& aReason) {
+  LOG(("Http2WebTransportSession::CloseSession %p aStatus=%x", this, aStatus));
+
+  SetSentFin(true);
+
+  Capsule capsule = Capsule::CloseWebTransportSession(aStatus, aReason);
+  CapsuleEncoder encoder;
+  encoder.EncodeCapsule(capsule);
+  SendCapsule(std::move(encoder));
+}
+
+uint64_t Http2WebTransportSession::StreamId() const { return mStreamID; }
+
+void Http2WebTransportSession::GetMaxDatagramSize() {}
+
+void Http2WebTransportSession::SendDatagram(nsTArray<uint8_t>&& aData,
+                                            uint64_t aTrackingId) {}
 
 }  // namespace mozilla::net
--- netwerk/protocol/http/Http2WebTransportSession.h
+++ netwerk/protocol/http/Http2WebTransportSession.h
@@ -6,21 +6,60 @@
 #ifndef mozilla_net_Http2WebTransportSession_h
 #define mozilla_net_Http2WebTransportSession_h
 
+#include <list>
+
+#include "CapsuleParser.h"
 #include "Http2StreamTunnel.h"
+#include "mozilla/UniquePtr.h"
+#include "nsRefPtrHashtable.h"
+#include "nsTHashMap.h"
+#include "nsHashKeys.h"
+#include "WebTransportSessionBase.h"
+#include "WebTransportStreamBase.h"
 
 namespace mozilla::net {
 
-class Http2WebTransportSession : public Http2StreamTunnel {
+class CapsuleEncoder;
+class Http2WebTransportSession final : public WebTransportSessionBase,
+                                       public Http2StreamTunnel,
+                                       public nsIOutputStreamCallback,
+                                       public nsIInputStreamCallback,
+                                       public CapsuleParser::Listener {
  public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIOUTPUTSTREAMCALLBACK
+  NS_DECL_NSIINPUTSTREAMCALLBACK
+
   Http2WebTransportSession(Http2Session* aSession, int32_t aPriority,
                            uint64_t aBcId,
                            nsHttpConnectionInfo* aConnectionInfo);
+  Http2WebTransportSession* GetHttp2WebTransportSession() override {
+    return this;
+  }
+  void CloseSession(uint32_t aStatus, const nsACString& aReason) override;
   void CloseStream(nsresult aReason) override;
+  uint64_t StreamId() const override;
+  void GetMaxDatagramSize() override;
+  void SendDatagram(nsTArray<uint8_t>&& aData, uint64_t aTrackingId) override;
+  void CreateOutgoingBidirectionalStream(
+      std::function<void(Result<RefPtr<WebTransportStreamBase>, nsresult>&&)>&&
+          aCallback) override {}
+  void CreateOutgoingUnidirectionalStream(
+      std::function<void(Result<RefPtr<WebTransportStreamBase>, nsresult>&&)>&&
+          aCallback) override {}
 
- protected:
+  bool OnCapsule(Capsule&& aCapsule) override;
+  void OnCapsuleParseFailure(nsresult aError) override;
+
+ private:
   virtual ~Http2WebTransportSession();
   nsresult GenerateHeaders(nsCString& aCompressedData,
                            uint8_t& aFirstFrameFlags) override;
+  void SendCapsule(CapsuleEncoder&& aCapsule);
+
+  size_t mWriteOffset{0};
+  std::list<CapsuleEncoder> mOutgoingQueue;
+  UniquePtr<CapsuleParser> mCapsuleParser;
 };
 
 }  // namespace mozilla::net
