7eacdda7f7ff2b14c11f046b1389f3883e7a7107
Ben Dean-Kawamura <bdk@mozilla.com>
2025-03-31 15:33:20 +0000

    Bug 1950404 - Remove call to SuggestBuilder.remoteSettingsServer, r=skhan,android-reviewers
    This has been replaced with SuggestBuilder.remoteSettingsService and the
    app-services code is now ignoring the `remoteSettingsServer` call.
    Let's stop sending it from Firefox-Android.
    
    Differential Revision: https://phabricator.services.mozilla.com/D242843

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/FxSuggest.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/FxSuggest.kt
@@ -5,11 +5,9 @@
 package org.mozilla.fenix.components
 
 import android.content.Context
-import mozilla.appservices.remotesettings.RemoteSettingsServer
 import mozilla.components.feature.fxsuggest.FxSuggestIngestionScheduler
 import mozilla.components.feature.fxsuggest.FxSuggestStorage
 import mozilla.components.support.remotesettings.RemoteSettingsService
-import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.perf.lazyMonitored
 
 /**
@@ -23,12 +21,6 @@ class FxSuggest(context: Context, remoteSettingsService: RemoteSettingsService) 
         FxSuggestStorage(
             context,
             remoteSettingsService,
-            // TODO(1950404): Remove this arg once Suggest is using the new remote settings API
-            remoteSettingsServer = if (context.settings().useProductionRemoteSettingsServer) {
-                RemoteSettingsServer.Prod
-            } else {
-                RemoteSettingsServer.Stage
-            },
         )
     }
 
