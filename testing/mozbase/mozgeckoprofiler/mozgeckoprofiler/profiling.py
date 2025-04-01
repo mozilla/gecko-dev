14b116f8a6c24e86c9257050a2959dd1db76eae8
Narcis Beleuzu <nbeleuzu@mozilla.com>
2025-04-01 17:47:27 +0300

    Backed out changeset 95b14b5b7063 (bug 1956752) as req by dev. CLOSED TREE

--- testing/mozbase/mozgeckoprofiler/mozgeckoprofiler/profiling.py
+++ testing/mozbase/mozgeckoprofiler/mozgeckoprofiler/profiling.py
@@ -1,14 +1,10 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import json
 import os
 import shutil
 import tempfile
-
-try:
-    import ujson as json
-except ImportError:
-    import json
 
 from mozlog import get_proxy_logger
 
