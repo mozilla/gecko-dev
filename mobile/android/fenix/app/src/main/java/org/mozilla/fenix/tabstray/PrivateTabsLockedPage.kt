82090e660ff60bc5221b63d22e642e415418bf20
gela <gela.malekpour@gmail.com>
2025-04-01 20:13:40 +0000

    Bug 1944177 - add UI for locked tabs in PBM tab tray r=android-reviewers,twhite
    Differential Revision: https://phabricator.services.mozilla.com/D243579

---
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabstray/PrivateTabsLockedPage.kt
@@ -0,0 +1,62 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.tabstray
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import mozilla.components.compose.base.annotation.FlexibleWindowLightDarkPreview
+import org.mozilla.fenix.R
+import org.mozilla.fenix.compose.button.PrimaryButton
+import org.mozilla.fenix.theme.FirefoxTheme
+import org.mozilla.fenix.theme.Theme
+
+/**
+ * UI for displaying the Unlock Private Tabs Page in the Tabs Tray.
+ *
+ * @param onUnlockClicked Invoked when the user taps the unlock button.
+ */
+@Composable
+internal fun PrivateTabsLockedPage(onUnlockClicked: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(id = R.string.pbm_authentication_unlock_private_tabs),
+            color = FirefoxTheme.colors.textPrimary,
+            textAlign = TextAlign.Center,
+            style = FirefoxTheme.typography.body1,
+        )
+
+        PrimaryButton(
+            onClick = onUnlockClicked,
+            icon = painterResource(id = R.drawable.ic_lock),
+            text = stringResource(id = R.string.pbm_authentication_unlock),
+        )
+    }
+}
+
+@FlexibleWindowLightDarkPreview
+@Composable
+private fun PrivateTabsLockedPagePreview() {
+    FirefoxTheme(Theme.Private) {
+        PrivateTabsLockedPage(
+            onUnlockClicked = {},
+        )
+    }
+}
