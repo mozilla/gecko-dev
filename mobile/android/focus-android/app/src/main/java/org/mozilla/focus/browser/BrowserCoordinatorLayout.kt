34a7ed3810dacb16e3140e610844e977222e98fd
mcarare <mcarare@mozilla.com>
2025-04-01 16:59:25 +0000

    Bug 1915087 - Remove custom ResizableKeyboardCoordinatorLayout. r=android-reviewers,boek
    Differential Revision: https://phabricator.services.mozilla.com/D239523

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/browser/BrowserCoordinatorLayout.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/browser/BrowserCoordinatorLayout.kt
@@ -1,30 +1,32 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-package org.mozilla.focus.widget
+package org.mozilla.focus.browser
 
 import android.content.Context
 import android.util.AttributeSet
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat.Type.displayCutout
+import androidx.core.view.WindowInsetsCompat.Type.systemBars
+import androidx.core.view.updatePadding
 
 /**
- * A CoordinatorLayout implementation that resizes dynamically based on whether a keyboard is visible or not.
+ * A [CoordinatorLayout] implementation used in the browser.
  */
-class ResizableKeyboardCoordinatorLayout @JvmOverloads constructor(
+class BrowserCoordinatorLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
 ) : CoordinatorLayout(context, attrs, defStyleAttr) {
-    private val delegate = ResizableKeyboardViewDelegate(this, attrs)
+    val persistentInsetsTypes = systemBars() or displayCutout()
 
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
-        delegate.onAttachedToWindow()
-    }
-
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        delegate.onDetachedFromWindow()
+    init {
+        ViewCompat.setOnApplyWindowInsetsListener(this) { _, windowInsets ->
+            val persistentInsets = windowInsets.getInsets(persistentInsetsTypes)
+            updatePadding(top = persistentInsets.top)
+            return@setOnApplyWindowInsetsListener windowInsets
+        }
     }
 
     override fun requestDisallowInterceptTouchEvent(b: Boolean) {
