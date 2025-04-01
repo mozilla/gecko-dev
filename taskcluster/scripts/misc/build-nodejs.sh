0c7e6253d5ef09bc83a1c03bbd482ad965efc66c
Mike Hommey <mh+mozilla@glandium.org>
2025-04-01 07:24:31 +0000

    Bug 1956435 - Build nodejs against libstdc++ 10. r=firefox-build-system-reviewers,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D243080

--- taskcluster/scripts/misc/build-nodejs.sh
+++ taskcluster/scripts/misc/build-nodejs.sh
@@ -7,14 +7,15 @@ workspace=$HOME/workspace
 
 cd $MOZ_FETCHES_DIR/$project
 
-gcc_major=8
+gcc_major=10
 export CFLAGS=--sysroot=$MOZ_FETCHES_DIR/sysroot
 export CXXFLAGS"=--sysroot=$MOZ_FETCHES_DIR/sysroot  -isystem $MOZ_FETCHES_DIR/sysroot/usr/include/c++/$gcc_major -isystem $MOZ_FETCHES_DIR/sysroot/usr/include/x86_64-linux-gnu/c++/$gcc_major"
 export LDFLAGS="--sysroot=$MOZ_FETCHES_DIR/sysroot -L$MOZ_FETCHES_DIR/sysroot/lib/x86_64-linux-gnu -L$MOZ_FETCHES_DIR/sysroot/usr/lib/x86_64-linux-gnu -L$MOZ_FETCHES_DIR/sysroot/usr/lib/gcc/x86_64-linux-gnu/$gcc_major"
 export CC=$MOZ_FETCHES_DIR/gcc/bin/gcc
 export CXX=$MOZ_FETCHES_DIR/gcc/bin/g++
 
-./configure --verbose --prefix=/
+# --partly-static allows the resulting binary to run on Ubuntu 18.04 (which has libstdc++ 8)
+./configure --verbose --prefix=/ --partly-static
 make -j$(nproc) install DESTDIR=$workspace/$project
 
 tar -C $workspace -acvf $artifact $project
