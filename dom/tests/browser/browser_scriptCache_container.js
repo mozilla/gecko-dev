e5c402bfc80920375ed46fba002340a61a5ec8b6
Tooru Fujisawa <arai_a@mac.com>
2025-04-01 03:11:19 +0000

    Bug 1949932 - Part 8: Add test for container for cached modules. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D242735

--- dom/tests/browser/browser_scriptCache_container.js
+++ dom/tests/browser/browser_scriptCache_container.js
@@ -1,16 +1,18 @@
-const TEST_URL =
-  "https://example.com/browser/dom/tests/browser/page_scriptCache_container.html";
-
 const TEST_SCRIPT_URL =
+  "https://example.com/browser/dom/tests/browser/page_scriptCache_container.html";
+const TEST_MODULE_URL =
+  "https://example.com/browser/dom/tests/browser/page_scriptCache_container_module.html";
+
+const TEST_SJS_URL =
   "https://example.com/browser/dom/tests/browser/counter_server.sjs";
 
-async function testScriptCache({ enableCache }) {
+async function testCache({ enableCache, type }) {
   await SpecialPowers.pushPrefEnv({
     set: [["dom.script_loader.navigation_cache", enableCache]],
   });
   registerCleanupFunction(() => SpecialPowers.popPrefEnv());
 
-  const response1 = await fetch(TEST_SCRIPT_URL + "?reset");
+  const response1 = await fetch(TEST_SJS_URL + "?reset");
   is(await response1.text(), "reset", "Server state should be reset");
 
   ChromeUtils.clearResourceCache();
@@ -28,11 +30,13 @@ async function testScriptCache({ enableCache }) {
     return tab;
   }
 
+  const url = type === "script" ? TEST_SCRIPT_URL : TEST_MODULE_URL;
+
   // Loading script from different containers should use separate cache.
-  const tab0 = await openTab(TEST_URL, 1);
+  const tab0 = await openTab(url, 1);
   is(await getCounter(tab0), "0");
 
-  const tab1 = await openTab(TEST_URL, 2);
+  const tab1 = await openTab(url, 2);
   is(await getCounter(tab1), "1");
 
   // Reloading the page should use the cached script, for each container.
@@ -46,14 +50,30 @@ async function testScriptCache({ enableCache }) {
   BrowserTestUtils.removeTab(tab1);
 }
 
-add_task(async function testNoCache() {
-  await testScriptCache({
+add_task(async function testScriptNoCache() {
+  await testCache({
     enableCache: false,
+    type: "script",
   });
 });
 
-add_task(async function testCache() {
-  await testScriptCache({
+add_task(async function testScriptCache() {
+  await testCache({
     enableCache: true,
+    type: "script",
+  });
+});
+
+add_task(async function testModuleNoCache() {
+  await testCache({
+    enableCache: false,
+    type: "module",
+  });
+});
+
+add_task(async function testModuleCache() {
+  await testCache({
+    enableCache: true,
+    type: "module",
   });
 });
