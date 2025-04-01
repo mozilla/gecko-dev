86f9c56574b5ce8a0a1c932d40effa732efae342
Thomas Wisniewski <twisniewski@mozilla.com>
2025-04-01 17:58:20 +0000

    Bug 1934189 - change automated webcompat intervention test for www.fdj.fr to a regression test; r=denschub
    Differential Revision: https://phabricator.services.mozilla.com/D243829

--- testing/webcompat/interventions/tests/test_1934189_fdj_fr.py
+++ testing/webcompat/interventions/tests/test_1934189_fdj_fr.py
@@ -4,11 +4,8 @@ from webdriver.error import NoSuchElementException, UnexpectedAlertOpenException
 URL = "https://www.fdj.fr/jeux-illiko/instant-euromillions"
 COOKIES_ACCEPT_BUTTON_CSS = """button[title="J'accepte"]"""
 PLAY_BUTTON_CSS = """[id="Instant Euromillions-btn-play"]"""
-GAME_IFRAME_CSS = "#wofOpeningIframe"
 UNSUPPORTED_ALERT_MSG = "Cet appareil ou ce navigateur n’est pas compatible avec ce jeu"
-RESTRICTED_CSS = "#wsi-restriction-messages-modal-title"
 NEED_VPN_TEXT = "Accès refusé"
-LOGIN_CSS = "#wsi-login-credentials-title"
 
 
 async def start_playing(client):
@@ -24,21 +21,12 @@ async def start_playing(client):
 
 @pytest.mark.only_platforms("android")
 @pytest.mark.asyncio
-@pytest.mark.with_interventions
-async def test_enabled(client):
-    await start_playing(client)
-    client.switch_frame(client.await_css(GAME_IFRAME_CSS))
-    denied, login = client.await_first_element_of(
-        [client.css(RESTRICTED_CSS), client.css(LOGIN_CSS)], is_displayed=True
-    )
-    assert denied or login
-
-
-@pytest.mark.only_platforms("android")
-@pytest.mark.asyncio
 @pytest.mark.without_interventions
 async def test_disabled(client):
+    saw_msg = False
     try:
         await start_playing(client)
     except UnexpectedAlertOpenException as e:
-        assert UNSUPPORTED_ALERT_MSG in str(e)
+        if UNSUPPORTED_ALERT_MSG in str(e):
+            saw_msg = True
+    assert not saw_msg
