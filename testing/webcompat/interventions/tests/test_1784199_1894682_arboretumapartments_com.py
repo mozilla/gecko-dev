bcd437841444e330defba3dd78bd467f82f86468
Thomas Wisniewski <twisniewski@mozilla.com>
2025-04-01 17:58:19 +0000

    Bug 1894682 - remove webcompat Entrata intervention for arboretumapartments.com; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D243825

--- testing/webcompat/interventions/tests/test_1784199_1894682_arboretumapartments_com.py
+++ testing/webcompat/interventions/tests/test_1784199_1894682_arboretumapartments_com.py
@@ -1,15 +1,13 @@
+import asyncio
+
 import pytest
 
 URL = "https://www.arboretumapartments.com/"
 
 
 @pytest.mark.asyncio
-@pytest.mark.with_interventions
-async def test_enabled(client):
-    assert await client.test_entrata_banner_hidden(URL)
-
-
-@pytest.mark.asyncio
 @pytest.mark.without_interventions
 async def test_disabled(client):
-    assert not await client.test_entrata_banner_hidden(URL)
+    await client.navigate(URL)
+    await asyncio.sleep(2)
+    assert not client.find_css("#browser-warning-popup")
