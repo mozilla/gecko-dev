e01d896eacd0ecb23735147ea40b3f4a4f90cce4
Tooru Fujisawa <arai_a@mac.com>
2025-04-01 03:11:19 +0000

    Bug 1949932 - Part 4: Add DevTools Network Monitor tests for the cached modules. r=devtools-reviewers,frontend-codestyle-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D242731

--- devtools/client/netmonitor/test/.eslintrc.js
+++ devtools/client/netmonitor/test/.eslintrc.js
@@ -4,4 +4,14 @@ module.exports = {
   env: {
     jest: true,
   },
+  overrides: [
+    {
+      // eslint-plugin-html doesn't automatically detect module sections in
+      // html files. Enable these as a module here.
+      files: ["html_module-script-cache.html"],
+      parserOptions: {
+        sourceType: "module",
+      },
+    },
+  ],
 };
--- devtools/client/netmonitor/test/browser_net_script_cache.js
+++ devtools/client/netmonitor/test/browser_net_script_cache.js
@@ -4,7 +4,12 @@
 "use strict";
 
 // Test the script caches in the network monitor.
-add_task(async function () {
+async function do_test_script_cache(enableCache) {
+  await SpecialPowers.pushPrefEnv({
+    set: [["dom.script_loader.navigation_cache", enableCache]],
+  });
+  registerCleanupFunction(() => SpecialPowers.popPrefEnv());
+
   const { monitor } = await initNetMonitor(SCRIPT_CACHE_URL, {
     enableCache: true,
     requestCount: 1,
@@ -40,6 +45,8 @@ add_task(async function () {
       fullMimeType: "text/javascript",
     },
   };
+
+  is(requests[0].querySelector(".requests-list-type").textContent, "html");
 
   // All script elements should create corresponding requests.
   for (let i = 1; i < requests.length; ++i) {
@@ -109,4 +116,137 @@ add_task(async function () {
     });
   }
   await teardown(monitor);
+}
+
+add_task(async function test_script_cache_disabled() {
+  await do_test_script_cache(false);
+});
+
+add_task(async function test_script_cache_enabled() {
+  await do_test_script_cache(true);
+});
+
+/**
+ * Importing the same module script multiple times within the same document
+ * should not generate multiple fetch, regardless of whether the script cache
+ * is used or not, and there should be only one request shown in the network
+ * monitor.
+ */
+async function do_test_module_cache(enableCache) {
+  await SpecialPowers.pushPrefEnv({
+    set: [["dom.script_loader.navigation_cache", enableCache]],
+  });
+  registerCleanupFunction(() => SpecialPowers.popPrefEnv());
+
+  const { monitor } = await initNetMonitor(MODULE_SCRIPT_CACHE_URL, {
+    enableCache: true,
+    requestCount: 1,
+  });
+  info("Starting test... ");
+
+  const { document, store, windowRequire } = monitor.panelWin;
+  const { getDisplayedRequests, getSortedRequests } = windowRequire(
+    "devtools/client/netmonitor/src/selectors/index"
+  );
+  const Actions = windowRequire("devtools/client/netmonitor/src/actions/index");
+  store.dispatch(Actions.batchEnable(false));
+
+  const waitForEvents = waitForNetworkEvents(monitor, 2);
+
+  BrowserTestUtils.startLoadingURIString(
+    gBrowser.selectedBrowser,
+    MODULE_SCRIPT_CACHE_URL
+  );
+  await waitForEvents;
+
+  const requests = document.querySelectorAll(".request-list-item");
+
+  is(requests.length, 2, "There should be 2 requests");
+
+  const requestData = {
+    uri: HTTPS_EXAMPLE_URL + "sjs_test-module-script.sjs",
+    details: {
+      status: 200,
+      statusText: "OK (cached)",
+      displayedStatus: "cached",
+      type: "js",
+      fullMimeType: "text/javascript",
+    },
+  };
+
+  is(requests[0].querySelector(".requests-list-type").textContent, "html");
+
+  const request = requests[1];
+
+  const requestsListStatus = request.querySelector(".status-code");
+  EventUtils.sendMouseEvent({ type: "mouseover" }, requestsListStatus);
+  await waitUntil(() => requestsListStatus.title);
+
+  await verifyRequestItemTarget(
+    document,
+    getDisplayedRequests(store.getState()),
+    getSortedRequests(store.getState())[1],
+    "GET",
+    requestData.uri,
+    requestData.details
+  );
+
+  EventUtils.sendMouseEvent({ type: "mousedown" }, request);
+
+  const wait = waitForDOM(document, "#responseHeaders");
+  clickOnSidebarTab(document, "headers");
+  await wait;
+
+  const responseScope = document.querySelectorAll(
+    "#headers-panel .accordion tr[id^='/responseHeaders']"
+  );
+
+  const responseHeaders = [
+    {
+      name: "cache-control",
+      value: "max-age=10000",
+      index: 1,
+    },
+    {
+      name: "connection",
+      value: "close",
+      index: 2,
+    },
+    {
+      name: "content-length",
+      value: "53",
+      index: 3,
+    },
+    {
+      name: "content-type",
+      value: "text/javascript",
+      index: 4,
+    },
+    {
+      name: "server",
+      value: "httpd.js",
+      index: 6,
+    },
+  ];
+  responseHeaders.forEach(header => {
+    is(
+      responseScope[header.index - 1].querySelector(".treeLabel").innerHTML,
+      header.name,
+      `${header.name} label`
+    );
+    is(
+      responseScope[header.index - 1].querySelector(".objectBox").innerHTML,
+      `${header.value}`,
+      `${header.name} value`
+    );
+  });
+  await teardown(monitor);
+}
+
+add_task(async function test_module_cache_disabled() {
+  await do_test_module_cache(false);
+});
+
+add_task(async function test_module_cache_enabled() {
+  await do_test_module_cache(true);
 });
