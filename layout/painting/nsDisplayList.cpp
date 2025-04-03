a7d1486ccd27af38c353f7a97d43011f721fe91d
Jens Stutte <jstutte@mozilla.com>
2025-04-03 13:37:43 +0000

    Bug 1957634 - Remove obsolete TODO comment from the FramesWithDepth comparator. r=emilio DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D244337

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -2346,7 +2346,6 @@ struct FramesWithDepth {
     // We want to sort so that the shallowest item (highest depth value) is
     // first. Round to have some error tolerance (multiply with 8 translates
     // effectively to <<3).
-    // TODO: We might not want/need any tolerance here, see bug 1957561.
     double lDepth = round(mDepth * 8.);
     double rDepth = round(aOther.mDepth * 8.);
     return lDepth > rDepth;
