83761963d9a4c29e8abe6e4bbbd2fd7c3b27becc
Jan Varga <jvarga@mozilla.com>
2025-04-09 13:41:08 +0000

    Bug 1957334 - QM: Introduce ClientStorageScope class à la PersistenceScope; r=dom-storage-reviewers,jstutte
    Directory locks currently support specifying either a concrete client type or
    all client types. However, in the future, we will need more flexible scoping,
    for example, to support operations like updating the metadata file that resides
    alongside client directories within the same origin directory.
    
    Introducing a `ClientStorageScope` class, modeled after `PersistenceScope`,
    will make it easier to express such use cases and extend support for special
    purpose locks beyond just client types.
    
    This patch adds the basic structure and functionality of `ClientStorageScope`.
    Support for additional scope types such as metadata will be implemented in a
    follow-up patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D243629

---
+++ dom/quota/ClientStorageScope.h
@@ -0,0 +1,123 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef DOM_QUOTA_CLIENTSTORAGESCOPE_H_
+#define DOM_QUOTA_CLIENTSTORAGESCOPE_H_
+
+#include "mozilla/Assertions.h"
+#include "mozilla/Variant.h"
+#include "mozilla/dom/quota/Client.h"
+
+namespace mozilla::dom::quota {
+
+/**
+ * Represents a scope within an origin directory, currently covering either a
+ * specific client (`Client`) or a match-all scope (`Null`).
+ *
+ * The use of "Storage" in the class name is intentional. Unlike
+ * `PersistenceScope` and `OriginScope`, which match only specific directories,
+ * this scope is meant to cover all entries within an origin directory. That
+ * includes client specific folders (e.g., idb/, fs/) and, in the future, files
+ * like metadata that exist alongside them.
+ *
+ * This class provides operations to check the current scope type
+ * (`Client` or `Null`), set the scope type, retrieve a client type, and match
+ * it with another scope.
+ */
+class ClientStorageScope {
+  class Client {
+    quota::Client::Type mClientType;
+
+   public:
+    explicit Client(quota::Client::Type aClientType)
+        : mClientType(aClientType) {}
+
+    quota::Client::Type GetClientType() const { return mClientType; }
+  };
+
+  struct Null {};
+
+  using DataType = Variant<Client, Null>;
+
+  DataType mData;
+
+ public:
+  ClientStorageScope() : mData(Null()) {}
+
+  static ClientStorageScope CreateFromClient(quota::Client::Type aClientType) {
+    return ClientStorageScope(std::move(Client(aClientType)));
+  }
+
+  static ClientStorageScope CreateFromNull() {
+    return ClientStorageScope(std::move(Null()));
+  }
+
+  bool IsClient() const { return mData.is<Client>(); }
+
+  bool IsNull() const { return mData.is<Null>(); }
+
+  void SetFromClient(quota::Client::Type aClientType) {
+    mData = AsVariant(Client(aClientType));
+  }
+
+  void SetFromNull() { mData = AsVariant(Null()); }
+
+  quota::Client::Type GetClientType() const {
+    MOZ_ASSERT(IsClient());
+
+    return mData.as<Client>().GetClientType();
+  }
+
+  bool Matches(const ClientStorageScope& aOther) const {
+    struct Matcher {
+      const ClientStorageScope& mThis;
+
+      explicit Matcher(const ClientStorageScope& aThis) : mThis(aThis) {}
+
+      bool operator()(const Client& aOther) {
+        return mThis.MatchesClient(aOther);
+      }
+
+      bool operator()(const Null& aOther) { return true; }
+    };
+
+    return aOther.mData.match(Matcher(*this));
+  }
+
+ private:
+  // Move constructors
+  explicit ClientStorageScope(const Client&& aClient) : mData(aClient) {}
+
+  explicit ClientStorageScope(const Null&& aNull) : mData(aNull) {}
+
+  // Copy constructor
+  explicit ClientStorageScope(const DataType& aOther) : mData(aOther) {}
+
+  bool MatchesClient(const Client& aOther) const {
+    struct ClientMatcher {
+      const Client& mOther;
+
+      explicit ClientMatcher(const Client& aOther) : mOther(aOther) {}
+
+      bool operator()(const Client& aThis) {
+        return aThis.GetClientType() == mOther.GetClientType();
+      }
+
+      bool operator()(const Null& aThis) {
+        // Null covers everything.
+        return true;
+      }
+    };
+
+    return mData.match(ClientMatcher(aOther));
+  }
+
+  bool operator==(const ClientStorageScope& aOther) = delete;
+};
+
+}  // namespace mozilla::dom::quota
+
+#endif  // DOM_QUOTA_CLIENTSTORAGESCOPE_H_
---
+++ dom/quota/test/gtest/TestClientStorageScope.cpp
@@ -0,0 +1,65 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "gtest/gtest.h"
+#include "mozilla/dom/quota/ClientStorageScope.h"
+
+namespace mozilla::dom::quota {
+
+TEST(DOM_Quota_ClientStorageScope, SanityChecks)
+{
+  // Sanity checks.
+
+  {
+    const auto clientStorageScope(
+        ClientStorageScope::CreateFromClient(Client::IDB));
+    ASSERT_TRUE(clientStorageScope.IsClient());
+    ASSERT_EQ(clientStorageScope.GetClientType(), Client::IDB);
+  }
+
+  {
+    const auto clientStorageScope(ClientStorageScope::CreateFromNull());
+    ASSERT_TRUE(clientStorageScope.IsNull());
+  }
+}
+
+TEST(DOM_Quota_ClientStorageScope, MatchesClient)
+{
+  // Test each client storage scope type against particular client types.
+
+  {
+    const auto clientStorageScope(
+        ClientStorageScope::CreateFromClient(Client::IDB));
+
+    ASSERT_TRUE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::IDB)));
+    ASSERT_FALSE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::DOMCACHE)));
+    ASSERT_FALSE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::SDB)));
+    ASSERT_FALSE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::FILESYSTEM)));
+    ASSERT_FALSE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::LS)));
+  }
+
+  {
+    const auto clientStorageScope(ClientStorageScope::CreateFromNull());
+
+    ASSERT_TRUE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::IDB)));
+    ASSERT_TRUE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::DOMCACHE)));
+    ASSERT_TRUE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::SDB)));
+    ASSERT_TRUE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::FILESYSTEM)));
+    ASSERT_TRUE(clientStorageScope.Matches(
+        ClientStorageScope::CreateFromClient(Client::LS)));
+  }
+}
+
+}  //  namespace mozilla::dom::quota
