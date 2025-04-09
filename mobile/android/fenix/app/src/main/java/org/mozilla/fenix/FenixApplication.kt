601256c3cc6f397b018995810fd3f586570f50ee
smolnar <smolnar@mozilla.com>
2025-04-09 21:27:38 +0300

    Backed out changeset 5d63c8c8fd38 (bug 1947488) for causing fenix debug test failures CLOSED TREE

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
@@ -193,6 +193,12 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
         if (components.fenixOnboarding.userHasBeenOnboarded()) {
             initializeGlean(this, logger, settings.isTelemetryEnabled, components.core.client)
         }
+
+        // We avoid blocking the main thread on startup by setting startup metrics on the background thread.
+        val store = components.core.store
+        GlobalScope.launch(IO) {
+            setStartupMetrics(store, settings)
+        }
     }
 
     @VisibleForTesting
@@ -376,12 +382,11 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
         }
 
         fun queueMetrics() {
-            queue.runIfReadyOrQueue {
-                setStartupMetrics(components.core.store, settings())
-                // Because it may be slow to capture the storage stats, it might be preferred to
-                // create a WorkManager task for this metric, however, I ran out of
-                // implementation time and WorkManager is harder to test.
-                if (SDK_INT >= Build.VERSION_CODES.O) { // required by StorageStatsMetrics.
+            if (SDK_INT >= Build.VERSION_CODES.O) { // required by StorageStatsMetrics.
+                queue.runIfReadyOrQueue {
+                    // Because it may be slow to capture the storage stats, it might be preferred to
+                    // create a WorkManager task for this metric, however, I ran out of
+                    // implementation time and WorkManager is harder to test.
                     StorageStatsMetrics.report(this.applicationContext)
                 }
             }
@@ -715,13 +720,12 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
      */
     @Suppress("ComplexMethod", "LongMethod")
     @VisibleForTesting
-    @OptIn(DelicateCoroutinesApi::class)
     internal fun setStartupMetrics(
         browserStore: BrowserStore,
         settings: Settings,
         browsersCache: BrowsersCache = BrowsersCache,
         mozillaProductDetector: MozillaProductDetector = MozillaProductDetector,
-    ) = GlobalScope.launch(Dispatchers.IO) {
+    ) {
         setPreferenceMetrics(settings)
         with(Metrics) {
             // Set this early to guarantee it's in every ping from here on.
