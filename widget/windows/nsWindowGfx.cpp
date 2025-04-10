c10db731a1053abe8921f05b0bc6e899bd545d1a
Emilio Cobos Álvarez <emilio@crisal.io>
2025-04-10 20:52:22 +0000

    Bug 1958631 - Don't clear the window DC on unaccelerated windows.  a=pascalc
    Apparently, Windows can happily display your window half-painted, even
    though you're between ::BeginPaint() and ::EndPaint().
    
    For accelerated windows it doesn't really matter because we use DWM for
    the window contents, but for unbuffered windows like tooltips it does.
    
    This is the minimal patch for uplift, more cleanups incoming.
    
    Original Revision: https://phabricator.services.mozilla.com/D244524
    
    Differential Revision: https://phabricator.services.mozilla.com/D245092

--- widget/windows/nsWindowGfx.cpp
+++ widget/windows/nsWindowGfx.cpp
@@ -207,7 +207,8 @@ bool nsWindow::OnPaint(uint32_t aNestingLevel) {
     regionToClear.OrWith(ComputeNonClientRegion());
     mNeedsNCAreaClear = false;
   }
-  if (!regionToClear.IsEmpty()) {
+  if (!regionToClear.IsEmpty() &&
+      renderer->GetBackendType() != LayersBackend::LAYERS_NONE) {
     auto black = reinterpret_cast<HBRUSH>(::GetStockObject(BLACK_BRUSH));
     // We could use RegionToHRGN, but at least for simple regions (and possibly
     // for complex ones too?) FillRect is faster; see bug 1946365 comment 12.
