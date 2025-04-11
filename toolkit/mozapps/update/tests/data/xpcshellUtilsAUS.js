96841babfe3fe2c57a3f9efe20140202e499a084
Robin Steuber <bytesized@mozilla.com>
2025-04-11 23:49:24 +0000

    Bug 1923376 - Add an argument to the updater to convey which updater instance it is r=bytesized a=diannaS
    Note that this patch does not implement the proper usage of the argument. It reads the argument but then does nothing with it.
    
    Original Revision: https://phabricator.services.mozilla.com/D225421
    
    Differential Revision: https://phabricator.services.mozilla.com/D242459

--- toolkit/mozapps/update/tests/data/xpcshellUtilsAUS.js
+++ toolkit/mozapps/update/tests/data/xpcshellUtilsAUS.js
@@ -2030,14 +2030,16 @@ function runUpdate(
 
   setAppBundleModTime();
 
-  let args = [updatesDirPath, installDirPath];
-  if (aSwitchApp) {
-    args[2] = stageDirPath;
-    args[3] = pid + "/replace";
-  } else {
-    args[2] = applyToDirPath;
-    args[3] = pid;
-  }
+  // The version 3 argument format looks like
+  // updater 3 patch-dir install-dir apply-to-dir which-invocation [wait-pid [callback-working-dir callback-path args...]]
+  let args = [
+    "3",
+    updatesDirPath,
+    installDirPath,
+    aSwitchApp ? stageDirPath : applyToDirPath,
+    "first",
+    aSwitchApp ? pid + "/replace" : pid,
+  ];
 
   let launchBin = gIsServiceTest && isInvalidArgTest ? callbackApp : updateBin;
 
@@ -2045,6 +2047,8 @@ function runUpdate(
     args = args.concat([callbackApp.parent.path, callbackApp.path]);
     args = args.concat(gCallbackArgs);
   } else if (gIsServiceTest) {
+    // We are jumping straight to the second invocation in this case
+    args[4] = "second";
     args = ["launch-service", updateBin.path].concat(args);
   } else if (aCallbackPath) {
     args = args.concat([callbackApp.parent.path, aCallbackPath]);
