2e34c227b98d6f3276cff51d6ce60acf4bab0c28
Erich Gubler <erichdongubler@gmail.com>
2025-04-11 01:44:33 +0000

    Bug 1959285 - test: improve diagnostic for missing test manifest base revision r=ahal
    When `git log` is given an argument it doesn't recognize as a commit
    hash in its database, it tries to resolve it as a path in the repository
    at `HEAD`. If that doesn't refer to a path that exists, then it bails
    with a fatal error at the ambiguity, like so:
    
    ```
    fatal: ambiguous argument 'b56a9af51519d2e77e05efa672a13e6be2e9ebc': unknown revision or path not in the working tree.
    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'
    Error running mach:
    
    ┆   mach test dom/serviceworkers/test/test_serviceworker_interfaces.js
    ```
    
    In the case of fetching a test manifest for, say, a `mach test`
    invocation, this is unnecessarily confusing, because we intend a commit
    hash. Improve the diagnostic emitted in this case using the `--`
    argument suggested by Git CLI in the above message. This clarifies the
    diagnostic to the following:
    
    ```
    fatal: bad revision 'b56a9af51519d2e77e05efa672a13e6be2e9ebc'
    Error running mach:
    
    ┆   mach test dom/serviceworkers/test/test_serviceworker_interfaces.js
    ```
    
    Differential Revision: https://phabricator.services.mozilla.com/D244840

--- testing/web-platform/manifestdownload.py
+++ testing/web-platform/manifestdownload.py
@@ -51,6 +51,7 @@ def get_commits(logger, repo_root):
                 "--format=%H",
                 "-n50",
                 base_rev,
+                "--",
                 "testing/web-platform/tests",
                 "testing/web-platform/mozilla/tests",
             ).splitlines()
