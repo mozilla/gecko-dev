dd0b0181c40124d9d341fccaf708a0617d3a67ef
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2025-04-14 14:04:33 +0000

    Bug 1958070 - move giant actor list out of BrowserGlue into a dedicated file, r=firefox-desktop-core-reviewers ,mossop
    What it says on the tin, really.
    
    This mostly makes it easier to find this list due to naming, and helps contain
    startup logic to BrowserGlue proper. A giant definition list of actors is not
    really logic - if there weren't some `onPreferenceChanged` handlers, we could
    consider moving it all to a yaml file or other mechanism and compile it in...
    
    Differential Revision: https://phabricator.services.mozilla.com/D244423

--- browser/base/content/test/performance/browser_startup.js
+++ browser/base/content/test/performance/browser_startup.js
@@ -29,6 +29,7 @@ const startupPhases = {
     allowlist: {
       modules: new Set([
         "resource:///modules/BrowserGlue.sys.mjs",
+        "moz-src:///browser/components/DesktopActorRegistry.sys.mjs",
         "resource:///modules/StartupRecorder.sys.mjs",
         "resource://gre/modules/AppConstants.sys.mjs",
         "resource://gre/modules/ActorManagerParent.sys.mjs",
