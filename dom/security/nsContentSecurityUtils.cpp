f6ab798ce07296f486481953e2e8ba1b9e674f02
Simon Friedberger <simon@mozilla.com>
2025-04-14 21:20:55 +0000

    Bug 1958801: Add CSP to geckoview.xhtml r=geckoview-reviewers,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D245441

--- dom/security/nsContentSecurityUtils.cpp
+++ dom/security/nsContentSecurityUtils.cpp
@@ -1969,7 +1969,6 @@ void nsContentSecurityUtils::AssertChromePageHasCSP(Document* aDocument) {
 
   static nsLiteralCString sAllowedChromePagesWithNoCSP[] = {
       "chrome://browser/content/shopping/review-checker.xhtml"_ns,
-      "chrome://geckoview/content/geckoview.xhtml"_ns,
       // Test files
       "chrome://mochikit/"_ns,
       "chrome://mochitests/"_ns,
