b0545a14369db7b1cd269b2a8e9855f7f0b263ee
Cosmin Sabou <csabou@mozilla.com>
2025-04-15 21:40:50 +0300

    Backed out changeset 875d51727bbd (bug 1950001) for causing xpcshell failures on test_curl. CLOSED TREE

--- devtools/client/shared/test/xpcshell/test_curl.js
+++ devtools/client/shared/test/xpcshell/test_curl.js
@@ -230,15 +230,18 @@ add_task(async function () {
   );
 
   // Check binary data
-  const dataBinaryParam = `--data-binary \\\n  ${isWin() ? "" : "$"}'------------14808\\r\\n`;
+  const dataBinaryPos = cmd.indexOf("--data-binary");
+  const dataBinaryParam = `--data-binary ${isWin() ? "" : "$"}${escapeNewline(
+    quote(request.postDataText)
+  )}`;
   Assert.notStrictEqual(
-    cmd.indexOf("--data-binary"),
+    dataBinaryPos,
     -1,
     "--data-binary param present in curl output"
   );
-
-  Assert.ok(
-    cmd.includes(dataBinaryParam),
+  equal(
+    cmd.substr(dataBinaryPos, dataBinaryParam.length),
+    dataBinaryParam,
     "proper multipart data present in curl output"
   );
 });
@@ -350,6 +353,14 @@ function quote(str) {
   return QUOTE + escaped + QUOTE;
 }
 
+function escapeNewline(txt) {
+  if (isWin()) {
+    // Add `"` to close quote, then escape newline outside of quote, then start new quote
+    return txt.replace(/[\r\n]{1,2}/g, '"^$&$&"');
+  }
+  return txt.replace(/\r/g, "\\r").replace(/\n/g, "\\n");
+}
+
 // Header param is formatted as -H "Header: value" or -H 'Header: value'
 function headerParam(h) {
   return "-H " + quote(h);
@@ -381,6 +392,6 @@ function parseCurl(curlCmd) {
   // This monster regexp parses the command line into an array of arguments,
   // recognizing quoted args with matching quotes and escaped quotes inside:
   // [ "curl 'url'", "--standalone-arg", "-arg-with-quoted-string 'value\'s'" ]
-  const matchRe = /[-A-Za-z1-9]+(?: ([\^\\"']+)(?:\\\1|.)*?\1)?/g;
+  const matchRe = /[-A-Za-z1-9]+(?: \$?([\"'])(?:\\\1|.)*?\1)?/g;
   return curlCmd.match(matchRe);
 }
