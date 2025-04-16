984b1d3a696fc694b26b0d190005901a6bcf5ccf
Henrik Skupin <mail@hskupin.info>
2025-04-16 09:41:58 +0000

    Bug 1960734 - [remote] Improve documentation for recommended preferences for test suites using profiles from "/testing/profiles". r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D245691

--- remote/shared/RecommendedPreferences.sys.mjs
+++ remote/shared/RecommendedPreferences.sys.mjs
@@ -43,6 +43,7 @@ if (Services.appinfo.processType != Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT) {
 // - remote/test/puppeteer/packages/browsers/src/browser-data/firefox.ts
 // - testing/geckodriver/src/prefs.rs
 // - testing/marionette/client/marionette_driver/geckoinstance.py
+// - testing/profiles/
 //
 // The preferences in `firefox.ts`, `prefs.rs` and `geckoinstance.py`
 // will be applied before the application starts, and should typically be used
@@ -61,6 +62,10 @@ if (Services.appinfo.processType != Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT) {
 // several lists of preferences, either common or specific to a given application
 // (Firefox Desktop, Fennec, Thunderbird).
 //
+// Some test types may disable recommended preferences. Search for
+// `"remote.prefs.recommended", false` in `/testing/profiles` to find
+// `user.js` files that do so.
+//
 // Depending on how users interact with the Remote Agent, they will use different
 // combinations of preferences. So it's important to update the preferences files
 // so that all users have the proper preferences.
@@ -73,6 +78,8 @@ if (Services.appinfo.processType != Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT) {
 //   - Create a PR to upstream the change on `browser-data/firefox.ts` to puppeteer
 // - Otherwise, if the preference can be set after startup:
 //   - Add the preference to `RecommendedPreferences.sys.mjs`
+// - If a `user.js` file in `/testing/profiles` disables recommended preferences,
+//   identify affected test suites and set the new preference where needed.
 const COMMON_PREFERENCES = new Map([
   // Make sure Shield doesn't hit the network.
   ["app.normandy.api_url", ""],
