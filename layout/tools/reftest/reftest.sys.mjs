0fda1adaf8ce2730f8a3c4838453850bc3c7494e
Simon Friedberger <simon@mozilla.com>
2025-04-17 15:32:52 +0000

    Bug 1958801: Use browser.style instead of setAttribute r=tschuster,webdriver-reviewers,whimboo,dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D245562

--- layout/tools/reftest/reftest.sys.mjs
+++ layout/tools/reftest/reftest.sys.mjs
@@ -189,9 +189,16 @@ export function OnRefTestLoad(win) {
   g.browser.setAttribute("remote", g.browserIsRemote ? "true" : "false");
   // Make sure the browser element is exactly 800x1000, no matter
   // what size our window is
-  g.browser.setAttribute(
-    "style",
-    "padding: 0px; margin: 0px; border:none; min-width: 800px; min-height: 1000px; max-width: 800px; max-height: 1000px; color-scheme: env(-moz-content-preferred-color-scheme)"
+  g.browser.style.setProperty("padding", "0px");
+  g.browser.style.setProperty("margin", "0px");
+  g.browser.style.setProperty("border", "none");
+  g.browser.style.setProperty("min-width", "800px");
+  g.browser.style.setProperty("min-height", "1000px");
+  g.browser.style.setProperty("max-width", "800px");
+  g.browser.style.setProperty("max-height", "1000px");
+  g.browser.style.setProperty(
+    "color-scheme",
+    "env(-moz-content-preferred-color-scheme)"
   );
 
   if (Services.appinfo.OS == "Android") {
@@ -1214,7 +1221,7 @@ function RecordResult(testRunTime, errorMsg, typeSpecificResults) {
         CleanUpCrashDumpFiles();
         StartCurrentURI(URL_TARGET_TYPE_REFERENCE);
         break;
-      case URL_TARGET_TYPE_REFERENCE:
+      case URL_TARGET_TYPE_REFERENCE: {
         let pathToTestPdf = g.testPrintOutput;
         let pathToRefPdf = typeSpecificResults;
         comparePdfs(pathToTestPdf, pathToRefPdf, function (error, results) {
@@ -1251,7 +1258,7 @@ function RecordResult(testRunTime, errorMsg, typeSpecificResults) {
             }
             results.forEach(function (result) {
               output = outputPair[result.passed];
-              let extra = { status_msg: output.n };
+              let extraOpt = { status_msg: output.n };
               ++g.testResults[output.n];
               logger.testEnd(
                 g.urls[0].identifier,
@@ -1259,13 +1266,14 @@ function RecordResult(testRunTime, errorMsg, typeSpecificResults) {
                 output.s[1],
                 result.description,
                 null,
-                extra
+                extraOpt
               );
             });
           }
           FinishTestItem();
         });
         break;
+      }
       default:
         throw new Error("Unexpected state.");
     }
@@ -1323,18 +1331,18 @@ function RecordResult(testRunTime, errorMsg, typeSpecificResults) {
     }
     var index = 0;
     typeSpecificResults.forEach(function (result) {
-      var output = outputPair[result.passed];
-      var extra = { status_msg: output.n };
+      var output2 = outputPair[result.passed];
+      var extraOpt = { status_msg: output2.n };
 
-      ++g.testResults[output.n];
+      ++g.testResults[output2.n];
       logger.testStatus(
         g.urls[0].identifier,
         result.description + " item " + ++index,
-        output.s[0],
-        output.s[1],
+        output2.s[0],
+        output2.s[1],
         null,
         null,
-        extra
+        extraOpt
       );
     });
 
@@ -1370,7 +1378,7 @@ function RecordResult(testRunTime, errorMsg, typeSpecificResults) {
       CleanUpCrashDumpFiles();
       StartCurrentURI(URL_TARGET_TYPE_REFERENCE);
       break;
-    case URL_TARGET_TYPE_REFERENCE:
+    case URL_TARGET_TYPE_REFERENCE: {
       // Both documents have been loaded. Compare the renderings and see
       // if the comparison result matches the expected result specified
       // in the manifest.
@@ -1588,6 +1596,7 @@ function RecordResult(testRunTime, errorMsg, typeSpecificResults) {
       CleanUpCrashDumpFiles();
       FinishTestItem();
       break;
+    }
     default:
       throw new Error("Unexpected state.");
   }
@@ -2236,12 +2245,12 @@ function comparePdfs(pathToTestPdf, pathToRefPdf, callback) {
                     let refTextItems = texts[1].items;
                     let testText;
                     let refText;
-                    let passed = refTextItems.every(function (o, i) {
+                    let passed = refTextItems.every(function (o, index) {
                       refText = o.str;
-                      if (!testTextItems[i]) {
+                      if (!testTextItems[index]) {
                         return false;
                       }
-                      testText = testTextItems[i].str;
+                      testText = testTextItems[index].str;
                       return testText === refText;
                     });
                     let description;
