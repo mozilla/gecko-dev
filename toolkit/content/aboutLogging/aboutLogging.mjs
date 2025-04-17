cff867fd4ab6c869685261f3de8e01716e77f9c7
Daniel Holbert <dholbert@cs.stanford.edu>
2025-04-17 23:06:38 +0000

    Bug 1961287: Add print logging to the Graphics preset on about:logging. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D245998

--- toolkit/content/aboutLogging/aboutLogging.mjs
+++ toolkit/content/aboutLogging/aboutLogging.mjs
@@ -150,7 +150,7 @@ const gLoggingPresets = {
   },
   gfx: {
     modules:
-      "webrender::*:5,webrender_bindings::*:5,webrender_types::*:5,gfx2d:5,WebRenderBridgeParent:5,DcompSurface:5,apz.displayport:5,layout:5,dl.content:5,dl.parent:5,nsRefreshDriver:5,fontlist:5,fontinit:5,textrun:5,textrunui:5,textperf:5",
+      "webrender::*:5,webrender_bindings::*:5,webrender_types::*:5,gfx2d:5,WebRenderBridgeParent:5,DcompSurface:5,apz.displayport:5,layout:5,dl.content:5,dl.parent:5,nsRefreshDriver:5,fontlist:5,fontinit:5,textrun:5,textrunui:5,textperf:5,printing:5",
     l10nIds: {
       label: "about-logging-preset-gfx-label",
       description: "about-logging-preset-gfx-description",
