4015cb28cdca32c1e123f448901c503765f5acac
longsonr <longsonr@gmail.com>
2025-04-17 20:41:55 +0000

    Bug 1954044 - Implement https://github.com/w3c/editing/issues/483 so that setPathData can accept a plain javascript object that matches the structure of SVGPathSegment r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D245342

--- dom/svg/SVGPathElement.h
+++ dom/svg/SVGPathElement.h
@@ -18,6 +18,7 @@ nsresult NS_NewSVGPathElement(
 namespace mozilla::dom {
 
 struct SVGPathDataSettings;
+struct SVGPathSegmentInit;
 class SVGPathSegment;
 
 using SVGPathElementBase = SVGGeometryElement;
@@ -85,7 +86,7 @@ class SVGPathElement final : public SVGPathElementBase {
   MOZ_CAN_RUN_SCRIPT
   void GetPathData(const SVGPathDataSettings& aOptions,
                    nsTArray<RefPtr<SVGPathSegment>>& aValues);
-  void SetPathData(const Sequence<OwningNonNull<SVGPathSegment>>& aValues);
+  void SetPathData(const Sequence<SVGPathSegmentInit>& aValues);
 
   static bool IsDPropertyChangedViaCSS(const ComputedStyle& aNewStyle,
                                        const ComputedStyle& aOldStyle);
