9da6f0197de675748913e631ca2d9f23e818bc4b
scottdowne <sdowne@mozilla.com>
2025-04-17 13:28:11 +0000

    Bug 1960875 - Newtab custom wallpapers fixing category item icon  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D245769
    
    Differential Revision: https://phabricator.services.mozilla.com/D245839

--- browser/extensions/newtab/data/content/activity-stream.bundle.js
+++ browser/extensions/newtab/data/content/activity-stream.bundle.js
@@ -12085,7 +12085,7 @@ class _WallpaperCategories extends (external_React_default()).PureComponent {
       }
       return /*#__PURE__*/external_React_default().createElement("div", {
         key: category
-      }, /*#__PURE__*/external_React_default().createElement("input", WallpaperCategories_extends({
+      }, /*#__PURE__*/external_React_default().createElement("button", WallpaperCategories_extends({
         ref: el => {
           if (el) {
             this.categoryRef[index] = el;
@@ -12093,7 +12093,6 @@ class _WallpaperCategories extends (external_React_default()).PureComponent {
         },
         id: category,
         style: style,
-        type: "button",
         onKeyDown: e => this.handleCategoryKeyDown(e, category)
         // Add overrides for custom wallpaper upload UI
         ,
