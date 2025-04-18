40f6c733ad10d3792cb5e77d8b1e0546cec18d40
Dão Gottwald <dao@mozilla.com>
2025-04-18 03:14:59 +0000

    Bug 1961224 - Remove bogus "panel" class from tab group panel. r=sthompson,tabbrowser-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D245959

--- browser/components/tabbrowser/content/tabgroup-menu.js
+++ browser/components/tabbrowser/content/tabgroup-menu.js
@@ -200,7 +200,7 @@
     static markup = /*html*/ `
     <panel
         type="arrow"
-        class="panel tab-group-editor-panel"
+        class="tab-group-editor-panel"
         orient="vertical"
         role="dialog"
         ignorekeys="true"
