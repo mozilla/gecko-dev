c7bb0874c978106dca43a6ffd448e11076e89da4
Erich Gubler <erichdongubler@gmail.com>
2025-04-18 15:22:08 +0000

    Bug 1960961 - refactor(webgpu): clarify early `requestAdapter` rejection logic r=webgpu-reviewers,teoxoy
    Differential Revision: https://phabricator.services.mozilla.com/D245776

--- dom/webgpu/Instance.cpp
+++ dom/webgpu/Instance.cpp
@@ -105,37 +105,36 @@ already_AddRefed<dom::Promise> Instance::RequestAdapter(
   // -
   // Check if we should allow the request.
 
-  const auto errStr = [&]() -> std::optional<std::string_view> {
-#ifndef EARLY_BETA_OR_EARLIER
-    if (true) {
-      return "WebGPU is not yet available in Release or late Beta builds.";
+  std::optional<std::string_view> rejectionMessage = {};
+  const auto rejectIf = [&rejectionMessage](bool condition,
+                                            const char* message) {
+    if (condition && !rejectionMessage.has_value()) {
+      rejectionMessage = message;
     }
+  };
 
-    // NOTE: Deliberately left after the above check so that we only enter
-    // here if it's removed. Above is a more informative diagnostic, while the
-    // check is still present.
-    //
-    // Follow-up to remove this check:
-    // <https://bugzilla.mozilla.org/show_bug.cgi?id=1942431>
-    if (dom::WorkerPrivate* wp = dom::GetCurrentThreadWorkerPrivate()) {
-      if (wp->IsServiceWorker()) {
-        return "WebGPU in service workers is not yet available in Release or "
-               "late Beta builds; see "
-               "<https://bugzilla.mozilla.org/show_bug.cgi?id=1942431>.";
-      }
-    }
+#ifndef EARLY_BETA_OR_EARLIER
+  rejectIf(true, "WebGPU is not yet available in Release or late Beta builds.");
+
+  // NOTE: Deliberately left after the above check so that we only enter
+  // here if it's removed. Above is a more informative diagnostic, while the
+  // check is still present.
+  //
+  // Follow-up to remove this check:
+  // <https://bugzilla.mozilla.org/show_bug.cgi?id=1942431>
+  if (dom::WorkerPrivate* wp = dom::GetCurrentThreadWorkerPrivate()) {
+    rejectIf(wp->IsServiceWorker(),
+             "WebGPU in service workers is not yet available in Release or "
+             "late Beta builds; see "
+             "<https://bugzilla.mozilla.org/show_bug.cgi?id=1942431>.");
+  }
 #endif
-    if (!gfx::gfxVars::AllowWebGPU()) {
-      return "WebGPU is disabled by blocklist.";
-    }
-    if (!StaticPrefs::dom_webgpu_enabled()) {
-      return "WebGPU is disabled because the `dom.webgpu.enabled` pref. is set "
-             "to `false`.";
-    }
-    return {};
-  }();
-  if (errStr) {
-    promise->MaybeRejectWithNotSupportedError(ToCString(*errStr));
+  rejectIf(!gfx::gfxVars::AllowWebGPU(), "WebGPU is disabled by blocklist.");
+  rejectIf(!StaticPrefs::dom_webgpu_enabled(),
+           "WebGPU is disabled because the `dom.webgpu.enabled` pref. is set "
+           "to `false`.");
+  if (rejectionMessage) {
+    promise->MaybeRejectWithNotSupportedError(ToCString(*rejectionMessage));
     return promise.forget();
   }
 
