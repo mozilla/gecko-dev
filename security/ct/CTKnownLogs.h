63f1e9a9f93b12d2dcb9d40cf3b48f308c2d4f54
ffxbld <ffxbld@mozilla.com>
2025-04-21 12:37:17 +0000

    No Bug, mozilla-central repo-update HSTS HPKP remote-settings mobile-experiments ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D246096

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1750936836000000);
+static const PRTime kCTExpirationTime = INT64_C(1751282394000000);
 
 namespace mozilla::ct {
 
