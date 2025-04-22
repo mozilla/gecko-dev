2cbe166b1327c31d9a462ee66568479833e2cd2a
Butkovits Atila <abutkovits@mozilla.com>
2025-04-22 23:39:03 +0300

    Backed out changeset 783b3a296474 (bug 1957102) for causing Gecko Bustage. CLOSED TREE

--- toolkit/components/ml/content/EngineProcess.sys.mjs
+++ toolkit/components/ml/content/EngineProcess.sys.mjs
@@ -150,10 +150,6 @@ const FEATURES = {
   "smart-tab-topic": {
     engineId: "smart-tab-topic-engine",
   },
-  // see toolkit/components/ml/content/nlp/EmbeddingsGenerator.sys.mjs
-  "simple-text-embedder": {
-    engineId: "simple-text-embedder-engine",
-  },
 };
 
 /**
