f0407ec47549808af7d1487ed35a8b4019efc8cf
Goloman Adrian <agoloman@mozilla.com>
2025-04-23 16:28:48 +0300

    Backed out 2 changesets (bug 1953949, bug 1954538) for causing bc failures @browser_test_profile_selector.js.
    Backed out changeset 298aa10942ea (bug 1954538)
    Backed out changeset d08e2e97aa83 (bug 1953949)

--- browser/components/profiles/tests/browser/browser_test_db_lazily_created.js
+++ browser/components/profiles/tests/browser/browser_test_db_lazily_created.js
@@ -3,24 +3,14 @@
 
 "use strict";
 
-const { sinon } = ChromeUtils.importESModule(
-  "resource://testing-common/Sinon.sys.mjs"
-);
-
-add_setup(() => {
-  // Mock the executable process so we doon't launch a new process
-  sinon.stub(SelectableProfileService, "execProcess");
-
-  registerCleanupFunction(() => {
-    sinon.restore();
-  });
-});
-
 add_task(async function test_dbLazilyCreated() {
   Assert.ok(
     !SelectableProfileService.initialized,
     `Selectable Profile Service should not be initialized because the default profile has no storeID`
   );
+
+  // Mock the executable process so we doon't launch a new process
+  SelectableProfileService.execProcess = () => {};
 
   await SelectableProfileService.maybeSetupDataStore();
   ok(
