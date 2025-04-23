b67bbde72273192e75e19b6f18759ea2840dc225
Thomas Wisniewski <twisniewski@mozilla.com>
2025-04-23 22:38:14 +0000

    Bug 1961048 - add a test which ensures that toggling interventions on and off in about:compat works; r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D245823

--- browser/extensions/webcompat/tests/browser/browser_aboutcompat.js
+++ browser/extensions/webcompat/tests/browser/browser_aboutcompat.js
@@ -17,6 +17,99 @@ add_task(async function test_about_compat_loads_properly() {
       "SmartBlock shims are listed"
     );
     ok(true, "Interventions are listed");
+
+    // also choose an intervention and a shim with content-scripts, and confirm that toggling them
+    // on and off works (by checking that their content-scripts are de-registered and re-registered).
+    const bgWin = content.wrappedJSObject.browser.extension.getBackgroundPage();
+    const interventionWithContentScripts =
+      bgWin.interventions._availableInterventions.find(
+        i => i.active && i.interventions?.find(v => v.content_scripts)
+      );
+    const shimWithContentScripts = [...bgWin.shims.shims.values()].find(
+      s => s._contentScriptRegistrations.length
+    );
+
+    async function findRegisteredScript(id) {
+      return (
+        await content.wrappedJSObject.browser.scripting.getRegisteredContentScripts(
+          { ids: [id] }
+        )
+      )[0];
+    }
+
+    // both should have their content scripts registered at startup
+    const interventionRCSId = `webcompat intervention for ${interventionWithContentScripts.label}`;
+    const shimRCSId = `shim-${shimWithContentScripts.id}-0`;
+    ok(
+      await findRegisteredScript(interventionRCSId),
+      `Found registered script for intervention: '${interventionRCSId}'`
+    );
+    ok(
+      await findRegisteredScript(shimRCSId),
+      `Found registered script for shim: '${shimRCSId}'`
+    );
+
+    async function testToggling(
+      buttonEnabled,
+      buttonDisabled,
+      interventionOrShim,
+      rcsId,
+      type
+    ) {
+      // click to disable the intervention/shim
+      content.document.querySelector(buttonEnabled).click();
+      await ContentTaskUtils.waitForCondition(
+        () => content.document.querySelector(buttonDisabled),
+        `toggle button for ${type} now says 'enable'`
+      );
+
+      await ContentTaskUtils.waitForCondition(
+        () => !interventionOrShim.active && !interventionOrShim.enabled,
+        `${type} is inactive`
+      );
+
+      // verify that its content scripts have been de-registered
+      ok(
+        !(await findRegisteredScript(rcsId)),
+        `Found no registered script for ${type}: '${rcsId}'`
+      );
+
+      // click to re-enable the intervention/shim
+      content.document.querySelector(buttonDisabled).click();
+      await ContentTaskUtils.waitForCondition(
+        () => content.document.querySelector(buttonEnabled),
+        `toggle button for ${type} again says 'disable'`
+      );
+
+      await ContentTaskUtils.waitForCondition(
+        () => interventionOrShim.active || interventionOrShim.enabled,
+        `${type} is active`
+      );
+
+      // verify that its content scripts have been re-registered
+      ok(
+        await findRegisteredScript(rcsId),
+        `Found registered script for ${type}: '${rcsId}'`
+      );
+    }
+
+    // toggle the intervention
+    await testToggling(
+      `tr[data-id='${interventionWithContentScripts.id}'] button[data-l10n-id=label-disable]`,
+      `tr[data-id='${interventionWithContentScripts.id}'] button[data-l10n-id=label-enable]`,
+      interventionWithContentScripts,
+      interventionRCSId,
+      "intervention"
+    );
+
+    // toggle the shim
+    await testToggling(
+      `tr[data-id='${shimWithContentScripts.id}'] button[data-l10n-id=label-disable]`,
+      `tr[data-id='${shimWithContentScripts.id}'] button[data-l10n-id=label-enable]`,
+      shimWithContentScripts,
+      shimRCSId,
+      "shim"
+    );
   });
 
   await BrowserTestUtils.removeTab(tab);
