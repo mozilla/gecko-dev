188e43d0035ed8f0cd71a0385c6e87fcd5c5500a
Sebastian Hengst <archaeopteryx@coole-files.de>
2025-04-23 19:18:23 +0000

    Bug 1952611 - revert setting automatic fluent support for accesskey for moz-button to fix bug 1942186. r=akulyk,reusable-components-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D246486

--- toolkit/content/widgets/moz-button/moz-button.stories.mjs
+++ toolkit/content/widgets/moz-button/moz-button.stories.mjs
@@ -69,6 +69,7 @@ const Template = ({
   <moz-button
     @click=${clickHandler}
     data-l10n-id=${l10nId}
+    data-l10n-attrs="label"
     type=${type}
     size=${size}
     ?disabled=${disabled}
