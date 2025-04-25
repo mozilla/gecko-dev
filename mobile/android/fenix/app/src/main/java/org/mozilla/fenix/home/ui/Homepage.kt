6e5ad1d2275a32827de80b522b1ba7ff0a5da0c5
Devota Aabel <daabel@mozilla.com>
2025-04-25 16:33:23 +0000

    Bug 1962554- Add bookmarks impression telemetry to compose homepage. r=gl,android-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D246681

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/ui/Homepage.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/ui/Homepage.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.unit.dp
 import mozilla.components.compose.base.button.TertiaryButton
 import mozilla.telemetry.glean.private.NoExtras
 import org.mozilla.fenix.GleanMetrics.History
+import org.mozilla.fenix.GleanMetrics.HomeBookmarks
 import org.mozilla.fenix.GleanMetrics.RecentlyVisitedHomepage
 import org.mozilla.fenix.R
 import org.mozilla.fenix.compose.MessageCard
@@ -285,6 +286,10 @@ private fun BookmarksSection(
     cardBackgroundColor: Color,
     interactor: BookmarksInteractor,
 ) {
+    LaunchedEffect(Unit) {
+        HomeBookmarks.shown.record(NoExtras())
+    }
+
     Spacer(modifier = Modifier.height(40.dp))
 
     HomeSectionHeader(
