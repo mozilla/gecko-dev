307316c77977f5a77d3af5a582098918b6b0ab43
Jan-Niklas Jaeschke <jjaschke@mozilla.com>
2025-04-28 13:58:38 +0000

    Bug 1959959 - Navigation API: Implement `navigation.reload()`. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D246587

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -4142,12 +4142,15 @@ class Document : public nsINode,
     --mThrowOnDynamicMarkupInsertionCounter;
   }
 
+  // https://html.spec.whatwg.org/#unload-counter
   bool ShouldIgnoreOpens() const { return mIgnoreOpensDuringUnloadCounter; }
 
+  // https://html.spec.whatwg.org/#unload-counter
   void IncrementIgnoreOpensDuringUnloadCounter() {
     ++mIgnoreOpensDuringUnloadCounter;
   }
 
+  // https://html.spec.whatwg.org/#unload-counter
   void DecrementIgnoreOpensDuringUnloadCounter() {
     MOZ_ASSERT(mIgnoreOpensDuringUnloadCounter);
     --mIgnoreOpensDuringUnloadCounter;
