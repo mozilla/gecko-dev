833ffcfd370e8701d48bbb8678fb3a6ab74b8504
Aaron Train <aaron.train@gmail.com>
2025-04-29 18:01:48 +0000

    Bug 1962708 - Clean and update android-components UI test Task transform r=isabel_rios
    Differential Revision: https://phabricator.services.mozilla.com/D246796

--- taskcluster/android_taskgraph/transforms/ui_tests.py
+++ taskcluster/android_taskgraph/transforms/ui_tests.py
@@ -32,14 +32,45 @@ def set_component_attribute(config, tasks):
 @transforms.add
 def define_ui_test_command_line(config, tasks):
     for task in tasks:
+        component = task["attributes"]["component"]
+        device_type = "arm"  # This maps to existing Flank configurations in automation/taskcluster/androidTest (e.g, flank-arm.yml)
+
+        apk_app, apk_test = None, None
+
+        if component == "samples-browser":
+            # Case 2: Exact match for "samples-browser" – gecko paths with "-debug"
+            apk_app = "./samples/browser/build/outputs/apk/gecko/debug/samples-browser-gecko-debug.apk"
+            apk_test = "./samples/browser/build/outputs/apk/androidTest/gecko/debug/samples-browser-gecko-debug-androidTest.apk"
+
+        elif component.startswith("samples-"):
+            # Case 3: Other samples-* (e.g., samples-glean)
+            sample = component.replace("samples-", "")
+            apk_app = (
+                f"./samples/{sample}/build/outputs/apk/debug/samples-{sample}-debug.apk"
+            )
+            apk_test = f"./samples/{sample}/build/outputs/apk/androidTest/debug/samples-{sample}-debug-androidTest.apk"
+
+        elif "-" in component:
+            # Case 1a: Component with dash (e.g., feature-share → components/feature/share)
+            category, submodule = component.split("-", 1)
+            apk_app = "./samples/browser/build/outputs/apk/gecko/debug/samples-browser-gecko-debug.apk"
+            apk_test = f"./components/{category}/{submodule}/build/outputs/apk/androidTest/debug/{component}-debug-androidTest.apk"
+
+        else:
+            # Case 1b: Component with no dash (e.g., browser → components/browser/engine-gecko)
+            apk_app = "./samples/browser/build/outputs/apk/gecko/debug/samples-browser-gecko-debug.apk"
+            apk_test = f"./components/{component}/engine-gecko/build/outputs/apk/androidTest/debug/browser-engine-gecko-debug-androidTest.apk"
+
         run = task.setdefault("run", {})
         post_gradlew = run.setdefault("post-gradlew", [])
         post_gradlew.append(
             [
-                "automation/taskcluster/androidTest/ui-test.sh",
-                task["attributes"]["component"],
-                "arm",
-                "1",
+                "python3",
+                "../../../taskcluster/scripts/tests/test-lab.py",
+                device_type,
+                apk_app,
+                "--apk_test",
+                apk_test,
             ]
         )
 
