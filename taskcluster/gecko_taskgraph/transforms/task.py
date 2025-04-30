81907b158181cd8d9ea22d23c3192ad902c2f298
Ben Hearsum <ben@mozilla.com>
2025-04-30 16:20:41 +0000

    Bug 1948315: switch release-version-bump to landoscript r=releng-reviewers,taskgraph-reviewers,ahal
    Tested on try in: https://treeherder.mozilla.org/jobs?repo=try&searchStr=version-bump&revision=83b0a833fe77e0a01510e7178f5b912300541fe4
    
    Differential Revision: https://phabricator.services.mozilla.com/D246674

--- taskcluster/gecko_taskgraph/transforms/task.py
+++ taskcluster/gecko_taskgraph/transforms/task.py
@@ -1569,6 +1569,7 @@ def build_treescript_payload(config, task, task_def):
                 ],
             }
         ],
+        Optional("bump-files"): [str],
     },
 )
 def build_landoscript_payload(config, task, task_def):
@@ -1656,6 +1657,13 @@ def build_landoscript_payload(config, task, task_def):
         }
         task_def["payload"]["tag_info"] = tag_info
         actions.append("tag")
+
+    if worker.get("bump-files"):
+        bump_info = {}
+        bump_info["next_version"] = release_config["next_version"]
+        bump_info["files"] = worker["bump-files"]
+        task_def["payload"]["version_bump_info"] = bump_info
+        actions.append("version_bump")
 
 
 @payload_builder(
