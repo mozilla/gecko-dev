ab6153dc22c7750660a130f66ff71181f650dfcd
Tom Schuster <tschuster@mozilla.com>
2025-05-02 08:25:42 +0000

    Bug 1955578 - Collect clobbered HTMLDocument properties. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D242534

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -12048,6 +12048,7 @@ void Document::Destroy() {
   }
 
   ReportDocumentUseCounters();
+  ReportShadowedHTMLDocumentProperties();
   ReportLCP();
   SetDevToolsWatchingDOMMutations(false);
 
@@ -16929,6 +16930,18 @@ void Document::ReportDocumentUseCounters() {
       printf_stderr("USE_COUNTER_DOCUMENT: %s - %s\n", metricName,
                     urlForLogging->get());
     }
+  }
+}
+
+void Document::ReportShadowedHTMLDocumentProperties() {
+  if (!ShouldIncludeInTelemetry()) {
+    return;
+  }
+
+  for (const nsString& property : mShadowedHTMLDocumentProperties) {
+    glean::security::ShadowedHtmlDocumentPropertyAccessExtra extra = {};
+    extra.name = Some(NS_ConvertUTF16toUTF8(property));
+    glean::security::shadowed_html_document_property_access.Record(Some(extra));
   }
 }
 
