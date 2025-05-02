c6e54a9a4acb45fea4b4ddaf58c450778796bcf2
Marco Bonardo <mbonardo@mozilla.com>
2025-05-02 09:54:39 +0000

    Bug 1962061 - Fix sqlite-vec build on Solaris.r=asuth
    Adds patches to sqlite-vec since we need them while waiting for upstream pulls.
    
    Replaces `replace-in-file` vendoring option with a .patch file for now, as
    otherwise it forces CRLF line endings due to bug 1963411.
    Keeping a list of patches is cleaner anyway.
    
    Some files are being replaced due to CRLF -> LF conversion.
    
    Differential Revision: https://phabricator.services.mozilla.com/D247229

--- third_party/sqlite3/ext/sqlite-vec/sqlite-vec.c
+++ third_party/sqlite3/ext/sqlite-vec/sqlite-vec.c
@@ -65,9 +65,11 @@ SQLITE_EXTENSION_INIT3
 #ifndef __EMSCRIPTEN__
 #ifndef __COSMOPOLITAN__
 #ifndef __wasi__
+#ifndef __sun__
 typedef u_int8_t uint8_t;
 typedef u_int16_t uint16_t;
 typedef u_int64_t uint64_t;
+#endif
 #endif
 #endif
 #endif
