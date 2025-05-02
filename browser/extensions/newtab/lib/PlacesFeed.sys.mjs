c30801347552203883b3dc232ee34f43d7833c04
Beth Rennie <beth@brennie.ca>
2025-05-02 14:09:39 +0000

    Bug 1961629 - Use ExperimentFeature.getEnrollmentMetadata() in browser/extensions/newtab (again) r=mconley,home-newtab-reviewers
    Missed a few occurrences.
    
    Differential Revision: https://phabricator.services.mozilla.com/D247474

--- browser/extensions/newtab/lib/PlacesFeed.sys.mjs
+++ browser/extensions/newtab/lib/PlacesFeed.sys.mjs
@@ -298,21 +298,15 @@ export class PlacesFeed {
       lazy.NimbusFeatures.pocketNewtab.getVariable("sendToPocket");
     // An experiment to send the user directly to Pocket's signup page.
     if (sendToPocket && !lazy.pktApi.isUserLoggedIn()) {
-      let pocketNewtabExperiment = lazy.ExperimentAPI.getExperimentMetaData({
-        featureId: "pocketNewtab",
-      });
-      if (!pocketNewtabExperiment) {
-        pocketNewtabExperiment = lazy.ExperimentAPI.getRolloutMetaData({
-          featureId: "pocketNewtab",
-        });
-      }
+      let pocketNewtabExperiment =
+        lazy.NimbusFeatures.pocketNewtab.getEnrollmentMetadata();
       const pocketSiteHost = Services.prefs.getStringPref(
         "extensions.pocket.site"
       ); // getpocket.com
       let utmSource = "firefox_newtab_save_button";
       // We want to know if the user is in a Pocket newtab related experiment.
       let utmCampaign = pocketNewtabExperiment?.slug;
-      let utmContent = pocketNewtabExperiment?.branch?.slug;
+      let utmContent = pocketNewtabExperiment?.branch;
 
       const url = new URL(`https://${pocketSiteHost}/signup`);
       url.searchParams.append("utm_source", utmSource);
