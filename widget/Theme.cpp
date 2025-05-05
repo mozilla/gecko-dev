808fb6f6daaa407813705735f785a5850852dfdd
Emilio Cobos Álvarez <emilio@crisal.io>
2025-05-05 10:16:30 +0000

    Bug 1964022 - Update libadwaita colors. r=stransky
    See comments for sources.
    
    Differential Revision: https://phabricator.services.mozilla.com/D247559

--- widget/Theme.cpp
+++ widget/Theme.cpp
@@ -317,6 +317,7 @@ std::pair<sRGBColor, sRGBColor> Theme::ComputeButtonColors(
     return aColors.SystemNs(StyleSystemColor::Buttonface);
   }();
 
+  // TODO(emilio): This should probably use Buttonborder or something?
   const sRGBColor borderColor =
       ComputeBorderColor(aState, aColors, OutlineCoversBorder::Yes);
   return std::make_pair(sRGBColor::FromABGR(backgroundColor), borderColor);
