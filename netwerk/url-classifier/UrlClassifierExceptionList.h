028d418d528b5f66f798672cfd09b62d43393c1c
Emma Zuehlcke <emz@mozilla.com>
2025-05-06 13:55:37 +0000

    Bug 1960959 - Improve lookup performance of url classifier exception list. r=bvandersloot
    Switching the data structure from an array to nested hash maps.
    
    Differential Revision: https://phabricator.services.mozilla.com/D247625

--- netwerk/url-classifier/UrlClassifierExceptionList.h
+++ netwerk/url-classifier/UrlClassifierExceptionList.h
@@ -7,6 +7,8 @@
 #ifndef mozilla_UrlClassifierExceptionList_h
 #define mozilla_UrlClassifierExceptionList_h
 
+#include "nsHashKeys.h"
+#include "nsTHashMap.h"
 #include "nsIUrlClassifierExceptionList.h"
 #include "nsISupports.h"
 #include "nsTArray.h"
@@ -27,8 +29,35 @@ class UrlClassifierExceptionList final : public nsIUrlClassifierExceptionList {
  private:
   ~UrlClassifierExceptionList() = default;
 
+  // A list of exception entries
+  using ExceptionEntryArray =
+      nsTArray<RefPtr<nsIUrlClassifierExceptionListEntry>>;
+
+  // A map from (schemeless) site to a list of exception entries.
+  using SiteToEntries = nsTHashMap<nsCStringHashKey, ExceptionEntryArray>;
+
+  // Helper method to check if any exception in the array matches the given
+  // load.
+  static bool ExceptionListMatchesLoad(ExceptionEntryArray* aExceptions,
+                                       nsIURI* aURI, nsIURI* aTopLevelURI,
+                                       bool aIsPrivateBrowsing);
+
+  // Helper method to extract the schemeless site from a URL pattern.
+  NS_IMETHODIMP GetSchemelessSiteFromUrlPattern(const nsACString& aUrlPattern,
+                                                nsACString& aSite);
+
+  // The feature this exception list is for, e.g. "tracking-protection".
   nsCString mFeature;
-  nsTArray<RefPtr<nsIUrlClassifierExceptionListEntry>> mEntries;
+
+  // A two stage hash map to store the (top level) site-specific exception
+  // entries.
+  // The outer hash map key is the top level (schemeless) site.
+  // The inner hash map key is the (schemeless) site of the load to be checked.
+  nsTHashMap<nsCStringHashKey, SiteToEntries> mExceptions;
+
+  // A map of exception list entries which apply across all top level sites.
+  // The hash map key is the (schemeless) site of the load to be checked.
+  nsTHashMap<nsCStringHashKey, ExceptionEntryArray> mGlobalExceptions;
 };
 
 }  // namespace mozilla::net
