3707736bbe9d7c4e3552524abe1686e6014930e3
Andrea Marchesini <amarchesini@mozilla.com>
2025-05-07 12:28:10 +0000

    Bug 1964943 - Reduce to 400-days the max-age attribute value, r=valentin,cookie-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D248199

--- netwerk/cookie/CookieParser.cpp
+++ netwerk/cookie/CookieParser.cpp
@@ -699,7 +699,7 @@ bool CookieParser::GetExpiry(CookieStruct& aCookieData,
   // check for max-age attribute first; this overrides expires attribute
   int64_t maxage = 0;
   if (ParseMaxAgeAttribute(aMaxage, &maxage)) {
-    if (maxage == INT64_MIN || maxage == INT64_MAX) {
+    if (maxage == INT64_MIN) {
       aCookieData.expiry() = maxage;
     } else {
       CheckedInt<int64_t> value(aCurrentTime);
