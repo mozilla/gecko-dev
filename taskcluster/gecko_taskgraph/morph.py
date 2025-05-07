d3b8117046530a479d5957d7bbbe8dc6e2c79ec1
Julien Cristau <jcristau@mozilla.com>
2025-05-07 14:38:47 +0000

    Bug 1964918 - don't summarize `gecko.v2.trunk.revision.foo` scopes to `gecko.v2.trunk.*` r=taskgraph-reviewers,bhearsum
    We only have `queue:route:index.{trust_domain}.v2.trunk.revision.*`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D248188

--- taskcluster/gecko_taskgraph/morph.py
+++ taskcluster/gecko_taskgraph/morph.py
@@ -125,6 +125,7 @@ def derive_misc_task(
 # in each Gecko `assume:repo:hg.mozilla.org/...` role.
 SCOPE_SUMMARY_REGEXPS = [
     re.compile(r"(index:insert-task:docker\.images\.v1\.[^.]*\.).*"),
+    re.compile(r"(index:insert-task:gecko\.v2\.trunk\.revision\.).*"),
     re.compile(r"(index:insert-task:gecko\.v2\.[^.]*\.).*"),
     re.compile(r"(index:insert-task:comm\.v2\.[^.]*\.).*"),
 ]
