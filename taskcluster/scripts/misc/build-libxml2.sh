b56c6602eaf6a6b9042469986051ffdf989c8fef
Mike Hommey <mh+mozilla@glandium.org>
2025-05-07 22:38:24 +0000

    Bug 1963424 - Statically link libxml2 to clang on Linux too. r=firefox-build-system-reviewers,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D247549

--- taskcluster/scripts/misc/build-libxml2.sh
+++ taskcluster/scripts/misc/build-libxml2.sh
@@ -13,18 +13,34 @@ TARGET=${1?"First argument must be a valid value for CMAKE_C_COMPILER_TARGET"}
 # actually need it, so just remove its creation.
 sed -i /libxml2\\.rc/d CMakeLists.txt
 
-cmake \
+case "$TARGET" in
+*-pc-windows-msvc)
+  EXTRA_CMAKE_FLAGS="
+    -DCMAKE_C_COMPILER=clang-cl
+    -DCMAKE_LINKER=lld-link
+    -DCMAKE_MT=llvm-mt
+    -DCMAKE_C_FLAGS=\"-fuse-ld=lld -Xclang -ivfsoverlay -Xclang $MOZ_FETCHES_DIR/vs/overlay.yaml -winsysroot $MOZ_FETCHES_DIR/vs\"
+    -DCMAKE_EXE_LINKER_FLAGS=\"-winsysroot:$MOZ_FETCHES_DIR/vs\"
+    -DCMAKE_SYSTEM_NAME=Windows
+    -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
+  "
+  ;;
+*-unknown-linux-gnu)
+  EXTRA_CMAKE_FLAGS="
+    -DCMAKE_C_COMPILER=clang
+    -DCMAKE_LINKER=clang
+    -DCMAKE_C_FLAGS=\"-fuse-ld=lld --sysroot=$MOZ_FETCHES_DIR/sysroot\"
+    -DCMAKE_EXE_LINKER_FLAGS=\"--sysroot=$MOZ_FETCHES_DIR/sysroot\"
+  "
+  ;;
+esac
+
+eval cmake \
   -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=Off \
-  -DCMAKE_C_COMPILER=clang-cl \
   -DCMAKE_C_COMPILER_TARGET=$TARGET \
-  -DCMAKE_LINKER=lld-link \
-  -DCMAKE_MT=llvm-mt \
-  -DCMAKE_C_FLAGS="-fuse-ld=lld -Xclang -ivfsoverlay -Xclang $MOZ_FETCHES_DIR/vs/overlay.yaml -winsysroot $MOZ_FETCHES_DIR/vs" \
-  -DCMAKE_EXE_LINKER_FLAGS="-winsysroot:$MOZ_FETCHES_DIR/vs" \
-  -DCMAKE_SYSTEM_NAME=Windows \
-  -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded \
+  $EXTRA_CMAKE_FLAGS \
   -DLIBXML2_WITH_C14N=OFF \
   -DLIBXML2_WITH_CATALOG=OFF \
   -DLIBXML2_WITH_DEBUG=OFF \
