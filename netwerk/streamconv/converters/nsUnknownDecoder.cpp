ac3c2a2e6fd30e8dd7b949a02c3c9d70b5b34a77
Cristina Horotan <chorotan@mozilla.com>
2025-05-07 03:37:52 +0300

    Revert "Bug 1948543 - Do not use file extension when sniffing mime type r=necko-reviewers,willdurand,kershaw" for causing dt failures at 7f63794d20da687e22e93c93c59446439e136958
    This reverts commit 7f63794d20da687e22e93c93c59446439e136958.

--- netwerk/streamconv/converters/nsUnknownDecoder.cpp
+++ netwerk/streamconv/converters/nsUnknownDecoder.cpp
@@ -24,7 +24,6 @@
 #include "nsQueryObject.h"
 #include "nsComponentManagerUtils.h"
 #include "nsServiceManagerUtils.h"
-#include "mozilla/StaticPrefs_network.h"
 
 #include <algorithm>
 
@@ -481,14 +480,9 @@ void nsUnknownDecoder::DetermineContentType(nsIRequest* aRequest) {
     return;
   }
 
-  nsCOMPtr<nsIURI> uri;
-  NS_GetFinalChannelURI(channel, getter_AddRefs(uri));
-
   // We don't know what this is yet.  Before we just give up, try
   // the URI from the request.
-  if ((StaticPrefs::network_sniff_use_extension() ||
-       (uri && uri->SchemeIs("file"))) &&
-      SniffURI(aRequest)) {
+  if (SniffURI(aRequest)) {
 #ifdef DEBUG
     MutexAutoLock lock(mMutex);
     NS_ASSERTION(!mContentType.IsEmpty(),
@@ -564,7 +558,7 @@ bool nsUnknownDecoder::SniffForHTML(nsIRequest* aRequest) {
 
 bool nsUnknownDecoder::SniffForXML(nsIRequest* aRequest) {
   // First see whether we can glean anything from the uri...
-  if (!StaticPrefs::network_sniff_use_extension() || !SniffURI(aRequest)) {
+  if (!SniffURI(aRequest)) {
     // Oh well; just generic XML will have to do
     MutexAutoLock lock(mMutex);
     mContentType = TEXT_XML;
