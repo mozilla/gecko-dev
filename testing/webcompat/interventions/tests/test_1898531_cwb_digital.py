ff92fcb4b7b90048c8f2beadb405270e83c37b96
Thomas Wisniewski <twisniewski@mozilla.com>
2025-05-07 16:21:23 +0000

    Bug 1898531 - remove UA override for cwb.digital; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D247682

--- testing/webcompat/interventions/tests/test_1898531_cwb_digital.py
+++ testing/webcompat/interventions/tests/test_1898531_cwb_digital.py
@@ -4,14 +4,11 @@ URL = "https://www.cwb.digital/apps/cwbDigital/#_frmLogin"
 LOGIN_CSS = "#okta_login"
 
 
-@pytest.mark.asyncio
-@pytest.mark.with_interventions
-async def test_enabled(client):
-    await client.navigate(URL)
-    assert client.await_css(LOGIN_CSS, is_displayed=True)
+# This is just a regression test.
 
 
 @pytest.mark.asyncio
 @pytest.mark.without_interventions
 async def test_disabled(client):
-    await client.navigate(URL, await_console_message="ReferenceError: $ is not defined")
+    await client.navigate(URL)
+    assert client.await_css(LOGIN_CSS, is_displayed=True)
