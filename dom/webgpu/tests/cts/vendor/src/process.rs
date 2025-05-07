9c4d8963b0f8ef1cb1bbd7839a499b0fcc585a8c
Erich Gubler <erichdongubler@gmail.com>
2025-05-07 18:58:17 +0000

    Bug 1964704 - refactor(webgpu): CTS revendor: remove `lazy_format` usage r=webgpu-reviewers,nical
    Differential Revision: https://phabricator.services.mozilla.com/D248021

--- dom/webgpu/tests/cts/vendor/src/process.rs
+++ dom/webgpu/tests/cts/vendor/src/process.rs
@@ -1,12 +1,11 @@
 use std::ffi::OsString;
 
-use format::lazy_format;
 use miette::{Context, IntoDiagnostic};
 
 pub(crate) fn which(name: &'static str, desc: &str) -> miette::Result<OsString> {
     let found = ::which::which(name)
         .into_diagnostic()
-        .wrap_err(lazy_format!("failed to find `{name}` executable"))?;
+        .wrap_err_with(|| format!("failed to find `{name}` executable"))?;
     log::debug!("using {desc} from {}", found.display());
     Ok(found.file_name().unwrap().to_owned())
 }
