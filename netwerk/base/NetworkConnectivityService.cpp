a09c4390868c63439f5c2cdf32b96cd188c40bb0
Atila Butkovits <abutkovits@mozilla.com>
2025-05-07 20:06:36 +0300

    Revert "Bug 1956081 - Don't perform any connectivity checks before idle-startup-finished r=necko-reviewers,kershaw" for causing failures at test_network_connectivity_service.js.
    This reverts commit 7dc137eccf79594132d129f4f0d0f7b1e0c3ae12.

--- netwerk/base/NetworkConnectivityService.cpp
+++ netwerk/base/NetworkConnectivityService.cpp
@@ -225,11 +225,6 @@ void NetworkConnectivityService::PerformChecks() {
     }
   }
 
-  if (StaticPrefs::network_connectivity_service_wait_for_idle_startup() &&
-      !mIdleStartupDone) {
-    return;
-  }
-
   RecheckDNS();
   RecheckIPConnectivity();
 }
@@ -443,7 +438,6 @@ NetworkConnectivityService::Observe(nsISupports* aSubject, const char* aTopic,
                   .Equals(aData)) {
     PerformChecks();
   } else if (!strcmp(aTopic, "browser-idle-startup-tasks-finished")) {
-    mIdleStartupDone = true;
     PerformChecks();
   }
 
