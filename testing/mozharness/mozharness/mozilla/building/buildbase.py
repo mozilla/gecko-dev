8986a7d78946cf5e7821b6c5499368a8aa7a589b
Julien Cristau <jcristau@mozilla.com>
2025-05-07 12:52:13 +0000

    Bug 1964465 - remove stale arm-partner-sample1 build variant. r=bhearsum,nalexander
    These builds were removed in bug 1307030
    
    Differential Revision: https://phabricator.services.mozilla.com/D247929

--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -284,7 +284,6 @@ class BuildOptionParser:
         "x86_64-debug-isolated-process": path_base
         + "%s_x86_64_debug_isolated_process.py",
         "x86_64-profile-generate": path_base + "%s_x86_64_profile_generate.py",
-        "arm-partner-sample1": path_base + "%s_arm_partner_sample1.py",
         "aarch64": path_base + "%s_aarch64.py",
         "aarch64-lite": path_base + "%s_aarch64_lite.py",
         "aarch64-debug": path_base + "%s_aarch64_debug.py",
