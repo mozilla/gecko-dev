a2755ead0ed631cc70eefc8a980337313c6e54c5
Atila Butkovits <abutkovits@mozilla.com>
2025-05-07 20:11:46 +0300

    Revert "Bug 1964030 - Make sure only one GetPACFromDHCP runnable is active at any time r=necko-reviewers,kershaw" for causing high frequency Gtest failures complaining about TestPACMan.
    This reverts commit adbedad5ba4c33649926f2c5ac2de24490a4d63a.

--- netwerk/base/nsPACMan.cpp
+++ netwerk/base/nsPACMan.cpp
@@ -36,11 +36,6 @@ LazyLogModule gProxyLog("proxy");
 #define MOZ_WPAD_URL "http://wpad/wpad.dat"
 #define MOZ_DHCP_WPAD_OPTION 252
 
-// If a GetOption call is in progress (which may block)
-// this will be set to true so we don't dispatch another task
-// until the pending one is complete.
-static Atomic<bool> sGetOptionInProgress(false);
-
 // These pointers are declared in nsProtocolProxyService.cpp
 extern const char kProxyType_HTTPS[];
 extern const char kProxyType_DIRECT[];
@@ -592,16 +587,11 @@ nsresult nsPACMan::GetPACFromDHCP(nsACString& aSpec) {
          MOZ_DHCP_WPAD_OPTION));
     return NS_ERROR_NOT_IMPLEMENTED;
   }
-  if (sGetOptionInProgress) {
-    LOG(("GetPACFromDHCP task already in progress"));
-    return NS_ERROR_IN_PROGRESS;
-  }
 
   MonitorAutoLock lock(mMonitor);
   mPACStringFromDHCP.Truncate();
 
   RefPtr<nsPACMan> self = this;
-  sGetOptionInProgress = true;
   rv = NS_DispatchBackgroundTask(
       NS_NewRunnableFunction(
           "nsPACMan::GetPACFromDHCP",
@@ -622,13 +612,11 @@ nsresult nsPACMan::GetPACFromDHCP(nsACString& aSpec) {
             }
             MonitorAutoLock lock(self->mMonitor);
             self->mPACStringFromDHCP = spec;
-            sGetOptionInProgress = false;
             self->mMonitor.NotifyAll();
           }),
       NS_DISPATCH_EVENT_MAY_BLOCK);
 
   if (NS_FAILED(rv)) {
-    sGetOptionInProgress = false;
     return rv;
   }
 
