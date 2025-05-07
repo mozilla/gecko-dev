709cdf44d2fbf868400247959fe04675a4ea7c56
Julian Descottes <jdescottes@mozilla.com>
2025-05-07 16:39:29 +0000

    Bug 1964990 - [devtools] Add mandatory global to lazy import in CacheEntry.sys.mjs r=ochameau,devtools-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D248246

--- devtools/shared/platform/CacheEntry.sys.mjs
+++ devtools/shared/platform/CacheEntry.sys.mjs
@@ -4,10 +4,14 @@
 
 const lazy = {};
 
-ChromeUtils.defineESModuleGetters(lazy, {
-  NetworkHelper:
-    "resource://devtools/shared/network-observer/NetworkHelper.sys.mjs",
-});
+ChromeUtils.defineESModuleGetters(
+  lazy,
+  {
+    NetworkHelper:
+      "resource://devtools/shared/network-observer/NetworkHelper.sys.mjs",
+  },
+  { global: "contextual" }
+);
 
 /**
  * Global cache session object.
