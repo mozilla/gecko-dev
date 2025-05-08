a6988114711528866f43543a1abcf1ecfeea0541
AndiAJ <andiaj@users.noreply.github.com>
2025-05-08 09:05:56 +0000

    Bug 1965161 - Temporarily disable network connection interactions in UI test setup r=ohorvath
    Follow-up from [[ https://phabricator.services.mozilla.com/D248245 | D248245 ]]
    
    Differential Revision: https://phabricator.services.mozilla.com/D248381

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/helpers/TestSetup.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/helpers/TestSetup.kt
@@ -62,7 +62,11 @@ open class TestSetup {
             // This will only work in case of a RetryTestRule execution.
             AppAndSystemHelper.clearDownloadsFolder()
             // Make sure the Wifi and Mobile Data connections are on.
-            AppAndSystemHelper.setNetworkEnabled(true)
+
+            // Disabled due to network connections problems encountered recently
+            // See https://bugzilla.mozilla.org/show_bug.cgi?id=1964989 for the disabled UI tests
+            // AppAndSystemHelper.setNetworkEnabled(true)
+
             // Make sure that the data saver system setting is disabled.
             enableDataSaverSystemSetting(enabled = false)
             // Clear bookmarks left after a failed test, before a retry.
