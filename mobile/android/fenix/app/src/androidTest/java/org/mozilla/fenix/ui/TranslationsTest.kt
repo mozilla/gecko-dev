436efa3093ed2e727e01a513b684ce73eee24de4
AndiAJ <andiaj@users.noreply.github.com>
2025-05-08 14:30:22 +0000

    Bug 1965222 - Disable downloadLanguageWhileDataSaverModeIsOnTest UI test r=aaronmt
    In this UI test we disable the wifi network connection only relying on mobile network and afterwards try to download in "data saving" mode a language.
    Since we aren't enabling our network connection in our setup we should disable this UI test for the moment.
    
    Differential Revision: https://phabricator.services.mozilla.com/D248418

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/TranslationsTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/TranslationsTest.kt
@@ -6,6 +6,7 @@ package org.mozilla.fenix.ui
 
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.core.net.toUri
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.customannotations.SkipLeaks
@@ -397,6 +398,7 @@ class TranslationsTest : TestSetup() {
     }
 
     // TestRail link: https://mozilla.testrail.io/index.php?/cases/view/2440963
+    @Ignore("Failing, see https://bugzilla.mozilla.org/show_bug.cgi?id=1965222")
     @Test
     fun downloadLanguageWhileDataSaverModeIsOnTest() {
         val firstTestPage = TestAssetHelper.getFirstForeignWebPageAsset(mockWebServer)
