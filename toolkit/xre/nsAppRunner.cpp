7f0bb132d8d2516537739505747c1ba15c00c9d1
agoloman <agoloman@mozilla.com>
2025-05-09 14:05:26 +0300

    Revert "Bug 1945770 - Make call to host registry portal earlier before LookAndFeel is initialized r=emilio" for causing build bustages @WidgetUtilsGtk.cpp.
    This reverts commit c3f013dc0acc8c132cff09455dd94e21e9a733d5.

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -4507,12 +4507,6 @@ int XREMain::XRE_mainInit(bool* aExitFlag) {
     *aExitFlag = true;
     return 0;
   }
-
-#  ifdef MOZ_WIDGET_GTK
-  if (XRE_IsParentProcess()) {
-    widget::RegisterHostApp();
-  }
-#  endif
 #endif
 
   rv = XRE_InitCommandLine(gArgc, gArgv);
