70d9629dad47aae5610b3fa55b5acfed85dee181
Mark Banner <standard8@mozilla.com>
2025-05-09 10:18:35 +0000

    Bug 1964675 - Add TypeScript configuration for urlbar code. r=urlbar-reviewers,jteow
    Differential Revision: https://phabricator.services.mozilla.com/D247914

---
+++ browser/components/urlbar/types/urlbar.ts
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// These types are commonly passed as parameters with the Urlbar code. We
+// define them here to avoid having to `@import` them into each module.
+// TypeScript will still warn about attempting to call `new UrlbarController()`
+// and similar actions because these are only defined as types and not values.
+type UrlbarController = import("../UrlbarController.sys.mjs").UrlbarController;
+type UrlbarInput = import("../UrlbarInput.sys.mjs").UrlbarInput;
+type UrlbarQueryContext = import("../UrlbarUtils.sys.mjs").UrlbarQueryContext;
+type UrlbarResult = import("../UrlbarResult.sys.mjs").UrlbarResult;
