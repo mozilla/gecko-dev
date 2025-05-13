070f048911b02a3f8b20feccaa89da53c8d8446a
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2025-05-13 12:30:36 +0000

    Bug 1958948 - fix category selection error in settings for CSS injection, r=settings-reviewers,mossop
    Differential Revision: https://phabricator.services.mozilla.com/D249005

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -311,14 +311,18 @@ async function gotoPref(
   }
 
   let item;
+  let unknownCategory = false;
   if (category != "paneSearchResults") {
     // Hide second level headers in normal view
     for (let element of document.querySelectorAll(".search-header")) {
       element.hidden = true;
     }
 
-    item = categories.querySelector(".category[value=" + category + "]");
+    item = categories.querySelector(
+      ".category[value=" + CSS.escape(category) + "]"
+    );
     if (!item || item.hidden) {
+      unknownCategory = true;
       category = kDefaultCategoryInternalName;
       item = categories.querySelector(".category[value=" + category + "]");
     }
@@ -326,6 +330,7 @@ async function gotoPref(
 
   if (
     gLastCategory.category ||
+    unknownCategory ||
     category != kDefaultCategoryInternalName ||
     subcategory
   ) {
