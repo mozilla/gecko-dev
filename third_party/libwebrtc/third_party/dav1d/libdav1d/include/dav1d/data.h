77222b485b07f97efab6c61132b94d2ba1e62db6
Michael Froman <mfroman@mozilla.com>
2025-05-13 21:59:57 -0500

    Bug 1963852 (MOZ) - add 3 headers to libwebrtc's third_party/dav1d trampoline setup

---
+++ third_party/libwebrtc/third_party/dav1d/libdav1d/include/dav1d/data.h
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/*
+ * Copyright 2025 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+// This is a trampoline header meant to redirect from where libwebrtc code
+// expects third_party headers to where those headers exist in the Mozilla
+// Firefox repository.
+#include "dav1d/data.h"
+
