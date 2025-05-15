baaeabfe68814c17d6abb177b4ed92d23497c112
Mike Conley <mconley@mozilla.com>
2025-05-15 16:23:23 +0000

    Bug 1966209 - Remove newtab_visit_id from newtab-content ping. r=nbarrett,home-newtab-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D249134

--- browser/extensions/newtab/lib/TelemetryFeed.sys.mjs
+++ browser/extensions/newtab/lib/TelemetryFeed.sys.mjs
@@ -635,7 +635,6 @@ export class TelemetryFeed {
           });
         } else if (["spoc", "organic"].includes(card_type)) {
           const gleanData = {
-            newtab_visit_id: session.session_id,
             is_sponsored: card_type === "spoc",
             ...(format ? { format } : {}),
             ...(section
@@ -664,7 +663,10 @@ export class TelemetryFeed {
                   recommendation_id,
                 }),
           };
-          Glean.pocket.click.record(gleanData);
+          Glean.pocket.click.record({
+            ...gleanData,
+            newtab_visit_id: session.session_id,
+          });
           if (this.privatePingEnabled) {
             Glean.newtabContent.click.record(gleanData);
           }
@@ -710,7 +712,6 @@ export class TelemetryFeed {
           topic,
         } = action.data.value ?? {};
         const gleanData = {
-          newtab_visit_id: session.session_id,
           tile_id,
           // We conditionally add in a few props.
           ...(corpus_item_id ? { corpus_item_id } : {}),
@@ -735,7 +736,10 @@ export class TelemetryFeed {
               }
             : {}),
         };
-        Glean.pocket.thumbVotingInteraction.record(gleanData);
+        Glean.pocket.thumbVotingInteraction.record({
+          ...gleanData,
+          newtab_visit_id: session.session_id,
+        });
         if (this.privatePingEnabled) {
           Glean.newtabContent.thumbVotingInteraction.record(gleanData);
         }
@@ -1164,24 +1168,33 @@ export class TelemetryFeed {
     if (session) {
       const { section, section_position, event_source, is_section_followed } =
         action.data;
-      const gleanData = {
+      const gleanDataForPrivatePing = {
         newtab_visit_id: session.session_id,
         section,
         section_position,
         event_source,
       };
 
+      const gleanDataForNewtabPing = {
+        ...gleanDataForPrivatePing,
+        newtab_visit_id: session.session_id,
+      };
+
       switch (action.type) {
         case "BLOCK_SECTION":
-          Glean.newtab.sectionsBlockSection.record(gleanData);
+          Glean.newtab.sectionsBlockSection.record(gleanDataForNewtabPing);
           if (this.privatePingEnabled) {
-            Glean.newtabContent.sectionsBlockSection.record(gleanData);
+            Glean.newtabContent.sectionsBlockSection.record(
+              gleanDataForPrivatePing
+            );
           }
           break;
         case "UNBLOCK_SECTION":
-          Glean.newtab.sectionsUnblockSection.record(gleanData);
+          Glean.newtab.sectionsUnblockSection.record(gleanDataForNewtabPing);
           if (this.privatePingEnabled) {
-            Glean.newtabContent.sectionsUnblockSection.record(gleanData);
+            Glean.newtabContent.sectionsUnblockSection.record(
+              gleanDataForPrivatePing
+            );
           }
           break;
         case "CARD_SECTION_IMPRESSION":
@@ -1193,7 +1206,6 @@ export class TelemetryFeed {
           });
           if (this.privatePingEnabled) {
             Glean.newtabContent.sectionsImpression.record({
-              newtab_visit_id: session.session_id,
               section,
               section_position,
               is_section_followed,
@@ -1201,16 +1213,20 @@ export class TelemetryFeed {
           }
           break;
         case "FOLLOW_SECTION": {
-          Glean.newtab.sectionsFollowSection.record(gleanData);
+          Glean.newtab.sectionsFollowSection.record(gleanDataForNewtabPing);
           if (this.privatePingEnabled) {
-            Glean.newtabContent.sectionsFollowSection.record(gleanData);
+            Glean.newtabContent.sectionsFollowSection.record(
+              gleanDataForPrivatePing
+            );
           }
           break;
         }
         case "UNFOLLOW_SECTION":
-          Glean.newtab.sectionsUnfollowSection.record(gleanData);
+          Glean.newtab.sectionsUnfollowSection.record(gleanDataForNewtabPing);
           if (this.privatePingEnabled) {
-            Glean.newtabContent.sectionsUnfollowSection.record(gleanData);
+            Glean.newtabContent.sectionsUnfollowSection.record(
+              gleanDataForPrivatePing
+            );
           }
           break;
         default:
@@ -1383,7 +1399,6 @@ export class TelemetryFeed {
       const { corpus_item_id, scheduled_corpus_item_id } = datum;
       if (datum.is_pocket_card) {
         const gleanData = {
-          newtab_visit_id: session.session_id,
           is_sponsored: datum.card_type === "spoc",
           ...(datum.format ? { format: datum.format } : {}),
           position: datum.pos,
@@ -1408,7 +1423,10 @@ export class TelemetryFeed {
                 recommendation_id: datum.recommendation_id,
               }),
         };
-        Glean.pocket.dismiss.record(gleanData);
+        Glean.pocket.dismiss.record({
+          ...gleanData,
+          newtab_visit_id: session.session_id,
+        });
         if (this.privatePingEnabled) {
           Glean.newtabContent.dismiss.record(gleanData);
         }
@@ -1471,7 +1489,6 @@ export class TelemetryFeed {
       } else {
         const { corpus_item_id, scheduled_corpus_item_id } = tile;
         const gleanData = {
-          newtab_visit_id: session.session_id,
           is_sponsored: tile.type === "spoc",
           ...(tile.format ? { format: tile.format } : {}),
           ...(tile.section
@@ -1498,7 +1515,10 @@ export class TelemetryFeed {
                 recommendation_id: tile.recommendation_id,
               }),
         };
-        Glean.pocket.impression.record(gleanData);
+        Glean.pocket.impression.record({
+          ...gleanData,
+          newtab_visit_id: session.session_id,
+        });
 
         if (this.privatePingEnabled) {
           Glean.newtabContent.impression.record(gleanData);
