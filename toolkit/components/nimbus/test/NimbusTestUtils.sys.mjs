bf90c13bb2525b97ec994d05153d6c51fc414092
Beth Rennie <beth@brennie.ca>
2025-05-15 13:44:44 +0000

    Bug 1959831 - Use NimbusFeatures in ExperimentStore r=nimbus-reviewers,relud
    Differential Revision: https://phabricator.services.mozilla.com/D248167

--- toolkit/components/nimbus/test/NimbusTestUtils.sys.mjs
+++ toolkit/components/nimbus/test/NimbusTestUtils.sys.mjs
@@ -354,7 +354,7 @@ export const NimbusTestUtils = {
 
     return () => {
       for (const { featureId } of features) {
-        delete NimbusFeatures[featureId];
+        NimbusFeatures[featureId];
       }
     };
   },
