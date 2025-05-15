47f754014f9fc74498d3f0143b91de0a80e0e6f0
Jon Coppeard <jcoppeard@mozilla.com>
2025-05-15 12:34:47 +0000

    Bug 1966325 - Add test case r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D249389

---
+++ js/src/jit-test/tests/gc/bug-1966325.js
@@ -0,0 +1,17 @@
+abcd1 = 5;
+function ff() {
+  for (
+    let ii = 10;
+    ii;
+    (() => {
+      ii--;
+      function fff() {
+        fff;
+      }
+      v = { 1() {}, h(xx) {} };
+    })()
+  )
+    for (let i, jjj = 1; jjj--, jjj; ) {}
+  this.oomTest(ff);
+}
+ff();
