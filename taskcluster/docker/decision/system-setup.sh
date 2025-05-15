b4705283903e09bfb8441e7b118938bc72216244
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-15 15:04:03 +0000

    Bug 1840829 - Install orjson in the decision task image. r=taskgraph-reviewers,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D248459

--- taskcluster/docker/decision/system-setup.sh
+++ taskcluster/docker/decision/system-setup.sh
@@ -8,7 +8,14 @@ apt-get update
 apt-get install \
     python \
     sudo \
-    python3-yaml
+    python3-yaml \
+    python3-pip
 
+pip install --disable-pip-version-check --quiet --no-cache-dir orjson==3.10.15
+
+apt-get remove --purge python3-pip
+apt-get autoremove --purge
+apt-get clean
+apt-get autoclean
 rm -rf /var/lib/apt/lists/
 rm "$0"
