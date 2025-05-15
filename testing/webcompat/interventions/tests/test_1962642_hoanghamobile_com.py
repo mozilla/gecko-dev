d5951357801cd60f213a4c4f6d35f988b1d263ce
Thomas Wisniewski <twisniewski@mozilla.com>
2025-05-15 17:03:41 +0000

    Bug 1962642 - add a UA override for hoanghamobile.com on Android to get the mobile site; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D248959

---
+++ testing/webcompat/interventions/tests/test_1962642_hoanghamobile_com.py
@@ -0,0 +1,23 @@
+import pytest
+
+URL = "https://hoanghamobile.com"
+MOBILE_CSS = ".mobile-wrapper"
+DESKTOP_CSS = ".body-wrapper"
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(MOBILE_CSS, is_displayed=True)
+    assert not client.find_css(DESKTOP_CSS, is_displayed=True)
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(DESKTOP_CSS, is_displayed=True)
+    assert not client.find_css(MOBILE_CSS, is_displayed=True)
