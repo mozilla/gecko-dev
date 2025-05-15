c3942dbdf4639b569700839a9d30aae26d8c7050
Gabriel Luong <gabriel.luong@gmail.com>
2025-05-15 04:19:49 +0000

    Bug 1966150 - Part 2: Only enable content recommendations feature if the locale is supported r=android-reviewers,devota
    Differential Revision: https://phabricator.services.mozilla.com/D249141

--- mobile/android/fenix/app/src/test/java/org/mozilla/fenix/home/pocket/ContentRecommendationsFeatureHelperTest.kt
+++ mobile/android/fenix/app/src/test/java/org/mozilla/fenix/home/pocket/ContentRecommendationsFeatureHelperTest.kt
@@ -63,4 +63,124 @@ class ContentRecommendationsFeatureHelperTest {
 
         assertTrue(ContentRecommendationsFeatureHelper.isPocketSponsoredStoriesFeatureEnabled(testContext))
     }
+
+    @Test
+    fun `GIVEN unsupported locale WHEN content recommendations feature enabled check is invoked THEN return false`() {
+        val locale = Locale.Builder().setLanguage("ro").setRegion("RO").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertFalse(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported fr locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("fr").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported fr-FR locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("fr").setRegion("FR").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported es locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("es").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported es-ES locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("es").setRegion("ES").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported it locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("it").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported it-IT locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("it").setRegion("IT").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported en locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("en").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported en-CA locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("en").setRegion("CA").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported en-GB locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("en").setRegion("GB").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported en-US locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("en").setRegion("US").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported de locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("de").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported de-DE locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("de").setRegion("DE").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported de-AT locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("de").setRegion("AT").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
+
+    @Test
+    fun `GIVEN supported de-CH locale WHEN content recommendations feature enabled check is invoked THEN return true`() {
+        val locale = Locale.Builder().setLanguage("de").setRegion("CH").build()
+        LocaleManager.setNewLocale(testContext, localeUseCase = null, locale = locale)
+
+        assertTrue(ContentRecommendationsFeatureHelper.isContentRecommendationsFeatureEnabled(testContext))
+    }
 }
