bbd9dd6b62c45054529ba7068a18c8fb35a085b8
Thomas Wisniewski <twisniewski@mozilla.com>
2025-05-15 17:03:45 +0000

    Bug 1943987 - add an Android-only CSS webcompat intervention for andisearch.com to keep their layout on-screen; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D249456

---
+++ testing/webcompat/interventions/tests/test_1943987_andisearch_com.py
@@ -0,0 +1,30 @@
+import pytest
+
+URL = "https://andisearch.com"
+MSGBAR_CSS = ".rcw-sender"
+
+
+async def is_msgbar_offscreen(client):
+    await client.navigate(URL, wait="none")
+    msgbar = client.await_css(MSGBAR_CSS, is_displayed=True)
+    return client.execute_script(
+        """
+        const msgbar = arguments[0];
+        return msgbar.getBoundingClientRect().bottom > window.innerHeight;
+    """,
+        msgbar,
+    )
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    assert not await is_msgbar_offscreen(client)
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    assert await is_msgbar_offscreen(client)
