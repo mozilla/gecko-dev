f0ef299df582d35f5fb419cf3f26de2c755edd48
Michael Froman <mfroman@mozilla.com>
2025-05-16 13:14:03 -0500

    Bug 1963852 - Vendor libwebrtc from 7cc68fa675
    Upstream commit: https://webrtc.googlesource.com/src/+/7cc68fa675dc8da18afb969c1643951a7a13538d
        Fix variable shadowing in the Audio Processing Module
    
        Bug: webrtc:42223409
        Change-Id: I7ba3678ae4ff30e5637fca8894da2928b7e25f12
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/382741
        Reviewed-by: Per Åhgren <peah@webrtc.org>
        Commit-Queue: Gustaf Ullberg <gustaf@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#44231}

--- third_party/libwebrtc/modules/audio_processing/test/aec_dump_based_simulator.cc
+++ third_party/libwebrtc/modules/audio_processing/test/aec_dump_based_simulator.cc
@@ -19,6 +19,7 @@
 #include <iostream>
 #include <memory>
 #include <optional>
+#include <sstream>  // no-presubmit-check TODO(webrtc:8982)
 #include <string>
 #include <utility>
 
@@ -50,7 +51,7 @@ bool VerifyFixedBitExactness(const webrtc::audioproc::Stream& msg,
     return false;
   } else {
     const int16_t* frame_data = frame.data.data();
-    for (int k = 0; k < frame.num_channels * frame.samples_per_channel; ++k) {
+    for (int k = 0; k < frame.num_channels_ * frame.samples_per_channel_; ++k) {
       if (msg.output_data().data()[k] != frame_data[k]) {
         return false;
       }
