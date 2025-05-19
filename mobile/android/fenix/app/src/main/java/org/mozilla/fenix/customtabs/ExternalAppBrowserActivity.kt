a781148332770b5ed1ea040f70e3f1b4f7688a2a
Mugurell <Mugurell@users.noreply.github.com>
2025-05-19 07:32:49 +0000

    Bug 1966095 - Consider enter animations finished for the custom tabs activity if restarted r=android-reviewers,matt-tighe
    When restarting the current activity to apply system changes (or for other reasons)
    the enter animations set for when first entering this activity are not applied
    anymore so there is no reason to wait for these animations to finish before acting
    upon users touches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D249622

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/customtabs/ExternalAppBrowserActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/customtabs/ExternalAppBrowserActivity.kt
@@ -6,6 +6,7 @@ package org.mozilla.fenix.customtabs
 
 import android.app.assist.AssistContent
 import android.os.Build
+import android.os.Bundle
 import android.view.MotionEvent
 import androidx.annotation.RequiresApi
 import androidx.annotation.VisibleForTesting
@@ -25,7 +26,7 @@ const val EXTRA_IS_SANDBOX_CUSTOM_TAB = "org.mozilla.fenix.customtabs.EXTRA_IS_S
  */
 @Suppress("TooManyFunctions")
 open class ExternalAppBrowserActivity : HomeActivity() {
-    var isFinishedAnimating = false
+    private var isFinishedAnimating = false
 
     override fun onResume() {
         super.onResume()
@@ -76,6 +77,11 @@ open class ExternalAppBrowserActivity : HomeActivity() {
         super.onProvideAssistContent(outContent)
         val currentTabUrl = getExternalTab()?.content?.url
         outContent?.webUri = currentTabUrl?.let { it.toUri() }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        isFinishedAnimating = true
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
