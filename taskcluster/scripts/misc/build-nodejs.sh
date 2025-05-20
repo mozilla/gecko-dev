29478b48a419e213b1bc084eb56372cd19344885
Mike Hommey <mh+mozilla@glandium.org>
2025-05-20 16:23:47 +0000

    Bug 1942260 - Build node 22 for Linux from source. r=firefox-build-system-reviewers,sergesanspaille
    While here, build node 18 on faster workers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D243081

--- taskcluster/scripts/misc/build-nodejs.sh
+++ taskcluster/scripts/misc/build-nodejs.sh
@@ -14,6 +14,9 @@ export LDFLAGS="--sysroot=$MOZ_FETCHES_DIR/sysroot -L$MOZ_FETCHES_DIR/sysroot/li
 export CC=$MOZ_FETCHES_DIR/gcc/bin/gcc
 export CXX=$MOZ_FETCHES_DIR/gcc/bin/g++
 
+# The glibc in our sysroot doesn't have `sys/random.h`/`getrandom`.
+sed -i '/HAVE_SYS_RANDOM_H/d;/HAVE_GETRANDOM/d' deps/cares/config/linux/ares_config.h
+
 # --partly-static allows the resulting binary to run on Ubuntu 18.04 (which has libstdc++ 8)
 ./configure --verbose --prefix=/ --partly-static
 make -j$(nproc) install DESTDIR=$workspace/$project
