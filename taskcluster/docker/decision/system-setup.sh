d2d03a5298277bb717d09b6734c76d30e11110c7
Julien Cristau <jcristau@mozilla.com>
2025-05-20 08:43:27 +0000

    Bug 1967222 - rebase decision docker image on debian 12 r=releng-reviewers,bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D250025

--- taskcluster/docker/decision/system-setup.sh
+++ taskcluster/docker/decision/system-setup.sh
@@ -6,12 +6,12 @@ test "$(whoami)" == 'root'
 
 apt-get update
 apt-get install \
-    python \
+    python-is-python3 \
     sudo \
     python3-yaml \
     python3-pip
 
-pip install --disable-pip-version-check --quiet --no-cache-dir orjson==3.10.15
+pip install --break-system-packages --disable-pip-version-check --quiet --no-cache-dir orjson==3.10.15
 
 apt-get remove --purge python3-pip
 apt-get autoremove --purge
