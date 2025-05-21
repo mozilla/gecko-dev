80fd4f194d3a92f888a53555f9727cc9a91fe997
Greg Stoll <gstoll@mozilla.com>
2025-05-21 16:30:51 +0000

    Bug 1961170 part 5 - make SendDispatchToDropTargetAndResumeEndDragSession() take allowedFiles r=dlp-reviewers,handyman
    Differential Revision: https://phabricator.services.mozilla.com/D248522

--- widget/nsBaseDragService.cpp
+++ widget/nsBaseDragService.cpp
@@ -1302,11 +1302,27 @@ nsBaseDragSession::SendStoreDropTargetAndDelayEndDragSession(
 
 NS_IMETHODIMP
 nsBaseDragSession::SendDispatchToDropTargetAndResumeEndDragSession(
-    bool aShouldDrop) {
+    bool aShouldDrop, const nsTArray<RefPtr<nsIFile>>& aAllowedFiles) {
   MOZ_ASSERT(mDelayedDropBrowserParent);
+  nsTHashSet<nsString> allowedFilePaths;
+  if (aShouldDrop) {
+    for (const auto& allowedFile : aAllowedFiles) {
+      nsString filePath;
+      nsresult rv = allowedFile->GetPath(filePath);
+      if (NS_FAILED(rv)) {
+        // Something has gone wrong, cancel the drop
+        Unused << mDelayedDropBrowserParent
+                      ->SendDispatchToDropTargetAndResumeEndDragSession(
+                          false /* aShouldDrop */, nsTHashSet<nsString>());
+        mDelayedDropBrowserParent = nullptr;
+        return rv;
+      }
+      allowedFilePaths.Insert(filePath);
+    }
+  }
   Unused << mDelayedDropBrowserParent
                 ->SendDispatchToDropTargetAndResumeEndDragSession(
-                    aShouldDrop, allowedFilePaths);
+                    aShouldDrop, std::move(allowedFilePaths));
   mDelayedDropBrowserParent = nullptr;
   return NS_OK;
 }
