7f0ea4845e136a3c74067e86e164dd1b7ad50ada
iulian moraru <imoraru@mozilla.com>
2025-05-22 18:57:28 +0300

    Revert "Bug 1967417 - Remove default parameters from LoadInfo constructor. r=smaug" for causing mass build bustages on EarlyHintPreloader.cpp.
    This reverts commit 60270087850b8c51bb9ad8d75ffbab50f6344683.

--- netwerk/base/LoadInfo.h
+++ netwerk/base/LoadInfo.h
@@ -114,9 +114,11 @@ class LoadInfo final : public nsILoadInfo {
   LoadInfo(nsIPrincipal* aLoadingPrincipal, nsIPrincipal* aTriggeringPrincipal,
            nsINode* aLoadingContext, nsSecurityFlags aSecurityFlags,
            nsContentPolicyType aContentPolicyType,
-           const Maybe<mozilla::dom::ClientInfo>& aLoadingClientInfo,
-           const Maybe<mozilla::dom::ServiceWorkerDescriptor>& aController,
-           uint32_t aSandboxFlags);
+           const Maybe<mozilla::dom::ClientInfo>& aLoadingClientInfo =
+               Maybe<mozilla::dom::ClientInfo>(),
+           const Maybe<mozilla::dom::ServiceWorkerDescriptor>& aController =
+               Maybe<mozilla::dom::ServiceWorkerDescriptor>(),
+           uint32_t aSandboxFlags = 0);
 
   // Use factory function CreateForDocument
   // Used for TYPE_DOCUMENT load.
