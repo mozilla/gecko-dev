7cc1de1f8a194b306a8da660977be2ff59a7adc6
Beth Rennie <beth@brennie.ca>
2025-05-22 21:43:42 +0000

    Bug 1956080 - Use async experiment cleanup in browser_address_autofill_nimbus r=credential-management-reviewers,dimi
    ExperimentManager.unenroll() is now effectively async when running in a
    browser test (because we are now executing SQL queries during
    unenrollment) so all cleanup functions that trigger unenrollment are
    async as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D250556

--- browser/extensions/formautofill/test/browser/address/browser_address_autofill_nimbus.js
+++ browser/extensions/formautofill/test/browser/address/browser_address_autofill_nimbus.js
@@ -41,7 +41,7 @@ add_task(async function test_address_autofill_feature_enabled() {
     "Address autofill should be available when feature is enabled in nimbus."
   );
 
-  cleanupExperiment();
+  await cleanupExperiment();
 });
 
 add_task(async function test_address_autofill_feature_disabled() {
@@ -66,5 +66,5 @@ add_task(async function test_address_autofill_feature_disabled() {
     "Address autofill shouldn't be available when feature is off in nimbus."
   );
 
-  cleanupExperiment();
+  await cleanupExperiment();
 });
