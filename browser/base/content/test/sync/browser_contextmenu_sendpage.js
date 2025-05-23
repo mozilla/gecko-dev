64d8d51b4c5f3a72afe7b6dd7ab6050ae4583a89
Maxx Crawford <maxx.crawford@gmail.com>
2025-05-23 22:45:57 +0000

    Bug 1968258 - Enable extensions.pocket.enabled pref for browser/base/content/test/sync/browser_contextmenu_sendpage.js test r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D251055

--- browser/base/content/test/sync/browser_contextmenu_sendpage.js
+++ browser/base/content/test/sync/browser_contextmenu_sendpage.js
@@ -38,6 +38,10 @@ add_setup(async function () {
     });
   sinon.stub(Weave.Service.clientsEngine, "getClientType").returns("desktop");
   await BrowserTestUtils.openNewForegroundTab(gBrowser, "about:mozilla");
+  // Bug 1968055 - Temporarily enabled pocket pref while we remove the pref entirely
+  await SpecialPowers.pushPrefEnv({
+    set: [["extensions.pocket.enabled", true]],
+  });
 });
 
 add_task(async function test_page_contextmenu() {
