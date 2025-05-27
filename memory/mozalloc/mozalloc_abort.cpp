0a4984e68053e25cd634b7ca4b6c723d27da5317
Jesse Schwartzentruber <truber@mozilla.com>
2025-05-27 05:06:14 +0000

    Bug 1952291 - Call native abort for all libFuzzer, AFL, and Fuzzilli builds. r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D240645

--- memory/mozalloc/mozalloc_abort.cpp
+++ memory/mozalloc/mozalloc_abort.cpp
@@ -57,8 +57,9 @@ void fillAbortMessage(char (&msg)[N], uintptr_t retAddress) {
 }
 #endif
 
-#if defined(XP_UNIX) && !defined(MOZ_ASAN) && !defined(MOZ_TSAN) && \
-    !defined(MOZ_UBSAN)
+#if defined(XP_UNIX) && !defined(MOZ_ASAN) && !defined(MOZ_TSAN) &&    \
+    !defined(MOZ_UBSAN) && !defined(LIBFUZZER) && !defined(AFLFUZZ) && \
+    !defined(FUZZING_JS_FUZZILLI)
 // Define abort() here, so that it is used instead of the system abort(). This
 // lets us control the behavior when aborting, in order to get better results
 // on *NIX platforms. See mozalloc_abort for details.
