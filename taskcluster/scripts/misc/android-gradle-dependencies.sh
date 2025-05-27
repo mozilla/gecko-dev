4e948cdc16493459b379bb4f464e9ee09aae0c57
Ryan VanderMeulen <ryanvm@gmail.com>
2025-05-27 01:53:41 +0000

    Bug 1963103 - Update Gradle to version 8.14.1. r=android-reviewers,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D251107

--- taskcluster/scripts/misc/android-gradle-dependencies.sh
+++ taskcluster/scripts/misc/android-gradle-dependencies.sh
@@ -14,8 +14,8 @@ export MOZCONFIG=mobile/android/config/mozconfigs/android-arm-gradle-dependencie
 ./mach build
 ./mach gradle downloadDependencies --no-configuration-cache
 ./mach android gradle-dependencies --no-configuration-cache
-./mach gradle -p mobile/android/fenix detekt lint assembleDebug :benchmark:assembleBenchmark
+./mach gradle -p mobile/android/fenix lint :benchmark:assembleBenchmark
 ./mach gradle -p mobile/android/focus-android lint
-./mach gradle -p mobile/android/android-components -Pcoverage detekt lint service-nimbus:testReleaseUnitTest samples-browser:testGeckoDebugUnitTest tooling-lint:test
+./mach gradle -p mobile/android/android-components -Pcoverage detekt lint tooling-lint:test lib-auth:assemble lib-auth:assembleAndroidTest lib-auth:testRelease lib-auth:lintRelease lib-auth:publish
 
 . taskcluster/scripts/misc/android-gradle-dependencies/after.sh
