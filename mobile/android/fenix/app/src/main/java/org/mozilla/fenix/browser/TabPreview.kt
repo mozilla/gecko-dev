424834ed4663fa0b27ed829d12bb5d96795ebc27
Mugurell <Mugurell@users.noreply.github.com>
2025-05-27 15:48:02 +0000

    Bug 1963428 - part 1 - Conditionally show the composable toolbar in the tab preview r=android-reviewers,skhan
    Differential Revision: https://phabricator.services.mozilla.com/D249565

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/TabPreview.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/TabPreview.kt
@@ -10,13 +10,27 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.doOnNextLayout
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
+import mozilla.components.browser.state.helper.Target
 import mozilla.components.browser.state.selector.getNormalOrPrivateTabs
 import mozilla.components.browser.state.selector.selectedTab
 import mozilla.components.browser.thumbnails.loader.ThumbnailLoader
+import mozilla.components.compose.base.Divider
+import mozilla.components.compose.base.theme.AcornTheme
+import mozilla.components.compose.browser.toolbar.BrowserToolbar
+import mozilla.components.compose.browser.toolbar.concept.Action.ActionButton
+import mozilla.components.compose.browser.toolbar.concept.Action.TabCounterAction
+import mozilla.components.compose.browser.toolbar.store.BrowserToolbarInteraction.BrowserToolbarEvent
+import mozilla.components.compose.browser.toolbar.store.BrowserToolbarState
+import mozilla.components.compose.browser.toolbar.store.BrowserToolbarStore
+import mozilla.components.compose.browser.toolbar.store.DisplayState
 import mozilla.components.concept.base.images.ImageLoadRequest
 import org.mozilla.fenix.R
 import org.mozilla.fenix.components.toolbar.ToolbarPosition
@@ -40,7 +54,7 @@ class TabPreview @JvmOverloads constructor(
     private val binding = TabPreviewBinding.inflate(LayoutInflater.from(context), this)
     private val thumbnailLoader = ThumbnailLoader(context.components.core.thumbnailStorage)
 
-    private var mockToolbarView: View = binding.fakeToolbar
+    private lateinit var mockToolbarView: View
 
     init {
         initializeView()
@@ -48,32 +62,23 @@ class TabPreview @JvmOverloads constructor(
 
     @Suppress("LongMethod")
     private fun initializeView() {
+        bindToolbar()
+
         val isToolbarAtTop = context.settings().toolbarPosition == ToolbarPosition.TOP
-
-        binding.fakeToolbar.isVisible = true
-        mockToolbarView = binding.fakeToolbar
-
         if (isToolbarAtTop) {
             mockToolbarView.updateLayoutParams<LayoutParams> {
                 gravity = Gravity.TOP
             }
-            mockToolbarView.background = AppCompatResources.getDrawable(
-                context,
-                ThemeManager.resolveAttribute(R.attr.bottomBarBackgroundTop, context),
-            )
         }
-
-        // Change view properties to avoid confusing the UI tests
-        binding.tabButton.findViewById<View>(R.id.counter_box)?.id = View.NO_ID
-        binding.tabButton.findViewById<View>(R.id.counter_text)?.id = View.NO_ID
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
 
-        currentOpenedTabsCount?.let {
-            binding.tabButton.setCount(it)
-        }
+        updateToolbar(
+            new = {},
+            old = { binding.tabButton.setCount(currentOpenedTabsCount) },
+        )
 
         binding.previewThumbnail.translationY = if (context.settings().toolbarPosition == ToolbarPosition.TOP) {
             mockToolbarView.height.toFloat()
@@ -96,11 +101,107 @@ class TabPreview @JvmOverloads constructor(
         }
     }
 
-    private val currentOpenedTabsCount: Int?
+    private val currentOpenedTabsCount: Int
         get() {
             val store = context.components.core.store
             return store.state.selectedTab?.let {
                 store.state.getNormalOrPrivateTabs(it.content.private).size
+            } ?: store.state.tabs.size
+        }
+
+    private fun bindToolbar() {
+        mockToolbarView = updateToolbar(
+            new = { buildComposableToolbar() },
+            old = { buildToolbarView() },
+        )
+    }
+
+    private fun buildToolbarView(): View {
+        // Change view properties to avoid confusing the UI tests
+        binding.tabButton.findViewById<View>(R.id.counter_box)?.id = NO_ID
+        binding.tabButton.findViewById<View>(R.id.counter_text)?.id = NO_ID
+
+        binding.fakeToolbar.isVisible = true
+        binding.fakeToolbar.background = AppCompatResources.getDrawable(
+            context,
+            ThemeManager.resolveAttribute(R.attr.bottomBarBackgroundTop, context),
+        )
+
+        return binding.fakeToolbar
+    }
+
+    private fun buildComposableToolbar(): ComposeView {
+        return binding.composableToolbar.apply {
+            setContent {
+                AcornTheme {
+                    // Ensure the divider is shown together with the toolbar
+                    Box {
+                        BrowserToolbar(
+                            store = buildComposableToolbarStore(),
+                            browserStore = context.components.core.store,
+                            onTextEdit = {},
+                            onTextCommit = {},
+                            target = Target.SelectedTab,
+                        )
+
+                        Divider(
+                            modifier = Modifier.align(
+                                when (context.settings().shouldUseBottomToolbar) {
+                                    true -> Alignment.TopCenter
+                                    false -> Alignment.BottomCenter
+                                },
+                            ),
+                        )
+                    }
+                }
+            }.apply {
+                isVisible = true
             }
         }
+    }
+
+    private fun buildComposableToolbarStore(): BrowserToolbarStore {
+        val tabsCount = currentOpenedTabsCount
+
+        return BrowserToolbarStore(
+            BrowserToolbarState(
+                displayState = DisplayState(
+                    browserActionsStart = listOf(
+                        ActionButton(
+                            icon = R.drawable.mozac_ic_home_24,
+                            contentDescription = R.string.browser_toolbar_home,
+                            onClick = object : BrowserToolbarEvent {},
+                        ),
+                    ),
+                    browserActionsEnd = listOf(
+                        TabCounterAction(
+                            count = tabsCount,
+                            contentDescription = context.getString(
+                                R.string.mozac_tab_counter_open_tab_tray, tabsCount.toString(),
+                            ),
+                            showPrivacyMask = context.components.core.store.state.selectedTab?.content?.private == true,
+                            onClick = object : BrowserToolbarEvent {},
+                        ),
+                        ActionButton(
+                            icon = R.drawable.mozac_ic_ellipsis_vertical_24,
+                            contentDescription = R.string.content_description_menu,
+                            onClick = object : BrowserToolbarEvent {},
+                        ),
+                    ),
+                ),
+            ),
+        )
+    }
+
+    /**
+     * Pass in the desired configuration for both the `new` composable toolbar and the `old` toolbar View
+     * with this method then deciding what to use depending on the actual toolbar currently is use.
+     */
+    private inline fun <T> updateToolbar(
+        new: () -> T,
+        old: () -> T,
+    ): T = when (context.settings().shouldUseComposableToolbar) {
+        true -> new()
+        false -> old()
+    }
 }
