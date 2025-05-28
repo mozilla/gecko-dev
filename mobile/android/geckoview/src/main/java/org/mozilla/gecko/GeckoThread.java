c42c5d1f5cfa649d87d50b607223a5db48218569
DonalMe <76789979+DonalMe@users.noreply.github.com>
2025-05-28 19:20:14 -0400

    Revert "Bug 1960752 - Preallocate WebExtension process and GPU process on the Java side. r=geckoview-reviewers,nalexander" to address the performance regression in bug 1966109
    This reverts commit 105b346c709b2dd80cbe1379a99178b07859c140.

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoThread.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoThread.java
@@ -493,17 +493,8 @@ public class GeckoThread extends Thread {
     initGeckoEnvironment();
 
     if ((mInitInfo.flags & FLAG_PRELOAD_CHILD) != 0) {
-      // Preload any child processes we need as soon as we want to display any web page.
-      GeckoProcessManager.getInstance()
-          .preload(
-              // The GPU process is the child process we need first.
-              GeckoProcessType.GPU,
-              // We also usually need two content processes: One for the tab contents,
-              // and one for the WebExtension process. If our embedder doesn't use web
-              // extensions, the second content process can still be useful once a
-              // second GeckoSession is started.
-              GeckoProcessType.CONTENT,
-              GeckoProcessType.CONTENT);
+      // Preload the content ("tab") child process.
+      GeckoProcessManager.getInstance().preload(GeckoProcessType.CONTENT);
     }
 
     if ((mInitInfo.flags & FLAG_DEBUGGING) != 0) {
