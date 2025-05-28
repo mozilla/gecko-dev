9cf5078b5570558774aad437ad8a7299cbcdd5f8
AndiAJ <andiaj@users.noreply.github.com>
2025-05-28 14:40:52 +0000

    Bug 1968912 - Fix verifyMainMenuItemsTest efficiency UI test r=jajohnson,aaronmt,ohorvath
    The UI test started to fail after [[ https://phabricator.services.mozilla.com/D249266 | D249266 ]] landed.
    
    Because the "New private tab" button is not displayed anymore in the composable main menu I've changed it with the bookmarks button.
    
    All UI tests successfully passed on the experimental task. ✅
    
    I've also update the description of the button as it was incorrect.
    
    Differential Revision: https://phabricator.services.mozilla.com/D251499

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/efficiency/selectors/MainMenuComposeSelectors.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/efficiency/selectors/MainMenuComposeSelectors.kt
@@ -10,11 +10,20 @@ object MainMenuComposeSelectors {
     val NEW_PRIVATE_TAB_BUTTON = Selector(
         strategy = SelectorStrategy.COMPOSE_BY_TEXT,
         value = getStringResource(R.string.browser_menu_new_private_tab),
-        description = "Find in page close button",
+        description = "Main menu New private tab button",
+        // Removed in https://bugzilla.mozilla.org/show_bug.cgi?id=1966222 as part of the menu redesign effort
+        groups = listOf("removedIn=141"),
+    )
+
+    val BOOKMARKS_BUTTON = Selector(
+        strategy = SelectorStrategy.COMPOSE_BY_CONTENT_DESCRIPTION,
+        value = "Bookmarks",
+        description = "Main menu Bookmarks button",
         groups = listOf("requiredForPage"),
     )
 
     val all = listOf(
         NEW_PRIVATE_TAB_BUTTON,
+        BOOKMARKS_BUTTON,
     )
 }
