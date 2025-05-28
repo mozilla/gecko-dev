264aa4ce8e045aee0a0be1234aebc821aa0ea091
Andreas Farre <farre@mozilla.com>
2025-05-28 16:14:03 +0000

    Bug 1968194 - Part 1: Make sure to fire navigate event when History.go is called. r=smaug,jjaschke
    A smallish re-factor aligning us more with spec allows us to use our new
    reload path, which also aligns us more with spec. Also fixes issues
    around event cancellability.
    
    Differential Revision: https://phabricator.services.mozilla.com/D250902

--- docshell/shistory/ChildSHistory.cpp
+++ docshell/shistory/ChildSHistory.cpp
@@ -182,18 +182,11 @@ void ChildSHistory::Go(int32_t aOffset, bool aRequireUserInteraction,
 }
 
 void ChildSHistory::AsyncGo(int32_t aOffset, bool aRequireUserInteraction,
-                            bool aUserActivation, CallerType aCallerType,
-                            ErrorResult& aRv) {
+                            bool aUserActivation) {
   CheckedInt<int32_t> index = Index();
   MOZ_LOG(gSHLog, LogLevel::Debug,
           ("ChildSHistory::AsyncGo(%d), current index = %d", aOffset,
            index.value()));
-  nsresult rv = mBrowsingContext->CheckNavigationRateLimit(aCallerType);
-  if (NS_FAILED(rv)) {
-    MOZ_LOG(gSHLog, LogLevel::Debug, ("Rejected"));
-    aRv.Throw(rv);
-    return;
-  }
 
   RefPtr<PendingAsyncHistoryNavigation> asyncNav =
       new PendingAsyncHistoryNavigation(this, aOffset, aRequireUserInteraction,
