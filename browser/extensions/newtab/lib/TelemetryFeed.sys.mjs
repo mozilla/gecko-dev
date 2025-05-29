aafa662349fdf9760e4e8d607014da512befd49e
Nathan Barrett <nbarrett@mozilla.com>
2025-05-29 19:27:05 +0000

    Bug 1962043 - Fix followed_sections metric r=home-newtab-reviewers,amy
    Differential Revision: https://phabricator.services.mozilla.com/D247186

--- browser/extensions/newtab/lib/TelemetryFeed.sys.mjs
+++ browser/extensions/newtab/lib/TelemetryFeed.sys.mjs
@@ -63,7 +63,6 @@ const BLANK_HOMEPAGE_URL = "chrome://browser/content/blanktab.html";
 const PREF_PRIVATE_PING_ENABLED = "telemetry.privatePing.enabled";
 const PREF_REDACT_NEWTAB_PING_NEABLED =
   "telemetry.privatePing.redactNewtabPing.enabled";
-const PREF_FOLLOWED_SECTIONS = "discoverystream.sections.following";
 const PREF_PRIVATE_PING_INFERRED_ENABLED =
   "telemetry.privatePing.inferredInterests.enabled";
 const PREF_NEWTAB_PING_ENABLED = "browser.newtabpage.ping.enabled";
@@ -247,20 +246,26 @@ export class TelemetryFeed {
   }
 
   /**
-   * Retrieves most recently followed sections, ordered alphabetically. (maximum 2 sections)
+   * Retrieves most recently followed sections (maximum 2 sections)
    * @returns {String[]} comma separated string of section UUID's
    */
   getFollowedSections() {
-    const followedString = this._prefs.get(PREF_FOLLOWED_SECTIONS);
+    const sections =
+      this.store?.getState()?.DiscoveryStream.sectionPersonalization;
+    if (sections) {
+      // filter to only include followedTopics
+      const followed = Object.entries(sections).filter(
+        ([, info]) => info.isFollowed
+      );
+      // sort from most recently followed to oldest. If followedAt is falsey, treat it as the oldest
+      followed.sort((a, b) => {
+        const aDate = a[1].followedAt ? new Date(a[1].followedAt) : 0;
+        const bDate = b[1].followedAt ? new Date(b[1].followedAt) : 0;
+        return bDate - aDate;
+      });
 
-    if (followedString?.length) {
-      const items = followedString
-        .split(",")
-        .map(item => item.trim())
-        .filter(_item => _item);
-      return items.slice(-2).sort();
+      return followed.slice(0, 2).map(([sectionId]) => sectionId);
     }
-
     return [];
   }
 
