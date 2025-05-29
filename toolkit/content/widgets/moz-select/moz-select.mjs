438d119704864c76dcbb5b700278f6cfe80bdb53
Serban Stanca <sstanca@mozilla.com>
2025-05-29 06:42:52 +0300

    Revert "Bug 1941426 - Add icon support for moz-select options r=reusable-components-reviewers,tgiles" for causing  mochitests failures in browser_parsable_css.js.
    This reverts commit ade98c9f6fe111337583bc33a0fcdd7d745fde5b.

--- toolkit/content/widgets/moz-select/moz-select.mjs
+++ toolkit/content/widgets/moz-select/moz-select.mjs
@@ -2,13 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {
-  createRef,
-  html,
-  ref,
-  classMap,
-  ifDefined,
-} from "../vendor/lit.all.mjs";
+import { createRef, html, ref } from "../vendor/lit.all.mjs";
 import { MozBaseInputElement, MozLitElement } from "../lit-utils.mjs";
 
 /**
@@ -44,7 +38,7 @@ export default class MozSelect extends MozBaseInputElement {
   firstUpdated(changedProperties) {
     super.firstUpdated(changedProperties);
     this.optionsMutationObserver.observe(this, {
-      attributeFilter: ["label", "value", "iconsrc"],
+      attributeFilter: ["label", "value"],
       childList: true,
       subtree: true,
     });
@@ -58,14 +52,6 @@ export default class MozSelect extends MozBaseInputElement {
     }
   }
 
-  get _selectedOptionIconSrc() {
-    if (!this.inputEl) {
-      return "";
-    }
-    const selectedOption = this.options[this.inputEl.selectedIndex];
-    return selectedOption?.iconSrc ?? "";
-  }
-
   /**
    * Internal - populates the select element with options from the light DOM slot.
    */
@@ -76,12 +62,7 @@ export default class MozSelect extends MozBaseInputElement {
       if (node.localName === "moz-option") {
         const optionValue = node.getAttribute("value");
         const optionLabel = node.getAttribute("label");
-        const optionIconSrc = node.getAttribute("iconsrc");
-        this.options.push({
-          value: optionValue,
-          label: optionLabel,
-          iconSrc: optionIconSrc,
-        });
+        this.options.push({ value: optionValue, label: optionLabel });
       }
     }
   }
@@ -106,26 +87,9 @@ export default class MozSelect extends MozBaseInputElement {
     />`;
   }
 
-  selectedOptionIconTemplate() {
-    if (this._selectedOptionIconSrc) {
-      return html`<img
-        src=${this._selectedOptionIconSrc}
-        role="presentation"
-        class="select-option-icon"
-      />`;
-    }
-    return null;
-  }
-
   inputTemplate() {
-    const classes = classMap({
-      "select-wrapper": true,
-      "with-icon": !!this._selectedOptionIconSrc,
-    });
-
     return html`
-      <div class=${ifDefined(classes)}>
-        ${this.selectedOptionIconTemplate()}
+      <div class="select-wrapper">
         <select
           id="input"
           name=${this.name}
@@ -139,18 +103,13 @@ export default class MozSelect extends MozBaseInputElement {
             option => html`
               <option
                 value=${option.value}
-                ?selected=${option.value == this.value}
+                ?selected=${option.value === this.value}
               >
                 ${option.label}
               </option>
             `
           )}
         </select>
-        <img
-          src="chrome://global/skin/icons/arrow-down.svg"
-          role="presentation"
-          class="select-chevron-icon"
-        />
       </div>
       <slot
         @slotchange=${this.populateOptions}
@@ -168,22 +127,19 @@ customElements.define("moz-select", MozSelect);
  * @tagname moz-option
  * @property {string} value - The value of the option
  * @property {string} label - The label of the option
- * @property {string} iconSrc - The path to the icon of the the option
  */
 export class MozOption extends MozLitElement {
   static properties = {
     // Reflect the attribute so that moz-select can detect changes with a MutationObserver
     value: { type: String, reflect: true },
     // Reflect the attribute so that moz-select can detect changes with a MutationObserver
-    label: { type: String, reflect: true, fluent: true },
-    iconSrc: { type: String, reflect: true },
+    label: { type: String, reflect: true },
   };
 
   constructor() {
     super();
     this.value = "";
     this.label = "";
-    this.iconSrc = "";
   }
 
   render() {
--- toolkit/content/widgets/moz-select/moz-select.stories.mjs
+++ toolkit/content/widgets/moz-select/moz-select.stories.mjs
@@ -25,68 +25,20 @@ moz-select-label =
 moz-select-description =
   .label = Select an option
   .description = This is a description for the select dropdown
-moz-option-1 =
-    .label = Option 1
-moz-option-2 =
-    .label = Option 2
-moz-option-3 =
-    .label = Option 3
-moz-option-a =
-    .label = Option A
-moz-option-b =
-    .label = Option B
-moz-option-c =
-    .label = Option C
-moz-option-d =
-    .label = Option D
     `,
   },
 };
 
 const DEFAULT_OPTIONS = [
-  { value: "1", l10nId: "moz-option-1" },
-  { value: "2", l10nId: "moz-option-2" },
-  { value: "3", l10nId: "moz-option-3" },
+  { value: "1", label: "Option 1" },
+  { value: "2", label: "Option 2" },
+  { value: "3", label: "Option 3" },
 ];
 const OTHER_OPTIONS = [
-  { value: "A", l10nId: "moz-option-a" },
-  { value: "B", l10nId: "moz-option-b" },
-  { value: "C", l10nId: "moz-option-c" },
-  { value: "D", l10nId: "moz-option-d" },
-];
-const WITH_ICON_DEFAULT_OPTIONS = [
-  {
-    value: "1",
-    l10nId: "moz-option-1",
-    iconSrc: "chrome://global/skin/icons/settings.svg",
-  },
-  {
-    value: "2",
-    l10nId: "moz-option-2",
-    iconSrc: "chrome://global/skin/icons/info.svg",
-  },
-  {
-    value: "3",
-    l10nId: "moz-option-3",
-    iconSrc: "chrome://global/skin/icons/warning.svg",
-  },
-];
-const WITH_ICON_OTHER_OPTIONS = [
-  {
-    value: "A",
-    l10nId: "moz-option-a",
-    iconSrc: "chrome://global/skin/icons/heart.svg",
-  },
-  {
-    value: "B",
-    l10nId: "moz-option-b",
-    iconSrc: "chrome://global/skin/icons/edit.svg",
-  },
-  {
-    value: "C",
-    l10nId: "moz-option-c",
-    iconSrc: "chrome://global/skin/icons/delete.svg",
-  },
+  { value: "A", label: "Option A" },
+  { value: "B", label: "Option B" },
+  { value: "C", label: "Option C" },
+  { value: "D", label: "Option D" },
 ];
 
 const Template = ({
@@ -121,11 +73,7 @@ const Template = ({
         : ""}
       ${options.map(
         opt =>
-          html`<moz-option
-            value=${opt.value}
-            data-l10n-id=${opt.l10nId}
-            iconsrc=${opt.iconSrc}
-          ></moz-option>`
+          html`<moz-option value=${opt.value} label=${opt.label}></moz-option>`
       )}
     </moz-select>
   </div>
@@ -163,19 +111,6 @@ WithSlottedDescription.args = {
   ...Default.args,
   description: "This is a custom slotted description.",
   hasSlottedDescription: true,
-};
-
-export const WithSelectedOptionIcon = args => {
-  const options = args.useOtherOptions
-    ? WITH_ICON_OTHER_OPTIONS
-    : WITH_ICON_DEFAULT_OPTIONS;
-
-  return Template({ ...args, options });
-};
-
-WithSelectedOptionIcon.args = {
-  ...Default.args,
-  useOtherOptions: false,
 };
 
 export const Disabled = Template.bind({});
