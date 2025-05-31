bda11e2bb18bab5ba22abf08d0c3470c078b59c0
mike a <mavduevskiy@mozilla.com>
2025-05-31 18:46:17 +0000

    Bug 1967448 - Enable shared pref listener for PBM feature every time the app becomes active  a=RyanVM
    I could only reproduce this bug on a pixel device with a non-debug build. Given those conditions are met, the OS becames very aggressive at garbage collecting, and clears registered shared pref the moment the app hits onPause. With the clean intall it would be when the OS requests a notification permossions.
    
        It is somewhat documented in the registerOnSharedPreferenceChangeListener doc by Google, but to make GC to keep the object alive keeping a hard link to it within the file or even components wasn't enough. Having it linked on the app level did do the trick though, but that would be too much.
    
        The feature listenes to the shared preferences to know when they were changes by the user, so it looks safe to enabled/disable those listeners when the app gets active and stops being active.
    
    Original Revision: https://phabricator.services.mozilla.com/D251089
    
    Differential Revision: https://phabricator.services.mozilla.com/D251412

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/lifecycle/PrivateBrowsingLockFeature.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/lifecycle/PrivateBrowsingLockFeature.kt
@@ -41,6 +41,19 @@ interface PrivateBrowsingLockStorage {
      * @param listener A lambda that receives the new boolean value when it changes.
      */
     fun addFeatureStateListener(listener: (Boolean) -> Unit)
+
+    /**
+     * Starts observing shared preferences for changes in the feature flag.
+     *
+     * NB: some devices may garbage collect preference listeners very aggressively,
+     * so this method should be invoked each time the feature becomes active.
+     */
+    fun startObservingSharedPrefs()
+
+    /**
+     * Starts observing shared preferences.
+     */
+    fun stopObservingSharedPrefs()
 }
 
 /**
@@ -60,15 +73,19 @@ class DefaultPrivateBrowsingLockStorage(
         }
     }
 
-    init {
-        preferences.registerOnSharedPreferenceChangeListener(onFeatureStateChanged)
-    }
-
     override val isFeatureEnabled: Boolean
         get() = preferences.getBoolean(privateBrowsingLockPrefKey, false)
 
     override fun addFeatureStateListener(listener: (Boolean) -> Unit) {
         this.listener = listener
+    }
+
+    override fun startObservingSharedPrefs() {
+        preferences.registerOnSharedPreferenceChangeListener(onFeatureStateChanged)
+    }
+
+    override fun stopObservingSharedPrefs() {
+        preferences.unregisterOnSharedPreferenceChangeListener(onFeatureStateChanged)
     }
 }
 
@@ -182,6 +199,16 @@ class PrivateBrowsingLockFeature(
         if (owner is Activity && !owner.isChangingConfigurations) {
             maybeLockPrivateModeOnStop()
         }
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        super.onResume(owner)
+        storage.startObservingSharedPrefs()
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        super.onPause(owner)
+        storage.stopObservingSharedPrefs()
     }
 
     private fun maybeLockPrivateModeOnStop() {
