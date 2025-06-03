fe3b889eb0a2e8996738ecc05ae99ecd070ee4c9
mike a <mavduevskiy@mozilla.com>
2025-06-03 23:16:29 +0000

    Bug 1967448 - Lock PBM during onPause instead of onStop  a=RyanVM
    It looks like there might be a substansial delay between onStop and onPause calls, during testing on my pixel 7 it would reach 1 second, which might allow the user to reopen the app from the launcher even before the app hits onStop. The patch aims to lock private tabs as soon as the app hits onPause. It looks like a safe approach as Fragment recieves onStop even just right after the app (we are lookng at less than 0.005 delay, depending on the number of fragments alive), so the feature will be locked effectively as soon as the fragment becomes inactive.
    
    Original Revision: https://phabricator.services.mozilla.com/D252204
    
    Differential Revision: https://phabricator.services.mozilla.com/D252384

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/lifecycle/PrivateBrowsingLockFeature.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/lifecycle/PrivateBrowsingLockFeature.kt
@@ -177,14 +177,14 @@ class PrivateBrowsingLockFeature(
         }
     }
 
-    override fun onStop(owner: LifecycleOwner) {
-        super.onStop(owner)
+    override fun onPause(owner: LifecycleOwner) {
+        super.onPause(owner)
 
         if (!isFeatureEnabled) return
 
         // lock when activity hits onStop and it isn’t a config-change restart
         if (owner is Activity && !owner.isChangingConfigurations) {
-            maybeLockPrivateModeOnStop()
+            maybeLockPrivateModeOnPause()
         }
     }
 
@@ -193,7 +193,7 @@ class PrivateBrowsingLockFeature(
         storage.startObservingSharedPrefs()
     }
 
-    private fun maybeLockPrivateModeOnStop() {
+    private fun maybeLockPrivateModeOnPause() {
         // When the app gets inactive with opened tabs, we lock the private mode.
         if (browserStore.state.privateTabs.isNotEmpty()) {
             appStore.dispatch(
