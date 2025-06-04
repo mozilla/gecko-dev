ed295df7bed874c698661b8fe9e7526f68d71284
Iain Ireland <iireland@mozilla.com>
2025-06-04 18:53:38 +0000

    Bug 1960433: Enable trial inlining of baseline-interpreted scripts r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D251073

--- js/src/jit/WarpOracle.cpp
+++ js/src/jit/WarpOracle.cpp
@@ -1020,7 +1020,7 @@ AbortReasonOr<bool> WarpScriptOracle::maybeInlineCall(
   }
 
   RootedFunction targetFunction(cx_, inlineData->target);
-  if (!TrialInliner::canInline(targetFunction, script_, loc)) {
+  if (!TrialInliner::canInline(cx_, targetFunction, script_, loc)) {
     return false;
   }
 
