5814ea89429be468dbdd2ee5ef116309898c35c0
Florian Quèze <florian@queze.net>
2025-06-04 13:55:17 +0000

    Bug 1970274 - set the test.wait300msAfterTabSwitch pref in toolkit/modules/tests/browser/browser_LayoutUtils.js to silence frequent failures, r=jmaher.
    Differential Revision: https://phabricator.services.mozilla.com/D252462

--- toolkit/modules/tests/browser/browser_LayoutUtils.js
+++ toolkit/modules/tests/browser/browser_LayoutUtils.js
@@ -1,6 +1,12 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+add_task(async function () {
+  await SpecialPowers.pushPrefEnv({
+    set: [["test.wait300msAfterTabSwitch", true]],
+  });
+});
+
 add_task(async function test_rectToBrowserRect() {
   const tab = await BrowserTestUtils.openNewForegroundTab(
     gBrowser,
