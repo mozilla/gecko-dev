32be858e54915496fb52e6b5627c7efd5ff5a8f7
Mark Banner <standard8@mozilla.com>
2025-06-04 17:54:07 +0000

    Bug 1969878 - Make _MLSuggest.#modelEngines use a real Map, rather than a Map like object. r=urlbar-reviewers,jteow
    Differential Revision: https://phabricator.services.mozilla.com/D252134

--- browser/components/urlbar/private/MLSuggest.sys.mjs
+++ browser/components/urlbar/private/MLSuggest.sys.mjs
@@ -13,6 +13,10 @@ ChromeUtils.defineESModuleGetters(lazy, {
   UrlbarPrefs: "resource:///modules/UrlbarPrefs.sys.mjs",
 });
 
+/**
+ * @typedef {Awaited<ReturnType<import("chrome://global/content/ml/EngineProcess.sys.mjs").createEngine>>} MLEngine
+ */
+
 // List of prepositions used in subject cleaning.
 const PREPOSITIONS = ["in", "at", "on", "for", "to", "near"];
 
@@ -26,7 +30,10 @@ const NAME_PUNCTUATION_EXCEPT_DOT = NAME_PUNCTUATION.filter(p => p !== ".");
  * @class
  */
 class _MLSuggest {
-  #modelEngines = {};
+  /**
+   * @type {Map<string, MLEngine>}
+   */
+  #modelEngines = new Map();
 
   INTENT_OPTIONS = {
     taskName: "text-classification",
@@ -42,7 +49,11 @@ class _MLSuggest {
     numThreads: 2,
   };
 
-  // Helper to wrap createEngine for testing purpose
+  /**
+   * Helper to wrap createEngine for testing purposes.
+   *
+   * @returns {MLEngine}
+   */
   createEngine(args) {
     return lazy.createEngine(args);
   }
@@ -130,12 +141,12 @@ class _MLSuggest {
    * Shuts down all initialized engines.
    */
   async shutdown() {
-    for (const [key, engine] of Object.entries(this.#modelEngines)) {
+    for (const [key, engine] of this.#modelEngines.entries()) {
       try {
         await engine.terminate?.();
       } finally {
         // Remove each engine after termination
-        delete this.#modelEngines[key];
+        this.#modelEngines.delete(key);
       }
     }
   }
@@ -144,13 +155,14 @@ class _MLSuggest {
     const featureId = options.featureId;
 
     // uses cache if engine was used
-    if (this.#modelEngines[featureId]) {
-      return this.#modelEngines[featureId];
+    let engine = this.#modelEngines.get(featureId);
+    if (engine) {
+      return engine;
     }
 
-    const engine = await this.createEngine(options);
+    engine = await this.createEngine(options);
     // Cache the engine
-    this.#modelEngines[featureId] = engine;
+    this.#modelEngines.set(featureId, engine);
     return engine;
   }
 
@@ -166,8 +178,9 @@ class _MLSuggest {
    *   The intent results or null if the model is not initialized.
    */
   async _findIntent(query, options = {}) {
-    const engineIntentClassifier =
-      this.#modelEngines[this.INTENT_OPTIONS.featureId];
+    const engineIntentClassifier = this.#modelEngines.get(
+      this.INTENT_OPTIONS.featureId
+    );
     if (!engineIntentClassifier) {
       return null;
     }
@@ -181,7 +194,7 @@ class _MLSuggest {
     } catch (error) {
       // engine could timeout or fail, so remove that from cache
       // and reinitialize
-      this.#modelEngines[this.INTENT_OPTIONS.featureId] = null;
+      this.#modelEngines.delete(this.INTENT_OPTIONS.featureId);
       this.#initializeModelEngine(this.INTENT_OPTIONS);
       return null;
     }
@@ -200,13 +213,13 @@ class _MLSuggest {
    *   The NER results or null if the model is not initialized.
    */
   async _findNER(query, options = {}) {
-    const engineNER = this.#modelEngines[this.NER_OPTIONS.featureId];
+    const engineNER = this.#modelEngines.get(this.NER_OPTIONS.featureId);
     try {
       return engineNER?.run({ args: [query], options });
     } catch (error) {
       // engine could timeout or fail, so remove that from cache
       // and reinitialize
-      this.#modelEngines[this.NER_OPTIONS.featureId] = null;
+      this.#modelEngines.delete(this.NER_OPTIONS.featureId);
       this.#initializeModelEngine(this.NER_OPTIONS);
       return null;
     }
