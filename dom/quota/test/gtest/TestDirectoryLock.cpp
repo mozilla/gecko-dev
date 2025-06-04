9ffeecd1a97b8f7a63b0d3888993d8beb6f88139
Jan Varga <Jan.Varga@gmail.com>
2025-06-04 16:50:01 +0000

    Bug 1968869 - QM: Add testing for directory lock blocking behavior; r=dom-storage-reviewers,aiunusov
    Differential Revision: https://phabricator.services.mozilla.com/D252191

--- dom/quota/test/gtest/TestDirectoryLock.cpp
+++ dom/quota/test/gtest/TestDirectoryLock.cpp
@@ -20,6 +20,12 @@ class DOM_Quota_DirectoryLock : public QuotaManagerDependencyFixture {
   static void TearDownTestCase() { ASSERT_NO_FATAL_FAILURE(ShutdownFixture()); }
 };
 
+using BoolTripleTestParams = std::tuple<bool, bool, bool>;
+
+class DOM_Quota_DirectoryLockWithBoolTriple
+    : public DOM_Quota_DirectoryLock,
+      public testing::WithParamInterface<BoolTripleTestParams> {};
+
 TEST_F(DOM_Quota_DirectoryLock, MutableManagerRef) {
   PerformOnBackgroundThread([]() {
     QuotaManager* quotaManager = QuotaManager::Get();
@@ -81,5 +87,64 @@ TEST_F(DOM_Quota_DirectoryLock, Drop_Timing) {
     sharedDirectoryLock = nullptr;
   });
 }
+
+TEST_P(DOM_Quota_DirectoryLockWithBoolTriple, BlockingBehavior) {
+  const BoolTripleTestParams& param = GetParam();
+
+  const bool firstExclusive = std::get<0>(param);
+  const bool secondExclusive = std::get<1>(param);
+  const bool secondMustWait = std::get<2>(param);
+
+  PerformOnBackgroundThread([firstExclusive, secondExclusive,
+                             secondMustWait]() {
+    QuotaManager* quotaManager = QuotaManager::Get();
+    ASSERT_TRUE(quotaManager);
+
+    RefPtr<ClientDirectoryLock> directoryLock1 =
+        quotaManager->CreateDirectoryLock(GetTestClientMetadata(),
+                                          /* aExclusive */ firstExclusive);
+    Await(directoryLock1->Acquire());
+
+    RefPtr<ClientDirectoryLock> directoryLock2 =
+        quotaManager->CreateDirectoryLock(GetTestClientMetadata(),
+                                          /* aExclusive */ secondExclusive);
+
+    EXPECT_EQ(directoryLock2->MustWait(), secondMustWait);
+    EXPECT_EQ(directoryLock2->LocksMustWaitFor().IsEmpty(), !secondMustWait);
+
+    DropDirectoryLock(directoryLock1);
+  });
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    , DOM_Quota_DirectoryLockWithBoolTriple,
+    testing::Values(std::make_tuple(/* firstExclusive */ false,
+                                    /* secondExclusive */ false,
+                                    /* secondMustWait */ false),
+                    std::make_tuple(/* firstExclusive */ false,
+                                    /* secondExclusive */ true,
+                                    /* secondMustWait */ true),
+                    std::make_tuple(/* firstExclusive */ true,
+                                    /* secondExclusive */ false,
+                                    /* secondMustWait */ true),
+                    std::make_tuple(/* firstExclusive */ true,
+                                    /* secondExclusive */ true,
+                                    /* secondMustWait */ true)),
+    [](const testing::TestParamInfo<BoolTripleTestParams>& aParam)
+        -> std::string {
+      const BoolTripleTestParams& param = aParam.param;
+
+      const bool firstExclusive = std::get<0>(param);
+      const bool secondExclusive = std::get<1>(param);
+      const bool secondMustWait = std::get<2>(param);
+
+      std::stringstream ss;
+
+      ss << (firstExclusive ? "FirstExclusive" : "FirstShared") << "_"
+         << (secondExclusive ? "SecondExclusive" : "SecondShared") << "_"
+         << (secondMustWait ? "SecondMustWait" : "SecondMustNotWait");
+
+      return ss.str();
+    });
 
 }  // namespace mozilla::dom::quota::test
