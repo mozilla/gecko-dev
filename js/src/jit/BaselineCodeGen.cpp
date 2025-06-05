41521e1e3e00c0739615b5c5eaea2ff703672446
Iain Ireland <iireland@mozilla.com>
2025-06-05 23:57:58 +0000

    Bug 1960433: Enable trial inlining of baseline-interpreted scripts r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D251073

--- js/src/jit/BaselineCodeGen.cpp
+++ js/src/jit/BaselineCodeGen.cpp
@@ -420,6 +420,7 @@ void BaselineCompilerHandler::maybeDisableIon() {
   if (analysis_.isInliningDisabled()) {
     script()->setUninlineable();
   }
+  script()->jitScript()->setRanBytecodeAnalysis();
 }
 
 // On most platforms we use a dedicated bytecode PC register to avoid many
@@ -1357,10 +1358,22 @@ void BaselineInterpreterCodeGen::emitInitFrameFields(Register nonFunctionEnv) {
   masm.bind(&done);
   masm.storePtr(scratch1, frame.addressOfInterpreterScript());
 
-  // Initialize icScript and interpreterICEntry
+  // Load the ICScript in scratch2..
+  Label inlined, haveICScript;
+  masm.branchTest32(Assembler::NonZero, frame.addressOfDescriptor(),
+                    Imm32(FrameDescriptor::HasInlinedICScript), &inlined);
   masm.loadJitScript(scratch1, scratch2);
   masm.computeEffectiveAddress(Address(scratch2, JitScript::offsetOfICScript()),
                                scratch2);
+  masm.jump(&haveICScript);
+  masm.bind(&inlined);
+  masm.loadPtr(Address(FramePointer, 0), scratch2);
+  masm.loadPtr(
+      Address(scratch2, BaselineStubFrameLayout::InlinedICScriptOffsetFromFP),
+      scratch2);
+  masm.bind(&haveICScript);
+
+  // Initialize icScript and interpreterICEntry
   masm.storePtr(scratch2, frame.addressOfICScript());
   masm.computeEffectiveAddress(Address(scratch2, ICScript::offsetOfICEntries()),
                                scratch2);
@@ -1736,6 +1749,11 @@ bool BaselineCompilerCodeGen::emitWarmUpCounterIncrement() {
 
 template <>
 bool BaselineInterpreterCodeGen::emitWarmUpCounterIncrement() {
+  // Emit no warm-up counter increments if Baseline is disabled.
+  if (!JitOptions.baselineJit) {
+    return true;
+  }
+
   Register scriptReg = R2.scratchReg();
   Register countReg = R0.scratchReg();
 
