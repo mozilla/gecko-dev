7528339679519d366f178b710b317f0b949bcd8b
mcheang <mcheang@mozilla.com>
2025-06-05 22:53:09 +0000

    Bug 1964110 - Add test for Special Action SET_SEARCH_MODE. r=jteow,aminomancer,omc-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D250539

--- toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_set_search_mode.js
+++ toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_set_search_mode.js
@@ -1,22 +1,37 @@
 /* Any copyright is dedicated to the Public Domain.
    https://creativecommons.org/publicdomain/zero/1.0/ */
 
+/**
+ * Verifies that the SET_SEARCH_MODE action correctly enters search mode
+ * with the expected engine and properties.
+ */
+
 "use strict";
 
+ChromeUtils.defineLazyGetter(this, "UrlbarTestUtils", () => {
+  const { UrlbarTestUtils } = ChromeUtils.importESModule(
+    "resource://testing-common/UrlbarTestUtils.sys.mjs"
+  );
+  UrlbarTestUtils.init(this);
+  return UrlbarTestUtils;
+});
+
 add_task(async function test_SET_SEARCH_MODE() {
-  const action = {
+  let searchMode = {
+    engineName: "Bing",
+    isGeneralPurposeEngine: true,
+    source: 3,
+    isPreview: false,
+    entry: "other",
+  };
+
+  let action = {
     type: "SET_SEARCH_MODE",
-    data: {
-      engineName: "Perplexity",
-      isGeneralPurposeEngine: true,
-      source: 3,
-      isPreview: false,
-      entry: "other",
-    },
+    data: searchMode,
     dismiss: true,
   };
-  await SMATestUtils.validateAction(action);
 
-  // TODO: Add action execution test
-  // Bug 1964110 - Add test for Special Action to set search mode
+  await SMATestUtils.executeAndValidateAction(action);
+  await UrlbarTestUtils.assertSearchMode(window, searchMode);
+  await UrlbarTestUtils.exitSearchMode(window);
 });
