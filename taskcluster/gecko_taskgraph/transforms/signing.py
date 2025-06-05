56c3f1e61b02cc6de493a17e73dab3ea0ab4ae24
Julien Cristau <jcristau@mozilla.com>
2025-06-05 17:24:19 +0000

    Bug 1970375 - don't retry notarization tasks. r=releng-reviewers,taskgraph-reviewers,Eijebong DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D252505

--- taskcluster/gecko_taskgraph/transforms/signing.py
+++ taskcluster/gecko_taskgraph/transforms/signing.py
@@ -208,6 +208,7 @@ def make_task_description(config, jobs):
                     attributes.get("build_type"),
                 )
             )
+            task["retries"] = 0
         elif "macosx" in build_platform:
             # iscript overrides
             task["worker"]["implementation"] = "iscript"
