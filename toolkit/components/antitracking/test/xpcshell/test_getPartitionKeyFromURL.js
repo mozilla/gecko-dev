966ebabffcfbd3cf21e9f8800892b1dfbdfd5e3e
Benjamin VanderSloot <bvandersloot@mozilla.com>
2025-06-05 16:41:15 +0000

    Bug 1970207 - Partitioning by Site should not include the port - r=anti-tracking-reviewers,emz
    The decision to include the port predated the spec definition of Site.
    So with all partiitoning aligning on Site between browsers, we should
    conform.
    
    When we enable this pref, suddenly contexts with a top level with non
    standard port have a different partition key. This means that when
    computing the partition key to grab their data, the place they reach to
    may be different. This is acceptable due to the low prevalence of top
    level non-standard ports. That data will still be there and accessible
    if we flip the pref back in the case of a critical incident.
    
    Differential Revision: https://phabricator.services.mozilla.com/D252386

--- toolkit/components/antitracking/test/xpcshell/test_getPartitionKeyFromURL.js
+++ toolkit/components/antitracking/test/xpcshell/test_getPartitionKeyFromURL.js
@@ -77,7 +77,7 @@ const TEST_CASES = [
   },
   {
     url: "http://127.0.0.1:8080/",
-    partitionKeySite: "(http,127.0.0.1,8080)",
+    partitionKeySite: "(http,127.0.0.1)",
     partitionKeyWithoutSite: "127.0.0.1",
   },
   {
@@ -87,7 +87,7 @@ const TEST_CASES = [
   },
   {
     url: "http://[2001:db8::ff00:42:8329]:8080",
-    partitionKeySite: "(http,[2001:db8::ff00:42:8329],8080)",
+    partitionKeySite: "(http,[2001:db8::ff00:42:8329])",
     partitionKeyWithoutSite: "[2001:db8::ff00:42:8329]",
   },
   // Tests for moz-extension
@@ -109,7 +109,7 @@ const TEST_CASES = [
   },
   {
     url: "http://com:8080",
-    partitionKeySite: "(http,com,8080)",
+    partitionKeySite: "(http,com)",
     partitionKeyWithoutSite: "com",
   },
 ];
