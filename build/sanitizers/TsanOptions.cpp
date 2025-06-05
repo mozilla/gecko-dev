87f7621e873d3313dcc2177a5d917e978a76dfa0
stransky <stransky@redhat.com>
2025-06-05 12:25:53 +0000

    Bug 1953677 [Linux] Allow data race at Intel/MESA drivers r=decoder
    Differential Revision: https://phabricator.services.mozilla.com/D252478

--- build/sanitizers/TsanOptions.cpp
+++ build/sanitizers/TsanOptions.cpp
@@ -168,6 +168,8 @@ extern "C" MOZ_EXPORT const char* __tsan_default_suppressions() {
          // Bug 1825171
          "race:libffi.so\n"
          "race:mozilla::widget::WaylandBuffer::BufferReleaseCallbackHandler\n"
+          // Bug 1953677
+         "race:i965_dri.so\n"
 
 
 
