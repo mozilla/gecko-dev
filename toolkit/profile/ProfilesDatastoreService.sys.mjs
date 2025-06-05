00890c6c92c8b23be261ded70b10c6241978e136
Beth Rennie <beth@brennie.ca>
2025-06-05 11:42:51 +0000

    Bug 1969994 - Make the Nimbus profile ID persistent  a=dmeehan
    Bug 1956080 added the Nimbus profile ID but due to pref branch
    interactions it wasn't actually persistent. As a result, we need to
    wipe the NimbusEnrollments table and re-run the
    import-enrollments-to-sql migration.
    
    Original Revision: https://phabricator.services.mozilla.com/D252248
    
    Differential Revision: https://phabricator.services.mozilla.com/D252507

--- toolkit/profile/ProfilesDatastoreService.sys.mjs
+++ toolkit/profile/ProfilesDatastoreService.sys.mjs
@@ -61,7 +61,7 @@ class ProfilesDatastoreServiceClass {
   async createTables() {
     // TODO: (Bug 1902320) Handle exceptions on connection opening
     let currentVersion = await this.#connection.getSchemaVersion();
-    if (currentVersion == 2) {
+    if (currentVersion == 3) {
       return;
     }
 
@@ -121,6 +121,11 @@ class ProfilesDatastoreServiceClass {
       });
 
       await this.#connection.setSchemaVersion(2);
+    }
+
+    if (currentVersion < 3) {
+      await this.#connection.execute("DELETE FROM NimbusEnrollments;");
+      await this.#connection.setSchemaVersion(3);
     }
   }
 
