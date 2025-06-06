13c0a21a605e8053ea001fe5d547df36917f6409
Daniel Minor <dminor@mozilla.com>
2025-06-06 14:52:07 +0000

    Bug 1967744 - Skip explict-resource-management/bug1934365.js on debug; r=mgaudet
    This is timing out in CI for some debug jobs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D251853

--- js/src/jit-test/tests/explicit-resource-management/bug1934365.js
+++ js/src/jit-test/tests/explicit-resource-management/bug1934365.js
@@ -1,4 +1,4 @@
-// |jit-test| skip-if: !getBuildConfiguration("explicit-resource-management")
+// |jit-test| skip-if: !getBuildConfiguration("explicit-resource-management") || getBuildConfiguration("debug")
 
 if ('oomTest' in this) {
   function b() {
