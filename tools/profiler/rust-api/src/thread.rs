7ff1a407fc6ab08fe1f6b9f7b7e726b294b26eeb
asthetik <asthetik.sun@outlook.com>
2025-06-06 20:17:57 +0000

    Bug 1968272 - fix formatting of documentation comment. r=sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D252897

--- tools/profiler/rust-api/src/thread.rs
+++ tools/profiler/rust-api/src/thread.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-///! Profiler API for thread registration and unregistration.
+//! Profiler API for thread registration and unregistration.
 use crate::gecko_bindings::bindings;
 use std::ffi::CString;
 
