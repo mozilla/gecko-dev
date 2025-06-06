1fdb2705ec6179de612450ff915d6b68b19930d7
serge-sans-paille <sguelton@mozilla.com>
2025-06-06 14:42:25 +0000

    Bug 1966311 - Use weak android symbol instead of dlopen/dlsym for android/Ashmem.cpp r=glandium
    This requires adding libandroid to libmozglue so that symbol lookup
    can find the actual symbol.
    
    Differential Revision: https://phabricator.services.mozilla.com/D249226

--- mozglue/android/Ashmem.cpp
+++ mozglue/android/Ashmem.cpp
@@ -3,7 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include <cstring>
-#include <dlfcn.h>
 #include <fcntl.h>
 #include <linux/ashmem.h>
 #include <stdio.h>
@@ -12,22 +11,16 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
+#include <android/sharedmem.h>
+
 #include "Ashmem.h"
 
 namespace mozilla {
 namespace android {
 
-static void* libhandle() {
-  static void* handle = dlopen("libandroid.so", RTLD_LAZY | RTLD_LOCAL);
-  return handle;
-}
-
 int ashmem_create(const char* name, size_t size) {
-  static auto fCreate =
-      (int (*)(const char*, size_t))dlsym(libhandle(), "ASharedMemory_create");
-
-  if (fCreate) {
-    return fCreate(name, size);
+  if (__builtin_available(android 26, *)) {
+    return ASharedMemory_create(name, size);
   }
 
   int fd = open("/" ASHMEM_NAME_DEF, O_RDWR);
@@ -50,20 +43,16 @@ int ashmem_create(const char* name, size_t size) {
 }
 
 size_t ashmem_getSize(int fd) {
-  static auto fGetSize =
-      (size_t (*)(int))dlsym(libhandle(), "ASharedMemory_getSize");
-  if (fGetSize) {
-    return fGetSize(fd);
+  if (__builtin_available(android 26, *)) {
+    return ASharedMemory_getSize(fd);
   }
 
   return (size_t)ioctl(fd, ASHMEM_GET_SIZE, nullptr);
 }
 
 int ashmem_setProt(int fd, int prot) {
-  static auto fSetProt =
-      (int (*)(int, int))dlsym(libhandle(), "ASharedMemory_setProt");
-  if (fSetProt) {
-    return fSetProt(fd, prot);
+  if (__builtin_available(android 26, *)) {
+    return ASharedMemory_setProt(fd, prot);
   }
 
   return ioctl(fd, ASHMEM_SET_PROT_MASK, prot);
