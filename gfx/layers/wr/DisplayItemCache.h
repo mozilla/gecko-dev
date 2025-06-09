56164e434f17747908dee768730f6c297e8ab795
Nicolas Silva <nical@fastmail.com>
2025-06-09 14:20:30 +0000

    Bug 1923846 - Render VT old-state snapshots offscreen without touching the main scene. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D252501

--- gfx/layers/wr/DisplayItemCache.h
+++ gfx/layers/wr/DisplayItemCache.h
@@ -188,7 +188,7 @@ class MOZ_RAII AutoDisplayItemCacheSuppressor {
  public:
   explicit AutoDisplayItemCacheSuppressor(DisplayItemCache* aCache)
       : mCache(aCache) {
-    mWasSuppressed = mCache->SetSuppressed(true);
+    mWasSuppressed = !mCache || mCache->SetSuppressed(true);
   }
 
   // Note that this restores the original state rather than unconditionally
@@ -197,7 +197,11 @@ class MOZ_RAII AutoDisplayItemCacheSuppressor {
   // decide to expand its usage to other scenarios and end up with nested
   // suppressions, in which case restoring the state back to what we found it
   // is better.
-  ~AutoDisplayItemCacheSuppressor() { mCache->SetSuppressed(mWasSuppressed); }
+  ~AutoDisplayItemCacheSuppressor() {
+    if (mCache) {
+      mCache->SetSuppressed(mWasSuppressed);
+    }
+  }
 
  private:
   DisplayItemCache* mCache;
