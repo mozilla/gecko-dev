8208ad2ea9eea8fe8a200dedc6a7f1d702ce7529
Segun Famisa <sfamisa@mozilla.com>
2025-06-09 10:43:11 +0000

    Bug 1970675 - Revert changes that reworked StartupState detection logic for PerfStartup telemetry r=android-reviewers,matt-tighe
    Revert "Bug 1959449 - Create new StartupState detection logic in StartupStateDetector r=android-reviewers,matt-tighe"
    
    This reverts commit 48b4c9956892c8910dd2e6399ee147db5eb1c921.
    
    Revert "Bug 1959449 - Fix PerfStartup telemetry - delete StartupStateProvider in favour of StartupStateDetector r=android-reviewers,matt-tighe"
    
    This reverts commit 8be013bfd1bbd05afba125020706e05362959164.
    
    Differential Revision: https://phabricator.services.mozilla.com/D252706

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
@@ -274,7 +274,6 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
         registerActivityLifecycleCallbacks(visibilityLifecycleCallback)
         registerActivityLifecycleCallbacks(MarkersActivityLifecycleCallbacks(components.core.engine))
 
-        components.performance.startupStateDetector.registerInAppOnCreate(this)
         components.appStartReasonProvider.registerInAppOnCreate(this)
         components.startupActivityLog.registerInAppOnCreate(this)
         initVisualCompletenessQueueAndQueueTasks()
