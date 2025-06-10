50625c3d49d4978c5704d9aa60d988760a6dfde0
Serban Stanca <sstanca@mozilla.com>
2025-06-10 12:21:46 +0300

    Revert "Bug 1958723: [webdriver] Improve error for attempts to install unpacked extension permanently. r=webdriver-reviewers,whimboo" for causing webdriver failures in moz_permanent.py.
    This reverts commit cc5f900836da6755e5af98f49a0883c86a76c783.

--- testing/web-platform/mozilla/tests/webdriver/bidi/web_extension/moz_permanent.py
+++ testing/web-platform/mozilla/tests/webdriver/bidi/web_extension/moz_permanent.py
@@ -1,6 +1,5 @@
 import pytest
 from support.addons import is_addon_temporary_installed
-from support.helpers import clear_pref, set_pref
 from tests.bidi.web_extension import assert_extension_id
 from webdriver.bidi import error
 
@@ -12,47 +11,36 @@ pytestmark = pytest.mark.asyncio
     "permanent", [None, False, True], ids=["default", "temporary", "permanent"]
 )
 @pytest.mark.parametrize("mode", ["archivePath", "base64", "path"])
-@pytest.mark.parametrize("signed", [True, False], ids=["signed", "unsigned"])
 async def test_install_with_permanent(
-    bidi_session, current_session, extension_data, mode, permanent, signed
+    bidi_session, current_session, extension_data, mode, permanent
 ):
-    if mode == "path" and signed:
-        # Unpacked extensions are not signed
-        return
     data = {"type": mode}
-    unsigned_tag = "" if signed or mode == "path" else "Unsigned"
-    extension_data_value = extension_data[f"{mode}{unsigned_tag}"]
     if mode == "base64":
-        data.update({"value": extension_data_value})
+        data.update({"value": extension_data[mode]})
     else:
-        data.update({"path": extension_data_value})
+        data.update({"path": extension_data[mode]})
 
     kwargs = {"moz:permanent": permanent} if permanent is not None else {}
 
-    if permanent and not signed:
-        try:
-            with pytest.raises(error.InvalidWebExtensionException):
-                set_pref(current_session, "xpinstall.signatures.required", True)
-                await bidi_session.web_extension.install(
-                    extension_data=data,
-                    **kwargs,
-                )
-        finally:
-            clear_pref(current_session, "xpinstall.signatures.required")
+    if mode == "path" and permanent:
+        # Only signed webextensions in XPI format can be installed permanently
+        with pytest.raises(error.InvalidWebExtensionException):
+            await bidi_session.web_extension.install(
+                extension_data=data,
+                **kwargs,
+            )
         return
 
-    try:
-        set_pref(current_session, "xpinstall.signatures.required", True)
-        web_extension = await bidi_session.web_extension.install(
-            extension_data=data,
-            **kwargs,
-        )
+    web_extension = await bidi_session.web_extension.install(
+        extension_data=data,
+        **kwargs,
+    )
 
+    try:
         assert_extension_id(web_extension, extension_data)
         assert is_addon_temporary_installed(current_session, web_extension) is not bool(
             permanent
         )
     finally:
         # Clean up the extension.
-        clear_pref(current_session, "xpinstall.signatures.required")
         await bidi_session.web_extension.uninstall(extension=web_extension)
