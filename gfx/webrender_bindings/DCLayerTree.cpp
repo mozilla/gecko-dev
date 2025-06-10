99f02d7a9456d4cfe2abd9c2a4a1eb8f3f41e22a
Sotaro Ikeda <sotaro.ikeda.g@gmail.com>
2025-06-10 11:15:01 +0000

    Bug 1945683 - Enable layer compositor on Windows in Nightly r=gfx-reviewers,lsalzman
    All known rendering bugs of layer compositor have been addressed. Then is seems OK to remove nightly only limitation of pref gfx.webrender.layer-compositor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D251788

--- gfx/webrender_bindings/DCLayerTree.cpp
+++ gfx/webrender_bindings/DCLayerTree.cpp
@@ -547,13 +547,8 @@ bool DCLayerTree::UseNativeCompositor() const {
 }
 
 bool DCLayerTree::UseLayerCompositor() const {
-// Only allow the layer compositor in nightly builds, for now.
-#ifdef NIGHTLY_BUILD
   return UseNativeCompositor() &&
          StaticPrefs::gfx_webrender_layer_compositor_AtStartup();
-#else
-  return false;
-#endif
 }
 
 void DCLayerTree::DisableNativeCompositor() {
