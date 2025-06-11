cec83ae76eca6cbb06a4d17b6ccfb39d9565f091
Alexandru Marc <amarc@mozila.com>
2025-06-11 18:58:41 +0300

    Revert "Bug 1955343 - Part 2: Pass actions to GeckoView r=nrishel,geckoview-reviewers,android-reviewers,nika,geckoview-api-reviewers,ohall" for causing android build bustages @ GeckoWebNotificationDelegateTest
    This reverts commit 42724e284b6f318b67486a1aeb7603817421d447.

--- mobile/android/android-components/components/feature/webnotifications/src/main/java/mozilla/components/feature/webnotifications/WebNotificationIntentProcessor.kt
+++ mobile/android/android-components/components/feature/webnotifications/src/main/java/mozilla/components/feature/webnotifications/WebNotificationIntentProcessor.kt
@@ -28,7 +28,7 @@ class WebNotificationIntentProcessor(
         return when (engineNotification) {
             null -> false
             else -> {
-                engine.handleWebNotificationClick(engineNotification, intent.action)
+                engine.handleWebNotificationClick(engineNotification)
                 true
             }
         }
--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/WebNotification.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/WebNotification.java
@@ -10,8 +10,6 @@ import android.os.Parcelable;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.UiThread;
-import androidx.core.os.ParcelCompat;
-import java.util.Arrays;
 import java.util.Objects;
 import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.annotation.WrapForJNI;
@@ -115,8 +113,6 @@ public class WebNotification implements Parcelable {
    */
   public final @NonNull int[] vibrate;
 
-  public final @NonNull WebNotificationAction[] actions;
-
   @WrapForJNI
   /* package */ WebNotification(
       @Nullable final String title,
@@ -130,8 +126,7 @@ public class WebNotification implements Parcelable {
       @NonNull final String source,
       final boolean silent,
       final boolean privateBrowsing,
-      @NonNull final int[] vibrate,
-      @NonNull final Object[] actions) {
+      @NonNull final int[] vibrate) {
     this.tag = tag;
     this.mCookie = cookie;
     this.title = title;
@@ -144,7 +139,6 @@ public class WebNotification implements Parcelable {
     this.silent = silent;
     this.vibrate = vibrate;
     this.privateBrowsing = privateBrowsing;
-    this.actions = Arrays.copyOf(actions, actions.length, WebNotificationAction[].class);
   }
 
   /**
@@ -155,20 +149,7 @@ public class WebNotification implements Parcelable {
   @UiThread
   public void click() {
     ThreadUtils.assertOnUiThread();
-    GeckoAppShell.onNotificationClick(tag, null);
-  }
-
-  /**
-   * This should be called when the user taps or clicks a notification action. Note that this does
-   * not automatically dismiss the notification as far as Web Content is concerned. For that, see
-   * {@link #dismiss()}.
-   *
-   * @param action The action name if an action button is clicked, otherwise null.
-   */
-  @UiThread
-  public void click(final @NonNull String action) {
-    ThreadUtils.assertOnUiThread();
-    GeckoAppShell.onNotificationClick(tag, action);
+    GeckoAppShell.onNotificationClick(tag, mCookie);
   }
 
   /**
@@ -178,11 +159,11 @@ public class WebNotification implements Parcelable {
   @UiThread
   public void dismiss() {
     ThreadUtils.assertOnUiThread();
-    GeckoAppShell.onNotificationClose(tag);
+    GeckoAppShell.onNotificationClose(tag, mCookie);
   }
 
   // Increment this value whenever anything changes in the parcelable representation.
-  private static final int VERSION = 2;
+  private static final int VERSION = 1;
 
   // To avoid TransactionTooLargeException, we only store small imageUrls
   private static final int IMAGE_URL_LENGTH_MAX = 150;
@@ -211,14 +192,9 @@ public class WebNotification implements Parcelable {
     dest.writeInt(silent ? 1 : 0);
     dest.writeInt(privateBrowsing ? 1 : 0);
     dest.writeIntArray(vibrate);
-    dest.writeParcelableArray(actions, 0);
   }
 
   private WebNotification(final Parcel in) {
-    final int version = in.readInt();
-    if (version != 1 && version != 2) {
-      throw new ParcelFormatException("Mismatched version: " + version + " expected: " + VERSION);
-    }
     title = in.readString();
     tag = Objects.requireNonNull(in.readString());
     mCookie = in.readString();
@@ -231,26 +207,17 @@ public class WebNotification implements Parcelable {
     silent = in.readInt() == 1;
     privateBrowsing = in.readInt() == 1;
     vibrate = Objects.requireNonNull(in.createIntArray());
-
-    // TODO: This exists for existing notifications opened from older releases before app updates.
-    // We should be able to remove version 1 support after a few release cycles.
-    // See bug 1970892.
-    if (version == 1) {
-      actions = new WebNotificationAction[0];
-      return;
-    }
-
-    final Parcelable[] actionParcels =
-        Objects.requireNonNull(
-            ParcelCompat.readParcelableArrayTyped(
-                in, WebNotificationAction.class.getClassLoader(), WebNotificationAction.class));
-    actions = Arrays.copyOf(actionParcels, actionParcels.length, WebNotificationAction[].class);
   }
 
   public static final Creator<WebNotification> CREATOR =
       new Creator<>() {
         @Override
         public WebNotification createFromParcel(final Parcel in) {
+          final int version = in.readInt();
+          if (version != VERSION) {
+            throw new ParcelFormatException(
+                "Mismatched version: " + version + " expected: " + VERSION);
+          }
           return new WebNotification(in);
         }
 
