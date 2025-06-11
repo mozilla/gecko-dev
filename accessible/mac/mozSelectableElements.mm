a369fbfe4dc03d06232fd40ca7ce6126693572b9
Cristina Horotan <chorotan@mozilla.com>
2025-06-11 04:16:08 +0300

    Revert "Bug 1970742: Unnest any AXGroup children before filtering AXSelectableChildren r=eeejay" for causing ba failures at browser_selectables.js
    This reverts commit 54016e89f5db497ebd7781aff00eae677a0b927c.

--- accessible/mac/mozSelectableElements.mm
+++ accessible/mac/mozSelectableElements.mm
@@ -114,7 +114,7 @@ using namespace mozilla::a11y;
 @implementation mozListboxAccessible
 
 - (BOOL)moxIgnoreChild:(mozAccessible*)child {
-  if (!child || [[child moxRole] isEqualToString:@"AXGroup"]) {
+  if (!child || child->mRole == roles::GROUPING) {
     return YES;
   }
 
