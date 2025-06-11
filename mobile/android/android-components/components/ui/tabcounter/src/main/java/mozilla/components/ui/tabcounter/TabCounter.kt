72bb47e90070556da32560dfe03c1c38c0984932
Noah Bond <nbond@mozilla.com>
2025-06-11 04:17:50 +0000

    Bug 1964523 - Part 6 - Migrate `ui-tabcounter` to M3 r=android-reviewers,gl
    Differential Revision: https://phabricator.services.mozilla.com/D252742

--- mobile/android/android-components/components/ui/tabcounter/src/main/java/mozilla/components/ui/tabcounter/TabCounter.kt
+++ mobile/android/android-components/components/ui/tabcounter/src/main/java/mozilla/components/ui/tabcounter/TabCounter.kt
@@ -9,8 +9,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
