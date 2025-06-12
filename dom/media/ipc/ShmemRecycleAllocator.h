94eacdf16e4854813b5922d7abd78c70649aa5a3
Andrew Osmond <aosmond@mozilla.com>
2025-06-12 15:46:23 +0000

    Bug 1965601 - Part 8. Add tickets for better control over buffer recycling with ShmemRecycleAllocator. r=media-playback-reviewers,padenot
    Tickets allow one to track which buffers were used for a specific
    transaction. While in RemoteDecoderChild, it is always able to free all
    outstanding buffers whenever a response is received,
    RemoteMediaDataEncoderChild requires more nuanced control where we only
    free a subset.
    
    This is particularly useful during encoding where the MediaDataEncoder
    contract allows for multiple outstanding Encode requests. Since they
    will be resolved in order, we can recycle buffers without waiting for
    the other transactions to complete.
    
    Differential Revision: https://phabricator.services.mozilla.com/D253088

--- dom/media/ipc/ShmemRecycleAllocator.h
+++ dom/media/ipc/ShmemRecycleAllocator.h
@@ -10,12 +10,34 @@
 
 namespace mozilla {
 
+// When used with ShmemRecycleAllocator instances, it allows for preventing a
+// subset of shmems from being recycled until a transaction is complete. This is
+// useful in cases such as encoding, where an arbitrary number of frames may be
+// sent for encoding before any others complete. This instead allows us to
+// recycle shmems as individual transactions complete without waiting for all
+// outstanding to be complete first.
+class ShmemRecycleTicket {
+ public:
+  NS_INLINE_DECL_REFCOUNTING_ONEVENTTARGET(ShmemRecycleTicket)
+
+  ShmemRecycleTicket() = default;
+
+ private:
+  template <class T>
+  friend class ShmemRecycleAllocator;
+
+  ~ShmemRecycleTicket() { MOZ_DIAGNOSTIC_ASSERT(mUsedShmems.IsEmpty()); }
+
+  AutoTArray<ShmemBuffer, 4> mUsedShmems;
+};
+
 template <class T>
 class ShmemRecycleAllocator {
  public:
   explicit ShmemRecycleAllocator(T* aActor)
       : mActor(aActor), mPool(1, ShmemPool::PoolType::DynamicPool) {}
   ShmemBuffer AllocateBuffer(size_t aSize,
+                             ShmemRecycleTicket* aTicket = nullptr,
                              ShmemPool::AllocationPolicy aPolicy =
                                  ShmemPool::AllocationPolicy::Unsafe) {
     ShmemBuffer buffer = mPool.Get(mActor, aSize, aPolicy);
@@ -23,7 +45,11 @@ class ShmemRecycleAllocator {
       return buffer;
     }
     MOZ_DIAGNOSTIC_ASSERT(aSize <= buffer.Get().Size<uint8_t>());
-    mUsedShmems.AppendElement(buffer.Get());
+    if (aTicket) {
+      aTicket->mUsedShmems.AppendElement(buffer.Get());
+    } else {
+      mUsedShmems.AppendElement(buffer.Get());
+    }
     mNeedCleanup = true;
     return buffer;
   }
@@ -35,6 +61,13 @@ class ShmemRecycleAllocator {
       ReleaseBuffer(ShmemBuffer(mem.Get()));
     }
     mUsedShmems.Clear();
+  }
+
+  void ReleaseTicket(ShmemRecycleTicket* aTicket) {
+    for (auto&& mem : aTicket->mUsedShmems) {
+      ReleaseBuffer(ShmemBuffer(mem.Get()));
+    }
+    aTicket->mUsedShmems.Clear();
   }
 
   void CleanupShmemRecycleAllocator() {
