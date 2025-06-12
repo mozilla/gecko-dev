73df90147954f00f79ac4713f0155bad17b4133d
Denis <dpalmeiro@mozilla.com>
2025-06-12 00:28:14 +0000

    Bug 1966952: Remove unused JS telemetry probes: JS_PAGELOAD_EXECUTION_MS, JS_PAGELOAD_DELAZIFICATION_MS, JS_PAGELOAD_XDR_ENCODING_MS, JS_PAGELOAD_BASELINE_COMPILE_MS, JS_PAGELOAD_GC_MS, JS_PAGELOAD_PARSE_MS, JS_PAGELOAD_PROTECT_MS. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D253241

--- js/src/gc/GC.cpp
+++ js/src/gc/GC.cpp
@@ -4734,13 +4734,6 @@ struct MOZ_RAII AutoSetZoneSliceThresholds {
 
 void GCRuntime::collect(bool nonincrementalByAPI, const SliceBudget& budget,
                         JS::GCReason reason) {
-  TimeStamp startTime = TimeStamp::Now();
-  auto timer = MakeScopeExit([&] {
-    if (Realm* realm = rt->mainContextFromOwnThread()->realm()) {
-      realm->timers.gcTime += TimeStamp::Now() - startTime;
-    }
-  });
-
   auto clearGCOptions = MakeScopeExit([&] {
     if (!isIncrementalGCInProgress()) {
       maybeGcOptions = Nothing();
