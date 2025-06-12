85a16ea613fc911aa8b0696fb9b895a661db8587
Florian Quèze <florian@queze.net>
2025-06-12 18:01:29 +0000

    Bug 1952432 - Set the test.wait300msAfterTabSwitch pref in browser/components/shell/test/browser_1119088.js, r=jmaher.
    Differential Revision: https://phabricator.services.mozilla.com/D253489

--- browser/components/shell/test/browser_1119088.js
+++ browser/components/shell/test/browser_1119088.js
@@ -89,6 +89,12 @@ function restoreDefaultBackground() {
   setAndCheckDesktopBackgroundCLI(defaultBackgroundPath);
 }
 
+add_setup(async function () {
+  await SpecialPowers.pushPrefEnv({
+    set: [["test.wait300msAfterTabSwitch", true]],
+  });
+});
+
 /**
  * Tests "Set As Desktop Background" platform implementation on macOS.
  *
