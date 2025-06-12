7a4f2b4d82a0c0ab62181424d70c652b6f37ce68
Nicholas Rishel <nrishel@mozilla.com>
2025-06-12 20:25:22 +0000

    Bug 1966829 - Add a Taskbar Tabs window manager. r=mossop,cdupuis,frontend-codestyle-reviewers
    This class manages the creation and removal of Taskbar Tabs windows. This maintains state necessary to determine what window a tab should return to if ejected from Taskbar Tabs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D249717

--- browser/components/taskbartabs/test/browser/head.js
+++ browser/components/taskbartabs/test/browser/head.js
@@ -2,41 +2,45 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+"use strict";
+
+ChromeUtils.defineESModuleGetters(this, {
+  TaskbarTabsRegistry:
+    "resource:///modules/taskbartabs/TaskbarTabsRegistry.sys.mjs",
+  TaskbarTabsWindowManager:
+    "resource:///modules/taskbartabs/TaskbarTabsWindowManager.sys.mjs",
+});
+
+add_setup(async () => {
+  await SpecialPowers.pushPrefEnv({
+    set: [["network.dns.localDomains", "www.test.com"]],
+  });
+});
+
 /**
  * Creates a web app window with the given tab,
  * then returns the window object for testing.
  *
- * @param {Tab} tab
- *        The tab that the web app should open with,
- *        about:blank will be opened if this value is null.
+ * @param {Tab} aTab
+ *        The tab that the web app should open with
  * @returns {Promise}
  *        The web app window object.
  */
-async function openTaskbarTabWindow(tab = null) {
-  let extraOptions = Cc["@mozilla.org/hash-property-bag;1"].createInstance(
-    Ci.nsIWritablePropertyBag2
-  );
-  extraOptions.setPropertyAsBool("taskbartab", true);
+async function openTaskbarTabWindow(aTab = null) {
+  const url = Services.io.newURI("https://www.test.com");
+  const userContextId = 0;
 
-  let args = Cc["@mozilla.org/array;1"].createInstance(Ci.nsIMutableArray);
+  const registry = new TaskbarTabsRegistry();
+  const taskbarTab = registry.findOrCreateTaskbarTab(url, userContextId);
+  const windowManager = new TaskbarTabsWindowManager();
 
-  args.appendElement(tab);
-  args.appendElement(extraOptions);
-  args.appendElement(null);
+  const windowPromise = BrowserTestUtils.waitForNewWindow();
 
-  // Simulate opening a taskbar tab window
-  let win = Services.ww.openWindow(
-    null,
-    AppConstants.BROWSER_CHROME_URL,
-    "_blank",
-    "chrome,dialog=no,titlebar,close,toolbar,location,personalbar=no,status,menubar=no,resizable,minimizable,scrollbars",
-    args
-  );
+  if (aTab) {
+    windowManager.replaceTabWithWindow(taskbarTab, aTab);
+  } else {
+    windowManager.openWindow(taskbarTab);
+  }
 
-  await new Promise(resolve => {
-    win.addEventListener("load", resolve, { once: true });
-  });
-  await win.delayedStartupPromise;
-
-  return win;
+  return await windowPromise;
 }
