2a9b479a7342a63776f8498a5e3992fcd300b6d0
Rhodwell Malicdem <rmalicdem@mozilla.com>
2025-06-13 16:13:58 +0000

    Bug 1969109 - Add test selectors for MainMenu compose r=android-reviewers,skhan
    Differential Revision: https://phabricator.services.mozilla.com/D251761

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/efficiency/selectors/MainMenuComposeSelectors.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/efficiency/selectors/MainMenuComposeSelectors.kt
@@ -15,15 +15,63 @@ object MainMenuComposeSelectors {
         groups = listOf("removedIn=141"),
     )
 
+    val EXTENSIONS_BUTTON = Selector(
+        strategy = SelectorStrategy.COMPOSE_BY_CONTENT_DESCRIPTION,
+        value = getStringResource(R.string.browser_menu_extensions) + getStringResource(R.string.browser_menu_try_a_recommended_extension_description),
+        description = "Main menu Extensions button",
+        groups = listOf("requiredForPage"),
+    )
+
     val BOOKMARKS_BUTTON = Selector(
         strategy = SelectorStrategy.COMPOSE_BY_CONTENT_DESCRIPTION,
-        value = "Bookmarks",
+        value = getStringResource(R.string.library_bookmarks),
         description = "Main menu Bookmarks button",
+        groups = listOf("requiredForPage"),
+    )
+
+    val HISTORY_BUTTON = Selector(
+        strategy = SelectorStrategy.COMPOSE_BY_CONTENT_DESCRIPTION,
+        value = getStringResource(R.string.library_history),
+        description = "Main menu History button",
+        groups = listOf("requiredForPage"),
+    )
+
+    val DOWLOADS_BUTTON = Selector(
+        strategy = SelectorStrategy.COMPOSE_BY_CONTENT_DESCRIPTION,
+        value = getStringResource(R.string.library_downloads),
+        description = "Main menu Downloads button",
+        groups = listOf("requiredForPage"),
+    )
+
+    val PASSWORDS_BUTTON = Selector(
+        strategy = SelectorStrategy.COMPOSE_BY_CONTENT_DESCRIPTION,
+        value = getStringResource(R.string.browser_menu_passwords),
+        description = "Main menu Passwords button",
+        groups = listOf("requiredForPage"),
+    )
+
+    val SIGN_IN_BUTTON = Selector(
+        strategy = SelectorStrategy.COMPOSE_BY_CONTENT_DESCRIPTION,
+        value = getStringResource(R.string.browser_menu_sign_in) + getStringResource(R.string.browser_menu_sign_in_caption),
+        description = "Main menu Sign in button",
+        groups = listOf("requiredForPage"),
+    )
+
+    val SETTINGS_BUTTON = Selector(
+        strategy = SelectorStrategy.COMPOSE_BY_CONTENT_DESCRIPTION,
+        value = getStringResource(R.string.browser_menu_settings),
+        description = "Main menu Settings button",
         groups = listOf("requiredForPage"),
     )
 
     val all = listOf(
         NEW_PRIVATE_TAB_BUTTON,
+        EXTENSIONS_BUTTON,
         BOOKMARKS_BUTTON,
+        HISTORY_BUTTON,
+        DOWLOADS_BUTTON,
+        PASSWORDS_BUTTON,
+        SIGN_IN_BUTTON,
+        SETTINGS_BUTTON,
     )
 }
