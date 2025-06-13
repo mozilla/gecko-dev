ccd5538f56f8410f931fd0726b32b875693b6b91
Julien Cristau <jcristau@mozilla.com>
2025-06-13 12:32:50 +0000

    Bug 1971964 - don't pass the `push` parameter in the merge automation cron jobs r=releng-reviewers,taskgraph-reviewers,bhearsum DONTBUILD
    The `push: true` is passed down from the action task to the
    merge-automation task's payload, but it's only a valid parameter for
    treescript, not landoscript.
    
    Differential Revision: https://phabricator.services.mozilla.com/D253644

--- taskcluster/gecko_taskgraph/actions/merge_automation.py
+++ taskcluster/gecko_taskgraph/actions/merge_automation.py
@@ -32,7 +32,7 @@ def is_release_promotion_available(parameters):
             },
             "push": {
                 "type": "boolean",
-                "description": "Push changes using to_repo and to_branch",
+                "description": "Push changes using to_repo and to_branch (mercurial only)",
                 "default": False,
             },
             "behavior": {
