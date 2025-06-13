dcf04599c75991e94a63793500cfbff3383a219c
Cosmin Sabou <csabou@mozilla.com>
2025-06-13 04:55:45 +0300

    Revert "Bug 1964496 - Migrate normandy heartbeat storage to use ProfileDatastoreService r=nimbus-reviewers,profiles-reviewers,beth,jhirsch" for causing xpcshell failures on test_NimbusTestUtils.js.
    This reverts commit b5d4a94db6336b1dec4cf7998076ac70e05d6cd2.

--- toolkit/profile/ProfilesDatastoreService.sys.mjs
+++ toolkit/profile/ProfilesDatastoreService.sys.mjs
@@ -61,7 +61,7 @@ class ProfilesDatastoreServiceClass {
   async createTables() {
     // TODO: (Bug 1902320) Handle exceptions on connection opening
     let currentVersion = await this.#connection.getSchemaVersion();
-    if (currentVersion == 4) {
+    if (currentVersion == 3) {
       return;
     }
 
@@ -126,23 +126,6 @@ class ProfilesDatastoreServiceClass {
     if (currentVersion < 3) {
       await this.#connection.execute("DELETE FROM NimbusEnrollments;");
       await this.#connection.setSchemaVersion(3);
-    }
-
-    if (currentVersion < 4) {
-      await this.#connection.executeTransaction(async () => {
-        const createHeartbeatTable = `
-            CREATE TABLE IF NOT EXISTS "Heartbeats" (
-              id              INTEGER NOT NULL,
-              recipeId        TEXT NOT NULL UNIQUE,
-              lastShown       INTEGER,
-              lastInteraction	INTEGER,
-              PRIMARY KEY(id)
-            );`;
-
-        await this.#connection.execute(createHeartbeatTable);
-      });
-
-      await this.#connection.setSchemaVersion(4);
     }
   }
 
