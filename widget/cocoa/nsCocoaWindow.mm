bf9cebccdd3384241c2ab12d2f77aed6d29ff1e1
Jeff Muizelaar <jmuizelaar@mozilla.com>
2025-06-13 20:10:06 +0000

    Bug 1972107 -  Add a profiler marker for HandleMainThreadCATransaction. r=mstange
    Differential Revision: https://phabricator.services.mozilla.com/D253719

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -840,6 +840,8 @@ void nsCocoaWindow::PaintWindowInContentLayer() {
 }
 
 void nsCocoaWindow::HandleMainThreadCATransaction() {
+  AUTO_PROFILER_TRACING_MARKER("Paint", "HandleMainThreadCATransaction",
+                               GRAPHICS);
   WillPaintWindow();
 
   if (GetWindowRenderer()->GetBackendType() == LayersBackend::LAYERS_NONE) {
