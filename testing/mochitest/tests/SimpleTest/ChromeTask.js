60f35534a3f174f382211180ecb841651fba3eac
Tom Schuster <tschuster@mozilla.com>
2025-06-16 10:08:19 +0000

    Bug 1968479 - Use evalInSandbox instead of eval in ChromeTask.spawn. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D251362

--- testing/mochitest/tests/SimpleTest/ChromeTask.js
+++ testing/mochitest/tests/SimpleTest/ChromeTask.js
@@ -55,13 +55,19 @@ function ChromeTask_ChromeScript() {
     /* eslint-enable no-unused-vars */
 
     try {
-      let runnablestr = `
-        (() => {
-          return (${source});
-        })();`;
-
-      // eslint-disable-next-line no-eval
-      let runnable = eval(runnablestr);
+      let sb = Cu.Sandbox(Services.scriptSecurityManager.getSystemPrincipal(), {
+        sandboxPrototype: {
+          __proto__: globalThis,
+          Assert,
+          ok,
+          is,
+          isnot,
+          todo,
+          todo_is,
+          info,
+        },
+      });
+      let runnable = Cu.evalInSandbox(`(() => (${source}))()`, sb);
       let result = await runnable.call(this, aData.arg);
       sendAsyncMessage("chrome-task:complete", {
         id,
