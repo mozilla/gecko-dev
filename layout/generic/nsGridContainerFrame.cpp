2c80d5fc00a89ca9f7f693601f0e9b95539ee2d7
Ting-Yu Lin <aethanyc@gmail.com>
2025-06-16 21:12:46 +0000

    Bug 1971972 - Declare maybeType as a const-reference to fix a build error on gcc. r=layout-reviewers,emilio,AlaskanEmily
    Differential Revision: https://phabricator.services.mozilla.com/D253741

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -6345,7 +6345,7 @@ void nsGridContainerFrame::Tracks::ResolveIntrinsicSizeForNonSpanningItems(
   // Accumulate the required size types and compute the contributions.
   {
     EnumSet<GridIntrinsicSizeType> sizeTypesToCalculate;
-    for (const auto maybeType : {baseSizeType, limitType}) {
+    for (const auto& maybeType : {baseSizeType, limitType}) {
       if (maybeType) {
         sizeTypesToCalculate += *maybeType;
       }
