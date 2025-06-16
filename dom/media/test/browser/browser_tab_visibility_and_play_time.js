8bf7a6958ad0f08618f1754df517d54fea5727a0
alwu <alwu@mozilla.com>
2025-06-16 17:24:18 +0000

    Bug 1775978 - part1 : apply changes from ESLint. r=media-playback-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D253566

--- dom/media/test/browser/browser_tab_visibility_and_play_time.js
+++ dom/media/test/browser/browser_tab_visibility_and_play_time.js
@@ -5,16 +5,16 @@
  * because we can't check the snapshot histogram in the content process.
  * The actual probe checking happens in `test_accumulated_play_time.html`.
  */
-"use strict";
+'use strict';
 
 const PAGE_URL =
-  "https://example.com/browser/dom/media/test/browser/file_media.html";
+    'https://example.com/browser/dom/media/test/browser/file_media.html';
 
 // This HDR tests will only pass on platforms that accurately report color
 // depth in their VideoInfo structures. Presently, that is only true for
 // macOS.
 
-const reportsColorDepthFromVideoData = AppConstants.platform == "macosx";
+const reportsColorDepthFromVideoData = AppConstants.platform == 'macosx';
 
 add_task(async function testChangingTabVisibilityAffectsInvisiblePlayTime() {
   const originalTab = gBrowser.selectedTab;
@@ -52,13 +52,9 @@ async function openMediaTab(url) {
   await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
     content.waitForOnTimeUpdate = element => {
       return new Promise(resolve => {
-        element.addEventListener(
-          "timeupdate",
-          () => {
-            resolve();
-          },
-          { once: true }
-        );
+        element.addEventListener('timeupdate', () => {
+          resolve();
+        }, {once: true});
       });
     };
 
@@ -71,10 +67,9 @@ async function openMediaTab(url) {
     };
     content.assertValueEqualTo = (videoChrome, checkType, expectedValue) => {
       content.assertAttributeDefined(videoChrome, checkType);
-      is(
-        videoChrome[checkType],
-        expectedValue,
-        `${checkType} equals to ${expectedValue}`
+      is(videoChrome[checkType], expectedValue,
+         `${checkType} equals to ${expectedValue}`
+
       );
     };
     content.assertValueConstantlyIncreases = async (videoChrome, checkType) => {
@@ -82,20 +77,16 @@ async function openMediaTab(url) {
       const valueSnapshot = videoChrome[checkType];
       await content.waitForOnTimeUpdate(videoChrome);
       Assert.greater(
-        videoChrome[checkType],
-        valueSnapshot,
-        `${checkType} keeps increasing`
-      );
+          videoChrome[checkType], valueSnapshot,
+          `${checkType} keeps increasing`);
     };
     content.assertValueKeptUnchanged = async (videoChrome, checkType) => {
       content.assertAttributeDefined(videoChrome, checkType);
       const valueSnapshot = videoChrome[checkType];
       await content.sleep(1000);
       Assert.equal(
-        videoChrome[checkType],
-        valueSnapshot,
-        `${checkType} keeps unchanged`
-      );
+          videoChrome[checkType], valueSnapshot,
+          `${checkType} keeps unchanged`);
     };
   });
   return tab;
@@ -108,111 +99,85 @@ function startMedia({
   shouldAccumulateHDRTime,
 }) {
   return SpecialPowers.spawn(
-    mediaTab.linkedBrowser,
-    [
-      shouldAccumulateTime,
-      shouldAccumulateInvisibleTime,
-      shouldAccumulateHDRTime,
-    ],
-    async (accumulateTime, accumulateInvisibleTime, accumulateHDRTime) => {
-      const video = content.document.getElementById("video");
-      ok(
-        await video.play().then(
-          () => true,
-          () => false
-        ),
-        "video started playing"
-      );
-      const videoChrome = SpecialPowers.wrap(video);
-      if (accumulateTime) {
-        await content.assertValueConstantlyIncreases(
-          videoChrome,
-          "totalVideoPlayTime"
-        );
-      } else {
-        await content.assertValueKeptUnchanged(
-          videoChrome,
-          "totalVideoPlayTime"
-        );
-      }
-      if (accumulateInvisibleTime) {
-        await content.assertValueConstantlyIncreases(
-          videoChrome,
-          "invisiblePlayTime"
-        );
-      } else {
-        await content.assertValueKeptUnchanged(
-          videoChrome,
-          "invisiblePlayTime"
-        );
-      }
-
-      const videoHDR = content.document.getElementById("videoHDR");
-
-      // HDR test video might not decode on all platforms, so catch
-      // the play() command and exit early in such a case. Failure to
-      // decode might manifest as a timeout, so add a rejection race
-      // to catch that.
-      let didDecode = true;
-      const playPromise = videoHDR.play().then(
-        () => true,
-        () => false
-      );
-      /* eslint-disable mozilla/no-arbitrary-setTimeout */
-      const tooSlowPromise = new Promise(resolve =>
-        setTimeout(() => {
-          info("videoHDR timed out.");
-          didDecode = false;
-          resolve(false);
-        }, 1000)
-      );
-      /* eslint-enable mozilla/no-arbitrary-setTimeout */
-
-      let didPlay = await Promise.race([playPromise, tooSlowPromise]).catch(
-        err => {
-          info("videoHDR failed to decode with error: " + err.message);
-          didDecode = false;
-          return false;
+      mediaTab.linkedBrowser,
+      [
+        shouldAccumulateTime,
+        shouldAccumulateInvisibleTime,
+        shouldAccumulateHDRTime,
+      ],
+      async (accumulateTime, accumulateInvisibleTime, accumulateHDRTime) => {
+        const video = content.document.getElementById('video');
+        ok(await video.play().then(() => true, () => false),
+           'video started playing');
+        const videoChrome = SpecialPowers.wrap(video);
+        if (accumulateTime) {
+          await content.assertValueConstantlyIncreases(
+              videoChrome, 'totalVideoPlayTime');
+        } else {
+          await content.assertValueKeptUnchanged(
+              videoChrome, 'totalVideoPlayTime');
         }
-      );
+        if (accumulateInvisibleTime) {
+          await content.assertValueConstantlyIncreases(
+              videoChrome, 'invisiblePlayTime');
+        } else {
+          await content.assertValueKeptUnchanged(
+              videoChrome, 'invisiblePlayTime');
+        }
 
-      if (!didDecode) {
-        return;
-      }
+        const videoHDR = content.document.getElementById('videoHDR');
 
-      ok(didPlay, "videoHDR started playing");
-      const videoHDRChrome = SpecialPowers.wrap(videoHDR);
-      if (accumulateHDRTime) {
-        await content.assertValueConstantlyIncreases(
-          videoHDRChrome,
-          "totalVideoHDRPlayTime"
-        );
-      } else {
-        await content.assertValueKeptUnchanged(
-          videoHDRChrome,
-          "totalVideoHDRPlayTime"
-        );
-      }
-    }
-  );
+        // HDR test video might not decode on all platforms, so catch
+        // the play() command and exit early in such a case. Failure to
+        // decode might manifest as a timeout, so add a rejection race
+        // to catch that.
+        let didDecode = true;
+        const playPromise = videoHDR.play().then(() => true, () => false);
+        /* eslint-disable mozilla/no-arbitrary-setTimeout */
+        const tooSlowPromise = new Promise(resolve => setTimeout(() => {
+                                             info('videoHDR timed out.');
+                                             didDecode = false;
+                                             resolve(false);
+                                           }, 1000));
+        /* eslint-enable mozilla/no-arbitrary-setTimeout */
+
+        let didPlay =
+            await Promise.race([playPromise, tooSlowPromise]).catch(err => {
+              info('videoHDR failed to decode with error: ' + err.message);
+              didDecode = false;
+              return false;
+            });
+
+        if (!didDecode) {
+          return;
+        }
+
+        ok(didPlay, 'videoHDR started playing');
+        const videoHDRChrome = SpecialPowers.wrap(videoHDR);
+        if (accumulateHDRTime) {
+          await content.assertValueConstantlyIncreases(
+              videoHDRChrome, 'totalVideoHDRPlayTime');
+        } else {
+          await content.assertValueKeptUnchanged(
+              videoHDRChrome, 'totalVideoHDRPlayTime');
+        }
+      });
 }
 
 function pauseMedia(tab) {
   return SpecialPowers.spawn(tab.linkedBrowser, [], async _ => {
-    const video = content.document.getElementById("video");
+    const video = content.document.getElementById('video');
     video.pause();
-    ok(true, "video paused");
+    ok(true, 'video paused');
     const videoChrome = SpecialPowers.wrap(video);
-    await content.assertValueKeptUnchanged(videoChrome, "totalVideoPlayTime");
-    await content.assertValueKeptUnchanged(videoChrome, "invisiblePlayTime");
+    await content.assertValueKeptUnchanged(videoChrome, 'totalVideoPlayTime');
+    await content.assertValueKeptUnchanged(videoChrome, 'invisiblePlayTime');
 
-    const videoHDR = content.document.getElementById("videoHDR");
+    const videoHDR = content.document.getElementById('videoHDR');
     videoHDR.pause();
-    ok(true, "videoHDR paused");
+    ok(true, 'videoHDR paused');
     const videoHDRChrome = SpecialPowers.wrap(videoHDR);
     await content.assertValueKeptUnchanged(
-      videoHDRChrome,
-      "totalVideoHDRPlayTime"
-    );
+        videoHDRChrome, 'totalVideoHDRPlayTime');
   });
 }
