a62128735c3cd222f9d8a6359db587377406c26b
agoloman <agoloman@mozilla.com>
2025-06-17 16:18:32 +0300

    Revert "Bug 1930738 - Consider not lowering the request priority of images with the LOAD_BACKGROUND flag r=necko-reviewers,jesup" for causing wpt failures @fetchpriority-disabled.h2.html.
    This reverts commit 2108706110f392894b2f7c249217ba42d89c5529.

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -835,6 +835,11 @@ static void AdjustPriorityForImages(nsIChannel* aChannel,
       priority += FETCH_PRIORITY_ADJUSTMENT_FOR(images, aFetchPriority);
     }
 
+    // Further reduce priority for background loads
+    if (aLoadFlags & nsIRequest::LOAD_BACKGROUND) {
+      ++priority;
+    }
+
     supportsPriority->AdjustPriority(priority);
   }
 
