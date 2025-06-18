cd2b24c90a2791949aab28c97c09312ddbb4bce1
Anna Kulyk <akulyk@mozilla.com>
2025-06-18 21:28:10 +0000

    Bug 1970870 - Add a title to the opens in new tab icon in moz-box-link r=reusable-components-reviewers,accessibility-frontend-reviewers,fluent-reviewers,tgiles,bolsson,ayeddi
    Differential Revision: https://phabricator.services.mozilla.com/D252936

--- toolkit/content/widgets/moz-box-link/moz-box-link.mjs
+++ toolkit/content/widgets/moz-box-link/moz-box-link.mjs
@@ -5,6 +5,8 @@
 import { MozBoxBase } from "../lit-utils.mjs";
 import { html } from "../vendor/lit.all.mjs";
 
+window.MozXULElement?.insertFTLIfNeeded("toolkit/global/mozBoxBase.ftl");
+
 /**
  * A link with a box-like shape that allows for custom title and description.
  *
@@ -54,12 +56,18 @@ export default class MozBoxLink extends MozBoxBase {
             class="button"
             is="moz-support-link"
             support-page=${supportPage}
+            data-l10n-id="moz-box-link-anchor"
           >
             ${template}
           </a>`
-        : html`<a class="button" href=${this.href} target="_blank"
-            >${template}</a
-          >`}
+        : html`<a
+            class="button"
+            href=${this.href}
+            target="_blank"
+            data-l10n-id="moz-box-link-anchor"
+          >
+            ${template}
+          </a>`}
     `;
   }
 }
