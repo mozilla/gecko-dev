1f2de52f35aa67b517d38a34ec2d4a3ec298fa97
Julian Seward <jseward@acm.org>
2025-06-18 05:47:19 +0000

    Bug 1972566 - temporarily disable testcase wasm/unroll7.js on esr140.  r=jandem.
    Bug 1972357 pref'd off wasm loop unrolling for the 140 line.  That is harmless
    in itself, but unfortunately caused wasm/unroll7.js to fail since the purpose
    of that test is to check that unrolling is happening as expected.
    
    This patch just sets the test to be always-skipped, effectively disabling it.
    We can reenable it on central once 140 is out the door.
    
    Differential Revision: https://phabricator.services.mozilla.com/D254010

--- js/src/jit-test/tests/wasm/unroll7.js
+++ js/src/jit-test/tests/wasm/unroll7.js
@@ -1,4 +1,4 @@
-// |jit-test| skip-if: wasmCompileMode() != "ion"
+// |jit-test| skip-if: true
 
 // Tests that unrolling actually transforms a loop into something new, by
 // unrolling a loop with just one FP mul and one FP add in it and checking that
