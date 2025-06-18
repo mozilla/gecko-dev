e3f6c359d32b7252db1f8de6e639560f432b8355
Thomas Wisniewski <twisniewski@mozilla.com>
2025-06-18 16:18:23 +0000

    Bug 1909448 - remove our UA override for fire.honeywell.com; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D253850

--- testing/webcompat/interventions/tests/test_1909448_fire_honeywell_com.py
+++ testing/webcompat/interventions/tests/test_1909448_fire_honeywell_com.py
@@ -5,14 +5,7 @@ LANDING_PAGE_CSS = ".landing-page"
 
 
 @pytest.mark.asyncio
-@pytest.mark.with_interventions
-async def test_enabled(client):
-    await client.navigate(URL, wait="load")
-    assert client.await_css(LANDING_PAGE_CSS)
-
-
-@pytest.mark.asyncio
 @pytest.mark.without_interventions
-async def test_disabled(client):
+async def test_regression(client):
     await client.navigate(URL, wait="load")
-    assert not client.find_css(LANDING_PAGE_CSS)
+    assert client.find_css(LANDING_PAGE_CSS)
