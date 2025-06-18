846c8bcf78e18befc7e2468063f57982df0d01b3
Justin Link <jlink@mozilla.com>
2025-06-18 15:34:10 +0000

    Bug 1972151: Re-arrange some math a little bit to avoid some unnecessary precision loss in floating point calculations. r=sefeng
    Differential Revision: https://phabricator.services.mozilla.com/D253736

--- dom/performance/LargestContentfulPaint.cpp
+++ dom/performance/LargestContentfulPaint.cpp
@@ -428,14 +428,10 @@ void LargestContentfulPaint::UpdateSize(
                                  AppUnitsPerCSSPixel());
     LOG("  boundingClientArea = %f", boundingClientArea);
 
-    // Let scaleFactor be boundingClientArea / naturalArea.
-    double scaleFactor = boundingClientArea / naturalArea;
-    LOG("  scaleFactor = %f", scaleFactor);
-
-    // If scaleFactor is greater than 1, then divide area by scaleFactor.
-    if (scaleFactor > 1) {
-      LOG("  area before sacled doown %f", area);
-      area = area / scaleFactor;
+    // If the scale factor is greater than 1, then adjust area.
+    if (boundingClientArea > naturalArea) {
+      LOG("  area before scaled down %f", area);
+      area *= (naturalArea / boundingClientArea);
     }
   }
 
