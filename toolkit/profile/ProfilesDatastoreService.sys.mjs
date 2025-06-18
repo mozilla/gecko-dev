c1a8ab66897350e0a0feef82976b7dc4c604d829
Daniel Thorn <dthorn@mozilla.com>
2025-06-18 18:23:01 +0000

    Bug 1964496 - Migrate normandy heartbeat storage to use ProfileDatastoreService r=nimbus-reviewers,profiles-reviewers,beth,jhirsch
    Differential Revision: https://phabricator.services.mozilla.com/D250103

--- toolkit/profile/ProfilesDatastoreService.sys.mjs
+++ toolkit/profile/ProfilesDatastoreService.sys.mjs
@@ -61,7 +61,7 @@ class ProfilesDatastoreServiceClass {
   async createTables() {
     // TODO: (Bug 1902320) Handle exceptions on connection opening
     let currentVersion = await this.#connection.getSchemaVersion();
-    if (currentVersion == 4) {
+    if (currentVersion == 5) {
       return;
     }
 
@@ -135,6 +135,23 @@ class ProfilesDatastoreServiceClass {
         await this.#connection.execute("DELETE FROM NimbusEnrollments;");
       });
       await this.#connection.setSchemaVersion(4);
+    }
+
+    if (currentVersion < 5) {
+      await this.#connection.executeTransaction(async () => {
+        const createHeartbeatTable = `
+            CREATE TABLE IF NOT EXISTS "Heartbeats" (
+              id              INTEGER NOT NULL,
+              recipeId        TEXT NOT NULL UNIQUE,
+              lastShown       INTEGER,
+              lastInteraction	INTEGER,
+              PRIMARY KEY(id)
+            );`;
+
+        await this.#connection.execute(createHeartbeatTable);
+      });
+
+      await this.#connection.setSchemaVersion(5);
     }
   }
 
