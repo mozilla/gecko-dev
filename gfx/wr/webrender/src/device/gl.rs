7796572aa37b565cf8fe25e5cc47c8586761c766
Jamie Nicol <jnicol@mozilla.com>
2025-06-19 18:33:01 +0000

    Bug 1971954 - Stop blocking GL_KHR_debug on Samsung Xclipse GPUs. r=gfx-reviewers,lsalzman
    We previously blocked this extension on Samsung Xclipse GPUs in bug
    1928322. The previous patch in this series now works around this bug,
    meaning we are able to remove the block.
    
    Differential Revision: https://phabricator.services.mozilla.com/D253631

--- gfx/wr/webrender/src/device/gl.rs
+++ gfx/wr/webrender/src/device/gl.rs
@@ -1478,19 +1478,14 @@ impl Device {
             extensions.push(gl.get_string_i(gl::EXTENSIONS, i));
         }
 
-        let is_xclipse = renderer_name.starts_with("ANGLE (Samsung Xclipse");
+        // We block this on Mali Valhall GPUs as the extension's functions always return
+        // GL_OUT_OF_MEMORY, causing us to panic in debug builds.
+        let supports_khr_debug = supports_extension(&extensions, "GL_KHR_debug")
+            && !is_mali_valhall(&renderer_name);
 
         // On debug builds, assert that each GL call is error-free. We don't do
         // this on release builds because the synchronous call can stall the
         // pipeline.
-        // We block this on Mali Valhall GPUs as the extension's functions always return
-        // GL_OUT_OF_MEMORY, causing us to panic in debug builds.
-        // Blocked on Xclipse GPUs as glGetDebugMessageLog returns an incorrect count,
-        // leading to an out-of-bounds index in gleam.
-        let supports_khr_debug =
-            supports_extension(&extensions, "GL_KHR_debug")
-            && !is_mali_valhall(&renderer_name)
-            && !is_xclipse;
         if panic_on_gl_error || cfg!(debug_assertions) {
             gl = gl::ErrorReactingGl::wrap(gl, move |gl, name, code| {
                 if supports_khr_debug {
@@ -2658,7 +2653,7 @@ impl Device {
                 desc.external,
                 desc.pixel_type,
                 None,
-            );            
+            );
         }
 
         // Set up FBOs, if required.
