8a744eaf3861bd3429bceac97fce091b835f65a5
Alex Hochheiden <ahochheiden@mozilla.com>
2025-06-19 22:23:21 +0000

    Bug 1972681 - Bump vendored `cbor2` to version `5.6.5` r=ahal,mach-reviewers
    Updated cbor2 v4.1.2 -> v5.6.5
    
    Differential Revision: https://phabricator.services.mozilla.com/D254120

--- third_party/python/cbor2/cbor2/types.py
+++ third_party/python/cbor2/cbor2/types.py
@@ -1,87 +1,15 @@
-from collections import Mapping
+from warnings import warn
 
+from ._types import CBORDecodeEOF as CBORDecodeEOF
+from ._types import CBORDecodeError as CBORDecodeError
+from ._types import CBORDecodeValueError as CBORDecodeValueError
+from ._types import CBOREncodeError as CBOREncodeError
+from ._types import CBOREncodeTypeError as CBOREncodeTypeError
+from ._types import CBOREncodeValueError as CBOREncodeValueError
+from ._types import CBORError as CBORError
+from ._types import CBORSimpleValue as CBORSimpleValue
+from ._types import CBORTag as CBORTag
+from ._types import FrozenDict as FrozenDict
+from ._types import undefined as undefined
 
-class CBORTag(object):
-    """
-    Represents a CBOR semantic tag.
-
-    :param int tag: tag number
-    :param value: encapsulated value (any object)
-    """
-
-    __slots__ = 'tag', 'value'
-
-    def __init__(self, tag, value):
-        self.tag = tag
-        self.value = value
-
-    def __eq__(self, other):
-        if isinstance(other, CBORTag):
-            return self.tag == other.tag and self.value == other.value
-        return NotImplemented
-
-    def __repr__(self):
-        return 'CBORTag({self.tag}, {self.value!r})'.format(self=self)
-
-
-class CBORSimpleValue(object):
-    """
-    Represents a CBOR "simple value".
-
-    :param int value: the value (0-255)
-    """
-
-    __slots__ = 'value'
-
-    def __init__(self, value):
-        if value < 0 or value > 255:
-            raise TypeError('simple value too big')
-        self.value = value
-
-    def __eq__(self, other):
-        if isinstance(other, CBORSimpleValue):
-            return self.value == other.value
-        elif isinstance(other, int):
-            return self.value == other
-        return NotImplemented
-
-    def __repr__(self):
-        return 'CBORSimpleValue({self.value})'.format(self=self)
-
-
-class FrozenDict(Mapping):
-    """
-    A hashable, immutable mapping type.
-
-    The arguments to ``FrozenDict`` are processed just like those to ``dict``.
-    """
-
-    def __init__(self, *args, **kwargs):
-        self._d = dict(*args, **kwargs)
-        self._hash = None
-
-    def __iter__(self):
-        return iter(self._d)
-
-    def __len__(self):
-        return len(self._d)
-
-    def __getitem__(self, key):
-        return self._d[key]
-
-    def __repr__(self):
-        return "%s(%s)" % (self.__class__.__name__, self._d)
-
-    def __hash__(self):
-        if self._hash is None:
-            self._hash = hash((frozenset(self), frozenset(self.values())))
-        return self._hash
-
-
-class UndefinedType(object):
-    __slots__ = ()
-
-
-#: Represents the "undefined" value.
-undefined = UndefinedType()
-break_marker = object()
+warn("The cbor2.types module has been deprecated. Instead import everything directly from cbor2.")
