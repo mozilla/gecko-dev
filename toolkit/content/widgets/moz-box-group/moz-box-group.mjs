b3c675f0813f5838f4ebd9711a031917458c6b20
hannajones <hjones@mozilla.com>
2025-06-23 15:33:17 +0000

    Bug 1956853 - Add support for type="list" to moz-box-group r=akulyk,reusable-components-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D251409

--- toolkit/content/widgets/moz-box-group/moz-box-group.mjs
+++ toolkit/content/widgets/moz-box-group/moz-box-group.mjs
@@ -10,17 +10,59 @@ import { MozLitElement } from "../lit-utils.mjs";
  * moz-box-button elements and provide the expected styles.
  *
  * @tagname moz-box-group
- * * @slot default - Slot for rendering various moz-box-* elements.
+ * @property {string} type - The type of the group, either "list" or undefined.
+ * @slot default - Slot for rendering various moz-box-* elements.
+ * @slot <index> - Slots used to assign moz-box-* elements to <li> elements when
+ *   the group is type="list".
  */
 export default class MozBoxGroup extends MozLitElement {
+  static properties = {
+    type: { type: String },
+    listItems: { type: Array, state: true },
+  };
+
+  constructor() {
+    super();
+    this.listItems = [];
+  }
+
+  slotTemplate() {
+    if (this.type == "list") {
+      return html`<ul class="list">
+          ${this.listItems.map((_, i) => {
+            return html`<li>
+              <slot name=${i}></slot>
+            </li> `;
+          })}
+        </ul>
+        <slot hidden @slotchange=${this.handleSlotchange}></slot>`;
+    }
+    return html`<slot></slot>`;
+  }
+
+  handleSlotchange() {
+    let boxElements = this.querySelectorAll(
+      "moz-box-item, moz-box-button, moz-box-link"
+    );
+    this.listItems = Array.from(boxElements);
+  }
+
   render() {
     return html`
       <link
         rel="stylesheet"
         href="chrome://global/content/elements/moz-box-group.css"
       />
-      <slot></slot>
+      ${this.slotTemplate()}
     `;
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has("listItems") && this.listItems.length) {
+      this.listItems.forEach((item, i) => {
+        item.slot = i;
+      });
+    }
   }
 }
 customElements.define("moz-box-group", MozBoxGroup);
--- toolkit/content/widgets/moz-box-group/moz-box-group.stories.mjs
+++ toolkit/content/widgets/moz-box-group/moz-box-group.stories.mjs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { html } from "../vendor/lit.all.mjs";
+import { html, ifDefined } from "../vendor/lit.all.mjs";
 import "./moz-box-group.mjs";
 
 export default {
@@ -24,16 +24,47 @@ moz-box-link =
   },
 };
 
-const Template = () => html`
-  <moz-box-group>
-    <moz-box-item data-l10n-id="moz-box-item"></moz-box-item>
+const Template = ({ type }) => html`
+  <style>
+    .delete {
+      margin-top: var(--space-medium);
+    }
+  </style>
+  <moz-box-group type=${ifDefined(type)}>
+    <moz-box-item data-l10n-id="moz-box-item">
+      <moz-toggle slot="actions"></moz-toggle>
+    </moz-box-item>
     <moz-box-link data-l10n-id="moz-box-link"></moz-box-link>
     <moz-box-button data-l10n-id="moz-box-button-1"></moz-box-button>
     <moz-box-button data-l10n-id="moz-box-button-2"></moz-box-button>
   </moz-box-group>
+  ${type == "list"
+    ? html`<moz-button class="delete" @click=${appendItem}>
+        Add an item
+      </moz-button>`
+    : ""}
 `;
 
+const appendItem = event => {
+  let group = event.target.getRootNode().querySelector("moz-box-group");
+
+  let boxItem = document.createElement("moz-box-item");
+  boxItem.label = "New box item";
+  boxItem.description = "New items are added to the list";
+
+  let actionButton = document.createElement("moz-button");
+  actionButton.addEventListener("click", () => boxItem.remove());
+  actionButton.iconSrc = "chrome://global/skin/icons/delete.svg";
+  actionButton.slot = "actions";
+  boxItem.append(actionButton);
+
+  group.prepend(boxItem);
+};
+
 export const Default = Template.bind({});
-Default.args = {
-  variant: "default",
+Default.args = {};
+
+export const List = Template.bind({});
+List.args = {
+  type: "list",
 };
