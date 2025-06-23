b12c924cfbe7b8e331df868db4d7eb14c6808400
Greg Stoll <gstoll@mozilla.com>
2025-06-23 14:15:36 +0000

    Bug 1963385 part 7 - make tests that terminate the DLP agent wait until the process has terminated r=dlp-reviewers,handyman
    This gets rid of some intermittency now that more gtests are terminating
    and restarting the agent.
    
    Differential Revision: https://phabricator.services.mozilla.com/D252576

--- toolkit/components/contentanalysis/tests/gtest/TestContentAnalysisUtils.cpp
+++ toolkit/components/contentanalysis/tests/gtest/TestContentAnalysisUtils.cpp
@@ -10,6 +10,8 @@
 #include <rpc.h>
 #include <windows.h>
 
+using namespace mozilla;
+
 MozAgentInfo LaunchAgentNormal(const wchar_t* aToBlock,
                                const wchar_t* aToWarn /* = L"warn" */) {
   nsString pipeName;
@@ -97,6 +99,138 @@ void LaunchAgentWithCommandLineArguments(const nsString& cmdLineArguments,
 
   agentInfo.processInfo = processInfo;
   agentInfo.client = std::move(clientPtr);
+}
+
+void SendRequestAndExpectResponse(
+    RefPtr<contentanalysis::ContentAnalysis> contentAnalysis,
+    const nsCOMPtr<nsIContentAnalysisRequest>& request,
+    Maybe<bool> expectedShouldAllow,
+    Maybe<nsIContentAnalysisResponse::Action> expectedAction,
+    Maybe<bool> expectedIsCached) {
+  SendRequestAndExpectResponseInternal(contentAnalysis, request,
+                                       expectedShouldAllow, expectedAction,
+                                       expectedIsCached, false);
+}
+
+void SendRequestAndExpectResponseInternal(
+    RefPtr<contentanalysis::ContentAnalysis> contentAnalysis,
+    const nsCOMPtr<nsIContentAnalysisRequest>& request,
+    Maybe<bool> expectedShouldAllow,
+    Maybe<nsIContentAnalysisResponse::Action> expectedAction,
+    Maybe<bool> expectedIsCached, bool aIsEarlyResponse) {
+  if (aIsEarlyResponse) {
+    EXPECT_FALSE(expectedAction.isSome())
+        << "Early responses do not have an action";
+    EXPECT_FALSE(expectedIsCached.isSome())
+        << "Early responses do not have an isCached value";
+  }
+
+  bool gotResponse = false;
+  bool gotAcknowledgement = false;
+  nsCString requestToken;
+  MOZ_ALWAYS_SUCCEEDS(request->GetRequestToken(requestToken));
+  if (requestToken.IsEmpty()) {
+    MOZ_ALWAYS_SUCCEEDS(request->SetRequestToken(GenerateUUID()));
+  }
+
+  // Make timedOut a RefPtr so if we get a response from content analysis
+  // after this function has finished we can safely check that (and don't
+  // start accessing stack values that don't exist anymore)
+  RefPtr timedOut = MakeRefPtr<media::Refcountable<BoolStruct>>();
+  auto callback = MakeRefPtr<contentanalysis::ContentAnalysisCallback>(
+      [&, timedOut](nsIContentAnalysisResult* result) {
+        EXPECT_TRUE(NS_IsMainThread());
+        if (timedOut->mValue) {
+          return;
+        }
+        if (expectedShouldAllow.isSome()) {
+          EXPECT_EQ(*expectedShouldAllow, result->GetShouldAllowContent());
+        }
+        if (aIsEarlyResponse) {
+          // We will not get an acknowledgement for early responses,
+          // so just set gotAcknowledgement to true so we don't wait for it.
+          gotAcknowledgement = true;
+        } else {
+          nsCOMPtr<nsIContentAnalysisResponse> response =
+              do_QueryInterface(result);
+          EXPECT_TRUE(response);
+          if (expectedAction.isSome()) {
+            EXPECT_EQ(*expectedAction, response->GetAction());
+          }
+          if (expectedIsCached.isSome()) {
+            bool isCached;
+            MOZ_ALWAYS_SUCCEEDS(response->GetIsCachedResponse(&isCached));
+            EXPECT_EQ(*expectedIsCached, isCached);
+          }
+          nsCString requestToken, originalRequestToken;
+          MOZ_ALWAYS_SUCCEEDS(response->GetRequestToken(requestToken));
+          MOZ_ALWAYS_SUCCEEDS(request->GetRequestToken(originalRequestToken));
+          EXPECT_EQ(originalRequestToken, requestToken);
+          nsCString userActionId, originalUserActionId;
+          MOZ_ALWAYS_SUCCEEDS(response->GetUserActionId(userActionId));
+          MOZ_ALWAYS_SUCCEEDS(request->GetUserActionId(originalUserActionId));
+          EXPECT_EQ(originalUserActionId, userActionId);
+        }
+        gotResponse = true;
+      },
+      [&gotResponse, &gotAcknowledgement, timedOut](nsresult error) {
+        EXPECT_TRUE(NS_IsMainThread());
+        if (timedOut->mValue) {
+          return;
+        }
+        const char* errorName = mozilla::GetStaticErrorName(error);
+        errorName = errorName ? errorName : "";
+        printf("Got error response code %s(%x)\n", errorName, error);
+        // Errors should not have errorCode NS_OK
+        EXPECT_NE(NS_OK, error);
+        gotResponse = true;
+        // An acknowledgement won't be sent, so don't wait for one
+        gotAcknowledgement = true;
+        FAIL() << "Got error response";
+      });
+
+  auto rawAcknowledgementObserver = MakeRefPtr<RawAcknowledgementObserver>();
+  nsCOMPtr<nsIObserverService> obsServ =
+      mozilla::services::GetObserverService();
+  if (!aIsEarlyResponse) {
+    MOZ_ALWAYS_SUCCEEDS(obsServ->AddObserver(
+        rawAcknowledgementObserver, "dlp-acknowledgement-sent-raw", false));
+  }
+
+  AutoTArray<RefPtr<nsIContentAnalysisRequest>, 1> requests{request.get()};
+  MOZ_ALWAYS_SUCCEEDS(contentAnalysis->AnalyzeContentRequestsCallback(
+      requests, true, callback));
+  RefPtr<CancelableRunnable> timer = QueueTimeoutToMainThread(timedOut);
+
+  mozilla::SpinEventLoopUntil(
+      "Waiting for ContentAnalysis result"_ns, [&, timedOut]() {
+        if (timedOut->mValue) {
+          return true;
+        }
+
+        auto acknowledgements =
+            rawAcknowledgementObserver->GetAcknowledgements();
+        nsCString requestToken;
+        MOZ_ALWAYS_SUCCEEDS(request->GetRequestToken(requestToken));
+        for (const auto& acknowledgement : acknowledgements) {
+          if (nsCString(acknowledgement.request_token()) == requestToken) {
+            // Wait for the acknowledgement to happen to avoid background
+            // activity that might interfere with other tests.
+            gotAcknowledgement = true;
+            break;
+          }
+        }
+
+        return gotResponse && gotAcknowledgement;
+      });
+  timer->Cancel();
+  EXPECT_TRUE(gotResponse);
+  EXPECT_TRUE(gotAcknowledgement);
+  EXPECT_FALSE(timedOut->mValue);
+  if (!aIsEarlyResponse) {
+    obsServ->RemoveObserver(rawAcknowledgementObserver,
+                            "dlp-acknowledgement-sent-raw");
+  }
 }
 
 NS_IMPL_ISUPPORTS(RawAcknowledgementObserver, nsIObserver);
