7b63b88bfd5550b9e4405123f422074b342d0a1c
Nan Jiang <njiang028@gmail.com>
2025-06-24 21:38:21 +0000

    Bug 1973819 - Ensure interest url data is populated for classification r=bdk a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D254880

--- toolkit/components/contentrelevancy/ContentRelevancyManager.sys.mjs
+++ toolkit/components/contentrelevancy/ContentRelevancyManager.sys.mjs
@@ -376,6 +376,7 @@ class RelevancyManager {
       ) ??
       false
     ) {
+      await this.#storeManager.store.ensureInterestDataPopulated();
       interestVector = await this.#storeManager.store.ingest(urls);
     }
 
