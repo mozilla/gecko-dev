6200a5551473cb00adc3538fb9c2931c3610fddf
Thomas Wisniewski <twisniewski@mozilla.com>
2025-06-24 16:05:54 +0000

    Bug 1972511 - add a UA override for cmyportal.tkc.co.jp's authentication pages; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D254313

---
+++ testing/webcompat/interventions/tests/test_1972511_cmyportal_tkc_co_jp.py
@@ -0,0 +1,21 @@
+import pytest
+
+URL = "https://cmyportal.tkc.co.jp/auth/7vvepsyabl/tfa"
+SUPPORTED_CSS = "#user_user_id"
+UNSUPPORTED_CSS = "a[href*=chromesetup]"
+
+
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(SUPPORTED_CSS, is_displayed=True)
+    assert not client.find_css(UNSUPPORTED_CSS, is_displayed=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(UNSUPPORTED_CSS, is_displayed=True)
+    assert not client.find_css(SUPPORTED_CSS, is_displayed=True)
