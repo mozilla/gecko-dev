29680489c7da80912acc355c8d0d4ee3205d2169
Mugurell <Mugurell@users.noreply.github.com>
2025-06-25 13:47:50 +0000

    Bug 1953740 - part 6 - Cleanup - Add a prebuilt empty SearchFragmentState to use in tests r=android-reviewers,pollymce
    Tests in later commits would need to initialize SearchFragmentStore which needs
    a default SearchFragmentState.
    To avoid having to initialize the initial state with 30+ properties a new prebuilt
    empty SearchFragmentState is now available to use.
    
    Differential Revision: https://phabricator.services.mozilla.com/D252160

---
+++ mobile/android/fenix/app/src/test/java/org/mozilla/fenix/search/fixtures/SearchFragmentState.kt
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.search.fixtures
+
+import org.mozilla.fenix.components.metrics.MetricsUtils
+import org.mozilla.fenix.search.SearchEngineSource
+import org.mozilla.fenix.search.SearchFragmentState
+
+/**
+ * Prebuilt empty [SearchFragmentState].
+ */
+val EMPTY_SEARCH_FRAGMENT_STATE = SearchFragmentState(
+    query = "",
+    url = "",
+    searchTerms = "",
+    searchEngineSource = SearchEngineSource.None,
+    defaultEngine = null,
+    showSearchSuggestions = false,
+    showSearchSuggestionsHint = false,
+    showSearchShortcuts = false,
+    areShortcutsAvailable = false,
+    showSearchShortcutsSetting = false,
+    showClipboardSuggestions = false,
+    showSearchTermHistory = false,
+    showHistorySuggestionsForCurrentEngine = false,
+    showAllHistorySuggestions = false,
+    showBookmarksSuggestionsForCurrentEngine = false,
+    showAllBookmarkSuggestions = false,
+    showSyncedTabsSuggestionsForCurrentEngine = false,
+    showAllSyncedTabsSuggestions = false,
+    showSessionSuggestionsForCurrentEngine = false,
+    showAllSessionSuggestions = false,
+    showSponsoredSuggestions = false,
+    showNonSponsoredSuggestions = false,
+    showTrendingSearches = false,
+    showRecentSearches = false,
+    showShortcutsSuggestions = false,
+    showQrButton = false,
+    tabId = null,
+    pastedText = null,
+    searchAccessPoint = MetricsUtils.Source.NONE,
+    clipboardHasUrl = false,
+)
