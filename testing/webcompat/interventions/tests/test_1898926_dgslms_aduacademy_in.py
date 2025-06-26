11708b59d9348ab8dd672e2982181a01d877d07d
Thomas Wisniewski <twisniewski@mozilla.com>
2025-06-26 20:19:09 +0000

    Bug 1898926 - add an Android-only UA override for dgslms.aduacademy.in; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D255055

---
+++ testing/webcompat/interventions/tests/test_1898926_dgslms_aduacademy_in.py
@@ -0,0 +1,24 @@
+import pytest
+
+URL = "https://dgslms.aduacademy.in/"
+
+SUPPORTED_CSS = "#txtLoginId"
+UNSUPPORTED_TEXT = "You are using an unsupported browser"
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(SUPPORTED_CSS, is_displayed=True)
+    assert not client.find_text(UNSUPPORTED_TEXT, is_displayed=True)
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_text(UNSUPPORTED_TEXT, is_displayed=True)
+    assert not client.find_css(SUPPORTED_CSS, is_displayed=True)
