81ab14c0c70115c49789f955f542fc70081e969b
Thomas Wisniewski <twisniewski@mozilla.com>
2025-06-26 20:19:07 +0000

    Bug 1899932 - add a UA override for ct-tc.apps.cic.gc.ca; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D255045

---
+++ testing/webcompat/interventions/tests/test_1899932_ct-tc_apps_cic_gc_ca.py
@@ -0,0 +1,22 @@
+import pytest
+
+URL = "https://ct-tc.apps.cic.gc.ca/?lang=en"
+
+SUPPORTED_CSS = "#login-application-input"
+UNSUPPORTED_TEXT = "Sorry, we don’t support this browser"
+
+
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(SUPPORTED_CSS, is_displayed=True)
+    assert not client.find_text(UNSUPPORTED_TEXT, is_displayed=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_text(UNSUPPORTED_TEXT, is_displayed=True)
+    assert not client.find_css(SUPPORTED_CSS, is_displayed=True)
