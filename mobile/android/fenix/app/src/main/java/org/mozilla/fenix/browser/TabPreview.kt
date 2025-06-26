8af5696ea82362f6c3aa9a18b35d5ce994780ec9
Mugurell <Mugurell@users.noreply.github.com>
2025-06-26 07:41:34 +0000

    Bug 1971166 - part 2 - Highlight the domain of the current displayed URL in tab previews also r=android-reviewers,pollymce
    This will ensure the display URL shown in tab previews will exactly match
    the display URL of the tab the user swipes to.
    
    Differential Revision: https://phabricator.services.mozilla.com/D254940

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/TabPreview.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/TabPreview.kt
@@ -18,6 +18,7 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.doOnNextLayout
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
+import kotlinx.coroutines.launch
 import mozilla.components.browser.state.selector.getNormalOrPrivateTabs
 import mozilla.components.browser.state.selector.selectedTab
 import mozilla.components.browser.state.state.TabSessionState
@@ -35,10 +36,12 @@ import mozilla.components.compose.browser.toolbar.store.BrowserToolbarState
 import mozilla.components.compose.browser.toolbar.store.BrowserToolbarStore
 import mozilla.components.compose.browser.toolbar.store.DisplayState
 import mozilla.components.concept.base.images.ImageLoadRequest
+import mozilla.components.support.ktx.android.view.toScope
 import mozilla.components.support.ktx.kotlin.isContentUrl
 import mozilla.components.support.ktx.util.URLStringUtils
 import org.mozilla.fenix.R
 import org.mozilla.fenix.components.toolbar.ToolbarPosition
+import org.mozilla.fenix.components.toolbar.URLDomainHighlight.getRegistrableDomainOrHostIndexRange
 import org.mozilla.fenix.databinding.TabPreviewBinding
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.settings
@@ -109,16 +112,18 @@ class TabPreview @JvmOverloads constructor(
 
             updateToolbar(
                 new = {
-                    browserToolbarStore.dispatch(
-                        PageOriginUpdated(
-                            buildComposableToolbarPageOrigin(destination),
-                        ),
-                    )
-                    browserToolbarStore.dispatch(
-                        BrowserDisplayToolbarAction.PageActionsStartUpdated(
-                            buildComposableToolbarPageStartActions(destination),
-                        ),
-                    )
+                    toScope().launch {
+                        browserToolbarStore.dispatch(
+                            PageOriginUpdated(
+                                buildComposableToolbarPageOrigin(destination),
+                            ),
+                        )
+                        browserToolbarStore.dispatch(
+                            BrowserDisplayToolbarAction.PageActionsStartUpdated(
+                                buildComposableToolbarPageStartActions(destination),
+                            ),
+                        )
+                    }
                 },
                 old = {},
             )
@@ -241,13 +246,17 @@ class TabPreview @JvmOverloads constructor(
         }
     }
 
-    private fun buildComposableToolbarPageOrigin(tab: TabSessionState): PageOrigin {
-        val urlString = URLStringUtils.toDisplayUrl(tab.content.url).toString()
+    private suspend fun buildComposableToolbarPageOrigin(tab: TabSessionState): PageOrigin {
+        val displayUrl = URLStringUtils.toDisplayUrl(tab.content.url).toString()
+        val registrableDomainIndexRange = getRegistrableDomainOrHostIndexRange(
+            tab.content.url, displayUrl, context.components.publicSuffixList,
+        )
 
         return PageOrigin(
             hint = R.string.search_hint,
             title = null,
-            url = urlString,
+            url = displayUrl,
+            registrableDomainIndexRange = registrableDomainIndexRange,
             onClick = object : BrowserToolbarEvent {},
         )
     }
