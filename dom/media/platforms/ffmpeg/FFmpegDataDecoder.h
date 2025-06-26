a294e696bf98289f6aabe6b5b35385d29e94278e
Andrew Osmond <aosmond@gmail.com>
2025-06-26 02:14:42 +0000

    Bug 1972902 - Part 4. Align ffmpeg decoder selection logic with encoder. r=media-playback-reviewers,alwu
    Differential Revision: https://phabricator.services.mozilla.com/D254298

--- dom/media/platforms/ffmpeg/FFmpegDataDecoder.h
+++ dom/media/platforms/ffmpeg/FFmpegDataDecoder.h
@@ -40,9 +40,11 @@ class FFmpegDataDecoder<LIBAV_VER>
   RefPtr<FlushPromise> Flush() override;
   RefPtr<ShutdownPromise> Shutdown() override;
 
-  static AVCodec* FindAVCodec(FFmpegLibWrapper* aLib, AVCodecID aCodec);
+  static AVCodec* FindSoftwareAVCodec(FFmpegLibWrapper* aLib, AVCodecID aCodec);
 #ifdef MOZ_USE_HWDECODE
-  static AVCodec* FindHardwareAVCodec(FFmpegLibWrapper* aLib, AVCodecID aCodec);
+  static AVCodec* FindHardwareAVCodec(
+      FFmpegLibWrapper* aLib, AVCodecID aCodec,
+      AVHWDeviceType aDeviceType = AV_HWDEVICE_TYPE_NONE);
 #endif
 
  protected:
