68eb2a14891e1c7e346f56b797dbf08291c59013
Thomas Wisniewski <twisniewski@mozilla.com>
2025-06-26 20:19:07 +0000

    Bug 1898943 - add a desktop-only UA override for carelink.minimed.eu; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D255047

---
+++ testing/webcompat/interventions/tests/test_1898943_carelink_minimed_eu.py
@@ -0,0 +1,24 @@
+import pytest
+
+URL = "https://carelink.minimed.eu/"
+
+SUPPORTED_CSS = "#landing-login-button-id"
+UNSUPPORTED_TEXT = "Your environment does not match the supported settings"
+
+
+@pytest.mark.skip_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(SUPPORTED_CSS, is_displayed=True)
+    assert not client.find_text(UNSUPPORTED_TEXT, is_displayed=True)
+
+
+@pytest.mark.skip_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_text(UNSUPPORTED_TEXT, is_displayed=True)
+    assert not client.find_css(SUPPORTED_CSS, is_displayed=True)
