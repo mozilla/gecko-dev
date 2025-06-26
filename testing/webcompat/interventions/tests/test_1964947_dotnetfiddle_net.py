0676f41cf981125320cc231154899d70355cc60b
Thomas Wisniewski <twisniewski@mozilla.com>
2025-06-26 20:19:08 +0000

    Bug 1964947 - add a UA override for dotnetfiddle.net; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D255054

---
+++ testing/webcompat/interventions/tests/test_1964947_dotnetfiddle_net.py
@@ -0,0 +1,24 @@
+import pytest
+
+URL = "https://dotnetfiddle.net/"
+
+MOBILE_CSS = "html.ui-mobile"
+DESKTOP_CSS = "#top-navbar"
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.with_interventions
+async def test_enabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(MOBILE_CSS, is_displayed=True)
+    assert not client.find_css(DESKTOP_CSS, is_displayed=True)
+
+
+@pytest.mark.only_platforms("android")
+@pytest.mark.asyncio
+@pytest.mark.without_interventions
+async def test_disabled(client):
+    await client.navigate(URL, wait="none")
+    assert client.await_css(DESKTOP_CSS, is_displayed=True)
+    assert not client.find_css(MOBILE_CSS, is_displayed=True)
