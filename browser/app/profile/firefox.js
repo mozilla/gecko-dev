6ec9186296c44487fb306c5a4905b511d1a20fe1
Beth Rennie <beth@brennie.ca>
2025-06-26 15:01:49 +0000

    Bug 1973779 - Use the NimbusEnrollments table as source of truth for enrollments on Nightly  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D254856
    
    Differential Revision: https://phabricator.services.mozilla.com/D255139

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2039,7 +2039,7 @@ pref("nimbus.profilesdatastoreservice.enabled", true);
 
 // Should Nimbus read from the shared ProfilesDatastoreService?
 // TODO(bug 1972426): Enable this behaviour by default and remove this pref.
-#if defined(NIGHTLY_BUIILD)
+#if defined(NIGHTLY_BUILD)
 pref("nimbus.profilesdatastoreservice.read.enabled", true);
 #else
 pref("nimbus.profilesdatastoreservice.read.enabled", false);
