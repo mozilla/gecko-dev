148ba12b2d2a0fcb3f49df5b74adce4602347d02
Denis Palmeiro <dpalmeiro@mozilla.com>
2025-06-27 00:12:33 +0000

    Bug 1967780: Add checks for public IP ranges, roots from the builtin list, and wildcard names. r=jesup,mt
    Differential Revision: https://phabricator.services.mozilla.com/D253054

--- tools/performance/PageloadEvent.cpp
+++ tools/performance/PageloadEvent.cpp
@@ -4,21 +4,34 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/Components.h"
 #include "mozilla/Maybe.h"
 #include "mozilla/PageloadEvent.h"
 #include "mozilla/RandomNum.h"
+#include "mozilla/ScopeExit.h"
 #include "mozilla/glean/DomMetrics.h"
+
+#include "nsIChannel.h"
+#include "nsIEffectiveTLDService.h"
+#include "nsITransportSecurityInfo.h"
+#include "nsIURI.h"
+#include "nsIX509Cert.h"
+
+#include "ScopedNSSTypes.h"
+#include "cert.h"
+#include "portreg.h"
 
 namespace mozilla::performance::pageload_event {
 
 // We don't want to record an event for every page load, so instead we
 // randomly sample the events based on the channel.
 //
-// For nightly, 10% of page loads will be sent as page_load_etld pings, and
+// For nightly, 10% of page loads will be sent as page_load_domain pings, and
 // all other page loads will be sent using the default page_load ping.
 //
-// For release and beta, only 0.1% of page loads will be sent as page_load_etld
-// pings, and 10% of the other page loads will be sent using the default ping.
+// For release and beta, only 0.1% of page loads will be sent as
+// page_load_domain pings, and 10% of the other page loads will be sent using
+// the default ping.
 #ifdef NIGHTLY_BUILD
 static constexpr uint64_t kNormalSamplingInterval = 1;   // Every pageload.
 static constexpr uint64_t kDomainSamplingInterval = 10;  // Every 10 pageloads.
@@ -66,12 +79,171 @@ void PageloadEventData::SetUserFeature(UserFeature aFeature) {
   userFeatures = mozilla::Some(value);
 }
 
-bool PageloadEventData::MaybeSetDomain(nsCString& aDomain) {
-  if (aDomain.IsEmpty()) {
+// Check for a wildcard in the given cn and return the wildcard
+// basename if the hn matches this wildcard.
+//
+// Using the rules outlined in RFC 2818 and 9525, wildcard names
+// must start with "*." in the left-most label.
+//
+// e.g. `cn = *..example.com` would match `hn = foo.example.com`
+//      and we would just return example.com in this case.
+static bool DomainMatchesWildcard(char* cn, const char* hn,
+                                  nsCString& newDomainOut) {
+  if (!cn) {
     return false;
   }
 
-  mDomain = mozilla::Some(aDomain);
+  // Check if cn contains a wildcard. Must start with "*.".
+  const bool wildcard = PORT_Strncmp(cn, "*.", 2) == 0;
+
+  if (!wildcard) {
+    return false;
+  }
+
+  // Get suffix after wildcard label.
+  const char* cn_suffix = cn + 2;
+
+  // Check for dot in hn
+  const char* hn_suffix = PORT_Strchr(hn, '.');
+  if (!hn_suffix) {
+    return false;
+  }
+
+  // Skip the dot
+  hn_suffix++;
+
+  // Need three labels to match a wildcard
+  if (!PORT_Strchr(hn_suffix, '.')) {
+    return false;
+  }
+
+  // Check if the wildcard suffix matches the hn suffix.
+  // If true, return the wildcard suffix.
+  if (PORT_Strcasecmp(cn_suffix, hn_suffix) == 0) {
+    newDomainOut.Assign(cn_suffix);
+    return true;
+  }
+
+  return false;
+}
+
+// There are several conditions before we can assign an etld+1 domain:
+// 1.  The server's IP address must be a public IP.
+// 2.  The suffix must be on the PSL (Public Suffix List).
+// 3.  The certificate chain root must be in the built in root list to
+// ensure the trust anchor is public.
+// 4.  If the domain matches a wildcard name, then replace it with the
+// basename of the wildcard instead.
+bool PageloadEventData::MaybeSetPublicRegistrableDomain(nsCOMPtr<nsIURI> aURI,
+                                                        nsIChannel* aChannel) {
+  MOZ_ASSERT(aChannel, "Expecting a valid channel.");
+
+  nsCOMPtr<nsIEffectiveTLDService> tldService =
+      mozilla::components::EffectiveTLD::Service();
+  if (!tldService) {
+    return false;
+  }
+
+  // Make sure the IP address range of the host is public.
+  nsCOMPtr<nsILoadInfo> loadInfo = aChannel->LoadInfo();
+  if (loadInfo->GetIpAddressSpace() != nsILoadInfo::IPAddressSpace::Public) {
+    return false;
+  }
+
+  nsCOMPtr<nsITransportSecurityInfo> tsi;
+  nsresult rv = aChannel->GetSecurityInfo(getter_AddRefs(tsi));
+  if (NS_FAILED(rv) || !tsi) {
+    return false;
+  }
+
+  // Make sure the cert root is in the builtin list.
+  bool rootIsBuiltIn = false;
+  rv = tsi->GetIsBuiltCertChainRootBuiltInRoot(&rootIsBuiltIn);
+  if (NS_FAILED(rv) || !rootIsBuiltIn) {
+    return false;
+  }
+
+  // Make sure the suffix is on the PSL.
+  bool hasKnownPublicSuffix = false;
+  rv = tldService->HasKnownPublicSuffix(aURI, &hasKnownPublicSuffix);
+  if (NS_FAILED(rv) || !hasKnownPublicSuffix) {
+    return false;
+  }
+
+  // Get cert for wildcard matching.
+  nsCOMPtr<nsIX509Cert> cert;
+  rv = tsi->GetServerCert(getter_AddRefs(cert));
+  if (NS_FAILED(rv) || !cert) {
+    return false;
+  }
+
+  UniqueCERTCertificate nssCert(cert->GetCert());
+  if (!nssCert) {
+    return false;
+  }
+
+  // Get ETLD+1 from url, or return on failure.
+  nsAutoCString currentBaseDomain;
+  rv = tldService->GetBaseDomain(aURI, 0, currentBaseDomain);
+  if (NS_FAILED(rv) || currentBaseDomain.IsEmpty()) {
+    return false;
+  }
+
+  SECStatus secrv = SECFailure;
+
+  // cn and cnbuf is used by CERT_RFC1485_EscapeAndQuote as an output
+  // buffer and will add a null terminator.
+  const size_t cnBufLen = 255;
+  char cnBuf[cnBufLen];
+
+  UniquePLArenaPool arena(PORT_NewArena(DER_DEFAULT_CHUNKSIZE));
+  if (!arena) {
+    return false;
+  }
+
+  // Get subject alternate namelist.
+  SECItem subAltName = {siBuffer, nullptr, 0};
+  auto onScopeExit = mozilla::MakeScopeExit(
+      [&]() { SECITEM_FreeItem(&subAltName, PR_FALSE); });
+
+  secrv = CERT_FindCertExtension(nssCert.get(), SEC_OID_X509_SUBJECT_ALT_NAME,
+                                 &subAltName);
+  if (secrv != SECSuccess) {
+    return false;
+  }
+
+  CERTGeneralName* nameList =
+      CERT_DecodeAltNameExtension(arena.get(), &subAltName);
+  if (!nameList) {
+    return false;
+  }
+
+  // Loop through the subject alternate namelist and check if any of
+  // them are wildcards and match the domain we were given. If a match
+  // was found, replace the domain with the wildcard basename.
+  CERTGeneralName* current = nameList;
+  const char* hn = currentBaseDomain.get();
+  do {
+    if (current->type == certDNSName) {
+      // EscapeAndQuote will copy and add a null terminator.
+      secrv = CERT_RFC1485_EscapeAndQuote(cnBuf, cnBufLen,
+                                          (char*)current->name.other.data,
+                                          current->name.other.len);
+      if (secrv != SECSuccess) {
+        return false;
+      }
+
+      nsCString newDomain;
+      if (DomainMatchesWildcard(cnBuf, hn, newDomain)) {
+        mDomain = mozilla::Some(newDomain);
+        return true;
+      }
+    }
+    current = CERT_GetNextGeneralName(current);
+  } while (current && current != nameList);
+
+  // If a matching wildcard wasn't found, use the original etld+1.
+  mDomain = mozilla::Some(currentBaseDomain);
   return true;
 }
 
