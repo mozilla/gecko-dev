3df58e8a52386ccd91159b0824031f4eb6d139da
Serban Stanca <sstanca@mozilla.com>
2025-06-28 01:44:31 +0300

    Revert "Bug 1973135 - Update WebGPU CTS to 97733807d3c40aeb165cf2fbe4137f23c92446ea" for causing webgpu failures.
    This reverts commit d0d8c1c36ec4e6703f55f0e7333f107ecd974574.

--- dom/webgpu/tests/cts/checkout/src/webgpu/shader/execution/expression/unary/u32_conversion.cache.ts
+++ dom/webgpu/tests/cts/checkout/src/webgpu/shader/execution/expression/unary/u32_conversion.cache.ts
@@ -65,12 +65,8 @@ export const d = makeCaseCache('unary/u32_conversion', {
         return { input: f32(f), expected: u32(0) };
       }
 
-      const largestU32WhichIsF32 = 0xffffff00;
-      if (f >= largestU32WhichIsF32) {
-        // This is intentionally not kValue.u32.max
-        // See https://github.com/gpuweb/gpuweb/issues/5043
-
-        return { input: f32(f), expected: u32(largestU32WhichIsF32) };
+      if (f >= kValue.u32.max) {
+        return { input: f32(f), expected: u32(kValue.u32.max) };
       }
 
       // All f32 no larger than 2^24 has a precise integer part and a fractional
