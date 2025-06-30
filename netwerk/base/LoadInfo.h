1e1d2fb511db186f5fad4a7e6adae89ac5724cf5
Serban Stanca <sstanca@mozilla.com>
2025-06-30 12:59:02 +0300

    Revert "Bug 1974504 - change default IpAddressSpace values to unknown for loadinfo and browsing context. r=necko-reviewers,kershaw" for causing multiple failures.
    This reverts commit 2d29f572b53160635ad406e71d710912dbb82187.

--- netwerk/base/LoadInfo.h
+++ netwerk/base/LoadInfo.h
@@ -407,8 +407,8 @@ class LoadInfo final : public nsILoadInfo {
   nsILoadInfo::StoragePermissionState mStoragePermission =
       nsILoadInfo::NoStoragePermission;
   // IP Address space of the parent browsing context.
-  nsILoadInfo::IPAddressSpace mParentIPAddressSpace = nsILoadInfo::Unknown;
-  nsILoadInfo::IPAddressSpace mIPAddressSpace = nsILoadInfo::Unknown;
+  nsILoadInfo::IPAddressSpace mParentIPAddressSpace = nsILoadInfo::Public;
+  nsILoadInfo::IPAddressSpace mIPAddressSpace = nsILoadInfo::Public;
 
   Maybe<RFPTargetSet> mOverriddenFingerprintingSettings;
 #ifdef DEBUG
