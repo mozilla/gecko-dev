55c5ee4a66642f0772b04d2ea334086d53b77311
Ben Dean-Kawamura <bdk@mozilla.com>
2025-06-30 15:30:39 +0000

    Bug 1804078 - Shared UniFFI module, r=bgruber
    I mostly moved code from the templates into `UniFFI.sys.mjs` without
    changing anything.
    
    The one change came from a `./mach lint` warning: we needed parentheses
    in the `!(instanceof Uint8Array)` check in `FfiConverterBytes`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D255148

--- toolkit/components/uniffi-bindgen-gecko-js/src/templates/js/Types.sys.mjs
+++ toolkit/components/uniffi-bindgen-gecko-js/src/templates/js/Types.sys.mjs
@@ -4,52 +4,7 @@
 {% match type_def %}
 
 {%- when TypeDefinition::Simple(type_node) %}
-{%- let ffi_converter = type_node.ffi_converter %}
-{%- match type_node.ty %}
-{%- when Type::Boolean %}
-{%- include "Boolean.sys.mjs" %}
-
-{%- when Type::UInt8 %}
-{%- include "UInt8.sys.mjs" %}
-
-{%- when Type::UInt16 %}
-{%- include "UInt16.sys.mjs" %}
-
-{%- when Type::UInt32 %}
-{%- include "UInt32.sys.mjs" %}
-
-{%- when Type::UInt64 %}
-{%- include "UInt64.sys.mjs" %}
-
-{%- when Type::Int8 %}
-{%- include "Int8.sys.mjs" %}
-
-{%- when Type::Int16 %}
-{%- include "Int16.sys.mjs" %}
-
-{%- when Type::Int32 %}
-{%- include "Int32.sys.mjs" %}
-
-{%- when Type::Int64 %}
-{%- include "Int64.sys.mjs" %}
-
-{%- when Type::Float32 %}
-{%- include "Float32.sys.mjs" %}
-
-{%- when Type::Float64 %}
-{%- include "Float64.sys.mjs" %}
-
-{%- when Type::String %}
-{%- include "String.sys.mjs" %}
-
-{%- when Type::Bytes %}
-{%- include "Bytes.sys.mjs" %}
-
-{%- else %}
-
-throw("Unexpected type in `TypeDefinition::Simple` {{ "{type_node:?}"|format }}")
-
-{%- endmatch %}
+{# No code needed, the ffi converter class is defined in the shared UniFFI.sys.mjs module #}
 
 {%- when TypeDefinition::Optional(optional) %}
 {%- include "Optional.sys.mjs" %}
