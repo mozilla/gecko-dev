0ddb62725f77f98c590c5375a8e47f79a0f40ca5
Simon Friedberger <simon@mozilla.com>
2025-07-02 07:33:49 +0000

    Bug 1973740: Fix full screen menu entries on Mac  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D254872
    
    Differential Revision: https://phabricator.services.mozilla.com/D255623

--- browser/base/content/test/fullscreen/browser_fullscreen_ui_state.js
+++ browser/base/content/test/fullscreen/browser_fullscreen_ui_state.js
@@ -3,6 +3,27 @@
 
 "use strict";
 
+async function click_hamburger_fs_toggle() {
+  // show panel
+  let menuShown = BrowserTestUtils.waitForEvent(PanelUI.panel, "ViewShown");
+  PanelUI.show();
+  await menuShown;
+
+  // click button
+  let hamburgerMenuButton = window.document.getElementById(
+    "appMenu-fullscreen-button2"
+  );
+
+  // clicking the button might hide the menu
+  let menuHidden = BrowserTestUtils.waitForEvent(PanelUI.panel, "popuphidden");
+
+  hamburgerMenuButton.click();
+
+  // try to hide panel explicitly
+  PanelUI.hide();
+  await menuHidden;
+}
+
 async function check_states(fullscreen) {
   // These are the menu items in the View menu
   const fsEnableMac = document.getElementById("enterFullScreenItem");
@@ -72,7 +93,7 @@ async function toggle_fs() {
 }
 
 add_task(async function test_fullscreen_ui_state() {
-  // We add the custom toolbar button to make sure it works
+  // We add the custom button to the toolbar to make sure it works
   CustomizableUI.addWidgetToArea(
     "fullscreen-button",
     CustomizableUI.AREA_NAVBAR
@@ -86,4 +107,96 @@ add_task(async function test_fullscreen_ui_state() {
   inFullScreen = await toggle_fs();
   is(inFullScreen, false, "Full screen should be OFF");
   await check_states(inFullScreen);
+});
+
+add_task(async function test_f11_fullscreen() {
+  const fullScreenEntered = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  info("Pressing F11 to enter fullscreen");
+  EventUtils.synthesizeKey("KEY_F11", {});
+  await fullScreenEntered;
+  ok(window.fullScreen, "Full screen should be ON");
+
+  const fullScreenExited = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  info("Pressing F11 to exit fullscreen");
+  EventUtils.synthesizeKey("KEY_F11", {});
+  await fullScreenExited;
+  ok(!window.fullScreen, "Full screen should be OFF");
+});
+
+add_task(async function test_mac_fullscreen_shortcut() {
+  if (AppConstants.platform !== "macosx") {
+    info("Skipping test for MacOS shortcut on non-Mac");
+    return;
+  }
+
+  const fullScreenEntered = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  info("Pressing Ctrl+Cmd+F to enter fullscreen");
+  EventUtils.synthesizeKey("f", { ctrlKey: true, metaKey: true }, window);
+  await fullScreenEntered;
+  ok(window.fullScreen, "Full screen should be ON");
+
+  const fullScreenExited = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  info("Pressing Ctrl+Cmd+F to enter fullscreen");
+  EventUtils.synthesizeKey("f", { ctrlKey: true, metaKey: true }, window);
+  await fullScreenExited;
+  ok(!window.fullScreen, "Full screen should be OFF");
+});
+
+add_task(async function test_menubar_click() {
+  let fsEnable;
+  let fsDisable;
+  if (AppConstants.platform === "macosx") {
+    fsEnable = document.getElementById("enterFullScreenItem");
+    fsDisable = document.getElementById("exitFullScreenItem");
+  } else {
+    fsEnable = document.getElementById("fullScreenItem");
+    fsDisable = fsEnable;
+  }
+
+  const fullScreenEntered = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  info("Pressing Enter Full Screen");
+  fsEnable.click();
+  await fullScreenEntered;
+  ok(window.fullScreen, "Full screen should be ON");
+
+  const fullScreenExited = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  fsDisable.click();
+  await fullScreenExited;
+  ok(!window.fullScreen, "Full screen should be OFF");
+});
+
+add_task(async function test_hamburger_menu_click() {
+  const fullScreenEntered = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  info("Pressing little double arrow icon");
+  await click_hamburger_fs_toggle();
+  await fullScreenEntered;
+  ok(window.fullScreen, "Full screen should be ON");
+
+  const fullScreenExited = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  info("Pressing little double arrow icon");
+  await click_hamburger_fs_toggle();
+  await fullScreenExited;
+  ok(!window.fullScreen, "Full screen should be OFF");
+});
+
+add_task(async function test_custom_menu_icon_click() {
+  // We add the custom button to the toolbar to make sure it works
+  CustomizableUI.addWidgetToArea(
+    "fullscreen-button",
+    CustomizableUI.AREA_NAVBAR
+  );
+
+  registerCleanupFunction(() => CustomizableUI.reset());
+  const toolbarButton = document.getElementById("fullscreen-button");
+  const fullScreenEntered = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  info("Pressing custom double arrow icon");
+  toolbarButton.click();
+  await fullScreenEntered;
+  ok(window.fullScreen, "Full screen should be ON");
+
+  const fullScreenExited = BrowserTestUtils.waitForEvent(window, "fullscreen");
+  info("Pressing custom double arrow icon");
+  toolbarButton.click();
+  await fullScreenExited;
+  ok(!window.fullScreen, "Full screen should be OFF");
 });
