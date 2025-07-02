6b331e7505d923cd9b6921066a0a841757bd3412
Nicholas Poon <npoon@mozilla.com>
2025-07-02 13:26:17 +0000

    Bug 1972390 - Fix BaselineProfileGenerator annotation KDoc r=android-reviewers,tthibaud
    Differential Revision: https://phabricator.services.mozilla.com/D255205

--- mobile/android/fenix/benchmark/src/main/java/org/mozilla/fenix/benchmark/baselineprofile/BaselineProfileGenerator.kt
+++ mobile/android/fenix/benchmark/src/main/java/org/mozilla/fenix/benchmark/baselineprofile/BaselineProfileGenerator.kt
@@ -5,12 +5,12 @@
 package org.mozilla.fenix.benchmark.baselineprofile
 
 /**
- * A custom annotation to to identify Baseline Profile Generator tests.
+ * A custom annotation used to identify Baseline Profile Generator tests.
  * All BaselineProfileGenerator tests can be run in a flank configuration with:
  *   test-targets:
  *     - annotation org.mozilla.fenix.benchmark.baselineprofile.BaselineProfileGenerator
  *
- * Please remember to update [flank-arm64-v8a-baseline-profile.yml](https://searchfox.org/mozilla-central/source/mobile/android/fenix/automation/taskcluster/androidTest/flank-arm64-v8a-baseline-profile.yml)
+ * Please remember to update [arm64-v8a-baseline-profile.yml](https://searchfox.org/mozilla-central/source/mobile/android/test_infra/flank-configs/fenix/arm64-v8a-baseline-profile.yml)
  * and any other use of this annotation if its name or package is changed.
  */
 annotation class BaselineProfileGenerator
