1630f0587dd498bd3395f6aec221c38793570d83
Emilio Cobos Álvarez <emilio@crisal.io>
2025-07-03 09:54:48 +0000

    Bug 1974573 - Stop setting browser color-scheme from contentTheme.js. r=dao,desktop-theme-reviewers,fxview-reviewers,sclements a=pascalc
    This was found originally in bug 1973777. This sets the color scheme of
    the sidebar based off the newtab colors, so the fact that it worked was
    total luck.
    
    This was added for bug 1786627 for Firefox View. Instead, make
    firefoxview.css and common-shared work with both light and dark modes
    via light-dark, and since it wants the same color-scheme as the new tab,
    do so.
    
    That makes sure that the sidebar box and the sidebar <browser> don't get
    into inconsistent color schemes. This matters specially for the legacy
    sidebar since they don't set a background in the <browser> directly. But
    it's a good cleanup regardless.
    
    Differential Revision: https://phabricator.services.mozilla.com/D254857

--- browser/base/content/contentTheme.js
+++ browser/base/content/contentTheme.js
@@ -43,31 +43,18 @@
       {
         lwtProperty: "ntp_text",
         processColor(rgbaChannels, element) {
-          // We only have access to the browser when we're in a chrome
-          // docshell, so for now only set the color scheme in that case, and
-          // use the `lwt-newtab-brighttext` attribute as a fallback mechanism.
-          let browserStyle =
-            element.ownerGlobal?.docShell?.chromeEventHandler.style;
-
           element.toggleAttribute("lwt-newtab", !!rgbaChannels);
           if (!rgbaChannels) {
             element.toggleAttribute(
               "lwt-newtab-brighttext",
               prefersDarkQuery.matches
             );
-            if (browserStyle) {
-              browserStyle.colorScheme = "";
-            }
             return null;
           }
 
           const { r, g, b, a } = rgbaChannels;
           let darkMode = !_isTextColorDark(r, g, b);
           element.toggleAttribute("lwt-newtab-brighttext", darkMode);
-          if (browserStyle) {
-            browserStyle.colorScheme = darkMode ? "dark" : "light";
-          }
-
           return `rgba(${r}, ${g}, ${b}, ${a})`;
         },
       },
