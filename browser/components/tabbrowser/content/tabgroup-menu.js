449feed9c7f170986b00461527d19d0f04682c85
Nick Grato <ngrato@gmail.com>
2025-07-03 16:49:53 +0000

    Bug 1968030 - Smart tab checkbox is not correct component r=desktop-theme-reviewers,tabbrowser-reviewers,dao,sthompson
    The check boxes and labels have tech dept and need to be updated to official lit checkbox components. They are currently disabled buttons and are not sufficient for ADA testing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D251942

--- browser/components/tabbrowser/content/tabgroup-menu.js
+++ browser/components/tabbrowser/content/tabgroup-menu.js
@@ -117,12 +117,11 @@
     static suggestionsSection = /*html*/ `
       <html:div id="tab-group-suggestions-container" hidden="true">
 
-        <checkbox
-          checked="true"
-          type="checkbox"
+        <html:moz-checkbox
+          checked=""
           id="tab-group-select-checkbox"
           data-l10n-id="tab-group-editor-select-suggestions">
-        </checkbox>
+        </html:moz-checkbox>
 
         <html:div id="tab-group-suggestions"></html:div>
 
@@ -621,14 +620,13 @@
       this.#selectSuggestionsCheckbox = this.querySelector(
         "#tab-group-select-checkbox"
       );
-      this.#selectSuggestionsCheckbox.addEventListener(
-        "CheckboxStateChange",
-        () => {
-          this.#selectSuggestionsCheckbox.checked
-            ? this.#handleSelectAll()
-            : this.#handleDeselectAll();
+      this.#selectSuggestionsCheckbox.addEventListener("change", e => {
+        if (e.target.checked) {
+          this.#handleSelectAll();
+        } else {
+          this.#handleDeselectAll();
         }
-      );
+      });
       this.#suggestionsMessageContainer = this.querySelector(
         "#tab-group-suggestions-message-container"
       );
@@ -1169,18 +1167,18 @@
       });
     }
 
-    #createRow(tab, index) {
-      // Create Row
-      let row = document.createXULElement("toolbaritem");
-      row.setAttribute("context", "tabContextMenu");
-      row.setAttribute("id", `tab-bar-${index}`);
-
+    #createRow(tab) {
       // Create Checkbox
-      let checkbox = document.createXULElement("checkbox");
+      let checkbox = document.createElement("moz-checkbox");
       checkbox.value = tab;
-      checkbox.setAttribute("checked", true);
-      checkbox.classList.add("tab-group-suggestion-checkbox");
-      checkbox.addEventListener("CheckboxStateChange", e => {
+      checkbox.label = tab.label;
+      checkbox.iconSrc = tab.image;
+      checkbox.checked = true;
+      checkbox.classList.add(
+        "tab-group-suggestion-checkbox",
+        "text-truncated-ellipsis"
+      );
+      checkbox.addEventListener("change", e => {
         const isChecked = e.target.checked;
         const currentTab = e.target.value;
 
@@ -1193,25 +1191,8 @@
         }
       });
 
-      row.appendChild(checkbox);
-
-      // Create Row Label
-      let label = document.createXULElement("toolbarbutton");
-      label.classList.add(
-        "all-tabs-button",
-        "subviewbutton",
-        "subviewbutton-iconic",
-        "tab-group-suggestion-label"
-      );
-      label.setAttribute("flex", "1");
-      label.setAttribute("crop", "end");
-      label.label = tab.label;
-      label.image = tab.image;
-      label.disabled = true;
-      row.appendChild(label);
-
       // Apply Row to Suggestions
-      this.#suggestions.appendChild(row);
+      this.#suggestions.appendChild(checkbox);
     }
 
     /**
@@ -1279,11 +1260,14 @@
       this.#suggestedTabs = [];
       this.#selectedSuggestedTabs = [];
       if (this.#suggestions) {
-        this.#suggestions.innerHTML = "";
+        this.#suggestions.replaceChildren();
+      }
+      if (this.#selectSuggestionsCheckbox) {
+        this.#selectSuggestionsCheckbox.checkbox = true;
       }
       this.#showSmartSuggestionsContainer(false);
       if (this.#suggestionsOptinContainer) {
-        this.#suggestionsOptinContainer.innerHTML = "";
+        this.#suggestionsOptinContainer.replaceChildren();
       }
     }
 
