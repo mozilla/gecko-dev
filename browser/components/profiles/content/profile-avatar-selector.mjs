6b6c885b459d3b58fd6e769a971e1f9b07d3eee2
Norisz Fay <nfay@mozilla.com>
2025-07-03 17:40:51 +0300

    Revert "Bug 1965532 - Create profiles custom avatar selection UI. r=profiles-reviewers,desktop-theme-reviewers,tbersentes,jhirsch,fluent-reviewers,bolsson" for causing bc failures on browser_parsable_css.js
    This reverts commit 3fe3ab4c0c20b2d34665499c4f3bdd61e06b1890.

--- browser/components/profiles/content/profile-avatar-selector.mjs
+++ browser/components/profiles/content/profile-avatar-selector.mjs
@@ -4,86 +4,25 @@
 
 import { MozLitElement } from "chrome://global/content/lit-utils.mjs";
 import { html } from "chrome://global/content/vendor/lit.all.mjs";
-import { Region, ViewDimensions } from "./avatarSelectionHelpers.mjs";
-
-const VIEWS = {
-  ICON: "icon",
-  CUSTOM: "custom",
-  CROP: "crop",
-};
-
-const STATES = {
-  SELECTED: "selected",
-  RESIZING: "resizing",
-};
 
 /**
  * Element used for displaying an avatar on the about:editprofile and about:newprofile pages.
  */
 export class ProfileAvatarSelector extends MozLitElement {
-  #moverId = "";
-
   static properties = {
     value: { type: String },
-    view: { type: String },
+    state: { type: String },
   };
 
   static queries = {
-    input: "#custom-image-input",
+    input: "#custom-image",
     saveButton: "#save-button",
-    customAvatarCropArea: ".custom-avatar-crop-area",
-    customAvatarImage: "#custom-avatar-image",
-    avatarSelectionContainer: "#avatar-selection-container",
-    highlight: "#highlight",
-    iconTabButton: "#icon",
-    customTabButton: "#custom",
-    topLeftMover: "#mover-topLeft",
-    topRightMover: "#mover-topRight",
-    bottomLeftMover: "#mover-bottomLeft",
-    bottomRightMover: "#mover-bottomRight",
   };
 
   constructor() {
     super();
 
-    this.setView(VIEWS.ICON);
-    this.viewDimensions = new ViewDimensions();
-    this.avatarRegion = new Region(this.viewDimensions);
-
-    this.state = STATES.SELECTED;
-  }
-
-  setView(newView) {
-    if (this.view === VIEWS.CROP) {
-      this.cropViewEnd();
-    }
-
-    switch (newView) {
-      case VIEWS.ICON:
-        this.view = VIEWS.ICON;
-        break;
-      case VIEWS.CUSTOM:
-        this.view = VIEWS.CUSTOM;
-        break;
-      case VIEWS.CROP:
-        this.view = VIEWS.CROP;
-        this.cropViewStart();
-        break;
-    }
-  }
-
-  cropViewStart() {
-    window.addEventListener("pointerdown", this);
-    window.addEventListener("pointermove", this);
-    window.addEventListener("pointerup", this);
-    document.documentElement.classList.add("disable-text-selection");
-  }
-
-  cropViewEnd() {
-    window.removeEventListener("pointerdown", this);
-    window.removeEventListener("pointermove", this);
-    window.removeEventListener("pointerup", this);
-    document.documentElement.classList.remove("disable-text-selection");
+    this.state = "icon";
   }
 
   getAvatarL10nId(value) {
@@ -105,13 +44,9 @@ export class ProfileAvatarSelector extends MozLitElement {
     return "";
   }
 
-  handleTabClick(event) {
+  handleTabClick(event, tabName) {
     event.stopImmediatePropagation();
-    if (event.target.id === "icon") {
-      this.setView(VIEWS.ICON);
-    } else {
-      this.setView(VIEWS.CUSTOM);
-    }
+    this.state = tabName;
   }
 
   iconTabContentTemplate() {
@@ -173,82 +108,78 @@ export class ProfileAvatarSelector extends MozLitElement {
   }
 
   customTabUploadFileContentTemplate() {
-    return html`<div
-        class="custom-avatar-add-image-header"
-        data-l10n-id="avatar-selector-add-image"
-      ></div>
-      <div class="custom-avatar-upload-area">
-        <input
-          @change=${this.handleFileUpload}
-          id="custom-image-input"
-          type="file"
-          accept="image/*"
-          label="Upload a file"
-        />
-        <div id="file-messages">
-          <img src="chrome://browser/skin/open.svg" />
-          <span
-            id="upload-text"
-            data-l10n-id="avatar-selector-upload-file"
-          ></span>
-          <span id="drag-text" data-l10n-id="avatar-selector-drag-file"></span>
-        </div>
-      </div>`;
+    return html`<div class="custom-avatar-area">
+      <input
+        @change=${this.handleFileUpload}
+        id="custom-image"
+        type="file"
+        accept="image/*"
+        label="Upload a file"
+      />
+      <div id="file-messages">
+        <img src="chrome://browser/skin/open.svg" />
+        <span
+          id="upload-text"
+          data-l10n-id="avatar-selector-upload-file"
+        ></span>
+        <span id="drag-text" data-l10n-id="avatar-selector-drag-file"></span>
+      </div>
+    </div>`;
+  }
+
+  handleCancelClick(event) {
+    event.stopImmediatePropagation();
+
+    this.state = "custom";
+    if (this.blobURL) {
+      URL.revokeObjectURL(this.blobURL);
+    }
+    this.file = null;
+  }
+
+  async handleSaveClick(event) {
+    event.stopImmediatePropagation();
+
+    const img = new Image();
+    img.src = this.blobURL;
+    await img.decode();
+
+    const size = 512;
+    const canvas = new OffscreenCanvas(size, size);
+    canvas.width = canvas.height = size;
+    const ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
+    ctx.clip();
+
+    const scale = size / Math.min(img.width, img.height);
+    const x = (size - img.width * scale) / 2;
+    const y = (size - img.height * scale) / 2;
+    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+
+    const blob = await canvas.convertToBlob({ type: "image/png" });
+    const circularFile = new File([blob], this.file.name, {
+      type: "image/png",
+    });
+
+    document.dispatchEvent(
+      new CustomEvent("Profiles:CustomAvatarUpload", {
+        detail: { file: circularFile },
+      })
+    );
+
+    if (this.blobURL) {
+      URL.revokeObjectURL(this.blobURL);
+    }
+
+    this.state = "custom";
+    this.hidden = true;
   }
 
   customTabViewImageTemplate() {
-    return html`<div class="custom-avatar-crop-header">
-        <moz-button
-          id="back-button"
-          @click=${this.handleCancelClick}
-          type="icon ghost"
-          iconSrc="chrome://global/skin/icons/arrow-left.svg"
-        ></moz-button>
-        <span data-l10n-id="avatar-selector-crop"></span>
-        <div id="spacer"></div>
-      </div>
-      <div class="custom-avatar-crop-area">
-        <div id="avatar-selection-container">
-          <div id="highlight" class="highlight" tabindex="0">
-            <div id="highlight-background"></div>
-            <div
-              id="mover-topLeft"
-              class="mover-target direction-topLeft"
-              tabindex="0"
-            >
-              <div class="mover"></div>
-            </div>
-
-            <div
-              id="mover-topRight"
-              class="mover-target direction-topRight"
-              tabindex="0"
-            >
-              <div class="mover"></div>
-            </div>
-
-            <div
-              id="mover-bottomRight"
-              class="mover-target direction-bottomRight"
-              tabindex="0"
-            >
-              <div class="mover"></div>
-            </div>
-
-            <div
-              id="mover-bottomLeft"
-              class="mover-target direction-bottomLeft"
-              tabindex="0"
-            >
-              <div class="mover"></div>
-            </div>
-          </div>
-        </div>
-        <img
-          id="custom-avatar-image"
-          src=${this.blobURL}
-          @load=${this.imageLoaded}
-        />
+    return html`<div class="custom-avatar-area">
+        <img id="custom-avatar-image" src=${this.blobURL} />
       </div>
       <moz-button-group class="custom-avatar-actions"
         ><moz-button
@@ -264,243 +195,6 @@ export class ProfileAvatarSelector extends MozLitElement {
       ></moz-button-group>`;
   }
 
-  handleCancelClick(event) {
-    event.stopImmediatePropagation();
-
-    this.setView(VIEWS.CUSTOM);
-    if (this.blobURL) {
-      URL.revokeObjectURL(this.blobURL);
-    }
-    this.file = null;
-  }
-
-  async handleSaveClick(event) {
-    event.stopImmediatePropagation();
-
-    const img = new Image();
-    img.src = this.blobURL;
-    await img.decode();
-
-    const { width: imageWidth, height: imageHeight } = img;
-    const scale =
-      imageWidth <= imageHeight
-        ? imageWidth / this.customAvatarCropArea.clientWidth
-        : imageHeight / this.customAvatarCropArea.clientHeight;
-
-    // eslint-disable-next-line no-shadow
-    const { left, top, radius } = this.avatarRegion.dimensions;
-    // eslint-disable-next-line no-shadow
-    const { devicePixelRatio } = this.viewDimensions.dimensions;
-    const { scrollTop, scrollLeft } = this.customAvatarCropArea;
-
-    // Create the canvas so it is a square around the selected area.
-    const scaledRadius = Math.round(radius * scale * devicePixelRatio);
-    const squareSize = scaledRadius * 2;
-    const squareCanvas = new OffscreenCanvas(squareSize, squareSize);
-    const squareCtx = squareCanvas.getContext("2d");
-
-    // Crop the canvas so it is a circle.
-    squareCtx.beginPath();
-    squareCtx.arc(scaledRadius, scaledRadius, scaledRadius, 0, Math.PI * 2);
-    squareCtx.clip();
-
-    const sourceX = Math.round((left + scrollLeft) * scale);
-    const sourceY = Math.round((top + scrollTop) * scale);
-    const sourceWidth = Math.round(radius * 2 * scale);
-
-    // Draw the image onto the canvas.
-    squareCtx.drawImage(
-      img,
-      sourceX,
-      sourceY,
-      sourceWidth,
-      sourceWidth,
-      0,
-      0,
-      squareSize,
-      squareSize
-    );
-
-    const blob = await squareCanvas.convertToBlob({ type: "image/png" });
-    const circularFile = new File([blob], this.file.name, {
-      type: "image/png",
-    });
-
-    document.dispatchEvent(
-      new CustomEvent("Profiles:CustomAvatarUpload", {
-        detail: { file: circularFile },
-      })
-    );
-
-    if (this.blobURL) {
-      URL.revokeObjectURL(this.blobURL);
-    }
-
-    this.setView(VIEWS.CUSTOM);
-    this.hidden = true;
-  }
-
-  updateViewDimensions() {
-    let { width, height } = this.customAvatarImage;
-
-    this.viewDimensions.dimensions = {
-      width: this.customAvatarCropArea.clientWidth,
-      height: this.customAvatarCropArea.clientHeight,
-      devicePixelRatio: window.devicePixelRatio,
-    };
-
-    if (width > height) {
-      this.customAvatarImage.classList.add("height-full");
-    } else {
-      this.customAvatarImage.classList.add("width-full");
-    }
-  }
-
-  imageLoaded() {
-    this.updateViewDimensions();
-    this.setInitialAvatarSelection();
-  }
-
-  setInitialAvatarSelection() {
-    let diameter = Math.min(
-      this.viewDimensions.width,
-      this.viewDimensions.height
-    );
-
-    let left =
-      Math.floor(this.viewDimensions.width / 2) - Math.floor(diameter / 2);
-    // eslint-disable-next-line no-shadow
-    let top =
-      Math.floor(this.viewDimensions.height / 2) - Math.floor(diameter / 2);
-
-    let right = left + diameter;
-    let bottom = top + diameter;
-
-    this.avatarRegion.resizeToSquare({ left, top, right, bottom });
-
-    this.drawSelectionContainer();
-  }
-
-  drawSelectionContainer() {
-    // eslint-disable-next-line no-shadow
-    let { top, left, width, height } = this.avatarRegion.dimensions;
-
-    this.highlight.style = `top:${top}px;left:${left}px;width:${width}px;height:${height}px;`;
-  }
-
-  getCoordinatesFromEvent(event) {
-    let { clientX, clientY, movementX, movementY } = event;
-    let rect = this.avatarSelectionContainer.getBoundingClientRect();
-
-    return { x: clientX - rect.x, y: clientY - rect.y, movementX, movementY };
-  }
-
-  handleEvent(event) {
-    if (this.view !== VIEWS.CROP) {
-      return;
-    }
-
-    switch (event.type) {
-      case "pointerdown":
-        this.handlePointerDown(event);
-        break;
-      case "pointermove":
-        this.handlePointerMove(event);
-        break;
-      case "pointerup":
-        this.handlePointerUp(event);
-        break;
-    }
-  }
-
-  handlePointerDown(event) {
-    let targetId = event.originalTarget?.id;
-    if (
-      [
-        "highlight",
-        "mover-topLeft",
-        "mover-topRight",
-        "mover-bottomRight",
-        "mover-bottomLeft",
-      ].includes(targetId)
-    ) {
-      this.state = STATES.RESIZING;
-      this.#moverId = targetId;
-    }
-  }
-
-  handlePointerMove(event) {
-    if (this.state === STATES.RESIZING) {
-      let { x, y, movementX, movementY } = this.getCoordinatesFromEvent(event);
-      this.handleResizingPointerMove(x, y, movementX, movementY);
-    }
-  }
-
-  handleResizingPointerMove(x, y, movementX, movementY) {
-    switch (this.#moverId) {
-      case "highlight": {
-        this.avatarRegion.resizeToSquare(
-          {
-            left: this.avatarRegion.left + movementX,
-            top: this.avatarRegion.top + movementY,
-            right: this.avatarRegion.right + movementX,
-            bottom: this.avatarRegion.bottom + movementY,
-          },
-          this.#moverId
-        );
-        break;
-      }
-      case "mover-topLeft": {
-        this.avatarRegion.resizeToSquare(
-          {
-            left: x,
-            top: y,
-          },
-          this.#moverId
-        );
-        break;
-      }
-      case "mover-topRight": {
-        this.avatarRegion.resizeToSquare(
-          {
-            top: y,
-            right: x,
-          },
-          this.#moverId
-        );
-        break;
-      }
-      case "mover-bottomRight": {
-        this.avatarRegion.resizeToSquare(
-          {
-            right: x,
-            bottom: y,
-          },
-          this.#moverId
-        );
-        break;
-      }
-      case "mover-bottomLeft": {
-        this.avatarRegion.resizeToSquare(
-          {
-            left: x,
-            bottom: y,
-          },
-          this.#moverId
-        );
-        break;
-      }
-    }
-
-    this.drawSelectionContainer();
-  }
-
-  handlePointerUp() {
-    this.state = STATES.SELECTED;
-    this.#moverId = "";
-    this.avatarRegion.sortCoords();
-  }
-
   handleFileUpload(event) {
     const [file] = event.target.files;
     this.file = file;
@@ -510,18 +204,18 @@ export class ProfileAvatarSelector extends MozLitElement {
     }
 
     this.blobURL = URL.createObjectURL(file);
-    this.setView(VIEWS.CROP);
+    this.state = "crop";
   }
 
   contentTemplate() {
-    switch (this.view) {
-      case VIEWS.ICON: {
+    switch (this.state) {
+      case "icon": {
         return this.iconTabContentTemplate();
       }
-      case VIEWS.CUSTOM: {
+      case "custom": {
         return this.customTabUploadFileContentTemplate();
       }
-      case VIEWS.CROP: {
+      case "crop": {
         return this.customTabViewImageTemplate();
       }
     }
@@ -534,25 +228,21 @@ export class ProfileAvatarSelector extends MozLitElement {
         href="chrome://browser/content/profiles/profile-avatar-selector.css"
       />
       <moz-card id="avatar-selector">
-        <div id="content">
-          <div class="button-group">
-            <moz-button
-              id="icon"
-              type=${this.view === VIEWS.ICON ? "primary" : "default"}
-              size="small"
-              data-l10n-id="avatar-selector-icon-tab"
-              @click=${this.handleTabClick}
-            ></moz-button>
-            <moz-button
-              id="custom"
-              type=${this.view === VIEWS.ICON ? "default" : "primary"}
-              size="small"
-              data-l10n-id="avatar-selector-custom-tab"
-              @click=${this.handleTabClick}
-            ></moz-button>
-          </div>
-          ${this.contentTemplate()}
+        <div class="button-group">
+          <moz-button
+            type=${this.state === "icon" ? "primary" : "default"}
+            size="small"
+            data-l10n-id="avatar-selector-icon-tab"
+            @click=${e => this.handleTabClick(e, "icon")}
+          ></moz-button>
+          <moz-button
+            type=${this.state === "custom" ? "primary" : "default"}
+            size="small"
+            data-l10n-id="avatar-selector-custom-tab"
+            @click=${e => this.handleTabClick(e, "custom")}
+          ></moz-button>
         </div>
+        ${this.contentTemplate()}
       </moz-card>`;
   }
 }
