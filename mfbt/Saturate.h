5443c1e869eb86c407d587fd0d0bcce7c34c2342
Norisz Fay <nfay@mozilla.com>
2025-07-03 17:39:37 +0300

    Revert "Bug 1975419 - Use compiler intrinsic for saturated arithmetic r=emilio" for causing SM bustages on Saturate.h
    This reverts commit c414351fca733bd84796a638572dea79a2c79ab1.

--- mfbt/Saturate.h
+++ mfbt/Saturate.h
@@ -55,9 +55,6 @@ class SaturateOp {
   // Compound operators
 
   const T& operator+=(const T& aRhs) const {
-#if defined(__has_builtin) && __has_builtin(__builtin_elementwise_add_sat)
-    return mValue = __builtin_elementwise_add_sat(mValue, aRhs);
-#else
     const T min = std::numeric_limits<T>::min();
     const T max = std::numeric_limits<T>::max();
 
@@ -67,13 +64,9 @@ class SaturateOp {
       mValue = (min - aRhs) > mValue ? min : mValue + aRhs;
     }
     return mValue;
-#endif
   }
 
   const T& operator-=(const T& aRhs) const {
-#if defined(__has_builtin) && __has_builtin(__builtin_elementwise_sub_sat)
-    return mValue = __builtin_elementwise_sub_sat(mValue, aRhs);
-#else
     const T min = std::numeric_limits<T>::min();
     const T max = std::numeric_limits<T>::max();
 
@@ -83,7 +76,6 @@ class SaturateOp {
       mValue = (max + aRhs) < mValue ? max : mValue - aRhs;
     }
     return mValue;
-#endif
   }
 
   // Increment and decrement operators
