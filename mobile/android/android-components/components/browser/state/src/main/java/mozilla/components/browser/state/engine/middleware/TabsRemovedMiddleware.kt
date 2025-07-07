6093edc299685357b4b1b18f4eff5206af2975aa
Jonathan Almeida <jalmeida@mozilla.com>
2025-07-07 16:11:29 +0000

    Bug 1958738 - Remove code that monitors last state update r=kaya,android-reviewers
    This patch waits for a final `onStateUpdated` callback, which may never
    be received if the tabs are removed on app close by the user or the
    system (i.e. user action to swipe the app away or system reclaiming
    resources). This means that we may never end up calling
    `EngineSession.close` which performs various clean-ups on the Gecko side.
    
    One of those clean-up tasks is to remove local storage for private tabs
    which is what is described in the linked bug for Focus.
    
    This patch reverts cleanly when removing the following
    (mozilla-firefox git) commits in the order below:
    
    This reverts commit 11c0a6758b4de08230c26beb733f0e370080b026.
    This reverst commit 5a6fc7a77b26f59ab30980663a711f5e6f161a22.
    This reverst commit 9da314a63eede709dac25efc486112cd78ee7708.
    
    Differential Revision: https://phabricator.services.mozilla.com/D256154

--- mobile/android/android-components/components/browser/state/src/main/java/mozilla/components/browser/state/engine/middleware/TabsRemovedMiddleware.kt
+++ mobile/android/android-components/components/browser/state/src/main/java/mozilla/components/browser/state/engine/middleware/TabsRemovedMiddleware.kt
@@ -4,50 +4,21 @@
 
 package mozilla.components.browser.state.engine.middleware
 
-import androidx.annotation.VisibleForTesting
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import mozilla.components.browser.state.action.BrowserAction
 import mozilla.components.browser.state.action.CustomTabListAction
 import mozilla.components.browser.state.action.EngineAction
 import mozilla.components.browser.state.action.TabListAction
-import mozilla.components.browser.state.action.UndoAction
 import mozilla.components.browser.state.selector.findCustomTab
 import mozilla.components.browser.state.selector.findTab
 import mozilla.components.browser.state.selector.normalTabs
 import mozilla.components.browser.state.selector.privateTabs
 import mozilla.components.browser.state.state.BrowserState
-import mozilla.components.browser.state.state.CustomTabSessionState
 import mozilla.components.browser.state.state.SessionState
 import mozilla.components.concept.engine.EngineSession
-import mozilla.components.concept.engine.EngineSessionState
 import mozilla.components.lib.state.Middleware
 import mozilla.components.lib.state.MiddlewareContext
-
-@VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-internal class SessionsPendingDeletion {
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    var sessions: MutableMap<String, EngineSession> = mutableMapOf()
-    private var mutex = Mutex()
-
-    suspend fun add(id: String, session: EngineSession) = mutex.withLock {
-        sessions[id] = session
-    }
-
-    suspend fun remove(id: String) = mutex.withLock {
-        sessions.remove(id)
-    }
-
-    suspend fun removeAll(callback: (EngineSession) -> Unit) = mutex.withLock {
-        val keys = sessions.keys.toList()
-        for (key in keys) {
-            sessions[key]?.also(callback)
-            sessions.remove(key)
-        }
-    }
-}
 
 /**
  * [Middleware] responsible for closing and unlinking [EngineSession] instances whenever tabs get
@@ -56,9 +27,6 @@ internal class SessionsPendingDeletion {
 internal class TabsRemovedMiddleware(
     private val scope: CoroutineScope,
 ) : Middleware<BrowserState, BrowserAction> {
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    val sessionsPendingDeletion = SessionsPendingDeletion()
-
     @Suppress("ComplexMethod")
     override fun invoke(
         context: MiddlewareContext<BrowserState, BrowserAction>,
@@ -79,7 +47,6 @@ internal class TabsRemovedMiddleware(
             is CustomTabListAction.RemoveCustomTabAction -> context.state.findCustomTab(action.tabId)?.let {
                 onTabsRemoved(context, listOf(it))
             }
-            is UndoAction.ClearRecoverableTabs, UndoAction.RestoreRecoverableTabs -> clearSessionsPendingDeletion()
             else -> {
                 // no-op
             }
@@ -94,50 +61,15 @@ internal class TabsRemovedMiddleware(
     ) {
         tabs.forEach { tab ->
             if (tab.engineState.engineSession != null) {
-                // We don't have a way to recover custom tabs, so let's not observe and just close
-                // the session.
-                if (tab is CustomTabSessionState) {
-                    scope.launch { tab.engineState.engineSession?.close() }
-                } else {
-                    // With the addition of [SHIP](https://bugzilla.mozilla.org/show_bug.cgi?id=1736121)
-                    // Our tab state may be out of sync with GeckoView. Let's wait for one more `onStateUpdated`
-                    // event before we close the engine session.
-                    waitForFinalStateUpdate(context, tab)
-                }
-
                 context.dispatch(
                     EngineAction.UnlinkEngineSessionAction(
                         tab.id,
                     ),
                 )
-            }
-        }
-    }
-
-    private fun waitForFinalStateUpdate(
-        context: MiddlewareContext<BrowserState, BrowserAction>,
-        sessionState: SessionState,
-    ) {
-        sessionState.mediaSessionState?.controller?.pause()
-        sessionState.engineState.engineSession?.also {
-            it.register(object : EngineSession.Observer {
-                override fun onStateUpdated(state: EngineSessionState) {
-                    context.store.dispatch(UndoAction.UpdateEngineStateForRecoverableTab(sessionState.id, state))
-                    scope.launch {
-                        sessionsPendingDeletion.remove(sessionState.id)
-                    }
-                    it.close()
+                scope.launch {
+                    tab.engineState.engineSession?.close()
                 }
-            })
-            scope.launch {
-                sessionsPendingDeletion.add(sessionState.id, it)
             }
-        }
-    }
-
-    private fun clearSessionsPendingDeletion() = scope.launch {
-        sessionsPendingDeletion.removeAll {
-            it.close()
         }
     }
 }
--- mobile/android/android-components/components/browser/state/src/test/java/mozilla/components/browser/state/engine/middleware/TabsRemovedMiddlewareTest.kt
+++ mobile/android/android-components/components/browser/state/src/test/java/mozilla/components/browser/state/engine/middleware/TabsRemovedMiddlewareTest.kt
@@ -8,29 +8,23 @@ import mozilla.components.browser.state.action.BrowserAction
 import mozilla.components.browser.state.action.CustomTabListAction
 import mozilla.components.browser.state.action.EngineAction
 import mozilla.components.browser.state.action.TabListAction
-import mozilla.components.browser.state.action.UndoAction
 import mozilla.components.browser.state.selector.findCustomTab
 import mozilla.components.browser.state.selector.findTab
 import mozilla.components.browser.state.selector.findTabOrCustomTab
 import mozilla.components.browser.state.state.BrowserState
-import mozilla.components.browser.state.state.MediaSessionState
 import mozilla.components.browser.state.state.createCustomTab
 import mozilla.components.browser.state.state.createTab
 import mozilla.components.browser.state.store.BrowserStore
 import mozilla.components.concept.engine.EngineSession
-import mozilla.components.concept.engine.mediasession.MediaSession
 import mozilla.components.lib.state.Middleware
 import mozilla.components.lib.state.MiddlewareContext
-import mozilla.components.support.test.argumentCaptor
 import mozilla.components.support.test.ext.joinBlocking
 import mozilla.components.support.test.libstate.ext.waitUntilIdle
 import mozilla.components.support.test.mock
 import mozilla.components.support.test.rule.MainCoroutineRule
 import mozilla.components.support.test.rule.runTestOnMain
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
-import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.never
@@ -43,7 +37,7 @@ class TabsRemovedMiddlewareTest {
     private val scope = coroutinesTestRule.scope
 
     @Test
-    fun `unlinks engine session when tab is removed`() = runTestOnMain {
+    fun `closes and unlinks engine session when tab is removed`() = runTestOnMain {
         val middleware = TabsRemovedMiddleware(scope)
 
         val tab = createTab("https://www.mozilla.org", id = "1")
@@ -52,42 +46,17 @@ class TabsRemovedMiddlewareTest {
             middleware = listOf(middleware, ConsumeRemoveTabActionsMiddleware()),
         )
 
-        linkEngineSession(store, tab.id)
+        val engineSession = linkEngineSession(store, tab.id)
         store.dispatch(TabListAction.RemoveTabAction(tab.id)).joinBlocking()
         store.waitUntilIdle()
         dispatcher.scheduler.advanceUntilIdle()
 
         assertNull(store.state.findTab(tab.id)?.engineState?.engineSession)
-        assertEquals(1, middleware.sessionsPendingDeletion.sessions.size)
+        verify(engineSession).close()
     }
 
     @Test
-    fun `pause any media that is playing when tab is removed`() = runTestOnMain {
-        val middleware = TabsRemovedMiddleware(scope)
-        val controller: MediaSession.Controller = mock()
-
-        val tab = createTab(
-            url = "https://www.mozilla.org",
-            id = "1",
-            mediaSessionState = MediaSessionState(controller),
-        )
-        val store = BrowserStore(
-            initialState = BrowserState(tabs = listOf(tab)),
-            middleware = listOf(middleware, ConsumeRemoveTabActionsMiddleware()),
-        )
-
-        linkEngineSession(store, tab.id)
-        store.dispatch(TabListAction.RemoveTabAction(tab.id)).joinBlocking()
-        store.waitUntilIdle()
-        dispatcher.scheduler.advanceUntilIdle()
-
-        assertNull(store.state.findTab(tab.id)?.engineState?.engineSession)
-        assertEquals(1, middleware.sessionsPendingDeletion.sessions.size)
-        verify(controller).pause()
-    }
-
-    @Test
-    fun `unlinks engine session when list of tabs are removed`() = runTestOnMain {
+    fun `closes and unlinks engine session when list of tabs are removed`() = runTestOnMain {
         val middleware = TabsRemovedMiddleware(scope)
 
         val tab1 = createTab("https://www.mozilla.org", id = "1", private = false)
@@ -99,9 +68,9 @@ class TabsRemovedMiddlewareTest {
             middleware = listOf(middleware, ConsumeRemoveTabActionsMiddleware()),
         )
 
-        listOf(tab1, tab2, tab3).forEach {
-            linkEngineSession(store, it.id)
-        }
+        val engineSession1 = linkEngineSession(store, tab1.id)
+        val engineSession2 = linkEngineSession(store, tab2.id)
+        val engineSession3 = linkEngineSession(store, tab3.id)
 
         store.dispatch(TabListAction.RemoveTabsAction(listOf(tab1.id, tab2.id))).joinBlocking()
         store.waitUntilIdle()
@@ -110,11 +79,13 @@ class TabsRemovedMiddlewareTest {
         assertNull(store.state.findTab(tab1.id)?.engineState?.engineSession)
         assertNull(store.state.findTab(tab2.id)?.engineState?.engineSession)
         assertNotNull(store.state.findTab(tab3.id)?.engineState?.engineSession)
-        assertEquals(2, middleware.sessionsPendingDeletion.sessions.size)
+        verify(engineSession1).close()
+        verify(engineSession2).close()
+        verify(engineSession3, never()).close()
     }
 
     @Test
-    fun `unlinks engine session when all normal tabs are removed`() = runTestOnMain {
+    fun `closes and unlinks engine session when all normal tabs are removed`() = runTestOnMain {
         val middleware = TabsRemovedMiddleware(scope)
 
         val tab1 = createTab("https://www.mozilla.org", id = "1", private = false)
@@ -125,9 +96,9 @@ class TabsRemovedMiddlewareTest {
             middleware = listOf(middleware, ConsumeRemoveTabActionsMiddleware()),
         )
 
-        listOf(tab1, tab2, tab3).forEach {
-            linkEngineSession(store, it.id)
-        }
+        val engineSession1 = linkEngineSession(store, tab1.id)
+        val engineSession2 = linkEngineSession(store, tab2.id)
+        val engineSession3 = linkEngineSession(store, tab3.id)
 
         store.dispatch(TabListAction.RemoveAllNormalTabsAction).joinBlocking()
         store.waitUntilIdle()
@@ -136,11 +107,13 @@ class TabsRemovedMiddlewareTest {
         assertNull(store.state.findTab(tab1.id)?.engineState?.engineSession)
         assertNull(store.state.findTab(tab2.id)?.engineState?.engineSession)
         assertNotNull(store.state.findTab(tab3.id)?.engineState?.engineSession)
-        assertEquals(2, middleware.sessionsPendingDeletion.sessions.size)
+        verify(engineSession1).close()
+        verify(engineSession2).close()
+        verify(engineSession3, never()).close()
     }
 
     @Test
-    fun `unlinks engine session when all private tabs are removed`() = runTestOnMain {
+    fun `closes and unlinks engine session when all private tabs are removed`() = runTestOnMain {
         val middleware = TabsRemovedMiddleware(scope)
 
         val tab1 = createTab("https://www.mozilla.org", id = "1", private = true)
@@ -151,9 +124,9 @@ class TabsRemovedMiddlewareTest {
             middleware = listOf(middleware, ConsumeRemoveTabActionsMiddleware()),
         )
 
-        listOf(tab1, tab2, tab3).forEach {
-            linkEngineSession(store, it.id)
-        }
+        val engineSession1 = linkEngineSession(store, tab1.id)
+        val engineSession2 = linkEngineSession(store, tab2.id)
+        val engineSession3 = linkEngineSession(store, tab3.id)
 
         store.dispatch(TabListAction.RemoveAllPrivateTabsAction).joinBlocking()
         store.waitUntilIdle()
@@ -162,11 +135,13 @@ class TabsRemovedMiddlewareTest {
         assertNull(store.state.findTab(tab1.id)?.engineState?.engineSession)
         assertNull(store.state.findTab(tab2.id)?.engineState?.engineSession)
         assertNotNull(store.state.findTab(tab3.id)?.engineState?.engineSession)
-        assertEquals(2, middleware.sessionsPendingDeletion.sessions.size)
+        verify(engineSession1).close()
+        verify(engineSession2).close()
+        verify(engineSession3, never()).close()
     }
 
     @Test
-    fun `unlinks engine session when all tabs are removed`() = runTestOnMain {
+    fun `closes and unlinks engine session when all tabs are removed`() = runTestOnMain {
         val middleware = TabsRemovedMiddleware(scope)
 
         val tab1 = createTab("https://www.mozilla.org", id = "1", private = true)
@@ -177,9 +152,9 @@ class TabsRemovedMiddlewareTest {
             middleware = listOf(middleware, ConsumeRemoveTabActionsMiddleware()),
         )
 
-        listOf(tab1, tab2, tab3).forEach {
-            linkEngineSession(store, it.id)
-        }
+        val engineSession1 = linkEngineSession(store, tab1.id)
+        val engineSession2 = linkEngineSession(store, tab2.id)
+        val engineSession3 = linkEngineSession(store, tab3.id)
 
         store.dispatch(TabListAction.RemoveAllTabsAction()).joinBlocking()
         store.waitUntilIdle()
@@ -188,7 +163,9 @@ class TabsRemovedMiddlewareTest {
         assertNull(store.state.findTab(tab1.id)?.engineState?.engineSession)
         assertNull(store.state.findTab(tab2.id)?.engineState?.engineSession)
         assertNotNull(store.state.findCustomTab(tab3.id)?.engineState?.engineSession)
-        assertEquals(2, middleware.sessionsPendingDeletion.sessions.size)
+        verify(engineSession1).close()
+        verify(engineSession2).close()
+        verify(engineSession3, never()).close()
     }
 
     @Test
@@ -236,106 +213,6 @@ class TabsRemovedMiddlewareTest {
         verify(engineSession1).close()
         verify(engineSession2).close()
         verify(engineSession3, never()).close()
-    }
-
-    @Test
-    fun `closes engine session after tab receives final state update`() = runTestOnMain {
-        val middleware = TabsRemovedMiddleware(scope)
-
-        val tab1 = createTab("https://www.getpocket.com", id = "3")
-        val store = BrowserStore(
-            initialState = BrowserState(tabs = listOf(tab1)),
-            middleware = listOf(middleware, ConsumeRemoveTabActionsMiddleware()),
-        )
-
-        val engineSession1 = linkEngineSession(store, tab1.id)
-
-        val observerCaptor = argumentCaptor<EngineSession.Observer>()
-        store.dispatch(TabListAction.RemoveAllTabsAction()).joinBlocking()
-        store.waitUntilIdle()
-        dispatcher.scheduler.advanceUntilIdle()
-
-        verify(engineSession1).register(observerCaptor.capture())
-
-        assertEquals(1, middleware.sessionsPendingDeletion.sessions.size)
-
-        observerCaptor.value.onStateUpdated(mock())
-
-        store.waitUntilIdle()
-        dispatcher.scheduler.advanceUntilIdle()
-
-        verify(engineSession1).close()
-        assertTrue(middleware.sessionsPendingDeletion.sessions.isEmpty())
-    }
-
-    @Test
-    fun `close engine sessions pending deletion when processing ClearRecoverableTabs`() = runTestOnMain {
-        val middleware = TabsRemovedMiddleware(scope)
-
-        val tab1 = createTab("https://www.mozilla.org", id = "1", private = false)
-        val tab2 = createTab("https://www.firefox.com", id = "2", private = false)
-        val tab3 = createTab("https://www.getpocket.com", id = "3", private = true)
-        val store = BrowserStore(
-            initialState = BrowserState(tabs = listOf(tab1, tab2, tab3)),
-            middleware = listOf(middleware, ConsumeRemoveTabActionsMiddleware()),
-        )
-
-        val engineSession1 = linkEngineSession(store, tab1.id)
-        val engineSession2 = linkEngineSession(store, tab2.id)
-        val engineSession3 = linkEngineSession(store, tab3.id)
-
-        store.dispatch(TabListAction.RemoveAllNormalTabsAction).joinBlocking()
-        store.waitUntilIdle()
-        dispatcher.scheduler.advanceUntilIdle()
-
-        assertNull(store.state.findTab(tab1.id)?.engineState?.engineSession)
-        assertNull(store.state.findTab(tab2.id)?.engineState?.engineSession)
-        assertNotNull(store.state.findTab(tab3.id)?.engineState?.engineSession)
-        assertEquals(2, middleware.sessionsPendingDeletion.sessions.size)
-
-        store.dispatch(UndoAction.ClearRecoverableTabs("noop")).joinBlocking()
-        store.waitUntilIdle()
-        dispatcher.scheduler.advanceUntilIdle()
-
-        verify(engineSession1).close()
-        verify(engineSession2).close()
-        verify(engineSession3, never()).close()
-        assertTrue(middleware.sessionsPendingDeletion.sessions.isEmpty())
-    }
-
-    @Test
-    fun `close engine sessions pending deletion when processing RestoreRecoverableTabs`() = runTestOnMain {
-        val middleware = TabsRemovedMiddleware(scope)
-
-        val tab1 = createTab("https://www.mozilla.org", id = "1", private = false)
-        val tab2 = createTab("https://www.firefox.com", id = "2", private = false)
-        val tab3 = createTab("https://www.getpocket.com", id = "3", private = true)
-        val store = BrowserStore(
-            initialState = BrowserState(tabs = listOf(tab1, tab2, tab3)),
-            middleware = listOf(middleware, ConsumeRemoveTabActionsMiddleware()),
-        )
-
-        val engineSession1 = linkEngineSession(store, tab1.id)
-        val engineSession2 = linkEngineSession(store, tab2.id)
-        val engineSession3 = linkEngineSession(store, tab3.id)
-
-        store.dispatch(TabListAction.RemoveAllNormalTabsAction).joinBlocking()
-        store.waitUntilIdle()
-        dispatcher.scheduler.advanceUntilIdle()
-
-        assertNull(store.state.findTab(tab1.id)?.engineState?.engineSession)
-        assertNull(store.state.findTab(tab2.id)?.engineState?.engineSession)
-        assertNotNull(store.state.findTab(tab3.id)?.engineState?.engineSession)
-        assertEquals(2, middleware.sessionsPendingDeletion.sessions.size)
-
-        store.dispatch(UndoAction.RestoreRecoverableTabs).joinBlocking()
-        store.waitUntilIdle()
-        dispatcher.scheduler.advanceUntilIdle()
-
-        verify(engineSession1).close()
-        verify(engineSession2).close()
-        verify(engineSession3, never()).close()
-        assertTrue(middleware.sessionsPendingDeletion.sessions.isEmpty())
     }
 
     private fun linkEngineSession(store: BrowserStore, tabId: String): EngineSession {
